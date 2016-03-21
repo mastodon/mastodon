02696a063eabec1715c4d0b359744fef10049486
Eugen Rochko <eugen@zeonfederated.com>
2016-03-21 17:18:30 +0100

    Change default avatar, redirect to home after sign up

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -17,6 +17,6 @@ class Auth::RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(resource)
-    account_path(resource.account)
+    root_path
   end
 end
