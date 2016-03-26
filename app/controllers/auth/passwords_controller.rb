43bb8a25e702badd4a98934e0e28e65190a1b7cb
Eugen Rochko <eugen@zeonfederated.com>
2016-03-26 01:14:37 +0100

    Remember user logins by default

--- app/controllers/auth/passwords_controller.rb
+++ app/controllers/auth/passwords_controller.rb
@@ -1,34 +1,3 @@
 class Auth::PasswordsController < Devise::PasswordsController
   layout 'auth'
-
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
-
-  # POST /resource/password
-  # def create
-  #   super
-  # end
-
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
-
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
 end
--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,27 +1,8 @@
 class Auth::SessionsController < Devise::SessionsController
   layout 'auth'
 
-  # before_filter :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
-  # end
+  def create
+    params[:user].merge!(remember_me: 1)
+    super
+  end
 end
