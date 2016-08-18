8459acd1234e9efcbf106e3eaa00ca871717d386
Eugen Rochko <eugen@zeonfederated.com>
2016-08-18 17:48:57 +0200

    Fix for force SSL issue with websockets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  force_ssl if: "ENV['LOCAL_HTTPS'] == 'true'"
+
   # Profiling
   before_action do
     if (current_user && current_user.admin?) || Rails.env == 'development'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = ENV['LOCAL_HTTPS'] == 'true'
+  config.force_ssl = false
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
