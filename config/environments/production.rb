413e700fe027ed7a4fdac325a9369d1481d52458
Eugen Rochko <eugen@zeonfederated.com>
2016-09-05 01:26:08 +0200

    Enhancing test suite but I think the problem might have been caching setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
--- spec/controllers/api/statuses_controller_spec.rb
+++ spec/controllers/api/statuses_controller_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Api::StatusesController, type: :controller do
+  render_views
+
   let(:user)  { Fabricate(:user, account: Fabricate(:account, username: 'alice')) }
   let(:token) { double acceptable?: true, resource_owner_id: user.id }
 
   before do
+    stub_request(:post, "https://pubsubhubbub.superfeedr.com/").to_return(:status => 200, :body => "", :headers => {})
     allow(controller).to receive(:doorkeeper_token) { token }
   end
 
@@ -18,22 +21,82 @@ RSpec.describe Api::StatusesController, type: :controller do
   end
 
   describe 'GET #home' do
-    it 'returns http success'
+    it 'returns http success' do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
   end
 
   describe 'GET #mentions' do
-    it 'returns http success'
+    it 'returns http success' do
+      get :mentions
+      expect(response).to have_http_status(:success)
+    end
   end
 
   describe 'POST #create' do
-    it 'returns http success'
+    before do
+      post :create, params: { status: 'Hello world' }
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
   end
 
   describe 'POST #reblog' do
-    it 'returns http success'
+    let(:status) { Fabricate(:status, account: user.account) }
+
+    before do
+      post :reblog, params: { id: status.id }
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'updates the reblogs count' do
+      expect(status.reblogs_count).to eq 1
+    end
+
+    it 'updates the reblogged attribute' do
+      expect(user.account.reblogged?(status)).to be true
+    end
+
+    it 'return json with updated attributes' do
+      hash_body = JSON.parse(response.body).with_indifferent_access
+
+      expect(hash_body[:reblog][:id]).to eq status.id
+      expect(hash_body[:reblog][:reblogs_count]).to eq 1
+      expect(hash_body[:reblog][:reblogged]).to be true
+    end
   end
 
   describe 'POST #favourite' do
-    it 'returns http success'
+    let(:status) { Fabricate(:status, account: user.account) }
+
+    before do
+      post :favourite, params: { id: status.id }
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'updates the favourites count' do
+      expect(status.favourites_count).to eq 1
+    end
+
+    it 'updates the favourited attribute' do
+      expect(user.account.favourited?(status)).to be true
+    end
+
+    it 'return json with updated attributes' do
+      hash_body = JSON.parse(response.body).with_indifferent_access
+
+      expect(hash_body[:id]).to eq status.id
+      expect(hash_body[:favourites_count]).to eq 1
+      expect(hash_body[:favourited]).to be true
+    end
   end
 end
