1215ab57ff921a4ae72a26f6c033240354bf31fd
Eugen Rochko <eugen@zeonfederated.com>
2016-09-24 14:59:52 +0200

    Some extra scopes for user sorting

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
 
   has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
 
+  scope :prolific, -> { joins('inner join statuses on statuses.account_id = users.account_id').select('users.*, count(statuses.id) as statuses_count').group('users.id').order('statuses_count desc') }
+  scope :recent,   -> { order('created_at desc') }
+
   def admin?
     self.admin
   end
