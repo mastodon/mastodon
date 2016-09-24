3b56350121998abb249f14a45c656c2655d6344e
Eugen Rochko <eugen@zeonfederated.com>
2016-09-24 13:53:54 +0200

    Fix #6 - Rate limit GET reqs to 300/5min, POST to 100/5min

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,5 +1,9 @@
 class Rack::Attack
-  throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.ip
+  throttle('get-req/ip', limit: 300, period: 5.minutes) do |req|
+    req.ip if req.get?
+  end
+
+  throttle('post-req/ip', limit: 100, period: 5.minutes) do |req|
+    req.ip if req.post?
   end
 end
