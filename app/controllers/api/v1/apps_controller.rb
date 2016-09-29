62b057b085ea7ff20765731d64a79e38f19174d4
Eugen Rochko <eugen@zeonfederated.com>
2016-09-30 00:03:08 +0200

    Adjust client registration API

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -2,12 +2,6 @@ class Api::V1::AppsController < ApiController
   respond_to :json
 
   def create
-    @app = Doorkeeper::Application.create!(app_params)
-  end
-
-  private
-
-  def app_params
-    params.permit(:name, :redirect_uri)
+    @app = Doorkeeper::Application.create!(name: params[:client_name], redirect_uri: params[:redirect_uris])
   end
 end
