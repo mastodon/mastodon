492224b93fc11f1c0bc127ede8376bf243925caa
Eugen Rochko <eugen@zeonfederated.com>
2016-09-30 22:40:31 +0200

    Allow non-https redirect URIs for OAuth apps (AndStatus seems to require this)

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -76,7 +76,7 @@ Doorkeeper.configure do
   # by default in non-development environments). OAuth2 delegates security in
   # communication to the HTTPS protocol so it is wise to keep this enabled.
   #
-  # force_ssl_in_redirect_uri !Rails.env.development?
+  force_ssl_in_redirect_uri false
 
   # Specify what grant flows are enabled in array of Strings. The valid
   # strings and the flows they enable are:
