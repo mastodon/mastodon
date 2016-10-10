d7c55853e9d180290ae3cb30b2f329ebab8748ce
Kai Schaper <303@posteo.de>
2016-10-10 04:01:10 +0200

    set up Mocha/Chai/Enzyme for React component unit testing

---
+++ spec/javascript/components/loading_indicator.test.jsx
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+global.React = React;
+
+import LoadingIndicator from '../../../app/assets/javascripts/components/components/loading_indicator'
+
+describe('<LoadingIndicator />', function() {
+  it('renders text that indicates loading', function() {
+    const wrapper = shallow(<LoadingIndicator />);
+    expect(wrapper.text()).to.match(/loading/i);
+  });
+});
