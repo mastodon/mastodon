4eeb7947bd9209ec137a032cf66c47041c27c07e
Kai Schaper <303@posteo.de>
2016-10-11 04:07:32 +0200

    add stylesheet; add story for Button component

--- storybook/config.js
+++ storybook/config.js
@@ -2,13 +2,17 @@ import { configure } from '@kadira/storybook';
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
+import './storybook.css'
+// for now just simply $ rake asset:precompile && mv public/assets/application-ebf... storybook/application.css
+import './application.css'
+
 window.storiesOf = storiesOf;
 window.action = action;
 window.React = React;
 
 function loadStories () {
   require('./stories/loading_indicator.story.jsx');
-  // You can require as many stories as you need.
+  require('./stories/button.story.jsx');
 }
 
 configure(loadStories, module);
---
+++ storybook/stories/button.story.jsx
@@ -0,0 +1,15 @@
+import Button from '../../app/assets/javascripts/components/components/button.jsx'
+
+storiesOf('Button', module)
+  .add('default state', () => (
+    <Button text="submit" onClick={action('clicked')} />
+  ))
+  .add('secondary', () => (
+    <Button secondary text="submit" onClick={action('clicked')} />
+  ))
+  .add('disabled', () => (
+    <Button disabled text="submit" onClick={action('clicked')} />
+  ))
+  .add('block', () => (
+    <Button block text="submit" onClick={action('clicked')} />
+  ));
