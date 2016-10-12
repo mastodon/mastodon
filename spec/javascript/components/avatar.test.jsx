cbfc12044dda7cb6ee48e18fe3e769de2c1c0ac1
Kai Schaper <303@posteo.de>
2016-10-12 18:18:55 +0200

    actually use const

--- spec/javascript/components/avatar.test.jsx
+++ spec/javascript/components/avatar.test.jsx
@@ -6,7 +6,7 @@ import Avatar from '../../../app/assets/javascripts/components/components/avatar
 describe('<Avatar />', () => {
   const src = '/path/to/image.jpg';
   const size = 100;
-  const wrapper = render(<Avatar src={src} size={100} />);
+  const wrapper = render(<Avatar src={src} size={size} />);
 
   it('renders an img element with the given src', () => {
     expect(wrapper.find('img')).to.have.attr('src', `${src}`);
