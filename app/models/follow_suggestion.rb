edefd87adfadd6c1d7f8087459406464815a5fd0
Eugen Rochko <eugen@zeonfederated.com>
2016-10-15 12:58:34 +0200

    Fix multiple results return from FollowSuggestion.get

--- app/models/follow_suggestion.rb
+++ app/models/follow_suggestion.rb
@@ -1,8 +1,8 @@
 class FollowSuggestion
-  def self.get(for_account_id)
+  def self.get(for_account_id, limit = 6)
     neo = Neography::Rest.new
     account_ids = neo.execute_query('START a=node:account_index(Account={id}) MATCH (a)-[:follows]->(b)-[:follows]->(c) WHERE a <> c AND NOT (a)-[:follows]->(c) RETURN DISTINCT c.account_id', id: for_account_id)
-    Account.where(id: account_ids['data'].first) unless account_ids.empty?
+    Account.where(id: account_ids['data'].map(&:first)).limit(limit) unless account_ids.empty?
   rescue Neography::NeographyError, Excon::Error::Socket => e
     Rails.logger.error e
     []
