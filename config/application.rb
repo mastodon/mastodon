a59d10d3df21befac6da57d92aed3087b7c10d6a
Eugen Rochko <eugen@zeonfederated.com>
2016-11-02 13:12:18 +0100

    Fix missing default headers

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,9 @@ module Mastodon
     end
 
     config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'DENY'
+      'X-Frame-Options' => 'DENY',
+      'X-Content-Type-Options' => 'nosniff',
+      'X-XSS-Protection' => '1; mode=block'
     }
   end
 end
