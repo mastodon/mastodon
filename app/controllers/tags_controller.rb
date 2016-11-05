a698b767c175e7ca59122da11412660a22519067
Eugen Rochko <eugen@zeonfederated.com>
2016-11-05 17:44:14 +0100

    Fix linking of remote hashtags in UI, add public view of hashtags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,7 @@
 class TagsController < ApplicationController
+  layout 'public'
+
   def show
+    @statuses = Tag.find_by!(name: params[:id].downcase).statuses.order('id desc').with_includes.with_counters.paginate(page: params[:page], per_page: 10)
   end
 end
