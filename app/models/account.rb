f5c6baf29dde4d89c1c4611ac11b1da939523f06
Eugen Rochko <eugen@zeonfederated.com>
2016-11-07 17:35:25 +0100

    Fix account and hashtag regex

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
   include Targetable
 
-  MENTION_RE = /(?:^|[\s\.>*+])@([a-z0-9_]+(?:@[a-z0-9\.\-]+)?)/i
+  MENTION_RE = /(?:^|[^\/\w])@([a-z0-9_]+(?:@[a-z0-9\.\-]+)?)/i
   IMAGE_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif'].freeze
 
   # Local users
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :statuses
 
-  HASHTAG_RE = /[?:^|\s|\.|>]#([[:word:]_]+)/i
+  HASHTAG_RE = /(?:^|[^\/\w])#([[:word:]_]+)/i
 
   validates :name, presence: true, uniqueness: true
 
