295fc0e612904637a6e34d167d5a282ba33a6854
June Rhodes <jrhodes@redpointgames.com.au>
2016-11-27 13:39:46 +1100

    Add dev_start.sh to let people get a development environment started quickly

---
+++ dev_start.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+# Environment variables
+export REDIS_HOST=localhost
+export REDIS_PORT=6379
+export DB_HOST=localhost
+export DB_USER=postgres
+export DB_DEV_NAME=postgres
+export DB_PASS=postgres
+export DB_PORT=5432
+
+# Federation
+export LOCAL_DOMAIN=localhost
+export LOCAL_HTTPS=false
+
+# Application secrets
+if [ ! -f .secret.paperclip ]; then
+  echo "$(rake secret)" > .secret.paperclip
+fi
+if [ ! -f .secret.keybase ]; then
+  echo "$(rake secret)" > .secret.keybase
+fi
+export PAPERCLIP_SECRET=$(<.secret.paperclip)
+export SECRET_KEY_BASE=$(<.secret.keybase)
+
+# E-mail configuration
+export SMTP_SERVER=smtp.mailgun.org
+export SMTP_PORT=587
+export SMTP_LOGIN=
+export SMTP_PASSWORD=
+export SMTP_FROM_ADDRESS=notifications@example.com
+
+# Set the rails environment to development.
+export RAILS_ENV=development
+
+# Install dependencies
+#bundle install
+
+# Install Node.js dependencies
+if [ ! -d node_modules ]; then
+  yarn
+fi
+
+# Upgrade database
+rails db:migrate
+
+# Compile assets; if you set RAILS_ENV to production, you 
+# need to uncomment this line.
+#rails assets:precompile
+
+# Run web server
+rails server
