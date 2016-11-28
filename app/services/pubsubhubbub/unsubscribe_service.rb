7e5e33df48e507e18a226edc8dfecbf554d38647
Eugen Rochko <eugen@zeonfederated.com>
2016-11-28 18:14:49 +0100

    Fix Pubsubhubbub::UnsubscribeService

--- app/services/pubsubhubbub/unsubscribe_service.rb
+++ app/services/pubsubhubbub/unsubscribe_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Pubsubhubbub::SubscribeService < BaseService
+class Pubsubhubbub::UnsubscribeService < BaseService
   def call(account, callback)
     return ['Invalid topic URL', 422] if account.nil?
 
