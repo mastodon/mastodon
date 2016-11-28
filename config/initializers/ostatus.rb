41ef277da39cc70b15d11519edbbdac82511f2a2
Eugen <eugen@zeonfederated.com>
2016-11-28 21:21:05 +0100

    Fix URLs in ApplicationController.renderer

--- config/initializers/ostatus.rb
+++ config/initializers/ostatus.rb
@@ -1,14 +1,21 @@
-port = ENV.fetch('PORT') { 3000 }
-
+port  = ENV.fetch('PORT') { 3000 }
+host  = ENV.fetch('LOCAL_DOMAIN') { "localhost:#{port}" }
+https = ENV['LOCAL_HTTPS'] == 'true'
+  
 Rails.application.configure do
-  config.x.local_domain = ENV.fetch('LOCAL_DOMAIN') { "localhost:#{port}" }
-  config.x.hub_url      = ENV.fetch('HUB_URL')      { 'https://pubsubhubbub.superfeedr.com' }
-  config.x.use_https    = ENV['LOCAL_HTTPS'] == 'true'
+  config.x.local_domain = host
+  config.x.hub_url      = ENV.fetch('HUB_URL') { 'https://pubsubhubbub.superfeedr.com' }
+  config.x.use_https    = https
   config.x.use_s3       = ENV['S3_ENABLED'] == 'true'
 
-  config.action_mailer.default_url_options = { host: config.x.local_domain, protocol: config.x.use_https ? 'https://' : 'http://', trailing_slash: false }
+  config.action_mailer.default_url_options = { host: host, protocol: https ? 'https://' : 'http://', trailing_slash: false }
 
   if Rails.env.production?
-    config.action_cable.allowed_request_origins = ["http#{config.x.use_https ? 's' : ''}://#{config.x.local_domain}"]
+    config.action_cable.allowed_request_origins = ["http#{https ? 's' : ''}://#{host}"]
   end
 end
+
+ApplicationController.renderer.defaults.merge!(
+  http_host: host,
+  https: https
+)
