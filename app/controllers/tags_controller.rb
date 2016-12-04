d236dcded20b1a561b1f396fbb423811f27c77e5
Eugen Rochko <eugen@zeonfederated.com>
2016-12-04 16:56:45 +0100

    Fix public tags page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,7 +4,8 @@ class TagsController < ApplicationController
   layout 'public'
 
   def show
-    @statuses = Tag.find_by!(name: params[:id].downcase).statuses.order('id desc').paginate_by_max_id(20, params[:max_id] || nil)
-  	@statuses = cache_collection(@statuses, Status)
+    @tag      = Tag.find_by!(name: params[:id].downcase)
+    @statuses = @tag.statuses.order('id desc').paginate_by_max_id(20, params[:max_id])
+    @statuses = cache_collection(@statuses, Status)
   end
 end
