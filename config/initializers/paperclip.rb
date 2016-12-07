8b93f45f3d8d6741a67e884e28b540cae197e45e
Eugen Rochko <eugen@zeonfederated.com>
2016-12-07 17:19:29 +0100

    Fix paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,18 +4,13 @@ if ENV['S3_ENABLED'] == 'true'
   Aws.eager_autoload!(services: %w(S3))
 
   Paperclip::Attachment.default_options[:storage]        = :s3
-  Paperclip::Attachment.default_options[:s3_protocol]    = ':https'
+  Paperclip::Attachment.default_options[:s3_protocol]    = 'https'
   Paperclip::Attachment.default_options[:url]            = ':s3_domain_url'
   Paperclip::Attachment.default_options[:s3_host_name]   = ENV.fetch('S3_HOSTNAME') { "s3-#{ENV.fetch('S3_REGION')}.amazonaws.com" }
   Paperclip::Attachment.default_options[:path]           = '/:class/:attachment/:id_partition/:style/:filename'
   Paperclip::Attachment.default_options[:s3_headers]     = { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
   Paperclip::Attachment.default_options[:s3_permissions] = 'public'
   Paperclip::Attachment.default_options[:s3_region]      = ENV.fetch('S3_REGION') { 'us-east-1' }
-
-  unless ENV['S3_CLOUDFRONT_HOST'].blank?
-    Paperclip::Attachment.default_options[:url]           = ':s3_alias_url'
-    Paperclip::Attachment.default_options[:s3_host_alias] = ENV['S3_CLOUDFRONT_HOST']
-  end
 
   Paperclip::Attachment.default_options[:s3_credentials] = {
     bucket: ENV.fetch('S3_BUCKET'),
@@ -30,5 +25,10 @@ if ENV['S3_ENABLED'] == 'true'
     }
 
     Paperclip::Attachment.default_options[:url] = ':s3_path_url'
+  end
+
+  unless ENV['S3_CLOUDFRONT_HOST'].blank?
+    Paperclip::Attachment.default_options[:url]           = ':s3_alias_url'
+    Paperclip::Attachment.default_options[:s3_host_alias] = ENV['S3_CLOUDFRONT_HOST']
   end
 end
