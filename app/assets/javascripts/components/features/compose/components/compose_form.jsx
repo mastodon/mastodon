8c6e331c1f42378f54b414b97e32fcf339e16632
Anthony Bellew <anthonyreflected@gmail.com>
2016-12-23 07:48:41 -0700

    Update compose_form.jsx

--- app/assets/javascripts/components/features/compose/components/compose_form.jsx
+++ app/assets/javascripts/components/features/compose/components/compose_form.jsx
@@ -130,7 +130,8 @@ const ComposeForm = React.createClass({
           <Toggle checked={this.props.unlisted} onChange={this.handleChangeListability} />
           <span style={{ display: 'inline-block', verticalAlign: 'middle', marginBottom: '14px', marginLeft: '8px', color: '#9baec8' }}><FormattedMessage id='compose_form.unlisted' defaultMessage='Do not display in public timeline' /></span>
         </label>
-        <FormattedMessage id='compose_form.unlisted_caveat' defaultMessage='(Private posts will never display on public timeline.)' />
+
+        <span style={{ display: 'block', verticalAlign: 'middle', marginTop: '10px', marginBottom: '10px', marginLeft: '8px', color: '#9baec8' }}><FormattedMessage id='compose_form.unlisted_caveat' defaultMessage='(Private posts will never display in public timeline.)' /></span>
 
         <label style={{ display: 'block', lineHeight: '24px', verticalAlign: 'middle' }}>
           <Toggle checked={this.props.sensitive} onChange={this.handleChangeSensitivity} />
