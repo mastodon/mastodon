136e18b875f1743a5aced6f0a07cd949f2f19a5a
Eugen Rochko <eugen@zeonfederated.com>
2017-01-02 14:19:02 +0100

    Unblocks also federate

--- app/services/unblock_service.rb
+++ app/services/unblock_service.rb
@@ -2,6 +2,9 @@
 
 class UnblockService < BaseService
   def call(account, target_account)
-    account.unblock!(target_account) if account.blocking?(target_account)
+    return unless account.blocking?(target_account)
+
+    unblock = account.unblock!(target_account)
+    NotificationWorker.perform_async(unblock.stream_entry.id, target_account.id) unless target_account.local?
   end
 end
