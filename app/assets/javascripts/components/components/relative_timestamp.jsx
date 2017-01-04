98b83aca372fabdfc32b05c1eb72c80a79102e53
Eugen Rochko <eugen@zeonfederated.com>
2017-01-04 15:43:28 +0100

    Fix #391 - relative timestamps now contain an exact datetime in title

--- app/assets/javascripts/components/components/relative_timestamp.jsx
+++ app/assets/javascripts/components/components/relative_timestamp.jsx
@@ -1,15 +1,18 @@
-import {
-  FormattedMessage,
-  FormattedDate,
-  FormattedRelative
-} from 'react-intl';
+import { injectIntl, FormattedRelative } from 'react-intl';
 
-const RelativeTimestamp = ({ timestamp }) => {
-  return <FormattedRelative value={new Date(timestamp)} />;
+const RelativeTimestamp = ({ intl, timestamp }) => {
+  const date = new Date(timestamp);
+
+  return (
+    <time dateTime={timestamp} title={intl.formatDate(date, { hour12: false, year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' })}>
+      <FormattedRelative value={date} />
+    </time>
+  );
 };
 
 RelativeTimestamp.propTypes = {
+  intl: React.PropTypes.object.isRequired,
   timestamp: React.PropTypes.string.isRequired
 };
 
-export default RelativeTimestamp;
+export default injectIntl(RelativeTimestamp);
