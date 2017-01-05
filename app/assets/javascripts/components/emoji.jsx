c318e6e42e2903af90be205760a5a2ba00e378b8
Misty De Meo <mistydemeo@gmail.com>
2017-01-04 22:23:02 -0800

    Display native emoji on browsers which support it

--- app/assets/javascripts/components/emoji.jsx
+++ app/assets/javascripts/components/emoji.jsx
@@ -1,9 +1,18 @@
 import emojione from 'emojione';
+import detectVersion from 'mojibaka';
 
 emojione.imageType    = 'png';
 emojione.sprites      = false;
 emojione.imagePathPNG = '/emoji/';
 
+let emoji_version = detectVersion();
+
 export default function emojify(text) {
-  return emojione.toImage(text);
+  // Browser too old to support native emoji
+  if (emoji_version < 6.1) {
+    return emojione.toImage(text);
+  // Convert short codes into native emoji
+  } else {
+    return emojione.shortnameToUnicode(text);
+  }
 };
