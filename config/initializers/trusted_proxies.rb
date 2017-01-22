956da43e19e484f94df91f715e4a88794a25dbec
Eugen <eugen@zeonfederated.com>
2017-01-22 23:07:31 +0100

    Fix error

--- config/initializers/trusted_proxies.rb
+++ config/initializers/trusted_proxies.rb
@@ -1,7 +1,11 @@
 module Rack
   class Request
     def trusted_proxy?(ip)
-      Rails.application.config.action_dispatch.trusted_proxies.any? { |proxy| proxy === ip }
+      if Rails.application.config.action_dispatch.trusted_proxies.nil?
+        super
+      else
+        Rails.application.config.action_dispatch.trusted_proxies.any? { |proxy| proxy === ip }
+      end
     end
   end
 end
