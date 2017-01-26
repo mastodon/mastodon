7329fbd8a453bbd2fcbec8bd63b1390bfe7bc7b8
Eugen Rochko <eugen@zeonfederated.com>
2017-01-26 18:48:56 +0100

    Fix up timeout, improve contrast on "show more", add responsive style for extremely wide monitors

--- app/assets/javascripts/components/actions/notifications.jsx
+++ app/assets/javascripts/components/actions/notifications.jsx
@@ -40,7 +40,7 @@ export function updateNotifications(notification, intlMessages, intlLocale) {
     // Desktop notifications
     if (typeof window.Notification !== 'undefined' && showAlert) {
       const title = new IntlMessageFormat(intlMessages[`notification.${notification.type}`], intlLocale).format({ name: notification.account.display_name.length > 0 ? notification.account.display_name : notification.account.username });
-      const body  = $('<p>').html(notification.status ? notification.status.content : '').text();
+      const body  = (notification.status && notification.status.spoiler_text.length > 0) ? notification.status.spoiler_text : $('<p>').html(notification.status ? notification.status.content : '').text();
 
       new Notification(title, { body, icon: notification.account.avatar, tag: notification.id });
     }
