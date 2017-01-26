88c6f6f2dcf1e756df28155838720d60ce89a48d
Decky Coss <coss@cosstropolis.com>
2017-01-26 12:49:28 -0700

    Issue #423: add select/deselect all buttons to domains checklist

--- app/assets/javascripts/application_public.js
+++ app/assets/javascripts/application_public.js
@@ -3,6 +3,23 @@
 //= require extras
 //= require best_in_place
 
+function blurAfterDelay(el){
+  setTimeout(function(_el){
+    $(_el).blur();
+  }, 100, el);
+}
+
 $(function () {
   $(".best_in_place").best_in_place();
+
+  // settings/blocks extras
+
+  $('.checklist_outer #select_all').on('click', function() {
+    blurAfterDelay(this);
+    $(".checklist_outer .checklist_item input[type='checkbox']").prop("checked", true);
+  });
+  $('.checklist_outer #deselect_all').on('click', function() {
+    blurAfterDelay(this);
+    $(".checklist_outer .checklist_item input[type='checkbox']").prop("checked", false);
+  });
 });
