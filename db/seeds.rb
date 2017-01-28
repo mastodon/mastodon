1992575d577bf5b4c408a0104ad310e17846466a
Eugen Rochko <eugen@zeonfederated.com>
2017-01-29 00:12:27 +0100

    Fix #569 - seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ web_app.save!
 
 if Rails.env.development?
   domain = ENV['LOCAL_DOMAIN'] || Rails.configuration.x.local_domain
-  admin  = Account.where(username: 'admin').first_or_initialize(username: 'admin').save!
+  admin  = Account.where(username: 'admin').first_or_initialize(username: 'admin')
+  admin.save!
   User.where(email: "admin@#{domain}").first_or_initialize(email: "admin@#{domain}", password: 'mastodonadmin', password_confirmation: 'mastodonadmin', confirmed_at: Time.now.utc, admin: true, account: admin).save!
 end
