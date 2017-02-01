1da0ce5c7cf6a5b90e28a810c8b3ef4003239383
Eugen Rochko <eugen@zeonfederated.com>
2017-02-01 12:13:16 +0100

    Looks like log level :debug is required for activerecord times to be tracked

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
