b21f7c28f6832817d5de616ab0c4c2d3c28d90b0
Eugen Rochko <eugen@zeonfederated.com>
2017-04-04 13:02:49 +0200

    Move OStatus processing back into default queue

--- app/workers/processing_worker.rb
+++ app/workers/processing_worker.rb
@@ -3,7 +3,7 @@
 class ProcessingWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: 'pull', backtrace: true
+  sidekiq_options backtrace: true
 
   def perform(account_id, body)
     ProcessFeedService.new.call(body, Account.find(account_id))
