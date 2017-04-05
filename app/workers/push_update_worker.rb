96ef9338208e09cbc52a49a3d7171d877eab3c43
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-04 20:36:03 -0700

    Replacing the broadcast method with the one defined in the feed manager

--- app/workers/push_update_worker.rb
+++ app/workers/push_update_worker.rb
@@ -8,6 +8,6 @@ class PushUpdateWorker
     status = Status.find(status_id)
     message = inline_render(account, 'api/v1/statuses/show', status)
 
-    broadcast(account_id, type: 'update', timeline: timeline, message: message)
+    ActionCable.server.broadcast("timeline:#{account_id}", type: 'update', timeline: timeline, message: message)
   end
 end
