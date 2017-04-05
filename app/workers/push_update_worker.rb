0069c01285d7fc6b97220fd678f6e5b82f301b1a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-04 20:39:14 -0700

    Moving the queue_at into the worker

--- app/workers/push_update_worker.rb
+++ app/workers/push_update_worker.rb
@@ -7,7 +7,7 @@ class PushUpdateWorker
     account = Account.find(account_id)
     status = Status.find(status_id)
     message = inline_render(account, 'api/v1/statuses/show', status)
-
+    queue_at = (Time.now.to_f * 1000.0).to_i
     ActionCable.server.broadcast("timeline:#{account_id}", type: 'update', timeline: timeline, message: message)
   end
 end
