cfe91ac984fcabcc1980e88367dd636f0a8cc799
Eugen Rochko <eugen@zeonfederated.com>
2017-04-05 13:32:57 +0200

    Add index on mentions status_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403172249) do
+ActiveRecord::Schema.define(version: 20170405112956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20170403172249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id", "status_id"], name: "index_mentions_on_account_id_and_status_id", unique: true, using: :btree
+    t.index ["status_id"], name: "index_mentions_on_status_id", using: :btree
   end
 
   create_table "mutes", force: :cascade do |t|
