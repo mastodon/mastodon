9638894233d31368733574217e4d173e4cd5d13c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-04 20:51:18 -0700

    Moving in the inline render

--- app/workers/push_update_worker.rb
+++ app/workers/push_update_worker.rb
@@ -6,8 +6,14 @@ class PushUpdateWorker
   def perform(timeline, account_id, status_id)
     account = Account.find(account_id)
     status = Status.find(status_id)
-    message = inline_render(account, 'api/v1/statuses/show', status)
+    message = Rabl::Renderer.new(
+      'api/v1/statuses/show', 
+      status, 
+      view_path: 'app/views', 
+      format: :json, 
+      scope: InlineRablScope.new(account)
+    )
 
-    ActionCable.server.broadcast("timeline:#{account_id}", type: 'update', timeline: timeline, message: message)
+    ActionCable.server.broadcast("timeline:#{account_id}", type: 'update', timeline: timeline, message: message.render)
   end
 end
