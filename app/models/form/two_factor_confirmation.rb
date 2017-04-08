5e04e32ad76d1d36bc176c78b34021de6e088fc8
Eugen Rochko <eugen@zeonfederated.com>
2017-04-08 22:19:40 +0200

    Clean up

--- app/models/form/two_factor_confirmation.rb
+++ app/models/form/two_factor_confirmation.rb
@@ -4,10 +4,4 @@ class Form::TwoFactorConfirmation
   include ActiveModel::Model
 
   attr_accessor :code
-
-  def self.from_hash(hash)
-    obj = new
-    hash.each { |key, value| obj.send("#{key}=", value) }
-    obj
-  end
 end
