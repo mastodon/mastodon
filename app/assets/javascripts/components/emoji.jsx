155aba9265ade0dba9a4b61cebc419b73d819055
Kane York <rikingcoding@gmail.com>
2017-04-08 00:13:43 -0700

    move comment, fix variable names

--- app/assets/javascripts/components/emoji.jsx
+++ app/assets/javascripts/components/emoji.jsx
@@ -2,7 +2,8 @@ import emojione from 'emojione';
 
 const toImage = str => shortnameToImage(unicodeToImage(str));
 
-const blacklistedHex = {"00ae": true, "00a9": true, "2122": true};
+// Blocked from using iages - these look better as text than as images
+const useTextList = {"00ae": true, "00a9": true, "2122": true};
 
 const unicodeToImage = str => {
   const mappedUnicode = emojione.mapUnicodeToShort();
@@ -15,11 +16,10 @@ const unicodeToImage = str => {
     const unicode  = emojione.jsEscapeMap[unicodeChar];
     const short    = mappedUnicode[unicode];
     const filename = emojione.emojioneList[short].fname;
-    // Blacklisted - these have better font support than images
-    if (blacklistedHex[filename])
-      return unicodeChar;
-    }
     const alt      = emojione.convert(unicode.toUpperCase());
+    if (useTextList[filename])
+      return alt;
+    }
 
     return `<img draggable="false" class="emojione" alt="${alt}" src="/emoji/${filename}.svg" />`;
   });
@@ -32,7 +32,7 @@ const shortnameToImage = str => str.replace(emojione.regShortNames, shortname =>
 
   const unicode = emojione.emojioneList[shortname].unicode[emojione.emojioneList[shortname].unicode.length - 1];
   const alt     = emojione.convert(unicode.toUpperCase());
-  if (blacklistedHex[unicode]) {
+  if (useTextList[unicode]) {
     return alt;
   }
 
