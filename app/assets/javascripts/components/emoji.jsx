92686965ef4e9c099028af42b4405ac807cfffd5
Kane York <rikingcoding@gmail.com>
2017-04-08 00:04:03 -0700

    Blacklist :tm:, :copyright: from emojification

--- app/assets/javascripts/components/emoji.jsx
+++ app/assets/javascripts/components/emoji.jsx
@@ -2,6 +2,8 @@ import emojione from 'emojione';
 
 const toImage = str => shortnameToImage(unicodeToImage(str));
 
+const blacklistedHex = {"00ae": true, "00a9": true, "2122": true};
+
 const unicodeToImage = str => {
   const mappedUnicode = emojione.mapUnicodeToShort();
 
@@ -13,6 +15,10 @@ const unicodeToImage = str => {
     const unicode  = emojione.jsEscapeMap[unicodeChar];
     const short    = mappedUnicode[unicode];
     const filename = emojione.emojioneList[short].fname;
+    // Blacklisted - these have better font support than images
+    if (blacklistedHex[filename])
+      return unicodeChar;
+    }
     const alt      = emojione.convert(unicode.toUpperCase());
 
     return `<img draggable="false" class="emojione" alt="${alt}" src="/emoji/${filename}.svg" />`;
@@ -26,6 +32,9 @@ const shortnameToImage = str => str.replace(emojione.regShortNames, shortname =>
 
   const unicode = emojione.emojioneList[shortname].unicode[emojione.emojioneList[shortname].unicode.length - 1];
   const alt     = emojione.convert(unicode.toUpperCase());
+  if (blacklistedHex[unicode]) {
+    return alt;
+  }
 
   return `<img draggable="false" class="emojione" alt="${alt}" src="/emoji/${unicode}.svg" />`;
 });
