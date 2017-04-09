71706f21c28f5ae623ee69810fe26a34fb79b446
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-09 08:39:41 -0400

    Ignore implied formats for catch all route requests (#1340)
    A request to `/test` would show the custom 404 page, but a request to
    `/test.test` would return a 404 with an empty body.
    
    This change ignores the format on incoming catch all route requests, so that the
    html 404 page is returned on these requests.

---
+++ spec/requests/catch_all_route_request_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "The catch all route" do
+  describe "with a simple value" do
+    it "returns a 404 page as html" do
+      get "/test"
+
+      expect(response.status).to eq 404
+      expect(response.content_type).to eq "text/html"
+    end
+  end
+
+  describe "with an implied format" do
+    it "returns a 404 page as html" do
+      get "/test.test"
+
+      expect(response.status).to eq 404
+      expect(response.content_type).to eq "text/html"
+    end
+  end
+end
