e86246fee6eca12fb7a675de02042cc3ead9e4a7
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-09 08:15:26 -0400

    Ignore implied formats for catch all route requests
    A request to `/test` would show the custom 404 page, but a request to
    `/test.test` would return a 404 with an empty body.
    
    This change ignores the format on incoming catch all route requests, so that the
    html 404 page is returned on these requests.

--- config/routes.rb
+++ config/routes.rb
@@ -194,5 +194,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  match '*unmatched_route', via: :all, to: 'application#raise_not_found'
+  match '*unmatched_route',
+    via: :all,
+    to: 'application#raise_not_found',
+    format: false
 end
