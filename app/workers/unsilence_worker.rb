a48730ee9663a84405b6fc37f51454d0627d61bd
Lynn Cyrin <firemagelynn@gmail.com>
2017-04-09 02:34:06 -0700

    fix boolean issue

--- app/workers/unsilence_worker.rb
+++ app/workers/unsilence_worker.rb
@@ -4,7 +4,7 @@ class UnsilenceWorker
   include Sidekiq::Worker
 
   def perform(account_id)
-    Account.find(account_id).update(silenced: true)
+    Account.find(account_id).update(silenced: false)
   rescue ActiveRecord::RecordNotFound
     true
   end
