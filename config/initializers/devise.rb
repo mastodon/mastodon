fd6295c942f2b0046487fd111f677c837a890948
Yann <y.guern@rock-hosting.com>
2017-04-10 23:13:45 +0200

    Add mail validation on registration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -152,7 +152,7 @@ Devise.setup do |config|
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  # config.email_regexp = /\A[^@]+@[^@]+\z/
+  config.email_regexp = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
