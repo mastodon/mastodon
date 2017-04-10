c2f5548f0762a9d81010a40ba1c5657b85e869c5
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-10 10:51:29 -0400

    Use #page instead of #paginate in controllers

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -5,8 +5,6 @@ module AccountsHelper
     {
       previous_label: safe_join([fa_icon('chevron-left'), t('pagination.prev')], ' '),
       next_label: safe_join([t('pagination.next'), fa_icon('chevron-right')], ' '),
-      inner_window: 1,
-      outer_window: 0,
     }
   end
 end
---
+++ config/initializers/kaminari_config.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+Kaminari.configure do |config|
+  config.default_per_page = 40
+  config.window = 1
+end
