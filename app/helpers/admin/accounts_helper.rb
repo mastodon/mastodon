ba7950f4533b8efe5c743109c4b1395b925972a3
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-11 16:58:44 -0400

    Move filter_link_to class formation to separate method in admin/accounts helper

--- app/helpers/admin/accounts_helper.rb
+++ app/helpers/admin/accounts_helper.rb
@@ -6,10 +6,16 @@ module Admin::AccountsHelper
   end
 
   def filter_link_to(text, more_params)
-    link_to text, filter_params(more_params), class: params.merge(more_params).compact == params.compact ? 'selected' : ''
+    link_to text, filter_params(more_params), class: filtered_link_class(more_params)
   end
 
   def table_link_to(icon, text, path, options = {})
     link_to safe_join([fa_icon(icon), text]), path, options.merge(class: 'table-action-link')
+  end
+
+  private
+
+  def filtered_link_class(more_params)
+    params.merge(more_params).compact == params.compact ? 'selected' : ''
   end
 end
