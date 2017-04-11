8d52a75c11b40c2a20410b3093848aaf801bab78
James Moore <james@panic.com>
2017-04-11 09:48:09 -0700

    delivery fix
    # Conflicts:
    #	config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,8 @@ Rails.application.configure do
     :enable_starttls_auto => ENV['SMTP_ENABLE_STARTTLS_AUTO'] || true,
   }
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = ENV.fetch('SMTP_DELIVERY_METHOD', 'smtp').to_sym
+
 
   config.react.variant = :production
 
