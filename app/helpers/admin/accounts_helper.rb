eecd950c574f9c687b3fbe7f1b2ba3bb00d62628
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-11 17:04:54 -0400

    Remove deprecated #compact method usage on strong parameters

--- app/helpers/admin/accounts_helper.rb
+++ app/helpers/admin/accounts_helper.rb
@@ -6,7 +6,8 @@ module Admin::AccountsHelper
   end
 
   def filter_link_to(text, more_params)
-    link_to text, filter_params(more_params), class: filtered_link_class(more_params)
+    new_url = filtered_url_for(more_params)
+    link_to text, new_url, class: filter_link_class(new_url)
   end
 
   def table_link_to(icon, text, path, options = {})
@@ -15,7 +16,11 @@ module Admin::AccountsHelper
 
   private
 
-  def filtered_link_class(more_params)
-    params.merge(more_params).compact == params.compact ? 'selected' : ''
+  def filter_link_class(new_url)
+    filtered_url_for(params) == new_url ? 'selected' : ''
+  end
+
+  def filtered_url_for(params)
+    url_for filter_params(params)
   end
 end
