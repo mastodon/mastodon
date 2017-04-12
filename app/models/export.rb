fd5821705e2a443eae1e62dbf1c74cbf0561d946
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-12 15:43:42 -0400

    Refactor Export to take an account and know about the export types

--- app/models/export.rb
+++ app/models/export.rb
@@ -2,13 +2,27 @@
 require 'csv'
 
 class Export
-  attr_reader :accounts
+  attr_reader :account
 
-  def initialize(accounts)
-    @accounts = accounts
+  def initialize(account)
+    @account = account
   end
 
-  def to_csv
+  def to_blocked_accounts_csv
+    to_csv @account.blocking
+  end
+
+  def to_muted_accounts_csv
+    to_csv @account.muting
+  end
+
+  def to_following_accounts_csv
+    to_csv @account.following
+  end
+
+  private
+
+  def to_csv(accounts)
     CSV.generate do |csv|
       accounts.each do |account|
         csv << [(account.local? ? account.local_username_and_domain : account.acct)]
