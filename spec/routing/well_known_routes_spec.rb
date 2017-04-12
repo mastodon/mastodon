b7cb46cf07146579b69d39f5cf4d7352cb975320
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-12 16:56:08 -0400

    Update well-known routes

--- spec/routing/well_known_routes_spec.rb
+++ spec/routing/well_known_routes_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'the host-meta route' do
   it 'routes to correct place with xml format' do
     expect(get('/.well-known/host-meta')).
-      to route_to('xrd#host_meta', format: 'xml')
+      to route_to('well_known/host_meta#show', format: 'xml')
   end
 end
 
 describe 'the webfinger route' do
   it 'routes to correct place with json format' do
     expect(get('/.well-known/webfinger')).
-      to route_to('xrd#webfinger', format: 'json')
+      to route_to('well_known/webfinger#show', format: 'json')
   end
 end
