7dd0970d82b5112271b91fe2040516e42c1d6c17
Thomas Citharel <tcit@tcit.fr>
2017-04-12 12:52:34 +0200

    Insert instance in registration emails and improve them a bit
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,10 @@ class UserMailer < Devise::Mailer
   def confirmation_instructions(user, token, _opts = {})
     @resource = user
     @token    = token
+    @instance = Rails.configuration.x.local_domain
 
     I18n.with_locale(@resource.locale || I18n.default_locale) do
-      mail to: @resource.unconfirmed_email.blank? ? @resource.email : @resource.unconfirmed_email, subject: I18n.t('devise.mailer.confirmation_instructions.subject')
+      mail to: @resource.unconfirmed_email.blank? ? @resource.email : @resource.unconfirmed_email, subject: I18n.t('devise.mailer.confirmation_instructions.subject', instance: @instance)
     end
   end
 
