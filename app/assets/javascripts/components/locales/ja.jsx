1ea486100ca990000c08ac662e9c60bb5c5c8772
pinfort <pinfo.walter@gmail.com>
2017-04-12 20:21:34 +0900

    再度変更

--- app/assets/javascripts/components/locales/ja.jsx
+++ app/assets/javascripts/components/locales/ja.jsx
@@ -62,11 +62,11 @@ const ja = {
   "notification.reblog": "{name}さんがあなたのトゥートをブーストしました",
   "notification.mention": "{name}さんがあなたに返信しました",
   "notifications.column_settings.alert": "デスクトップ通知",
-  "notifications.column_settings.show": "表示項目",
-  "notifications.column_settings.follow": "新しいフォロワー:",
-  "notifications.column_settings.favourite": "いいね:",
-  "notifications.column_settings.mention": "返信:",
-  "notifications.column_settings.reblog": "ブースト:",
+  "notifications.column_settings.show": "カラムに表示",
+  "notifications.column_settings.follow": "新しいフォロワー",
+  "notifications.column_settings.favourite": "お気に入り",
+  "notifications.column_settings.mention": "返信",
+  "notifications.column_settings.reblog": "ブースト",
 };
 
 export default ja;
