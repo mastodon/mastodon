958d7f53c05b6808146d4331438d2bf638560d26
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-12 22:27:44 -0400

    Add admin/silences and admin/suspensions controllers

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -2,32 +2,12 @@
 
 module Admin
   class AccountsController < BaseController
-    before_action :set_account, except: :index
-
     def index
       @accounts = filtered_accounts.page(params[:page])
     end
 
-    def show; end
-
-    def suspend
-      Admin::SuspensionWorker.perform_async(@account.id)
-      redirect_to admin_accounts_path
-    end
-
-    def unsuspend
-      @account.update(suspended: false)
-      redirect_to admin_accounts_path
-    end
-
-    def silence
-      @account.update(silenced: true)
-      redirect_to admin_accounts_path
-    end
-
-    def unsilence
-      @account.update(silenced: false)
-      redirect_to admin_accounts_path
+    def show
+      @account = Account.find(params[:id])
     end
 
     private
@@ -43,12 +23,8 @@ module Admin
         :by_domain,
         :silenced,
         :recent,
-        :suspended,
+        :suspended
       )
-    end
-
-    def set_account
-      @account = Account.find(params[:id])
     end
   end
 end
