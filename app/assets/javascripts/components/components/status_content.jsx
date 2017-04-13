b525d384966b7b2c06c1a7907a3a5974c8eaacaa
Rachel <lastexyle@gmail.com>
2017-04-12 21:00:31 -0700

    Trim whitespace around paragraphs

--- app/assets/javascripts/components/components/status_content.jsx
+++ app/assets/javascripts/components/components/status_content.jsx
@@ -92,7 +92,7 @@ const StatusContent = React.createClass({
     const { status } = this.props;
     const { hidden } = this.state;
 
-    const content = { __html: emojify(status.get('content')) };
+    const content = { __html: emojify(status.get('content').replace(/\s*(<p>|<\/p>)\s*/g, '$1').replace(/\s{2}/g, ' &nbsp;')) };
     const spoilerContent = { __html: emojify(escapeTextContentForBrowser(status.get('spoiler_text', ''))) };
     const directionStyle = { direction: 'ltr' };
 
