1893133b8bd49f3e6969a4dc407f788981b6f9ad
zacanger <zac@zacanger.com>
2017-04-13 11:21:40 -0600

    Fix missing tabIndex.

--- app/assets/javascripts/components/components/load_more.jsx
+++ app/assets/javascripts/components/components/load_more.jsx
@@ -1,7 +1,7 @@
 import { FormattedMessage } from 'react-intl';
 
 const LoadMore = ({ onClick }) => (
-  <span className='load-more' role='button' onClick={onClick}>
+  <span tabIndex='0' className='load-more' role='button' onClick={onClick}>
     <FormattedMessage id='status.load_more' defaultMessage='Load more' />
   </span>
 );
