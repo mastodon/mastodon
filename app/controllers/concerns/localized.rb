4a673d804ed9b1fcad71f6458a4bbe5bbf655ca4
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-14 16:35:49 -0400

    Use a more accurate locale when supplied by browser headers
    Previously we were using a matching option which would use the first locale
    available which matched the locale portion, even if a region was specified.
    
    This changes to first try to find an exact match, and then fall back to the
    region, and then fall back to the  default.

--- app/controllers/concerns/localized.rb
+++ app/controllers/concerns/localized.rb
@@ -27,7 +27,7 @@ module Localized
 
   def default_locale
     ENV.fetch('DEFAULT_LOCALE') {
-      http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+      http_accept_language.language_region_compatible_from(I18n.available_locales) || I18n.default_locale
     }
   end
 end
