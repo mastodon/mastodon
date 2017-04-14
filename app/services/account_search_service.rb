eb3fa5ef5addf815076d66075a5b872563240b1f
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-14 11:29:56 -0400

    Extract methods for local and remote results

--- app/services/account_search_service.rb
+++ app/services/account_search_service.rb
@@ -17,11 +17,7 @@ class AccountSearchService < BaseService
   def search_service_results
     return [] if query_blank_or_hashtag?
 
-    if domain_is_local?
-      results = account.nil? ? Account.search_for(query_username, limit) : Account.advanced_search_for(query_username, account, limit)
-    else
-      results = account.nil? ? Account.search_for("#{query_username} #{query_domain}", limit) : Account.advanced_search_for("#{query_username} #{query_domain}", account, limit)
-    end
+    results = search_results
 
     results = [exact_match] + results.reject { |a| a.id == exact_match.id } if exact_match
 
@@ -61,6 +57,30 @@ class AccountSearchService < BaseService
       Account.find_local(query_username)
     else
       Account.find_remote(query_username, query_domain)
+    end
+  end
+
+  def search_results
+    if domain_is_local?
+      local_search_results
+    else
+      remote_search_results
+    end
+  end
+
+  def local_search_results
+    if account
+      Account.advanced_search_for(query_username, account, limit)
+    else
+      Account.search_for(query_username, limit)
+    end
+  end
+
+  def remote_search_results
+    if account
+      Account.advanced_search_for("#{query_username} #{query_domain}", account, limit)
+    else
+      Account.search_for("#{query_username} #{query_domain}", limit)
     end
   end
 end
