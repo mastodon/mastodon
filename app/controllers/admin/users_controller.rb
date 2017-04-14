6a70da24d975caed5784d95b58058ca40b467a3f
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-14 14:30:37 -0400

    Add filtering to admin/users page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,11 +3,25 @@
 module Admin
   class UsersController < BaseController
     def index
-      @users = User.all.page(params[:page])
+      @users = filtered_users.page(params[:page])
     end
 
     def show
       @user = User.find(params[:id])
+    end
+
+    private
+
+    def filtered_users
+      UserFilter.new(filter_params).results
+    end
+
+    def filter_params
+      params.permit(
+        :admin,
+        :confirmed,
+        :unconfirmed
+      )
     end
   end
 end
