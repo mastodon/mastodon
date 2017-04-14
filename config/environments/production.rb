6902e0307b990914184397fb881f8d16d219d76a
Miguel Jacq <mig@mig5.net>
2017-04-15 09:06:11 +1000

    Assume that SMTP_LOGIN and SMTP_PASSWORD will be blank if we set SMTP_AUTH_METHOD to none

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :port                 => ENV['SMTP_PORT'],
     :address              => ENV['SMTP_SERVER'],
-    :user_name            => ENV['SMTP_LOGIN'] == 'none' ? nil : ENV['SMTP_LOGIN'].presence,
-    :password             => ENV['SMTP_LOGIN'] == 'none' ? nil : ENV['SMTP_PASSWORD'].presence,
+    :user_name            => ENV['SMTP_LOGIN'],
+    :password             => ENV['SMTP_PASSWORD'],
     :domain               => ENV['SMTP_DOMAIN'] || config.x.local_domain,
     :authentication       => ENV['SMTP_AUTH_METHOD'] == 'none' ? nil : ENV['SMTP_AUTH_METHOD'] || :plain,
     :openssl_verify_mode  => ENV['SMTP_OPENSSL_VERIFY_MODE'],
