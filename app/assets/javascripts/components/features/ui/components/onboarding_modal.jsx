e5d5ea9715f601dac2336541611dd0352c736b3b
Eugen Rochko <eugen@zeonfederated.com>
2017-04-15 00:06:54 +0200

    Add animations to onboarding pager changes, improve wording and styling

--- app/assets/javascripts/components/features/ui/components/onboarding_modal.jsx
+++ app/assets/javascripts/components/features/ui/components/onboarding_modal.jsx
@@ -3,6 +3,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
 import Permalink from '../../../components/permalink';
+import { TransitionMotion, spring } from 'react-motion';
 
 const PageOne = ({ acct, domain }) => (
   <div className='onboarding-modal__page onboarding-modal__page-one'>
@@ -13,60 +14,80 @@ const PageOne = ({ acct, domain }) => (
     <div>
       <h1><FormattedMessage id='onboarding.page_one.welcome' defaultMessage='Welcome to Mastodon!' /></h1>
       <p><FormattedMessage id='onboarding.page_one.federation' defaultMessage='Mastodon is a decentralized federation of different server instances linking up and forming one larger social network.' /></p>
-      <p><FormattedMessage id='onboarding.page_one.handle' defaultMessage='You are on {domain}, so your full handle is {handle}' values={{ domain: domain, handle: <strong>{acct}@{domain}</strong> }}/></p>
+      <p><FormattedMessage id='onboarding.page_one.handle' defaultMessage='You are on {domain}, so your full handle is {handle}' values={{ domain, handle: <strong>{acct}@{domain}</strong> }}/></p>
     </div>
   </div>
 );
 
 const PageTwo = () => (
   <div className='onboarding-modal__page onboarding-modal__page-two'>
-    <img className="onboarding-modal__image onboard-compose" src={'/onboarding/onboard-compose.jpg'} />
+    <img className="onboarding-modal__image onboard-compose" src='/onboarding/onboard-compose.jpg' />
     <p><FormattedMessage id='onboarding.page_two.compose' defaultMessage='Write posts from the compose column. You can upload images, change privacy settings, and add content warnings with the icons below.' /></p>
   </div>
 );
 
 const PageThree = () => (
   <div className='onboarding-modal__page onboarding-modal__page-three'>
-    <img className="onboarding-modal__image onboard-compose" src={'/onboarding/onboard-search.jpg'} />
-    <p><FormattedMessage id='onboarding.page_three.search' defaultMessage='Use the search bar to find users and look at hashtags, such as #MastoArt and #Introductions.' /></p>
-    <p><FormattedMessage id='onboarding.page_three.profile' defaultMessage='Click "Edit Profile" to change your avatar, bio, and display name.' /></p>
+    <img className="onboarding-modal__image onboard-compose" src='/onboarding/onboard-search.jpg' />
+    <p><FormattedMessage id='onboarding.page_three.search' defaultMessage='Use the search bar to find people and look at hashtags, such as {illustration} and {introductions}. To look for a person who is not on this instance, use their full handle.' values={{ illustration: <Permalink to='/timelines/tag/illustration' href='/tags/illustration'>#illustration</Permalink>, introductions: <Permalink to='/timelines/tag/introductions' href='/tags/introductions'>#introductions</Permalink> }}/></p>
+    <p><FormattedMessage id='onboarding.page_three.profile' defaultMessage='Click "Edit Profile" to change your avatar, bio, and display name. There, you will also find other preferences.' /></p>
   </div>
 );
 
 const PageFour = () => (
   <div className='onboarding-modal__page onboarding-modal__page-four'>
-    <img className="onboarding-modal__image onboard-column" src={'/onboarding/onboard-home.jpg'} />
-    <p><FormattedMessage id='onboarding.page_four.home' defaultMessage='The Home Timeline shows posts from users you follow.'/></p>
-    <img className="onboarding-modal__image onboard-column" src={"/onboarding/onboard-notifications.jpg"} />
-    <p><FormattedMessage id='onboarding.page_four.notifications' defaultMessage='The Notifications Column shows when a user boosts, favorites, or replies to your posts; and when you have a new follower.' /></p>
-    <p><FormattedMessage id='onboarding.page_four.filter' defaultMessage='Each column can be customized using the settings menu in the top right.' /><img className="onboard-sliders" src={"/onboarding/onboard-sliders.png"}></img></p>
+    <div className='figure'>
+      <img className="onboarding-modal__image onboard-column" src='/onboarding/onboard-home.jpg' />
+      <FormattedMessage id='onboarding.page_four.home' defaultMessage='Home timeline shows posts from yourself, people you follow, and the posts they share'/>
+    </div>
+
+    <div className='figure'>
+      <img className="onboarding-modal__image onboard-column" src="/onboarding/onboard-notifications.jpg" />
+      <FormattedMessage id='onboarding.page_four.notifications' defaultMessage='The notifications column shows when a user shares, favourites, or replies to your posts, and when you have a new follower' />
+    </div>
+
+    <p><FormattedMessage id='onboarding.page_four.filter' defaultMessage='Each column can be customized using the {icon} menu.' values={{ icon: <i className='fa fa-sliders' /> }} /></p>
   </div>
 );
 
-const PageFive = () => (
+const PageFive = ({ domain }) => (
   <div className='onboarding-modal__page onboarding-modal__page-five'>
-    <img className="onboarding-modal__image onboard-column" src={"/onboarding/onboard-getting-started.jpg"} />
-    <p><FormattedMessage id='onboarding.page_five.getting-started' defaultMessage='The Getting Started Column changes based on your needs.'/></p>
-    <img className="onboarding-modal__image onboard-column" src={"/onboarding/onboard-local-timeline.jpg"} />
-    <p><FormattedMessage id='onboarding.page_five.local-timeline' defaultMessage='The Local Timeline shows public posts from every user on your instance.' /></p>
-    <img className="onboarding-modal__image onboard-column" src={"/onboarding/onboard-federated-timeline.jpg"} />
-    <p><FormattedMessage id='onboarding.page_five.federated-timeline' defaultMessage='The Federated Timeline shows public posts from the whole known network of instances.' /></p>
-    <p><FormattedMessage id='onboarding.page_five.public' defaultMessage='These are the Public Timelines, a great way to find people to follow.' /></p>
+    <div className='figure'>
+      <img className="onboarding-modal__image onboard-column" src="/onboarding/onboard-local-timeline.jpg" />
+      <FormattedMessage id='onboarding.page_five.local-timeline' defaultMessage='Local timeline lists public posts originating from people on {domain}, your local instance' values={{ domain }} />
+    </div>
+
+    <div className='figure'>
+      <img className="onboarding-modal__image onboard-column" src="/onboarding/onboard-federated-timeline.jpg" />
+      <FormattedMessage id='onboarding.page_five.federated-timeline' defaultMessage='Federated timeline lists public posts from everyone who people on {domain} follow, regardless of which instance they are from' values={{ domain }} />
+    </div>
+
+    <p><FormattedMessage id='onboarding.page_five.public' defaultMessage='These are the public timelines, a great way to discover content.' /></p>
   </div>
 );
 
-const PageSix = ({ admin }) => (
-  <div className='onboarding-modal__page onboarding-modal__page-six'>
-    <p>
-      <FormattedMessage id='onboarding.page_six.admin' defaultMessage="Your instance's admin is {admin}." values={{ admin: <Permalink href={admin.get('url')} to={`/accounts/${admin.get('id')}`}>@{admin.get('acct')}</Permalink> }} />
-      <br />
-      <FormattedMessage id='onboarding.page_six.read_guidelines' defaultMessage='Please read the {guidelines} of your instance.' values={{ guidelines: <a href='/about/more' target='_blank'><FormattedMessage id='onboarding.page_six.guidelines' defaultMessage='community guidelines' /></a> }}/>
-    </p>
-    <p><FormattedMessage id='onboarding.page_six.github' defaultMessage='Mastodon is free open-source software. You can report bugs, request features, or contribute to the code on {github}.' values={{ github: <a href='https://github.com/tootsuite/mastodon' target='_blank' rel='noopener'>GitHub</a> }} /></p>
-    <p><FormattedMessage id='onboarding.page_six.apps_available' defaultMessage='There are {apps} available for iOS and Android.' values={{ apps: <a href='https://github.com/tootsuite/documentation/blob/master/Using-Mastodon/Apps.md' target='_blank' rel='noopener'><FormattedMessage id='onboarding.page_six.various_app' defaultMessage='mobile apps' /></a> }} /></p>
-    <p><strong><FormattedMessage id='onboarding.page_six.have_fun' defaultMessage='Bon Appetoot!' /></strong></p>
-  </div>
-);
+const PageSix = ({ admin }) => {
+  let adminSection = '';
+
+  if (admin) {
+    adminSection = (
+      <p>
+        <FormattedMessage id='onboarding.page_six.admin' defaultMessage="Your instance's admin is {admin}." values={{ admin: <Permalink href={admin.get('url')} to={`/accounts/${admin.get('id')}`}>@{admin.get('acct')}</Permalink> }} />
+        <br />
+        <FormattedMessage id='onboarding.page_six.read_guidelines' defaultMessage='Please, do not forget to read the {guidelines}!' values={{ guidelines: <a href='/about/more' target='_blank'><FormattedMessage id='onboarding.page_six.guidelines' defaultMessage='community guidelines' /></a> }}/>
+      </p>
+    );
+  }
+
+  return (
+    <div className='onboarding-modal__page onboarding-modal__page-six'>
+      <h1><FormattedMessage id='onboarding.page_six.almost_done' defaultMessage='Almost done...' /></h1>
+      {adminSection}
+      <p><FormattedMessage id='onboarding.page_six.github' defaultMessage='Mastodon is free open-source software. You can report bugs, request features, or contribute to the code on {github}.' values={{ github: <a href='https://github.com/tootsuite/mastodon' target='_blank' rel='noopener'>GitHub</a> }} /></p>
+      <p><FormattedMessage id='onboarding.page_six.apps_available' defaultMessage='There are {apps} available for iOS, Android and other platforms. And now... Bon Appetoot!' values={{ apps: <a href='https://github.com/tootsuite/documentation/blob/master/Using-Mastodon/Apps.md' target='_blank' rel='noopener'><FormattedMessage id='onboarding.page_six.various_app' defaultMessage='various mobile apps' /></a> }} /></p>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   me: state.getIn(['accounts', state.getIn(['meta', 'me'])]),
@@ -120,7 +141,7 @@ const OnboardingModal = React.createClass({
       <PageTwo />,
       <PageThree />,
       <PageFour />,
-      <PageFive />,
+      <PageFive domain={domain} />,
       <PageSix admin={admin} />
     ];
 
@@ -135,11 +156,22 @@ const OnboardingModal = React.createClass({
       nextOrDoneBtn = <a href='#' onClick={this.handleNext.bind(null, pages.length)} className='onboarding-modal__nav onboarding-modal__done'><FormattedMessage id='onboarding.next' defaultMessage='Done' /></a>;
     }
 
+    const styles = pages.map((page, i) => ({
+      key: i,
+      style: { opacity: spring(i === currentIndex ? 1 : 0) }
+    }));
+
     return (
       <div className='modal-root__modal onboarding-modal'>
-        <div className='onboarding-modal__pager'>
-          {pages.map((page, i) => <div key={i} className={i === currentIndex ? 'active' : null}>{page}</div>)}
-        </div>
+        <TransitionMotion styles={styles}>
+          {interpolatedStyles =>
+            <div className='onboarding-modal__pager'>
+              {pages.map((page, i) =>
+                <div key={i} style={{ opacity: interpolatedStyles[i].style.opacity, pointerEvents: i === currentIndex ? 'auto' : 'none' }}>{page}</div>
+              )}
+            </div>
+          }
+        </TransitionMotion>
 
         <div className='onboarding-modal__paginator'>
           <div>
