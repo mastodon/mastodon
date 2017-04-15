1c8477eab214629d26d9526b386bf75729917060
Marcin Cieślak <saper@saper.info>
2017-04-15 16:46:27 +0200

    Give SINGLE_USER a chance to register (#1820)
    An attempt to open a brand new Mastodon instance configured
    as SINGLE_USER_MODE=true will cause an exception.
    
    Enable temporary registration if we have no users in the database
    
    Fixes #1817

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
   private
 
   def authenticate_user!
-    redirect_to(Rails.configuration.x.single_user_mode ? account_path(Account.first) : about_path) unless user_signed_in?
+    redirect_to(single_user_mode? ? account_path(Account.first) : about_path) unless user_signed_in?
   end
 
   def find_or_create_access_token
