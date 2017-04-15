6670e6d33fa735b6199e3b962f62ed6d1442bae1
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-15 10:44:59 -0400

    Add password reset for users from admin accounts area (#1841)

--- config/routes.rb
+++ config/routes.rb
@@ -86,6 +86,7 @@ Rails.application.routes.draw do
     end
 
     resources :accounts, only: [:index, :show] do
+      resource :reset, only: [:create]
       resource :silence, only: [:create, :destroy]
       resource :suspension, only: [:create, :destroy]
     end
