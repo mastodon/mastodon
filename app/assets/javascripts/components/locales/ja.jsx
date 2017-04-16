75f416a4925c3cb60c5a4919cdafc45392b3be00
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-16 08:55:04 -0400

    Fix yaml issue in ja locale (#1916)
    * Fix yaml parse issue in ja.yml locale
    
    * Fix issue in locales/ja.jsx

--- app/assets/javascripts/components/locales/ja.jsx
+++ app/assets/javascripts/components/locales/ja.jsx
@@ -92,7 +92,7 @@ const ja = {
   "report.target": "問題のユーザー",
   "search.placeholder": "検索",
   "search.status_by": "{name}からの投稿",
-  "search_results.total": "{count} {count, plural, one {result} other {results}} 件",  
+  "search_results.total": "{count} {count, plural, one {result} other {results}} 件",
   "status.delete": "削除",
   "status.favourite": "お気に入り",
   "status.load_more": "もっと見る",
