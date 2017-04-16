5ca29c5bb2ed46b82544180c49578ea51d94bf5a
ButterflyOfFire <ButterflyOfFire@protonmail.com>
2017-04-16 07:26:10 +0100

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,8 @@ module Mastodon
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [
-      :ar,
       :en,
+      :ar,
       :bg,
       :de,
       :eo,
