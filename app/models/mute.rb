630de52fdd93e7465d0fb9dac5f35db30a2545d2
alpaca-tc <alpaca-tc@alpaca.tc>
2017-04-17 22:54:33 +0900

    Required foreign keys (#2003)
    * Add `required: true` option to foreign column
    
    * Fixes NoMethodError
    
    ```
    > Favourite.new.valid?
    NoMethodError: undefined method `reblog?' for nil:NilClass
    ```

--- app/models/mute.rb
+++ app/models/mute.rb
@@ -3,9 +3,8 @@
 class Mute < ApplicationRecord
   include Paginable
 
-  belongs_to :account
-  belongs_to :target_account, class_name: 'Account'
+  belongs_to :account, required: true
+  belongs_to :target_account, class_name: 'Account', required: true
 
-  validates :account, :target_account, presence: true
   validates :account_id, uniqueness: { scope: :target_account_id }
 end
