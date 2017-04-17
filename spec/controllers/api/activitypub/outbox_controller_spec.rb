60431d4bb1df384975b3cc849e74f00aca699213
Evan Minto <evan.minto@gmail.com>
2017-04-16 22:56:22 -0700

    First test working.

--- spec/controllers/api/activitypub/outbox_controller_spec.rb
+++ spec/controllers/api/activitypub/outbox_controller_spec.rb
@@ -4,17 +4,13 @@ RSpec.describe Api::Activitypub::OutboxController, type: :controller do
   render_views
 
   let(:user)  { Fabricate(:user, account: Fabricate(:account, username: 'alice')) }
-  let(:app)   { Fabricate(:application, name: 'Test app', website: 'http://testapp.com') }
-  let(:token) { double acceptable?: true, resource_owner_id: user.id, application: app }
-
-  before do
-    allow(controller).to receive(:doorkeeper_token) { token }
-  end
 
   describe 'GET #show' do
     it 'returns http success' do
-      get :show
+      @request.env['HTTP_ACCEPT'] = 'application/activity+json'
+      get :show, id: user.account.id
       expect(response).to have_http_status(:success)
+      expect(response.header['Content-Type']).to include 'application/activity+json'
     end
   end
 end
