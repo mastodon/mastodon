22f2a848a5da0bd3de1e17fb2c551978923d73c8
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-18 12:03:29 -0400

    Link to admin/reports filtered views from admin account show view

--- app/helpers/admin/filter_helper.rb
+++ app/helpers/admin/filter_helper.rb
@@ -2,7 +2,7 @@
 
 module Admin::FilterHelper
   ACCOUNT_FILTERS = %i[local remote by_domain silenced suspended recent].freeze
-  REPORT_FILTERS = %i[resolved].freeze
+  REPORT_FILTERS = %i[resolved account_id target_account_id].freeze
 
   FILTERS = ACCOUNT_FILTERS + REPORT_FILTERS
 
