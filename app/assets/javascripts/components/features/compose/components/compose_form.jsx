944af4652b63ecfd4e492e53117bc72191369fc6
Tomohiro Suwa <neoen.gsn@gmail.com>
2017-04-19 00:08:58 +0900

    Disable toot button when above 500 characters

--- app/assets/javascripts/components/features/compose/components/compose_form.jsx
+++ app/assets/javascripts/components/features/compose/components/compose_form.jsx
@@ -198,7 +198,7 @@ const ComposeForm = React.createClass({
 
           <div style={{ display: 'flex' }}>
             <div style={{ paddingTop: '10px', marginRight: '16px', lineHeight: '36px' }}><CharacterCounter max={500} text={[this.props.spoiler_text, this.props.text].join('')} /></div>
-            <div style={{ paddingTop: '10px' }}><Button text={publishText} onClick={this.handleSubmit} disabled={disabled} /></div>
+            <div style={{ paddingTop: '10px' }}><Button text={publishText} onClick={this.handleSubmit} disabled={disabled || this.props.text.length > 500} /></div>
           </div>
         </div>
       </div>
