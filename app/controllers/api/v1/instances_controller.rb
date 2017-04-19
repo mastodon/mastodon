b218450abd63a9a833c21dc85e7875795e6435f4
Wogan May <wogan.may@gmail.com>
2017-04-19 06:44:07 +0000

    Extend instances#show to return more information about the instance

--- app/controllers/api/v1/instances_controller.rb
+++ app/controllers/api/v1/instances_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class Api::V1::InstancesController < ApiController
+  before_action :set_instance_presenter, only: [:show]
   respond_to :json
 
   def show; end
+    
+  def set_instance_presenter
+    @instance_presenter = InstancePresenter.new
+  end
 end
