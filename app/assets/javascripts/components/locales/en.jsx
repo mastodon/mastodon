c3b90bc672dc802ae616855353b87f623142d6b9
Matthew Walsh <mattwcstrfan@gmail.com>
2017-04-18 23:11:50 -0700

    Add monologuing text strings to en

--- app/assets/javascripts/components/locales/en.jsx
+++ app/assets/javascripts/components/locales/en.jsx
@@ -33,6 +33,8 @@ const en = {
   "column.home": "Home",
   "column.notifications": "Notifications",
   "column.public": "Federated timeline",
+  "compose_form.monologuing": "Monologue mode",
+  "compose_form.monologuing_on": "You are monologuing. Your entire status will be saved on the server, however only the first paragraph (400 characters max) will be displayed on timelines. You can format your text using Markdown.",
   "compose_form.placeholder": "What is on your mind?",
   "compose_form.privacy_disclaimer": "Your private status will be delivered to mentioned users on {domains}. Do you trust {domainsCount, plural, one {that server} other {those servers}}? Post privacy only works on Mastodon instances. If {domains} {domainsCount, plural, one {is not a Mastodon instance} other {are not Mastodon instances}}, there will be no indication that your post is private, and it may be boosted or otherwise made visible to unintended recipients.",
   "compose_form.publish": "Toot",
