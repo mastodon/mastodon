c0a9c21ab10532b76a88ad3b3af59129d5c1aaba
Matthew Walsh <mattwcstrfan@gmail.com>
2017-04-19 15:48:18 -0700

    db version set to wrong value in merge (oops)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418005355) do
+ActiveRecord::Schema.define(version: 20170418160728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
