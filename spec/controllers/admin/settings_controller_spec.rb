27d41c4490a332443842c80e8dc0db42bfd6ab38
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-20 09:41:32 -0400

    Add coverage for admin/settings#update

--- spec/controllers/admin/settings_controller_spec.rb
+++ spec/controllers/admin/settings_controller_spec.rb
@@ -3,14 +3,27 @@ require 'rails_helper'
 RSpec.describe Admin::SettingsController, type: :controller do
   render_views
 
-  describe 'GET #index' do
+  describe 'When signed in as an admin' do
     before do
       sign_in Fabricate(:user, admin: true), scope: :user
     end
 
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'PUT #update' do
+      it 'updates a settings value' do
+        Setting.site_title = 'Original'
+        patch :update, params: { id: 'site_title', setting: { value: 'New title' } }
+
+        expect(response).to redirect_to(admin_settings_path)
+        expect(Setting.site_title).to eq 'New title'
+      end
     end
   end
 end
