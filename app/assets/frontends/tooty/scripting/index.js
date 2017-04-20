3dc09554a839ef19f984d6503711b0072f95a969
Kibigo <marrus-sh@users.noreply.github.com>
2017-04-20 16:34:57 -0700

    Initial split

---
+++ app/assets/frontends/tooty/scripting/index.js
@@ -0,0 +1,15 @@
+//= require_self
+//= require react_ujs
+
+window.React    = require('react');
+window.ReactDOM = require('react-dom');
+window.Perf     = require('react-addons-perf');
+
+if (!window.Intl) {
+  require('intl');
+  require('intl/locale-data/jsonp/en.js');
+}
+
+//= require_tree .
+
+window.Mastodon = require('./containers/mastodon');
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,24 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(application_public.js custom.css)
+Rails.application.config.assets.precompile += %w(custom.css)
+
+# Skin assets
+if ENV['SKIN'] and not ENV['SKIN'].empty?
+  for skin in ENV['SKIN'].split("|") do
+    Rails.application.config.assets.precompile += [skin + ".css", skin + ".js"]
+  end
+else
+  Rails.application.config.assets.precompile += %w(rooty.css rooty.js)
+end
+
+# Frontend assets
+if ENV['FRONTEND'] and not ENV['FRONTEND'].empty?
+  for frontend in ENV['FRONTEND'].split("|") do
+    Rails.application.config.assets.precompile += [frontend + ".css", frontend + ".js"]
+  end
+else
+  Rails.application.config.assets.precompile += %w(tooty.css tooty.js)
+end
 
 Rails.application.config.assets.initialize_on_precompile = true
