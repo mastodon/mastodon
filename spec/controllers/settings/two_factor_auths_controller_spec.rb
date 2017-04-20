892e86abb642221868fb05bb536f1a9fa6970613
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-20 18:20:58 -0400

    Add spec coverage for settings/two_factor_auth area

---
+++ spec/controllers/settings/two_factor_auths_controller_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+describe Settings::TwoFactorAuthsController do
+  render_views
+
+  let(:user) { Fabricate(:user) }
+  before do
+    sign_in user, scope: :user
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get :show
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #new' do
+    describe 'when user requires otp for login already' do
+      it 'redirects to show page' do
+        user.update(otp_required_for_login: true)
+        get :new
+
+        expect(response).to redirect_to(settings_two_factor_auth_path)
+      end
+    end
+
+    describe 'when user does not require otp for login' do
+      it 'returns http success' do
+        get :new
+
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    before do
+      user.otp_secret = User.generate_otp_secret(32)
+      user.save!
+    end
+
+    describe 'when creation succeeds' do
+      it 'renders page with success' do
+        allow_any_instance_of(User).to receive(:validate_and_consume_otp!).with('123456').and_return(true)
+
+        post :create, params: { form_two_factor_confirmation: { code: '123456' } }
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:create)
+      end
+    end
+
+    describe 'when creation fails' do
+      it 'renders the new view' do
+        allow_any_instance_of(User).to receive(:validate_and_consume_otp!).with('123456').and_return(false)
+
+        post :create, params: { form_two_factor_confirmation: { code: '123456' } }
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'POST #disable' do
+    before do
+      user.update(otp_required_for_login: true)
+    end
+    it 'turns off otp requirement' do
+      post :disable
+
+      expect(response).to redirect_to(settings_two_factor_auth_path)
+      user.reload
+      expect(user.otp_required_for_login).to eq(false)
+    end
+  end
+
+  describe 'POST #recovery_codes' do
+    it 'updates the codes and shows them on a view' do
+      before = user.otp_backup_codes
+
+      post :recovery_codes
+      user.reload
+
+      expect(user.otp_backup_codes).not_to eq(before)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:recovery_codes)
+    end
+  end
+end
