bb04a9be52e005fb8bbeef22e5b8d30f0d202903
Masoud Abkenar <ampbox@gmail.com>
2017-04-24 22:39:15 +0200

    Persian translation (#2405)
    * Persian translation
    
    * adding :fa to make the site use the rtl stylesheet
    
    based on [this comment](https://github.com/tootsuite/mastodon/pull/2405#issuecomment-296721105)
    
    * fix missing quotes
    
    * add 'fa' locale

--- app/assets/javascripts/components/locales/index.jsx
+++ app/assets/javascripts/components/locales/index.jsx
@@ -2,6 +2,7 @@ import ar from './ar';
 import en from './en';
 import de from './de';
 import es from './es';
+import fa from './fa';
 import hr from './hr';
 import hu from './hu';
 import io from './io';
@@ -27,6 +28,7 @@ const locales = {
   en,
   de,
   es,
+  fa,
   hr,
   hu,
   io,
