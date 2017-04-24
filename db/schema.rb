9b543228d1b9df5b5adb8418c36ffc4f492ec9d4
alpaca-tc <alpaca-tc@alpaca.tc>
2017-04-24 20:30:34 +0900

    Add index to statuses_tags#status_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418160728) do
+ActiveRecord::Schema.define(version: 20170424112722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -255,6 +255,7 @@ ActiveRecord::Schema.define(version: 20170418160728) do
   create_table "statuses_tags", id: false, force: :cascade do |t|
     t.bigint  "status_id", null: false
     t.integer "tag_id",    null: false
+    t.index ["status_id"], name: "index_statuses_tags_on_status_id", using: :btree
     t.index ["tag_id", "status_id"], name: "index_statuses_tags_on_tag_id_and_status_id", unique: true, using: :btree
   end
 
