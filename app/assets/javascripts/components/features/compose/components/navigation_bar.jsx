b032de76c8176ef9bee0d107153216e2e27ba2c3
Ash Furrow <ash@ashfurrow.com>
2017-04-25 01:11:05 +0200

    Prepends username above toot box with an @.

--- app/assets/javascripts/components/features/compose/components/navigation_bar.jsx
+++ app/assets/javascripts/components/features/compose/components/navigation_bar.jsx
@@ -15,7 +15,7 @@ class NavigationBar extends React.PureComponent {
 
         <div className='navigation-bar__profile'>
           <Permalink href={this.props.account.get('url')} to={`/accounts/${this.props.account.get('id')}`}>
-            <strong className='navigation-bar__profile-account'>{this.props.account.get('acct')}</strong>
+            <strong className='navigation-bar__profile-account'>@{this.props.account.get('acct')}</strong>
           </Permalink>
           <a href='/settings/profile' className='navigation-bar__profile-edit'><FormattedMessage id='navigation_bar.edit_profile' defaultMessage='Edit profile' /></a>
         </div>
