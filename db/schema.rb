0d2ddd0fabc9c9401360c6e661913c6bb9670002
Francis Chong <francis@ignition.hk>
2017-04-26 00:46:30 +0800

    Fixes #1985
    - add migration AddMediaAttachmentMeta, which add meta field to media_attachments
    - before saving attachment, set file meta if needed
    - add meta in api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424112722) do
+ActiveRecord::Schema.define(version: 20170425131920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 20170424112722) do
     t.datetime "updated_at",                     null: false
     t.string   "shortcode"
     t.integer  "type",              default: 0,  null: false
+    t.json     "file_meta"
     t.index ["shortcode"], name: "index_media_attachments_on_shortcode", unique: true, using: :btree
     t.index ["status_id"], name: "index_media_attachments_on_status_id", using: :btree
   end
