3aba600536e0663da70fb87b260bbe24be936211
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-26 10:24:04 -0400

    Use blocked_domain method to reduce passed vars

--- app/services/block_domain_service.rb
+++ app/services/block_domain_service.rb
@@ -12,32 +12,36 @@ class BlockDomainService < BaseService
 
   def process_domain_block
     if domain_block.silence?
-      silence_accounts!(domain_block.domain)
+      silence_accounts!
     else
-      suspend_accounts!(domain_block.domain)
+      suspend_accounts!
     end
   end
 
-  def silence_accounts!(domain)
-    Account.where(domain: domain).update_all(silenced: true)
-    clear_media!(domain_block.domain) if domain_block.reject_media?
+  def silence_accounts!
+    Account.where(domain: blocked_domain).update_all(silenced: true)
+    clear_media! if domain_block.reject_media?
   end
 
-  def clear_media!(domain)
-    Account.where(domain: domain).find_each do |account|
+  def clear_media!
+    Account.where(domain: blocked_domain).find_each do |account|
       account.avatar.destroy
       account.header.destroy
     end
 
-    MediaAttachment.where(account: Account.where(domain: domain)).reorder(nil).find_each do |attachment|
+    MediaAttachment.where(account: Account.where(domain: blocked_domain)).reorder(nil).find_each do |attachment|
       attachment.file.destroy
     end
   end
 
-  def suspend_accounts!(domain)
-    Account.where(domain: domain).where(suspended: false).find_each do |account|
+  def suspend_accounts!
+    Account.where(domain: blocked_domain).where(suspended: false).find_each do |account|
       account.subscription(api_subscription_url(account.id)).unsubscribe if account.subscribed?
       SuspendAccountService.new.call(account)
     end
+  end
+
+  def blocked_domain
+    domain_block.domain
   end
 end
