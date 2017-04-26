95e743e21beb4b8032725faa6ba5ec0fff54c2a5
Eugen Rochko <eugen@zeonfederated.com>
2017-04-26 05:01:47 +0200

    Fix rubocop style issues Fix #1681 - return existing access token when applicable instead of creating new

--- app/lib/sanitize_config.rb
+++ app/lib/sanitize_config.rb
@@ -5,11 +5,11 @@ class Sanitize
     HTTP_PROTOCOLS ||= ['http', 'https', :relative].freeze
 
     MASTODON_STRICT ||= freeze_config(
-      elements: %w[p br span a],
+      elements: %w(p br span a),
 
       attributes: {
-        'a'    => %w[href],
-        'span' => %w[class],
+        'a'    => %w(href),
+        'span' => %w(class),
       },
 
       protocols: {
@@ -17,19 +17,22 @@ class Sanitize
       }
     )
 
-    MASTODON_OEMBED ||= freeze_config merge(RELAXED,
-      elements: RELAXED[:elements] + %w[audio embed iframe source video],
+    MASTODON_OEMBED ||= freeze_config merge(
+      RELAXED,
+      elements: RELAXED[:elements] + %w(audio embed iframe source video),
 
-      attributes: merge(RELAXED[:attributes],
-        'audio'  => %w[controls],
-        'embed'  => %w[height src type width],
-        'iframe' => %w[allowfullscreen frameborder height scrolling src width],
-        'source' => %w[src type],
-        'video'  => %w[controls height loop width],
+      attributes: merge(
+        RELAXED[:attributes],
+        'audio'  => %w(controls),
+        'embed'  => %w(height src type width),
+        'iframe' => %w(allowfullscreen frameborder height scrolling src width),
+        'source' => %w(src type),
+        'video'  => %w(controls height loop width),
         'div'    => [:data]
       ),
 
-      protocols: merge(RELAXED[:protocols],
+      protocols: merge(
+        RELAXED[:protocols],
         'embed'  => { 'src' => HTTP_PROTOCOLS },
         'iframe' => { 'src' => HTTP_PROTOCOLS },
         'source' => { 'src' => HTTP_PROTOCOLS }
