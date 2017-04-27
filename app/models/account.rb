00403b16337eb92c6d9260811f4f2d7bf2c4c620
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-27 15:49:10 -0400

    Simplify excluded accounts list on account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -46,6 +46,8 @@ class Account < ApplicationRecord
   # Block relationships
   has_many :block_relationships, class_name: 'Block', foreign_key: 'account_id', dependent: :destroy
   has_many :blocking, -> { order('blocks.id desc') }, through: :block_relationships, source: :target_account
+  has_many :blocked_by_relationships, class_name: 'Block', foreign_key: :target_account_id, dependent: :destroy
+  has_many :blocked_by, -> { order('blocks.id desc') }, through: :blocked_by_relationships, source: :account
 
   # Mute relationships
   has_many :mute_relationships, class_name: 'Mute', foreign_key: 'account_id', dependent: :destroy
@@ -212,7 +214,7 @@ class Account < ApplicationRecord
   end
 
   def excluded_from_timeline_account_ids
-    Rails.cache.fetch("exclude_account_ids_for:#{id}") { Block.where(account: self).pluck(:target_account_id) + Block.where(target_account: self).pluck(:account_id) + Mute.where(account: self).pluck(:target_account_id) }
+    Rails.cache.fetch("exclude_account_ids_for:#{id}") { blocking.pluck(:target_account_id) + blocked_by.pluck(:account_id) + muting.pluck(:target_account_id) }
   end
 
   class << self
