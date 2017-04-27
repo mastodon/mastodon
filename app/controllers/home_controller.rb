7f2da41700229e4825e74e41aa7f78a8be4cb810
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-04-28 01:01:34 +0900

    use Doorkeeper::OAuth::Scopes.from_string

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,6 +18,12 @@ class HomeController < ApplicationController
   end
 
   def find_or_create_access_token
-    Doorkeeper::AccessToken.find_or_create_for(Doorkeeper::Application.where(superapp: true).first, current_user.id, 'read write follow', Doorkeeper.configuration.access_token_expires_in, Doorkeeper.configuration.refresh_token_enabled?)
+    Doorkeeper::AccessToken.find_or_create_for(
+      Doorkeeper::Application.where(superapp: true).first,
+      current_user.id,
+      Doorkeeper::OAuth::Scopes.from_string('read write follow'),
+      Doorkeeper.configuration.access_token_expires_in,
+      Doorkeeper.configuration.refresh_token_enabled?
+    )
   end
 end
