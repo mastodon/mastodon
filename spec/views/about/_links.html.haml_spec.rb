a823509b99eb428b1f68ed89594b88f48546253a
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-28 11:54:03 -0400

    Fix broken view spec for about/links (#2591)

--- spec/views/about/_links.html.haml_spec.rb
+++ spec/views/about/_links.html.haml_spec.rb
@@ -3,33 +3,36 @@
 require 'rails_helper'
 
 describe 'about/_links.html.haml' do
-  it 'does not show sign in link when signed in' do
-    instance_presenter = double(:instance_presenter, open_registrations: true)
-    assign(:instance_presenter, instance_presenter)
-    allow(view).to receive(:user_signed_in?).and_return(true)
-    render 'about/links', instance: InstancePresenter.new
+  context 'when signed in' do
+    before do
+      allow(view).to receive(:user_signed_in?).and_return(true)
+    end
 
-    expect(rendered).to have_content(I18n.t('about.get_started'))
-    expect(rendered).not_to have_content(I18n.t('auth.login'))
+    it 'does not show sign in link' do
+      render 'about/links', instance: InstancePresenter.new
+
+      expect(rendered).to have_content(I18n.t('about.get_started'))
+      expect(rendered).not_to have_content(I18n.t('auth.login'))
+    end
   end
 
-  it 'shows sign in link when signed out' do
-    instance_presenter = double(:instance_presenter, open_registrations: true)
-    assign(:instance_presenter, instance_presenter)
-    allow(view).to receive(:user_signed_in?).and_return(false)
-    render 'about/links', instance: InstancePresenter.new
+  context 'when signed out' do
+    before do
+      allow(view).to receive(:user_signed_in?).and_return(false)
+    end
 
-    expect(rendered).to have_content(I18n.t('about.get_started'))
-    expect(rendered).to have_content(I18n.t('auth.login'))
-  end
+    it 'shows get started link when registrations are allowed' do
+      render 'about/links', instance: double(open_registrations: true)
 
-  it 'shows sign in link when register closed' do
-    instance_presenter = double(:instance_presenter, open_registrations: false)
-    assign(:instance_presenter, instance_presenter)
-    allow(view).to receive(:user_signed_in?).and_return(false)
-    render
+      expect(rendered).to have_content(I18n.t('about.get_started'))
+      expect(rendered).to have_content(I18n.t('auth.login'))
+    end
 
-    expect(rendered).not_to have_content(I18n.t('about.get_started'))
-    expect(rendered).to have_content(I18n.t('auth.login'))
+    it 'hides get started link when registrations are closed' do
+      render 'about/links', instance: double(open_registrations: false)
+
+      expect(rendered).not_to have_content(I18n.t('about.get_started'))
+      expect(rendered).to have_content(I18n.t('auth.login'))
+    end
   end
 end
