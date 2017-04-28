e766d46fc77dc84f5dfd1ccd8413a1b68bf208e2
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-04-29 08:17:10 +0900

    Use ws protocol in streaming API base URL

--- config/initializers/ostatus.rb
+++ config/initializers/ostatus.rb
@@ -12,9 +12,9 @@ Rails.application.configure do
   config.x.use_s3       = ENV['S3_ENABLED'] == 'true'
 
   config.action_mailer.default_url_options = { host: web_host, protocol: https ? 'https://' : 'http://', trailing_slash: false }
-  config.x.streaming_api_base_url          = 'http://localhost:4000'
+  config.x.streaming_api_base_url          = 'ws://localhost:4000'
 
   if Rails.env.production?
-    config.x.streaming_api_base_url = ENV.fetch('STREAMING_API_BASE_URL') { "http#{https ? 's' : ''}://#{web_host}" }
+    config.x.streaming_api_base_url = ENV.fetch('STREAMING_API_BASE_URL') { "ws#{https ? 's' : ''}://#{web_host}" }
   end
 end
