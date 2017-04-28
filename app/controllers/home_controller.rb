1bc9f4a6dfd086f58ec05a2aad6861a4941a7d9e
Tristan Mahé <gled@remote-shell.net>
2017-04-27 20:43:38 -0700

    make rubocop happy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,15 +19,15 @@ class HomeController < ApplicationController
   var _paq = _paq || [];
   /* tracker methods like 'setCustomDimension' should be called before 'trackPageView' */
   _paq.push(['setDocumentTitle', document.domain + '/' + document.title]);
-  _paq.push(['setCookieDomain', '*."+ENV['LOCAL_DOMAIN']+"']);
-  _paq.push(['setDomains', ['*."+ENV['LOCAL_DOMAIN']+"']]);
+  _paq.push(['setCookieDomain', '*." + ENV['LOCAL_DOMAIN'] + "']);
+  _paq.push(['setDomains', ['*." + ENV['LOCAL_DOMAIN'] + "']]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u='//"+ENV['PIWIK_DOMAIN']+"/';
+    var u='//" + ENV['PIWIK_DOMAIN'] + "/';
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '"+ENV['PIWIK_SITEID']+"']);
-    _paq.push(['setUserId', '"+piwik_user_id+"']);
+    _paq.push(['setSiteId', '" + ENV['PIWIK_SITEID'] + "']);
+    _paq.push(['setUserId', '" + piwik_user_id + "']);
     _paq.push(['trackVisibleContentImpressions']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
