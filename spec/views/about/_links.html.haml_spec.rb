319e9330088804e022e435465e845edcbd12893f
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-28 10:21:37 -0400

    Fix broken spec for about/links view partial

--- spec/views/about/_links.html.haml_spec.rb
+++ spec/views/about/_links.html.haml_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe 'about/_links.html.haml' do
   it 'does not show sign in link when signed in' do
     allow(view).to receive(:user_signed_in?).and_return(true)
-    render
+    render 'about/links', instance: InstancePresenter.new
 
     expect(rendered).to have_content(I18n.t('about.get_started'))
     expect(rendered).not_to have_content(I18n.t('auth.login'))
@@ -13,7 +13,7 @@ describe 'about/_links.html.haml' do
 
   it 'shows sign in link when signed out' do
     allow(view).to receive(:user_signed_in?).and_return(false)
-    render
+    render 'about/links', instance: InstancePresenter.new
 
     expect(rendered).to have_content(I18n.t('about.get_started'))
     expect(rendered).to have_content(I18n.t('auth.login'))
