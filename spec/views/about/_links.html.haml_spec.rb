71e73e36cde53950e243f458fe5130271b62705e
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-04-28 23:44:57 +0900

    Fix spec for hide get started (#2585)

--- spec/views/about/_links.html.haml_spec.rb
+++ spec/views/about/_links.html.haml_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 describe 'about/_links.html.haml' do
   it 'does not show sign in link when signed in' do
+    instance_presenter = double(:instance_presenter, open_registrations: true)
+    assign(:instance_presenter, instance_presenter)
     allow(view).to receive(:user_signed_in?).and_return(true)
     render
 
@@ -12,10 +14,22 @@ describe 'about/_links.html.haml' do
   end
 
   it 'shows sign in link when signed out' do
+    instance_presenter = double(:instance_presenter, open_registrations: true)
+    assign(:instance_presenter, instance_presenter)
     allow(view).to receive(:user_signed_in?).and_return(false)
     render
 
     expect(rendered).to have_content(I18n.t('about.get_started'))
+    expect(rendered).to have_content(I18n.t('auth.login'))
+  end
+
+  it 'shows sign in link when register closed' do
+    instance_presenter = double(:instance_presenter, open_registrations: false)
+    assign(:instance_presenter, instance_presenter)
+    allow(view).to receive(:user_signed_in?).and_return(false)
+    render
+
+    expect(rendered).not_to have_content(I18n.t('about.get_started'))
     expect(rendered).to have_content(I18n.t('auth.login'))
   end
 end
