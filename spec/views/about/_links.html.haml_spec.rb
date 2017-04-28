9262f6968becb7f046396b783b60be93073601a8
Matt Jankowski <mjankowski@thoughtbot.com>
2017-04-28 10:45:24 -0400

    Fix broken spec for about/links view partial (#2586)

--- spec/views/about/_links.html.haml_spec.rb
+++ spec/views/about/_links.html.haml_spec.rb
@@ -7,7 +7,7 @@ describe 'about/_links.html.haml' do
     instance_presenter = double(:instance_presenter, open_registrations: true)
     assign(:instance_presenter, instance_presenter)
     allow(view).to receive(:user_signed_in?).and_return(true)
-    render
+    render 'about/links', instance: InstancePresenter.new
 
     expect(rendered).to have_content(I18n.t('about.get_started'))
     expect(rendered).not_to have_content(I18n.t('auth.login'))
@@ -17,7 +17,7 @@ describe 'about/_links.html.haml' do
     instance_presenter = double(:instance_presenter, open_registrations: true)
     assign(:instance_presenter, instance_presenter)
     allow(view).to receive(:user_signed_in?).and_return(false)
-    render
+    render 'about/links', instance: InstancePresenter.new
 
     expect(rendered).to have_content(I18n.t('about.get_started'))
     expect(rendered).to have_content(I18n.t('auth.login'))
