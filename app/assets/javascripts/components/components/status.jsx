3cb3a4240c544941be099c9c89fba5397cb27271
Rey Tucker <git@ryantucker.us>
2017-04-30 17:44:30 -0400

    status: if spoiler_text is present, treat media as sensitive (#1010)
    In the event that there is a CW, go ahead and treat any
    attached media as sensitive.

--- app/assets/javascripts/components/components/status.jsx
+++ app/assets/javascripts/components/components/status.jsx
@@ -65,9 +65,9 @@ class Status extends React.PureComponent {
       if (status.get('media_attachments').some(item => item.get('type') === 'unknown')) {
 
       } else if (status.getIn(['media_attachments', 0, 'type']) === 'video') {
-        media = <VideoPlayer media={status.getIn(['media_attachments', 0])} sensitive={status.get('sensitive')} onOpenVideo={this.props.onOpenVideo} />;
+        media = <VideoPlayer media={status.getIn(['media_attachments', 0])} sensitive={status.get('sensitive') || status.get('spoiler_text').length > 0} onOpenVideo={this.props.onOpenVideo} />;
       } else {
-        media = <MediaGallery media={status.get('media_attachments')} sensitive={status.get('sensitive')} height={110} onOpenMedia={this.props.onOpenMedia} autoPlayGif={this.props.autoPlayGif} />;
+        media = <MediaGallery media={status.get('media_attachments')} sensitive={status.get('sensitive') || status.get('spoiler_text').length > 0} height={110} onOpenMedia={this.props.onOpenMedia} autoPlayGif={this.props.autoPlayGif} />;
       }
     }
 
