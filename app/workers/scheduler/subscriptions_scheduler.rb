51381885935c3c5308296363e7bb80dd625fcdbd
Eugen Rochko <eugen@zeonfederated.com>
2017-05-04 22:11:08 +0200

    Extract #expiring_accounts into method

--- app/workers/scheduler/subscriptions_scheduler.rb
+++ app/workers/scheduler/subscriptions_scheduler.rb
@@ -5,8 +5,16 @@ class Scheduler::SubscriptionsScheduler
   include Sidekiq::Worker
 
   def perform
-    Account.expiring(1.day.from_now).partitioned.pluck(:id) do |id|
+    Rails.logger.debug 'Queueing PuSH re-subscriptions'
+
+    expiring_accounts.pluck(:id) do |id|
       Pubsubhubbub::SubscribeWorker.perform_async(id)
     end
+  end
+
+  private
+
+  def expiring_accounts
+    Account.expiring(1.day.from_now).partitioned
   end
 end
