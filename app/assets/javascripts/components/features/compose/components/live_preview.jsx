8a857fef18f87d631f17d148081b99547e7c2fab
北䑓如法 <algebraicallyClosedField@gmail.com>
2017-05-04 14:15:19 +0900

    Avoid flicker when you are composing a toot

--- app/assets/javascripts/components/features/compose/components/live_preview.jsx
+++ app/assets/javascripts/components/features/compose/components/live_preview.jsx
@@ -1,18 +1,37 @@
 import PropTypes from 'prop-types';
 import emojify from '../../../emoji';
+import { debounce } from 'react-decoration';
 const loadScriptOnce = require('load-script-once');
 
 class LivePreview extends React.PureComponent {
+  constructor (props, context) {
+    super(props, context);
+    this.state = {
+      textToRender: ''
+    };
+  }
 
-  componentDidUpdate() {
+  @debounce(375)
+  changeTextToRender() {
+    const text = this.props.text.replace(/\n/g, '<br>');
+
+    this.setState({ textToRender: text });
+    this.render();
     const node  = ReactDOM.findDOMNode(this);
     if (MathJax != undefined) {
-	  MathJax.Hub.Queue(["Typeset", MathJax.Hub, node]);
+      MathJax.Hub.Queue(["Typeset", MathJax.Hub, node]);
     }
+  }
+    
+  componentWillUpdate() {
+  }
+  
+  componentDidUpdate() {
+    this.changeTextToRender();
   }
   
   render () {
-    const text = this.props.text.replace(/\n/g, '<br>');
+    const text = this.state.textToRender;
     return <div dangerouslySetInnerHTML={{ __html: emojify(text)}} />
   }
 
