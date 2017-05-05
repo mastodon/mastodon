a2cd590cd2c0b47b3c5d3e78ebd9a80dcd8d3f8b
Koala Yeung <koalay@gmail.com>
2017-05-05 18:25:19 +0800

    translationRunner: auto detect existing languages
    * Auto detect existing rfc5646 language tag in *.json filenames
      in `app/javascript/mastodon/locale` folder. No need to manually
      define every new language in the languages array here.

--- config/webpack/translationRunner.js
+++ config/webpack/translationRunner.js
@@ -1,34 +1,20 @@
+/*eslint no-console: "off"*/
 const manageTranslations = require('react-intl-translations-manager').default;
+const fs = require('fs');
+
+const translationsDirectory = 'app/javascript/mastodon/locales';
+const localeFn = /^([a-z]{2,3}(|\-[A-Z]+))\.json$/;
+const languages = fs.readdirSync('/home/koala/Workspace/mastodon/app/javascript/mastodon/locales/').reduce((acc, fn) => {
+  if (fn.match(localeFn)) {
+    acc.push(fn.replace(localeFn, '$1'));
+  }
+  return acc;
+}, []);
 
 manageTranslations({
   messagesDirectory: 'build/messages',
-  translationsDirectory: 'app/javascript/mastodon/locales/',
+  translationsDirectory,
   detectDuplicateIds: false,
   singleMessagesFile: true,
-  languages: [
-    'ar',
-    'en',
-    'de',
-    'es',
-    'fa',
-    'hr',
-    'hu',
-    'io',
-    'it',
-    'fr',
-    'nl',
-    'no',
-    'oc',
-    'pt',
-    'pt-BR',
-    'uk',
-    'fi',
-    'eo',
-    'ru',
-    'ja',
-    'zh-HK',
-    'zh-CN',
-    'bg',
-    'id',
-  ],
-})
+  languages,
+});
