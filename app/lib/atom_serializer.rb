7631c6ad397eecbeefafa0dbe351cb2b4546ad5e
Loic Dachary <loic@dachary.org>
2017-05-09 11:51:56 +0200

    Add AGPL-3+ copyright notice in source files
    Signed-off-by: Loic Dachary <loic@dachary.org>
    Signed-off-by: Richard M. Stallman <rms@gnu.org>

--- app/lib/atom_serializer.rb
+++ app/lib/atom_serializer.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
+#
+# Mastodon, a GNU Social-compatible microblogging server
+# Copyright (C) 2016-2017 Eugen Rochko & al (see the AUTHORS file)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 class AtomSerializer
   include RoutingHelper
