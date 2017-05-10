a791bffd4cbaa52a3ce42b0f1a0d074c0d082442
Matt Jankowski <mjankowski@thoughtbot.com>
2017-05-10 12:58:55 -0400

    more specs!

--- spec/workers/feed_insert_worker_spec.rb
+++ spec/workers/feed_insert_worker_spec.rb
@@ -30,8 +30,23 @@ describe FeedInsertWorker do
     end
 
     context 'when there are real records' do
-      it 'skips the push when there is a filter'
-      it 'pushes the status onto the home timeline without filter'
+      it 'skips the push when there is a filter' do
+        instance = double(push: nil, filter?: true)
+        allow(FeedManager).to receive(:instance).and_return(instance)
+        result = subject.perform(status.id, follower.id)
+
+        expect(result).to be_nil
+        expect(instance).not_to have_received(:push)
+      end
+
+      it 'pushes the status onto the home timeline without filter' do
+        instance = double(push: nil, filter?: false)
+        allow(FeedManager).to receive(:instance).and_return(instance)
+        result = subject.perform(status.id, follower.id)
+
+        expect(result).to be_nil
+        expect(instance).to have_received(:push).with(:home, follower, status)
+      end
     end
   end
 end
