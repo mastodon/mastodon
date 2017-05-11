88fd5cb68829f87ca3ec74d590299de508fac274
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-05-11 20:34:05 +0900

    Focus tab of Mastodon when clicking notification (#2989)

--- app/javascript/mastodon/actions/notifications.js
+++ app/javascript/mastodon/actions/notifications.js
@@ -50,7 +50,11 @@ export function updateNotifications(notification, intlMessages, intlLocale) {
       const title = new IntlMessageFormat(intlMessages[`notification.${notification.type}`], intlLocale).format({ name: notification.account.display_name.length > 0 ? notification.account.display_name : notification.account.username });
       const body  = (notification.status && notification.status.spoiler_text.length > 0) ? unescapeHTML(notification.status.spoiler_text) : unescapeHTML(notification.status ? notification.status.content : '');
 
-      new Notification(title, { body, icon: notification.account.avatar, tag: notification.id });
+      const notify = new Notification(title, { body, icon: notification.account.avatar, tag: notification.id });
+      notify.addEventListener('click', () => {
+        window.focus();
+        notify.close();
+      });
     }
   };
 };
