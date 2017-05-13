cecbcc73bf1b2c997bcb0a75edc08efd4962853c
Stephen Burgess <stephenburgess8@gmail.com>
2017-05-13 11:55:07 -0500

    Revert "fix(safari9): Allow app to run in Safari 9"
    This reverts commit f36564ad8563e0a10d267fcac08c493fb11ba3db.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,9 @@
 import main from '../mastodon/main';
 
 if (!window.Intl || !Object.assign || !Number.isNaN ||
-  !window.Symbol || !Array.prototype.includes) {
+    !window.Symbol || !Array.prototype.includes) {
   // load polyfills dynamically
-  require('intl');
-  require('intl/locale-data/jsonp/en.js');
-  import('../mastodon/polyfills').then(main());
+  import('../mastodon/polyfills').then(main);
 } else {
   main();
 }
