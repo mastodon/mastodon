021e02172ffb1c788acd402db27f6dfdccad8b90
Stephen Burgess <stephenburgess8@gmail.com>
2017-05-13 13:42:34 -0500

    fix(catch): Error
    Make import promise catch a noop with commented out console.log for a debugging hint.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@ if (!window.Intl || !Object.assign || !Number.isNaN ||
     !window.Symbol || !Array.prototype.includes) {
   // load polyfills dynamically
   import('../mastodon/polyfills').then(main).catch(e => {
-    console.log(e)
+    // console.log(e); // no-op
   });
 } else {
   main();
