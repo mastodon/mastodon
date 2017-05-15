86ddfacadb6940a35ad43b966577d2ca8cf2e214
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-16 02:16:20 +0900

    Build query with arel node

--- app/models/status.rb
+++ app/models/status.rb
@@ -55,7 +55,7 @@ class Status < ApplicationRecord
   validates_with StatusLengthValidator
   validates :reblog, uniqueness: { scope: :account }, if: 'reblog?'
 
-  default_scope { order('id desc') }
+  default_scope { order(id: :desc) }
 
   scope :remote, -> { where.not(uri: nil) }
   scope :local, -> { where(uri: nil) }
