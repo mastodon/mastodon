abe0d9421f2bc6227360f9420aeac2cefbcdc023
Stephen Burgess <stephenburgess8@gmail.com>
2017-05-16 19:59:43 -0500

    Catch import promise in application.js (#3041)
    * fix(polyfil): Catch error from promise
    
    https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
    
    * fix(catch): Error
    
    Make import promise catch a noop with commented out console.log for a debugging hint.
    
    * fix(eslint): Disable eslint console rule
    
    * fix(application): Use console error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import main from '../mastodon/main';
 if (!window.Intl || !Object.assign || !Number.isNaN ||
     !window.Symbol || !Array.prototype.includes) {
   // load polyfills dynamically
-  import('../mastodon/polyfills').then(main);
+  import('../mastodon/polyfills').then(main).catch(e => {
+    console.error(e) // eslint-disable-line no-console
+  });
 } else {
   main();
 }
