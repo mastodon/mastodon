cd88be62f11938af66952106ae54b8ce1479d3b3
Matt Jankowski <mjankowski@thoughtbot.com>
2017-05-19 09:01:05 -0400

    Add spec for precompute feed service

--- app/services/precompute_feed_service.rb
+++ app/services/precompute_feed_service.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class PrecomputeFeedService < BaseService
-  # Fill up a user's home/mentions feed from DB and return a subset
-  # @param [Symbol] type :home or :mentions
-  # @param [Account] account
-  def call(_, account)
+  def call(account)
     redis.pipelined do
       # NOTE: Added `id desc, account_id desc` to `ORDER BY` section to optimize query.
       Status.as_home_timeline(account).order(account_id: :desc).limit(FeedManager::MAX_ITEMS / 4).each do |status|
