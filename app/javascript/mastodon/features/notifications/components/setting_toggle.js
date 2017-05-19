5ef183dcac5ab2dbd48e81c11926c47907a97b0f
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-05-19 16:57:47 +0200

    chore(notifications/setting_toggle): Lint

--- app/javascript/mastodon/features/notifications/components/setting_toggle.js
+++ app/javascript/mastodon/features/notifications/components/setting_toggle.js
@@ -4,6 +4,15 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import Toggle from 'react-toggle';
 
 class SettingToggle extends React.PureComponent {
+
+  static propTypes = {
+    settings: ImmutablePropTypes.map.isRequired,
+    settingKey: PropTypes.array.isRequired,
+    label: PropTypes.node.isRequired,
+    onChange: PropTypes.func.isRequired,
+    htmlFor: PropTypes.string
+  }
+
   onChange = (e) => {
     this.props.onChange(this.props.settingKey, e.target.checked);
   }
@@ -12,20 +21,13 @@ class SettingToggle extends React.PureComponent {
     const { settings, settingKey, label, onChange, htmlFor = '' } = this.props;
 
     return (
-    <label htmlFor={htmlFor} className='setting-toggle__label'>
+      <label htmlFor={htmlFor} className='setting-toggle__label'>
         <Toggle checked={settings.getIn(settingKey)} onChange={this.onChange} />
         <span className='setting-toggle'>{label}</span>
       </label>
-    )
+    );
   }
-}
 
-SettingToggle.propTypes = {
-  settings: ImmutablePropTypes.map.isRequired,
-  settingKey: PropTypes.array.isRequired,
-  label: PropTypes.node.isRequired,
-  onChange: PropTypes.func.isRequired,
-  htmlFor: PropTypes.string
-};
+}
 
 export default SettingToggle;
