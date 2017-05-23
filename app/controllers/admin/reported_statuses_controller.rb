9dd02da17c32973b508cee4ff1e7b42d6db49b70
alpaca-tc <alpaca-tc@alpaca.tc>
2017-05-23 22:48:43 +0900

    Toggle sensitive from admin page

--- app/controllers/admin/reported_statuses_controller.rb
+++ app/controllers/admin/reported_statuses_controller.rb
@@ -2,17 +2,31 @@
 
 module Admin
   class ReportedStatusesController < BaseController
-    def destroy
-      status = Status.find params[:id]
+    before_action :set_report
+    before_action :set_status
 
-      RemovalWorker.perform_async(status.id)
-      redirect_to admin_report_path(report)
+    def update
+      @status.update(status_params)
+      redirect_to admin_report_path(@report)
+    end
+
+    def destroy
+      RemovalWorker.perform_async(@status.id)
+      redirect_to admin_report_path(@report)
     end
 
     private
 
-    def report
-      Report.find(params[:report_id])
+    def status_params
+      params.require(:status).permit(:sensitive)
+    end
+
+    def set_report
+      @report = Report.find(params[:report_id])
+    end
+
+    def set_status
+      @status = @report.statuses.find(params[:id])
     end
   end
 end
