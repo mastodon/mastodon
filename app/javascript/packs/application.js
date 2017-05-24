7458e5831bfa6b79828757821a3a719447e9e010
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-05-24 07:24:33 +0200

    chore: Add note about polyfill bundle splitting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,9 @@ const needsExtraPolyfills = !(
   window.requestIdleCallback
 );
 
+// Latest version of Firefox and Safari do not have IntersectionObserver.
+// Edge does not have requestIdleCallback.
+// This avoids shipping them all the polyfills.
 if (needsBasePolyfills) {
   Promise.all([
     import('../mastodon/base_polyfills'),
