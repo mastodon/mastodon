fc066d85e59b64c7e0dec0b28f0314620fbb50db
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-05-25 13:40:12 +0200

    feat: Focus client if open

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,16 @@ const handlePush = (event) => {
 const handleNotificationClick = (event) => {
   console.log('click', event);
   event.notification.close();
-  event.waitUntil(clients.openWindow(event.notification.data.url));
+
+  const reactToNotificationClick = self.clients.matchAll().then(clientList => {
+    if (clientList.length !== 0) {
+      clientList[0].focus();
+    } else {
+      self.clients.openWindow(event.notification.data.url)
+    }
+  });
+
+  event.waitUntil(reactToNotificationClick);
 };
 
 self.addEventListener('push', handlePush);
