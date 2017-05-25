5439e8840e65e94833b64b5bea91a68b30ed7df4
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-05-25 13:59:40 +0200

    refactor: Move push logic to WebPushSubscription

--- app/models/web_push_subscription.rb
+++ app/models/web_push_subscription.rb
@@ -12,4 +12,60 @@
 #
 
 class WebPushSubscription < ApplicationRecord
+  include RoutingHelper
+
+  def push(notification)
+    begin
+      # TODO: Why is notification.from_account.hub_url nil?
+      name = if notification.from_account.display_name.empty? then
+               "#{notification.from_account.username}@#{notification.from_account.hub_url}"
+             else
+               notification.from_account.display_name
+             end
+
+      # TODO: Move somewhere else
+      titles = {
+        'Mention' => "#{name} mentioned you",
+        'Follow' => "#{name} followed you",
+        'FollowRequest' => "#{name} requested to follow you",
+        'Favourite' => "#{name} favourited your status",
+        'Status' => "#{name} boosted your status",
+      }
+
+      title = titles[notification.activity_type]
+      url = case notification.activity_type
+              when 'Mention' then web_url("statuses/#{notification.target_status.id}")
+              when 'Follow' then web_url("accounts/#{notification.follow.id}")
+              when 'FollowRequest' then web_url('follow_requests')
+              when 'Favourite' then web_url("statuses/#{notification.target_status.id}")
+              when 'Status' then web_url("statuses/#{notification.target_status.id}")
+            end
+
+      Webpush.payload_send(
+        message: JSON.generate(
+          title: title,
+          options: {
+            body: notification.status.text,
+            tag: notification.id,
+            timestamp: notification.created_at,
+            icon: notification.from_account.avatar_static_url,
+            data: {
+              url: url,
+            }
+          }
+        ),
+        endpoint: endpoint,
+        p256dh: key_p256dh,
+        auth: key_auth,
+        vapid: {
+          private_key: Redis.current.get('vapid_private_key'),
+          public_key: Redis.current.get('vapid_public_key')
+        }
+      )
+    rescue Webpush::InvalidSubscription
+      destroy!
+    rescue Webpush::ResponseError
+      destroy!
+    end
+  end
 end
--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -66,62 +66,7 @@ class NotifyService < BaseService
 
   def send_push_notifications
     @recipient.web_push_subscriptions.each do |web_subscription|
-        push_to_subscriber(web_subscription)
-      end
-  end
-
-  def push_to_subscriber(web_subscription)
-    begin
-      # TODO: Why is @notification.from_account.hub_url nil?
-      name = if @notification.from_account.display_name.empty? then
-               "#{@notification.from_account.username}@#{@notification.from_account.hub_url}"
-             else
-               @notification.from_account.display_name
-             end
-
-      # TODO: Move somewhere else
-      titles = {
-        'Mention' => "#{name} mentioned you",
-        'Follow' => "#{name} followed you",
-        'FollowRequest' => "#{name} requested to follow you",
-        'Favourite' => "#{name} favourited your status",
-        'Status' => "#{name} boosted your status",
-      }
-
-      title = titles[@notification.activity_type]
-      url = case @notification.activity_type
-        when 'Mention' then web_url("statuses/#{@notification.target_status.id}")
-        when 'Follow' then web_url("accounts/#{@notification.follow.id}")
-        when 'FollowRequest' then web_url('follow_requests')
-        when 'Favourite' then web_url("statuses/#{@notification.target_status.id}")
-        when 'Status' then web_url("statuses/#{@notification.target_status.id}")
-      end
-
-      Webpush.payload_send(
-        message: JSON.generate(
-          title: title,
-          options: {
-            body: @notification.status.text,
-            tag: @notification.id,
-            timestamp: @notification.created_at,
-            icon: @notification.from_account.avatar_static_url,
-            data: {
-              url: url,
-            }
-          }
-        ),
-        endpoint: web_subscription.endpoint,
-        p256dh: web_subscription.key_p256dh,
-        auth: web_subscription.key_auth,
-        vapid: {
-          private_key: Redis.current.get('vapid_private_key'),
-          public_key: Redis.current.get('vapid_public_key')
-        }
-      )
-    rescue Webpush::InvalidSubscription
-      web_subscription.destroy
-    rescue Webpush::ResponseError
-      web_subscription.destroy
+      web_subscription.push(@notification)
     end
   end
 
