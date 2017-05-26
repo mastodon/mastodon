ca4bd9332b08bcd7532d0148fda7f7b883bf6af9
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-05-26 22:17:51 +0200

    refactor: Simplify logic

--- app/models/web_push_subscription.rb
+++ app/models/web_push_subscription.rb
@@ -18,39 +18,15 @@ class WebPushSubscription < ApplicationRecord
   include StreamEntriesHelper
   include ActionView::Helpers::TranslationHelper
 
-  before_save :send_welcome_notification
+  before_create :send_welcome_notification
 
-  def push(notification) # rubocop:disable Metrics/CyclomaticComplexity
-    return if data && data.key?('alerts') && !data['alerts'][notification.type.to_s]
+  def push(notification)
+    return unless pushable? notification
 
-    name = display_name(notification.from_account)
-
-    title =
-      case notification.type
-      when :mention then translate('push_notifications.mention.title', name: name)
-      when :follow then translate('push_notifications.follow.title', name: name)
-      when :follow_request then translate('push_notifications.follow_request.title', name: name)
-      when :favourite then translate('push_notifications.favourite.title', name: name)
-      when :reblog then translate('push_notifications.reblog.title', name: name)
-      end
-
-    body =
-      case notification.type
-      when :mention then notification.target_status.text
-      when :follow then notification.from_account.note
-      when :follow_request then notification.from_account.note
-      when :favourite then notification.target_status.text
-      when :reblog then notification.target_status.text
-      end
-
-    url =
-      case notification.type
-      when :mention then web_url("statuses/#{notification.target_status.id}")
-      when :follow then web_url("accounts/#{notification.follow.id}")
-      when :follow_request then web_url('follow_requests')
-      when :favourite then web_url("statuses/#{notification.target_status.id}")
-      when :reblog then web_url("statuses/#{notification.target_status.id}")
-      end
+    name = display_name notification.from_account
+    title = title_str(name, notification)
+    body = body_str notification
+    url = url_str notification
 
     Webpush.payload_send(
       message: JSON.generate(
@@ -88,12 +64,49 @@ class WebPushSubscription < ApplicationRecord
 
   private
 
+  def title_str(name, notification)
+    case notification.type
+    when :mention then translate('push_notifications.mention.title', name: name)
+    when :follow then translate('push_notifications.follow.title', name: name)
+    when :follow_request then translate('push_notifications.follow_request.title', name: name)
+    when :favourite then translate('push_notifications.favourite.title', name: name)
+    when :reblog then translate('push_notifications.reblog.title', name: name)
+    end
+  end
+
+  def body_str(notification)
+    case notification.type
+    when :mention then notification.target_status.text
+    when :follow then notification.from_account.note
+    when :follow_request then notification.from_account.note
+    when :favourite then notification.target_status.text
+    when :reblog then notification.target_status.text
+    end
+  end
+
+  def url_str(notification)
+    case notification.type
+    when :mention then web_url("statuses/#{notification.target_status.id}")
+    when :follow then web_url("accounts/#{notification.follow.id}")
+    when :follow_request then web_url('follow_requests')
+    when :favourite then web_url("statuses/#{notification.target_status.id}")
+    when :reblog then web_url("statuses/#{notification.target_status.id}")
+    end
+  end
+
+  def pushable?(notification)
+    data && data.key?('alerts') && data['alerts'][notification.type.to_s]
+  end
+
   def send_welcome_notification
     Webpush.payload_send(
       message: JSON.generate(
         title: translate('push_notifications.subscribed.title'),
         options: {
           body: translate('push_notifications.subscribed.body'),
+          data: {
+            url: web_url('notifications'),
+          },
         }
       ),
       endpoint: endpoint,
