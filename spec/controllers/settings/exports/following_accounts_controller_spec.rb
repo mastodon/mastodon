6c7f593d99822b24afb009d375c1d6fe57e5a581
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-05-28 16:07:50 +0900

    Spec output of Settings::Exports::FollowingAccountsController

--- spec/controllers/settings/exports/following_accounts_controller_spec.rb
+++ spec/controllers/settings/exports/following_accounts_controller_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 describe Settings::Exports::FollowingAccountsController do
   render_views
 
-  before do
-    sign_in Fabricate(:user), scope: :user
-  end
-
   describe 'GET #index' do
     it 'returns a csv of the following accounts' do
+      user = Fabricate(:user)
+      user.account.follow!(Fabricate(:account, username: 'username', domain: 'domain'))
+
+      sign_in user, scope: :user
       get :index, format: :csv
 
       expect(response).to have_http_status(:success)
+      expect(response.body).to eq "username@domain\n"
       expect(response.content_type).to eq 'text/csv'
       expect(response.headers['Content-Disposition']).to eq 'attachment; filename="following_accounts.csv"'
     end
