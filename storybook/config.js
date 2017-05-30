7359c5a70152b813dbcbf9d836531cdcfff3e8ef
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-05-30 16:37:25 +0900

    Remove unused React Storybook addon

--- storybook/config.js
+++ storybook/config.js
@@ -1,5 +1,4 @@
 import { configure, setAddon } from '@kadira/storybook';
-import IntlAddon from 'react-storybook-addon-intl';
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import { addLocaleData } from 'react-intl';
@@ -7,7 +6,6 @@ import en from 'react-intl/locale-data/en';
 import '../app/javascript/styles/application.scss';
 import './storybook.scss';
 
-setAddon(IntlAddon);
 addLocaleData(en);
 
 let req = require.context('./stories/', true, /.story.js$/);
