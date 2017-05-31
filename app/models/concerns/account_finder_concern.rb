dedb1c34c4bfb48c2cea4ba1bbd30fcbff64f4a8
Matt Jankowski <mjankowski@thoughtbot.com>
2017-05-31 10:46:31 -0400

    Move account finder methods to concern

--- app/models/concerns/account_finder_concern.rb
+++ app/models/concerns/account_finder_concern.rb
@@ -4,8 +4,25 @@ module AccountFinderConcern
   extend ActiveSupport::Concern
 
   class_methods do
-  end
+    def find_local!(username)
+      find_remote!(username, nil)
+    end
 
-  included do
+    def find_remote!(username, domain)
+      raise ActiveRecord::RecordNotFound if username.blank?
+      where('lower(accounts.username) = ?', username.downcase).where(domain.nil? ? { domain: nil } : 'lower(accounts.domain) = ?', domain&.downcase).take!
+    end
+
+    def find_local(username)
+      find_local!(username)
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
+
+    def find_remote(username, domain)
+      find_remote!(username, domain)
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
   end
 end
