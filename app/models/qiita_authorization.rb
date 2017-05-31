f517f0de6e3f3f70fdd24f3e0d26650e0d96ddb7
Tomoya Chiba <tomo.asleep@gmail.com>
2017-05-31 13:34:40 +0900

    Add missing schema documents

--- app/models/qiita_authorization.rb
+++ app/models/qiita_authorization.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: qiita_authorizations
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  uid        :string
+#  token      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class QiitaAuthorization < ApplicationRecord
   belongs_to :user, inverse_of: :qiita_authorization
 end
