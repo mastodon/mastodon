3e33e9f234b26426c689c2adb2326f371a57fc45
Matt Jankowski <mjankowski@thoughtbot.com>
2017-05-31 12:17:23 -0400

    Use an AccountFinder class to simplify lookup

--- app/models/concerns/account_finder_concern.rb
+++ app/models/concerns/account_finder_concern.rb
@@ -3,11 +3,6 @@
 module AccountFinderConcern
   extend ActiveSupport::Concern
 
-  included do
-    private_class_method :matching_username
-    private_class_method :matching_domain
-  end
-
   class_methods do
     def find_local!(username)
       find_local(username) || raise(ActiveRecord::RecordNotFound)
@@ -22,19 +17,41 @@ module AccountFinderConcern
     end
 
     def find_remote(username, domain)
-      matching_username(username).merge(matching_domain(domain)).take
+      AccountFinder.new(username, domain).account
+    end
+  end
+
+  class AccountFinder
+    attr_reader :username, :domain
+
+    def initialize(username, domain)
+      @username = username
+      @domain = domain
     end
 
-    def matching_username(username)
+    def account
+      scoped_accounts.take
+    end
+
+    private
+
+    def scoped_accounts
+      Account.unscoped.tap do |scope|
+        scope.merge! matching_username
+        scope.merge! matching_domain
+      end
+    end
+
+    def matching_username
       raise(ActiveRecord::RecordNotFound) if username.blank?
-      where(arel_table[:username].lower.eq username.downcase)
+      Account.where(Account.arel_table[:username].lower.eq username.downcase)
     end
 
-    def matching_domain(domain)
+    def matching_domain
       if domain.nil?
-        where(domain: nil)
+        Account.where(domain: nil)
       else
-        where(arel_table[:domain].lower.eq domain.downcase)
+        Account.where(Account.arel_table[:domain].lower.eq domain.downcase)
       end
     end
   end
