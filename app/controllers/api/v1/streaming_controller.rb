2942c6fc661a928e75e0134139cd21fd0de5b4d3
zunda <zundan@gmail.com>
2017-06-04 23:12:15 -1000

    Use the new Ruby 1.9 hash syntax

--- app/controllers/api/v1/streaming_controller.rb
+++ app/controllers/api/v1/streaming_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::StreamingController < ApiController
     if Rails.configuration.x.streaming_api_base_url != request.host
       uri = URI.parse(request.url)
       uri.host = URI.parse(Rails.configuration.x.streaming_api_base_url).host
-      redirect_to uri.to_s, :status => 301
+      redirect_to uri.to_s, status: 301
     else
       raise ActiveRecord::RecordNotFound
     end
