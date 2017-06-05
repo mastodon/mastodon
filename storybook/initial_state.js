512182158cac36a590fc1c4c787a4ece51867c67
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-06-05 11:09:31 +0900

    Add OnboadingModal story

---
+++ storybook/initial_state.js
@@ -0,0 +1,24 @@
+export default {
+  meta: {
+    admin: 1,
+    domain: 'example.com',
+    me: 2,
+  },
+  accounts: {
+    1: {
+      acct: 'admin',
+      avatar: '/avatars/original/missing.png',
+      id: 1,
+      url: 'https://example.com/@admin',
+    },
+    2: {
+      acct: 'user',
+      avatar: '/avatars/original/missing.png',
+      id: 1,
+      url: 'https://example.com/@user',
+    },
+  },
+  media_attachments: {
+    accept_content_types: [],
+  },
+};
