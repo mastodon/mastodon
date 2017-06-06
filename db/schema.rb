004672aa6c482b212e36b9e794d107be456a11da
unarist <m.unarist@gmail.com>
2017-06-06 23:07:06 +0900

    Fix tag search order and not to use tsvector (#3611)
    * Sort results by the name
    * Switch search method to simple `LIKE` matching instead of tsvector/tsquery
    
    Previously we used scores from ts_rank_cd() to sort results, but it didn't work
    because the function returns same score for all results. It's not for calculate
    similarity of single words. Sometimes this bug even push out exact matching tag
    from results.
    
    Additionally, PostgreSQL supports prefix searching with standard btree index.
    Using it offers simpler code, but also less index size and some speed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604144747) do
+ActiveRecord::Schema.define(version: 20170606113804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -320,7 +320,7 @@ ActiveRecord::Schema.define(version: 20170604144747) do
     t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index "to_tsvector('simple'::regconfig, (name)::text)", name: "hashtag_search_index", using: :gin
+    t.index "name text_pattern_ops", name: "hashtag_search_index"
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
