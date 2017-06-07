1ad16d2a20da623fec1d50dd1011beb957af3fd4
Matt Jankowski <mjankowski@thoughtbot.com>
2017-06-07 10:38:49 -0400

    Move match_data variable into method

--- app/controllers/concerns/rate_limit_headers.rb
+++ app/controllers/concerns/rate_limit_headers.rb
@@ -11,14 +11,17 @@ module RateLimitHeaders
 
   def set_rate_limit_headers
     now        = Time.now.utc
-    match_data = request.env['rack.attack.throttle_data']['api']
 
-    response.headers['X-RateLimit-Limit']     = match_data[:limit].to_s
-    response.headers['X-RateLimit-Remaining'] = (match_data[:limit] - match_data[:count]).to_s
-    response.headers['X-RateLimit-Reset']     = (now + (match_data[:period] - now.to_i % match_data[:period])).iso8601(6)
+    response.headers['X-RateLimit-Limit']     = api_throttle_data[:limit].to_s
+    response.headers['X-RateLimit-Remaining'] = (api_throttle_data[:limit] - api_throttle_data[:count]).to_s
+    response.headers['X-RateLimit-Reset']     = (now + (api_throttle_data[:period] - now.to_i % api_throttle_data[:period])).iso8601(6)
   end
 
   def rate_limited_request?
     !request.env['rack.attack.throttle_data'].nil?
+  end
+
+  def api_throttle_data
+    request.env['rack.attack.throttle_data']['api']
   end
 end
