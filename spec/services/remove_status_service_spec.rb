6ea23d8ddb90fcfc23c192a3b7d8b059b74a9d48
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-06-12 12:23:47 +0900

    Pipeline unpush in RemoveStatusService

--- spec/services/remove_status_service_spec.rb
+++ spec/services/remove_status_service_spec.rb
@@ -18,31 +18,87 @@ RSpec.describe RemoveStatusService do
     Fabricate(:subscription, account: alice, callback_url: 'http://example.com/push', confirmed: true, expires_at: 30.days.from_now)
     jeff.follow!(alice)
     hank.follow!(alice)
+  end
 
-    @status = PostStatusService.new.call(alice, 'Hello @bob@example.com')
-    Fabricate(:status, account: bill, reblog: @status, uri: 'hoge')
-    subject.call(@status)
+  context 'when status is a reblog' do
+    it 'does not raise when it does not have to remove from home feeds' do
+      status = Fabricate(:status, account: bob, reblog: Fabricate(:status))
+      subject.call(status)
+    end
+
+    it 'reinserts the original status if the reblog is in home feed' do
+      reblog = Fabricate(:status)
+      status = Fabricate(:status, account: alice, reblog: reblog)
+      Redis.current.zadd(FeedManager.instance.key(:home, alice.id), status.id, reblog.id)
+
+      subject.call(status)
+
+      expect(Feed.new(:home, alice).get(1)).to include reblog
+    end
+
+    it 'does not reinsert the original status if the reblog is not in home feed' do
+      reblog = Fabricate(:status)
+      status = Fabricate(:status, account: alice, reblog: reblog)
+
+      subject.call(status)
+
+      expect(Feed.new(:home, alice).get(1)).not_to include reblog
+    end
+  end
+
+  context 'when status is not a reblog' do
+    it 'removes status from home feed' do
+      status = Fabricate(:status, account: alice, reblog: nil)
+      Redis.current.zadd(FeedManager.instance.key(:home, alice.id), status.id, status.id)
+
+      subject.call(status)
+
+      expect(Feed.new(:home, alice).get(1)).not_to include status
+    end
+  end
+
+  it "publishes deletion to timeline" do
+    status = Fabricate(:status, account: alice, reblog: nil)
+
+    timeline_messages = []
+    allow(Redis.current).to(receive(:publish)) do |key, message|
+      timeline_messages << message if key == "timeline:#{alice.id}"
+    end
+
+    subject.call(status)
+
+    expect(timeline_messages).to include Oj.dump(event: :delete, payload: status.id)
   end
 
   it 'removes status from author\'s home feed' do
-    expect(Feed.new(:home, alice).get(10)).to_not include(@status.id)
+    status = Fabricate(:status, account: alice)
+    subject.call(status)
+    expect(Feed.new(:home, alice).get(10)).to_not include(status.id)
   end
 
   it 'removes status from local follower\'s home feed' do
-    expect(Feed.new(:home, jeff).get(10)).to_not include(@status.id)
+    status = Fabricate(:status, account: alice)
+    subject.call(status)
+    expect(Feed.new(:home, jeff).get(10)).to_not include(status.id)
   end
 
   it 'sends PuSH update to PuSH subscribers' do
+    status = Fabricate(:status, account: alice)
+    subject.call(status)
     expect(a_request(:post, 'http://example.com/push').with { |req|
       req.body.match(OStatus::TagManager::VERBS[:delete])
     }).to have_been_made
   end
 
   it 'sends delete activity to followers' do
+    status = PostStatusService.new.call(alice, 'Hello @bob@example.com')
+    subject.call(status)
     expect(a_request(:post, 'http://example.com/inbox')).to have_been_made.twice
   end
 
   it 'sends Salmon slap to previously mentioned users' do
+    status = PostStatusService.new.call(alice, 'Hello @bob@example.com')
+    subject.call(status)
     expect(a_request(:post, "http://example.com/salmon").with { |req|
       xml = OStatus2::Salmon.new.unpack(req.body)
       xml.match(OStatus::TagManager::VERBS[:delete])
@@ -50,6 +106,9 @@ RSpec.describe RemoveStatusService do
   end
 
   it 'sends delete activity to rebloggers' do
+    status = PostStatusService.new.call(alice, 'Hello @bob@example.com')
+    Fabricate(:status, account: bill, reblog: status, uri: 'hoge')
+    subject.call(status)
     expect(a_request(:post, 'http://example2.com/inbox')).to have_been_made
   end
 end
