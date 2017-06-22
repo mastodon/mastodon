4feb244994d099b35a3d5b78bc63b671d169c3ee
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-06-22 19:56:19 +0200

    feat: Lazy load Compose reducers

--- app/javascript/mastodon/reducers/index.js
+++ app/javascript/mastodon/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux-immutable';
 import timelines from './timelines';
 import meta from './meta';
-import compose from './compose';
 import alerts from './alerts';
 import { loadingBarReducer } from 'react-redux-loading-bar';
 import modal from './modal';
@@ -9,9 +8,7 @@ import user_lists from './user_lists';
 import accounts from './accounts';
 import accounts_counters from './accounts_counters';
 import statuses from './statuses';
-import media_attachments from './media_attachments';
 import relationships from './relationships';
-import search from './search';
 import notifications from './notifications';
 import settings from './settings';
 import status_lists from './status_lists';
@@ -19,10 +16,9 @@ import cards from './cards';
 import reports from './reports';
 import contexts from './contexts';
 
-export default combineReducers({
+const reducers = {
   timelines,
   meta,
-  compose,
   alerts,
   loadingBar: loadingBarReducer,
   modal,
@@ -30,13 +26,20 @@ export default combineReducers({
   status_lists,
   accounts,
   accounts_counters,
-  media_attachments,
   statuses,
   relationships,
-  search,
   notifications,
   settings,
   cards,
   reports,
   contexts,
-});
+};
+
+export function createReducer(asyncReducers) {
+  return combineReducers({
+    ...reducers,
+    ...asyncReducers,
+  });
+}
+
+export default combineReducers(reducers);
--- app/javascript/mastodon/store/configureStore.js
+++ app/javascript/mastodon/store/configureStore.js
@@ -1,15 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import appReducer from '../reducers';
+import appReducer, { createReducer } from '../reducers';
 import loadingBarMiddleware from '../middleware/loading_bar';
 import errorsMiddleware from '../middleware/errors';
 import soundsMiddleware from '../middleware/sounds';
 
 export default function configureStore() {
-  return createStore(appReducer, compose(applyMiddleware(
+  const store = createStore(appReducer, compose(applyMiddleware(
     thunk,
     loadingBarMiddleware({ promiseTypeSuffixes: ['REQUEST', 'SUCCESS', 'FAIL'] }),
     errorsMiddleware(),
     soundsMiddleware()
   ), window.devToolsExtension ? window.devToolsExtension() : f => f));
+
+  store.asyncReducers = { };
+
+  return store;
 };
+
+export function injectAsyncReducer(store, name, asyncReducer) {
+  store.asyncReducers[name] = asyncReducer;
+  store.replaceReducer(createReducer(store.asyncReducers));
+}
