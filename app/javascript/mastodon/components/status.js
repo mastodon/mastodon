4fe45dda9a046fc18e20c3d3568348b6b5aefa07
kibigo! <marrus-sh@users.noreply.github.com>
2017-06-21 20:10:11 -0700

    Updates height upon collapsing

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -86,6 +86,10 @@ export default class Status extends ImmutablePureComponent {
     return super.shouldComponentUpdate(nextProps, nextState);
   }
 
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.isCollapsed !== this.state.isCollapsed) this.saveHeight();
+  }
+
   componentDidMount () {
     const node = this.node;
 
