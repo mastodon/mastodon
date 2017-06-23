5911f9c7cd0f2d7fe6470399d774b72203b86f09
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-06-23 13:10:00 +0200

    refactor: Avoid converting the raw state for each lazy hydration

--- app/javascript/mastodon/store/configureStore.js
+++ app/javascript/mastodon/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import appReducer, { createReducer } from '../reducers';
 import { hydrateStoreLazy } from '../actions/store';
-import { initialState } from '../containers/mastodon';
+import { hydrateAction } from '../containers/mastodon';
 import loadingBarMiddleware from '../middleware/loading_bar';
 import errorsMiddleware from '../middleware/errors';
 import soundsMiddleware from '../middleware/sounds';
@@ -22,8 +22,15 @@ export default function configureStore() {
 
 export function injectAsyncReducer(store, name, asyncReducer) {
   if (!store.asyncReducers[name]) {
+    // Keep track that we injected this reducer
     store.asyncReducers[name] = asyncReducer;
+
+    // Add the current reducer to the store
     store.replaceReducer(createReducer(store.asyncReducers));
-    store.dispatch(hydrateStoreLazy(name, initialState));
+
+    // The state this reducer handles defaults to its initial state (stored inside the reducer)
+    // But that state may be out of date because of the server-side hydration, so we replay
+    // the hydration action but only for this reducer (all async reducers must listen for this dynamic action)
+    store.dispatch(hydrateStoreLazy(name, hydrateAction.state));
   }
 }
