138e5a0b1ebc7d21c1f2a73c9742cef600c5af40
unarist <m.unarist@gmail.com>
2017-06-24 21:03:52 +0900

    Fix webpack config for Windows (#3926)

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -1,7 +1,7 @@
 module.exports = {
   test: /\.js$/,
   // include react-intl because transform-react-remove-prop-types needs to apply to it
-  exclude: /node_modules\/(?!react-intl)/,
+  exclude: /node_modules[\/\\](?!react-intl)/,
   loader: 'babel-loader',
   options: {
     forceEnv: process.env.NODE_ENV || 'development',
