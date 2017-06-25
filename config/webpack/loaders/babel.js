787f3b4321b232e6090126f4c03e8be216e01d1d
unarist <m.unarist@gmail.com>
2017-06-25 19:23:22 +0900

    Fix react-intl/locale-data import issue on production build
    Webpack seems to fail to import `react-intl/locale-data/*.js` if those
    files has been proceed by babel, and this also breaks applying our translation.
    
    Note that this won't be a problem on English locale, because react-intl
    includes it as default and works fine without manually added locale-data.
    Also this issue seems to only occurs on production build, but I'm not sure
    about reason.

--- config/webpack/loaders/babel.js
+++ config/webpack/loaders/babel.js
@@ -1,7 +1,10 @@
 module.exports = {
   test: /\.js$/,
   // include react-intl because transform-react-remove-prop-types needs to apply to it
-  exclude: /node_modules[\/\\](?!react-intl)/,
+  exclude: {
+    test: /node_modules/,
+    exclude: /react-intl[\/\\](?!locale-data)/,
+  },
   loader: 'babel-loader',
   options: {
     forceEnv: process.env.NODE_ENV || 'development',
