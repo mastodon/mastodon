ed7dc1704dc3ce82567d9aac366b095f02ce181f
Eugen Rochko <eugen@zeonfederated.com>
2017-06-25 23:51:32 +0200

    Bind web UI access tokens to sessions (#3940)
    * Add overview of active sessions
    
    * Better display of browser/platform name
    
    * Improve how browser information is stored and displayed for sessions overview
    
    * Fix test
    
    * Fix #2347 - Bind web UI access token to session
    
    When you logout, session also destroys the access token, so it's no longer
    valid. If access token is destroyed some other way, the session is also
    destroyed, requiring a re-login.
    
    Fix #1681 - Add scheduler to remove revoked access tokens and grants
    
    * Fix test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624134742) do
+ActiveRecord::Schema.define(version: 20170625140443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -257,6 +257,7 @@ ActiveRecord::Schema.define(version: 20170624134742) do
     t.datetime "updated_at", null: false
     t.string "user_agent", default: "", null: false
     t.inet "ip"
+    t.integer "access_token_id"
     t.index ["session_id"], name: "index_session_activations_on_session_id", unique: true
     t.index ["user_id"], name: "index_session_activations_on_user_id"
   end
@@ -406,6 +407,7 @@ ActiveRecord::Schema.define(version: 20170624134742) do
   add_foreign_key "reports", "accounts", column: "action_taken_by_account_id", on_delete: :nullify
   add_foreign_key "reports", "accounts", column: "target_account_id", on_delete: :cascade
   add_foreign_key "reports", "accounts", on_delete: :cascade
+  add_foreign_key "session_activations", "oauth_access_tokens", column: "access_token_id", on_delete: :cascade
   add_foreign_key "session_activations", "users", on_delete: :cascade
   add_foreign_key "statuses", "accounts", column: "in_reply_to_account_id", on_delete: :nullify
   add_foreign_key "statuses", "accounts", on_delete: :cascade
