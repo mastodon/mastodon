42b82206322c73c4a4d7ac29ca9a781ab11e7b1a
Eugen Rochko <eugen@zeonfederated.com>
2017-06-27 00:04:00 +0200

    Fix #1624 - Send e-mail notifications to admins about new reports (#3949)

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -67,12 +67,4 @@ class NotificationMailer < ApplicationMailer
            )
     end
   end
-
-  private
-
-  def locale_for_account(account)
-    I18n.with_locale(account.user_locale || I18n.default_locale) do
-      yield
-    end
-  end
 end
