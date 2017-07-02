8b5f51b817ca90ed83420dfbac3b0dcfe6c8c5a2
James <james.vogon@gmail.com>
2017-07-02 17:47:51 +0000

    temporary awoo locale commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
