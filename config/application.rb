501c97ca9f0eb91c8b0f8d1577c048d0bcaf9656
James <james.vogon@gmail.com>
2017-07-02 17:47:51 +0000

    temporary awoo locale commit

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Mastodon
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [
+      :awoo,
       :en,
       :ar,
       :bg,
