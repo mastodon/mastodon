aab485288088b4c605f60e451eb5f006ddb9fd9c
Eugen Rochko <eugen@zeonfederated.com>
2017-07-07 14:50:33 +0200

    Fix notifications including wrong status in JSON

--- app/serializers/rest/notification_serializer.rb
+++ app/serializers/rest/notification_serializer.rb
@@ -4,7 +4,7 @@ class REST::NotificationSerializer < ActiveModel::Serializer
   attributes :id, :type, :created_at
 
   belongs_to :from_account, key: :account, serializer: REST::AccountSerializer
-  belongs_to :status, if: :status_type?,   serializer: REST::StatusSerializer
+  belongs_to :target_status, key: :status, if: :status_type?, serializer: REST::StatusSerializer
 
   def status_type?
     [:favourite, :reblog, :mention].include?(object.type)
