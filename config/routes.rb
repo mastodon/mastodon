7fa0897d3518a1338e042342c6c2a686e708fd27
ABCanG <abcang1015@gmail.com>
2017-07-09 01:58:22 +0900

    Improve admin page

--- config/routes.rb
+++ config/routes.rb
@@ -82,7 +82,7 @@ Rails.application.routes.draw do
     resources :instances, only: [:index]
 
     resources :reports, only: [:index, :show, :update] do
-      resources :reported_statuses, only: [:update, :destroy]
+      resources :reported_statuses, only: [:create, :update, :destroy]
     end
 
     resources :accounts, only: [:index, :show] do
@@ -96,6 +96,7 @@ Rails.application.routes.draw do
       resource :silence, only: [:create, :destroy]
       resource :suspension, only: [:create, :destroy]
       resource :confirmation, only: [:create]
+      resources :statuses, only: [:index, :create, :update, :destroy]
     end
 
     resources :users, only: [] do
