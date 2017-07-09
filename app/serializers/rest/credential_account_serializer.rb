05429cb8cfabc360c093e1aa212a2f682a590db2
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-07-09 21:29:38 +0900

    add raw_note

--- app/serializers/rest/credential_account_serializer.rb
+++ app/serializers/rest/credential_account_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class REST::CredentialAccountSerializer < REST::AccountSerializer
-  attributes :default_privacy
+  attributes :raw_note, :default_privacy
+
+  def raw_note
+    object.note
+  end
 
   def default_privacy
     object.user.setting_default_privacy
