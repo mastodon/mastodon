61c73ff76a9a9cc7d2d77aed2f301eca30bf9ccb
Matthew Walsh <mattwcstrfan@gmail.com>
2017-07-09 19:29:28 -0700

    Fabricate(:account) over Account.new

--- spec/controllers/authorize_follows_controller_spec.rb
+++ spec/controllers/authorize_follows_controller_spec.rb
@@ -94,7 +94,7 @@ describe AuthorizeFollowsController do
       end
 
       it 'follows account when found' do
-        target_account = Account.new
+        target_account = Fabricate(:account)
         result_account = double(target_account: target_account)
         service = double
         allow(FollowService).to receive(:new).and_return(service)
