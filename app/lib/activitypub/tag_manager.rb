59b77fa662d39ddcd3b0fc72be6c0db679e35719
Eugen Rochko <eugen@zeonfederated.com>
2017-07-12 23:25:05 +0200

    Adjust cc/to matrices

--- app/lib/activitypub/tag_manager.rb
+++ app/lib/activitypub/tag_manager.rb
@@ -32,18 +32,38 @@ class ActivityPub::TagManager
     end
   end
 
+  # Primary audience of a status
+  # Public statuses go out to primarily the public collection
+  # Unlisted and private statuses go out primarily to the followers collection
+  # Others go out only to the people they mention
   def to(status)
     case status.visibility
     when 'public'
-      COLLECTIONS[:public]
+      [COLLECTIONS[:public]]
     when 'unlisted', 'private'
-      account_followers_url(status.account)
+      [account_followers_url(status.account)]
     when 'direct'
       status.mentions.map { |mention| uri_for(mention.account) }
     end
   end
 
+  # Secondary audience of a status
+  # Public statuses go out to followers as well
+  # Unlisted statuses go to the public as well
+  # Both of those and private statuses also go to the people mentioned in them
+  # Direct ones don't have a secondary audience
   def cc(status)
-    COLLECTIONS[:public] if status.unlisted_visibility?
+    cc = []
+
+    case status.visibility
+    when 'public'
+      cc << account_followers_url(status.account)
+    when 'unlisted'
+      cc << COLLECTIONS[:public]
+    end
+
+    cc.concat(status.mentions.map { |mention| uri_for(mention.account) }) unless status.direct_visibility?
+
+    cc
   end
 end
