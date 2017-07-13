400f3d212e51f0458bdf26d0c97fbe8ade9bd2e7
Eugen Rochko <eugen@zeonfederated.com>
2017-07-13 03:00:42 +0200

    Fix wrong value being set instead of null

--- db/migrate/20170711225116_fix_null_booleans.rb
+++ db/migrate/20170711225116_fix_null_booleans.rb
@@ -1,17 +1,17 @@
 class FixNullBooleans < ActiveRecord::Migration[5.1]
   def change
     change_column_default :domain_blocks, :reject_media, false
-    change_column_null :domain_blocks, :reject_media, false, true
+    change_column_null :domain_blocks, :reject_media, false, false
 
     change_column_default :imports, :approved, false
-    change_column_null :imports, :approved, false, true
+    change_column_null :imports, :approved, false, false
 
-    change_column_null :statuses, :sensitive, false, true
-    change_column_null :statuses, :reply, false, true
+    change_column_null :statuses, :sensitive, false, false
+    change_column_null :statuses, :reply, false, false
 
-    change_column_null :users, :admin, false, true
+    change_column_null :users, :admin, false, false
 
     change_column_default :users, :otp_required_for_login, false
-    change_column_null :users, :otp_required_for_login, false, true
+    change_column_null :users, :otp_required_for_login, false, false
   end
 end
