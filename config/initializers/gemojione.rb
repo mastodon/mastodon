64caafcdbaf099349ee5024cc3a5aacb5b8d6b45
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-07-13 02:00:41 +0200

    feat(config): Add gemojione

---
+++ config/initializers/gemojione.rb
@@ -0,0 +1,18 @@
+Gemojione.asset_path = '/emoji/'
+Gemojione.use_svg = true
+
+Gemojione.module_eval do
+  # Downcase the file names
+  # Always use .svg
+  def self.image_url_for_name(name)
+    emoji = index.find_by_name(name)
+    "#{asset_host}#{ File.join(asset_path, emoji['unicode'].downcase) }.svg"
+  end
+
+  # Use emojione class instead of emoji
+  # Add draggable
+  # Remove width
+  def self.image_tag_for_moji(moji)
+    %Q{<img draggable="false" alt="#{moji}" class="emojione" src="#{ image_url_for_unicode_moji(moji) }">}
+  end
+end
