467456f7a19371fe4d50f25751a1d32a63e9f75c
Eugen Rochko <eugen@zeonfederated.com>
2017-07-24 16:17:55 +0200

    Move clean up of unconfirmed users to sidekiq-scheduler (#4336)
    * Move clean up of unconfirmed users to sidekiq-scheduler
    
    * mastodon:daily is now deprecated

---
+++ app/workers/scheduler/user_cleanup_scheduler.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'sidekiq-scheduler'
+
+class Scheduler::UserCleanupScheduler
+  include Sidekiq::Worker
+
+  def perform
+    User.where('confirmed_at is NULL AND confirmation_sent_at <= ?', 2.days.ago).find_in_batches do |batch|
+      Account.where(id: batch.map(&:account_id)).delete_all
+      User.where(id: batch.map(&:id)).delete_all
+    end
+  end
+end
