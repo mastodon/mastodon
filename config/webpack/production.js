8153b3ba0d35269087eaff491875531bc84b45e2
Satoshi Kojima <skoji@mac.com>
2017-07-25 21:41:37 +0900

    fix #4356 : place sw.js to assets/sw.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -48,7 +48,7 @@ module.exports = merge(sharedConfig, {
       ServiceWorker: {
         entry: path.join(__dirname, '../../app/javascript/mastodon/service_worker/entry.js'),
         cacheName: 'mastodon',
-        output: '../sw.js',
+        output: '../assets/sw.js',
         publicPath: '/sw.js',
         minify: true,
       },
