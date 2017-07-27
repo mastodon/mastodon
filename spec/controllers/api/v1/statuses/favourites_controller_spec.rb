4f0b638cda82810c83f89f23e83efefb60375514
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2017-07-27 22:16:07 +0900

    Introduce access token fabricators (#4401)

--- spec/controllers/api/v1/statuses/favourites_controller_spec.rb
+++ spec/controllers/api/v1/statuses/favourites_controller_spec.rb
@@ -7,7 +7,7 @@ describe Api::V1::Statuses::FavouritesController do
 
   let(:user)  { Fabricate(:user, account: Fabricate(:account, username: 'alice')) }
   let(:app)   { Fabricate(:application, name: 'Test app', website: 'http://testapp.com') }
-  let(:token) { double acceptable?: true, resource_owner_id: user.id, application: app }
+  let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'write', application: app) }
 
   context 'with an oauth token' do
     before do
