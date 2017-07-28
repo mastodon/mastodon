0c547faf92a5433bcf12811d76ece583c50beaf9
Surinna Curtis <ekiru.0@gmail.com>
2017-07-28 00:12:34 -0500

    Less gross passing of notifications flag

--- app/services/mute_service.rb
+++ app/services/mute_service.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 class MuteService < BaseService
-  def call(account, target_account, notifications: nil)
+  def call(account, target_account, **opts)
     return if account.id == target_account.id
     FeedManager.instance.clear_from_timeline(account, target_account)
-    # This unwieldy approach avoids duplicating the default value here
-    # and in mute!.
-    opts = {}
-    opts[:notifications] = notifications unless notifications.nil?
-    account.mute!(target_account, **opts)
+    account.mute!(target_account, **opts.slice(:notifications))
   end
 end
