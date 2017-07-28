99220b2a8f219ae90bb3ab124b8418234dca9bb6
Sorin Davidoi <sorin.davidoi@gmail.com>
2017-07-29 00:26:51 +0200

    fix(column_back_button): Keyboard accessible

--- app/javascript/mastodon/components/column_back_button.js
+++ app/javascript/mastodon/components/column_back_button.js
@@ -8,14 +8,16 @@ export default class ColumnBackButton extends React.PureComponent {
     router: PropTypes.object,
   };
 
-  handleClick = () => {
-    if (window.history && window.history.length === 1) this.context.router.history.push('/');
-    else this.context.router.history.goBack();
+  handleClick = (e) => {
+    if (!e.key || e.key === 'Enter') {
+      if (window.history && window.history.length === 1) this.context.router.history.push('/');
+      else this.context.router.history.goBack();
+    }
   }
 
   render () {
     return (
-      <div role='button' tabIndex='0' onClick={this.handleClick} className='column-back-button'>
+      <div role='button' tabIndex='0' onClick={this.handleClick} onKeyDown={this.handleClick} className='column-back-button'>
         <i className='fa fa-fw fa-chevron-left column-back-button__icon' />
         <FormattedMessage id='column_back_button.label' defaultMessage='Back' />
       </div>
