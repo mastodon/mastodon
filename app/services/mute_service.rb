6f7d00bfddb03ead2e5b4011cbe21c4e97b92421
Surinna Curtis <ekiru.0@gmail.com>
2017-07-27 23:28:57 -0500

    Add support for muting notifications in MuteService

--- app/services/mute_service.rb
+++ app/services/mute_service.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class MuteService < BaseService
-  def call(account, target_account)
+  def call(account, target_account, notifications: nil)
     return if account.id == target_account.id
     FeedManager.instance.clear_from_timeline(account, target_account)
-    account.mute!(target_account)
+    # This unwieldy approach avoids duplicating the default value here
+    # and in mute!.
+    opts = {}
+    opts[:notifications] = notifications unless notifications.nil?
+    account.mute!(target_account, **opts)
   end
 end
