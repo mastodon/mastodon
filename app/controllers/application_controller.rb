5545840b899fe96d215cb879edc4787bc3b33b82
tristar <tristar@urban-theory.net>
2017-07-30 11:50:04 +0900

    catgram: システム内部でのHTTP->HTTPSのリダイレクトを廃止（監視を可能にするため）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  force_ssl if: :https_enabled?
+  # force_ssl if: :https_enabled?
 
   include Localized
   include UserTrackingConcern
