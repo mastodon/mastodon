9fc69b2b4403c3dc475a275df6602614c0ad2430
Eugen Rochko <eugen@zeonfederated.com>
2017-07-31 05:01:38 +0200

    Simplify web UI character counter logic

--- app/javascript/mastodon/features/compose/util/counter.js
+++ app/javascript/mastodon/features/compose/util/counter.js
@@ -2,6 +2,6 @@ const urlPlaceholder = 'xxxxxxxxxxxxxxxxxxxxxxx';
 
 export function countableText(inputText) {
   return inputText
-    .replace(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g, urlPlaceholder)
+    .replace(/https?:\/\/\S+/g, urlPlaceholder)
     .replace(/(?:^|[^\/\w])@(([a-z0-9_]+)@[a-z0-9\.\-]+)/ig, '@$2');
 };
