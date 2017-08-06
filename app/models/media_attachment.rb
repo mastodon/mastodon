8ec7bf73df351319de0d8d633dcfdc48ed767d33
takayamaki <fsgiko@gmail.com>
2017-08-06 13:37:29 +0900

    media_attachmentのjpg変換をアップロード画像がpngの時にのみ行うように

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -105,7 +105,7 @@ class MediaAttachment < ApplicationRecord
     def file_processors(f)
       if f.file_content_type == 'image/gif'
         [:gif_transcoder]
-      elsif IMAGE_MIME_TYPES.include? f.file_content_type
+      elsif f.file_content_type == 'image/png'
         [:img_converter, :thumbnail]
       elsif VIDEO_MIME_TYPES.include? f.file_content_type
         [:video_transcoder]
