661c03d639afa9a21059b4f41791b3aa57ebf446
Eugen Rochko <eugen@zeonfederated.com>
2017-08-12 00:25:06 +0200

    Only PuSH-resubscribe to OStatus accounts

--- app/workers/scheduler/subscriptions_scheduler.rb
+++ app/workers/scheduler/subscriptions_scheduler.rb
@@ -8,13 +8,12 @@ class Scheduler::SubscriptionsScheduler
 
   def perform
     logger.info 'Queueing PuSH re-subscriptions'
-
     Pubsubhubbub::SubscribeWorker.push_bulk(expiring_accounts.pluck(:id))
   end
 
   private
 
   def expiring_accounts
-    Account.expiring(1.day.from_now).partitioned
+    Account.where(protocol: :ostatus).expiring(1.day.from_now).partitioned
   end
 end
