116b8a63633cfdff83582f1cc40559c2e3c2ae80
Eugen Rochko <eugen@zeonfederated.com>
2017-08-17 22:15:37 +0200

    Fix #4607 - Accept/reject activities use FollowRequest, which has inverse relations (#4616)

--- app/serializers/activitypub/accept_follow_serializer.rb
+++ app/serializers/activitypub/accept_follow_serializer.rb
@@ -3,7 +3,7 @@
 class ActivityPub::AcceptFollowSerializer < ActiveModel::Serializer
   attributes :id, :type, :actor
 
-  has_one :object, serializer: ActivityPub::FollowSerializer
+  has_one :object, serializer: ActivityPub::InverseFollowSerializer
 
   def id
     [ActivityPub::TagManager.instance.uri_for(object.target_account), '#accepts/follows/', object.id].join
