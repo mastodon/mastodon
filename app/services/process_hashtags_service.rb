a8a06cee406bef023afb941952b163c8c0154f25
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-08-18 16:11:12 +0900

    Always add nsfw tag when sensitive toot

--- app/services/process_hashtags_service.rb
+++ app/services/process_hashtags_service.rb
@@ -4,6 +4,8 @@ class ProcessHashtagsService < BaseService
   def call(status, tags = [])
     tags = Extractor.extract_hashtags(status.text) if status.local?
 
+    tags << 'nsfw' if status.sensitive?
+
     tags.map { |str| str.mb_chars.downcase }.uniq(&:to_s).each do |tag|
       status.tags << Tag.where(name: tag).first_or_initialize(name: tag)
     end
