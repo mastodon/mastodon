5af86d7fbfba85f6922bc805e7dd9b3294498967
Eugen Rochko <eugen@zeonfederated.com>
2017-08-19 23:50:24 +0200

    Fast-forward repeat follow requests

--- app/services/authorize_follow_service.rb
+++ app/services/authorize_follow_service.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class AuthorizeFollowService < BaseService
-  def call(source_account, target_account)
-    follow_request = FollowRequest.find_by!(account: source_account, target_account: target_account)
-    follow_request.authorize!
+  def call(source_account, target_account, options = {})
+    if options[:skip_follow_request]
+      follow_request = FollowRequest.new(account: source_account, target_account: target_account)
+    else
+      follow_request = FollowRequest.find_by!(account: source_account, target_account: target_account)
+      follow_request.authorize!
+    end
+
     create_notification(follow_request) unless source_account.local?
     follow_request
   end
