353fbe0ae80d645ae9569a282cd57c8bf9676b07
Eugen Rochko <eugen@zeonfederated.com>
2017-08-26 02:36:17 +0200

    Use Tombstone and _:atomUri in Delete activities as fallback

--- app/lib/activitypub/activity/delete.rb
+++ app/lib/activitypub/activity/delete.rb
@@ -2,7 +2,8 @@
 
 class ActivityPub::Activity::Delete < ActivityPub::Activity
   def perform
-    status = Status.find_by(uri: object_uri, account: @account)
+    status   = Status.find_by(uri: object_uri, account: @account)
+    status ||= Status.find_by(uri: @object['_:atomUri'], account: @account) if @object.is_a?(Hash) && @object['_:atomUri'].present?
 
     if status.nil?
       delete_later!(object_uri)
