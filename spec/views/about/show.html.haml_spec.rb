373709db1c06240e0d2f5191190a68eb92985ff8
lindwurm <lindwurm.q@gmail.com>
2017-08-27 11:13:51 +0900

    [maud] Change source code URL
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- spec/views/about/show.html.haml_spec.rb
+++ spec/views/about/show.html.haml_spec.rb
@@ -13,7 +13,7 @@ describe 'about/show.html.haml', without_verify_partial_doubles: true do
                                 site_title: 'something',
                                 site_description: 'something',
                                 version_number: '1.0',
-                                source_url: 'https://github.com/tootsuite/mastodon',
+                                source_url: 'https://github.com/lindwurm/mastodon',
                                 open_registrations: false,
                                 thumbnail: nil,
                                 closed_registrations_message: 'yes')
