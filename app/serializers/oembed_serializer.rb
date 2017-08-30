3f51a22d3b6fb1550b373cfd17b1125cc4747c52
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-08-30 21:03:17 +0900

    Add close tag of iframe for oEmbed response (#4745)
    * Add close tag of iframe for oEmbed response
    
    * add comma

--- app/serializers/oembed_serializer.rb
+++ app/serializers/oembed_serializer.rb
@@ -37,13 +37,16 @@ class OEmbedSerializer < ActiveModel::Serializer
   end
 
   def html
-    tag :iframe,
-        src: embed_short_account_status_url(object.account, object),
-        style: 'width: 100%; overflow: hidden',
-        frameborder: '0',
-        scrolling: 'no',
-        width: width,
-        height: height
+    attributes = {
+      src: embed_short_account_status_url(object.account, object),
+      style: 'width: 100%; overflow: hidden',
+      frameborder: '0',
+      scrolling: 'no',
+      width: width,
+      height: height,
+    }
+
+    content_tag :iframe, nil, attributes
   end
 
   def width
