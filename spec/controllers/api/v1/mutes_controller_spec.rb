c814d1967249ff262f1444ed9472190dadaacd5f
Surinna Curtis <ekiru.0@gmail.com>
2017-09-02 13:19:19 -0500

    Add more specs for the /api/v1/mutes/details endpoint

--- spec/controllers/api/v1/mutes_controller_spec.rb
+++ spec/controllers/api/v1/mutes_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Api::V1::MutesController, type: :controller do
   let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'follow') }
 
   before do
-    Fabricate(:mute, account: user.account)
+    Fabricate(:mute, account: user.account, hide_notifications: false)
     allow(controller).to receive(:doorkeeper_token) { token }
   end
 
@@ -20,10 +20,22 @@ RSpec.describe Api::V1::MutesController, type: :controller do
   end
 
   describe 'GET #details' do
-    it 'returns http success' do
+    before do
       get :details, params: { limit: 1 }
+    end
 
+    let(:mutes) { JSON.parse(response.body) }
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
+
+    it 'returns one mute' do
+      expect(mutes.size).to be(1)
+    end
+
+    it 'returns whether the mute hides notifications' do
+      expect(mutes.first["hide_notifications"]).to be(false)
+    end 
   end
 end
