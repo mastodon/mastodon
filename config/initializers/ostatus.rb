6994664a1391d6a027caec3d5ca9e022f41a0711
Adam Thurlow <thurloat@gmail.com>
2017-09-05 18:17:06 -0300

    swift-enable the paperclip! :paperclip: (#2322)

--- config/initializers/ostatus.rb
+++ config/initializers/ostatus.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   config.x.web_domain   = web_host
   config.x.use_https    = https
   config.x.use_s3       = ENV['S3_ENABLED'] == 'true'
+  config.x.use_swift    = ENV['SWIFT_ENABLED'] == 'true'
 
   config.x.alternate_domains = alternate_domains.split(/\s*,\s*/)
 
