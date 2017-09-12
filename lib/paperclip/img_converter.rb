39fca05005675e542ad362c620a33d8d3c1aa52c
takayamaki <fsgiko@gmail.com>
2017-09-12 16:16:04 +0900

    imagemagickのバージョンに依存して異なる動作になっていた部分を修正

--- lib/paperclip/img_converter.rb
+++ lib/paperclip/img_converter.rb
@@ -5,7 +5,9 @@ module Paperclip
   # to convert images to JPG (except for transparent PNG)
   class ImgConverter < Processor
     def make
-      if identify('-format "%[opaque]" :src', src: File.expand_path(file.path)).strip == 'true'
+      opaque = identify('-format "%[opaque]" :src', src: File.expand_path(file.path)).strip.downcase
+
+      if opaque == 'true'
         basename = File.basename(file.path, File.extname(file.path))
         dst_name = basename << ".jpg"
 
@@ -15,9 +17,7 @@ module Paperclip
                 src: File.expand_path(file.path),
                 dst: File.expand_path(dst.path))
 
-        src_filesize = identify('-format %b :src', src: File.expand_path(file.path)).to_i
-        dst_filesize = identify('-format %b :dst', dst: File.expand_path(dst.path)).to_i
-        if src_filesize > dst_filesize
+        if @file.size > dst.size
           attachment.instance.file_content_type = 'image/jpeg'
           return dst
         end
