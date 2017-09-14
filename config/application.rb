cba885013e963f674ea8c693732c244ae4265379
abcang <abcang1015@gmail.com>
2017-09-14 22:12:43 +0900

    Revert unique retry job (#4937)
    * Revert "Enable UniqueRetryJobMiddleware even when called from sidekiq worker (#4836)"
    
    This reverts commit 6859d4c0289e767955aac3f345074220fe200604.
    
    * Revert "Do not execute the job with the same arguments as the retry job (#4814)"
    
    This reverts commit be7ffa2d7539d5a1946a3933cb9d242b9fac0ddc.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ require_relative '../app/lib/exceptions'
 require_relative '../lib/paperclip/gif_transcoder'
 require_relative '../lib/paperclip/video_transcoder'
 require_relative '../lib/mastodon/version'
-require_relative '../lib/mastodon/unique_retry_job_middleware'
 
 Dotenv::Railtie.load
 
