62f4bb3625c6be0904a80d55fff9116202ed7182
Sam Dukhovni <dukhovni@duxovni.org>
2017-09-14 13:50:35 -0400

    Use ruby 1.9 hash syntax

--- app/workers/scheduler/ip_cleanup_scheduler.rb
+++ app/workers/scheduler/ip_cleanup_scheduler.rb
@@ -7,6 +7,6 @@ class Scheduler::IpCleanupScheduler
   def perform
     time_ago = 5.years.ago
     SessionActivation.where('updated_at < ?', time_ago).destroy_all
-    User.where('last_sign_in_at < ?', time_ago).update_all(:last_sign_in_ip => nil)
+    User.where('last_sign_in_at < ?', time_ago).update_all(last_sign_in_ip: nil)
   end
 end
