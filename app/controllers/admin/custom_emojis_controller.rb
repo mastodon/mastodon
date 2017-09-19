1664e52cbb5ec08db896127640bd0554cee1d384
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-09-19 12:06:13 +0900

    Fix custom emojis index (#5006)

--- app/controllers/admin/custom_emojis_controller.rb
+++ app/controllers/admin/custom_emojis_controller.rb
@@ -3,7 +3,7 @@
 module Admin
   class CustomEmojisController < BaseController
     def index
-      @custom_emojis = CustomEmoji.where(uri: nil)
+      @custom_emojis = CustomEmoji.where(domain: nil)
     end
 
     def new
