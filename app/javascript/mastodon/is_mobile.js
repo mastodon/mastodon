1825d90f695574bcb1d2ebd08d1cf8b6d2d25673
Nolan Lawson <nolan@nolanlawson.com>
2017-09-19 07:39:28 -0700

    Set touchstart listener to 'passive', remove 'once'

--- app/javascript/mastodon/is_mobile.js
+++ app/javascript/mastodon/is_mobile.js
@@ -1,3 +1,5 @@
+import detectPassiveEvents from 'detect-passive-events';
+
 const LAYOUT_BREAKPOINT = 1024;
 
 export function isMobile(width) {
@@ -5,11 +7,16 @@ export function isMobile(width) {
 };
 
 const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-let userTouching = false;
 
-window.addEventListener('touchstart', () => {
+let userTouching = false;
+let listenerOptions = detectPassiveEvents.hasSupport ? { passive: true } : false;
+
+function touchListener() {
   userTouching = true;
-}, { once: true });
+  window.removeEventListener('touchstart', touchListener, listenerOptions);
+}
+
+window.addEventListener('touchstart', touchListener, listenerOptions);
 
 export function isUserTouching() {
   return userTouching;
