fbbcd969d5ebf8a821fe4afc502eb88b3475a1e1
Naoki Kosaka <n.k@mail.yukimochi.net>
2017-09-19 21:05:40 +0900

    Fix non-local statuses are html_encoded in public_page.

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -22,7 +22,7 @@ class Formatter
     unless status.local?
       html = reformat(raw_content)
       html = encode_custom_emojis(html, status.emojis) if options[:custom_emojify]
-      return html
+      return html.html_safe # rubocop:disable Rails/OutputSafety
     end
 
     linkable_accounts = status.mentions.map(&:account)
@@ -39,7 +39,7 @@ class Formatter
   end
 
   def reformat(html)
-    sanitize(html, Sanitize::Config::MASTODON_STRICT).html_safe # rubocop:disable Rails/OutputSafety
+    sanitize(html, Sanitize::Config::MASTODON_STRICT)
   end
 
   def plaintext(status)
