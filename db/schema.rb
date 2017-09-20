dae1d1109b04c8d9ab035cb2bef5ad4078c9ad2f
aschmitz <aschmitz@lardbucket.org>
2017-09-19 23:48:35 -0500

    Transition reblogs to new Redis format
    This provides a one-way migration to transition old Redis reblog entries
    into the new format, with a separate tracking entry for reblogs.
    
    It is not invertible because doing so could (if timestamp IDs are used)
    require a database query for each status in each users' feed, which is
    likely to be a significant toll on major instances.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920024819) do
+ActiveRecord::Schema.define(version: 20170920032311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
