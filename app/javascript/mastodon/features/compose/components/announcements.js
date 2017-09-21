1679d23fe4b08b68249b4b29c82129f9b0d25efe
NCLS <himasoto@gmail.com>
2017-09-21 18:21:44 +0900

    お知らせ機能

---
+++ app/javascript/mastodon/features/compose/components/announcements.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import Immutable from 'immutable';
+import PropTypes from 'prop-types';
+import Link from 'react-router-dom/Link';
+import { defineMessages, injectIntl } from 'react-intl';
+import IconButton from '../../../components/announcement_icon_button';
+import Motion from 'react-motion/lib/Motion';
+import spring from 'react-motion/lib/spring';
+
+const Collapsable = ({ fullHeight, minHeight, isVisible, children }) => (
+  <Motion defaultStyle={{ height: isVisible ? fullHeight : minHeight }} style={{ height: spring(!isVisible ? minHeight : fullHeight) }}>
+    {({ height }) =>
+      <div style={{ height: `${height}px`, overflow: 'hidden' }}>
+        {children}
+      </div>
+    }
+  </Motion>
+);
+
+Collapsable.propTypes = {
+  fullHeight: PropTypes.number.isRequired,
+  minHeight: PropTypes.number.isRequired,
+  isVisible: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+const messages = defineMessages({
+  toggle_visible: { id: 'media_gallery.toggle_visible', defaultMessage: 'Toggle visibility' },
+  welcome: { id: 'welcome.message', defaultMessage: '{domain}へようこそ!' },
+});
+
+const hashtags = Immutable.fromJS([
+  '神崎ドン自己紹介',
+]);
+
+class Announcements extends React.PureComponent {
+
+  static propTypes = {
+    intl: PropTypes.object.isRequired,
+    homeSize: PropTypes.number,
+    isLoading: PropTypes.bool,
+  };
+
+  state = {
+    show: false,
+    isLoaded: false,
+  };
+
+  onClick = () => {
+    this.setState({ show: !this.state.show });
+  }
+  nl2br (text) {
+    return text.split(/(\n)/g).map((line, i) => {
+      if (line.match(/(\n)/g)) {
+        return React.createElement('br', { key: i });
+      }
+      return line;
+    });
+  }
+
+  render () {
+    const { intl } = this.props;
+
+    return (
+      <ul className='announcements'>
+        <li>
+          <Collapsable isVisible={this.state.show} fullHeight={300} minHeight={20} >
+            <div className='announcements__body'>
+              <p>{ this.nl2br(intl.formatMessage(messages.welcome, { domain: document.title }))}<br />
+			  <br />
+			  このインスタンスではBBCodeを使ってテキストをアニメーションさせたり、多くのカスタム絵文字を使うことが出来ます。<br />
+			  about/moreではBBCodeやカスタム絵文字の使い方などを見ることが出来ます。<br />
+			  <br />
+			  あなたがまだフォローする人を決めていない場合、下記のタグを付けて投稿しているユーザーを確認してみると良いかもしれません。</p>
+              {hashtags.map((hashtag, i) =>
+                <Link key={i} to={`/timelines/tag/${hashtag}`}>
+                  #{hashtag}
+                </Link>
+              )}
+            </div>
+          </Collapsable>
+          <div className='announcements__icon'>
+            <IconButton title={intl.formatMessage(messages.toggle_visible)} icon='caret-up' onClick={this.onClick} size={20} animate active={this.state.show} />
+          </div>
+        </li>
+      </ul>
+    );
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (!this.state.isLoaded) {
+      if (!nextProps.isLoading && (nextProps.homeSize === 0 || this.props.homeSize !== nextProps.homeSize)) {
+        this.setState({ show: nextProps.homeSize < 5, isLoaded: true });
+      }
+    }
+  }
+
+}
+
+export default injectIntl(Announcements);
