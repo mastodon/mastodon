b2820c3913266711716c6d91b2198d964881dbba
Eugen Rochko <eugen@zeonfederated.com>
2017-09-24 05:58:07 +0200

    Forgotten in #5039, change statuses_tags tag_id to bigint (#5070)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918125918) do
+ActiveRecord::Schema.define(version: 20170924022025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
