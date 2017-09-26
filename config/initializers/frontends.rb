86e617a839bd4eb45ace52ab226a4e93bb184ff0
kibigo! <marrus-sh@users.noreply.github.com>
2017-09-25 19:24:32 -0700

    Better themeing support!!

--- config/initializers/frontends.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.configure do
-  frontends = []
-  Rails.root.join('app', 'javascript', 'packs', 'frontends').each_child(false) { |f| frontends.push f.to_s }
-  config.x.available_frontends = frontends
-end
