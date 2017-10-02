5fc7c769fa5ff3dbc723dd3bcba8d299132cabec
Akihiko Odaki <nekomanma@pixiv.co.jp>
2017-10-03 00:37:42 +0900

    Do not raise any kind of HTTP error in FetchAtomService

--- spec/services/fetch_atom_service_spec.rb
+++ spec/services/fetch_atom_service_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe FetchAtomService do
+  it 'returns nil if URL is blank'
+  it 'processes URL'
+  it 'retries without ActivityPub'
+  it 'rescues SSL error'
+
+  it 'rescues HTTP error' do
+    expect(FetchAtomService.new.call('invalid')).to eq nil
+  end
 end
