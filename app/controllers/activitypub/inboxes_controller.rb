e994d15ba01b6aab326ab2f88cd3f183dd45374d
Thibaut Girka <thib@sitedethib.com>
2017-10-03 18:03:17 +0200

    Return sensible HTTP status for ActivityPub inbox processing

--- app/controllers/activitypub/inboxes_controller.rb
+++ app/controllers/activitypub/inboxes_controller.rb
@@ -9,9 +9,9 @@ class ActivityPub::InboxesController < Api::BaseController
     if signed_request_account
       upgrade_account
       process_payload
-      head 201
-    else
       head 202
+    else
+      head 401
     end
   end
 
