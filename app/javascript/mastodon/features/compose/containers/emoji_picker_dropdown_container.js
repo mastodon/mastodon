d2dee6ea431f52ab90c568b21b25169d5129c7a6
Eugen Rochko <eugen@zeonfederated.com>
2017-10-07 19:02:30 +0200

    Fix custom emoji in emoji picker, persist skin tone (#5258)

--- app/javascript/mastodon/features/compose/containers/emoji_picker_dropdown_container.js
+++ app/javascript/mastodon/features/compose/containers/emoji_picker_dropdown_container.js
@@ -1,9 +1,17 @@
 import { connect } from 'react-redux';
 import EmojiPickerDropdown from '../components/emoji_picker_dropdown';
+import { changeSetting } from '../../../actions/settings';
 
 const mapStateToProps = state => ({
   custom_emojis: state.get('custom_emojis'),
   autoPlay: state.getIn(['meta', 'auto_play_gif']),
+  skinTone: state.getIn(['settings', 'skinTone']),
 });
 
-export default connect(mapStateToProps)(EmojiPickerDropdown);
+const mapDispatchToProps = dispatch => ({
+  onSkinTone: skinTone => {
+    dispatch(changeSetting(['skinTone'], skinTone));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EmojiPickerDropdown);
