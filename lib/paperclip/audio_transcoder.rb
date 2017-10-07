85d5da86dfd3589e345a68c19fff7d7bc2524532
DJ Sundog <sundog@reclaim.technology>
2017-10-07 16:14:34 -0700

    raise Mastodon::ValidationError if audio file duration is over 60 secs

--- lib/paperclip/audio_transcoder.rb
+++ lib/paperclip/audio_transcoder.rb
@@ -3,6 +3,12 @@
 module Paperclip
   class AudioTranscoder < Paperclip::Processor
     def make
+      meta = ::Av.cli.identify(@file.path)
+      # {:length=>"0:00:02.14", :duration=>2.14, :audio_encode=>"mp3", :audio_bitrate=>"44100 Hz", :audio_channels=>"mono"}
+      if meta[:duration] > 60.0
+        raise Mastodon::ValidationError, "Audio uploads must be less than 60 seconds in length."
+      end
+      
       final_file = Paperclip::Transcoder.make(file, options, attachment)
       
       attachment.instance.file_file_name    = 'media.mp4'
