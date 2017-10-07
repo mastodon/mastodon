96ba3482b96504d7a8e2a7dc7dbfea41c86be74f
DJ Sundog <djsundog@gmail.com>
2017-10-07 19:54:10 +0000

    adding support for audio uploads, transcoded to mp4 videos

---
+++ lib/paperclip/audio_transcoder.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Paperclip
+  class AudioTranscoder < Paperclip::Processor
+    def make
+       final_file = Paperclip::Transcoder.make(file, options, attachment)
+
+       attachment.instance.file_file_name    = 'media.mp4'
+       attachment.instance.file_content_type = 'video/mp4'
+       attachment.instance.type              = MediaAttachment.types[:video]
+
+       final_file
+    end
+  end
+end
