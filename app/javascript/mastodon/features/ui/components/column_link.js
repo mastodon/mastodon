7de6d269d21e0ed7a3ede7c3d17089726a29d841
Nolan Lawson <nolan@nolanlawson.com>
2017-10-07 17:55:58 -0700

    Use ES module build of react-router-dom (#5264)

--- app/javascript/mastodon/features/ui/components/column_link.js
+++ app/javascript/mastodon/features/ui/components/column_link.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
 
 const ColumnLink = ({ icon, text, to, href, method }) => {
   if (href) {
