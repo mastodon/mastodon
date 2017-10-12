edd1a00fafb5d41afad0c177e7c2f2f6adae6d19
David Yip <yipdw@member.fsf.org>
2017-10-12 04:27:45 -0500

    Restore variable assignment in MuteService#call.
    291feba6f113588cce4f06206754b31eba60044b made MuteService return the
    result of Account#mute!; this commit restores that behavior.

--- app/services/mute_service.rb
+++ app/services/mute_service.rb
@@ -3,7 +3,7 @@
 class MuteService < BaseService
   def call(account, target_account, notifications: nil)
     return if account.id == target_account.id
-    account.mute!(target_account, notifications: notifications)
+    mute = account.mute!(target_account, notifications: notifications)
     BlockWorker.perform_async(account.id, target_account.id)
     mute
   end
