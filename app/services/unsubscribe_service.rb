2eab41cd1a38fd382a735f32c6ecba7ec193f6f2
abcang <abcang1015@gmail.com>
2017-10-14 21:38:57 +0900

    Close connection when succeeded posting (#5390)
    * Close connection when succeeded posting
    
    * Update webmock

--- app/services/unsubscribe_service.rb
+++ app/services/unsubscribe_service.rb
@@ -7,9 +7,10 @@ class UnsubscribeService < BaseService
     @account = account
 
     begin
-      @response = build_request.perform.flush
+      @response = build_request.perform
 
       Rails.logger.debug "PuSH unsubscribe for #{@account.acct} failed: #{@response.status}" unless @response.status.success?
+      @response.connection&.close
     rescue HTTP::Error, OpenSSL::SSL::SSLError => e
       Rails.logger.debug "PuSH unsubscribe for #{@account.acct} failed: #{e}"
     end
