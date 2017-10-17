5534c0e190d6a9d6604c85f150a16d474675132c
Nanamachi <town7.haruki@gmail.com>
2017-10-17 17:00:17 +0900

    fix bug which occurs when regist. param. is invalid

--- app/services/request_approval_service.rb
+++ app/services/request_approval_service.rb
@@ -2,6 +2,10 @@
 
 class RequestApprovalService < BaseService
   def call(user)
+    if user.id == nil
+      return
+    end
+
     if Setting.require_approval
       User.admins.each do |admin|
         UserMailer.new_user_waiting_for_approval(admin, user).deliver_later
