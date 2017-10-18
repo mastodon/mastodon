2c623bce4ebea0f5abb5daca0132430de2ba90f9
ncls7615 <himasoto@gmail.com>
2017-10-19 00:39:29 +0900

    修正

--- app/javascript/mastodon/reducers/statuses.js
+++ app/javascript/mastodon/reducers/statuses.js
@@ -40,6 +40,10 @@ import {
 } from '../actions/pin_statuses';
 import { SEARCH_FETCH_SUCCESS } from '../actions/search';
 import emojify from '../features/emoji/emoji';
+import {
+  VOTE_SUCCESS,
+  SET_ENQUETE_TIMEOUT,
+} from '../actions/enquetes';
 import { Map as ImmutableMap, fromJS } from 'immutable';
 import escapeTextContentForBrowser from 'escape-html';
 
@@ -142,7 +146,7 @@ export default function statuses(state = initialState, action) {
   case ACCOUNT_BLOCK_SUCCESS:
   case ACCOUNT_MUTE_SUCCESS:
     return filterStatuses(state, action.relationship);
-    case VOTE_SUCCESS:
+  case VOTE_SUCCESS:
     return state
       .setIn([action.status_id, 'vote'], true)
       .setIn([action.status_id, 'voted_num'], action.item_index);
