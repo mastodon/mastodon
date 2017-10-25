4370a273b34ec9bc15388d90a509156c75d35bb2
Nolan Lawson <nolan@nolanlawson.com>
2017-10-25 11:07:01 -0700

    add Windows icons for PWABuilder

--- app/serializers/manifest_serializer.rb
+++ app/serializers/manifest_serializer.rb
@@ -24,28 +24,40 @@ class ManifestSerializer < ActiveModel::Serializer
   def icons
     [
       {
-        src: '/android-chrome-48x48.png',
+        src: 'https://toot.cafe/android-chrome-48x48.png',
         sizes: '48x48',
         type: 'image/png',
       }, {
-        src: '/android-chrome-72x72.png',
+        src: 'https://toot.cafe/android-chrome-72x72.png',
         sizes: '72x72',
         type: 'image/png',
       }, {
-        src: '/android-chrome-96x96.png',
+        src: 'https://toot.cafe/android-chrome-96x96.png',
         sizes: '96x96',
         type: 'image/png',
       }, {
-        src: '/android-chrome-144x144.png',
+        src: 'https://toot.cafe/android-chrome-144x144.png',
         sizes: '144x144',
         type: 'image/png',
       }, {
-        src: '/android-chrome-192x192.png',
+        src: 'https://toot.cafe/android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       }, {
-        src: '/android-chrome-512x512.png',
+        src: 'https://toot.cafe/android-chrome-512x512.png',
         sizes: '512x512',
+        type: 'image/png',
+      }, {
+        src: 'https://toot.cafe/windows-44x44.png',
+        sizes: '44x44',
+        type: 'image/png',
+      }, {
+        src: 'https://toot.cafe/windows-50x50.png',
+        sizes: '50x50',
+        type: 'image/png',
+      }, {
+        src: 'https://toot.cafe/windows-150x150.png',
+        sizes: '150x150',
         type: 'image/png',
       }
     ]
