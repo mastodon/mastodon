ecfb68e831dac79e1fb3bccd1bec4bd7c3bfc88c
Yosuke Kabuto <bluewhale1982@gmail.com>
2017-11-08 13:40:54 +0900

    Add a test for FollowRequest#authorize!

--- spec/models/follow_request_spec.rb
+++ spec/models/follow_request_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe FollowRequest, type: :model do
-  describe '#authorize!'
-  describe '#reject!'
+  describe '#authorize!' do
+    let(:follow_request) { Fabricate(:follow_request, account: account, target_account: target_account) }
+    let(:account)        { Fabricate(:account) }
+    let(:target_account) { Fabricate(:account) }
+
+    it 'calls Account#follow!, MergeWorker.perform_async, and #destroy!' do
+      expect(account).to        receive(:follow!).with(target_account)
+      expect(MergeWorker).to    receive(:perform_async).with(target_account.id, account.id)
+      expect(follow_request).to receive(:destroy!)
+      follow_request.authorize!
+    end
+  end
 
   describe 'validations' do
     it 'has a valid fabricator' do
