8a5fc4417b447f097a5a2cbdc748995e2f8402de
Yosuke Kabuto <bluewhale1982@gmail.com>
2017-11-10 13:50:36 +0900

    Add tests for PreviewCard#save_with_optional_image!

--- spec/models/preview_card_spec.rb
+++ spec/models/preview_card_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe PreviewCard, type: :model do
+  describe '#save_with_optional_image!' do
+    let(:preview_card) { Fabricate(:preview_card) }
 
+    context 'object is valid' do
+      it 'keeps image' do
+        preview_card.save_with_optional_image!
+        expect(preview_card.image.present?).to be true
+      end
+    end
+
+    context 'object is invalid' do
+      before do
+        c = 0
+
+        allow(preview_card).to receive(:save!) do
+          c += 1
+          raise ActiveRecord::RecordInvalid if c <= 1
+        end
+      end
+
+      it 'removes image' do
+        preview_card.save_with_optional_image!
+        expect(preview_card.image.present?).to be false
+      end
+    end
+  end
 end
