081b8b1f6ee7dcdc8a8fcdbcd3dfc6ecbc6c6d7f
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-11-14 15:44:52 +0900

    Refactor remote_follow_spec.rb

--- spec/models/remote_follow_spec.rb
+++ spec/models/remote_follow_spec.rb
@@ -3,83 +3,65 @@
 require 'rails_helper'
 
 RSpec.describe RemoteFollow do
+  before do
+    stub_request(:get, 'https://quitter.no/.well-known/webfinger?resource=acct:gargron@quitter.no').to_return(request_fixture('webfinger.txt'))
+  end
+
+  let(:attrs)         { nil }
+  let(:remote_follow) { described_class.new(attrs) }
+
   describe '.initialize' do
-    let(:remote_follow) { RemoteFollow.new(option) }
+    subject { remote_follow.acct }
 
-    context 'option with acct' do
-      let(:option) { { acct: 'hoge@example.com' } }
+    context 'attrs with acct' do
+      let(:attrs) { { acct: 'gargron@quitter.no' } }
 
-      it 'sets acct' do
-        expect(remote_follow.acct).to eq 'hoge@example.com'
+      it 'returns acct' do
+        is_expected.to eq 'gargron@quitter.no'
       end
     end
 
-    context 'option without acct' do
-      let(:option) { {} }
+    context 'attrs without acct' do
+      let(:attrs) { {} }
 
-      it 'does not set acct' do
-        expect(remote_follow.acct).to be_nil
+      it do
+        is_expected.to be_nil
       end
     end
   end
 
   describe '#valid?' do
-    let(:remote_follow) { RemoteFollow.new }
+    subject { remote_follow.valid? }
 
-    context 'super is falsy' do
-      module InvalidSuper
-        def valid?
-          nil
-        end
-      end
-
-      before do
-        class RemoteFollow
-          include InvalidSuper
-        end
-      end
-
-      it 'returns false without calling #populate_template and #errors' do
-        expect(remote_follow).not_to receive(:populate_template)
-        expect(remote_follow).not_to receive(:errors)
-        expect(remote_follow.valid?).to be false
+    context 'attrs with acct' do
+      let(:attrs) { { acct: 'gargron@quitter.no' }}
+      
+      it do
+        is_expected.to be true
       end
     end
 
-    context 'super is truthy' do
-      module ValidSuper
-        def valid?
-          true
-        end
-      end
+    context 'attrs without acct' do
+      let(:attrs) { { } }
 
-      before do
-        class RemoteFollow
-          include ValidSuper
-        end
-      end
-
-      it 'calls #populate_template and #errors.empty?' do
-        expect(remote_follow).to receive(:populate_template)
-        expect(remote_follow).to receive_message_chain(:errors, :empty?)
-        remote_follow.valid?
+      it do
+        is_expected.to be false
       end
     end
   end
 
   describe '#subscribe_address_for' do
     before do
-      allow(remote_follow).to receive(:addressable_template).and_return(addressable_template)
+      remote_follow.valid?
     end
 
-    let(:account)                   { instance_double('Account', local_username_and_domain: local_username_and_domain) }
-    let(:addressable_template)      { instance_double('Addressable::Template') }
-    let(:local_username_and_domain) { 'hoge@example.com' }
-    let(:remote_follow)             { RemoteFollow.new }
+    let(:attrs)   { { acct: 'gargron@quitter.no' } }
+    let(:account) { Fabricate(:account, username: 'alice') }
 
-    it 'calls Addressable::Template#expand.to_s' do
-      expect(addressable_template).to receive_message_chain(:expand, :to_s).with(uri: local_username_and_domain).with(no_args)
-      remote_follow.subscribe_address_for(account)
+    subject { remote_follow.subscribe_address_for(account) }
+
+    it 'returns subscribe address' do
+      is_expected.to eq 'https://quitter.no/main/ostatussub?profile=alice%40cb6e6126.ngrok.io'
     end
   end
 end
