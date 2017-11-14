81e87cc4960284d1d0a4f54a40858d26f5b0cae0
Yosuke Kabuto <bluewhale1982@gmail.com>
2017-11-14 14:23:18 +0900

    Add tests for StreamEntry
    - `#object_type`
    - `#verb`
    - `#mentions`

--- spec/models/stream_entry_spec.rb
+++ spec/models/stream_entry_spec.rb
@@ -6,6 +6,88 @@ RSpec.describe StreamEntry, type: :model do
   let(:status)    { Fabricate(:status, account: alice) }
   let(:reblog)    { Fabricate(:status, account: bob, reblog: status) }
   let(:reply)     { Fabricate(:status, account: bob, thread: status) }
+  let(:stream_entry) { Fabricate(:stream_entry, activity: reply) }
+
+  describe '#object_type' do
+    before do
+      allow(stream_entry).to receive(:orphaned?).and_return(orphaned)
+      allow(stream_entry).to receive(:targeted?).and_return(targeted)
+    end
+
+    context 'orphaned? is true' do
+      let(:orphaned) { true }
+      let(:targeted) { false }
+
+      it 'returns :activity' do
+        expect(stream_entry.object_type).to be :activity
+      end
+    end
+
+    context 'targeted? is true' do
+      let(:orphaned) { false }
+      let(:targeted) { true }
+
+      it 'returns :activity' do
+        expect(stream_entry.object_type).to be :activity
+      end
+    end
+
+    context 'orphaned? and targeted? are false' do
+      let(:orphaned) { false }
+      let(:targeted) { false }
+
+      it 'calls status.object_type' do
+        expect(stream_entry).to receive_message_chain(:status, :object_type)
+        stream_entry.object_type
+      end
+    end
+  end
+
+  describe '#verb' do
+    before do
+      allow(stream_entry).to receive(:orphaned?).and_return(orphaned)
+    end
+
+    context 'orphaned? is true' do
+      let(:orphaned) { true }
+
+      it 'returns :delete' do
+        expect(stream_entry.verb).to be :delete
+      end
+    end
+
+    context 'orphaned? is false' do
+      let(:orphaned) { false }
+
+      it 'calls status.verb' do
+        expect(stream_entry).to receive_message_chain(:status, :verb)
+        stream_entry.verb
+      end
+    end
+  end
+
+  describe '#mentions' do
+    before do
+      allow(stream_entry).to receive(:orphaned?).and_return(orphaned)
+    end
+
+    context 'orphaned? is true' do
+      let(:orphaned) { true }
+
+      it 'returns []' do
+        expect(stream_entry.mentions).to eq []
+      end
+    end
+
+    context 'orphaned? is false' do
+      let(:orphaned) { false }
+
+      it 'calls status.mentions.map' do
+        expect(stream_entry).to receive_message_chain(:status, :mentions, :map)
+        stream_entry.mentions
+      end
+    end
+  end
 
   describe '#targeted?' do
     it 'returns true for a reblog' do
