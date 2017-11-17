dc6e031364d1f47a49ea47f20070a4e9986a4c68
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-11-17 11:26:34 +0900

    Redirect to reports when moderator (#5733)

--- config/routes.rb
+++ config/routes.rb
@@ -161,7 +161,13 @@ Rails.application.routes.draw do
     resources :account_moderation_notes, only: [:create, :destroy]
   end
 
-  get '/admin', to: redirect('/admin/settings/edit', status: 302)
+  authenticate :user, lambda { |u| u.admin? } do
+    get '/admin', to: redirect('/admin/settings/edit', status: 302)
+  end
+
+  authenticate :user, lambda { |u| u.moderator? } do
+    get '/admin', to: redirect('/admin/reports', status: 302)
+  end
 
   namespace :api do
     # PubSubHubbub outgoing subscriptions
