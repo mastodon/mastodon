7b97b2e84dcecfa2b8089823586ccf54ea29f6d9
Osamu Koga <osak.63@gmail.com>
2017-11-25 00:18:33 +0900

    末代のマネをしてお知らせを出すようにした

---
+++ app/javascript/mastodon/features/compose/components/announcements.js
@@ -0,0 +1,102 @@
+// Copied & Modified from https://github.com/lindwurm/mastodon
+import React from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import Immutable from 'immutable';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import classnames from 'classnames';
+
+class Announcement extends React.PureComponent {
+
+  static propTypes = {
+    item: ImmutablePropTypes.map,
+  }
+
+  render() {
+    const { item } = this.props;
+
+    const contents = [];
+    contents.push(<div key='body' className='announcements__body'>{item.get('text')}</div>);
+    if (item.get('icon')) {
+      contents.push(
+        <div key='icon' className='announcements__icon'>
+          <img src={item.get('icon')} alt='' />
+        </div>
+      );
+    }
+
+    const href = item.get('href');
+
+    const classname = classnames({
+      'announcements__item': true,
+      'announcements__item--clickable': !!href,
+    });
+
+    if (!href) {
+      return (<div className={classname}>{contents}</div>);
+    } else if (href.startsWith('/web/')) {
+      return (<Link to={item.get('href').slice(4)} className={classname}>{contents}</Link>);
+    } else {
+      return (<a href={item.get('url')} target='_blank' className={classname}>{contents}</a>);
+    }
+  }
+
+}
+
+const unexpired = (items, now) => items.filter((entry) => new Date(entry.get('expire')).getTime() > now);
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
+export default class Announcements extends React.PureComponent {
+
+  state = {
+    items: Immutable.List(),
+  }
+
+  constructor () {
+    super();
+    this.refresh();
+  }
+
+  componentWillUnmount() {
+    this.cancelPolling();
+  }
+
+  updateAnnouncements = (items) => {
+    const validItems = unexpired(items, Date.now());
+    this.setState({ items: validItems });
+    const timeout = validItems.isEmpty() ? ONE_DAY : new Date(validItems.get(0, 'expire')).getTime() - Date.now();
+    this.timer = setTimeout(this.refresh, timeout);
+  }
+
+  cancelPolling = () => {
+    if (this.timer !== null) {
+      clearTimeout(this.timer);
+      this.timer = null;
+    }
+  }
+
+  refresh = () => {
+    this.timer = null;
+
+    axios.get('https://mikutter.hachune.net/notification.json?platform=mastodon')
+      .then(resp => this.updateAnnouncements(Immutable.fromJS(resp.data) || Immutable.List()))
+      .catch(err => console.warn(err));
+  }
+
+  render() {
+    const { items } = this.state;
+
+    if (items.isEmpty()) {
+      return null;
+    }
+
+    return (
+      <ul className='announcements'>
+        <li>
+          <Announcement item={items.get(0)} />
+        </li>
+      </ul>
+    );
+  }
+
+}
