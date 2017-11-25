32987004c95aebfc390b7cd9e93d9a386095c0a0
William Pitcock <nenolod@dereferenced.org>
2017-11-24 18:36:08 -0600

    status: preserve visibility attribute when reblogging (infoleak fix) (#5789)
    this should fix *all* remaining visibility-related mastodon ostatus infoleaks.
    thanks to @csaurus@gnusocial.de for pointing out the infoleak.

--- app/models/status.rb
+++ app/models/status.rb
@@ -278,6 +278,7 @@ class Status < ApplicationRecord
 
   def set_visibility
     self.visibility = (account.locked? ? :private : :public) if visibility.nil?
+    self.visibility = reblog.visibility if reblog?
     self.sensitive  = false if sensitive.nil?
   end
 
