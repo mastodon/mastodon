1c42640f24255813175e747c4173e18225398975
unarist <m.unarist@gmail.com>
2017-11-28 03:23:14 +0900

    Merge indexes for reblog on statuses table
    We added an index for `[account_id, reblog_of_id]`, but we already have a similar index for `reblog_of_id`. Those index will be bigger according to statuses count. For example, `reblog_of_id` index uses 800MB for 10GB statuses table.
    
    So this patch swaps indexed columns like `[reblog_of_id, account_id]`, then it will covers both usage with single index.
    
    Since those index creation may take a while, I've also disabled previous index creation.

--- db/migrate/20171122120436_add_index_account_and_reblog_of_id_to_statuses.rb
+++ db/migrate/20171122120436_add_index_account_and_reblog_of_id_to_statuses.rb
@@ -1,6 +1,12 @@
 class AddIndexAccountAndReblogOfIdToStatuses < ActiveRecord::Migration[5.1]
-  def change
-    commit_db_transaction
-    add_index :statuses, [:account_id, :reblog_of_id], algorithm: :concurrently
+  disable_ddl_transaction!
+
+  def up
+    # This index has been superseded by migration 20171125185353
+    # add_index :statuses, [:account_id, :reblog_of_id], algorithm: :concurrently
+  end
+
+  def down
+    remove_index :statuses, [:account_id, :reblog_of_id] if index_exists?(:statuses, [:account_id, :reblog_of_id])
   end
 end
