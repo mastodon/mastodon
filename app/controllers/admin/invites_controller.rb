c811e9c731fe6816dcec70f1e18bb52e157fcf17
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-12-01 17:22:49 +0900

    Fix invites form path

--- app/controllers/admin/invites_controller.rb
+++ app/controllers/admin/invites_controller.rb
@@ -29,5 +29,11 @@ module Admin
       @invite.expire!
       redirect_to admin_invites_path
     end
+
+    private
+
+    def resource_params
+      params.require(:invite).permit(:max_uses, :expires_in)
+    end
   end
 end
