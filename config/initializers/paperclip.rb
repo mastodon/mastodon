bd42ccbdced5650b5356a55e57da638d46eb8df3
theboss <luckybeastakatheboss@gmail.com>
2017-12-09 12:28:36 +0900

    Fix typo in paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -19,7 +19,7 @@ if ENV['S3_ENABLED'] == 'true'
 
   s3_region   = ENV.fetch('S3_REGION')   { 'us-east-1' }
   s3_protocol = ENV.fetch('S3_PROTOCOL') { 'https' }
-  s3_hostname = ENV.fetch('S3_HOSTNAME') { "s3-#{s3_region}}.amazonaws.com" }
+  s3_hostname = ENV.fetch('S3_HOSTNAME') { "s3-#{s3_region}.amazonaws.com" }
 
   Paperclip::Attachment.default_options.merge!(
     storage: :s3,
