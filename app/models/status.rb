24f36ca9124353fdc3430c48b4afbb759276739d
Erin <sylphofelectricity@gmail.com>
2017-12-10 19:41:33 -0600

    Status#not_local_only scope should match nils too

--- app/models/status.rb
+++ app/models/status.rb
@@ -75,7 +75,7 @@ class Status < ApplicationRecord
   scope :not_excluded_by_account, ->(account) { where.not(account_id: account.excluded_from_timeline_account_ids) }
   scope :not_domain_blocked_by_account, ->(account) { account.excluded_from_timeline_domains.blank? ? left_outer_joins(:account) : left_outer_joins(:account).where('accounts.domain IS NULL OR accounts.domain NOT IN (?)', account.excluded_from_timeline_domains) }
 
-  scope :not_local_only, -> { where(local_only: false) }
+  scope :not_local_only, -> { where(local_only: [false, nil]) }
 
   cache_associated :account, :application, :media_attachments, :tags, :stream_entry, mentions: :account, reblog: [:account, :application, :stream_entry, :tags, :media_attachments, mentions: :account], thread: :account
 
