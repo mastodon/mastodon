0aeec0390b39d2367dd79bf85cfd7457ab6a9922
Lynx Kotoura <admin@sanin.link>
2017-12-14 01:37:23 +0900

    Redesign tootbox (#5919)
    * Redesign tootbox
    
    * Move counter into compose-form__buttons-wrapper
    
    Change font and remove shadow
    Refactor sass codes of compose-form

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -199,11 +199,11 @@ export default class ComposeForm extends ImmutablePureComponent {
             <SensitiveButtonContainer />
             <SpoilerButtonContainer />
           </div>
+          <div className='character-counter__wrapper'><CharacterCounter max={500} text={text} /></div>
+        </div>
 
-          <div className='compose-form__publish'>
-            <div className='character-counter__wrapper'><CharacterCounter max={500} text={text} /></div>
-            <div className='compose-form__publish-button-wrapper'><Button text={publishText} onClick={this.handleSubmit} disabled={disabled || this.props.is_uploading || length(text) > 500 || (text.length !== 0 && text.trim().length === 0)} block /></div>
-          </div>
+        <div className='compose-form__publish'>
+          <div className='compose-form__publish-button-wrapper'><Button text={publishText} onClick={this.handleSubmit} disabled={disabled || this.props.is_uploading || length(text) > 500 || (text.length !== 0 && text.trim().length === 0)} block /></div>
         </div>
       </div>
     );
