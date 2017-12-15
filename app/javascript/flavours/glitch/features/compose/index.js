e677dda07c55f085bcc67cef57ceb024862a729c
kibigo! <marrus-sh@users.noreply.github.com>
2017-12-15 12:49:23 -0800

    Fixes #244 to make search results scrollable

--- app/javascript/flavours/glitch/features/compose/index.js
+++ app/javascript/flavours/glitch/features/compose/index.js
@@ -112,7 +112,7 @@ export default class Compose extends React.PureComponent {
 
           <Motion defaultStyle={{ x: -100 }} style={{ x: spring(showSearch ? 0 : -100, { stiffness: 210, damping: 20 }) }}>
             {({ x }) =>
-              <div className='drawer__inner darker' style={{ transform: `translateX(${x}%)`, visibility: x === -100 ? 'hidden' : 'visible' }}>
+              <div className='drawer__inner darker scrollable optionally-scrollable' style={{ transform: `translateX(${x}%)`, visibility: x === -100 ? 'hidden' : 'visible' }}>
                 <SearchResultsContainer />
               </div>
             }
