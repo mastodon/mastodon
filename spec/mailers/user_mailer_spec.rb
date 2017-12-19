fe195d59f9cbd63d489f3afed7c867541e18a811
Patrick Figel <patrick@figel.email>
2017-12-19 19:21:11 +0100

    Add confirmation step for email changes
    This adds a confirmation step for email changes of existing users.
    Like the initial account confirmation, a confirmation link is sent
    to the new address.
    
    Additionally, a notification is sent to the existing address when
    the change is initiated. This message includes instruction to reset
    the password immediately or to contact the instance admin if the
    change was not initiated by the account owner.
    
    Fixes #3871

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -33,6 +33,20 @@ describe UserMailer, type: :mailer do
                      instance: Rails.configuration.x.local_domain
   end
 
+  describe 'reconfirmation_instructions' do
+    let(:mail) { UserMailer.confirmation_instructions(receiver, 'spec') }
+
+    it 'renders reconfirmation instructions' do
+      receiver.update!(email: 'new-email@example.com', locale: nil)
+      expect(mail.body.encoded).to include 'new-email@example.com'
+      expect(mail.body.encoded).to include 'spec'
+      expect(mail.body.encoded).to include Rails.configuration.x.local_domain
+      expect(mail.subject).to eq I18n.t('devise.mailer.reconfirmation_instructions.subject',
+                                        instance: Rails.configuration.x.local_domain,
+                                        locale: I18n.default_locale)
+    end
+  end
+
   describe 'reset_password_instructions' do
     let(:mail) { UserMailer.reset_password_instructions(receiver, 'spec') }
 
@@ -56,5 +70,17 @@ describe UserMailer, type: :mailer do
 
     include_examples 'localized subject',
                      'devise.mailer.password_change.subject'
+  end
+
+  describe 'email_changed' do
+    let(:mail) { UserMailer.email_changed(receiver) }
+
+    it 'renders email change notification' do
+      receiver.update!(locale: nil)
+      expect(mail.body.encoded).to include receiver.email
+    end
+
+    include_examples 'localized subject',
+                     'devise.mailer.email_changed.subject'
   end
 end
