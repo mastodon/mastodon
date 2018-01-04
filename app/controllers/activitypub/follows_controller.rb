c10f4bdb037d87444a76e52e85f046e7e59d753a
Eugen Rochko <eugen@zeonfederated.com>
2018-01-04 01:21:38 +0100

    Cache JSON of immutable ActivityPub representations (#6171)

--- app/controllers/activitypub/follows_controller.rb
+++ app/controllers/activitypub/follows_controller.rb
@@ -4,15 +4,19 @@ class ActivityPub::FollowsController < Api::BaseController
   include SignatureVerification
 
   def show
-    render(
-      json: FollowRequest.includes(:account).references(:account).find_by!(
-        id: params.require(:id),
-        accounts: { domain: nil, username: params.require(:account_username) },
-        target_account: signed_request_account
-      ),
-      serializer: ActivityPub::FollowSerializer,
-      adapter: ActivityPub::Adapter,
-      content_type: 'application/activity+json'
+    render json: follow_request,
+           serializer: ActivityPub::FollowSerializer,
+           adapter: ActivityPub::Adapter,
+           content_type: 'application/activity+json'
+  end
+
+  private
+
+  def follow_request
+    FollowRequest.includes(:account).references(:account).find_by!(
+      id: params.require(:id),
+      accounts: { domain: nil, username: params.require(:account_username) },
+      target_account: signed_request_account
     )
   end
 end
