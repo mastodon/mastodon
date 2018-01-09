a634fb1c756aece475c90c36fc796ecda721560a
Eugen Rochko <eugen@zeonfederated.com>
2018-01-09 17:25:20 +0100

    Skip OStatus reblogs of non-public statuses

--- app/lib/ostatus/activity/creation.rb
+++ app/lib/ostatus/activity/creation.rb
@@ -26,6 +26,9 @@ class OStatus::Activity::Creation < OStatus::Activity::Base
     cached_reblog = reblog
     status = nil
 
+    # Skip if the reblogged status is not public
+    return if cached_reblog && !(cached_reblog.public_visibility? || cached_reblog.unlisted_visibility?)
+
     media_attachments = save_media
 
     ApplicationRecord.transaction do
