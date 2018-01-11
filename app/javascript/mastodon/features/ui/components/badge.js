eafb3e73b3d28f8fb652a239c50052e8f6049ce6
kjwon15 <kjwonmail@gmail.com>
2018-01-12 03:56:48 +0900

    Implement follow_requests badge
    Related: #6242

---
+++ app/javascript/mastodon/features/ui/components/badge.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class Badge extends React.Component {
+  static propTypes = {
+    text: PropTypes.string,
+    update: PropTypes.func,
+  }
+
+  state = {
+    text: this.props.text,
+  }
+
+  componentWillMount () {
+    let result = this.props.update();
+    if (result instanceof Promise) {
+      result.then(response => {
+        this.setState({
+          text: response,
+        });
+      });
+    } else {
+      this.setState({
+        text: this.props.update(),
+      });
+    }
+  }
+
+  render() {
+    let { text } = this.state;
+    if (!text) {
+      return null;
+    }
+
+    return (
+      <span className='badge'>
+        {text}
+      </span>
+    );
+  }
+};
