a1657998fa2bfd52da1ee3d5bd2384bd6e372830
rinsuki <428rinsuki+git@gmail.com>
2018-01-17 14:32:56 +0900

    web share target

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -14,13 +14,20 @@ class SharesController < ApplicationController
   private
 
   def initial_state_params
+    text = params[:text]
+    if params[:title]
+      text = params[:title] + ' ' + text
+    end
+    if params[:url]
+      text += ' ' + params[:url]
+    end
     {
       settings: Web::Setting.find_by(user: current_user)&.data || {},
       push_subscription: current_account.user.web_push_subscription(current_session),
       current_account: current_account,
       token: current_session.token,
       admin: Account.find_local(Setting.site_contact_username),
-      text: params[:text],
+      text: text,
     }
   end
 
