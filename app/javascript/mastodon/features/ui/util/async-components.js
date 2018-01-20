79b34a0fa2e993967f22892b857dcf601c07bd88
Eugen Rochko <eugen@zeonfederated.com>
2018-01-20 01:32:37 +0100

    Restore onboarding modal (#6303)
    * Restore onboarding modal
    Revert 5ba8b3a396895ecec083c5258aaf9084d584a7c4
    
    * Change greeting elephant graphic, fix up some design issues
    
    * Fix wrong link color in onboarding modal

--- app/javascript/mastodon/features/ui/util/async-components.js
+++ app/javascript/mastodon/features/ui/util/async-components.js
@@ -94,6 +94,10 @@ export function Mutes () {
   return import(/* webpackChunkName: "features/mutes" */'../../mutes');
 }
 
+export function OnboardingModal () {
+  return import(/* webpackChunkName: "modals/onboarding_modal" */'../components/onboarding_modal');
+}
+
 export function MuteModal () {
   return import(/* webpackChunkName: "modals/mute_modal" */'../components/mute_modal');
 }
