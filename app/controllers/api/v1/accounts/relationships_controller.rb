3f35d4322266ee6f1bfab73a1161af2b0848573a
abcang <abcang1015@gmail.com>
2018-02-04 20:32:10 +0900

    Exclude nil from relationships array (#6427)

--- app/controllers/api/v1/accounts/relationships_controller.rb
+++ app/controllers/api/v1/accounts/relationships_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::Accounts::RelationshipsController < Api::BaseController
     accounts = Account.where(id: account_ids).select('id')
     # .where doesn't guarantee that our results are in the same order
     # we requested them, so return the "right" order to the requestor.
-    @accounts = accounts.index_by(&:id).values_at(*account_ids)
+    @accounts = accounts.index_by(&:id).values_at(*account_ids).compact
     render json: @accounts, each_serializer: REST::RelationshipSerializer, relationships: relationships
   end
 
