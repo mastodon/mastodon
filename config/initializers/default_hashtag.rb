862c5a8a8859cc5c85fbe26a7f7fc84ad2dd4127
theboss <luckybeastakatheboss@gmail.com>
2018-02-05 21:12:54 +0900

    Check Tag.table_exists? in default_hashtag initializer
    Fix bug: error occur while initial db:setup

--- config/initializers/default_hashtag.rb
+++ config/initializers/default_hashtag.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.x.default_hashtag = ENV['DEFAULT_HASHTAG']
-  config.x.default_hashtag_id = Tag.find_by(name: ENV['DEFAULT_HASHTAG'].downcase)
+  if Tag.table_exists?
+    config.x.default_hashtag = ENV['DEFAULT_HASHTAG']
+    config.x.default_hashtag_id = Tag.find_by(name: ENV['DEFAULT_HASHTAG'].downcase)
+  end
 end
