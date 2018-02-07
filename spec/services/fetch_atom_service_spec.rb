2bb393684baf5da1112923db7bcda920d606be78
Kazushige Tominaga <kazu9su@gmail.com>
2018-02-08 08:17:53 +0900

    Added #link_header spec (#6439)

--- spec/services/fetch_atom_service_spec.rb
+++ spec/services/fetch_atom_service_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe FetchAtomService do
+  describe '#link_header' do
+    context 'Link is Array' do
+      target = FetchAtomService.new
+      target.instance_variable_set('@response', 'Link' => [
+        '<http://example.com/>; rel="up"; meta="bar"',
+        '<http://example.com/foo>; rel="self"',
+      ])
+
+      it 'set first link as link_header' do
+        expect(target.send(:link_header).links[0].href).to eq 'http://example.com/'
+      end
+    end
+
+    context 'Link is not Array' do
+      target = FetchAtomService.new
+      target.instance_variable_set('@response', 'Link' => '<http://example.com/foo>; rel="self", <http://example.com/>; rel = "up"')
+
+      it { expect(target.send(:link_header).links[0].href).to eq 'http://example.com/foo' }
+    end
+  end
 end
