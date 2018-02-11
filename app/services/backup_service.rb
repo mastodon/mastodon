549a36ceaf480e90e5edac5f1a0e4cc67e30a845
Eugen Rochko <eugen@zeonfederated.com>
2018-02-11 22:04:45 +0100

    Export actor and private key in the archive

--- app/services/backup_service.rb
+++ app/services/backup_service.rb
@@ -17,7 +17,7 @@ class BackupService < BaseService
   private
 
   def build_json!
-    @collection = ActiveModelSerializers::SerializableResource.new(collection_presenter, serializer: ActivityPub::CollectionSerializer, adapter: ActivityPub::Adapter).as_json
+    @collection = serialize(collection_presenter, ActivityPub::CollectionSerializer)
   end
 
   def process!
@@ -36,20 +36,9 @@ class BackupService < BaseService
     File.open(tmp_file, 'wb') do |file|
       Zlib::GzipWriter.wrap(file) do |gz|
         Gem::Package::TarWriter.new(gz) do |tar|
-          MediaAttachment.attached.where(account: account).find_each do |m|
-            file = Paperclip.io_adapters.for(m.file).read
-            name = File.basename(m.file.path)
-
-            tar.add_file_simple(File.join('media', name), 0o444, file.size) do |io|
-              io.write(file)
-            end
-          end
-
-          json = Oj.dump(collection)
-
-          tar.add_file_simple('collection.json', 0o444, json.size) do |io|
-            io.write(json)
-          end
+          dump_media_attachments!(tar)
+          dump_outbox!(tar)
+          dump_actor!(tar)
         end
       end
     end
@@ -62,6 +51,58 @@ class BackupService < BaseService
     tmp_file.unlink
   end
 
+  def dump_media_attachments!(tar)
+    MediaAttachment.attached.where(account: account).find_each do |m|
+      file = Paperclip.io_adapters.for(m.file).read
+      name = File.basename(m.file.path)
+
+      tar.add_file_simple(File.join('media', name), 0o444, file.size) do |io|
+        io.write(file)
+      end
+    end
+  end
+
+  def dump_outbox!(tar)
+    json = Oj.dump(collection)
+
+    tar.add_file_simple('outbox.json', 0o444, json.size) do |io|
+      io.write(json)
+    end
+  end
+
+  def dump_actor!(tar)
+    actor = serialize(account, ActivityPub::ActorSerializer)
+
+    actor[:icon][:url]  = 'avatar' + File.extname(actor[:icon][:url])  if actor[:icon]
+    actor[:image][:url] = 'header' + File.extname(actor[:image][:url]) if actor[:image]
+
+    if account.avatar.exists?
+      file = Paperclip.io_adapters.for(account.avatar).read
+
+      tar.add_file_simple('avatar' + File.extname(account.avatar.path), 0o444, file.size) do |io|
+        io.write(file)
+      end
+    end
+
+    if account.header.exists?
+      file = Paperclip.io_adapters.for(account.header).read
+
+      tar.add_file_simple('header' + File.extname(account.header.path), 0o444, file.size) do |io|
+        io.write(file)
+      end
+    end
+
+    json = Oj.dump(actor)
+
+    tar.add_file_simple('actor.json', 0o444, json.size) do |io|
+      io.write(json)
+    end
+
+    tar.add_file_simple('key.pem', 0o444, account.private_key.size) do |io|
+      io.write(account.private_key)
+    end
+  end
+
   def collection_presenter
     ActivityPub::CollectionPresenter.new(
       id: account_outbox_url(account),
@@ -69,5 +110,13 @@ class BackupService < BaseService
       size: account.statuses_count,
       items: account.statuses.with_includes
     )
+  end
+
+  def serialize(object, serializer)
+    ActiveModelSerializers::SerializableResource.new(
+      object,
+      serializer: serializer,
+      adapter: ActivityPub::Adapter
+    ).as_json
   end
 end
