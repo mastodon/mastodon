62dd4ca24343230fd99ef8d5e2939dd5f02900d8
lindwurm <lindwurm.q@gmail.com>
2018-02-11 18:49:00 +0900

    [maud] track our source_base_url
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,12 +33,12 @@ module Mastodon
     end
 
     def source_base_url
-      'https://github.com/tootsuite/mastodon'
+      'https://github.com/lindwurm/mastodon'
     end
 
     # specify git tag or commit hash here
     def source_tag
-      nil
+      "hota/master"
     end
 
     def source_url
