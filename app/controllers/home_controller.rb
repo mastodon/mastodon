aa3c857b7b25495746cac9f326e1de52a3cc543a
北䑓如法 <algebraicallyClosedField@gmail.com>
2018-02-28 18:03:09 +0900

    Fix a missing commit when merging mastodon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -37,21 +37,6 @@ class HomeController < ApplicationController
     redirect_to(default_redirect_path)
   end
 
-  def set_initial_state_json
-    serializable_resource = ActiveModelSerializers::SerializableResource.new(InitialStatePresenter.new(initial_state_params), serializer: InitialStateSerializer)
-    @initial_state_json   = serializable_resource.to_json
-  end
-
-  def initial_state_params
-    {
-      settings: Web::Setting.find_by(user: current_user)&.data || {},
-      push_subscription: current_account.user.web_push_subscription(current_session),
-      current_account: current_account,
-      token: current_session.token,
-      admin: Account.find_local(Setting.site_contact_username),
-    }
-  end
-
   def default_redirect_path
     if request.path.start_with?('/web')
       new_user_session_path
