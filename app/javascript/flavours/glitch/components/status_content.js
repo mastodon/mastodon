841ef606a9b1bc3390830643606b3ec9d65d5120
Thibaut Girka <thib@sitedethib.com>
2018-03-12 18:39:07 +0100

    Remove text requirement when media attached from statuses (fixes #381)
    Ports cfa9b6e13ab3c434f3901df6f614d0aa94a3d1ed to glitchsoc

--- app/javascript/flavours/glitch/components/status_content.js
+++ app/javascript/flavours/glitch/components/status_content.js
@@ -23,7 +23,12 @@ export default class StatusContent extends React.PureComponent {
   };
 
   _updateStatusLinks () {
-    const node  = this.node;
+    const node = this.node;
+
+    if (!node) {
+      return;
+    }
+
     const links = node.querySelectorAll('a');
 
     for (var i = 0; i < links.length; ++i) {
@@ -125,6 +130,10 @@ export default class StatusContent extends React.PureComponent {
       parseClick,
       disabled,
     } = this.props;
+
+    if (status.get('content').length === 0) {
+      return null;
+    }
 
     const hidden = this.props.setExpansion ? !this.props.expanded : this.state.hidden;
 
--- app/javascript/flavours/glitch/reducers/compose.js
+++ app/javascript/flavours/glitch/reducers/compose.js
@@ -158,7 +158,6 @@ function appendMedia(state, media) {
     map.update('media_attachments', list => list.push(media));
     map.set('is_uploading', false);
     map.set('resetFileKey', Math.floor((Math.random() * 0x10000)));
-    map.update('text', oldText => `${oldText.trim()} ${media.get('text_url')}`);
     map.set('focusDate', new Date());
     map.set('idempotencyKey', uuid());
 
@@ -169,12 +168,10 @@ function appendMedia(state, media) {
 };
 
 function removeMedia(state, mediaId) {
-  const media    = state.get('media_attachments').find(item => item.get('id') === mediaId);
   const prevSize = state.get('media_attachments').size;
 
   return state.withMutations(map => {
     map.update('media_attachments', list => list.filterNot(item => item.get('id') === mediaId));
-    map.update('text', text => text.replace(media.get('text_url'), '').trim());
     map.set('idempotencyKey', uuid());
 
     if (prevSize === 1) {
