26f68b0642f3ec65e8e26e303893a74b7f014c09
Levi Bard <taktaktaktaktaktaktaktaktaktak@gmail.com>
2018-03-15 11:04:57 +0100

    Enable updating additional account information from user preferences via rest api Resolves #6553

--- app/controllers/api/v1/accounts/credentials_controller.rb
+++ app/controllers/api/v1/accounts/credentials_controller.rb
@@ -13,6 +13,7 @@ class Api::V1::Accounts::CredentialsController < Api::BaseController
   def update
     @account = current_account
     UpdateAccountService.new.call(@account, account_params, raise_error: true)
+    UserSettingsDecorator.new(current_user).update(user_settings_params) if user_settings_params
     ActivityPub::UpdateDistributionWorker.perform_async(@account.id)
     render json: @account, serializer: REST::CredentialAccountSerializer
   end
@@ -21,5 +22,21 @@ class Api::V1::Accounts::CredentialsController < Api::BaseController
 
   def account_params
     params.permit(:display_name, :note, :avatar, :header, :locked)
+  end
+
+  def user_settings_params
+    return nil unless params.key?(:source)
+
+    source_params = params.require(:source).permit(
+      :privacy,
+      :sensitive
+    )
+    sensitive = source_params.key?(:sensitive) ? source_params[:sensitive] : object.user.setting_default_sensitive
+    return ActionController::Parameters.new(
+      user: {
+        setting_default_privacy: source_params.key?(:privacy) ? source_params[:privacy] : object.user.setting_default_privacy,
+        setting_default_sensitive: sensitive ? '1' : '0', # database doesn't like boolean values for default_sensitive
+      }
+    ).require(:user).permit(:setting_default_privacy, :setting_default_sensitive).to_h
   end
 end
