00ad0a1542c8a2c9215cf179832112879b9abd3a
Thibaut Girka <thib@sitedethib.com>
2018-03-17 13:50:53 +0100

    Adjust spec to fit new behavior

--- spec/services/block_domain_from_account_service_spec.rb
+++ spec/services/block_domain_from_account_service_spec.rb
@@ -10,10 +10,4 @@ RSpec.describe BlockDomainFromAccountService, type: :service do
     subject.call(alice, 'evil.org')
     expect(alice.domain_blocking?('evil.org')).to be true
   end
-
-  it 'purge followers from blocked domain' do
-    wolf.follow!(alice)
-    subject.call(alice, 'evil.org')
-    expect(wolf.following?(alice)).to be false
-  end
 end
