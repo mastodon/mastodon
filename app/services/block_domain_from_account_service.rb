bdce3ac0294759af03c72a352f8cfcf1787f82d1
Thibaut Girka <thib@sitedethib.com>
2018-03-17 13:20:23 +0100

    Do not drop follow relationships on user-specific domain blocks
    This behavior is extremely confusing, especially since the wording (hiding)
    does not hint at this and that follow relations from the user to blocked accounts
    are preserved.

--- app/services/block_domain_from_account_service.rb
+++ app/services/block_domain_from_account_service.rb
@@ -3,6 +3,5 @@
 class BlockDomainFromAccountService < BaseService
   def call(account, domain)
     account.block_domain!(domain)
-    account.passive_relationships.where(account: Account.where(domain: domain)).delete_all
   end
 end
