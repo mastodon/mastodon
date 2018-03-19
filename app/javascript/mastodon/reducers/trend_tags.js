d72abcf351161b068496b38675fda1306d21de74
fvh_P <ko_kurihara@yahoo.co.jp>
2018-03-19 01:32:15 +0000

    cancel sorting trend tags in the UI side

--- app/javascript/mastodon/reducers/trend_tags.js
+++ app/javascript/mastodon/reducers/trend_tags.js
@@ -12,10 +12,7 @@ const initialState = Immutable.Map({
 export default function trend_tags(state = initialState, action) {
   switch(action.type) {
   case TREND_TAGS_SUCCESS:
-    const tmp = Immutable.fromJS(action.tags);
-    return state.set('tags', tmp.set('score', tmp.get('score').sort((a, b) => {
-      return b - a;
-    })));
+    return state.set('tags', Immutable.fromJS(action.tags));
   case TOGGLE_TREND_TAGS:
     return state.set('visible', !state.get('visible'));
   default:
