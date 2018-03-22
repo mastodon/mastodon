07ba315b3d1b307445ed2a431b512f203e9b8f8f
tristar <tristar@urban-theory.net>
2018-03-23 01:00:38 +0900

    catgram: SQLの構文エラーの修正

--- app/models/status.rb
+++ app/models/status.rb
@@ -185,7 +185,7 @@ class Status < ApplicationRecord
 
     def as_public_timeline(account = nil, local_only = false)
       query = timeline_scope(local_only).without_replies
-              .where('(text like ? or text like ? or text like ? or statuses.uri like ? or statuses.uri like ?)', '%猫%', '%ねこ%', '%cat%', '%cat%', '%kitt%', '%mstdn.jp%')
+              .where('(text like ? or text like ? or text like ? or text like ? or statuses.uri like ? or statuses.uri like ?)', '%猫%', '%ねこ%', '%cat%', '%kitt%', '%cat%', '%mstdn.jp%')
 
       apply_timeline_filters(query, account, local_only)
     end
