fb97e4a7dc40ff6bcacc2353004e4661a948e131
takayamaki <fsgiko@gmail.com>
2018-03-23 00:43:27 +0900

    foldableコンポーネントをスクロール可能に変更

--- app/javascript/mastodon/components/foldable.js
+++ app/javascript/mastodon/components/foldable.js
@@ -6,7 +6,7 @@ import spring from 'react-motion/lib/spring';
 const Foldable = ({ fullHeight, minHeight, isVisible, children }) => (
   <Motion defaultStyle={{ height: isVisible ? fullHeight : minHeight }} style={{ height: spring(!isVisible ? minHeight : fullHeight) }}>
     {({ height }) =>
-      (<div style={{ height: `${height}px`, overflow: 'hidden' }}>
+      (<div style={{ height: `${height}px`, maxHeight: '20vh' }} class='scrollable optionally-scrollable'>
         {children}
       </div>)
     }
