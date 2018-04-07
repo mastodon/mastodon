1822ce89f9d64e30edeea671acb2d1ca79359219
Thibaut Girka <thib@sitedethib.com>
2018-04-07 22:05:21 +0200

    [Glitch] retrieve custom emoji list via API instead of before page load
    Port b08ab329f4d149fd414e0539574f49062c571a8a to glitch-soc

--- app/javascript/flavours/glitch/containers/mastodon.js
+++ app/javascript/flavours/glitch/containers/mastodon.js
@@ -6,6 +6,7 @@ import { showOnboardingOnce } from 'flavours/glitch/actions/onboarding';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { ScrollContext } from 'react-router-scroll-4';
 import UI from 'flavours/glitch/features/ui';
+import { fetchCustomEmojis } from 'flavours/glitch/actions/custom_emojis';
 import { hydrateStore } from 'flavours/glitch/actions/store';
 import { connectUserStream } from 'flavours/glitch/actions/streaming';
 import { IntlProvider, addLocaleData } from 'react-intl';
@@ -18,6 +19,9 @@ addLocaleData(localeData);
 export const store = configureStore();
 const hydrateAction = hydrateStore(initialState);
 store.dispatch(hydrateAction);
+
+// load custom emojis
+store.dispatch(fetchCustomEmojis());
 
 export default class Mastodon extends React.PureComponent {
 
