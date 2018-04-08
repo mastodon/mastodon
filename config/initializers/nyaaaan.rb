daf6b6d449746f87fb5375791acd072aff319cc1
S.H <gamelinks007@gmail.com>
2018-04-08 15:12:56 +0900

    Create nyaaaan.rb

---
+++ config/initializers/nyaaaan.rb
@@ -0,0 +1,13 @@
+Nyaaaan.setup do |status|	
+  	
+    # 社会性フィルター機能	
+    nyaaaan = Nyaaaan::Lang.new('[ 　\n]?#(社会性フィルター)[ 　\n]?', [	
+      {	
+        pattern: '死ね',	
+        replace: 'にゃーん'	
+      },	
+  	
+    ])	
+    status = nyaaaan.convert(status) if nyaaaan.match(status)	
+    status	
+end
