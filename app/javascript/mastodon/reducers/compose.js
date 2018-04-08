9182903c9917f713eb44aae2b9f89dc286b3da37
Genbu Hase <hasegenbu@gmail.com>
2018-04-08 16:56:25 +0900

    fixup! merge-quote-v2.6.1

--- app/javascript/mastodon/reducers/compose.js
+++ app/javascript/mastodon/reducers/compose.js
@@ -52,7 +52,7 @@ const initialState = ImmutableMap({
   preselectDate: null,
   in_reply_to: null,
   quote_from: null,
-  quote_from_url: null,
+  quote_from_uri: null,
   is_composing: false,
   is_submitting: false,
   is_uploading: false,
@@ -239,7 +239,7 @@ export default function compose(state = initialState, action) {
     return state.withMutations(map => {
       map.set('in_reply_to', action.status.get('id'));
       map.set('quote_from', null);
-      map.set('quote_from_url', null);
+      map.set('quote_from_uri', null);
       map.set('text', statusToTextMentions(state, action.status));
       map.set('privacy', privacyPreference(action.status.get('visibility'), state.get('default_privacy')));
       map.set('focusDate', new Date());
@@ -259,20 +259,12 @@ export default function compose(state = initialState, action) {
     return state.withMutations(map => {
       map.set('in_reply_to', null);
       map.set('quote_from', action.status.get('id'));
-      map.set('quote_from_url', action.status.get('url'));
+      map.set('quote_from_uri', action.status.get('uri'));
       map.set('text', '');
       map.set('privacy', privacyPreference(action.status.get('visibility'), state.get('default_privacy')));
       map.set('focusDate', new Date());
       map.set('preselectDate', new Date());
       map.set('idempotencyKey', uuid());
-
-      if (action.status.get('spoiler_text').length > 0) {
-        map.set('spoiler', true);
-        map.set('spoiler_text', action.status.get('spoiler_text'));
-      } else {
-        map.set('spoiler', false);
-        map.set('spoiler_text', '');
-      }
     });
   case COMPOSE_REPLY_CANCEL:
   case COMPOSE_QUOTE_CANCEL:
@@ -280,7 +272,7 @@ export default function compose(state = initialState, action) {
     return state.withMutations(map => {
       map.set('in_reply_to', null);
       map.set('quote_from', null);
-      map.set('quote_from_url', null);
+      map.set('quote_from_uri', null);
       map.set('text', '');
       map.set('spoiler', false);
       map.set('spoiler_text', '');
