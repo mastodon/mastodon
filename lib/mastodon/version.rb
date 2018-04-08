88bcfaeb18182b780e4ea95174f02776a46400c5
Thibaut Girka <thib@sitedethib.com>
2018-04-08 15:43:26 +0200

    Point to glitch-soc source and not tootsuite on the public page
    (hello AGPLv3)

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def source_base_url
-      'https://github.com/tootsuite/mastodon'
+      'https://github.com/glitch-soc/mastodon'
     end
 
     # specify git tag or commit hash here
