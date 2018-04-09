f95d01b33dac5d80869f64b40afda41517e7c57d
S.H <gamelinks007@gmail.com>
2018-04-09 21:24:34 +0900

    Update mastodon_cthulhu.rb

--- config/initializers/mastodon_cthulhu.rb
+++ config/initializers/mastodon_cthulhu.rb
@@ -1,6 +1,6 @@
 MastodonCthulhu.setup do |status|	
   	
-　# クトゥルフ神話機能	
+　# クトゥルフ神話
   fortune = MastodonCthulhu::Random.new('[ 　\n]?#(クトゥルフ神話)[ 　\n]?', %w(いあいあくとぅるぅ いあいあはすたぁ いあいあつとぅぁぐぁ ふんぐるいむぐるうなふ うがふなぐる ふたぐん))
   status = fortune.convert(status) if fortune.match(status)	
 
