f39d0b4879ed3a9f82c426c7f2f8285ee376a04c
S.H <gamelinks007@gmail.com>
2018-04-09 22:16:57 +0900

    Delete mastodon_cthulhu.rb

--- config/initializers/mastodon_cthulhu.rb
+++
@@ -1,8 +0,0 @@
-MastodonCthulhu.setup do |status|	
-  	
-　# クトゥルフ神話
-  fortune = MastodonCthulhu::Random.new('[ 　\n]?#(クトゥルフ神話)[ 　\n]?', %w(いあいあくとぅるぅ いあいあはすたぁ いあいあつとぅぁぐぁ ふんぐるいむぐるうなふ うがふなぐる ふたぐん))
-  status = fortune.convert(status) if fortune.match(status)	
-
-  status
-end
