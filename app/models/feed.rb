ff439eb08727eae9cf5abae215f530c17506ec76
Eugen Rochko <eugen@zeonfederated.com>
2018-04-18 01:26:38 +0200

    Only execute from_database query with non-zero limit

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -10,12 +10,8 @@ class Feed
     if redis.exists("account:#{@account.id}:regeneration")
       from_database(limit, max_id, since_id)
     else
-      statuses = from_redis(limit, max_id, since_id)
-
-      statuses += from_database(limit - statuses.size,
-                                statuses.last&.id || max_id,
-                                since_id)
-
+      statuses  = from_redis(limit, max_id, since_id)
+      statuses += from_database(limit - statuses.size, statuses.last&.id || max_id, since_id) if statuses.size < limit
       statuses
     end
   end
