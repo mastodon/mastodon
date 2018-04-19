c3638c36ffa16c854db057d680e4a1cd422f1323
Eugen Rochko <eugen@zeonfederated.com>
2018-04-19 18:39:14 +0200

    Split accounts and general report info into two tables again

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -24,7 +24,7 @@ delegate(document, batchCheckboxClassName, 'change', () => {
   const checkAllElement = document.querySelector('#batch_checkbox_all');
   if (checkAllElement) {
     checkAllElement.checked = [].every.call(document.querySelectorAll(batchCheckboxClassName), (content) => content.checked);
-    checkAllElement.indeterminate = [].some.call(document.querySelectorAll(batchCheckboxClassName), (content) => content.checked);
+    checkAllElement.indeterminate = !checkAllElement.checked && [].some.call(document.querySelectorAll(batchCheckboxClassName), (content) => content.checked);
   }
 });
 
