566c800bb8f99fec208caee3557439bbbeff6931
Eugen Rochko <eugen@zeonfederated.com>
2018-04-30 14:52:45 +0200

    Serialize webfinger XML with Ox instead of Nokogiri
    25ms -> 0.5ms

--- spec/controllers/well_known/host_meta_controller_spec.rb
+++ spec/controllers/well_known/host_meta_controller_spec.rb
@@ -10,7 +10,7 @@ describe WellKnown::HostMetaController, type: :controller do
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq 'application/xrd+xml'
       expect(response.body).to eq <<XML
-<?xml version="1.0"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
   <Link rel="lrdd" type="application/xrd+xml" template="https://cb6e6126.ngrok.io/.well-known/webfinger?resource={uri}"/>
 </XRD>
