4baf62d56510dcc59ce15acf4a30579505d0bdd8
abcang <abcang1015@gmail.com>
2018-05-02 20:20:49 +0900

    Keep notification when muting_notifications is true

--- app/services/mute_service.rb
+++ app/services/mute_service.rb
@@ -3,9 +3,12 @@
 class MuteService < BaseService
   def call(account, target_account, notifications: nil)
     return if account.id == target_account.id
-    FeedManager.instance.clear_from_timeline(account, target_account)
+
     mute = account.mute!(target_account, notifications: notifications)
-    BlockWorker.perform_async(account.id, target_account.id)
-    mute
+    if mute.hide_notifications?
+      BlockWorker.perform_async(account.id, target_account.id)
+    else
+      FeedManager.instance.clear_from_timeline(account, target_account)
+    end
   end
 end
