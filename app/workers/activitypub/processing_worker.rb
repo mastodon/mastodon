55f55cd1b47f08ef8cd62775f1319024c30fc3b8
Eugen Rochko <eugen@zeonfederated.com>
2018-05-03 12:16:21 +0200

    Revert "Do not override timestamps for incoming toots (#7326)"
    This reverts commit bd367918328daedb37f49727f4e16e33679fdb15.

--- app/workers/activitypub/processing_worker.rb
+++ app/workers/activitypub/processing_worker.rb
@@ -6,6 +6,6 @@ class ActivityPub::ProcessingWorker
   sidekiq_options backtrace: true
 
   def perform(account_id, body)
-    ActivityPub::ProcessCollectionService.new.call(body, Account.find(account_id))
+    ActivityPub::ProcessCollectionService.new.call(body, Account.find(account_id), override_timestamps: true)
   end
 end
