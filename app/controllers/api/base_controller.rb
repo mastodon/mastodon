6ed578f6e047e8d12f8b0477249a3817b9d50546
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-05-07 12:34:17 +0900

    fix require_user!

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -66,10 +66,10 @@ class Api::BaseController < ApplicationController
   end
 
   def require_user!
-    if current_user && !current_user.disabled?
-      set_user_activity
-    elsif current_user
+    if current_user && current_user.disabled?
       render json: { error: 'Your login is currently disabled' }, status: 403
+    elsif current_user
+      set_user_activity
     else
       render json: { error: 'This method requires an authenticated user' }, status: 422
     end
