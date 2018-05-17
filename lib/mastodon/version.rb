a3e56f5d193024c4f5ee95397b80af4b390e1e33
MIYAGI Hikaru <hikaru.devel@js4.in>
2018-05-18 05:44:29 +0900

    'http' is away accidentally...

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -50,7 +50,7 @@ module Mastodon
     end
 
     def user_agent
-      @user_agent ||= "#{HTTP::Request::USER_AGENT} (Mastodon/#{Version}; +#{Rails.configuration.x.use_https ? 's' : ''}://#{Rails.configuration.x.web_domain}/)"
+      @user_agent ||= "#{HTTP::Request::USER_AGENT} (Mastodon/#{Version}; +http#{Rails.configuration.x.use_https ? 's' : ''}://#{Rails.configuration.x.web_domain}/)"
     end
   end
 end
