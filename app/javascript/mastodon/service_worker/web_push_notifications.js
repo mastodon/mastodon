834679a4d8a8b3b44b931a335260f5919075d41c
Eugen Rochko <eugen@zeonfederated.com>
2018-05-21 03:12:28 +0200

    Fix code style issues

--- app/javascript/mastodon/service_worker/web_push_notifications.js
+++ app/javascript/mastodon/service_worker/web_push_notifications.js
@@ -60,9 +60,7 @@ const formatMessage = (messageId, locale, values = {}) =>
   (new IntlMessageFormat(locales[locale][messageId], locale)).format(values);
 
 const htmlToPlainText = html =>
-  html.replace(/<br\s*\/?>/g, '\n')
-      .replace(/<\/p><p>/g, '\n\n')
-      .replace(/<[^>]*>/g, '');
+  html.replace(/<br\s*\/?>/g, '\n').replace(/<\/p><p>/g, '\n\n').replace(/<[^>]*>/g, '');
 
 const handlePush = (event) => {
   const { access_token, notification_id, preferred_locale, title, body, icon } = event.data.json();
