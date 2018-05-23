d6e2d912a5830a2dee286be35ce8d868c7fcc8ff
kedamaDQ <kedama@foresdon.jp>
2018-05-23 20:53:44 +0900

    Refactoring (Related to #4)

--- app/javascript/mastodon/features/emoji/emoji_mart_search_light.js
+++ app/javascript/mastodon/features/emoji/emoji_mart_search_light.js
@@ -56,13 +56,9 @@ function search(value, { emojisToShowFilter, maxResults, include, exclude, custo
 
     if (value.match(/^[a-z]_/i)) {
       const regExp = new RegExp(`^${value}`, 'i');
-      const matches = Object.keys(emojisList).map((emoji_id) => {
-        if (regExp.test(emoji_id)) {
-          return emojisList[emoji_id];
-        }
-      }).filter((a) => a);
-
-      return matches.slice(0, maxResults);
+      return Object.keys(emojisList).filter((emoji_id) => {
+        return regExp.test(emoji_id);
+      }).slice(0, maxResults);
     }
 
     let values = value.toLowerCase().split(/[\s|,|\-|_]+/),
