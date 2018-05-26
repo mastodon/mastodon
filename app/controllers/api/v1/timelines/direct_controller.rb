eb37cf830bdfdb641aec280a00474b1ba3dd5f98
takayamaki <fsgiko@gmail.com>
2018-05-26 18:23:25 +0900

    fix Status#as_direct_timeline querying

--- app/controllers/api/v1/timelines/direct_controller.rb
+++ app/controllers/api/v1/timelines/direct_controller.rb
@@ -31,7 +31,7 @@ class Api::V1::Timelines::DirectController < Api::BaseController
   end
 
   def direct_timeline_statuses
-    Status.as_direct_timeline(current_account)
+    Status.as_direct_timeline(current_account, params[:max_id], params[:since_id], limit_param(DEFAULT_STATUSES_LIMIT))
   end
 
   def insert_pagination_headers
