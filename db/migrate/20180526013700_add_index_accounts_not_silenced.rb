be1d38a93bfbfeaa653a7d15f80170317730ba19
tateisu <tateisu@gmail.com>
2018-05-28 03:42:05 +0900

    revoke index_accounts_not_silenced because direct timeline does not use it.

--- db/migrate/20180526013700_add_index_accounts_not_silenced.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexAccountsNotSilenced < ActiveRecord::Migration[5.0]
-  disable_ddl_transaction!
-  
-  def change
-    add_index :accounts,[:id], where: 'not silenced', algorithm: :concurrently, name: "index_accounts_not_silenced"
-  end
-end
-
