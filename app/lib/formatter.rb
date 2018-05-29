fe324e3c45d4b5917ff5c45f8053891f70fffa28
Noëlle Anthony <noelle.d.anthony@gmail.com>
2018-05-29 12:01:13 -0400

    Update formatter.rb

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -47,7 +47,7 @@ class Formatter
     while cur_c < chunks.length
       h = chunks[cur_c]
       h.gsub(/ /) { |match| "&nbsp;" }
-      h = "<tt>" + h + "</tt>"
+      h = "<tt style=\"font-family: monospace;\">" + h + "</tt>"
       chunks[cur_c] = h
       cur_c += 2
     end
