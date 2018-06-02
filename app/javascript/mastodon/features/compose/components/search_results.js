79d9f9e944649a26307d2e98f031a7e386d257ff
Eugen Rochko <eugen@zeonfederated.com>
2018-06-02 19:13:51 +0200

    Update search_results.js

--- app/javascript/mastodon/features/compose/components/search_results.js
+++ app/javascript/mastodon/features/compose/components/search_results.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { FormattedMessage } from 'react-intl';
 import AccountContainer from '../../../containers/account_container';
