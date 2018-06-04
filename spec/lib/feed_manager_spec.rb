a40e322f4b6784ef6cfee188e0e30167c32d27b9
David Yip <yipdw@member.fsf.org>
2018-06-03 23:02:01 -0500

    Fix spacing in some FeedManager examples.

--- spec/lib/feed_manager_spec.rb
+++ spec/lib/feed_manager_spec.rb
@@ -175,7 +175,7 @@ RSpec.describe FeedManager do
       it 'returns true for a status with a tag that matches a muted keyword' do
         Fabricate('Glitch::KeywordMute', account: alice, keyword: 'jorts')
         status = Fabricate(:status, account: bob)
-	status.tags << Fabricate(:tag, name: 'jorts')
+        status.tags << Fabricate(:tag, name: 'jorts')
 
         expect(FeedManager.instance.filter?(:home, status, alice.id)).to be true
       end
@@ -183,7 +183,7 @@ RSpec.describe FeedManager do
       it 'returns true for a status with a tag that matches an octothorpe-prefixed muted keyword' do
         Fabricate('Glitch::KeywordMute', account: alice, keyword: '#jorts')
         status = Fabricate(:status, account: bob)
-	status.tags << Fabricate(:tag, name: 'jorts')
+        status.tags << Fabricate(:tag, name: 'jorts')
 
         expect(FeedManager.instance.filter?(:home, status, alice.id)).to be true
       end
