b926b998c3460445f3f5e3e210e4cccb17b0245e
Eugen Rochko <eugen@zeonfederated.com>
2018-06-09 13:29:04 +0200

    Send rejections to followers when user hides domain they're on

--- app/services/block_domain_from_account_service.rb
+++ app/services/block_domain_from_account_service.rb
@@ -2,7 +2,37 @@
 
 class BlockDomainFromAccountService < BaseService
   def call(account, domain)
-    account.block_domain!(domain)
-    account.passive_relationships.where(account: Account.where(domain: domain)).delete_all
+    @account = account
+    @domain  = domain
+
+    @account.block_domain!(@domain)
+
+    reject_existing_followers!
+    reject_pending_follow_requests!
+  end
+
+  private
+
+  def reject_existing_followers!
+    @account.passive_relationships.where(account: Account.where(domain: @domain)).includes(:account).find_each do |follow|
+      reject_follow!(follow)
+    end
+  end
+
+  def reject_pending_follow_requests!
+    FollowRequest.where(target_account: @account).where(account: Account.where(domain: @domain)).includes(:account).find_each do |follow_request|
+      reject_follow!(follow_request)
+    end
+  end
+
+  def reject_follow!(follow)
+    json = Oj.dump(ActivityPub::LinkedDataSignature.new(ActiveModelSerializers::SerializableResource.new(
+      follow,
+      serializer: ActivityPub::RejectFollowSerializer,
+      adapter: ActivityPub::Adapter
+    ).as_json).sign!(@account))
+
+    ActivityPub::DeliveryWorker.perform_async(json, @account.id, follow.account.inbox_url)
+    follow.destroy
   end
 end
--- spec/services/block_domain_from_account_service_spec.rb
+++ spec/services/block_domain_from_account_service_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe BlockDomainFromAccountService, type: :service do
-  let!(:wolf) { Fabricate(:account, username: 'wolf', domain: 'evil.org') }
+  let!(:wolf) { Fabricate(:account, username: 'wolf', domain: 'evil.org', inbox_url: 'https://evil.org/inbox') }
   let!(:alice) { Fabricate(:account, username: 'alice') }
 
   subject { BlockDomainFromAccountService.new }
+
+  before do
+    stub_jsonld_contexts!
+    allow(ActivityPub::DeliveryWorker).to receive(:perform_async)
+  end
 
   it 'creates domain block' do
     subject.call(alice, 'evil.org')
@@ -15,5 +20,11 @@ RSpec.describe BlockDomainFromAccountService, type: :service do
     wolf.follow!(alice)
     subject.call(alice, 'evil.org')
     expect(wolf.following?(alice)).to be false
+  end
+
+  it 'sends Reject->Follow to followers from blocked domain' do
+    wolf.follow!(alice)
+    subject.call(alice, 'evil.org')
+    expect(ActivityPub::DeliveryWorker).to have_received(:perform_async).once
   end
 end
