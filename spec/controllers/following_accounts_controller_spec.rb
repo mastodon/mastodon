1ee2c080ffdccc2c4a24a395de21966c44ace191
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2018-06-14 10:08:20 +0900

    Add tests for following accounts controller

--- spec/controllers/following_accounts_controller_spec.rb
+++ spec/controllers/following_accounts_controller_spec.rb
@@ -8,18 +8,45 @@ describe FollowingAccountsController do
   let(:followee1) { Fabricate(:account) }
 
   describe 'GET #index' do
-    it 'assigns followees' do
-      follow0 = alice.follow!(followee0)
-      follow1 = alice.follow!(followee1)
+    let!(:follow0) { alice.follow!(followee0) }
+    let!(:follow1) { alice.follow!(followee1) }
 
-      get :index, params: { account_username: alice.username }
+    context 'when format is html' do
+      subject(:response) { get :index, params: { account_username: alice.username, format: :html } }
 
-      assigned = assigns(:follows).to_a
-      expect(assigned.size).to eq 2
-      expect(assigned[0]).to eq follow1
-      expect(assigned[1]).to eq follow0
+      it 'assigns follows' do
+        expect(response).to have_http_status(200)
 
-      expect(response).to have_http_status(200)
+        assigned = assigns(:follows).to_a
+        expect(assigned.size).to eq 2
+        expect(assigned[0]).to eq follow1
+        expect(assigned[1]).to eq follow0
+      end
+    end
+
+    context 'when format is json' do
+      subject(:response) { get :index, params: { account_username: alice.username, page: page, format: :json } }
+      subject(:body) { JSON.parse(response.body) }
+
+      context 'with page' do
+        let(:page) { 1 }
+
+        it 'returns followers' do
+          expect(response).to have_http_status(200)
+          expect(body['totalItems']).to eq 2
+          expect(body['partOf']).to be_present
+        end
+      end
+
+      context 'without page' do
+        let(:page) { nil }
+
+        it 'returns followers' do
+          expect(response).to have_http_status(200)
+          expect(body['totalItems']).to eq 2
+          expect(body['partOf']).to be_blank
+        end
+      end
     end
   end
 end
