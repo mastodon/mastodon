b40cd5eee7cdf0923b1d4779bbbc80e059894018
fvh_P <ko_kurihara@yahoo.co.jp>
2018-06-27 14:26:30 +0000

    change trend_tags_controller for trend tags history

--- app/controllers/api/v1/trend_tags_controller.rb
+++ app/controllers/api/v1/trend_tags_controller.rb
@@ -4,14 +4,29 @@ class Api::V1::TrendTagsController < Api::BaseController
   respond_to :json
 
   def show
-    trend_score = {
-      'updated_at' => redis.hget('trend_tags_management_data', 'updated_at'),
-      'score' => redis.zrevrange('trend_tags', 0, -1, withscores: true).to_h,
-    }
     render json: trend_score
   end
 
   private
+
+  def trend_score
+    if history_mode?
+      redis.lrange('trend_tags_history', 0, -1).map { |item| JSON.parse(item) }
+    else
+      {
+        'updated_at' => redis.hget('trend_tags_management_data', 'updated_at'),
+        'score' => redis.zrevrange('trend_tags', 0, -1, withscores: true).to_h,
+      }
+    end
+  end
+
+  def history_mode?
+    ActiveModel::Type::Boolean.new.cast(trend_tags_params[:history_mode]) 
+  end
+
+  def trend_tags_params
+    params.permit(:history_mode)
+  end
 
   def redis
     Redis.current
