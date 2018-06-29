c408baa5a66ec2d80f796662751898aebe9a6402
Eugen Rochko <eugen@zeonfederated.com>
2018-06-29 14:29:42 +0200

    Add streaming API event to allow syncing filters

--- app/models/custom_filter.rb
+++ app/models/custom_filter.rb
@@ -42,6 +42,7 @@ class CustomFilter < ApplicationRecord
 
   def remove_cache
     Rails.cache.delete("filters:#{account_id}")
+    Redis.current.publish("timeline:#{account_id}", Oj.dump(event: :filters_changed))
   end
 
   def context_must_be_valid
