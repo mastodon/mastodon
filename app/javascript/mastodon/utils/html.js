5fb711a33da92990aee744bb491ffad2cb31f6c3
Maciej Baron <thebezet@gmail.com>
2018-06-29 15:22:56 +0100

    Fix JS security issue in unescapeHTML()

--- app/javascript/mastodon/utils/html.js
+++ app/javascript/mastodon/utils/html.js
@@ -1,5 +1,6 @@
 export const unescapeHTML = (html) => {
   const wrapper = document.createElement('div');
   wrapper.innerHTML = html.replace(/<br\s*\/?>/g, '\n').replace(/<\/p><p>/g, '\n\n').replace(/<[^>]*>/g, '');
+  wrapper.innerHTML = wrapper.textContent;
   return wrapper.textContent;
 };
