a92746afe5ba8beb36860445c1550687a5275ed9
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-05 12:11:11 +0900

    Revert "add Koyaaan"
    This reverts commit 3089284e5bbb3bedd83b2d2d4c96687b198dde0a.
    
    	modified:   Gemfile
    	deleted:    config/initializers/Koyaaan.rb

--- config/initializers/Koyaaan.rb
+++
@@ -1,6 +0,0 @@
-Koyaaan.setup do |status|	
-  	
-    # こゃーん！	
-    fortune = Koyaaan::Random.new('[ 　\n]?#(社会性フィルター)[ 　\n]?', %w(こゃーん！))
-    status = fortune.convert(status) if fortune.match(status)
-end
