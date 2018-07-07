0d83d494a0b4f9c2ba0eb2c3c09fa541f4bf0a50
Eugen Rochko <eugen@zeonfederated.com>
2018-07-07 18:04:46 +0200

    Fix missing irreversible in filters API, expires_in param

--- app/serializers/rest/filter_serializer.rb
+++ app/serializers/rest/filter_serializer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class REST::FilterSerializer < ActiveModel::Serializer
-  attributes :id, :phrase, :context, :expires_at
+  attributes :id, :phrase, :context, :expires_at,
+             :irreversible
 end
