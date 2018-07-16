bcf157a1a9597595c61b1cce84bd62f63fcaa90b
Thibaut Girka <thib@sitedethib.com>
2018-07-16 16:42:52 +0200

    Avoid loading rails-ujs twice on vanilla flavour

--- app/javascript/core/common.js
+++ app/javascript/core/common.js
@@ -1,8 +1,5 @@
 //  This file will be loaded on all pages, regardless of theme.
 
-import { start } from 'rails-ujs';
 import 'font-awesome/css/font-awesome.css';
 
 require.context('../images/', true);
-
-start();
