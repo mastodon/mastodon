bc229c4eacb58d58d48f2a59dff74bfb9c3cda41
Eugen Rochko <eugen@zeonfederated.com>
2018-07-29 20:25:26 +0200

    Adjust context definition for movedTo

--- app/lib/activitypub/adapter.rb
+++ app/lib/activitypub/adapter.rb
@@ -9,7 +9,7 @@ class ActivityPub::Adapter < ActiveModelSerializers::Adapter::Base
       {
         'manuallyApprovesFollowers' => 'as:manuallyApprovesFollowers',
         'sensitive'                 => 'as:sensitive',
-        'movedTo'                   => 'as:movedTo',
+        'movedTo'                   => { '@id' => 'as:movedTo', '@type' => '@id' },
         'Hashtag'                   => 'as:Hashtag',
         'ostatus'                   => 'http://ostatus.org#',
         'atomUri'                   => 'ostatus:atomUri',
