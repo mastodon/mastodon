06dfe39d7940a4e2ef147a23d3596a28a64f73f6
S.H <gamelinks007@gmail.com>
2018-08-05 17:29:33 +0900

    Update mastoson_cthulhu.rb

--- config/initializers/mastoson_cthulhu.rb
+++ config/initializers/mastoson_cthulhu.rb
@@ -1,5 +1,3 @@
-require 'wandbox'
-
 MastodonCthulhu.setup do |status|	
   fortune = MastodonCthulhu::Random.new('[ 　\n]?#(クトゥルフ神話)[ 　\n]?', %w(いあ!いあ!くとぅるぅ! いあ!いあ!はすたぁ! いあ!いあ!つとぅぁぐぁ! ふんぐるいむぐるうなふ! うがふなぐる! ふたぐん! ふんぐるい！むぐるうなふ！くとぅぐあ！ふぉまるはうと！んがあ・ぐあ！なふるたぐん！いあ！くとぅぐあ！ いあ！いあ！はすたあ！はすたあ!くふあやく!ぶるぐとむ!ぶぐとらぐるん!ぶるぐとむ!あい！あい！はすたあ！))
   status = fortune.convert(status) if fortune.match(status)	
