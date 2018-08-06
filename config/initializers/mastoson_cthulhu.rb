207daf77d62f197a1a8d38d6837c07f158ad5989
S.H <gamelinks007@gmail.com>
2018-08-06 11:04:54 +0900

    fix #cpp

--- config/initializers/mastoson_cthulhu.rb
+++ config/initializers/mastoson_cthulhu.rb
@@ -19,7 +19,7 @@ MastodonCthulhu.setup do |status|
     end
     
     if system("g++ ./config/initializers/cplusplus.cpp --std=c++11") then 
-      s = "conpiled!"#Open3.capture3("./config/initializers/a.out")
+      s = Open3.capture3("./a.out")
       
       if s[0].length == 0 then
         status = status.replace(" #{s[1]} \n #cpp")
