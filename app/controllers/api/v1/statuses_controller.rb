34fc96727266c4f736e28f1af083b54867a972ed
Eugen Rochko <eugen@zeonfederated.com>
2018-08-13 13:53:09 +0200

    Crawl link card when requested from REST API if none exist
    Caveat: If there is a valid link in the status that does not have
    a valid preview that can be stored, then each request for the card
    will crawl it anew

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -35,7 +35,8 @@ class Api::V1::StatusesController < Api::BaseController
   end
 
   def card
-    @card = @status.preview_cards.first
+    @card   = @status.preview_cards.first
+    @card ||= FetchLinkCardService.new.call(@status)
 
     if @card.nil?
       render_empty
