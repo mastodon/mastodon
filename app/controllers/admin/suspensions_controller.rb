9d58daac6c860b599f8c266b8bb10c6170220dd3
Eugen Rochko <eugen@zeonfederated.com>
2018-08-23 21:51:56 +0200

    Fix regression when suspending not from report (#8400)
    Regression from #8353

--- app/controllers/admin/suspensions_controller.rb
+++ app/controllers/admin/suspensions_controller.rb
@@ -14,7 +14,7 @@ module Admin
       @suspension = Form::AdminSuspensionConfirmation.new(suspension_params)
 
       if suspension_params[:acct] == @account.acct
-        resolve_report! if suspension_params[:report_id]
+        resolve_report! if suspension_params[:report_id].present?
         perform_suspend!
         mark_reports_resolved!
         redirect_to admin_accounts_path
