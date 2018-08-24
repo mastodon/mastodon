8bb30dcf864115710e24c4593edc3e6e7ffbcafe
Eugen Rochko <eugen@zeonfederated.com>
2018-08-24 19:58:02 +0200

    Disable retries on all scheduled tasks

--- app/workers/scheduler/user_cleanup_scheduler.rb
+++ app/workers/scheduler/user_cleanup_scheduler.rb
@@ -3,7 +3,7 @@
 class Scheduler::UserCleanupScheduler
   include Sidekiq::Worker
 
-  sidekiq_options unique: :until_executed
+  sidekiq_options unique: :until_executed, retry: 0
 
   def perform
     User.where('confirmed_at is NULL AND confirmation_sent_at <= ?', 2.days.ago).reorder(nil).find_in_batches do |batch|
