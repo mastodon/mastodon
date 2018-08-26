f60968ff91100ecf344c69e4e8dc17b85643a540
tristar <tristar@urban-theory.net>
2018-08-26 15:22:51 +0900

    catgram: jemallocへの対応(イメージのベースをDebianに変更)

--- boot.sh
+++ boot.sh
@@ -8,4 +8,6 @@ LINK=/mastodon/public/packs
 test -d $LINK && test ! -L $LINK && rm -rf $LINK
 ln -sf /volumes/catgram-assets/packs $LINK
 
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.1
+
 $@
