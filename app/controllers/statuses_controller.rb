e8553e886c830ca29cace224d3fd38941c55e324
Renato "Lond" Cerqueira <renato@lond.com.br>
2018-08-28 20:04:26 +0200

    Add animate custom emoji param to embed pages

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -54,6 +54,7 @@ class StatusesController < ApplicationController
     skip_session!
     expires_in 180, public: true
     response.headers['X-Frame-Options'] = 'ALLOWALL'
+    @animate_custom_emoji = params[:animate_custom_emoji] == "1"
 
     render 'stream_entries/embed', layout: 'embedded'
   end
