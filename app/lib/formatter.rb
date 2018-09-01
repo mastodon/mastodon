d9a9cfc02abe136ff39ba480415c6dacbcd3761b
noellabo <noel.yoshiba@gmail.com>
2018-09-01 13:08:06 +0900

    fixup! modify-quote

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -185,7 +185,7 @@ class Formatter
     quote_content = format_in_quote(status.quote, options)
     url = TagManager.instance.url_for(status.quote)
     link = encode_and_link_urls(url)
-    html.sub(/(<[^>]+>)\z/, "<span class=\"quote-inline\"><br/>QT:[#{link}]</span>\\1")
+    html.sub(/(<[^>]+>)\z/, "<span class=\"quote-inline\"><br/>QT: #{quote_content} [#{link}]</span>\\1")
   end
 
   def rewrite(text, entities)
