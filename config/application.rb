14a300b4282356100d4ca2c0858ed49e5ee76932
Eugen Rochko <eugen@zeonfederated.com>
2018-09-02 22:31:53 +0200

    Enable ro and ta locales in Rails (#8583)
    * Enable ro and ta locales in Rails
    
    * Add Tamil and Romanian to language dropdown

--- config/application.rb
+++ config/application.rb
@@ -69,12 +69,14 @@ module Mastodon
       :pl,
       :pt,
       :'pt-BR',
+      :ro,
       :ru,
       :sk,
       :sl,
       :sr,
       :'sr-Latn',
       :sv,
+      :ta,
       :te,
       :th,
       :tr,
@@ -85,6 +87,7 @@ module Mastodon
     ]
 
     config.i18n.default_locale = ENV['DEFAULT_LOCALE']&.to_sym
+
     unless config.i18n.available_locales.include?(config.i18n.default_locale)
       config.i18n.default_locale = :en
     end
