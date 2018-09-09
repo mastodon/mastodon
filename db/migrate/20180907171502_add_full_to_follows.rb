f6948399047dff12268821de2edfef347a879708
TheInventrix <TheInventrix@users.noreply.github.com>
2018-09-09 00:10:30 -0600

    account for existing follow entries with new column

--- db/migrate/20180907171502_add_full_to_follows.rb
+++ db/migrate/20180907171502_add_full_to_follows.rb
@@ -1,5 +1,12 @@
 class AddFullToFollows < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_column :follows, :full, :boolean
+    change_column_null :follows, :full, true
+    change_column_default :follows, :full, true
   end
+  
+  def down
+    remove_column :follows, :full
+  end
+
 end
