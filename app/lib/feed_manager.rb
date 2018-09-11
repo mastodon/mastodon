059aafa0a7aa21e57b0de341f2c90ad93db298de
TheInventrix <TheInventrix@users.noreply.github.com>
2018-09-11 12:08:55 -0600

    don't merge in pre-existing locked posts

--- app/lib/feed_manager.rb
+++ app/lib/feed_manager.rb
@@ -154,6 +154,7 @@ class FeedManager
     return false if receiver_id == status.account_id
     return true  if status.reply? && (status.in_reply_to_id.nil? || status.in_reply_to_account_id.nil?)
     return true  if phrase_filtered?(status, receiver_id, :home)
+    return true  if status.private_visibility? && !Follow.where(account_id: receiver_id, target_account_id: status.account_id, full_access: true).exists?
 
     check_for_blocks = status.mentions.pluck(:account_id)
     check_for_blocks.concat([status.account_id])
