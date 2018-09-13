e070bfe7117bed6f84edbb58712244b17afde54a
luz.paz <luzpaz@users.noreply.github.com>
2018-09-13 18:03:41 -0400

    Misc. typos
    Found via `codespell -q 3 --skip="./app/javascript/mastodon/locales,./config/locales"`

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -116,7 +116,7 @@ SimpleForm.setup do |config|
   # You can define the class to use on all labels. Default is nil.
   # config.label_class = nil
 
-  # You can define the default class to be used on forms. Can be overriden
+  # You can define the default class to be used on forms. Can be overridden
   # with `html: { :class }`. Defaulting to none.
   # config.default_form_class = nil
 
