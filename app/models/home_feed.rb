e6c7804a069b2e775f2fd44e48f87dbd4bf321cd
Eugen Rochko <eugen@zeonfederated.com>
2018-09-20 00:36:30 +0200

    Allow min_id pagination in Feed#get

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -7,9 +7,9 @@ class HomeFeed < Feed
     @account = account
   end
 
-  def get(limit, max_id = nil, since_id = nil)
+  def get(limit, max_id = nil, since_id = nil, min_id = nil)
     if redis.exists("account:#{@account.id}:regeneration")
-      from_database(limit, max_id, since_id)
+      from_database(limit, max_id, since_id, min_id)
     else
       super
     end
@@ -17,9 +17,15 @@ class HomeFeed < Feed
 
   private
 
-  def from_database(limit, max_id, since_id)
-    Status.as_home_timeline(@account)
-          .paginate_by_max_id(limit, max_id, since_id)
-          .reject { |status| FeedManager.instance.filter?(:home, status, @account.id) }
+  def from_database(limit, max_id, since_id, min_id)
+    statuses = Status.as_home_timeline(@account)
+
+    statuses = if min_id.blank?
+                 statuses.paginate_by_max_id(limit, max_id, since_id)
+               else
+                 statuses.paginate_by_min_id(limit, min_id)
+               end
+
+    statuses.reject { |status| FeedManager.instance.filter?(:home, status, @account.id) }
   end
 end
