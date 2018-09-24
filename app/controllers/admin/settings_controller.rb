c24f80e2ac8040e48d29a972c4de5d6a8e321a5c
ashleyhull-versent <ashley.hull@versent.com.au>
2018-09-24 16:40:49 +1000

    Update settings_controller.rb

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -19,6 +19,7 @@ module Admin
       theme
       thumbnail
       hero
+      lildude
       min_invite_role
       activity_api_enabled
       peers_api_enabled
@@ -41,6 +42,7 @@ module Admin
     UPLOAD_SETTINGS = %w(
       thumbnail
       hero
+      lildude
     ).freeze
 
     def edit
