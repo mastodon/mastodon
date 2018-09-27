740baa7f364afcaa114a7977fbe581a382ae36f1
Eugen Rochko <eugen@zeonfederated.com>
2018-09-28 01:09:31 +0200

    Fix HomeFeed#from_database not using paginate_by_id

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -18,14 +18,8 @@ class HomeFeed < Feed
   private
 
   def from_database(limit, max_id, since_id, min_id)
-    statuses = Status.as_home_timeline(@account)
-
-    statuses = if min_id.blank?
-                 statuses.paginate_by_max_id(limit, max_id, since_id)
-               else
-                 statuses.paginate_by_min_id(limit, min_id)
-               end
-
-    statuses.reject { |status| FeedManager.instance.filter?(:home, status, @account.id) }
+    Status.as_home_timeline(@account)
+          .paginate_by_id(limit, max_id: max_id, since_id: since_id, min_id: min_id)
+          .reject { |status| FeedManager.instance.filter?(:home, status, @account.id) }
   end
 end
