230032573c94527b7124cbec4697908ffe617743
Eugen Rochko <eugen@zeonfederated.com>
2018-09-29 18:33:18 +0200

    Adjust bootsnap configuration for newer version

--- config/boot.rb
+++ config/boot.rb
@@ -1,14 +1,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap' # Speed up boot time by caching expensive operations.
+require 'bundler/setup'  # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
 Bootsnap.setup(
   cache_dir:            'tmp/cache',
-  development_mode:     ENV['RAILS_ENV'] == 'development',
+  development_mode:     ENV.fetch('RAILS_ENV', 'development') == 'development',
   load_path_cache:      true,
   autoload_paths_cache: true,
-  disable_trace:        true,
+  disable_trace:        false,
   compile_cache_iseq:   false,
   compile_cache_yaml:   false
 )
