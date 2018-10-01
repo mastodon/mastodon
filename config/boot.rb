ac15c26c293e5bd79300ec7ace7b93d8812ff412
abcang <abcang1015@gmail.com>
2018-10-01 21:06:40 +0900

    Fix requirie bootsnap (#8836)

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup'  # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require 'bootsnap' # Speed up boot time by caching expensive operations.
 
 Bootsnap.setup(
   cache_dir:            'tmp/cache',
