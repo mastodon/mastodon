mastodon/mastodon

commit 2d27c110610a848d30fe150c58bbd60ebf6fab7c

Set Content-Security-Policy rules through RoR's config (#8957)
* Set CSP rules in RoR's configuration

* Override CSP setting in the embed controller to allow frames

config/initializers/content_security_policy.rb
@@ -2,17 +2,20 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |p|
-#   p.default_src :self, :https
-#   p.font_src    :self, :https, :data
-#   p.img_src     :self, :https, :data
-#   p.object_src  :none
-#   p.script_src  :self, :https
-#   p.style_src   :self, :https, :unsafe_inline
-#
-#   # Specify URI for violation reports
-#   # p.report_uri "/csp-violation-report-endpoint"
-# end
+assets_host = Rails.configuration.action_controller.asset_host || "https://#{ENV['WEB_DOMAIN'] || ENV['LOCAL_DOMAIN']}"
+
+Rails.application.config.content_security_policy do |p|
+  p.base_uri        :none
+  p.default_src     :none
+  p.frame_ancestors :none
+  p.script_src      :self, assets_host
+  p.font_src        :self, assets_host
+  p.img_src         :self, :https, :data, :blob
+  p.style_src       :self, :unsafe_inline, assets_host
+  p.media_src       :self, :data, assets_host
+  p.frame_src       :self, :https
+  p.connect_src     :self, assets_host, Rails.configuration.x.streaming_api_base_url
+end
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:

app/controllers/statuses_controller.rb
@@ -19,6 +19,10 @@ class StatusesController < ApplicationController
   before_action :set_referrer_policy_header, only: [:show]
   before_action :set_cache_headers
 
+  content_security_policy only: :embed do |p|
+    p.frame_ancestors(false)
+  end
+
   def show
     respond_to do |format|
       format.html do

commit 9a14bb53692528ab94164c508a92134a3838e22c

Fix CSP headers blocking media and development environment
Regression from #8957

config/initializers/content_security_policy.rb
@@ -2,19 +2,29 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-assets_host = Rails.configuration.action_controller.asset_host || "https://#{ENV['WEB_DOMAIN'] || ENV['LOCAL_DOMAIN']}"
+base_host     = Rails.configuration.x.web_domain
+assets_host   = Rails.configuration.action_controller.asset_host
+assets_host ||= "http#{Rails.configuration.x.use_https ? 's' : ''}://#{base_host}"
 
 Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-  p.script_src      :self, assets_host
   p.font_src        :self, assets_host
-  p.img_src         :self, :https, :data, :blob
+  p.img_src         :self, :https, :data, :blob, assets_host
   p.style_src       :self, :unsafe_inline, assets_host
-  p.media_src       :self, :data, assets_host
+  p.media_src       :self, :https, :data, assets_host
   p.frame_src       :self, :https
-  p.connect_src     :self, assets_host, Rails.configuration.x.streaming_api_base_url
+
+  if Rails.env.development?
+    webpacker_urls = %w(ws http).map { |protocol| "#{protocol}#{Webpacker.dev_server.https? ? 's' : ''}://#{Webpacker.dev_server.host_with_port}" }
+
+    p.connect_src :self, assets_host, Rails.configuration.x.streaming_api_base_url, *webpacker_urls
+    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host
+  else
+    p.connect_src :self, assets_host, Rails.configuration.x.streaming_api_base_url
+    p.script_src  :self, assets_host
+  end
 end
 
 # Report CSP violations to a specified URI
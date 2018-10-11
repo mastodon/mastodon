9a14bb53692528ab94164c508a92134a3838e22c
Eugen Rochko <eugen@zeonfederated.com>
2018-10-12 01:04:41 +0200

    Fix CSP headers blocking media and development environment
    Regression from #8957

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,19 +2,29 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-assets_host = Rails.configuration.action_controller.asset_host || "https://#{ENV['WEB_DOMAIN'] || ENV['LOCAL_DOMAIN']}"
+base_host     = Rails.configuration.x.web_domain
+assets_host   = Rails.configuration.action_controller.asset_host
+assets_host ||= "http#{Rails.configuration.x.use_https ? 's' : ''}://#{base_host}"
 
 Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-  p.script_src      :self, assets_host
   p.font_src        :self, assets_host
-  p.img_src         :self, :https, :data, :blob
+  p.img_src         :self, :https, :data, :blob, assets_host
   p.style_src       :self, :unsafe_inline, assets_host
-  p.media_src       :self, :data, assets_host
+  p.media_src       :self, :https, :data, assets_host
   p.frame_src       :self, :https
-  p.connect_src     :self, assets_host, Rails.configuration.x.streaming_api_base_url
+
+  if Rails.env.development?
+    webpacker_urls = %w(ws http).map { |protocol| "#{protocol}#{Webpacker.dev_server.https? ? 's' : ''}://#{Webpacker.dev_server.host_with_port}" }
+
+    p.connect_src :self, assets_host, Rails.configuration.x.streaming_api_base_url, *webpacker_urls
+    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host
+  else
+    p.connect_src :self, assets_host, Rails.configuration.x.streaming_api_base_url
+    p.script_src  :self, assets_host
+  end
 end
 
 # Report CSP violations to a specified URI
