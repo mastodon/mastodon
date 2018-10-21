c5ffbe3bd1632f6de6df2ffadb29effc20408b3c
Masoud Abkenar <ampbox@gmail.com>
2018-10-21 13:32:42 +0200

    RTL: remove blank character inside bdi

--- app/javascript/mastodon/components/display_name.js
+++ app/javascript/mastodon/components/display_name.js
@@ -22,7 +22,7 @@ export default class DisplayName extends React.PureComponent {
 
     return (
       <span className='display-name'>
-        <bdi><strong className='display-name__html' dangerouslySetInnerHTML={displayNameHtml} /></bdi> {suffix}
+        <bdi><strong className='display-name__html' dangerouslySetInnerHTML={displayNameHtml}/></bdi> {suffix}
       </span>
     );
   }
