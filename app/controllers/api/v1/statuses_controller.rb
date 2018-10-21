9bbc4cd0a393647237c9845f01c82f00cfc04b03
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-22 01:10:01 +0900

    fix

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -52,7 +52,7 @@ class Api::V1::StatusesController < Api::BaseController
                                          status_params[:status],
                                          status_params[:in_reply_to_id].blank? ? nil : Status.find(status_params[:in_reply_to_id]),
                                          media_ids: status_params[:media_ids],
-                                         check_media,
+                                         check_media(),
                                          spoiler_text: status_params[:spoiler_text],
                                          visibility: status_params[:visibility],
                                          application: doorkeeper_token.application,
