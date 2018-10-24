468a37efbc7a589ab1fbb350cf83c00adc11263c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-24 18:07:25 +0900

    fix remove mastodon_cthuluhu

--- config/initializers/mastoson_cthulhu.rb
+++ config/initializers/mastoson_cthulhu.rb
@@ -1,3 +1,4 @@
+=begin
 MastodonCthulhu.setup do |status|	
   fortune = MastodonCthulhu::Random.new('[ 　\n]?#(クトゥルフ神話)[ 　\n]?', %w(いあ!いあ!くとぅるぅ! いあ!いあ!はすたぁ! いあ!いあ!つとぅぁぐぁ! ふんぐるいむぐるうなふ! うがふなぐる! ふたぐん! ふんぐるい！むぐるうなふ！くとぅぐあ！ふぉまるはうと！んがあ・ぐあ！なふるたぐん！いあ！くとぅぐあ！ いあ！いあ！はすたあ！はすたあ!くふあやく!ぶるぐとむ!ぶぐとらぐるん!ぶるぐとむ!あい！あい！はすたあ！))
   status = fortune.convert(status) if fortune.match(status)	
@@ -108,3 +109,4 @@ MastodonCthulhu.setup do |status|
   status = fortune.convert(status) if fortune.match(status)	
   status
 end
+=end
