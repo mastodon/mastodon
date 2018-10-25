461af7d95eaf806364382dbec819986f6075874a
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-25 16:42:39 +0900

    remove image_path

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -51,7 +51,7 @@ class Api::V1::StatusesController < Api::BaseController
                                          status_params[:status],
                                          status_params[:in_reply_to_id].blank? ? nil : Status.find(status_params[:in_reply_to_id]),
                                          media_ids: status_params[:media_ids],
-                                         sensitive: check_nsfw(set_image_path),
+                                         sensitive: check_nsfw,
                                          spoiler_text: status_params[:spoiler_text],
                                          visibility: status_params[:visibility],
                                          application: doorkeeper_token.application,
@@ -87,34 +87,14 @@ class Api::V1::StatusesController < Api::BaseController
     params.slice(:limit).permit(:limit).merge(core_params)
   end
 
-  def set_image_path
-    paths = Array.new
-
-    if status_params[:media_ids].class != nil.class
-      status_params[:media_ids].each do |id|
-
-        image = MediaAttachment.find(id)
-        if ENV['S3_REGION'].to_s != "" then
-          path =  "0" * (9 - image.id.to_s.size) + image.id.to_s
-          ps = "#{path[0] + path[1] + path[2]}/#{path[3] + path[4] + path[5]}/#{path[6] + path[7] + path[8]}/original/#{image.file_file_name.to_s}"
-          puts paths.push("https://s3-#{ENV['S3_REGION'].to_s}.amazonaws.com/#{ENV['S3_BUCKET']}/media_attachments/files/#{ps}")
-        else
-          path =  "0" * (9 - image.id.to_s.size) + image.id.to_s
-          ps = "#{path[0] + path[1] + path[2]}/#{path[3] + path[4] + path[5]}/#{path[6] + path[7] + path[8]}/original/#{image.file_file_name.to_s}"
-          puts paths.push("public/system/media_attachments/files/#{ps}")
-        end
-      end
-    end
-
-    return paths
-  end
-
-  def check_nsfw(paths)
+  def check_nsfw
     keys = JSON.parse(File.open("./key.json").read).to_h
 
     Dotenv.load
 
     vision = Google::Cloud::Vision.new project: keys["project_id"]
+
+    paths = MediaAttachment.where(id: media_ids).map { |attachment| full_asset_url(attachment.file.url(:original))
 
     paths.each do |path|
 
