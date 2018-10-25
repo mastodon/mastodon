2807dcc31efe843ad6377b6c4f38ca235a25d357
Gomasy <nyan@gomasy.jp>
2018-10-25 21:53:40 +0900

    Fix some fonts were not loaded

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,7 +10,7 @@ Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-  p.font_src        :self, assets_host
+  p.font_src        :self, :data, assets_host
   p.img_src         :self, :https, :data, :blob, assets_host
   p.style_src       :self, :unsafe_inline, assets_host
   p.media_src       :self, :https, :data, assets_host
