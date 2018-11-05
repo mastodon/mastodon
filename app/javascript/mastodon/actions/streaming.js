4c03e05a4e1a237f8a414a0861c03abe3269dbc8
James Kiesel <james.kiesel@gmail.com>
2018-11-06 06:53:25 +1300

    Allow joining several hashtags in a single column (#8904)
    * Nascent tag menu on frontend
    
    * Hook up frontend to search
    
    * Tag intersection backend first pass
    
    * Update yarnlock
    
    * WIP
    
    * Fix for tags not searching correctly
    
    * Make radio buttons function
    
    * Simplify radio buttons with modeOption
    
    * Better naming
    
    * Rearrange options
    
    * Add all/any/none functionality on backend
    
    * Small PR cleanup
    
    * Move to service from scope
    
    * Small cleanup, add proper service tests
    
    * Don't use send with user input :D
    
    * Set appropriate column header
    
    * Handle auto updating timeline
    
    * Fix up toggle function
    
    * Use tag value correctly
    
    * A bit more correct to use 'self' rather than 'all' in status scope
    
    * Fix some style issues
    
    * Fix more code style issues
    
    * Style select dropdown more better
    
    * Only use to_id'ed value to ensure no SQL injection
    
    * Revamp frontend to allow for multiple selects
    
    * Update backend / col header to account for more flexible tagging
    
    * Update brakeman ignore
    
    * Codeclimate suggestions
    
    * Fix presenter tag_url
    
    * Implement initial PR feedback
    
    * Handle additional tag streaming
    
    * CodeClimate tweak

--- app/javascript/mastodon/actions/streaming.js
+++ app/javascript/mastodon/actions/streaming.js
@@ -12,7 +12,7 @@ import { getLocale } from '../locales';
 
 const { messages } = getLocale();
 
-export function connectTimelineStream (timelineId, path, pollingRefresh = null) {
+export function connectTimelineStream (timelineId, path, pollingRefresh = null, accept = null) {
 
   return connectStream (path, pollingRefresh, (dispatch, getState) => {
     const locale = getState().getIn(['meta', 'locale']);
@@ -24,7 +24,7 @@ export function connectTimelineStream (timelineId, path, pollingRefresh = null) 
       onReceive (data) {
         switch(data.event) {
         case 'update':
-          dispatch(updateTimeline(timelineId, JSON.parse(data.payload)));
+          dispatch(updateTimeline(timelineId, JSON.parse(data.payload), accept));
           break;
         case 'delete':
           dispatch(deleteFromTimelines(data.payload));
@@ -51,6 +51,6 @@ const refreshHomeTimelineAndNotification = (dispatch, done) => {
 export const connectUserStream      = () => connectTimelineStream('home', 'user', refreshHomeTimelineAndNotification);
 export const connectCommunityStream = ({ onlyMedia } = {}) => connectTimelineStream(`community${onlyMedia ? ':media' : ''}`, `public:local${onlyMedia ? ':media' : ''}`);
 export const connectPublicStream    = ({ onlyMedia } = {}) => connectTimelineStream(`public${onlyMedia ? ':media' : ''}`, `public${onlyMedia ? ':media' : ''}`);
-export const connectHashtagStream   = tag => connectTimelineStream(`hashtag:${tag}`, `hashtag&tag=${tag}`);
+export const connectHashtagStream   = (id, tag, accept) => connectTimelineStream(`hashtag:${id}`, `hashtag&tag=${tag}`, null, accept);
 export const connectDirectStream    = () => connectTimelineStream('direct', 'direct');
 export const connectListStream      = id => connectTimelineStream(`list:${id}`, `list&list=${id}`);
