mastodon/mastodon

commit bbd8fdd185063d8b2464b20ee95c366718b75fc6

Perform deep comparison for card data when receiving new props (#9270)
Fixes #9226

app/javascript/mastodon/features/status/components/card.js
@@ -73,7 +73,7 @@ export default class Card extends React.PureComponent {
   };
 
   componentWillReceiveProps (nextProps) {
-    if (this.props.card !== nextProps.card) {
+    if (!this.props.card.equals(nextProps.card)) {
       this.setState({ embedded: false });
     }
   }

commit e0e6538d26185b01733db20ce7a0a2dd4f34ab53

Fix null error introduced in #9270

app/javascript/mastodon/features/status/components/card.js
@@ -73,7 +73,7 @@ export default class Card extends React.PureComponent {
   };
 
   componentWillReceiveProps (nextProps) {
-    if (!this.props.card.equals(nextProps.card)) {
+    if (!Immutable.is(this.props.card, nextProps.card)) {
       this.setState({ embedded: false });
     }
   }