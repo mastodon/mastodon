41635136b7a524ea057daeb6cc6d748164a3b0e5
Thibaut Girka <thib@sitedethib.com>
2018-11-21 17:10:45 +0100

    Allow more than 4 attachments per status in the backend

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -67,7 +67,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
         visibility: visibility_from_audience,
         thread: replied_to_status,
         conversation: conversation_from_uri(@object['conversation']),
-        media_attachment_ids: process_attachments.take(4).map(&:id),
+        media_attachment_ids: process_attachments.map(&:id),
       }
     end
   end
