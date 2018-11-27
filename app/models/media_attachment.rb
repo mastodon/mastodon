8c8575db747b96168eaa72f918fed39184bcba7f
Hugo Gameiro <hmgameiro@gmail.com>
2018-11-27 11:19:12 +0000

    add loglevel to ffmpeg in gif upload (#9368)

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -59,6 +59,7 @@ class MediaAttachment < ApplicationRecord
     format: 'mp4',
     convert_options: {
       output: {
+        'loglevel' => 'fatal',
         'movflags' => 'faststart',
         'pix_fmt'  => 'yuv420p',
         'vf'       => 'scale=\'trunc(iw/2)*2:trunc(ih/2)*2\'',
