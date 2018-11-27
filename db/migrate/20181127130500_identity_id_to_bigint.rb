f784d14d48e635cf6d62b32ce11bad66f427a377
Eugen Rochko <eugen@zeonfederated.com>
2018-11-27 13:12:41 +0100

    Change user_id column in identities to bigint and fix down-migration

--- db/migrate/20181127130500_identity_id_to_bigint.rb
+++ db/migrate/20181127130500_identity_id_to_bigint.rb
@@ -9,13 +9,19 @@ class IdentityIdToBigint < ActiveRecord::Migration[5.2]
     safety_assured do
       change_column_type_concurrently :identities, :id, :bigint
       cleanup_concurrent_column_type_change :identities, :id
+
+      change_column_type_concurrently :identities, :user_id, :bigint
+      cleanup_concurrent_column_type_change :identities, :user_id
     end
   end
 
   def down
     safety_assured do
-      change_column_type_concurrently :identities, :id, :bigint
+      change_column_type_concurrently :identities, :id, :integer
       cleanup_concurrent_column_type_change :identities, :id
+
+      change_column_type_concurrently :identities, :user_id, :integer
+      cleanup_concurrent_column_type_change :identities, :user_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -236,11 +236,11 @@ ActiveRecord::Schema.define(version: 2018_11_27_130500) do
   end
 
   create_table "identities", force: :cascade do |t|
-    t.integer "user_id"
     t.string "provider", default: "", null: false
     t.string "uid", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
@@ -651,7 +651,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_130500) do
   add_foreign_key "follow_requests", "accounts", name: "fk_76d644b0e7", on_delete: :cascade
   add_foreign_key "follows", "accounts", column: "target_account_id", name: "fk_745ca29eac", on_delete: :cascade
   add_foreign_key "follows", "accounts", name: "fk_32ed1b5560", on_delete: :cascade
-  add_foreign_key "identities", "users", on_delete: :cascade
+  add_foreign_key "identities", "users", name: "fk_bea040f377", on_delete: :cascade
   add_foreign_key "imports", "accounts", name: "fk_6db1b6e408", on_delete: :cascade
   add_foreign_key "invites", "users", on_delete: :cascade
   add_foreign_key "list_accounts", "accounts", on_delete: :cascade
