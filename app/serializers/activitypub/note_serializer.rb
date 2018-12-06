6b7fd41bdf7e2dc8afaa274520cc46a8c660ce50
James <abdl.link@protonmail.com>
2018-12-06 21:48:14 +0000

    All media from us is sensitive

--- app/serializers/activitypub/note_serializer.rb
+++ app/serializers/activitypub/note_serializer.rb
@@ -36,6 +36,10 @@ class ActivityPub::NoteSerializer < ActivityPub::Serializer
     object.preloadable_poll ? 'Question' : 'Note'
   end
 
+  def sensitive
+    true
+  end
+
   def summary
     object.spoiler_text.presence
   end
