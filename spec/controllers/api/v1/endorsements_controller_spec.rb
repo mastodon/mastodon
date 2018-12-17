e28ea0ade4119f4da51b3583f2907709c79f79fb
Yosuke Kabuto <bluewhale1982@gmail.com>
2018-12-17 14:46:37 +0900

    Add spec for Api::V1::EndorsementsController

---
+++ spec/controllers/api/v1/endorsements_controller_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::EndorsementsController, type: :controller do
+  let(:user)  { Fabricate(:user) }
+  let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'read:accounts') }
+
+  describe 'GET #index' do
+    it 'returns 200' do
+      allow(controller).to receive(:doorkeeper_token) { token }
+      get :index
+
+      expect(response).to have_http_status(200)
+    end
+  end
+end
