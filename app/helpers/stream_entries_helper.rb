bc113927d6abc0e629acd0450ea187538155d2a1
Thibaut Girka <thib@sitedethib.com>
2018-12-18 14:26:21 +0100

    Move hide_followers_count logic to an helper

--- app/helpers/stream_entries_helper.rb
+++ app/helpers/stream_entries_helper.rb
@@ -60,6 +60,10 @@ module StreamEntriesHelper
     end
   end
 
+  def hide_followers_count(account)
+    Setting.hide_followers_count || account.user&.setting_hide_followers_count
+  end
+
   def account_description(account)
     prepend_stats = [
       [
@@ -73,7 +77,7 @@ module StreamEntriesHelper
       ].join(' '),
     ]
 
-    unless Setting.hide_followers_count || account.user&.setting_hide_followers_count
+    unless hide_followers_count(account)
       prepend_stats << [
         number_to_human(account.followers_count, strip_insignificant_zeros: true),
         I18n.t('accounts.followers', count: account.followers_count),
