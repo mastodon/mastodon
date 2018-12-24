a8fa9702d722d8e121ed1768e9a1030f0c9856ed
noellabo <noel.yoshiba@gmail.com>
2018-12-24 13:35:34 +0900

    fix tagTL_bugfix

--- app/javascript/mastodon/reducers/timelines.js
+++ app/javascript/mastodon/reducers/timelines.js
@@ -88,7 +88,7 @@ const deleteStatus = (state, id, accountId, references) => {
 };
 
 const clearTimeline = (state, timeline) => {
-  return state.updateIn([timeline, 'items'], list => list.clear());
+  return state.updateIn([timeline, 'items'], list => ImmutableList());
 };
 
 const filterTimelines = (state, relationship, statuses) => {
