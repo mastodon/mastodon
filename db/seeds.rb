f76934fdd2e19ddca436dd9e745a9d98a32e3221
Remi Rampin <remirampin@gmail.com>
2019-01-06 22:58:40 -0500

    Fix db/seeds.rb
    Add "agreement: true" to avoid:
    > ActiveRecord::RecordInvalid: Validation failed: Agreement must be accepted

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@ if Rails.env.development?
   domain = ENV['LOCAL_DOMAIN'] || Rails.configuration.x.local_domain
   admin  = Account.where(username: 'admin').first_or_initialize(username: 'admin')
   admin.save(validate: false)
-  User.where(email: "admin@#{domain}").first_or_initialize(email: "admin@#{domain}", password: 'mastodonadmin', password_confirmation: 'mastodonadmin', confirmed_at: Time.now.utc, admin: true, account: admin).save!
+  User.where(email: "admin@#{domain}").first_or_initialize(email: "admin@#{domain}", password: 'mastodonadmin', password_confirmation: 'mastodonadmin', confirmed_at: Time.now.utc, admin: true, account: admin, agreement: true).save!
 end
