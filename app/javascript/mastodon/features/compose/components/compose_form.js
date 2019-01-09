4abb3ce6f2da6cd55bbcb9d3f3ad473752249ed1
mimikun <karaage_nennen@icloud.com>
2019-01-09 22:12:16 +0900

    fix compose_form.js

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -93,11 +93,7 @@ class ComposeForm extends ImmutablePureComponent {
     const { is_submitting, is_changing_upload, is_uploading, anyMedia } = this.props;
     const fulltext = [this.props.spoiler_text, countableText(this.props.text)].join('');
 
-<<<<<<< HEAD
-    if (is_submitting || is_uploading || length(fulltext) > 5000 || (fulltext.length !== 0 && fulltext.trim().length === 0 && !anyMedia)) {
-=======
-    if (is_submitting || is_uploading || is_changing_upload || length(fulltext) > 500 || (fulltext.length !== 0 && fulltext.trim().length === 0 && !anyMedia)) {
->>>>>>> upstream/master
+    if (is_submitting || is_uploading || is_changing_upload || length(fulltext) > 5000 || (fulltext.length !== 0 && fulltext.trim().length === 0 && !anyMedia)) {
       return;
     }
 
