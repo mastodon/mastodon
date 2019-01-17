89b5b0aaf65a43f7c5c2e67512f90781363d876d
Thibaut Girka <thib@sitedethib.com>
2019-01-17 12:49:14 +0100

    Add Tombstone model to remember object deletion

---
+++ app/models/tombstone.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: tombstones
+#
+#  id         :bigint(8)        not null, primary key
+#  account_id :bigint(8)
+#  uri        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Tombstone < ApplicationRecord
+end
