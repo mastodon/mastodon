2fa9961b65aa955dd3aebe147df65b17ef25e1e4
Eugen Rochko <eugen@zeonfederated.com>
2019-01-18 19:06:27 +0100

    Add `visibility` param to reblog REST API
    Use async worker for creating reblog notification to improve performance

--- app/models/status.rb
+++ app/models/status.rb
@@ -70,6 +70,7 @@ class Status < ApplicationRecord
   validates_with StatusLengthValidator
   validates_with DisallowedHashtagsValidator
   validates :reblog, uniqueness: { scope: :account }, if: :reblog?
+  validates :visibility, exclusion: { in: %w(direct limited) }, if: :reblog?
   validates_associated :owned_poll
 
   default_scope { recent }
