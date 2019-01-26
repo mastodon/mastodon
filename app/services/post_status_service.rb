009ee6b1d06bd3deb4e3dac162f0819aa0f61d31
Thibaut Girka <thib@sitedethib.com>
2019-01-26 18:37:26 +0100

    Prevent posting toots with media attachments from someone else

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -93,7 +93,7 @@ class PostStatusService < BaseService
 
     raise Mastodon::ValidationError, I18n.t('media_attachments.validations.too_many') if @options[:media_ids].size > 4
 
-    @media = MediaAttachment.where(status_id: nil).where(id: @options[:media_ids].take(4).map(&:to_i))
+    @media = @account.media_attachments.where(status_id: nil).where(id: @options[:media_ids].take(4).map(&:to_i))
 
     raise Mastodon::ValidationError, I18n.t('media_attachments.validations.images_and_video') if @media.size > 1 && @media.find(&:video?)
   end
