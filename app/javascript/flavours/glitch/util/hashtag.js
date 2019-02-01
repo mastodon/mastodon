762e4fdf557ff7cbdd9fce301060f11bb23cb84e
Thibaut Girka <thib@sitedethib.com>
2019-02-01 11:41:34 +0100

    Fix hashtag processing when sending toots
    This fixes crashes in pleroma when writing toots with a content warning,
    since pleroma inserts a “nsfw” hashtag that isn't part of the toot's text.

--- app/javascript/flavours/glitch/util/hashtag.js
+++ app/javascript/flavours/glitch/util/hashtag.js
@@ -2,7 +2,7 @@ export function recoverHashtags (recognizedTags, text) {
   return recognizedTags.map(tag => {
       const re = new RegExp(`(?:^|[^\/\)\w])#(${tag.name})`, 'i');
       const matched_hashtag = text.match(re);
-      return matched_hashtag ? matched_hashtag[1] : tag;
+      return matched_hashtag ? matched_hashtag[1] : null;
     }
-  );
+  ).filter(x => x !== null);
 }
