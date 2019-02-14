5aa147b67d7b9e4ef1dc322a1ab1e40bfc35efd2
rinsuki <428rinsuki+git@gmail.com>
2019-02-15 01:03:01 +0900

    Fix breaks when opening a reply tree in WebUI (#10046)
    fix #10045

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -85,7 +85,7 @@ class Status extends ImmutablePureComponent {
 
   // Track height changes we know about to compensate scrolling
   componentDidMount () {
-    this.didShowCard = !this.props.muted && !this.props.hidden && this.props.status.get('card');
+    this.didShowCard = !this.props.muted && !this.props.hidden && this.props.status && this.props.status.get('card');
   }
 
   getSnapshotBeforeUpdate () {
@@ -98,7 +98,7 @@ class Status extends ImmutablePureComponent {
 
   // Compensate height changes
   componentDidUpdate (prevProps, prevState, snapshot) {
-    const doShowCard  = !this.props.muted && !this.props.hidden && this.props.status.get('card');
+    const doShowCard  = !this.props.muted && !this.props.hidden && this.props.status && this.props.status.get('card');
     if (doShowCard && !this.didShowCard) {
       this.didShowCard = true;
       if (snapshot !== null && this.props.updateScrollBottom) {
