6a25570f564fb2c29301332df8b522c2eef1218b
usbsnowcrash <jeff@debug.ninja>
2019-02-19 23:56:29 -0500

    using activeserializers seems like a good idea...

--- app/controllers/well_known/node_info_controller.rb
+++ app/controllers/well_known/node_info_controller.rb
@@ -7,30 +7,11 @@ module WellKnown
     before_action { response.headers['Vary'] = 'Accept' }
 
     def index
-      discovery = {
-        links: [
-          {
-            rel: 'http://nodeinfo.diaspora.software/ns/schema/2.0',
-            href: node_info_schema_url('2.0'),
-          },
-          {
-            rel: 'http://nodeinfo.diaspora.software/ns/schema/2.1',
-            href: node_info_schema_url('2.1'),
-          },
-        ],
-      }
-      render json: discovery
+      render json: ActiveModelSerializers::SerializableResource.new({}, serializer: NodeDiscoverySerializer)
     end
 
     def show
-      node_info = Rails.cache.fetch('nodeinfo', expires_in: 10.minutes) { NodeInfoSerializer.new.node_info }
-
-      if params[:format] == '0'
-        node_info[:version] = '2.0'
-        node_info[:software].delete(:repository)
-      end
-
-      render json: node_info.as_json
+      render json: ActiveModelSerializers::SerializableResource.new({}, serializer: NodeInfoSerializer, version: "2.#{ params[:format] }")
     end
   end
 end
