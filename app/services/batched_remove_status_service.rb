090ee03a8d2c7f9d2dbe17bba0150e0c5c7ea3a2
NOGISAKA Sadata <ngsksdt@gmail.com>
2019-02-21 23:35:29 +0900

    Skip some methods in BatchedRemoveStatusService when account is nil
    Sometimes `bin/tootctl accounts cull` fails with below error:
    
    undefined method `followers_for_local_distribution' for nil:NilClass (NoMethodError)
    
    This commit makes BatchedRemoveStatusService to skip below methods when
    `account` is nil:
    
    - unpush_from_home_timelines()
    - unpush_from_list_timelines()
    - batch_stream_entries()

--- app/services/batched_remove_status_service.rb
+++ app/services/batched_remove_status_service.rb
@@ -35,10 +35,12 @@ class BatchedRemoveStatusService < BaseService
     statuses.group_by(&:account_id).each_value do |account_statuses|
       account = account_statuses.first.account
 
-      unpush_from_home_timelines(account, account_statuses)
-      unpush_from_list_timelines(account, account_statuses)
+      if account
+        unpush_from_home_timelines(account, account_statuses)
+        unpush_from_list_timelines(account, account_statuses)
 
-      batch_stream_entries(account, account_statuses) if account.local?
+        batch_stream_entries(account, account_statuses) if account.local?
+      end
     end
 
     # Cannot be batched
