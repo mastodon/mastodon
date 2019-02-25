5221c6733cd1ae9be40a18a866f455254e0ddf64
Thibaut Girka <thib@sitedethib.com>
2019-02-25 14:07:58 +0100

    Move exponential backoff logic to a worker concern

--- app/workers/activitypub/fetch_replies_worker.rb
+++ app/workers/activitypub/fetch_replies_worker.rb
@@ -2,12 +2,9 @@
 
 class ActivityPub::FetchRepliesWorker
   include Sidekiq::Worker
+  include ExponentialBackoff
 
   sidekiq_options queue: 'pull', retry: 3
-
-  sidekiq_retry_in do |count|
-    15 + 10 * (count**4) + rand(10 * (count**4))
-  end
 
   def perform(parent_status_id, replies_uri)
     ActivityPub::FetchRepliesService.new.call(Status.find(parent_status_id), replies_uri)
--- app/workers/fetch_reply_worker.rb
+++ app/workers/fetch_reply_worker.rb
@@ -2,12 +2,9 @@
 
 class FetchReplyWorker
   include Sidekiq::Worker
+  include ExponentialBackoff
 
   sidekiq_options queue: 'pull', retry: 3
-
-  sidekiq_retry_in do |count|
-    15 + 10 * (count**4) + rand(10 * (count**4))
-  end
 
   def perform(child_url)
     FetchRemoteStatusService.new.call(child_url)
