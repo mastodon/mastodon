8a47895e4b19a67373fb757b6ec88494a31ff3fc
Thibaut Girka <thib@sitedethib.com>
2019-03-04 20:37:54 +0100

    Do the same for RSS feeds

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -19,7 +19,7 @@ class Formatter
 
     raw_content = status.text
 
-    if status.poll && options[:inline_poll_options]
+    if options[:inline_poll_options] && status.poll
       raw_content = raw_content + '\n\n' + status.poll.options.map { |title| "- #{title}" }.join('\n')
     end
 
