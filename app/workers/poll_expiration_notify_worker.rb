9acf463ec3a72ced3f5dfa839a57f971cc7cf194
Thibaut Girka <thib@sitedethib.com>
2019-03-07 23:02:00 +0100

    Add new notification type for ending polls

--- app/workers/poll_expiration_notify_worker.rb
+++ app/workers/poll_expiration_notify_worker.rb
@@ -8,6 +8,7 @@ class PollExpirationNotifyWorker
   def perform(poll_id)
     poll = Poll.find(poll_id)
     ActivityPub::DistributePollUpdateWorker.perform_async(poll.status.id)
+    NotifyService.new.call(poll.account, poll)
   rescue ActiveRecord::RecordNotFound
     true
   end
