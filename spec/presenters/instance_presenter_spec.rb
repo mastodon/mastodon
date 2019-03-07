d0eb0a8ac02df8b5d2fc182ef12167e900eeae27
kPherox <admin@mail.kr-kp.com>
2019-03-07 18:12:17 +0900

    Change source code link

--- spec/presenters/instance_presenter_spec.rb
+++ spec/presenters/instance_presenter_spec.rb
@@ -119,8 +119,8 @@ describe InstancePresenter do
   end
 
   describe '#source_url' do
-    it 'returns "https://github.com/tootsuite/mastodon"' do
-      expect(instance_presenter.source_url).to eq('https://github.com/tootsuite/mastodon')
+    it 'returns "https://github.com/kPherox/mastodon"' do
+      expect(instance_presenter.source_url).to eq('https://github.com/kPherox/mastodon')
     end
   end
 
