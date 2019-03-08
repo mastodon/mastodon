ffdd542c23bdb59e9b5fb1889d2d8ea15ea7fe6b
Thibaut Girka <thib@sitedethib.com>
2019-03-08 01:31:41 +0100

    Fix UpdatePollSerializer

--- app/serializers/activitypub/update_poll_serializer.rb
+++ app/serializers/activitypub/update_poll_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ActivityPub::UpdateSerializer < ActiveModel::Serializer
+class ActivityPub::UpdatePollSerializer < ActiveModel::Serializer
   attributes :id, :type, :actor, :to
 
   has_one :object, serializer: ActivityPub::NoteSerializer
