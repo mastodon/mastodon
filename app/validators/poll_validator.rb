5de7722ce84d14cb97efa62bab5b2deeed12ff2d
Delta <kyubi.develop@gmail.com>
2019-03-24 05:58:22 +0900

    https://github.com/tootsuite/mastodon/issues/10190 対応
    expires_atの範囲を1秒両端伸ばした。

--- app/validators/poll_validator.rb
+++ app/validators/poll_validator.rb
@@ -7,13 +7,15 @@ class PollValidator < ActiveModel::Validator
   MIN_EXPIRATION   = 5.minutes.freeze
 
   def validate(poll)
-    current_time = Time.now.utc
+    current_time = Time.now.utc.freeze
+    border_min = current_time + MIN_EXPIRATION - 1.seconds
+    border_max = current_time + MAX_EXPIRATION + 1.seconds
 
     poll.errors.add(:options, I18n.t('polls.errors.too_few_options')) unless poll.options.size > 1
     poll.errors.add(:options, I18n.t('polls.errors.too_many_options', max: MAX_OPTIONS)) if poll.options.size > MAX_OPTIONS
     poll.errors.add(:options, I18n.t('polls.errors.over_character_limit', max: MAX_OPTION_CHARS)) if poll.options.any? { |option| option.mb_chars.grapheme_length > MAX_OPTION_CHARS }
     poll.errors.add(:options, I18n.t('polls.errors.duplicate_options')) unless poll.options.uniq.size == poll.options.size
-    poll.errors.add(:expires_at, I18n.t('polls.errors.duration_too_long')) if poll.expires_at.nil? || poll.expires_at - current_time > MAX_EXPIRATION
-    poll.errors.add(:expires_at, I18n.t('polls.errors.duration_too_short')) if poll.expires_at.present? && poll.expires_at - current_time < MIN_EXPIRATION
+    poll.errors.add(:expires_at, I18n.t('polls.errors.duration_too_long')) if poll.expires_at.nil? || border_max < poll.expires_at
+    poll.errors.add(:expires_at, I18n.t('polls.errors.duration_too_short')) if poll.expires_at.present? && poll.expires_at < border_min
   end
 end
