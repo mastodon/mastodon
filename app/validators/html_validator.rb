0dd78d242b6cf5551b3f78b77784dcae8d2c9978
Thibaut Girka <thib@sitedethib.com>
2019-03-23 23:30:18 +0100

    Report first HTML error instead on validation error

--- app/validators/html_validator.rb
+++ app/validators/html_validator.rb
@@ -3,13 +3,16 @@
 class HtmlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
-    record.errors.add(attribute, I18n.t('html_validator.invalid_markup')) unless valid_html?(value)
+    errors = html_errors(value)
+    unless errors.empty?
+      record.errors.add(attribute, I18n.t('html_validator.invalid_markup', error: errors.first.to_s))
+    end
   end
 
   private
 
-  def valid_html?(str)
+  def html_errors(str)
     fragment = Nokogiri::HTML.fragment(str)
-    fragment.errors.empty?
+    fragment.errors
   end
 end
