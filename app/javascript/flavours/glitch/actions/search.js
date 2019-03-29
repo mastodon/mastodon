59551ece31dede539d893e020c59a67b40f7ecdc
Eugen Rochko <eugen@zeonfederated.com>
2019-03-30 00:43:38 +0100

    [Glitch] Fix search in web UI not setting a limit, restore limit of 10
    Port 379c3e237fd271b91073f4b4a4a1626e84a7250f to glitch-soc, but changing
    the 5 limit to 10, since that's what we had previously

--- app/javascript/flavours/glitch/actions/search.js
+++ app/javascript/flavours/glitch/actions/search.js
@@ -37,6 +37,7 @@ export function submitSearch() {
       params: {
         q: value,
         resolve: true,
+        limit: 10,
       },
     }).then(response => {
       if (response.data.accounts) {
