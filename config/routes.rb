1b310ddba36ab74c5d24c482d7bcd3aa517b8088
noellabo <noel.yoshiba@gmail.com>
2019-04-07 08:44:17 +0900

    Revert "Add endorsement account controller to public profile page"
    This reverts commit acedf5f2f1da21e96f83fc24f267688afb32dbf0.

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,6 @@ Rails.application.routes.draw do
 
     resources :followers, only: [:index], controller: :follower_accounts
     resources :following, only: [:index], controller: :following_accounts
-    resources :endorsed, only: [:index], controller: :endorsement_accounts
     resource :follow, only: [:create], controller: :account_follow
     resource :unfollow, only: [:create], controller: :account_unfollow
 
