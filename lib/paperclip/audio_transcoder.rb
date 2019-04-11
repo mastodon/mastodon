f11ad4023c542f0b9012f60c75b0b82d7dc77e93
Izalia Mae <izalia@barkshark.tk>
2019-04-11 07:23:31 -0400

    add env var for max length of audio uploads

--- lib/paperclip/audio_transcoder.rb
+++ lib/paperclip/audio_transcoder.rb
@@ -3,10 +3,12 @@
 module Paperclip
   class AudioTranscoder < Paperclip::Processor
     def make
+      max_aud_len = (ENV['MAX_AUDIO_LENGTH'] || 60.0).to_f
+
       meta = ::Av.cli.identify(@file.path)
       # {:length=>"0:00:02.14", :duration=>2.14, :audio_encode=>"mp3", :audio_bitrate=>"44100 Hz", :audio_channels=>"mono"}
-      if meta[:duration] > 60.0
-        raise Mastodon::ValidationError, "Audio uploads must be less than 60 seconds in length."
+      if meta[:duration] > max_aud_len
+        raise Mastodon::ValidationError, "Audio uploads must be less than #{max_aud_len} seconds in length."
       end
       
       final_file = Paperclip::Transcoder.make(file, options, attachment)
