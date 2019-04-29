3e095cab83f3e88c5f5f4ca9d7029379ed5b5b56
Thibaut Girka <thib@sitedethib.com>
2019-04-29 19:00:27 +0200

    Add support for missing formatting tags

--- app/lib/sanitize_config.rb
+++ app/lib/sanitize_config.rb
@@ -20,7 +20,7 @@ class Sanitize
     end
 
     MASTODON_STRICT ||= freeze_config(
-      elements: %w(p br span a abbr del pre blockquote code b strong i em h1 h2 h3 h4 h5 ul ol li),
+      elements: %w(p br span a abbr del pre blockquote code b strong u sub i em h1 h2 h3 h4 h5 ul ol li),
 
       attributes: {
         'a'          => %w(href rel class title),
