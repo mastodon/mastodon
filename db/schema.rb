6dd3d241dbbb79b20ac2f989e6f4d782c5bad402
kjwon15 <kjwonmail@gmail.com>
2019-05-10 02:09:58 +0900

    Add missing migration script

--- db/schema.rb
+++ db/schema.rb
@@ -591,8 +591,8 @@ ActiveRecord::Schema.define(version: 2019_05_09_164208) do
   create_table "status_pins", force: :cascade do |t|
     t.bigint "account_id", null: false
     t.bigint "status_id", null: false
-    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.datetime "updated_at", default: -> { "now()" }, null: false
     t.index ["account_id", "status_id"], name: "index_status_pins_on_account_id_and_status_id", unique: true
   end
 
