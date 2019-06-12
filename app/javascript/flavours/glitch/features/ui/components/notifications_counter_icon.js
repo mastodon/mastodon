0f41be5581b4980be2ae7334c169272912abf9a7
Thibaut Girka <thib@sitedethib.com>
2019-06-12 20:58:47 +0200

    Fix notification badge

--- app/javascript/flavours/glitch/features/ui/components/notifications_counter_icon.js
+++ app/javascript/flavours/glitch/features/ui/components/notifications_counter_icon.js
@@ -1,9 +1,9 @@
 import { connect } from 'react-redux';
-import IconWithBadge from 'flavours/glitch/components/icon';
+import IconWithBadge from 'flavours/glitch/components/icon_with_badge';
 
 const mapStateToProps = state => ({
   count: state.getIn(['local_settings', 'notifications', 'tab_badge']) ? state.getIn(['notifications', 'unread']) : 0,
-  icon: 'bell',
+  id: 'bell',
 });
 
 export default connect(mapStateToProps)(IconWithBadge);
