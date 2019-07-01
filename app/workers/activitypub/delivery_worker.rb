f2795699dd7091f9204bf6a53314387f4752e427
Eugen Rochko <eugen@zeonfederated.com>
2019-07-02 00:59:53 +0200

    Change ActivityPub::DeliveryWorker to not retry HTTP 501 errors (#11233)

--- app/workers/activitypub/delivery_worker.rb
+++ app/workers/activitypub/delivery_worker.rb
@@ -51,7 +51,7 @@ class ActivityPub::DeliveryWorker
   end
 
   def response_error_unsalvageable?(response)
-    (400...500).cover?(response.code) && ![401, 408, 429].include?(response.code)
+    response.code == 501 || ((400...500).cover?(response.code) && ![401, 408, 429].include?(response.code))
   end
 
   def failure_tracker
