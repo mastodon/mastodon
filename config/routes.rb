ef1524639776fe7d7be2d5c414fc98dd2410a5f4
Eugen Rochko <eugen@zeonfederated.com>
2019-07-08 12:04:06 +0200

    Remove unused remote unfollow controller (#11250)

--- config/routes.rb
+++ config/routes.rb
@@ -141,8 +141,6 @@ Rails.application.routes.draw do
   get '/public', to: 'public_timelines#show', as: :public_timeline
   get '/media_proxy/:id/(*any)', to: 'media_proxy#show', as: :media_proxy
 
-  # Remote follow
-  resource :remote_unfollow, only: [:create]
   resource :authorize_interaction, only: [:show, :create]
   resource :share, only: [:show, :create]
 
