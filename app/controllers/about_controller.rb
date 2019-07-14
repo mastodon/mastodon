59ae2d52536a27fdc46cedff185b567a4af01ad1
Thibaut Girka <thib@sitedethib.com>
2019-07-14 16:35:52 +0200

    Add automatic blocklist display in /about/blocks
    Inspired by https://github.com/Gargron/mastodon.social-misc

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -4,9 +4,11 @@ class AboutController < ApplicationController
   layout 'public'
 
   before_action :require_open_federation!, only: [:show, :more]
+  before_action :check_blocklist_enabled, only: [:blocks]
+  before_action :require_user!, only: [:blocks], if: :blocklist_account_required?
   before_action :set_body_classes, only: :show
   before_action :set_instance_presenter
-  before_action :set_expires_in
+  before_action :set_expires_in, only: [:show, :more, :terms]
 
   skip_before_action :require_functional!, only: [:more, :terms]
 
@@ -18,11 +20,36 @@ class AboutController < ApplicationController
 
   def terms; end
 
+  def blocks
+    @blocks = DomainBlock.all
+  end
+
   private
 
   def require_open_federation!
     not_found if whitelist_mode?
   end
+
+  def check_blocklist_enabled
+    nil #TODO
+  end
+
+  def blocklist_account_required?
+    false #TODO
+  end
+
+  def block_severity_text(block)
+    if block.severity == 'suspend'
+      I18n.t('domain_blocks.suspension')
+    else
+      limitations = []
+      limitations << I18n.t('domain_blocks.media_block') if block.reject_media?
+      limitations << I18n.t('domain_blocks.silence') if block.severity == 'silence'
+      limitations.join(', ')
+    end
+  end
+
+  helper_method :block_severity_text
 
   def new_user
     User.new.tap do |user|
--- config/routes.rb
+++ config/routes.rb
@@ -423,9 +423,10 @@ Rails.application.routes.draw do
 
   get '/web/(*any)', to: 'home#index', as: :web
 
-  get '/about',      to: 'about#show'
-  get '/about/more', to: 'about#more'
-  get '/terms',      to: 'about#terms'
+  get '/about',        to: 'about#show'
+  get '/about/more',   to: 'about#more'
+  get '/about/blocks', to: 'about#blocks'
+  get '/terms',        to: 'about#terms'
 
   root 'home#index'
 
