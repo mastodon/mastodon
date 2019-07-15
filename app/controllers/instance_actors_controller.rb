dd333a596f903a867ded2927ca0002bacc919901
Thibaut Girka <thib@sitedethib.com>
2019-07-15 20:57:58 +0200

    Remove unused `icon` and `image` attributes from instance actor

--- app/controllers/instance_actors_controller.rb
+++ app/controllers/instance_actors_controller.rb
@@ -25,6 +25,6 @@ class InstanceActorsController < ApplicationController
   end
 
   def restrict_fields_to
-    %i(id type preferred_username inbox public_key endpoints icon image url manually_approves_followers)
+    %i(id type preferred_username inbox public_key endpoints url manually_approves_followers)
   end
 end
