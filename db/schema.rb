ff5f470e1929a2147f0db215867ae4bcc6e6a302
Thibaut Girka <thib@sitedethib.com>
2019-07-15 16:53:14 +0200

    Add migration script to create instance actor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_233204) do
+ActiveRecord::Schema.define(version: 2019_07_15_164535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
