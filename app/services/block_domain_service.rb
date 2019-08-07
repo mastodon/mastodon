c55a89a2e6efa126bf758cd9e6658ce060f94815
Thibaut Girka <thib@sitedethib.com>
2019-08-07 17:04:11 +0200

    Allow editing domain blocks

--- app/services/block_domain_service.rb
+++ app/services/block_domain_service.rb
@@ -3,12 +3,21 @@
 class BlockDomainService < BaseService
   attr_reader :domain_block
 
-  def call(domain_block)
+  def call(domain_block, update = false)
     @domain_block = domain_block
     process_domain_block!
+    process_retroactive_updates! if update
   end
 
   private
+
+  def process_retroactive_updates!
+    # If the domain block severity has been changed, undo the appropriate limitations
+    scope = Account.by_domain_and_subdomains(domain_block.domain)
+
+    scope.where(silenced_at: domain_block.created_at).in_batches.update_all(silenced_at: nil) unless domain_block.silence?
+    scope.where(suspended_at: domain_block.created_at).in_batches.update_all(suspended_at: nil) unless domain_block.suspend?
+  end
 
   def process_domain_block!
     clear_media! if domain_block.reject_media?
