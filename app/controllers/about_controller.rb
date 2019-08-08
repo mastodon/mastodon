00bdc2d53747e7a624f5d439fcce3613ad421db4
Thibaut Girka <thib@sitedethib.com>
2019-08-08 14:25:22 +0200

    Disable /about/blocks when in whitelist mode

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,7 +3,7 @@
 class AboutController < ApplicationController
   layout 'public'
 
-  before_action :require_open_federation!, only: [:show, :more]
+  before_action :require_open_federation!, only: [:show, :more, :blocks]
   before_action :check_blocklist_enabled, only: [:blocks]
   before_action :authenticate_user!, only: [:blocks], if: :blocklist_account_required?
   before_action :set_body_classes, only: :show
