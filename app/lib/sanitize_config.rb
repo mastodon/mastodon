317fa43d9a696db353612269cb35b7741a3dfc64
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2019-08-14 22:26:30 +0200

    Add support for <sup> formatting element
    This is based of 3e095cab83f3e88c5f5f4ca9d7029379ed5b5b56
    
    Related: https://git.pleroma.social/pleroma/pleroma/issues/1191

--- app/lib/sanitize_config.rb
+++ app/lib/sanitize_config.rb
@@ -39,7 +39,7 @@ class Sanitize
     end
 
     MASTODON_STRICT ||= freeze_config(
-      elements: %w(p br span a abbr del pre blockquote code b strong u sub i em h1 h2 h3 h4 h5 ul ol li),
+      elements: %w(p br span a abbr del pre blockquote code b strong u sub sup i em h1 h2 h3 h4 h5 ul ol li),
 
       attributes: {
         'a'          => %w(href rel class title),
