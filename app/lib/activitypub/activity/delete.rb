d96b44b3bbf2a7f5d361259f547b15a7e47ba689
Eugen Rochko <eugen@zeonfederated.com>
2019-08-21 23:20:36 +0200

    Fix remote and staff-removed statuses leaving media behind for a day
    The reason for unattaching media instead of removing it is to support
    delete & redraft functionality, but remote or staff-removed statuses
    will never be redrafted, so the media should be deleted immediately

--- app/lib/activitypub/activity/delete.rb
+++ app/lib/activitypub/activity/delete.rb
@@ -70,7 +70,7 @@ class ActivityPub::Activity::Delete < ActivityPub::Activity
   end
 
   def delete_now!
-    RemoveStatusService.new.call(@status)
+    RemoveStatusService.new.call(@status, redraft: false)
   end
 
   def payload
