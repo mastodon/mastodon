6ddbe0e44457aefba09c23ab2331e6f285f8c30c
Rob The Builder <rob@plank.life>
2019-08-26 23:47:19 +0530

    migration for active_record_store to store sessions

---
+++ db/migrate/20190826134425_add_sessions_table.rb
@@ -0,0 +1,12 @@
+class AddSessionsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sessions do |t|
+      t.string :session_id, :null => false
+      t.text :data
+      t.timestamps
+    end
+
+    add_index :sessions, :session_id, :unique => true
+    add_index :sessions, :updated_at
+  end
+end
