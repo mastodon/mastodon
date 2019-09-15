f08680b7137489da08cd0f62ef36f9a668625358
noellabo <noel.yoshiba@gmail.com>
2019-09-15 23:10:46 +0900

    Add short numeric notation to the dashboard

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -7,4 +7,12 @@ module Admin::DashboardHelper
 
     safe_join([feature, content_tag(:span, indicator, class: class_names)])
   end
+
+  def short_number_format(number)
+    if Setting.short_number_enabled
+      number_to_human number
+    else
+      number_with_delimiter number
+    end
+  end
 end
