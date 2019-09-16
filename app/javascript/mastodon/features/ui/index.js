0eabec41a06cdf8b6da7f2617fc079bb4975f925
Emma Winston <emma.winston@gmail.com>
2019-09-16 21:47:23 +0100

    Remove Mastodon from draft warning text

--- app/javascript/mastodon/features/ui/index.js
+++ app/javascript/mastodon/features/ui/index.js
@@ -55,7 +55,7 @@ import { previewState as previewVideoState } from './components/video_modal';
 import '../../components/status';
 
 const messages = defineMessages({
-  beforeUnload: { id: 'ui.beforeunload', defaultMessage: 'Your draft will be lost if you leave Mastodon.' },
+  beforeUnload: { id: 'ui.beforeunload', defaultMessage: 'Your draft will be lost if you leave.' },
 });
 
 const mapStateToProps = state => ({
