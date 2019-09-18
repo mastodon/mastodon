0e6390753d4a93af7e705f7cca5946e232298fc5
abcang <abcang1015@gmail.com>
2019-09-18 17:58:08 +0900

    Add users remember_token index (#11881)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_222339) do
+ActiveRecord::Schema.define(version: 2019_09_17_213523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -743,6 +743,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_222339) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["created_by_application_id"], name: "index_users_on_created_by_application_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
