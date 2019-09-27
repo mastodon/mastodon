07b057eabb9cc923aa7fc6bb851084af048ed5d2
abcang <abcang1015@gmail.com>
2019-09-27 22:24:13 +0900

    Validate Web::PushSubscription (#11971)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_213523) do
+ActiveRecord::Schema.define(version: 2019_09_27_124642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
