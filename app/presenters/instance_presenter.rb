c7f29c4f916aef1b65719a98441ade275287df80
Eugen Rochko <eugen@zeonfederated.com>
2019-10-01 00:19:48 +0200

    Fix active user count for different number of weeks using same cache
    Fix #12003

--- app/presenters/instance_presenter.rb
+++ app/presenters/instance_presenter.rb
@@ -21,7 +21,7 @@ class InstancePresenter
   end
 
   def active_user_count(weeks = 4)
-    Rails.cache.fetch('active_user_count') { Redis.current.pfcount(*(0...weeks).map { |i| "activity:logins:#{i.weeks.ago.utc.to_date.cweek}" }) }
+    Rails.cache.fetch("active_user_count/#{weeks}") { Redis.current.pfcount(*(0...weeks).map { |i| "activity:logins:#{i.weeks.ago.utc.to_date.cweek}" }) }
   end
 
   def status_count
