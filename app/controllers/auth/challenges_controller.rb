7fd54178d0ca7b2589fc44e24174b505801dddfb
Thibaut Girka <thib@sitedethib.com>
2019-09-30 13:37:13 +0200

    glitch-soc theme engine fixes for the challenges controller

--- app/controllers/auth/challenges_controller.rb
+++ app/controllers/auth/challenges_controller.rb
@@ -5,6 +5,7 @@ class Auth::ChallengesController < ApplicationController
 
   layout 'auth'
 
+  before_action :set_pack
   before_action :authenticate_user!
 
   skip_before_action :require_functional!
@@ -18,5 +19,11 @@ class Auth::ChallengesController < ApplicationController
       flash.now[:alert] = I18n.t('challenge.invalid_password')
       render_challenge
     end
+  end
+
+  private
+
+  def set_pack
+    use_pack 'auth'
   end
 end
