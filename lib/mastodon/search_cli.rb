3f925586dbe9ccd75c1a44bb13e398c831e801d7
Eugen Rochko <eugen@zeonfederated.com>
2019-10-02 20:58:11 +0200

    Rename --parallel to --processes

--- lib/mastodon/search_cli.rb
+++ lib/mastodon/search_cli.rb
@@ -6,7 +6,7 @@ require_relative 'cli_helper'
 
 module Mastodon
   class SearchCLI < Thor
-    option :parallel, default: 2, aliases: [:p]
+    option :processes, default: 2, aliases: [:p]
     desc 'deploy', 'Create or update an ElasticSearch index and populate it'
     long_desc <<~LONG_DESC
       If ElasticSearch is empty, this command will create the necessary indices
@@ -15,21 +15,21 @@ module Mastodon
       This command will also upgrade indices if the underlying schema has been
       changed since the last run.
 
-      With the --parallel option, specify the number of processes to run the command
-      with. The default is 2. If 'auto' is specified, the number is automatically
+      With the --processes option, parallelize execution of the command. The
+      default is 2. If "auto" is specified, the number is automatically
       derived from available CPUs.
     LONG_DESC
     def deploy
-      processed = Chewy::RakeHelper.upgrade parallel: parallel
-      Chewy::RakeHelper.sync(except: processed, parallel: parallel)
+      processed = Chewy::RakeHelper.upgrade(parallel: processes)
+      Chewy::RakeHelper.sync(except: processed, parallel: processes)
     end
 
     private
 
-    def parallel
-      return true if options[:parallel] == 'auto'
+    def processes
+      return true if options[:processes] == 'auto'
 
-      num = options[:parallel].to_i
+      num = options[:processes].to_i
 
       if num < 2
         nil
