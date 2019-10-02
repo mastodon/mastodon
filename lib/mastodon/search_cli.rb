68f9443851bea37afbf6fdef25881a04d1045374
noellabo <noel.yoshiba@gmail.com>
2019-10-03 02:34:21 +0900

    Add paralell option to tootctl search deploy

--- lib/mastodon/search_cli.rb
+++ lib/mastodon/search_cli.rb
@@ -6,6 +6,7 @@ require_relative 'cli_helper'
 
 module Mastodon
   class SearchCLI < Thor
+    option :parallel, default: 2, aliases: [:p]
     desc 'deploy', 'Create or update an ElasticSearch index and populate it'
     long_desc <<~LONG_DESC
       If ElasticSearch is empty, this command will create the necessary indices
@@ -13,10 +14,20 @@ module Mastodon
 
       This command will also upgrade indices if the underlying schema has been
       changed since the last run.
+
+      With the --parallel option, Execute tasks in parallel. The default is 2,
+      which specifies the number of processors. If 'auto' is specified,
+      it is automatically derived.
     LONG_DESC
     def deploy
-      processed = Chewy::RakeHelper.upgrade parallel: true
+      processed = Chewy::RakeHelper.upgrade parallel: parallel
       Chewy::RakeHelper.sync(except: processed)
+    end
+
+    private
+
+    def parallel
+      options[:parallel] == 'auto' ? true : Integer(options[:parallel], exception: false) || 1
     end
   end
 end
