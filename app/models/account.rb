7e2bff9959c8f538b1f8757c15791e14c70e9dec
Eugen Rochko <eugen@zeonfederated.com>
2019-10-09 01:59:07 +0200

    Fix attachment not being re-downloaded even if file is not stored
    Change the behaviour of remotable concern. Previously, it would skip
    downloading an attachment if the stored remote URL is identical to
    the new one. Now it would not be skipped if the attachment is not
    actually currently stored by Paperclip.

--- app/models/account.rb
+++ app/models/account.rb
@@ -310,10 +310,9 @@ class Account < ApplicationRecord
   def save_with_optional_media!
     save!
   rescue ActiveRecord::RecordInvalid
-    self.avatar              = nil
-    self.header              = nil
-    self[:avatar_remote_url] = ''
-    self[:header_remote_url] = ''
+    self.avatar = nil
+    self.header = nil
+
     save!
   end
 
