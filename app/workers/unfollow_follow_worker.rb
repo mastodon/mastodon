2b97b5c40fbaa44e149e6e835e0aa0e0da68dacc
Thibaut Girka <thib@sitedethib.com>
2019-11-04 19:19:21 +0100

    Fix show_reblogs not being preserved for moved account's followers

--- app/workers/unfollow_follow_worker.rb
+++ app/workers/unfollow_follow_worker.rb
@@ -10,7 +10,10 @@ class UnfollowFollowWorker
     old_target_account = Account.find(old_target_account_id)
     new_target_account = Account.find(new_target_account_id)
 
-    FollowService.new.call(follower_account, new_target_account, bypass_locked: bypass_locked)
+    follow = follower_account.active_relationships.find_by(target_account: old_target_account)
+    reblogs = follow&.show_reblogs?
+
+    FollowService.new.call(follower_account, new_target_account, reblogs: reblogs, bypass_locked: bypass_locked)
     UnfollowService.new.call(follower_account, old_target_account, skip_unmerge: true)
   rescue ActiveRecord::RecordNotFound, Mastodon::NotPermittedError
     true
