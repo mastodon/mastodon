be06e1369b055d4eaeba0c0ea7874478e8b42b55
Thibaut Girka <thib@sitedethib.com>
2019-11-13 13:49:43 +0100

    Add bookmarks item to single-column layout

--- app/javascript/mastodon/features/getting_started/index.js
+++ app/javascript/mastodon/features/getting_started/index.js
@@ -132,7 +132,7 @@ class GettingStarted extends ImmutablePureComponent {
       <ColumnLink key={i++} icon='list-ul' text={intl.formatMessage(messages.lists)} to='/lists' />
     );
 
-    height += 48*3;
+    height += 48*4;
 
     if (myAccount.get('locked') || unreadFollowRequests > 0) {
       navItems.push(<ColumnLink key={i++} icon='user-plus' text={intl.formatMessage(messages.follow_requests)} badge={badgeDisplay(unreadFollowRequests, 40)} to='/follow_requests' />);
