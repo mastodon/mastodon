0793d1bd5d5fd047cf07e39ae365949bfe61eced
mathieu.brunot <mathieu.brunot@monogramm.io>
2019-11-30 19:09:35 +0100

    :ok_hand: Remove unnecessary paranthesis
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- app/models/concerns/ldap_authenticable.rb
+++ app/models/concerns/ldap_authenticable.rb
@@ -15,7 +15,7 @@ module LdapAuthenticable
 
     def ldap_get_user(attributes = {})
       safe_username = attributes[Devise.ldap_uid.to_sym].first
-      if (Devise.ldap_uid_conversion_enabled)
+      if Devise.ldap_uid_conversion_enabled
         keys = Regexp.union(Devise.ldap_uid_conversion_search.chars)
         replacement = Devise.ldap_uid_conversion_replace
 
