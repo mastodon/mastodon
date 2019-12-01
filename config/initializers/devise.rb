d70268f0991ba69568112d4da5768e821d5983dd
Mathieu Brunot <mathieu.brunot@monogramm.io>
2019-12-01 07:21:28 +0100

    :sparkles: Convert LDAP username (#12461)
    * :sparkles: Convert LDAP username #12021
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :bug: Fix conversion var use
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :bug: Fix LDAP uid conversion test
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :ok_hand: Remove comments with ref to PR
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :ok_hand: Remove unnecessary paranthesis
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>
    
    * :wrench: Move space in conversion string
    
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -61,6 +61,12 @@ module Devise
   @@ldap_tls_no_verify = false
   mattr_accessor :ldap_search_filter
   @@ldap_search_filter = nil
+  mattr_accessor :ldap_uid_conversion_enabled
+  @@ldap_uid_conversion_enabled = false
+  mattr_accessor :ldap_uid_conversion_search
+  @@ldap_uid_conversion_search = nil
+  mattr_accessor :ldap_uid_conversion_replace
+  @@ldap_uid_conversion_replace = nil
 
   class Strategies::PamAuthenticatable
     def valid?
@@ -365,5 +371,8 @@ Devise.setup do |config|
     config.ldap_uid            = ENV.fetch('LDAP_UID', 'cn')
     config.ldap_tls_no_verify  = ENV['LDAP_TLS_NO_VERIFY'] == 'true'
     config.ldap_search_filter  = ENV.fetch('LDAP_SEARCH_FILTER', '%{uid}=%{email}')
+    config.ldap_uid_conversion_enabled  = ENV['LDAP_UID_CONVERSION_ENABLED'] == 'true'
+    config.ldap_uid_conversion_search   = ENV.fetch('LDAP_UID_CONVERSION_SEARCH', '.,- ')
+    config.ldap_uid_conversion_replace  = ENV.fetch('LDAP_UID_CONVERSION_REPLACE', '_')
   end
 end
