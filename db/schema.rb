c199dfe9269902f03f357bde9573a1b9567fd466
Stephen <stephen@stephendownward.ca>
2019-12-16 18:57:15 -0400

    partially reverted schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -653,8 +653,8 @@ ActiveRecord::Schema.define(version: 2019_12_12_003415) do
   create_table "status_pins", force: :cascade do |t|
     t.bigint "account_id", null: false
     t.bigint "status_id", null: false
-    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.datetime "updated_at", default: -> { "now()" }, null: false
     t.index ["account_id", "status_id"], name: "index_status_pins_on_account_id_and_status_id", unique: true
   end
 
