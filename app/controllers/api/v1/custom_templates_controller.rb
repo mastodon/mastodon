978e0d82ca28972cc008ff81c0617f8a08f4dd2e
Gomasy <nyan@gomasy.jp>
2019-12-18 10:36:21 +0900

    Add cache to custom templates API

--- app/controllers/api/v1/custom_templates_controller.rb
+++ app/controllers/api/v1/custom_templates_controller.rb
@@ -3,7 +3,10 @@
 class Api::V1::CustomTemplatesController < Api::BaseController
   respond_to :json
 
+  skip_before_action :set_cache_headers
+
   def index
-    render json: CustomTemplate.where(disabled: false), each_serializer: REST::CustomTemplateSerializer
+    expires_in 3.minutes, public: true
+    render_with_cache(each_serializer: REST::CustomTemplateSerializer) { CustomTemplate.where(disabled: false) }
   end
 end
