78f82c6b47a5b3d152408542f7c63b7d6f68ae85
Thibaut Girka <thib@sitedethib.com>
2019-12-29 21:41:23 +0100

    Remove leading “xmpp:” from auto-linked text

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -285,7 +285,7 @@ class Formatter
 
   def link_html(url)
     url    = Addressable::URI.parse(url).to_s
-    prefix = url.match(/\Ahttps?:\/\/(www\.)?/).to_s
+    prefix = url.match(/\A(https?:\/\/(www\.)?|xmpp:)/).to_s
     text   = url[prefix.length, 30]
     suffix = url[prefix.length + 30..-1]
     cutoff = url[prefix.length..-1].length > 30
