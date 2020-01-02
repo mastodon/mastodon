a40b8308fc1abb150d609320f6fbceb7b0f2cb61
Eugen Rochko <eugen@zeonfederated.com>
2020-01-02 23:10:42 +0100

    Fix more uncaught query param encoding errors

--- app/middleware/handle_bad_encoding_middleware.rb
+++ app/middleware/handle_bad_encoding_middleware.rb
@@ -1,16 +1,33 @@
 # frozen_string_literal: true
-# See: https://jamescrisp.org/2018/05/28/fixing-invalid-query-parameters-invalid-encoding-in-a-rails-app/
 
 class HandleBadEncodingMiddleware
+  SANITIZE_ENV_KEYS = %w(
+    HTTP_REFERER
+    PATH_INFO
+    REQUEST_URI
+    REQUEST_PATH
+    QUERY_STRING
+  ).freeze
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
+    SANITIZE_ENV_KEYS.each do |key|
+      str = CGI.unescape(env[key].to_s).force_encoding('UTF-8')
+      return [400, {}, ['Bad request']] unless str.valid_encoding?
+    end
+
     begin
-      Rack::Utils.parse_nested_query(env['QUERY_STRING'].to_s)
-    rescue Rack::Utils::InvalidParameterError
-      env['QUERY_STRING'] = ''
+      request = Rack::Request.new(env.dup)
+      request.params
+    rescue ArgumentError => e
+      if e.message =~ /invalid %-encoding/
+        return [400, {}, ['Bad request']]
+      else
+        raise e
+      end
     end
 
     @app.call(env)
--- spec/middleware/handle_bad_encoding_middleware_spec.rb
+++ spec/middleware/handle_bad_encoding_middleware_spec.rb
@@ -4,18 +4,33 @@ RSpec.describe HandleBadEncodingMiddleware do
   let(:app) { double() }
   let(:middleware) { HandleBadEncodingMiddleware.new(app) }
 
-  it "request with query string is unchanged" do
-    expect(app).to receive(:call).with("PATH" => "/some/path", "QUERY_STRING" => "name=fred")
-    middleware.call("PATH" => "/some/path", "QUERY_STRING" => "name=fred")
+  it 'passes requests with normal query parameters' do
+    env = env_for('/some/path?name=fred')
+    expect(app).to receive(:call).with(env)
+    middleware.call(env)
   end
 
-  it "request with no query string is unchanged" do
-    expect(app).to receive(:call).with("PATH" => "/some/path")
-    middleware.call("PATH" => "/some/path")
+  it 'passes requests with no query parameters' do
+    env = env_for('/some/path')
+    expect(app).to receive(:call).with(env)
+    middleware.call(env)
   end
 
-  it "request with invalid encoding in query string drops query string" do
-    expect(app).to receive(:call).with("QUERY_STRING" => "", "PATH" => "/some/path")
-    middleware.call("QUERY_STRING" => "q=%2Fsearch%2Fall%Forder%3Ddescending%26page%3D5%26sort%3Dcreated_at", "PATH" => "/some/path")
+  it 'drops requests with invalid %-encoding in parameters' do
+    env = env_for('/some/path')
+    env['QUERY_STRING'] = 'q=%2Fsearch%2Fall%Forder%3Ddescending%26page%3D5%26sort%3Dcreated_at'
+    response = middleware.call(env)
+    expect(response).to eq [400, {}, ['Bad request']]
+  end
+
+  it 'drops requests with invalid encoding in parameters' do
+    env = env_for('/some/path')
+    env['QUERY_STRING'] = 'info_hash=f%e5u6%ac%5d%df%c8S%fc%9c7%b3%ff%26A%c3y%85%a3&peer_id=-TR2840-tgshmuspym9s&port=51413&uploaded=0&downloaded=0&left=11675&numwant=0&key=1a314ff6&compact=1&supportcrypto=1&event=stopped'
+    response = middleware.call(env)
+    expect(response).to eq [400, {}, ['Bad request']]
+  end
+
+  def env_for(url, opts = {})
+    Rack::MockRequest.env_for(url, opts)
   end
 end
