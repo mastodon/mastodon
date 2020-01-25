381eff54b73f895cbc038152a04361de92178175
Eugen Rochko <eugen@zeonfederated.com>
2020-01-25 14:44:35 +0100

    Fix validations of reactions limit

--- app/validators/reaction_validator.rb
+++ app/validators/reaction_validator.rb
@@ -6,16 +6,20 @@ class ReactionValidator < ActiveModel::Validator
   LIMIT = 8
 
   def validate(reaction)
-    return if reaction.name.blank? || reaction.custom_emoji_id.present?
+    return if reaction.name.blank?
 
-    reaction.errors.add(:name, I18n.t('reactions.errors.unrecognized_emoji')) unless unicode_emoji?(reaction.name)
-    reaction.errors.add(:base, I18n.t('reactions.errors.limit_reached')) if limit_reached?(reaction)
+    reaction.errors.add(:name, I18n.t('reactions.errors.unrecognized_emoji')) if reaction.custom_emoji_id.blank? && !unicode_emoji?(reaction.name)
+    reaction.errors.add(:base, I18n.t('reactions.errors.limit_reached')) if new_reaction?(reaction) && limit_reached?(reaction)
   end
 
   private
 
   def unicode_emoji?(name)
     SUPPORTED_EMOJIS.include?(name)
+  end
+
+  def new_reaction?(reaction)
+    !reaction.announcement.announcement_reactions.where(name: reaction.name).exists?
   end
 
   def limit_reached?(reaction)
