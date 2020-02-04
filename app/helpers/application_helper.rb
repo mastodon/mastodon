45a52205e4da2a04eb9e7eca5dca1d1c80498338
noellabo <noel.yoshiba@gmail.com>
2020-02-04 21:01:27 +0900

    Restrict to default privacy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -136,7 +136,10 @@ module ApplicationHelper
       text: [params[:title], params[:text], params[:url]].compact.join(' '),
     }
 
-    state_params[:visibility] = params[:visibility] if %w(public unlisted private direct).include? params[:visibility]
+    permit_visibilities = %w(public unlisted private direct)
+    default_privacy     = current_account&.user&.setting_default_privacy
+    permit_visibilities.shift(permit_visibilities.index(default_privacy) + 1) if default_privacy.present?
+    state_params[:visibility] = params[:visibility] if permit_visibilities.include? params[:visibility]
 
     if user_signed_in?
       state_params[:settings]          = state_params[:settings].merge(Web::Setting.find_by(user: current_user)&.data || {})
