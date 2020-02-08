b1349342d200937665ca6486c4b3ba1bae2f9d05
Eugen Rochko <eugen@zeonfederated.com>
2020-02-08 21:22:38 +0100

    Fix rendering `<a>` without `href` when scheme unsupported (#13040)
    - Disallow links with relative paths
    - Disallow iframes with non-http protocols and relative paths
    
    Close #13037

--- spec/lib/sanitize_config_spec.rb
+++ spec/lib/sanitize_config_spec.rb
@@ -26,5 +26,21 @@ describe Sanitize::Config do
     it 'keep links in lists' do
       expect(Sanitize.fragment('<p>Check out:</p><ul><li><a href="https://joinmastodon.org" rel="nofollow noopener noreferrer" target="_blank">joinmastodon.org</a></li><li>Bar</li></ul>', subject)).to eq '<p>Check out:</p><p><a href="https://joinmastodon.org" rel="nofollow noopener noreferrer" target="_blank">joinmastodon.org</a><br>Bar</p>'
     end
+
+    it 'removes a without href' do
+      expect(Sanitize.fragment('<a>Test</a>', subject)).to eq 'Test'
+    end
+
+    it 'removes a without href and only keeps text content' do
+      expect(Sanitize.fragment('<a><span class="invisible">foo&amp;</span><span>Test</span></a>', subject)).to eq 'foo&amp;Test'
+    end
+
+    it 'removes a with unsupported scheme in href' do
+      expect(Sanitize.fragment('<a href="foo://bar">Test</a>', subject)).to eq 'Test'
+    end
+
+    it 'keeps a with href' do
+      expect(Sanitize.fragment('<a href="http://example.com">Test</a>', subject)).to eq '<a href="http://example.com" rel="nofollow noopener noreferrer" target="_blank">Test</a>'
+    end
   end
 end
