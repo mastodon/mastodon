dec8d68d877704d1176ac1f847cf0a9e232cba8c
Thibaut Girka <thib@sitedethib.com>
2020-02-16 14:12:07 +0100

    Change config to raise errors on incorrect parameters instead of silently ignoring them

---
+++ config/initializers/parameters.rb
@@ -0,0 +1,6 @@
+# Be sure to restart your server when you modify this file.
+
+# This file configures ActionController::Parameters to raise an error when
+# passing incorrect parameters instead of silently ignoring them.
+
+ActionController::Parameters.action_on_unpermitted_parameters = :raise
