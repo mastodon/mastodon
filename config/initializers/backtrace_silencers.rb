d573802ce7a798ea3e5e37864075f8d29aebcea5
Thibaut Girka <thib@sitedethib.com>
2020-02-18 01:21:32 +0100

    Disable backtrace silencers

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -4,4 +4,4 @@
 # Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
-# Rails.backtrace_cleaner.remove_silencers!
+Rails.backtrace_cleaner.remove_silencers!
