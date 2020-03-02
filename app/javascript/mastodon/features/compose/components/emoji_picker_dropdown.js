b728034df1c282dd99ecd1eaef04eec78b1196d4
Darius Kazemi <darius.kazemi@gmail.com>
2020-03-02 11:07:39 -0800

    bookmark links

--- app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
+++ app/javascript/mastodon/features/compose/components/emoji_picker_dropdown.js
@@ -358,7 +358,7 @@ class EmojiPickerDropdown extends React.PureComponent {
     return (
       <div className='emoji-picker-dropdown' onKeyDown={this.handleKeyDown}>
         <div ref={this.setTargetRef} className='emoji-button' title={title} aria-label={title} aria-expanded={active} role='button' onClick={this.onToggle} onKeyDown={this.onToggle} tabIndex={0}>
-          {button || <img
+          <img
             className={classNames('emojione', { 'pulse-loading': active && loading })}
             alt='🙂'
             src={`${assetHost}/emoji/1f602.svg`}
