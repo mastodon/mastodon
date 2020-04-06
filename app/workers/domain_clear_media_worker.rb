ea8500581a8849ba4000f0f9b60bede3c8b9977d
Thibaut Girka <thib@sitedethib.com>
2020-04-06 16:27:44 +0200

    Lower DomainClearMediaWorker to lowest-priority queue

--- app/workers/domain_clear_media_worker.rb
+++ app/workers/domain_clear_media_worker.rb
@@ -3,6 +3,8 @@
 class DomainClearMediaWorker
   include Sidekiq::Worker
 
+  sidekiq_options queue: 'pull'
+
   def perform(domain_block_id)
     ClearDomainMediaService.new.call(DomainBlock.find(domain_block_id))
   rescue ActiveRecord::RecordNotFound
