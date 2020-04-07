71e534fdb3cdcbf9d03e91468b0171d5133b3a13
Eugen Rochko <eugen@zeonfederated.com>
2020-04-08 00:09:35 +0200

    Change delivery failure tracking to work with hostnames instead of URLs

--- app/workers/activitypub/delivery_worker.rb
+++ app/workers/activitypub/delivery_worker.rb
@@ -12,7 +12,7 @@ class ActivityPub::DeliveryWorker
   HEADERS = { 'Content-Type' => 'application/activity+json' }.freeze
 
   def perform(json, source_account_id, inbox_url, options = {})
-    return if DeliveryFailureTracker.unavailable?(inbox_url)
+    return unless DeliveryFailureTracker.available?(inbox_url)
 
     @options        = options.with_indifferent_access
     @json           = json
