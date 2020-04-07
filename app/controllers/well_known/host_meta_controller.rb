d65a8a61215f3e37ffae91ac960090878fae1c91
Shlee <github@shl.ee>
2020-04-07 17:32:00 +0800

    Update host_meta_controller.rb

--- app/controllers/well_known/host_meta_controller.rb
+++ app/controllers/well_known/host_meta_controller.rb
@@ -2,6 +2,8 @@
 
 module WellKnown
   class HostMetaController < ActionController::Base
+    protect_from_forgery with: :exception
+    
     include RoutingHelper
 
     before_action { response.headers['Vary'] = 'Accept' }
