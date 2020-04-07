5d87b553498097ce59a577b3ccdcd7479de895f8
Shlee <github@shl.ee>
2020-04-07 17:34:48 +0800

    Update keybase_proof_config_controller.rb

--- app/controllers/well_known/keybase_proof_config_controller.rb
+++ app/controllers/well_known/keybase_proof_config_controller.rb
@@ -3,7 +3,7 @@
 module WellKnown
   class KeybaseProofConfigController < ActionController::Base
     protect_from_forgery with: :exception
-    
+
     def show
       render json: {}, serializer: ProofProvider::Keybase::ConfigSerializer, root: 'keybase_config'
     end
