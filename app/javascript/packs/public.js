0e362b7678e75cb71ce207fd45dd4dc0d955fdca
ThibG <thib@sitedethib.com>
2020-04-28 10:16:55 +0200

    Fix end-user-facing uses of inline CSS (#13438)
    * Move some inline styles to CSS files
    
    * Move default_account_display_name span to fix useless tags with duplicate id
    
    * Change handling of public pages spoiler text from inline CSS to dataset attribute
    
    * Use the `dir` HTML attribute instead of inline CSS
    
    * Move status action bar inline CSS to CSS file
    
    * Hide logo resources from CSS file, not inline CSS
    
    Fixes #11601
    
    * Move translation prompt styling from inline CSS to CSS file
    
    * Move “invited by” styling on registration form from inline to CSS file
    
    * Use the progress tag to display poll results in JS fallback
    
    * Fix poll results JS-less fallback when the user has voted for an option
    
    * Change account public page “moved” notice to use img tags instead of inline CSS
    
    * Move OTP hint inline CSS to SCSS file
    
    * Hide JS-less fallback vote progressbars from accessibility tools
    
    Co-authored-by: Eugen Rochko <eugen@zeonfederated.com>

--- app/javascript/packs/public.js
+++ app/javascript/packs/public.js
@@ -120,15 +120,13 @@ function main() {
     delegate(document, '.custom-emoji', 'mouseout', getEmojiAnimationHandler('data-static'));
 
     delegate(document, '.status__content__spoiler-link', 'click', function() {
-      const contentEl = this.parentNode.parentNode.querySelector('.e-content');
+      const statusEl = this.parentNode.parentNode;
 
-      if (contentEl.style.display === 'block') {
-        contentEl.style.display = 'none';
-        this.parentNode.style.marginBottom = 0;
+      if (statusEl.dataset.spoiler === 'expanded') {
+        statusEl.dataset.spoiler = 'folded';
         this.textContent = (new IntlMessageFormat(messages['status.show_more'] || 'Show more', locale)).format();
       } else {
-        contentEl.style.display = 'block';
-        this.parentNode.style.marginBottom = null;
+        statusEl.dataset.spoiler = 'expanded';
         this.textContent = (new IntlMessageFormat(messages['status.show_less'] || 'Show less', locale)).format();
       }
 
@@ -136,8 +134,8 @@ function main() {
     });
 
     [].forEach.call(document.querySelectorAll('.status__content__spoiler-link'), (spoilerLink) => {
-      const contentEl = spoilerLink.parentNode.parentNode.querySelector('.e-content');
-      const message = (contentEl.style.display === 'block') ? (messages['status.show_less'] || 'Show less') : (messages['status.show_more'] || 'Show more');
+      const statusEl = spoilerLink.parentNode.parentNode;
+      const message = (statusEl.dataset.spoiler === 'expanded') ? (messages['status.show_less'] || 'Show less') : (messages['status.show_more'] || 'Show more');
       spoilerLink.textContent = (new IntlMessageFormat(message, locale)).format();
     });
   });
@@ -170,7 +168,7 @@ function main() {
       if (target.value) {
         name.innerHTML = emojify(escapeTextContentForBrowser(target.value));
       } else {
-        name.textContent = document.querySelector('#default_account_display_name').textContent;
+        name.textContent = target.dataset.default;
       }
     }
   });
