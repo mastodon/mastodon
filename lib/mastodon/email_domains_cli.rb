d8cf9e1f317bc266fcd84e8820d01922d151b02f
tateisu <tateisu@gmail.com>
2020-05-03 05:37:42 +0900

    fix codeclimate issues.

--- lib/mastodon/email_domains_cli.rb
+++ lib/mastodon/email_domains_cli.rb
@@ -18,47 +18,44 @@ module Mastodon
     long_desc <<-LONG_DESC
       Block E-mail domains from a given DOMAIN.
 
-      When the --with-dns-records option is given, An attempt to resolve the 
-      given domain's DNS records will be made and the results will also be 
+      When the --with-dns-records option is given, An attempt to resolve the
+      given domain's DNS records will be made and the results will also be
       blacklisted.
     LONG_DESC
     def block(*domains)
-
-      scope = begin
-
+      begin
         if domains.empty?
           say('No domain(s) given', :red)
           exit(1)
         end
 
         domains.each do |domain|
-          email_domain_block = EmailDomainBlock.new(domain:domain, with_dns_records: options[:with_dns_records] || false )
-          if email_domain_block.save
-            log_action :create, email_domain_block
-            say("Blocked domain #{domain}", :green)
+          email_domain_block = EmailDomainBlock.new(domain: domain, with_dns_records: options[:with_dns_records] || false)
+          email_domain_block.save!
 
-            if email_domain_block.with_dns_records?
-              hostnames = []
-              ips       = []
+          log_action :create, email_domain_block
+          say("Blocked domain #{domain}", :green)
 
-              Resolv::DNS.open do |dns|
-                dns.timeouts = 1
-                hostnames = dns.getresources(email_domain_block.domain, Resolv::DNS::Resource::IN::MX).to_a.map { |e| e.exchange.to_s }
+          next unless email_domain_block.with_dns_records?
 
-                ([email_domain_block.domain] + hostnames).uniq.each do |hostname|
-                  ips.concat(dns.getresources(hostname, Resolv::DNS::Resource::IN::A).to_a.map { |e| e.address.to_s })
-                  ips.concat(dns.getresources(hostname, Resolv::DNS::Resource::IN::AAAA).to_a.map { |e| e.address.to_s })
-                end
-              end
+          hostnames = []
+          ips       = []
 
-              (hostnames + ips).each do |hostname|
-                another_email_domain_block = EmailDomainBlock.new(domain: hostname, parent: email_domain_block)
-                if another_email_domain_block.save
-                  log_action :create, another_email_domain_block
-                  say("Blocked domain #{hostname}", :green)
-                end
-              end
+          Resolv::DNS.open do |dns|
+            dns.timeouts = 1
+            hostnames = dns.getresources(email_domain_block.domain, Resolv::DNS::Resource::IN::MX).to_a.map { |e| e.exchange.to_s }
+
+            ([email_domain_block.domain] + hostnames).uniq.each do |hostname|
+              ips.concat(dns.getresources(hostname, Resolv::DNS::Resource::IN::A).to_a.map { |e| e.address.to_s })
+              ips.concat(dns.getresources(hostname, Resolv::DNS::Resource::IN::AAAA).to_a.map { |e| e.address.to_s })
             end
+          end
+
+          (hostnames + ips).each do |hostname|
+            another_email_domain_block = EmailDomainBlock.new(domain: hostname, parent: email_domain_block)
+            another_email_domain_block.save!
+            log_action :create, another_email_domain_block
+            say("Blocked domain #{hostname}", :green)
           end
         end
       end
@@ -68,24 +65,19 @@ module Mastodon
     long_desc <<-LONG_DESC
       Unblock E-mail domains from a given DOMAIN.
     LONG_DESC
-    def block(*domains)
-      dry_run = options[:dry_run] ? ' (DRY RUN)' : ''
-
-      scope = begin
-
+    def unblock(*domains)
+      begin
         if domains.empty?
           say('No domain(s) given', :red)
           exit(1)
         end
 
         EmailDomainBlock.where(domain: domains).find_each do |entry|
-          if entry.destroy 
-            log_action :create, entry
-            say("Unblocked domain #{domain}", :green)
-          end
+          entry.destroy!
+          log_action :create, entry
+          say("Unblocked domain #{domain}", :green)
         end
       end
     end
-
   end
 end
