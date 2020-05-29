041c7691827357ceecb5c2d1ef47761b097be163
Thibaut Girka <thib@sitedethib.com>
2020-05-30 00:06:02 +0200

    Move glitch-specific ruby-side locales to their own files

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,3 +4,4 @@ I18n.load_path += Dir[Rails.root.join('app', 'javascript', 'flavours', '*', 'nam
 I18n.load_path += Dir[Rails.root.join('app', 'javascript', 'flavours', '*', 'names', '*.{rb,yml}').to_s]
 I18n.load_path += Dir[Rails.root.join('app', 'javascript', 'skins', '*', '*', 'names.{rb,yml}').to_s]
 I18n.load_path += Dir[Rails.root.join('app', 'javascript', 'skins', '*', '*', 'names', '*.{rb,yml}').to_s]
+I18n.load_path += Dir[Rails.root.join('config', 'locales-glitch', '*.{rb,yml}').to_s]
