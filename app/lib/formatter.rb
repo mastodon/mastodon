a97403ec9c82958d28e658784e6eea4a9609560b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-06-29 00:55:52 +0200

    Strip www only if it is not a tdr

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'public_suffix'
 require 'singleton'
 require_relative './sanitize_config'
 
@@ -286,12 +287,17 @@ class Formatter
   end
 
   def link_html(url)
-    url    = Addressable::URI.parse(url).to_s
-    prefix = url.match(/\A(https?:\/\/(www\.)?|xmpp:)/).to_s
-    text   = url[prefix.length, 30]
-    suffix = url[prefix.length + 30..-1]
-    cutoff = url[prefix.length..-1].length > 30
+    url    = Addressable::URI.parse(url)
+    prefix = url.to_s.match(/\A(https?:\/\/|xmpp:)/).to_s
+    # only show www if it is not a trd and not xmpp protocol cause public_suffix can't parse it
+    unless prefix == 'xmpp:'
+      subdomain = PublicSuffix.parse(url.domain).trd
+      prefix << 'www.' unless subdomain.nil? && subdomain == 'www'
+    end
 
+    text   = url.to_s[prefix.length, 30]
+    suffix = url.to_s[prefix.length + 30..-1]
+    cutoff = url.to_s[prefix.length..-1].length > 30
     "<span class=\"invisible\">#{encode(prefix)}</span><span class=\"#{cutoff ? 'ellipsis' : ''}\">#{encode(text)}</span><span class=\"invisible\">#{encode(suffix)}</span>"
   end
 
