da3978c48e620c0a6a658e81fecde49189cdf5f6
Eugen Rochko <eugen@zeonfederated.com>
2020-07-10 22:09:42 +0200

    Disable E2EE API routes for the next release (#14283)

--- config/routes.rb
+++ config/routes.rb
@@ -340,22 +340,22 @@ Rails.application.routes.draw do
         end
       end
 
-      namespace :crypto do
-        resources :deliveries, only: :create
+      # namespace :crypto do
+      #   resources :deliveries, only: :create
 
-        namespace :keys do
-          resource :upload, only: [:create]
-          resource :query,  only: [:create]
-          resource :claim,  only: [:create]
-          resource :count,  only: [:show]
-        end
+      #   namespace :keys do
+      #     resource :upload, only: [:create]
+      #     resource :query,  only: [:create]
+      #     resource :claim,  only: [:create]
+      #     resource :count,  only: [:show]
+      #   end
 
-        resources :encrypted_messages, only: [:index] do
-          collection do
-            post :clear
-          end
-        end
-      end
+      #   resources :encrypted_messages, only: [:index] do
+      #     collection do
+      #       post :clear
+      #     end
+      #   end
+      # end
 
       resources :conversations, only: [:index, :destroy] do
         member do
