32cb53dc71e366b08433286ae78faef195eef058
Thibaut Girka <thib@sitedethib.com>
2020-07-12 18:01:11 +0200

    Fix removing a DomainAllow rule wiping known accounts in open federation mode
    Fixes #14296

--- app/services/unallow_domain_service.rb
+++ app/services/unallow_domain_service.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class UnallowDomainService < BaseService
+  include DomainControlHelper
+
   def call(domain_allow)
-    Account.where(domain: domain_allow.domain).find_each do |account|
-      SuspendAccountService.new.call(account, reserve_username: false)
-    end
+    suspend_accounts!(domain_allow.domain) if whitelist_mode?
 
     domain_allow.destroy
+  end
+
+  private
+
+  def suspend_accounts!(domain)
+    Account.where(domain: domain).find_each do |account|
+      SuspendAccountService.new.call(account, reserve_username: false)
+    end
   end
 end
