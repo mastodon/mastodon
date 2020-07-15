de175722ff41bac81eadc9ba63faf9c7376b67ff
mashiro <moezhx@outlook.com>
2020-07-16 03:45:44 +0800

    fix status index to allow retoots and replies in local timeline

--- db/migrate/20190823221802_add_local_index_to_statuses.rb
+++ db/migrate/20190823221802_add_local_index_to_statuses.rb
@@ -2,7 +2,8 @@ class AddLocalIndexToStatuses < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def up
-    add_index :statuses, [:id, :account_id], name: :index_statuses_local_20190824, algorithm: :concurrently, order: { id: :desc }, where: '(local OR (uri IS NULL)) AND deleted_at IS NULL AND visibility = 0 AND reblog_of_id IS NULL AND ((NOT reply) OR (in_reply_to_account_id = account_id))'
+    #add_index :statuses, [:id, :account_id], name: :index_statuses_local_20190824, algorithm: :concurrently, order: { id: :desc }, where: '(local OR (uri IS NULL)) AND deleted_at IS NULL AND visibility = 0 AND reblog_of_id IS NULL AND ((NOT reply) OR (in_reply_to_account_id = account_id))'
+    add_index :statuses, [:id, :account_id], name: :index_statuses_local_20190824, algorithm: :concurrently, order: { id: :desc }, where: '(local OR (uri IS NULL)) AND deleted_at IS NULL AND visibility = 0'
   end
 
   def down
