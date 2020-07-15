ad4b4f73fea19632f3667b8df13ad8c69bb4c199
mashiro <moezhx@outlook.com>
2020-07-16 03:19:58 +0800

    format twitter usernames as links

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -265,8 +265,9 @@ class Formatter
 
   def link_to_mention(entity, linkable_accounts)
     acct = entity[:screen_name]
+    username, domain = acct.split('@')
 
-    return link_to_account(acct) unless linkable_accounts
+    return link_to_account(acct) unless linkable_accounts and domain != "twitter.com"
 
     account = linkable_accounts.find { |item| TagManager.instance.same_acct?(item.acct, acct) }
     account ? mention_html(account) : "@#{encode(acct)}"
@@ -274,6 +275,10 @@ class Formatter
 
   def link_to_account(acct)
     username, domain = acct.split('@')
+
+    if domain == "twitter.com"
+      return mention_twitter_html(username)
+    end
 
     domain  = nil if TagManager.instance.local_domain?(domain)
     account = EntityCache.instance.mention(username, domain)
@@ -301,5 +306,9 @@ class Formatter
 
   def mention_html(account)
     "<span class=\"h-card\"><a href=\"#{encode(ActivityPub::TagManager.instance.url_for(account))}\" class=\"u-url mention\">@<span>#{encode(account.username)}</span></a></span>"
+  end
+
+  def mention_twitter_html(username)
+    "<span class=\"h-card\"><a href=\"https://twitter.com/#{username}\" class=\"u-url mention\">@<span>#{username}@twitter.com</span></a></span>"
   end
 end
