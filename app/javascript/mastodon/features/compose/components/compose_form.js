8f6207be496a727fb3acdc535df49497213e14ff
mashiro <moezhx@outlook.com>
2020-07-16 19:13:47 +0800

    MAX_CHARS => 1024

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -21,7 +21,7 @@ import { length } from 'stringz';
 import { countableText } from '../util/counter';
 import Icon from 'mastodon/components/icon';
 
-const MAX_CHARS = 5000;
+const MAX_CHARS = 1024;
 
 const allowedAroundShortCode = '><\u0085\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029\u0009\u000a\u000b\u000c\u000d';
 
