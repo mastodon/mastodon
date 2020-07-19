1811986d0891eef4993c22ee907ac4c1f451a944
noellabo <noel.yoshiba@gmail.com>
2020-07-19 15:19:26 +0900

    Changed the number of retries and rescued exceptions in ActivityPub::ProcessingWorker

--- app/workers/activitypub/processing_worker.rb
+++ app/workers/activitypub/processing_worker.rb
@@ -3,11 +3,11 @@
 class ActivityPub::ProcessingWorker
   include Sidekiq::Worker
 
-  sidekiq_options backtrace: true
+  sidekiq_options backtrace: true, retry: 8
 
   def perform(account_id, body, delivered_to_account_id = nil)
     ActivityPub::ProcessCollectionService.new.call(body, Account.find(account_id), override_timestamps: true, delivered_to_account_id: delivered_to_account_id, delivery: true)
-  rescue ActiveRecord::RecordInvalid => e
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotUnique => e
     Rails.logger.debug "Error processing incoming ActivityPub object: #{e}"
   end
 end
