469c4c78a3ce2f7065c7273fd2800f9a39191a21
ThibG <thib@sitedethib.com>
2020-08-02 18:47:09 +0200

    Fix audio player on Safari (#14485)

--- app/javascript/mastodon/features/audio/index.js
+++ app/javascript/mastodon/features/audio/index.js
@@ -115,6 +115,10 @@ class Audio extends React.PureComponent {
   }
 
   togglePlay = () => {
+    if (!this.audioContext) {
+      this._initAudioContext();
+    }
+
     if (this.state.paused) {
       this.setState({ paused: false }, () => this.audio.play());
     } else {
@@ -132,10 +136,6 @@ class Audio extends React.PureComponent {
 
   handlePlay = () => {
     this.setState({ paused: false });
-
-    if (this.canvas && !this.audioContext) {
-      this._initAudioContext();
-    }
 
     if (this.audioContext && this.audioContext.state === 'suspended') {
       this.audioContext.resume();
