7dc4c742650ac69ec9a4459b656e172283511e4c
ThibG <thib@sitedethib.com>
2020-08-12 12:11:15 +0200

    Add client-side validation in password change forms (#14564)
    * Fix client-side username validation at registration
    
    It used the Account::USERNAME_RE regexp which is for *remote* users,
    local user validation is stricter. Also take into account max username length.
    
    * Add client-side form validation for password change
    
    * Add client-side form validation to dedicated registration form
    
    Previous changes only applied to the /about page, not the dedicated form on
    /auth

--- app/javascript/packs/public.js
+++ app/javascript/packs/public.js
@@ -126,6 +126,18 @@ function main() {
       }
     });
 
+    delegate(document, '#user_password,#user_password_confirmation', 'input', () => {
+      const password = document.getElementById('user_password');
+      const confirmation = document.getElementById('user_password_confirmation');
+      if (!confirmation) return;
+
+      if (password.value && password.value !== confirmation.value) {
+        confirmation.setCustomValidity((new IntlMessageFormat(messages['password_confirmation.mismatching'] || 'Password confirmation does not match', locale)).format());
+      } else {
+        confirmation.setCustomValidity('');
+      }
+    });
+
     delegate(document, '.custom-emoji', 'mouseover', getEmojiAnimationHandler('data-original'));
     delegate(document, '.custom-emoji', 'mouseout', getEmojiAnimationHandler('data-static'));
 
