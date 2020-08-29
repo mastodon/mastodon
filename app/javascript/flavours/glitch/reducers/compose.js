30632adf9eda6d83a9b4269f23f11ced5e09cd93
Thibaut Girka <thib@sitedethib.com>
2020-08-29 17:48:01 +0200

    Fix replying to some remote toots switching to local-only

--- app/javascript/flavours/glitch/reducers/compose.js
+++ app/javascript/flavours/glitch/reducers/compose.js
@@ -184,7 +184,7 @@ function continueThread (state, status) {
     map.set('in_reply_to', status.id);
     map.update(
       'advanced_options',
-      map => map.merge(new ImmutableMap({ do_not_federate: /👁\ufe0f?\u200b?(?:<\/p>)?$/.test(status.content) }))
+      map => map.merge(new ImmutableMap({ do_not_federate: status.get('local_only') }))
     );
     map.set('privacy', status.visibility);
     map.set('sensitive', false);
@@ -383,7 +383,7 @@ export default function compose(state = initialState, action) {
       map.set('privacy', privacyPreference(action.status.get('visibility'), state.get('default_privacy')));
       map.update(
         'advanced_options',
-        map => map.merge(new ImmutableMap({ do_not_federate: /👁\ufe0f?\u200b?(?:<\/p>)?$/.test(action.status.get('content')) }))
+        map => map.merge(new ImmutableMap({ do_not_federate: action.status.get('local_only') }))
       );
       map.set('focusDate', new Date());
       map.set('caretPosition', null);
