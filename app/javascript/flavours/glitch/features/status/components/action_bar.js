38182fceb31f9f9995dc5c6fe4c40eaca2831bc0
Thibaut Girka <thib@sitedethib.com>
2020-08-31 23:24:07 +0200

    Fix crash when opening detailed view of a status you boosted

--- app/javascript/flavours/glitch/features/status/components/action_bar.js
+++ app/javascript/flavours/glitch/features/status/components/action_bar.js
@@ -16,6 +16,7 @@ const messages = defineMessages({
   reply: { id: 'status.reply', defaultMessage: 'Reply' },
   reblog: { id: 'status.reblog', defaultMessage: 'Boost' },
   reblog_private: { id: 'status.reblog_private', defaultMessage: 'Boost with original visibility' },
+  cancel_reblog_private: { id: 'status.cancel_reblog_private', defaultMessage: 'Unboost' },
   cannot_reblog: { id: 'status.cannot_reblog', defaultMessage: 'This post cannot be boosted' },
   favourite: { id: 'status.favourite', defaultMessage: 'Favourite' },
   bookmark: { id: 'status.bookmark', defaultMessage: 'Bookmark' },
