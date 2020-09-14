102b0b145e6095948fccf8a6c817f1a5096e22bc
Thibaut Girka <thib@sitedethib.com>
2020-09-14 13:22:12 +0200

    Add paragraph about browser add-ons when encountering some errors
    When a crash is caused by a NotFoundError exception, add a paragraph
    to the error page mentioning browser add-ons.
    
    Indeed, crashes with NotFoundError are often caused by browser extensions
    messing with the DOM in ways React.JS can't recover from (e.g. issues #13325
    and #14731).

--- app/javascript/mastodon/components/error_boundary.js
+++ app/javascript/mastodon/components/error_boundary.js
@@ -66,16 +66,19 @@ export default class ErrorBoundary extends React.PureComponent {
   }
 
   render() {
-    const { hasError, copied } = this.state;
+    const { hasError, copied, errorMessage } = this.state;
 
     if (!hasError) {
       return this.props.children;
     }
 
+    const likelyBrowserAddonIssue = errorMessage && errorMessage.includes('NotFoundError');
+
     return (
       <div className='error-boundary'>
         <div>
           <p className='error-boundary__error'><FormattedMessage id='error.unexpected_crash.explanation' defaultMessage='Due to a bug in our code or a browser compatibility issue, this page could not be displayed correctly.' /></p>
+          { likelyBrowserAddonIssue && <p><FormattedMessage id='error.unexpected_crash.browser_addon' defaultMessage='This error is likely caused by a browser add-on or automatic translation tools, please try disabling them.' /></p> }
           <p><FormattedMessage id='error.unexpected_crash.next_steps' defaultMessage='Try refreshing the page. If that does not help, you may still be able to use Mastodon through a different browser or native app.' /></p>
           <p className='error-boundary__footer'>Mastodon v{version} · <a href={source_url} rel='noopener noreferrer' target='_blank'><FormattedMessage id='errors.unexpected_crash.report_issue' defaultMessage='Report issue' /></a> · <button onClick={this.handleCopyStackTrace} className={copied ? 'copied' : ''}><FormattedMessage id='errors.unexpected_crash.copy_stacktrace' defaultMessage='Copy stacktrace to clipboard' /></button></p>
         </div>
