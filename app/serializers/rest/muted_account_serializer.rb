8ff701078fa69180ba38f6170aba7891e8753c08
aquarla <satosamu@gmail.com>
2020-10-06 10:06:55 +0900

    Remove useless checks

--- app/serializers/rest/muted_account_serializer.rb
+++ app/serializers/rest/muted_account_serializer.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 class REST::MutedAccountSerializer < REST::AccountSerializer
-  attribute :mute_expires_at, if: :current_user?
-
-  def current_user?
-    defined?(current_user) && !current_user.nil?
-  end
+  attribute :mute_expires_at
 
   def mute_expires_at
-    mute = current_user.account&.mute_relationships&.find_by(target_account_id: object.id)
+    mute = current_user.account.mute_relationships.find_by(target_account_id: object.id)
     mute && !mute.expired? ? mute.expires_at : nil
   end
 end
