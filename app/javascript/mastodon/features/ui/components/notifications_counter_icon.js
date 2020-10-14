288132c65ef6b22bd5b2d1dc8ddc9aa0005d07dc
Eugen Rochko <eugen@zeonfederated.com>
2020-10-14 20:54:20 +0200

    Change how missing desktop notifications permission is displayed
    Add missing controls for new notification type

--- app/javascript/mastodon/features/ui/components/notifications_counter_icon.js
+++ app/javascript/mastodon/features/ui/components/notifications_counter_icon.js
@@ -3,7 +3,6 @@ import IconWithBadge from 'mastodon/components/icon_with_badge';
 
 const mapStateToProps = state => ({
   count: state.getIn(['notifications', 'unread']),
-  issueBadge: state.getIn(['settings', 'notifications', 'alerts']).includes(true) && state.getIn(['notifications', 'browserSupport']) && state.getIn(['notifications', 'browserPermission']) !== 'granted',
   id: 'bell',
 });
 
--- app/javascript/mastodon/reducers/settings.js
+++ app/javascript/mastodon/reducers/settings.js
@@ -35,6 +35,7 @@ const initialState = ImmutableMap({
       reblog: false,
       mention: false,
       poll: false,
+      status: false,
     }),
 
     quickFilter: ImmutableMap({
@@ -50,6 +51,7 @@ const initialState = ImmutableMap({
       reblog: true,
       mention: true,
       poll: true,
+      status: true,
     }),
 
     sounds: ImmutableMap({
@@ -59,6 +61,7 @@ const initialState = ImmutableMap({
       reblog: true,
       mention: true,
       poll: true,
+      status: true,
     }),
   }),
 
