ca567c78716651eef3c1314fcd670280ff9e2d7a
Thibaut Girka <thib@sitedethib.com>
2020-10-30 12:30:49 +0100

    Relax Host parameter requirement to GET requests
    The only POST requests processed by Mastodon need objects/actors (including
    their host) to be explicitly mentioned in the request's body, so replaying
    a legitimate request to another host should not be a security issue.

--- app/controllers/concerns/signature_verification.rb
+++ app/controllers/concerns/signature_verification.rb
@@ -127,7 +127,7 @@ module SignatureVerification
   def verify_signature_strength!
     raise SignatureVerificationError, 'Mastodon requires the Date header or (created) pseudo-header to be signed' unless signed_headers.include?('date') || signed_headers.include?('(created)')
     raise SignatureVerificationError, 'Mastodon requires the Digest header or (request-target) pseudo-header to be signed' unless signed_headers.include?(Request::REQUEST_TARGET) || signed_headers.include?('digest')
-    raise SignatureVerificationError, 'Mastodon requires the Host header to be signed' unless signed_headers.include?('host')
+    raise SignatureVerificationError, 'Mastodon requires the Host header to be signed when doing a GET request' if request.get? && !signed_headers.include?('host')
     raise SignatureVerificationError, 'Mastodon requires the Digest header to be signed when doing a POST request' if request.post? && !signed_headers.include?('digest')
   end
 
