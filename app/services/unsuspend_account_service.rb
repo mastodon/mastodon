ee8cf246cfe8e05914ad7dcf81596f8535b3e161
ThibG <thib@sitedethib.com>
2020-11-07 13:16:54 +0100

    Fix crashes in SuspendAccountService/UnsuspendAccountService (#15100)
    * Fix crashes in SuspendAccountService/UnsuspendAccountService
    
    * Catch filesystem errors

--- app/services/unsuspend_account_service.rb
+++ app/services/unsuspend_account_service.rb
@@ -39,11 +39,15 @@ class UnsuspendAccountService < BaseService
         styles.each do |style|
           case Paperclip::Attachment.default_options[:storage]
           when :s3
-            attachment.s3_object(style).acl.put(Paperclip::Attachment.default_options[:s3_permissions])
+            attachment.s3_object(style).acl.put(acl: Paperclip::Attachment.default_options[:s3_permissions])
           when :fog
             # Not supported
           when :filesystem
-            FileUtils.chmod(0o666 & ~File.umask, attachment.path(style))
+            begin
+              FileUtils.chmod(0o666 & ~File.umask, attachment.path(style)) unless attachment.path(style).nil?
+            rescue Errno::ENOENT
+              Rails.logger.warn "Tried to change permission on non-existent file #{attachment.path(style)}"
+            end
           end
         end
       end
