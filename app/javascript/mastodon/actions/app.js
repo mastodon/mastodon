18ca4e0e9a3f74a6f21d329882b429f8f5227b0f
Eugen Rochko <eugen@zeonfederated.com>
2020-11-16 05:16:39 +0100

    Fix pop-out player appearing on mobile screens in web UI (#15157)
    Fix #15092

--- app/javascript/mastodon/actions/app.js
+++ app/javascript/mastodon/actions/app.js
@@ -8,3 +8,10 @@ export const focusApp = () => ({
 export const unfocusApp = () => ({
   type: APP_UNFOCUS,
 });
+
+export const APP_LAYOUT_CHANGE = 'APP_LAYOUT_CHANGE';
+
+export const changeLayout = layout => ({
+  type: APP_LAYOUT_CHANGE,
+  layout,
+});
