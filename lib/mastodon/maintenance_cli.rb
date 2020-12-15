3b699f309d1d18b0fcbea5d148038d057201f5b6
Claire <claire.github-309c@sitedethib.com>
2020-12-15 11:34:46 +0100

    Bump max supported schema version in maintenance script

--- lib/mastodon/maintenance_cli.rb
+++ lib/mastodon/maintenance_cli.rb
@@ -14,7 +14,7 @@ module Mastodon
     end
 
     MIN_SUPPORTED_VERSION = 2019_10_01_213028
-    MAX_SUPPORTED_VERSION = 2020_10_17_234926
+    MAX_SUPPORTED_VERSION = 2020_12_06_004238
 
     # Stubs to enjoy ActiveRecord queries while not depending on a particular
     # version of the code/database
