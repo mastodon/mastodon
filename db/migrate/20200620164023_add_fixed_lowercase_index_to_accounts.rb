0d42454086e2367a9dce0889c577eed716e70dd2
Claire <claire.github-309c@sitedethib.com>
2020-12-18 14:10:43 +0100

    Display a specific error message in case of index corruption

--- db/migrate/20200620164023_add_fixed_lowercase_index_to_accounts.rb
+++ db/migrate/20200620164023_add_fixed_lowercase_index_to_accounts.rb
@@ -1,6 +1,16 @@
 class AddFixedLowercaseIndexToAccounts < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
+  class CorruptionError < StandardError
+    def cause
+      nil
+    end
+
+    def backtrace
+      []
+    end
+  end
+
   def up
     if index_name_exists?(:accounts, 'old_index_accounts_on_username_and_domain_lower') && index_name_exists?(:accounts, 'index_accounts_on_username_and_domain_lower')
       remove_index :accounts, name: 'index_accounts_on_username_and_domain_lower'
@@ -8,7 +18,12 @@ class AddFixedLowercaseIndexToAccounts < ActiveRecord::Migration[5.2]
       rename_index :accounts, 'index_accounts_on_username_and_domain_lower', 'old_index_accounts_on_username_and_domain_lower'
     end
 
-    add_index :accounts, "lower (username), COALESCE(lower(domain), '')", name: 'index_accounts_on_username_and_domain_lower', unique: true, algorithm: :concurrently
+    begin
+      add_index :accounts, "lower (username), COALESCE(lower(domain), '')", name: 'index_accounts_on_username_and_domain_lower', unique: true, algorithm: :concurrently
+    rescue ActiveRecord::RecordNotUnique
+      raise CorruptionError, 'Migration failed because of index corruption, see https://docs.joinmastodon.org/admin/troubleshooting/index-corruption/#fixing'
+    end
+
     remove_index :accounts, name: 'old_index_accounts_on_username_and_domain_lower' if index_name_exists?(:accounts, 'old_index_accounts_on_username_and_domain_lower')
   end
 
