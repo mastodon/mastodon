ef2621fe90ccf9f1e5f8761ad983d97b1f69e69c
Claire <claire.github-309c@sitedethib.com>
2020-12-21 10:26:29 +0100

    Add DeleteAccountService tests for other associations and notifications

--- spec/services/delete_account_service_spec.rb
+++ spec/services/delete_account_service_spec.rb
@@ -3,21 +3,29 @@ require 'rails_helper'
 RSpec.describe DeleteAccountService, type: :service do
   shared_examples 'common behavior' do
     let!(:status) { Fabricate(:status, account: account) }
+    let!(:mention) { Fabricate(:mention, account: local_follower) }
+    let!(:status_with_mention) { Fabricate(:status, account: account, mentions: [mention]) }
     let!(:media_attachment) { Fabricate(:media_attachment, account: account) }
     let!(:notification) { Fabricate(:notification, account: account) }
-    let!(:favourite) { Fabricate(:favourite, account: account) }
+    let!(:favourite) { Fabricate(:favourite, account: account, status: Fabricate(:status, account: local_follower)) }
+    let!(:poll) { Fabricate(:poll, account: account) }
+    let!(:poll_vote) { Fabricate(:poll_vote, account: local_follower, poll: poll) }
 
-    before do
-      account.follow!(local_follower)
-      local_follower.follow!(account)
-      AccountPin.create!(account: local_follower, target_account: account)
-    end
+    let!(:active_relationship) { Fabricate(:follow, account: account, target_account: local_follower) }
+    let!(:passive_relationship) { Fabricate(:follow, account: local_follower, target_account: account) }
+    let!(:endorsement) { Fabricate(:account_pin, account: local_follower, target_account: account) }
+
+    let!(:mention_notification) { Fabricate(:notification, account: local_follower, activity: mention, type: :mention) }
+    let!(:status_notification) { Fabricate(:notification, account: local_follower, activity: status, type: :status) }
+    let!(:poll_notification) { Fabricate(:notification, account: local_follower, activity: poll, type: :poll) }
+    let!(:favourite_notification) { Fabricate(:notification, account: local_follower, activity: favourite, type: :favourite) }
+    let!(:follow_notification) { Fabricate(:notification, account: local_follower, activity: active_relationship, type: :follow) }
 
     subject do
       -> { described_class.new.call(account) }
     end
 
-    it 'deletes associated records' do
+    it 'deletes associated owned records' do
       is_expected.to change {
         [
           account.statuses,
@@ -26,9 +34,25 @@ RSpec.describe DeleteAccountService, type: :service do
           account.favourites,
           account.active_relationships,
           account.passive_relationships,
+          account.polls,
+        ].map(&:count)
+      }.from([2, 1, 1, 1, 1, 1, 1]).to([0, 0, 0, 0, 0, 0, 0])
+    end
+
+    it 'deletes associated target records' do
+      is_expected.to change {
+        [
           AccountPin.where(target_account: account),
         ].map(&:count)
-      }.from([1, 1, 1, 1, 1, 1, 1]).to([0, 0, 0, 0, 0, 0, 0])
+      }.from([1]).to([0])
+    end
+
+    it 'deletes associated target notifications' do
+      is_expected.to change {
+        [
+          'poll', 'favourite', 'status', 'mention', 'follow'
+        ].map { |type| Notification.where(type: type).count }
+      }.from([1, 1, 1, 1, 1]).to([0, 0, 0, 0, 0])
     end
   end
 
