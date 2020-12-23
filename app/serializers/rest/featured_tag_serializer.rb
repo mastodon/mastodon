8a8e643f8f385b77fd8f62f211b04283c52789a7
trwnh <a@trwnh.com>
2020-12-23 03:29:56 -0600

    please codeclimate

--- app/serializers/rest/featured_tag_serializer.rb
+++ app/serializers/rest/featured_tag_serializer.rb
@@ -2,14 +2,14 @@
 
 class REST::FeaturedTagSerializer < ActiveModel::Serializer
   include RoutingHelper
-  
+
   attributes :id, :name, :url, :statuses_count, :last_status_at
-  
+
 
   def id
     object.id.to_s
   end
-  
+
   def url
     short_account_tag_url(object.account, object.tag)
   end
