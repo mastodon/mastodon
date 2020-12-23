865fb240e5e4a47d8e351ff3173b817ee045e501
trwnh <a@trwnh.com>
2020-12-23 03:26:42 -0600

    Update featured_tag_serializer.rb

--- app/serializers/rest/featured_tag_serializer.rb
+++ app/serializers/rest/featured_tag_serializer.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class REST::FeaturedTagSerializer < ActiveModel::Serializer
+  include RoutingHelper
+  
   attributes :id, :name, :url, :statuses_count, :last_status_at
+  
 
   def id
     object.id.to_s
