e7510155dccc63bffd436cbb6de5a515555a8c71
mashirozx <moezhx@outlook.com>
2021-01-04 21:43:08 +0800

    fix: support remote code block highlight (rouge)

--- lib/sanitize_ext/sanitize_config.rb
+++ lib/sanitize_ext/sanitize_config.rb
@@ -32,6 +32,7 @@ class Sanitize
         next true if /^(mention|hashtag)$/.match?(e) # semantic classes
         next true if /^(ellipsis|invisible)$/.match?(e) # link formatting classes
         next true if /^quote-inline$/.match?(e) # quote inline classes
+        next true if /(ge|gs|o|ow|c|cm|cp|c1|cs|n|py|nl|ni|nb|bp|nf|na|nx|nc|nn|no|ne|nd|nt|kn|k|kc|kd|kp|kr|kt|nv|vg|vc|vi|m|il|mf|mh|mi|mo|s|sr|s2|sb|sh|sx|s1|ss|se|si|sc|sd|l|ld|p|w|err|gp|gi|gd|gh|gu|hll|lineno|highlight)/.match?(e) # ruby-rouge classes
       end
 
       node['class'] = class_list.join(' ')
@@ -84,6 +85,9 @@ class Sanitize
           'rel' => 'nofollow noopener noreferrer',
           'target' => '_blank',
         },
+        'pre' => {
+          'class' => 'highlight'
+        }
       },
 
       protocols: {},
