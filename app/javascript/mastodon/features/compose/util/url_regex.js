5828ce456111731a5ec4a5814d183337c0067245
Claire <claire.github-309c@sitedethib.com>
2021-01-11 18:03:21 +0100

    Fix some URLs being wrongly detected client-side

--- app/javascript/mastodon/features/compose/util/url_regex.js
+++ app/javascript/mastodon/features/compose/util/url_regex.js
@@ -1,24 +1,30 @@
 import regexSupplant from 'twitter-text/dist/lib/regexSupplant';
+import validUrlPrecedingChars from 'twitter-text/dist/regexp/validUrlPrecedingChars';
 import validDomain from 'twitter-text/dist/regexp/validDomain';
 import validPortNumber from 'twitter-text/dist/regexp/validPortNumber';
 import validUrlPath from 'twitter-text/dist/regexp/validUrlPath';
 import validUrlQueryChars from 'twitter-text/dist/regexp/validUrlQueryChars';
 import validUrlQueryEndingChars from 'twitter-text/dist/regexp/validUrlQueryEndingChars';
 
+// The difference with twitter-text's extractURL is that the protocol isn't
+// optional.
+
 export const urlRegex = regexSupplant(
   '('                                                          + // $1 URL
-    '(https?:\\/\\/)'                                          + // $2 Protocol
-    '(#{validDomain})'                                         + // $3 Domain(s)
-    '(?::(#{validPortNumber}))?'                               + // $4 Port number (optional)
-    '(\\/#{validUrlPath}*)?'                                   + // $5 URL Path
-    '(\\?#{validUrlQueryChars}*#{validUrlQueryEndingChars})?'  + // $6 Query String
+    '(#{validUrlPrecedingChars})'                              + // $2
+    '(https?:\\/\\/)'                                          + // $3 Protocol
+    '(#{validDomain})'                                         + // $4 Domain(s)
+    '(?::(#{validPortNumber}))?'                               + // $5 Port number (optional)
+    '(\\/#{validUrlPath}*)?'                                   + // $6 URL Path
+    '(\\?#{validUrlQueryChars}*#{validUrlQueryEndingChars})?'  + // $7 Query String
   ')',
   {
+    validUrlPrecedingChars,
     validDomain,
     validPortNumber,
     validUrlPath,
     validUrlQueryChars,
-    validUrlQueryEndingChars
+    validUrlQueryEndingChars,
   },
-  'gi'
+  'gi',
 );
