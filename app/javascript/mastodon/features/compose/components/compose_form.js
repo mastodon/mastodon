184cdc65f53613cefeb73bfc2cca11869033a824
susuonep <polatrakib4@gmail.com>
2021-01-23 15:25:36 +0800

    嘟文字数限制改为5000

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -86,7 +86,7 @@ class ComposeForm extends ImmutablePureComponent {
     const fulltext = this.getFulltextForCharacterCounting();
     const isOnlyWhitespace = fulltext.length !== 0 && fulltext.trim().length === 0;
 
-    return !(isSubmitting || isUploading || isChangingUpload || length(fulltext) > 500 || (isOnlyWhitespace && !anyMedia));
+    return !(isSubmitting || isUploading || isChangingUpload || length(fulltext) > 5000 || (isOnlyWhitespace && !anyMedia));
   }
 
   handleSubmit = () => {
@@ -249,7 +249,7 @@ class ComposeForm extends ImmutablePureComponent {
             <PrivacyDropdownContainer />
             <SpoilerButtonContainer />
           </div>
-          <div className='character-counter__wrapper'><CharacterCounter max={500} text={this.getFulltextForCharacterCounting()} /></div>
+          <div className='character-counter__wrapper'><CharacterCounter max={5000} text={this.getFulltextForCharacterCounting()} /></div>
         </div>
 
         <div className='compose-form__publish'>
--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -5,7 +5,7 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   attributes :uri, :title, :short_description, :description, :email,
              :version, :urls, :stats, :thumbnail,
-             :languages, :registrations, :approval_required, :invites_enabled
+             :languages, :registrations, :approval_required, :max_toot_chars, :invites_enabled
 
   has_one :contact_account, serializer: REST::AccountSerializer
 
@@ -63,6 +63,10 @@ class REST::InstanceSerializer < ActiveModel::Serializer
     Setting.registrations_mode == 'approved'
   end
 
+  def max_toot_chars
+    5000
+  end
+  
   def invites_enabled
     Setting.min_invite_role == 'user'
   end
