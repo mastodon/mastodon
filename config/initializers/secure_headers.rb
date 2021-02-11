5e54804882a92ed275c06f80b625f1f2d5975fb2
Aiden McClelland <me@drbonez.dev>
2021-02-10 20:03:08 -0700

    apply @cohosh's changes

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,10 @@
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+    secure: true,
+    httponly: true,
+    samesite: {
+      lax: true
+    }
+  }
+  config.csp = SecureHeaders::OPT_OUT
+end
