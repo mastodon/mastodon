81204673c5f622f77c5b5e6e1c5578a2a85b270e
Eugen Rochko <eugen@zeonfederated.com>
2021-03-15 07:57:03 +0100

    Fix configuration for sidekiq-unique-jobs after 7.x upgrade
    Remove locks from scheduled jobs

--- app/workers/scheduler/doorkeeper_cleanup_scheduler.rb
+++ app/workers/scheduler/doorkeeper_cleanup_scheduler.rb
@@ -3,7 +3,7 @@
 class Scheduler::DoorkeeperCleanupScheduler
   include Sidekiq::Worker
 
-  sidekiq_options lock: :until_executed, retry: 0
+  sidekiq_options retry: 0
 
   def perform
     Doorkeeper::AccessToken.where('revoked_at IS NOT NULL').where('revoked_at < NOW()').delete_all
