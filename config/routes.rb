9f3f5391aebda81fc2a6713b8bc07af5e71dbbf5
Eugen Rochko <eugen@zeonfederated.com>
2021-03-30 00:46:23 +0200

    Change health check

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: 'letter_opener' if Rails.env.development?
 
-  health_check_routes
+  get 'health', to: 'health#show'
 
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web, at: 'sidekiq', as: :sidekiq
