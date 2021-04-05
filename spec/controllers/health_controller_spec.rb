5c225b03db280c94dc3519dc5bad2cac86487e9b
Claire <claire.github-309c@sitedethib.com>
2021-04-05 14:59:05 +0200

    Fix glitch-soc-specific health check spec

--- spec/controllers/health_controller_spec.rb
+++ spec/controllers/health_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-describe HealthCheck::HealthCheckController do
+describe HealthController do
   render_views
 
   describe 'GET #show' do
-    subject(:response) { get :index, params: { format: :json } }
+    subject(:response) { get :show, params: { format: :json } }
 
     it 'returns the right response' do
       expect(response).to have_http_status 200
