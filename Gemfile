48d13a9e24399d76d43902903a03eec8e918ed5e
Mashiro <moezhx@outlook.com>
2021-04-08 00:12:08 +0800

    fixup: change rucaptcha redis cache storage

--- Gemfile
+++ Gemfile
@@ -155,6 +155,6 @@ end
 gem 'concurrent-ruby', require: false
 gem 'connection_pool', require: false
 
-gem 'rucaptcha', '~> 2.5.3'
-gem "sidekiq-statistic", "~> 1.4"
+gem 'rucaptcha', git: 'https://github.com/huacnlee/rucaptcha', ref: '3e8361a06521c45811d1487cceb41dc524ab9b69', branch: 'main'
+gem "sidekiq-statistic"
 gem 'xorcist', '~> 1.1'
--- config/initializers/rucaptcha.rb
+++ config/initializers/rucaptcha.rb
@@ -3,7 +3,8 @@ RuCaptcha.configure do
   # self.style = :colorful
   # Custom captcha code expire time if you need, default: 2 minutes
   # self.expires_in = 120
-  self.cache_store = :redis_store, ENV['CACHE_REDIS_URL'], REDIS_CACHE_PARAMS
+  # self.cache_store = :redis_cache_store, REDIS_CACHE_PARAMS
+  self.cache_store = Rails.application.config.cache_store
   # self.skip_cache_store_check = true
   # Chars length, default: 5, allows: [3 - 7]
   # self.length = 5
