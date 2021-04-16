651e6212228cbf81d5d84f7e51037d238048e8f2
Claire <claire.github-309c@sitedethib.com>
2021-04-16 20:36:58 +0200

    Change validation_extensions to please CodeClimate

--- lib/paperclip/validation_extensions.rb
+++ lib/paperclip/validation_extensions.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 # Monkey-patch various Paperclip validators for Ruby 3.0 compatibility
 
 module Paperclip
   module Validators
     module AttachmentSizeValidatorExtensions
-      def validate_each(record, attr_name, value)
+      def validate_each(record, attr_name, _value)
         base_attr_name = attr_name
         attr_name = "#{attr_name}_file_size".to_sym
         value = record.send(:read_attribute_for_validation, attr_name)
 
-        unless value.blank?
+        if value.present?
           options.slice(*Paperclip::Validators::AttachmentSizeValidator::AVAILABLE_CHECKS).each do |option, option_value|
             option_value = option_value.call(record) if option_value.is_a?(Proc)
             option_value = extract_option_value(option, option_value)
 
-            unless value.send(Paperclip::Validators::AttachmentSizeValidator::CHECKS[option], option_value)
-              error_message_key = options[:in] ? :in_between : option
-              [ attr_name, base_attr_name ].each do |error_attr_name|
-                record.errors.add(error_attr_name, error_message_key, **filtered_options(value).merge(
-                  :min => min_value_in_human_size(record),
-                  :max => max_value_in_human_size(record),
-                  :count => human_size(option_value)
-                ))
-              end
+            next if value.send(Paperclip::Validators::AttachmentSizeValidator::CHECKS[option], option_value)
+
+            error_message_key = options[:in] ? :in_between : option
+            [attr_name, base_attr_name].each do |error_attr_name|
+              record.errors.add(error_attr_name, error_message_key, **filtered_options(value).merge(
+                min: min_value_in_human_size(record),
+                max: max_value_in_human_size(record),
+                count: human_size(option_value)
+              ))
             end
           end
         end
@@ -30,12 +32,12 @@ module Paperclip
 
     module AttachmentContentTypeValidatorExtensions
       def mark_invalid(record, attribute, types)
-        record.errors.add attribute, :invalid, **options.merge(:types => types.join(', '))
+        record.errors.add attribute, :invalid, **options.merge({ types: types.join(', ') })
       end
     end
 
     module AttachmentPresenceValidatorExtensions
-      def validate_each(record, attribute, value)
+      def validate_each(record, attribute, _value)
         if record.send("#{attribute}_file_name").blank?
           record.errors.add(attribute, :blank, **options)
         end
@@ -44,7 +46,7 @@ module Paperclip
 
     module AttachmentFileNameValidatorExtensions
       def mark_invalid(record, attribute, patterns)
-        record.errors.add attribute, :invalid, options.merge(:names => patterns.join(', '))
+        record.errors.add attribute, :invalid, options.merge({ names: patterns.join(', ') })
       end
     end
   end
