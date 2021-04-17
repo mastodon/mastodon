10b2ebf327ed1b20dad20ab39bf1e2651a882380
noellabo <noel.yoshiba@gmail.com>
2021-04-18 04:05:07 +0900

    Fix style

--- app/lib/activitypub/activity/announce.rb
+++ app/lib/activitypub/activity/announce.rb
@@ -49,7 +49,7 @@ class ActivityPub::Activity::Announce < ActivityPub::Activity
         uri: @json['id'],
         created_at: @json['published'],
         override_timestamps: @options[:override_timestamps],
-        visibility: visibility_from_audience
+        visibility: visibility_from_audience,
       }
     end
   end
