8f385fc24edf8fb947c0975f482f32634fb0c755
fusagiko / takayamaki <takayamaki@users.noreply.github.com>
2021-05-03 16:29:11 +0900

    画像アップロードのサイズ制限を緩和

--- app/javascript/mastodon/utils/resize_image.js
+++ app/javascript/mastodon/utils/resize_image.js
@@ -1,6 +1,6 @@
 import EXIF from 'exif-js';
 
-const MAX_IMAGE_PIXELS = 1638400; // 1280x1280px
+const MAX_IMAGE_PIXELS = 4147200; // 2880x1440px
 
 const _browser_quirks = {};
 
--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -47,7 +47,7 @@ class MediaAttachment < ApplicationRecord
 
   IMAGE_STYLES = {
     original: {
-      pixels: 1_638_400, # 1280x1280px
+      pixels: 4_147_200, # 2880x1440px
       file_geometry_parser: FastGeometryParser,
     },
 
