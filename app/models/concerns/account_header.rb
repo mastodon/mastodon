43a0cfab767c0c27f95f48e0c53aab46ac53da18
noellabo <noel.yoshiba@gmail.com>
2021-05-09 20:11:10 +0900

    Add RedownloadAvatarWorker and RedownloadHeaderWorker

--- app/models/concerns/account_header.rb
+++ app/models/concerns/account_header.rb
@@ -22,7 +22,7 @@ module AccountHeader
     has_attached_file :header, styles: ->(f) { header_styles(f) }, convert_options: { all: '-strip' }, processors: [:lazy_thumbnail]
     validates_attachment_content_type :header, content_type: IMAGE_MIME_TYPES
     validates_attachment_size :header, less_than: LIMIT
-    remotable_attachment :header, LIMIT
+    remotable_attachment :header, LIMIT, suppress_errors: false
   end
 
   def header_original_url
