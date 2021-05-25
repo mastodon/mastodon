382904e00c6dc5efb2925acd93144ffb2d9d5a1e
Claire <claire.github-309c@sitedethib.com>
2021-05-25 14:00:34 +0200

    Fix migration script not being able to run if it fails midway

--- db/migrate/20210421121431_add_case_insensitive_btree_index_to_tags.rb
+++ db/migrate/20210421121431_add_case_insensitive_btree_index_to_tags.rb
@@ -1,8 +1,25 @@
 class AddCaseInsensitiveBtreeIndexToTags < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
+  class CorruptionError < StandardError
+    def cause
+      nil
+    end
+
+    def backtrace
+      []
+    end
+  end
+
   def up
-    safety_assured { execute 'CREATE UNIQUE INDEX CONCURRENTLY index_tags_on_name_lower_btree ON tags (lower(name) text_pattern_ops)' }
+    begin
+      safety_assured { execute 'CREATE UNIQUE INDEX CONCURRENTLY index_tags_on_name_lower_btree ON tags (lower(name) text_pattern_ops)' }
+    rescue ActiveRecord::StatementInvalid => e
+      remove_index :tags, name: 'index_tags_on_name_lower_btree'
+      e = CorruptionError.new('Migration failed because of index corruption, see https://docs.joinmastodon.org/admin/troubleshooting/index-corruption/#fixing') if e.is_a?(ActiveRecord::RecordNotUnique)
+      raise e
+    end
+
     remove_index :tags, name: 'index_tags_on_name_lower'
   end
 
