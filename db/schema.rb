0479e2393285829e65fa96357c39dc6f3353c7b0
Claire <claire.github-309c@sitedethib.com>
2021-05-26 19:32:46 +0200

    Fix account deletion sometimes failing because of optimistic locks
    In some rare occasions[1], deleting accounts would fail with a
    `StaleObjectError` exception.
    
    Indeed, account deletion manually sets the `AccountStat` values without
    handling cases where the optimistic locking on `AccountStat` would fail.
    
    To my knowledge, with the rewrite of account counters in #15913, the
    `DeleteAccountService` is now the only place that changes the counters in
    a way that is not atomic.
    
    Since in this specific case, we do not care about the previous values of the
    account counters, it appears we don't need locking at all for this table
    anymore.
    
    [1]: https://discourse.joinmastodon.org/t/account-cant-be-deleted/3602

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_001928) do
+ActiveRecord::Schema.define(version: 2021_05_26_193025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(version: 2021_05_07_001928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "last_status_at"
-    t.integer "lock_version", default: 0, null: false
     t.index ["account_id"], name: "index_account_stats_on_account_id", unique: true
   end
 
