73dec2d118cde1ccca435bf3c6cca708575404b7
Jeong Arm <kjwonmail@gmail.com>
2021-05-29 13:48:14 +0900

    Apply requested changes

--- db/post_migrate/20210524114545_remove_ghost_statuses.rb
+++ db/post_migrate/20210524114545_remove_ghost_statuses.rb
@@ -1,5 +1,5 @@
 class RemoveGhostStatuses < ActiveRecord::Migration[6.1]
   def change
-    Status.where(uri: Tombstone.select(:uri)).destroy_all
+    Status.where(uri: Tombstone.select(:uri)).delete_all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_001928) do
+ActiveRecord::Schema.define(version: 2021_05_24_114545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
