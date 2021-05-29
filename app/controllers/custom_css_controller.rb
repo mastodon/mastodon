aedc39b8a0b22af3e64aa7bbb50d87a0b3412d90
Jeong Arm <kjwonmail@gmail.com>
2021-05-29 13:39:40 +0900

    Additional fix for set-cookie

--- app/controllers/custom_css_controller.rb
+++ app/controllers/custom_css_controller.rb
@@ -3,6 +3,10 @@
 class CustomCssController < ApplicationController
   skip_before_action :store_current_location
   skip_before_action :require_functional!
+  skip_before_action :update_user_sign_in
+  skip_before_action :set_session_activity
+
+  skip_around_action :set_locale
 
   before_action :set_cache_headers
 
