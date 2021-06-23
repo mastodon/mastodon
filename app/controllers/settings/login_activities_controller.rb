979059fbc93664b38b720b369b31beaa89457879
Claire <claire.github-309c@sitedethib.com>
2021-06-23 12:21:40 +0200

    Fix login date not showing up in login history by using “public” pack

--- app/controllers/settings/login_activities_controller.rb
+++ app/controllers/settings/login_activities_controller.rb
@@ -4,4 +4,10 @@ class Settings::LoginActivitiesController < Settings::BaseController
   def index
     @login_activities = LoginActivity.where(user: current_user).order(id: :desc).page(params[:page])
   end
+
+  private
+
+  def set_pack
+    use_pack 'settings'
+  end
 end
