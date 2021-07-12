8a6e29aa44fe82b3c4dd126155cc586d9c25cc83
Claire <claire.github-309c@sitedethib.com>
2021-07-12 10:40:19 +0200

    Change references to tootsuite/mastodon to mastodon/mastodon

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'tootsuite/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
     end
 
     def source_base_url
