19ea6618b18ac5c11d4b0ac4eb8acb2276db6a89
Claire <claire.github-309c@sitedethib.com>
2021-07-13 12:57:07 +0200

    Small scroll/history behavior fixup to take weird browser patterns into account

--- app/javascript/flavours/glitch/containers/scroll_container.js
+++ app/javascript/flavours/glitch/containers/scroll_container.js
@@ -4,7 +4,8 @@ import { ScrollContainer as OriginalScrollContainer } from 'react-router-scroll-
 // new history state and remembering the scroll position when going back.
 // There are a few things we need to do differently, though.
 const defaultShouldUpdateScroll = (prevRouterProps, { location }) => {
-  return !(prevRouterProps?.location?.state?.mastodonModalKey || location.state?.mastodonModalKey);
+  // If the change is caused by opening a modal, do not scroll to top
+  return !(location.state?.mastodonModalKey && location.state?.mastodonModalKey !== prevRouterProps?.location?.state?.mastodonModalKey);
 }
 
 export default
