e4270cb55a133f4cb93290ff88b3fd2a3aa9f536
Claire <claire.github-309c@sitedethib.com>
2021-07-13 13:49:40 +0200

    Please CodeClimate

--- app/javascript/flavours/glitch/containers/scroll_container.js
+++ app/javascript/flavours/glitch/containers/scroll_container.js
@@ -6,11 +6,13 @@ import { ScrollContainer as OriginalScrollContainer } from 'react-router-scroll-
 const defaultShouldUpdateScroll = (prevRouterProps, { location }) => {
   // If the change is caused by opening a modal, do not scroll to top
   return !(location.state?.mastodonModalKey && location.state?.mastodonModalKey !== prevRouterProps?.location?.state?.mastodonModalKey);
-}
+};
 
 export default
 class ScrollContainer extends OriginalScrollContainer {
+
   static defaultProps = {
     shouldUpdateScroll: defaultShouldUpdateScroll,
   };
+
 }
