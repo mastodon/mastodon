4f02e90a31874db0ffbba934200447ed93f23f20
noellabo <noel.yoshiba@gmail.com>
2021-07-30 15:32:05 +0900

    Fix CircleDropdown
    (cherry picked from commit 11e61bf89b2452e0594bc53801deb43ddd8d09f8)

--- app/javascript/mastodon/features/compose/containers/circle_dropdown_container.js
+++ app/javascript/mastodon/features/compose/containers/circle_dropdown_container.js
@@ -2,16 +2,11 @@ import { connect } from 'react-redux';
 import CircleDropdown from '../components/circle_dropdown';
 import { changeComposeCircle } from '../../../actions/compose';
 
-const mapStateToProps = state => {
-  let value = state.getIn(['compose', 'circle_id']);
-  value = value === null ? '' : value;
-
-  return {
-    value: value,
-    visible: state.getIn(['compose', 'privacy']) === 'limited',
-    limitedReply: state.getIn(['compose', 'privacy']) === 'limited' && state.getIn(['compose', 'reply_status', 'visibility']) === 'limited',
-  };
-};
+const mapStateToProps = state => ({
+  value: state.getIn(['compose', 'circle_id']) ?? '',
+  visible: state.getIn(['compose', 'privacy']) === 'limited',
+  limitedReply: state.getIn(['compose', 'privacy']) === 'limited' && state.getIn(['compose', 'reply_status', 'visibility']) === 'limited',
+});
 
 const mapDispatchToProps = dispatch => ({
 
