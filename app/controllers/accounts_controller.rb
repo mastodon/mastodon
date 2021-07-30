dc102f5b354023fc79964e290c9260d035f6a59d
Joseph Cheung <24501026+joseph-cheung@users.noreply.github.com>
2021-07-30 23:46:17 +0800

    Update accounts_controller.rb
    Let the homepage displays only 10 toots for non-logged users

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -143,12 +143,16 @@ class AccountsController < ApplicationController
   end
 
   def cached_filtered_status_page
-    cache_collection_paginated_by_id(
-      filtered_statuses,
-      Status,
-      PAGE_SIZE,
-      params_slice(:max_id, :min_id, :since_id)
-    )
+    if user_signed_in?
+      cache_collection_paginated_by_id(
+        filtered_statuses,
+        Status,
+        PAGE_SIZE,
+        params_slice(:max_id, :min_id, :since_id)
+      )
+    else
+      cache_collection(filtered_statuses.limit(10), Status)
+    end
   end
 
   def params_slice(*keys)
