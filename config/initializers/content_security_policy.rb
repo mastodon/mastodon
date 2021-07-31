395b9ca2d9356cf74f74dcd5ed77aad3513eb1b1
Joseph Cheung <24501026+joseph-cheung@users.noreply.github.com>
2021-07-31 12:59:43 +0800

    Update content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -22,7 +22,7 @@ Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-+ p.font_src :self, assets_host, google_fonts_host
+  p.font_src        :self, assets_host, google_fonts_host
   p.img_src         :self, :https, :data, :blob, assets_host
   p.style_src       :self, assets_host
   p.media_src       :self, :https, :data, assets_host
