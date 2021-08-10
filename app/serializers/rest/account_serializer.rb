55c697c776a7f556409509352a1cb9a5958c46b3
Friedrich Kammer <h.f.kammer@gmx.de>
2021-08-10 22:19:39 +0200

    Update account_serializer.rb
    add user email to account data, which are provided in OAuth context (when a client authenticates at Mastodon and retrieves resource owner data)

--- app/serializers/rest/account_serializer.rb
+++ app/serializers/rest/account_serializer.rb
@@ -5,7 +5,7 @@ class REST::AccountSerializer < ActiveModel::Serializer
 
   attributes :id, :username, :acct, :display_name, :locked, :bot, :discoverable, :group, :created_at,
              :note, :url, :avatar, :avatar_static, :header, :header_static,
-             :followers_count, :following_count, :statuses_count, :last_status_at
+             :followers_count, :following_count, :statuses_count, :last_status_at, :user_email
 
   has_one :moved_to_account, key: :moved, serializer: REST::AccountSerializer, if: :moved_and_not_nested?
 
