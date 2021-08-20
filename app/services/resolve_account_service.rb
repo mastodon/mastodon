168272fe610f9b43b0efff9d5d4a1860e466a387
Claire <claire.github-309c@sitedethib.com>
2021-08-20 08:40:33 +0200

    Fix remotely-suspended accounts' toots being merged back into timelines (#16628)
    * Fix remotely-suspended accounts' toots being merged back into timelines
    
    * Mark remotely-deleted accounts as remotely suspended

--- app/services/resolve_account_service.rb
+++ app/services/resolve_account_service.rb
@@ -141,6 +141,7 @@ class ResolveAccountService < BaseService
   end
 
   def queue_deletion!
+    @account.suspend!(origin: :remote)
     AccountDeletionWorker.perform_async(@account.id, reserve_username: false, skip_activitypub: true)
   end
 
