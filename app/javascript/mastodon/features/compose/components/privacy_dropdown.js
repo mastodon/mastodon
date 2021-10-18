e45f3ba277df29f3bbc46127a3c2b86d8a1554fe
Jesse Karmani <jessekarmani@gmail.com>
2021-10-18 16:33:42 -0700

    Removes public and unlisted option for posts

--- app/javascript/mastodon/features/compose/components/privacy_dropdown.js
+++ app/javascript/mastodon/features/compose/components/privacy_dropdown.js
@@ -234,8 +234,6 @@ class PrivacyDropdown extends React.PureComponent {
     const { intl: { formatMessage } } = this.props;
 
     this.options = [
-      { icon: 'globe', value: 'public', text: formatMessage(messages.public_short), meta: formatMessage(messages.public_long) },
-      { icon: 'unlock', value: 'unlisted', text: formatMessage(messages.unlisted_short), meta: formatMessage(messages.unlisted_long) },
       { icon: 'lock', value: 'private', text: formatMessage(messages.private_short), meta: formatMessage(messages.private_long) },
     ];
 
