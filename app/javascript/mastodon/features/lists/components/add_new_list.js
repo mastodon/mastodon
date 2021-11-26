a78340093789ea691e9f9c94e26fbb5cb6e65a4f
Seszel <57016264+Seszel@users.noreply.github.com>
2021-11-26 20:56:10 +0100

    Single quote

--- app/javascript/mastodon/features/lists/components/add_new_list.js
+++ app/javascript/mastodon/features/lists/components/add_new_list.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { injectIntl, defineMessages } from "react-intl";
-import { useDispatch } from "react-redux";
-import { openModal } from "../../../actions/modal";
-import IconButton from "../../../components/icon_button";
+import React from 'react';
+import { injectIntl, defineMessages } from 'react-intl';
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../../actions/modal';
+import IconButton from '../../../components/icon_button';
 
 const messages = defineMessages({
   title: { id: "lists.new.create", defaultMessage: "Create a new list" },
