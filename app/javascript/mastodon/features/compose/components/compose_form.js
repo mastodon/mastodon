44f9519ce1e0ec47c37ea7d0cf35aad5b709106a
Bartłomiej Mumot <mumot1999@gmail.com>
2021-12-08 16:03:43 +0100

    refactor: delete old stream container

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -22,7 +22,6 @@ import { length } from 'stringz';
 import { countableText } from '../util/counter';
 import Icon from 'mastodon/components/icon';
 import StreamPreviewContainer from '../../stream/containers/StreamPreviewContainer';
-import Stream from '../../stream/containers/Stream';
 import StreamContainer from '../../../containers/stream_container';
 
 const allowedAroundShortCode = '><\u0085\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029\u0009\u000a\u000b\u000c\u000d';
