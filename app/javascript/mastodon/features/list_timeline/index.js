1b4e853aff08ec399ae36d47bd947614a4321cfc
Joanna Adamczyk <joannaa251@gmail.com>
2021-12-11 23:50:09 +0100

    changed class to functional component

--- app/javascript/mastodon/features/list_timeline/index.js
+++ app/javascript/mastodon/features/list_timeline/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import ImmutablePropTypes from 'react-immutable-proptypes';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import StatusListContainer from '../ui/containers/status_list_container';
 import Column from '../../components/column';
 import ColumnBackButton from '../../components/column_back_button';
@@ -10,14 +8,18 @@ import { addColumn, removeColumn, moveColumn } from '../../actions/columns';
 import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
 import { connectListStream } from '../../actions/streaming';
 import { expandListTimeline } from '../../actions/timelines';
-import { fetchList, deleteList, updateList } from '../../actions/lists';
+import {
+  fetchList,
+  fetchLists,
+  deleteList,
+  updateList,
+} from '../../actions/lists';
 import { openModal } from '../../actions/modal';
 import MissingIndicator from '../../components/missing_indicator';
 import LoadingIndicator from '../../components/loading_indicator';
 import Icon from 'mastodon/components/icon';
 import RadioButton from 'mastodon/components/radio_button';
 import { setupListEditor } from '../../actions/lists';
-import { fetchLists } from '../../actions/lists';
 
 const messages = defineMessages({
   deleteMessage: {
@@ -39,241 +41,183 @@ const messages = defineMessages({
   },
 });
 
-const mapStateToProps = (state, props) => ({
-  list: state.getIn(['lists', props.params.id]),
-  hasUnread:
+const ListTimeline = (props) => {
+  const { intl, columnId, multiColumn } = props;
+
+  const dispatch = useDispatch();
+
+  const [list, hasUnread] = useSelector((state) => [
+    state.getIn(['lists', props.params.id]),
     state.getIn(['timelines', `list:${props.params.id}`, 'unread']) > 0,
-});
+  ]);
 
-export default
-@connect(mapStateToProps)
-@injectIntl
-class ListTimeline extends React.PureComponent {
-  static contextTypes = {
-    router: PropTypes.object,
-  };
+  const deleteMessage = intl.formatMessage(messages.deleteMessage);
+  const deleteConfirm = intl.formatMessage(messages.deleteConfirm);
 
-  static propTypes = {
-    params: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
-    columnId: PropTypes.string,
-    hasUnread: PropTypes.bool,
-    multiColumn: PropTypes.bool,
-    list: PropTypes.oneOfType([ImmutablePropTypes.map, PropTypes.bool]),
-    intl: PropTypes.object.isRequired,
-  };
+  const pinned = !!columnId;
+  const title = list ? list.get('title') : props.params.id;
+  const replies_policy = list ? list.get('replies_policy') : undefined;
 
-  handlePin = () => {
-    const { columnId, dispatch } = this.props;
+  useEffect(() => {
+    dispatch(fetchLists());
+    dispatch(fetchList(props.params.id));
+    dispatch(setupListEditor(props.params.id));
+    dispatch(expandListTimeline(props.params.id));
 
+    return () => {
+      dispatch(connectListStream(props.params.id));
+    };
+  }, []);
+
+  useEffect(() => {
+    dispatch(fetchList(props.params.id));
+    dispatch(expandListTimeline(props.params.id));
+    dispatch(connectListStream(props.params.id));
+  }, [props.params.id]);
+
+  const handlePin = () => {
     if (columnId) {
       dispatch(removeColumn(columnId));
     } else {
-      dispatch(addColumn('LIST', { id: this.props.params.id }));
-      this.context.router.history.push('/');
+      dispatch(addColumn('LIST', { id: props.params.id }));
+      context.router.history.push('/');
     }
   };
 
-  handleMove = (dir) => {
-    const { columnId, dispatch } = this.props;
+  const handleMove = (dir) => {
     dispatch(moveColumn(columnId, dir));
   };
 
-  handleHeaderClick = () => {
-    this.column.scrollTop();
+  const handleHeaderClick = () => {
+    scrollTop();
   };
 
-  componentDidMount() {
-    const { dispatch } = this.props;
-    const { id } = this.props.params;
-    console.log('DIDMOUNT' + id);
-
-    dispatch(fetchLists());
-
-    dispatch(fetchList(id));
-    dispatch(setupListEditor(id));
-    dispatch(expandListTimeline(id));
-
-    this.disconnect = dispatch(connectListStream(id));
-  }
-
-  componentDidUpdate() {
-    const { dispatch } = this.props;
-    const { id } = this.props.params;
-    console.log('DIDUPDATE' + id);
-
-    dispatch(fetchList(id));
-    dispatch(setupListEditor(id));
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { dispatch } = this.props;
-    const { id } = nextProps.params;
-
-    console.log('WILLMOUNT' + id);
-
-    if (id !== this.props.params.id) {
-      if (this.disconnect) {
-        this.disconnect();
-        this.disconnect = null;
-      }
-
-      dispatch(fetchList(id));
-      dispatch(expandListTimeline(id));
-
-      this.disconnect = dispatch(connectListStream(id));
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.disconnect) {
-      this.disconnect();
-      this.disconnect = null;
-    }
-  }
-
-  setRef = (c) => {
-    this.column = c;
+  const handleLoadMore = (maxId) => {
+    dispatch(expandListTimeline(props.params.id, { maxId }));
   };
 
-  handleLoadMore = (maxId) => {
-    const { id } = this.props.params;
-    this.props.dispatch(expandListTimeline(id, { maxId }));
+  const handleEditClick = () => {
+    dispatch(openModal('NEW_LIST_ADDER', { listId: props.params.id }));
   };
 
-  handleEditClick = () => {
-    this.props.dispatch(
-      openModal('NEW_LIST_ADDER', { listId: this.props.params.id })
-    );
-  };
-
-  handleDeleteClick = () => {
-    const { dispatch, columnId, intl } = this.props;
-    const { id } = this.props.params;
-
+  const handleDeleteClick = () => {
     dispatch(
       openModal('CONFIRM', {
-        message: intl.formatMessage(messages.deleteMessage),
-        confirm: intl.formatMessage(messages.deleteConfirm),
+        message: deleteMessage,
+        confirm: deleteConfirm,
         onConfirm: () => {
-          dispatch(deleteList(id));
+          dispatch(deleteList(props.params.id));
 
           if (!!columnId) {
             dispatch(removeColumn(columnId));
           } else {
-            this.context.router.history.push('/lists');
+            context.router.history.push('/lists');
           }
         },
       })
     );
   };
 
-  handleRepliesPolicyChange = ({ target }) => {
-    const { dispatch } = this.props;
-    const { id } = this.props.params;
-    dispatch(updateList(id, undefined, false, undefined, target.value));
+  const handleRepliesPolicyChange = ({ target }) => {
+    dispatch(
+      updateList(props.params.id, undefined, false, undefined, target.value)
+    );
   };
 
-  render() {
-    const { hasUnread, columnId, multiColumn, list, intl } = this.props;
-    const { id } = this.props.params;
-    const pinned = !!columnId;
-    const title = list ? list.get('title') : id;
-    const replies_policy = list ? list.get('replies_policy') : undefined;
-
-    if (typeof list === 'undefined') {
-      return (
-        <Column>
-          <div className="scrollable">
-            <LoadingIndicator />
-          </div>
-        </Column>
-      );
-    } else if (list === false) {
-      return (
-        <Column>
-          <ColumnBackButton multiColumn={multiColumn} />
-          <MissingIndicator />
-        </Column>
-      );
-    }
-
+  if (typeof list === 'undefined') {
     return (
-      <Column bindToDocument={!multiColumn} ref={this.setRef} label={title}>
-        <ColumnHeader
-          icon="list-ul"
-          active={hasUnread}
-          title={title}
-          onPin={this.handlePin}
-          onMove={this.handleMove}
-          onClick={this.handleHeaderClick}
-          pinned={pinned}
-          multiColumn={multiColumn}
-        >
-          <div className="column-settings__row column-header__links">
-            <button
-              className="text-btn column-header__setting-btn"
-              tabIndex="0"
-              onClick={this.handleEditClick}
-            >
-              <Icon id="pencil" />{' '}
-              <FormattedMessage id="lists.edit" defaultMessage="Edit list" />
-            </button>
-
-            <button
-              className="text-btn column-header__setting-btn"
-              tabIndex="0"
-              onClick={this.handleDeleteClick}
-            >
-              <Icon id="trash" />{' '}
-              <FormattedMessage
-                id="lists.delete"
-                defaultMessage="Delete list"
-              />
-            </button>
-          </div>
-
-          {replies_policy !== undefined && (
-            <div role="group" aria-labelledby={`list-${id}-replies-policy`}>
-              <span
-                id={`list-${id}-replies-policy`}
-                className="column-settings__section"
-              >
-                <FormattedMessage
-                  id="lists.replies_policy.title"
-                  defaultMessage="Show replies to:"
-                />
-              </span>
-              <div className="column-settings__row">
-                {['none', 'list', 'followed'].map((policy) => (
-                  <RadioButton
-                    name="order"
-                    key={policy}
-                    value={policy}
-                    label={intl.formatMessage(messages[policy])}
-                    checked={replies_policy === policy}
-                    onChange={this.handleRepliesPolicyChange}
-                  />
-                ))}
-              </div>
-            </div>
-          )}
-        </ColumnHeader>
-
-        <StatusListContainer
-          trackScroll={!pinned}
-          scrollKey={`list_timeline-${columnId}`}
-          timelineId={`list:${id}`}
-          onLoadMore={this.handleLoadMore}
-          emptyMessage={
-            <FormattedMessage
-              id="empty_column.list"
-              defaultMessage="There is nothing in this list yet. When members of this list post new statuses, they will appear here."
-            />
-          }
-          bindToDocument={!multiColumn}
-        />
+      <Column>
+        <div className="scrollable">
+          <LoadingIndicator />
+        </div>
+      </Column>
+    );
+  } else if (list === false) {
+    return (
+      <Column>
+        <ColumnBackButton multiColumn={multiColumn} />
+        <MissingIndicator />
       </Column>
     );
   }
-}
+
+  return (
+    <Column bindToDocument={!multiColumn} label={title}>
+      <ColumnHeader
+        icon="list-ul"
+        active={hasUnread}
+        title={title}
+        onPin={handlePin}
+        onMove={handleMove}
+        onClick={handleHeaderClick}
+        pinned={pinned}
+        multiColumn={multiColumn}
+      >
+        <div className="column-settings__row column-header__links">
+          <button
+            className="text-btn column-header__setting-btn"
+            tabIndex="0"
+            onClick={handleEditClick}
+          >
+            <Icon id="pencil" />{' '}
+            <FormattedMessage id="lists.edit" defaultMessage="Edit list" />
+          </button>
+
+          <button
+            className="text-btn column-header__setting-btn"
+            tabIndex="0"
+            onClick={handleDeleteClick}
+          >
+            <Icon id="trash" />{' '}
+            <FormattedMessage id="lists.delete" defaultMessage="Delete list" />
+          </button>
+        </div>
+
+        {replies_policy !== undefined && (
+          <div
+            role="group"
+            aria-labelledby={`list-${props.params.id}-replies-policy`}
+          >
+            <span
+              id={`list-${props.params.id}-replies-policy`}
+              className="column-settings__section"
+            >
+              <FormattedMessage
+                id="lists.replies_policy.title"
+                defaultMessage="Show replies to:"
+              />
+            </span>
+            <div className="column-settings__row">
+              {['none', 'list', 'followed'].map((policy) => (
+                <RadioButton
+                  name="order"
+                  key={policy}
+                  value={policy}
+                  label={intl.formatMessage(messages[policy])}
+                  checked={replies_policy === policy}
+                  onChange={handleRepliesPolicyChange}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </ColumnHeader>
+
+      <StatusListContainer
+        trackScroll={!pinned}
+        scrollKey={`list_timeline-${columnId}`}
+        timelineId={`list:${props.params.id}`}
+        onLoadMore={handleLoadMore}
+        emptyMessage={
+          <FormattedMessage
+            id="empty_column.list"
+            defaultMessage="There is nothing in this list yet. When members of this list post new statuses, they will appear here."
+          />
+        }
+        bindToDocument={!multiColumn}
+      />
+    </Column>
+  );
+};
+
+export default injectIntl(ListTimeline);
