e0a2d22c73c75cb82d6e427a834b685e07f4a0ee
Bartłomiej Mumot <mumot1999@gmail.com>
2021-12-14 12:19:17 +0100

    disable typescript check at compile, move ts-loader as production dependency

--- config/webpack/rules/babelTypescript.js
+++ config/webpack/rules/babelTypescript.js
@@ -7,6 +7,7 @@ module.exports = {
       loader: 'ts-loader',
       options: {
         configFile: path.resolve(__dirname, '../tsconfig.json'),
+        transpileOnly: true
       },
     },
   ],
