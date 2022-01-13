db41704dc2a1ac8c55e81ccc2f14856ee9a7c5fb
Wonderfall <wonderfall@protonmail.com>
2022-01-13 21:33:47 +0100

    disable legacy XSS filtering
    Browsers are phasing out X-XSS-Protection, but Safari and IE still support it.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
     'Server'                 => 'Mastodon',
     'X-Frame-Options'        => 'DENY',
     'X-Content-Type-Options' => 'nosniff',
-    'X-XSS-Protection'       => '1; mode=block',
+    'X-XSS-Protection'       => '0',
     'Permissions-Policy'     => 'interest-cohort=()',
   }
 
