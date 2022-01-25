4e877bdd3a842c2806f993ee00b8581cb03ccd38
mashirozx <moezhx@outlook.com>
2022-01-25 15:51:32 +0800

    fix: social login button display

--- db/schema.rb
+++ db/schema.rb
@@ -899,6 +899,7 @@ ActiveRecord::Schema.define(version: 2022_04_29_101850) do
     t.boolean "local_only"
     t.string "content_type"
     t.bigint "quote_id"
+    t.datetime "edited_at"
     t.index ["account_id", "id", "visibility", "updated_at"], name: "index_statuses_20190820", order: { id: :desc }, where: "(deleted_at IS NULL)"
     t.index ["account_id"], name: "index_statuses_on_account_id"
     t.index ["deleted_at"], name: "index_statuses_on_deleted_at", where: "(deleted_at IS NOT NULL)"
@@ -1174,28 +1175,6 @@ ActiveRecord::Schema.define(version: 2022_04_29_101850) do
   SQL
   add_index "instances", ["domain"], name: "index_instances_on_domain", unique: true
 
-  create_view "user_ips", sql_definition: <<-SQL
-      SELECT t0.user_id,
-      t0.ip,
-      max(t0.used_at) AS used_at
-     FROM ( SELECT users.id AS user_id,
-              users.sign_up_ip AS ip,
-              users.created_at AS used_at
-             FROM users
-            WHERE (users.sign_up_ip IS NOT NULL)
-          UNION ALL
-           SELECT session_activations.user_id,
-              session_activations.ip,
-              session_activations.updated_at
-             FROM session_activations
-          UNION ALL
-           SELECT login_activities.user_id,
-              login_activities.ip,
-              login_activities.created_at
-             FROM login_activities
-            WHERE (login_activities.success = true)) t0
-    GROUP BY t0.user_id, t0.ip;
-  SQL
   create_view "account_summaries", materialized: true, sql_definition: <<-SQL
       SELECT accounts.id AS account_id,
       mode() WITHIN GROUP (ORDER BY t0.language) AS language,
