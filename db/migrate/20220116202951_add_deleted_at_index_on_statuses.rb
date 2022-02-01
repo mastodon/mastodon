a64b25d10564c729cacc621f345b343ac892e70a
Alexandra Dunn <dunn.alex@gmail.com>
2022-01-31 16:25:29 -0800

    AddDeletedAtIndexOnStatuses: add if_not_exist flag
    Otherwise migrations can fail when re-ran: https://gist.github.com/dunn/e3a04a9480cc7799c413e6b8cae00dee

--- db/migrate/20220116202951_add_deleted_at_index_on_statuses.rb
+++ db/migrate/20220116202951_add_deleted_at_index_on_statuses.rb
@@ -2,6 +2,6 @@ class AddDeletedAtIndexOnStatuses < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def change
-    add_index :statuses, :deleted_at, where: 'deleted_at IS NOT NULL', algorithm: :concurrently
+    add_index :statuses, :deleted_at, where: 'deleted_at IS NOT NULL', algorithm: :concurrently, if_not_exists: true
   end
 end
