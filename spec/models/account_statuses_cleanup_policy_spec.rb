472bc89611cb3ce0594cd7b42b730c3484899c33
Claire <claire.github-309c@sitedethib.com>
2022-02-10 22:00:10 +0100

    Fix some flaky tests that randomly failed because of item ordering (#17509)

--- spec/models/account_statuses_cleanup_policy_spec.rb
+++ spec/models/account_statuses_cleanup_policy_spec.rb
@@ -495,7 +495,7 @@ RSpec.describe AccountStatusesCleanupPolicy, type: :model do
       end
 
       it 'returns only normal statuses for deletion' do
-        expect(subject.pluck(:id).sort).to eq [very_old_status.id, faved4.id, faved5.id, reblogged4.id, reblogged5.id].sort
+        expect(subject.pluck(:id)).to match_array([very_old_status.id, faved4.id, faved5.id, reblogged4.id, reblogged5.id])
       end
     end
 
