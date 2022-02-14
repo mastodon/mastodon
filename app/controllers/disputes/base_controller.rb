41d52ee4b5d72b3f63bf05b61bfb08b4ea270042
Claire <claire.github-309c@sitedethib.com>
2022-02-14 21:54:01 +0100

    Fix issue with glitch-soc's theming system

--- app/controllers/disputes/base_controller.rb
+++ app/controllers/disputes/base_controller.rb
@@ -9,8 +9,13 @@ class Disputes::BaseController < ApplicationController
 
   before_action :set_body_classes
   before_action :authenticate_user!
+  before_action :set_pack
 
   private
+
+  def set_pack
+    use_pack 'admin'
+  end
 
   def set_body_classes
     @body_classes = 'admin'
