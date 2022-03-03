ee406e35900be7bd2ad4996ba5f48d097411cda3
Eugen Rochko <eugen@zeonfederated.com>
2022-03-04 00:25:15 +0100

    Fix duplicate notifications being possible after poll expiration

--- app/controllers/api/v1/follow_requests_controller.rb
+++ app/controllers/api/v1/follow_requests_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::FollowRequestsController < Api::BaseController
 
   def authorize
     AuthorizeFollowService.new.call(account, current_account)
-    NotifyService.new.call(current_account, :follow, Follow.find_by(account: account, target_account: current_account))
+    LocalNotificationWorker.perform_async(current_account.id, Follow.find_by(account: account, target_account: current_account).id, 'Follow', 'follow')
     render json: account, serializer: REST::RelationshipSerializer, relationships: relationships
   end
 
