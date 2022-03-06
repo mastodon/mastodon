1789e3ca187d160f61b440ebd256720c77a81412
Eugen Rochko <eugen@zeonfederated.com>
2022-03-06 23:54:16 +0100

    Add dummy classes to migration

--- db/migrate/20220304195405_migrate_hide_network_preference.rb
+++ db/migrate/20220304195405_migrate_hide_network_preference.rb
@@ -1,6 +1,16 @@
 class MigrateHideNetworkPreference < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
+  # Dummy classes, to make migration possible across version changes
+  class Account < ApplicationRecord
+    has_one :user, inverse_of: :account
+    scope :local, -> { where(domain: nil) }
+  end
+
+  class User < ApplicationRecord
+    belongs_to :account
+  end
+
   def up
     Setting.where(thing_type: 'User', var: 'hide_network').find_each do |setting|
       account = User.find(setting.thing_id).account
