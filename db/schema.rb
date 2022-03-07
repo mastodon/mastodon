edf09ec747ebba5a170e27eb13663462a116ec6c
Eugen Rochko <eugen@zeonfederated.com>
2022-03-07 09:36:47 +0100

    Add `/api/v1/accounts/familiar_followers` to REST API (#17700)
    * Add `/api/v1/accounts/familiar_followers` to REST API
    
    * Change hide network preference to be stored consistently for local and remote accounts
    
    * Add dummy classes to migration
    
    * Apply suggestions from code review
    
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>
    
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_041951) do
+ActiveRecord::Schema.define(version: 2022_03_04_195405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
