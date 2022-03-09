ee8ed418940c1ac1a8286efafd42622d77858ccf
Eugen Rochko <eugen@zeonfederated.com>
2022-03-09 07:33:17 +0100

    Change poll in status edit to have a structure closer to the real one

--- app/serializers/rest/status_edit_serializer.rb
+++ app/serializers/rest/status_edit_serializer.rb
@@ -5,16 +5,16 @@ class REST::StatusEditSerializer < ActiveModel::Serializer
 
   attributes :content, :spoiler_text, :sensitive, :created_at
 
-  attribute :poll, if: -> { object.poll_options.present? }
-
   has_many :ordered_media_attachments, key: :media_attachments, serializer: REST::MediaAttachmentSerializer
   has_many :emojis, serializer: REST::CustomEmojiSerializer
+
+  attribute :poll, if: -> { object.poll_options.present? }
 
   def content
     Formatter.instance.format(object)
   end
 
   def poll
-    { options: object.poll_options }
+    { options: object.poll_options.map { |title| { title: title } } }
   end
 end
