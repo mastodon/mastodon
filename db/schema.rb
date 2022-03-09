504fe2a71bd2cac4b9189932a42d3c687884e639
Claire <claire.github-309c@sitedethib.com>
2022-03-09 21:31:09 +0100

    Fix existing boosts possibly referencing deleted toots
    Follow-up to #17693

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_094650) do
+ActiveRecord::Schema.define(version: 2022_03_09_213005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
