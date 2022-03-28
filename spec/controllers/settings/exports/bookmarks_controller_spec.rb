dec1bbfc5c7265d2fa31bffd92004aa12c225616
Claire <claire.github-309c@sitedethib.com>
2022-03-28 11:29:02 +0200

    Fix failing test

--- spec/controllers/settings/exports/bookmarks_controller_spec.rb
+++ spec/controllers/settings/exports/bookmarks_controller_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 describe Settings::Exports::BookmarksController do
   render_views
 
-  describe 'GET #index' do
-    it 'returns a csv of the bookmarked toots' do
-      user = Fabricate(:user)
-      user.account.bookmarks.create!(status: Fabricate(:status, uri: 'https://foo.bar/statuses/1312'))
+  let(:user)    { Fabricate(:user) }
+  let(:account) { Fabricate(:account, domain: 'foo.bar') }
+  let(:status)  { Fabricate(:status, account: account, uri: 'https://foo.bar/statuses/1312') }
 
+  describe 'GET #index' do
+    before do
+      user.account.bookmarks.create!(status: status)
+    end
+
+    it 'returns a csv of the bookmarked toots' do
       sign_in user, scope: :user
       get :index, format: :csv
 
