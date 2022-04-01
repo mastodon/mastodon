814ced2e26e0108d509b7df6e1743a37f13c98a9
Darius Kazemi <darius@meedan.com>
2022-04-01 16:09:59 -0700

    Fix typo

--- app/controllers/api/v1/statuses_controller.rb
+++ app/controllers/api/v1/statuses_controller.rb
@@ -50,7 +50,7 @@ class Api::V1::StatusesController < Api::BaseController
       poll: status_params[:poll],
       idempotency: request.headers['Idempotency-Key'],
       with_rate_limit: true,
-      local_only: status_params[:local_only])
+      local_only: status_params[:local_only]
     )
 
     render json: @status, serializer: @status.is_a?(ScheduledStatus) ? REST::ScheduledStatusSerializer : REST::StatusSerializer
