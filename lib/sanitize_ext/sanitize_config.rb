f2b2614d0a3852259f21f7969a946fb3d8b7e96d
Claire <claire.github-309c@sitedethib.com>
2022-04-11 08:40:18 +0200

    Fix link sanitization for outgoing text/html and text/markdown toots
    Fixes #1739

--- lib/sanitize_ext/sanitize_config.rb
+++ lib/sanitize_ext/sanitize_config.rb
@@ -133,7 +133,7 @@ class Sanitize
       rel += ['nofollow', 'noopener', 'noreferrer'] unless TagManager.instance.local_url?(node['href'])
 
       if rel.empty?
-        node['rel']&.delete
+        node.remove_attribute('rel')
       else
         node['rel'] = rel.join(' ')
       end
@@ -144,7 +144,7 @@ class Sanitize
 
       node = env[:node]
       if node['target'] != '_blank' && TagManager.instance.local_url?(node['href'])
-        node['target']&.delete
+        node.remove_attribute('target')
       else
         node['target'] = '_blank'
       end
