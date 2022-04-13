fce9247065ba5f903ad7a53d38c531840b14c83f
trwnh <a@trwnh.com>
2022-04-13 03:51:06 -0500

    change capture groups to meet expectations

--- app/models/account.rb
+++ app/models/account.rb
@@ -62,7 +62,7 @@ class Account < ApplicationRecord
   )
 
   USERNAME_RE   = /[a-z0-9_]+([a-z0-9_\.-]+[a-z0-9_]+)?/i
-  MENTION_RE    = /(?:[^[:word:]|\/])(?:@(#{USERNAME_RE})+?(?:@([[:word:]\.\-]+[[:word:]]+)+?)?)/i
+  MENTION_RE    = /(?:^|[^\w\/])(?:@((?:#{USERNAME_RE})+?(?:@(?:[\w\.\-]+[\w]+)+?)?))/i
   URL_PREFIX_RE = /\Ahttp(s?):\/\/[^\/]+/
 
   include Attachmentable
