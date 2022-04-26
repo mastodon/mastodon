7e1fc2e92544e4dc0ffa1e765ad7b9c181a84a7d
Claire <claire.github-309c@sitedethib.com>
2022-04-26 18:00:23 +0200

    Change Direct Message warning to make admins' ability to access them clearer
    Fixes #18079

--- app/javascript/mastodon/features/compose/containers/warning_container.js
+++ app/javascript/mastodon/features/compose/containers/warning_container.js
@@ -48,7 +48,7 @@ const WarningWrapper = ({ needsLockWarning, hashtagWarning, directMessageWarning
   if (directMessageWarning) {
     const message = (
       <span>
-        <FormattedMessage id='compose_form.direct_message_warning' defaultMessage='This toot will only be sent to all the mentioned users.' /> <a href='/terms' target='_blank'><FormattedMessage id='compose_form.direct_message_warning_learn_more' defaultMessage='Learn more' /></a>
+        <FormattedMessage id='compose_form.direct_message_warning' defaultMessage='This post will only be sent to all the mentioned users, and may also be accessed by the operators of the involved servers.' /> <a href='/terms' target='_blank'><FormattedMessage id='compose_form.direct_message_warning_learn_more' defaultMessage='Learn more' /></a>
       </span>
     );
 
