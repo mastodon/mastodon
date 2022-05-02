cda1f663456948677eef8a5f15375faa85677837
Claire <claire.github-309c@sitedethib.com>
2022-05-02 16:52:11 +0200

    Fix compatibility with Friendica regarding pinned posts
    Fixes #18066

--- app/services/activitypub/fetch_featured_collection_service.rb
+++ app/services/activitypub/fetch_featured_collection_service.rb
@@ -7,19 +7,33 @@ class ActivityPub::FetchFeaturedCollectionService < BaseService
     return if account.featured_collection_url.blank? || account.suspended? || account.local?
 
     @account = account
-    @json    = fetch_resource(@account.featured_collection_url, true)
+    @json    = fetch_resource(@account.featured_collection_url, true, local_follower)
 
-    return unless supported_context?
+    return unless supported_context?(@json)
 
-    case @json['type']
-    when 'Collection', 'CollectionPage'
-      process_items @json['items']
-    when 'OrderedCollection', 'OrderedCollectionPage'
-      process_items @json['orderedItems']
-    end
+    process_items(collection_items(@json))
   end
 
   private
+
+  def collection_items(collection)
+    collection = fetch_collection(collection['first']) if collection['first'].present?
+    return unless collection.is_a?(Hash)
+
+    case collection['type']
+    when 'Collection', 'CollectionPage'
+      collection['items']
+    when 'OrderedCollection', 'OrderedCollectionPage'
+      collection['orderedItems']
+    end
+  end
+
+  def fetch_collection(collection_or_uri)
+    return collection_or_uri if collection_or_uri.is_a?(Hash)
+    return if invalid_origin?(collection_or_uri)
+
+    fetch_resource_without_id_validation(collection_or_uri, nil, true, local_follower)
+  end
 
   def process_items(items)
     status_ids = items.filter_map do |item|
@@ -51,10 +65,6 @@ class ActivityPub::FetchFeaturedCollectionService < BaseService
     to_add.each do |status_id|
       StatusPin.create!(account: @account, status_id: status_id)
     end
-  end
-
-  def supported_context?
-    super(@json)
   end
 
   def local_follower
