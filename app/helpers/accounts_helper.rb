cef1a63f22af284a2eec3650cc863e792f21ab78
Jesse Karmani <jessekarmani@gmail.com>
2022-05-16 13:12:32 -0700

    Just use img tag for uploaded logos since we can't specify an id to reference

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -100,13 +100,13 @@ module AccountsHelper
 
   def svg_logo
     logo_url = InstancePresenter.new.logo_transparent&.file&.url
-    tag = logo_url.present? ? tag(:use, 'href' => logo_url) : tag(:use, 'xlink:href' => '#decodon-logo')
-    content_tag(:svg, tag, 'viewBox' => '0 0 216.4144 232.00976')
+    tag = logo_url.present? ? tag(:img, 'src' => logo_url) : nil
+    tag || content_tag(:svg, tag(:use, 'xlink:href' => '#decodon-logo'), 'viewBox' => '0 0 216.4144 232.00976')
   end
 
   def svg_logo_full
     logo_url = InstancePresenter.new.logo&.file&.url
-    tag = logo_url.present? ? tag(:use, 'href' => logo_url) : tag(:use, 'xlink:href' => '#decodon-logo-full')
-    content_tag(:svg, tag, 'viewBox' => '0 0 713.35878 175.8678')
+    tag = logo_url.present? ? tag(:img, 'src' => logo_url) : nil
+    tag || content_tag(:svg, tag(:use, 'xlink:href' => '#decodon-logo-full'), 'viewBox' => '0 0 713.35878 175.8678')
   end
 end
