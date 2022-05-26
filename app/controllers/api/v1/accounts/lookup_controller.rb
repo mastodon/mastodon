93117ea7bfdb12c10b1b7ba748b370cfc8f263be
Claire <claire.github-309c@sitedethib.com>
2022-05-26 09:53:39 +0200

    Allow lookup of unconfirmed/unapproved accounts through /api/v1/accounts/lookup
    This is so that the API can still be used to check whether an username is free
    to use.

--- app/controllers/api/v1/accounts/lookup_controller.rb
+++ app/controllers/api/v1/accounts/lookup_controller.rb
@@ -3,8 +3,6 @@
 class Api::V1::Accounts::LookupController < Api::BaseController
   before_action -> { authorize_if_got_token! :read, :'read:accounts' }
   before_action :set_account
-  before_action :check_account_approval
-  before_action :check_account_confirmation
 
   def show
     render json: @account, serializer: REST::AccountSerializer
@@ -16,13 +14,5 @@ class Api::V1::Accounts::LookupController < Api::BaseController
     @account = ResolveAccountService.new.call(params[:acct], skip_webfinger: true) || raise(ActiveRecord::RecordNotFound)
   rescue Addressable::URI::InvalidURIError
     raise(ActiveRecord::RecordNotFound)
-  end
-
-  def check_account_approval
-    raise(ActiveRecord::RecordNotFound) if @account.local? && @account.user_pending?
-  end
-
-  def check_account_confirmation
-    raise(ActiveRecord::RecordNotFound) if @account.local? && !@account.user_confirmed?
   end
 end
