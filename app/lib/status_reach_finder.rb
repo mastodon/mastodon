27a4fcee74602ccfcc51a9a6e5acec9f4dc42bfd
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2022-05-30 18:42:57 +0000

    fix process to get inbox url

--- app/lib/status_reach_finder.rb
+++ app/lib/status_reach_finder.rb
@@ -16,33 +16,28 @@ class StatusReachFinder
   private
 
   def reached_account_inboxes
+    Account.where(id: reached_account_ids).inboxes
+  end
+
+  def reached_account_ids
     # When the status is a reblog, there are no interactions with it
     # directly, we assume all interactions are with the original one
 
     if @status.reblog?
-      reblogged_status_account = @status.reblog.account
-      if !reblogged_status_account.local? && reblogged_status_account.activitypub? && !reblogged_status_account.following?(@status.account)
-        [reblogged_status_account.inbox_url]
-      else
-        []
-      end
+      [reblog_of_account_id]
     else
-      Account.where(id: reached_account_ids).inboxes
-    end
-  end
-
-  def reached_account_ids
-    [
-      replied_to_account_id,
-      reblog_of_account_id,
-      mentioned_account_ids,
-      reblogs_account_ids,
-      favourites_account_ids,
-      replies_account_ids,
-    ].tap do |arr|
-      arr.flatten!
-      arr.compact!
-      arr.uniq!
+      [
+        replied_to_account_id,
+        reblog_of_account_id,
+        mentioned_account_ids,
+        reblogs_account_ids,
+        favourites_account_ids,
+        replies_account_ids,
+      ].tap do |arr|
+        arr.flatten!
+        arr.compact!
+        arr.uniq!
+      end
     end
   end
 
