8c82d6e24e64f8f2c91e3aa63931a2fc16cf2039
mashirozx <moezhx@outlook.com>
2022-06-09 23:50:40 +0800

    feat: allow admin to customize invite request hints

--- app/models/form/admin_settings.rb
+++ app/models/form/admin_settings.rb
@@ -33,6 +33,7 @@ class Form::AdminSettings
     show_domain_blocks
     show_domain_blocks_rationale
     noindex
+    invite_request_hint
     require_invite_text
   ).freeze
 
@@ -59,7 +60,7 @@ class Form::AdminSettings
 
   attr_accessor(*KEYS)
 
-  validates :site_short_description, :site_description, html: { wrap_with: :p }
+  validates :site_short_description, :site_description, :invite_request_hint, html: { wrap_with: :p }
   validates :site_extended_description, :site_terms, :closed_registrations_message, html: true
   validates :registrations_mode, inclusion: { in: %w(open approved none) }
   validates :min_invite_role, inclusion: { in: %w(disabled user moderator admin) }
