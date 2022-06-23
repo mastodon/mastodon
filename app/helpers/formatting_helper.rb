e543f843faf03b2091c1d618b722a9c25873517b
Claire <claire.github-309c@sitedethib.com>
2022-06-23 13:56:07 +0200

    Add “missing mentions” to the end of remote posts

--- app/helpers/formatting_helper.rb
+++ app/helpers/formatting_helper.rb
@@ -15,7 +15,27 @@ module FormattingHelper
   module_function :extract_status_plain_text
 
   def status_content_format(status)
-    html_aware_format(status.text, status.local?, preloaded_accounts: [status.account] + (status.respond_to?(:active_mentions) ? status.active_mentions.map(&:account) : []))
+    preloaded_accounts = [status.account] + (status.respond_to?(:active_mentions) ? status.active_mentions.map(&:account) : [])
+    output = html_aware_format(status.text, status.local?, preloaded_accounts: preloaded_accounts)
+
+    if status.respond_to?(:active_mentions)
+      unmatched_mentions = (status.active_mentions.map(&:account).to_a - [status.account]).index_by { |account| ActivityPub::TagManager.instance.url_for(account) }
+      Nokogiri::HTML5.fragment(output).css('a.mention').each do |a|
+        unmatched_mentions.delete(a['href'])
+      end
+
+      unless unmatched_mentions.empty?
+        extra_mentions = unmatched_mentions.values.sort_by(&:username).map do |account|
+          url = ActivityPub::TagManager.instance.url_for(account)
+          <<~HTML.squish
+            <span class="h-card"><a href="#{ERB::Util.h(url)}" class="u-url mention">@<span>#{ERB::Util.h(account.username)}</span></a></span>
+          HTML
+        end.join(' ')
+        output = [output, "\n<p>", extra_mentions, '</p>'].join('').html_safe # rubocop:disable Rails/OutputSafety
+      end
+    end
+
+    output
   end
 
   def rss_status_content_format(status)
