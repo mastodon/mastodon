47349b2967398d3feec39807cd56029dc9893594
Claire <claire.github-309c@sitedethib.com>
2022-08-07 08:28:22 +0200

    Fix /api/v1/admin/trends/tags using wrong serializer
    Fix regression from #18641

--- app/controllers/api/v1/admin/trends/tags_controller.rb
+++ app/controllers/api/v1/admin/trends/tags_controller.rb
@@ -3,6 +3,10 @@
 class Api::V1::Admin::Trends::TagsController < Api::V1::Trends::TagsController
   before_action -> { authorize_if_got_token! :'admin:read' }
 
+  def index
+    render json: @tags, each_serializer: REST::Admin::TagSerializer
+  end
+
   private
 
   def enabled?
