3777dc956279b3906da96f093df4ebf8687377db
Shlee <github@shl.ee>
2022-08-22 10:28:32 +0930

    Update uuid.js

--- app/javascript/mastodon/uuid.js
+++ app/javascript/mastodon/uuid.js
@@ -1,3 +1,3 @@
 export default function uuid(a) {
-  return a ? (a^Crypto.getRandomValues() * 16 >> a / 4).toString(16) : ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, uuid);
+  return a ? (a ^ (crypto.getRandomValues(new Uint32Array(1))[0] / 0xffffffff) * 16 >> a / 4).toString(16) : ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, uuid);
 };
