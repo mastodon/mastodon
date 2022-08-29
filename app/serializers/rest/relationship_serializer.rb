6aa0c88e8af7a045eaa28d184502f80ecfc016b5
Eugen Rochko <eugen@zeonfederated.com>
2022-08-30 00:40:36 +0200

    Add ability to filter followed accounts' posts by language

--- app/serializers/rest/relationship_serializer.rb
+++ app/serializers/rest/relationship_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class REST::RelationshipSerializer < ActiveModel::Serializer
-  attributes :id, :following, :showing_reblogs, :notifying, :followed_by,
+  attributes :id, :following, :showing_reblogs, :notifying, :languages, :followed_by,
              :blocking, :blocked_by, :muting, :muting_notifications, :requested,
              :domain_blocking, :endorsed, :note
 
@@ -23,6 +23,11 @@ class REST::RelationshipSerializer < ActiveModel::Serializer
     (instance_options[:relationships].following[object.id] || {})[:notify] ||
       (instance_options[:relationships].requested[object.id] || {})[:notify] ||
       false
+  end
+
+  def languages
+    (instance_options[:relationships].following[object.id] || {})[:languages] ||
+      (instance_options[:relationships].requested[object.id] || {})[:languages]
   end
 
   def followed_by
