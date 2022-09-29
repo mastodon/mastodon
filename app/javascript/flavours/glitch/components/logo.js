52e056cd61c33b63693889105ae819cd5f5783b4
Eugen Rochko <eugen@zeonfederated.com>
2022-09-29 04:39:33 +0200

    [Glitch] Fix logo on /web/start
    Partial port from 43b5d5e38d2b8ad8f1d1ad0911c3c1718159c912
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/components/logo.js
+++ app/javascript/flavours/glitch/components/logo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Logo = () => (
-  <svg viewBox='0 0 216.4144 232.00976' className='logo'>
-    <use xlinkHref='#mastodon-svg-logo' />
+  <svg viewBox='0 0 261 66' className='logo'>
+    <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
 );
 
