f4fe985a03ce174afb56a74c3704db22c7f699fa
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-10-04 06:08:38 +0900

    [Glitch] Fix missing await
    Port fc3d248df770024b5ce0f33d93029d71f5051e43 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/packs/home.js
+++ app/javascript/flavours/glitch/packs/home.js
@@ -2,7 +2,7 @@ import 'packs/public-path';
 import loadPolyfills from 'flavours/glitch/util/load_polyfills';
 
 loadPolyfills().then(async () => {
-  const { default: main } = import('flavours/glitch/util/main');
+  const { default: main } = await import('flavours/glitch/util/main');
 
   return main();
 }).catch(e => {
