12359fdfd2e8898a3e76c11be6e114960d25b595
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-10-09 01:36:30 +0000

    Add title to pages with missing title in Web UI

--- app/javascript/mastodon/features/directory/index.js
+++ app/javascript/mastodon/features/directory/index.js
@@ -13,7 +13,6 @@ import RadioButton from 'mastodon/components/radio_button';
 import LoadMore from 'mastodon/components/load_more';
 import ScrollContainer from 'mastodon/containers/scroll_container';
 import LoadingIndicator from 'mastodon/components/loading_indicator';
-import { title } from 'mastodon/initial_state';
 import { Helmet } from 'react-helmet';
 
 const messages = defineMessages({
@@ -169,7 +168,7 @@ class Directory extends React.PureComponent {
         {multiColumn && !pinned ? <ScrollContainer scrollKey='directory'>{scrollableArea}</ScrollContainer> : scrollableArea}
 
         <Helmet>
-          <title>{intl.formatMessage(messages.title)} - {title}</title>
+          <title>{intl.formatMessage(messages.title)}</title>
         </Helmet>
       </Column>
     );
