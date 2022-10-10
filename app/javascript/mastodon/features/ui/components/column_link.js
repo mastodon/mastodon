3eef8a7a81512f283300d2371d5b743611cb0d27
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-10-10 13:51:16 +0900

    Fix `ColumnLink` labels not disappearing in mobile UI (#19334)

--- app/javascript/mastodon/features/ui/components/column_link.js
+++ app/javascript/mastodon/features/ui/components/column_link.js
@@ -13,7 +13,7 @@ const ColumnLink = ({ icon, text, to, href, method, badge, transparent, ...other
     return (
       <a href={href} className={className} data-method={method} title={text} {...other}>
         {iconElement}
-        {text}
+        <span>{text}</span>
         {badgeElement}
       </a>
     );
@@ -21,7 +21,7 @@ const ColumnLink = ({ icon, text, to, href, method, badge, transparent, ...other
     return (
       <NavLink to={to} className={className} title={text} {...other}>
         {iconElement}
-        {text}
+        <span>{text}</span>
         {badgeElement}
       </NavLink>
     );
