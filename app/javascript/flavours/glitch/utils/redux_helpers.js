2535ec4fcb6247199a5746db1119d3dabed9d559
Claire <claire.github-309c@sitedethib.com>
2022-10-11 12:05:34 +0200

    Remove unused `redux_helpers` module

--- app/javascript/flavours/glitch/utils/redux_helpers.js
+++
@@ -1,8 +0,0 @@
-import { injectIntl } from 'react-intl';
-import { connect } from 'react-redux';
-
-//  Connects a component.
-export function wrap (Component, mapStateToProps, mapDispatchToProps, options) {
-  const withIntl = typeof options === 'object' ? options.withIntl : !!options;
-  return (withIntl ? injectIntl : i => i)(connect(mapStateToProps, mapDispatchToProps)(Component));
-}
