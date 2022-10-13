f01310dadbca94c74aa44e0a4725d49c5ac15434
Eugen Rochko <eugen@zeonfederated.com>
2022-10-14 01:44:23 +0200

    Fix trending statuses returning more than one post by the same author (#19349)

--- app/models/status_trend.rb
+++ app/models/status_trend.rb
@@ -17,5 +17,5 @@ class StatusTrend < ApplicationRecord
   belongs_to :status
   belongs_to :account
 
-  scope :allowed, -> { where(allowed: true) }
+  scope :allowed, -> { joins('INNER JOIN (SELECT account_id, MAX(score) AS max_score FROM status_trends GROUP BY account_id) AS grouped_status_trends ON status_trends.account_id = grouped_status_trends.account_id AND status_trends.score = grouped_status_trends.max_score').where(allowed: true) }
 end
