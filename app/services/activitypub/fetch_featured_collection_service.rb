mastodon/mastodon

commit b0e3f0312c3271a2705f912602fcba70f4ed8b69

Add synchronization of remote featured tags (#19380)
* Add LIMIT of featured tag to instance API response

* Add featured_tags_collection_url to Account

* Add synchronization of remote featured tags

* Deliver update activity when updating featured tag

* Remove featured_tags_collection_url

* Revert "Add featured_tags_collection_url to Account"

This reverts commit cff349fc27b104ded2df6bb5665132dc24dab09c.

* Add hashtag sync from featured collections

* Fix tag name normalize

* Add target option to fetch featured collection

* Refactor fetch_featured_tags_collection_service

* Add LIMIT of featured tag to v1/instance API response

app/services/activitypub/fetch_featured_collection_service.rb
@@ -3,10 +3,11 @@
 class ActivityPub::FetchFeaturedCollectionService < BaseService
   include JsonLdHelper
 
-  def call(account)
+  def call(account, **options)
     return if account.featured_collection_url.blank? || account.suspended? || account.local?
 
     @account = account
+    @options = options
     @json    = fetch_resource(@account.featured_collection_url, true, local_follower)
 
     return unless supported_context?(@json)
@@ -36,7 +37,15 @@ class ActivityPub::FetchFeaturedCollectionService < BaseService
   end
 
   def process_items(items)
+    process_note_items(items) if @options[:note]
+    process_hashtag_items(items) if @options[:hashtag]
+  end
+
+  def process_note_items(items)
     status_ids = items.filter_map do |item|
+      type = item['type']
+      next unless type == 'Note'
+
       uri = value_or_id(item)
       next if ActivityPub::TagManager.instance.local_uri?(uri)
 
@@ -64,6 +73,26 @@ class ActivityPub::FetchFeaturedCollectionService < BaseService
 
     to_add.each do |status_id|
       StatusPin.create!(account: @account, status_id: status_id)
+    end
+  end
+
+  def process_hashtag_items(items)
+    names     = items.filter_map { |item| item['type'] == 'Hashtag' && item['name']&.delete_prefix('#') }.map { |name| HashtagNormalizer.new.normalize(name) }
+    to_remove = []
+    to_add    = names
+
+    FeaturedTag.where(account: @account).map(&:name).each do |name|
+      if names.include?(name)
+        to_add.delete(name)
+      else
+        to_remove << name
+      end
+    end
+
+    FeaturedTag.includes(:tag).where(account: @account, tags: { name: to_remove }).delete_all unless to_remove.empty?
+
+    to_add.each do |name|
+      FeaturedTag.create!(account: @account, name: name)
     end
   end
 

app/controllers/api/v1/featured_tags_controller.rb
@@ -14,11 +14,13 @@ class Api::V1::FeaturedTagsController < Api::BaseController
 
   def create
     @featured_tag = current_account.featured_tags.create!(featured_tag_params)
+    ActivityPub::UpdateDistributionWorker.perform_in(3.minutes, current_account.id)
     render json: @featured_tag, serializer: REST::FeaturedTagSerializer
   end
 
   def destroy
     @featured_tag.destroy!
+    ActivityPub::UpdateDistributionWorker.perform_in(3.minutes, current_account.id)
     render_empty
   end
 

app/controllers/settings/featured_tags_controller.rb
@@ -13,6 +13,7 @@ class Settings::FeaturedTagsController < Settings::BaseController
     @featured_tag = current_account.featured_tags.new(featured_tag_params)
 
     if @featured_tag.save
+      ActivityPub::UpdateDistributionWorker.perform_in(3.minutes, current_account.id)
       redirect_to settings_featured_tags_path
     else
       set_featured_tags
@@ -24,6 +25,7 @@ class Settings::FeaturedTagsController < Settings::BaseController
 
   def destroy
     @featured_tag.destroy!
+    ActivityPub::UpdateDistributionWorker.perform_in(3.minutes, current_account.id)
     redirect_to settings_featured_tags_path
   end
 

app/models/featured_tag.rb
@@ -26,6 +26,8 @@ class FeaturedTag < ApplicationRecord
 
   attr_writer :name
 
+  LIMIT = 10
+
   def name
     tag_id.present? ? tag.name : @name
   end
@@ -50,7 +52,7 @@ class FeaturedTag < ApplicationRecord
   end
 
   def validate_featured_tags_limit
-    errors.add(:base, I18n.t('featured_tags.errors.limit')) if account.featured_tags.count >= 10
+    errors.add(:base, I18n.t('featured_tags.errors.limit')) if account.featured_tags.count >= LIMIT
   end
 
   def validate_tag_name

app/serializers/rest/instance_serializer.rb
@@ -47,6 +47,10 @@ class REST::InstanceSerializer < ActiveModel::Serializer
         streaming: Rails.configuration.x.streaming_api_base_url,
       },
 
+      accounts: {
+        max_featured_tags: FeaturedTag::LIMIT,
+      },
+
       statuses: {
         max_characters: StatusLengthValidator::MAX_CHARS,
         max_media_attachments: 4,

app/serializers/rest/v1/instance_serializer.rb
@@ -58,6 +58,10 @@ class REST::V1::InstanceSerializer < ActiveModel::Serializer
 
   def configuration
     {
+      accounts: {
+        max_featured_tags: FeaturedTag::LIMIT,
+      },
+
       statuses: {
         max_characters: StatusLengthValidator::MAX_CHARS,
         max_media_attachments: 4,

app/services/activitypub/fetch_featured_tags_collection_service.rb
@@ -1,0 +1,78 @@
+# frozen_string_literal: true
+
+class ActivityPub::FetchFeaturedTagsCollectionService < BaseService
+  include JsonLdHelper
+
+  def call(account, url)
+    return if url.blank? || account.suspended? || account.local?
+
+    @account = account
+    @json    = fetch_resource(url, true, local_follower)
+
+    return unless supported_context?(@json)
+
+    process_items(collection_items(@json))
+  end
+
+  private
+
+  def collection_items(collection)
+    all_items = []
+
+    collection = fetch_collection(collection['first']) if collection['first'].present?
+
+    while collection.is_a?(Hash)
+      items = begin
+        case collection['type']
+        when 'Collection', 'CollectionPage'
+          collection['items']
+        when 'OrderedCollection', 'OrderedCollectionPage'
+          collection['orderedItems']
+        end
+      end
+
+      break if items.blank?
+
+      all_items.concat(items)
+
+      break if all_items.size >= FeaturedTag::LIMIT
+
+      collection = collection['next'].present? ? fetch_collection(collection['next']) : nil
+    end
+
+    all_items
+  end
+
+  def fetch_collection(collection_or_uri)
+    return collection_or_uri if collection_or_uri.is_a?(Hash)
+    return if invalid_origin?(collection_or_uri)
+
+    fetch_resource_without_id_validation(collection_or_uri, local_follower, true)
+  end
+
+  def process_items(items)
+    names     = items.filter_map { |item| item['type'] == 'Hashtag' && item['name']&.delete_prefix('#') }.map { |name| HashtagNormalizer.new.normalize(name) }
+    to_remove = []
+    to_add    = names
+
+    FeaturedTag.where(account: @account).map(&:name).each do |name|
+      if names.include?(name)
+        to_add.delete(name)
+      else
+        to_remove << name
+      end
+    end
+
+    FeaturedTag.includes(:tag).where(account: @account, tags: { name: to_remove }).delete_all unless to_remove.empty?
+
+    to_add.each do |name|
+      FeaturedTag.create!(account: @account, name: name)
+    end
+  end
+
+  def local_follower
+    return @local_follower if defined?(@local_follower)
+
+    @local_follower = @account.followers.local.without_suspended.first
+  end
+end

app/services/activitypub/process_account_service.rb
@@ -39,6 +39,7 @@ class ActivityPub::ProcessAccountService < BaseService
 
     unless @options[:only_key] || @account.suspended?
       check_featured_collection! if @account.featured_collection_url.present?
+      check_featured_tags_collection! if @json['featuredTags'].present?
       check_links! unless @account.fields.empty?
     end
 
@@ -149,7 +150,11 @@ class ActivityPub::ProcessAccountService < BaseService
   end
 
   def check_featured_collection!
-    ActivityPub::SynchronizeFeaturedCollectionWorker.perform_async(@account.id)
+    ActivityPub::SynchronizeFeaturedCollectionWorker.perform_async(@account.id, { 'hashtag' => @json['featuredTags'].blank? })
+  end
+
+  def check_featured_tags_collection!
+    ActivityPub::SynchronizeFeaturedTagsCollectionWorker.perform_async(@account.id, @json['featuredTags'])
   end
 
   def check_links!

commit a3f946309dbff586aa584c5df665868f5460f495

Fix `FetchFeaturedCollectionService` spec
Regression from #19380

app/services/activitypub/fetch_featured_collection_service.rb
@@ -43,8 +43,7 @@ class ActivityPub::FetchFeaturedCollectionService < BaseService
 
   def process_note_items(items)
     status_ids = items.filter_map do |item|
-      type = item['type']
-      next unless type == 'Note'
+      next unless item.is_a?(String) || item['type'] == 'Note'
 
       uri = value_or_id(item)
       next if ActivityPub::TagManager.instance.local_uri?(uri)

spec/services/activitypub/fetch_featured_collection_service_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe ActivityPub::FetchFeaturedCollectionService, type: :service do
       stub_request(:get, 'https://example.com/account/pinned/3').to_return(status: 404)
       stub_request(:get, 'https://example.com/account/pinned/4').to_return(status: 200, body: Oj.dump(status_json_4))
 
-      subject.call(actor)
+      subject.call(actor, note: true, hashtag: false)
     end
 
     it 'sets expected posts as pinned posts' do