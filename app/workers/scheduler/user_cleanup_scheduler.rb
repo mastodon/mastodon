e546a53750ab567a9296736872e6b7814f0579f6
Claire <claire.github-309c@sitedethib.com>
2022-11-01 11:47:08 +0100

    Fix UserCleanupScheduler crash when an unconfirmed account has a moderation note
    Fixes #19109

--- app/workers/scheduler/user_cleanup_scheduler.rb
+++ app/workers/scheduler/user_cleanup_scheduler.rb
@@ -15,6 +15,8 @@ class Scheduler::UserCleanupScheduler
 
   def clean_unconfirmed_accounts!
     User.where('confirmed_at is NULL AND confirmation_sent_at <= ?', 2.days.ago).reorder(nil).find_in_batches do |batch|
+      # We have to do it separately because of missing database constraints
+      AccountModerationNote.where(account_id: batch.map(&:account_id)).delete_all
       Account.where(id: batch.map(&:account_id)).delete_all
       User.where(id: batch.map(&:id)).delete_all
     end
