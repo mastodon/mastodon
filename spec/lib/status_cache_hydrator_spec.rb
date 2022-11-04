1143ec201e1655a139f746b27e4b9ece9e65e1db
Claire <claire.github-309c@sitedethib.com>
2022-11-04 19:20:14 +0100

    Fix various issues with store hydration
    - Improve tests
    - Fix possible crash when application of a reblogged post isn't set
    - Fix discrepancies around favourited and reblogged attributes
    - Fix discrepancies around pinned attribute
    - Fix polls not being hydrated

--- spec/lib/status_cache_hydrator_spec.rb
+++ spec/lib/status_cache_hydrator_spec.rb
@@ -7,48 +7,105 @@ describe StatusCacheHydrator do
   let(:account) { Fabricate(:account) }
 
   describe '#hydrate' do
-    subject { described_class.new(status).hydrate(account.id) }
-
     let(:compare_to_hash) { InlineRenderer.render(status, account, :status) }
 
-    context 'when cache is warm' do
-      before do
-        Rails.cache.write("fan-out/#{status.id}", InlineRenderer.render(status, nil, :status))
+    shared_examples 'shared behavior' do
+      context 'when handling a new status' do
+        it 'renders the same attributes as a full render' do
+          expect(subject).to include(compare_to_hash)
+        end
       end
 
-      it 'renders the same attributes as a full render' do
-        expect(subject).to include(compare_to_hash)
+      context 'when handling a reblog' do
+        let(:reblog) { Fabricate(:status) }
+        let(:status) { Fabricate(:status, reblog: reblog) }
+
+        context 'that has been favourited' do
+          before do
+            FavouriteService.new.call(account, reblog)
+          end
+
+          it 'renders the same attributes as a full render' do
+            expect(subject).to include(compare_to_hash)
+          end
+        end
+
+        context 'that has been reblogged' do
+          before do
+            ReblogService.new.call(account, reblog)
+          end
+
+          it 'renders the same attributes as a full render' do
+            expect(subject).to include(compare_to_hash)
+          end
+        end
+
+        context 'that has been pinned' do
+          let(:reblog) { Fabricate(:status, account: account) }
+
+          before do
+            StatusPin.create!(account: account, status: reblog)
+          end
+
+          it 'renders the same attributes as a full render' do
+            expect(subject).to include(compare_to_hash)
+          end
+        end
+
+        context 'that has been followed tags' do
+          let(:followed_tag) { Fabricate(:tag) }
+
+          before do
+            reblog.tags << Fabricate(:tag)
+            reblog.tags << followed_tag
+            TagFollow.create!(tag: followed_tag, account: account, rate_limit: false)
+          end
+
+          it 'renders the same attributes as a full render' do
+            expect(subject).to include(compare_to_hash)
+          end
+        end
+
+        context 'that has a poll authored by the user' do
+          let(:poll) { Fabricate(:poll, account: account) }
+          let(:reblog) { Fabricate(:status, poll: poll, account: account) }
+
+          it 'renders the same attributes as a full render' do
+            expect(subject).to include(compare_to_hash)
+          end
+        end
+
+        context 'that has been voted in' do
+          let(:poll) { Fabricate(:poll, options: %w(Yellow Blue)) }
+          let(:reblog) { Fabricate(:status, poll: poll) }
+
+          before do
+            VoteService.new.call(account, poll, [0])
+          end
+
+          it 'renders the same attributes as a full render' do
+            expect(subject).to include(compare_to_hash)
+          end
+        end
       end
+    end
+
+    context 'when cache is warm' do
+      subject do
+        Rails.cache.write("fan-out/#{status.id}", InlineRenderer.render(status, nil, :status))
+        described_class.new(status).hydrate(account.id)
+      end
+
+      it_behaves_like 'shared behavior'
     end
 
     context 'when cache is cold' do
-      before do
+      subject do
         Rails.cache.delete("fan-out/#{status.id}")
+        described_class.new(status).hydrate(account.id)
       end
 
-      it 'renders the same attributes as a full render' do
-        expect(subject).to include(compare_to_hash)
-      end
-    end
-
-    context 'when account has favourited status' do
-      before do
-        FavouriteService.new.call(account, status)
-      end
-
-      it 'renders the same attributes as a full render' do
-        expect(subject).to include(compare_to_hash)
-      end
-    end
-
-    context 'when account has reblogged status' do
-      before do
-        ReblogService.new.call(account, status)
-      end
-
-      it 'renders the same attributes as a full render' do
-        expect(subject).to include(compare_to_hash)
-      end
+      it_behaves_like 'shared behavior'
     end
   end
 end
