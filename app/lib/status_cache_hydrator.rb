03b991de6c5874f3e7d1b6f6ff70b853b8daf639
Claire <claire.github-309c@sitedethib.com>
2022-11-04 19:33:16 +0100

    Fix various issues with store hydration (#19746)
    - Improve tests
    - Fix possible crash when application of a reblogged post isn't set
    - Fix discrepancies around favourited and reblogged attributes
    - Fix discrepancies around pinned attribute
    - Fix polls not being hydrated

--- app/lib/status_cache_hydrator.rb
+++ app/lib/status_cache_hydrator.rb
@@ -16,23 +16,35 @@ class StatusCacheHydrator
     # We take advantage of the fact that some relationships can only occur with an original status, not
     # the reblog that wraps it, so we can assume that some values are always false
     if payload[:reblog]
-      payload[:favourited] = false
-      payload[:reblogged]  = false
       payload[:muted]      = false
       payload[:bookmarked] = false
-      payload[:pinned]     = false
+      payload[:pinned]     = false if @status.account_id == account_id
       payload[:filtered]   = CustomFilter.apply_cached_filters(CustomFilter.cached_filters_for(@status.reblog_of_id), @status.reblog).map { |filter| ActiveModelSerializers::SerializableResource.new(filter, serializer: REST::FilterResultSerializer).as_json }
 
       # If the reblogged status is being delivered to the author who disabled the display of the application
       # used to create the status, we need to hydrate it here too
-      payload[:reblog][:application] = ActiveModelSerializers::SerializableResource.new(@status.reblog.application, serializer: REST::StatusSerializer::ApplicationSerializer).as_json if payload[:reblog][:application].nil? && @status.reblog.account_id == account_id
+      payload[:reblog][:application] = ActiveModelSerializers::SerializableResource.new(@status.reblog.application, serializer: REST::StatusSerializer::ApplicationSerializer).as_json if payload[:reblog][:application].nil? && @status.reblog.account_id == account_id && @status.reblog.application_id.present?
 
       payload[:reblog][:favourited] = Favourite.where(account_id: account_id, status_id: @status.reblog_of_id).exists?
       payload[:reblog][:reblogged]  = Status.where(account_id: account_id, reblog_of_id: @status.reblog_of_id).exists?
       payload[:reblog][:muted]      = ConversationMute.where(account_id: account_id, conversation_id: @status.reblog.conversation_id).exists?
       payload[:reblog][:bookmarked] = Bookmark.where(account_id: account_id, status_id: @status.reblog_of_id).exists?
-      payload[:reblog][:pinned]     = StatusPin.where(account_id: account_id, status_id: @status.reblog_of_id).exists?
+      payload[:reblog][:pinned]     = StatusPin.where(account_id: account_id, status_id: @status.reblog_of_id).exists? if @status.reblog.account_id == account_id
       payload[:reblog][:filtered]   = payload[:filtered]
+
+      if payload[:reblog][:poll]
+        if @status.reblog.account_id == account_id
+          payload[:reblog][:poll][:voted] = true
+          payload[:reblog][:poll][:own_votes] = []
+        else
+          own_votes = @status.reblog.poll.votes.where(account_id: account_id).pluck(:choice)
+          payload[:reblog][:poll][:voted] = !own_votes.empty?
+          payload[:reblog][:poll][:own_votes] = own_votes
+        end
+      end
+
+      payload[:favourited] = payload[:reblog][:favourited]
+      payload[:reblogged]  = payload[:reblog][:reblogged]
     else
       payload[:favourited] = Favourite.where(account_id: account_id, status_id: @status.id).exists?
       payload[:reblogged]  = Status.where(account_id: account_id, reblog_of_id: @status.id).exists?
