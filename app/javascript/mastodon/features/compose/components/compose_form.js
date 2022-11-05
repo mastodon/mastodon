mastodon/mastodon

commit 0165449e3a062238511489a8b23f3facd98258b6

A11y: Explicit <form> element around compose area (#19742)

app/javascript/mastodon/features/compose/components/compose_form.js
@@ -217,7 +217,7 @@ class ComposeForm extends ImmutablePureComponent {
     }
 
     return (
-      <div className='compose-form'>
+      <form className='compose-form'>
         <WarningContainer />
 
         <ReplyIndicatorContainer />
@@ -279,10 +279,16 @@ class ComposeForm extends ImmutablePureComponent {
 
         <div className='compose-form__publish'>
           <div className='compose-form__publish-button-wrapper'>
-            <Button text={publishText} onClick={this.handleSubmit} disabled={!this.canSubmit()} block />
+            <Button
+              type="submit"
+              text={publishText}
+              onClick={this.handleSubmit}
+              disabled={!this.canSubmit()}
+              block
+            />
           </div>
         </div>
-      </div>
+      </form>
     );
   }
 

app/javascript/mastodon/components/button.js
@@ -6,6 +6,7 @@ export default class Button extends React.PureComponent {
 
   static propTypes = {
     text: PropTypes.node,
+    type: PropTypes.string,
     onClick: PropTypes.func,
     disabled: PropTypes.bool,
     block: PropTypes.bool,
@@ -42,6 +43,7 @@ export default class Button extends React.PureComponent {
         onClick={this.handleClick}
         ref={this.setRef}
         title={this.props.title}
+        type={this.props.type}
       >
         {this.props.text || this.props.children}
       </button>

commit 4a1e4364c29b0b7604f9115a677bba05d08e2444

Fix compose form submission reloading web interface
Fix regression introduced by #19742

app/javascript/mastodon/features/compose/components/compose_form.js
@@ -93,7 +93,7 @@ class ComposeForm extends ImmutablePureComponent {
     return !(isSubmitting || isUploading || isChangingUpload || length(fulltext) > 500 || (isOnlyWhitespace && !anyMedia));
   }
 
-  handleSubmit = () => {
+  handleSubmit = (e) => {
     if (this.props.text !== this.autosuggestTextarea.textarea.value) {
       // Something changed the text inside the textarea (e.g. browser extensions like Grammarly)
       // Update the state to match the current text
@@ -105,6 +105,10 @@ class ComposeForm extends ImmutablePureComponent {
     }
 
     this.props.onSubmit(this.context.router ? this.context.router.history : null);
+
+    if (e) {
+      e.preventDefault();
+    }
   }
 
   onSuggestionsClearRequested = () => {
@@ -217,7 +221,7 @@ class ComposeForm extends ImmutablePureComponent {
     }
 
     return (
-      <form className='compose-form'>
+      <form className='compose-form' onSubmit={this.handleSubmit}>
         <WarningContainer />
 
         <ReplyIndicatorContainer />
@@ -282,7 +286,6 @@ class ComposeForm extends ImmutablePureComponent {
             <Button
               type="submit"
               text={publishText}
-              onClick={this.handleSubmit}
               disabled={!this.canSubmit()}
               block
             />