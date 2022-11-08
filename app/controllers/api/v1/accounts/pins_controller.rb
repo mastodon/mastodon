80e63402201d76d505aad86d64024ba730efcbd6
trwnh <a@trwnh.com>
2022-11-08 05:44:03 -0600

    Make account endorsements idempotent (fix #19045)

--- app/controllers/api/v1/accounts/pins_controller.rb
+++ app/controllers/api/v1/accounts/pins_controller.rb
@@ -8,7 +8,10 @@ class Api::V1::Accounts::PinsController < Api::BaseController
   before_action :set_account
 
   def create
-    AccountPin.create!(account: current_account, target_account: @account)
+    pin = AccountPin.find_by(account: current_account, target_account: @account)
+    if pin.nil?
+      AccountPin.create!(account: current_account, target_account: @account)
+    end
     render json: @account, serializer: REST::RelationshipSerializer, relationships: relationships_presenter
   end
 
