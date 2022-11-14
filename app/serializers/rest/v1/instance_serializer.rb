c05e445f032d68472cc5491d2538483c8913aa98
Tslmuun <mglalimusaa@gmail.com>
2022-11-14 10:56:19 +0800

    app 里支持上传9张图

--- app/serializers/rest/v1/instance_serializer.rb
+++ app/serializers/rest/v1/instance_serializer.rb
@@ -64,7 +64,7 @@ class REST::V1::InstanceSerializer < ActiveModel::Serializer
 
       statuses: {
         max_characters: StatusLengthValidator::MAX_CHARS,
-        max_media_attachments: 4,
+        max_media_attachments: 9,
         characters_reserved_per_url: StatusLengthValidator::URL_PLACEHOLDER_CHARS,
       },
 
