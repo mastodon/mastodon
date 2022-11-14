2ee3067cbf818841650cc7172f5176d68a311589
Kaspar Vollenweider <vok@panter.ch>
2022-11-14 15:30:36 +0100

    feat(rswag): add two demo specs for docs output

---
+++ spec/requests/api/v1/accounts/statuses_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+require 'swagger_helper'
+
+RSpec.describe Api::V1::Accounts::StatusesController, type: :request do
+  let(:account)        { Fabricate(:account, username: 'bob', domain: 'example.com') }
+  let(:other_account)        { Fabricate(:account, username: 'jane', domain: 'example.com') }
+  let!(:status)         { Fabricate(:status, account: account, edited_at: Time.zone.now, language: 'en') }
+  let!(:mention) { Fabricate(:mention, account: other_account, status: status) }
+  let(:private_status) { Fabricate(:status, account: account, visibility: :private) }
+  let!(:pin)           { Fabricate(:status_pin, account: account, status: status) }
+  let!(:private_pin)   { Fabricate(:status_pin, account: account, status: private_status) }
+
+  path '/api/v1/accounts/{account_id}/statuses' do
+    # You'll want to customize the parameter types...
+    parameter name: 'account_id', in: :path, type: :string, description: 'account_id'
+
+    get('list statuses') do
+      tags 'Api', 'V1', 'Accounts', 'Statuses'
+      operationId 'v1AccountsStatusesListStatus'
+      rswag_bearer_auth
+
+      include_context 'user token auth' do
+        let(:user_token_scopes) { 'read:statuses' }
+      end
+
+      response(200, 'successful') do
+        schema type: :array, items: { '$ref': '#/components/schemas/Status' }
+        let(:account_id) { account.id }
+
+        rswag_add_examples!
+        run_test!
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/reports_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+require 'swagger_helper'
+
+RSpec.describe Api::V1::ReportsController, type: :request do
+  let!(:status) { Fabricate(:status) }
+  let(:target_account) { status.account }
+  let(:category) { nil }
+  let(:forward) { nil }
+  let(:rule_ids) { nil }
+
+  path '/api/v1/reports' do
+    post('create report') do
+      tags 'Api', 'V1', 'Reports'
+      operationId 'v1ReportsCreateReport'
+      rswag_bearer_auth
+      parameter name: :payload, in: :body, type: :object, schema: {
+        type: :object,
+        properties: {
+          status_ids: { type: :array, items: { type: :integer } },
+          account_id: { type: :integer },
+          comment: { type: :string },
+          category: { type: :string, nullable: true },
+          rule_ids: { type: :array, items: { type: :string }, nullable: true },
+          forward: { type: :boolean, nullable: true },
+        },
+        required: %w(status_ids account_id),
+      }
+
+      include_context 'admin token auth' do
+        let(:admin_token_scopes) { 'write:reports' }
+      end
+
+      response(200, 'successful') do
+        schema type: :object, '$ref' => '#/components/schemas/Report'
+        let(:payload) do
+          {
+            status_ids: [status.id],
+            account_id: target_account.id,
+            comment: 'reasons',
+            category: category,
+            rule_ids: rule_ids,
+            forward: forward,
+          }
+        end
+
+        rswag_add_examples!(:successful)
+        run_test!
+      end
+    end
+  end
+end
