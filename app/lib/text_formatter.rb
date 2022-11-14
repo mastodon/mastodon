535030e328c38fa24df946b1e000950dd5190cd7
lindwurm <lindwurm.q@gmail.com>
2022-11-14 20:09:05 +0900

    text_formatter: increase display_url length to 64
    * and display prefix (https, www)
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- app/lib/text_formatter.rb
+++ app/lib/text_formatter.rb
@@ -74,12 +74,12 @@ class TextFormatter
     rel = with_rel_me? ? (DEFAULT_REL + %w(me)) : DEFAULT_REL
 
     prefix      = url.match(URL_PREFIX_REGEX).to_s
-    display_url = url[prefix.length, 30]
-    suffix      = url[prefix.length + 30..-1]
-    cutoff      = url[prefix.length..-1].length > 30
+    display_url = url[prefix.length, 64]
+    suffix      = url[prefix.length + 64..-1]
+    cutoff      = url[prefix.length..-1].length > 64
 
     <<~HTML.squish
-      <a href="#{h(url)}" target="_blank" rel="#{rel.join(' ')}"><span class="invisible">#{h(prefix)}</span><span class="#{cutoff ? 'ellipsis' : ''}">#{h(display_url)}</span><span class="invisible">#{h(suffix)}</span></a>
+      <a href="#{h(url)}" target="_blank" rel="#{rel.join(' ')}"><span class="#{cutoff ? 'ellipsis' : ''}">#{h(prefix)}#{h(display_url)}</span><span class="invisible">#{h(suffix)}</span></a>
     HTML
   rescue Addressable::URI::InvalidURIError, IDN::Idna::IdnaError
     h(entity[:url])
