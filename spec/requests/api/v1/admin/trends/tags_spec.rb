7d005c004c7dc846324419de251d2c35552c21f0
Kaspar Vollenweider <vok@panter.ch>
2022-11-19 15:01:05 +0100

    test(rswag): partially document api/v1/admin/trends/tags

--- spec/requests/api/v1/admin/trends/tags_spec.rb
+++ spec/requests/api/v1/admin/trends/tags_spec.rb
@@ -6,13 +6,27 @@ RSpec.describe Api::V1::Admin::Trends::TagsController, type: :request do
     get('list tags') do
       tags 'Api', 'V1', 'Admin', 'Trends', 'Tags'
       operationId 'v1AdminTrendsTagsListTag'
-      rswag_auth_scope
+      rswag_auth_scope %w(admin:read)
+      parameter name: 'limit', in: :query, type: :integer, required: false,
+                description: 'Maximum number of results to return. Defaults to 40.'
+      parameter name: 'max_id', in: :query, required: false, type: :string,
+                description: 'Internal parameter. Use HTTP Link header for pagination.'
+      parameter name: 'since_id', in: :query, required: false, type: :string,
+                description: 'Internal parameter. Use HTTP Link header for pagination.'
+      parameter name: 'offset', in: :query, required: false, type: :integer
 
-      include_context 'user token auth'
+      include_context 'admin token auth'
+
+      let(:account) { Fabricate(:account, trendable: true) }
+      let(:status) { Fabricate(:status, trendable: true, language: 'en', account: account) }
+      let!(:tag) { Fabricate(:tag, trendable: true, listable: true, usable: true, statuses: [status], accounts: [account]) }
 
       response(200, 'successful') do
+        schema type: :array, items: { '$ref' => '#/components/schemas/Tag' }
+        # TODO: convince this controller to actually produce examples. This Tag Trends thing is a bit of a tricky thing here... 
         rswag_add_examples!
         run_test!
+        # analyse_body_run_test!
       end
     end
   end
