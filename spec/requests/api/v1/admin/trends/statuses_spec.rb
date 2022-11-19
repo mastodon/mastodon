138640dabd214b6db8b2022c46937f8b5c048294
Kaspar Vollenweider <vok@panter.ch>
2022-11-19 14:46:13 +0100

    test(rswag): document api/v1/admin/trends/statuses

--- spec/requests/api/v1/admin/trends/statuses_spec.rb
+++ spec/requests/api/v1/admin/trends/statuses_spec.rb
@@ -6,11 +6,25 @@ RSpec.describe Api::V1::Admin::Trends::StatusesController, type: :request do
     get('list statuses') do
       tags 'Api', 'V1', 'Admin', 'Trends', 'Statuses'
       operationId 'v1AdminTrendsStatusesListStatus'
-      rswag_auth_scope
+      rswag_auth_scope %w(admin:read)
+      parameter name: 'limit', in: :query, type: :integer, required: false,
+                description: 'Maximum number of results to return. Defaults to 40.'
+      parameter name: 'max_id', in: :query, required: false, type: :string,
+                description: 'Internal parameter. Use HTTP Link header for pagination.'
+      parameter name: 'since_id', in: :query, required: false, type: :string,
+                description: 'Internal parameter. Use HTTP Link header for pagination.'
+      parameter name: 'offset', in: :query, required: false, type: :integer
 
-      include_context 'user token auth'
+      include_context 'admin token auth'
+
+      let(:account) { Fabricate(:account, trendable: true) }
+      let(:status) { Fabricate(:status, trendable: true, language: 'en') }
+      let!(:status_trend) do
+        StatusTrend.create!(status: status, account: account)
+      end
 
       response(200, 'successful') do
+        schema type: :array, items: { '$ref' => '#/components/schemas/Status' }
         rswag_add_examples!
         run_test!
       end
