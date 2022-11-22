3865efce87b9f429e4cd9c59fe1a0d66912100d1
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-22 15:37:27 +0100

    Update nodeinfo_controller_spec.rb

--- spec/controllers/well_known/nodeinfo_controller_spec.rb
+++ spec/controllers/well_known/nodeinfo_controller_spec.rb
@@ -1,13 +1,5 @@
 require 'rails_helper'
 
-RSpec::Matchers.define :match_response_schema do |schema|
-  match do |response|
-    schema_directory = "#{Dir.pwd}/spec/support/"
-    schema_path = "#{schema_directory}/#{schema}.json"
-    JSON::Validator.validate!(schema_path, response.body, strict: true)
-  end
-end
-
 describe WellKnown::NodeInfoController, type: :controller do
   render_views
 
