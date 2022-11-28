eaf1e847f729eb4e15487138ce97bc3053bc0a74
Kaspar Vollenweider <vok@panter.ch>
2022-11-29 00:11:50 +0100

    refactor(vacuum): backups
    - fix [namespace definition](https://github.com/rubocop/ruby-style-guide#namespace-definition)
    - inherit retention_period initialize from Vacuum::RetentionPeriod
    - vacuum_expired_backups!
      - in_batches(&:destroy_all) instead of in_batches.destroy_all,
        to ensure in_batches scope is not unwrapped - destroy_all realy
        runs in multiple batches, as expected

--- app/lib/vacuum/backups_vacuum.rb
+++ app/lib/vacuum/backups_vacuum.rb
@@ -1,25 +1,19 @@
 # frozen_string_literal: true
 
-class Vacuum::BackupsVacuum
-  def initialize(retention_period)
-    @retention_period = retention_period
-  end
+module Vacuum
+  class BackupsVacuum < Vacuum::RetentionPeriod
+    def perform
+      vacuum_expired_backups! if @retention_period.present?
+    end
 
-  def perform
-    vacuum_expired_backups! if retention_period?
-  end
+    private
 
-  private
+    def vacuum_expired_backups!
+      backups_past_retention_period.in_batches(&:destroy_all)
+    end
 
-  def vacuum_expired_backups!
-    backups_past_retention_period.in_batches.destroy_all
-  end
-
-  def backups_past_retention_period
-    Backup.unscoped.where(Backup.arel_table[:created_at].lt(@retention_period.ago))
-  end
-
-  def retention_period?
-    @retention_period.present?
+    def backups_past_retention_period
+      Backup.unscoped.where(created_at: ...@retention_period.ago)
+    end
   end
 end
