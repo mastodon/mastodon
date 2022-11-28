1a6674e68aafad039223b686ed09b3498cf1a05b
Kaspar Vollenweider <vok@panter.ch>
2022-11-29 00:18:39 +0100

    refactor(vacuum): feeds
    - fix [namespace definition](https://github.com/rubocop/ruby-style-guide#namespace-definition)
    - in_batches instead of find_in_batches - because it's users and lists
      items can just as well use the relition instead of maping the values
      from array
    - simplify methods - more explicit FeedManager.instance call in place -
      less obfuscation with no benefit

--- app/lib/vacuum/feeds_vacuum.rb
+++ app/lib/vacuum/feeds_vacuum.rb
@@ -1,34 +1,32 @@
 # frozen_string_literal: true
 
-class Vacuum::FeedsVacuum
-  def perform
-    vacuum_inactive_home_feeds!
-    vacuum_inactive_list_feeds!
-  end
-
-  private
-
-  def vacuum_inactive_home_feeds!
-    inactive_users.select(:id, :account_id).find_in_batches do |users|
-      feed_manager.clean_feeds!(:home, users.map(&:account_id))
+module Vacuum
+  class FeedsVacuum
+    def perform
+      vacuum_inactive_home_feeds!
+      vacuum_inactive_list_feeds!
     end
-  end
 
-  def vacuum_inactive_list_feeds!
-    inactive_users_lists.select(:id).find_in_batches do |lists|
-      feed_manager.clean_feeds!(:list, lists.map(&:id))
+    private
+
+    def vacuum_inactive_home_feeds!
+      inactive_users.select(:id, :account_id).in_batches do |users|
+        FeedManager.instance.clean_feeds!(:home, users.pluck(:account_id))
+      end
     end
-  end
 
-  def inactive_users
-    User.confirmed.inactive
-  end
+    def vacuum_inactive_list_feeds!
+      inactive_users_lists.select(:id).in_batches do |lists|
+        FeedManager.instance.clean_feeds!(:list, lists.ids)
+      end
+    end
 
-  def inactive_users_lists
-    List.where(account_id: inactive_users.select(:account_id))
-  end
+    def inactive_users
+      User.confirmed.inactive
+    end
 
-  def feed_manager
-    FeedManager.instance
+    def inactive_users_lists
+      List.where(account_id: inactive_users.select(:account_id))
+    end
   end
 end
