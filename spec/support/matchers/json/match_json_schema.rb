edd0c086ef4690c58c7a9fddfb02b6658c502c88
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-12-01 11:17:55 +0100

    use Rails.root.join to construct the path

--- spec/support/matchers/json/match_json_schema.rb
+++ spec/support/matchers/json/match_json_schema.rb
@@ -1,7 +1,6 @@
 RSpec::Matchers.define :match_json_schema do |schema|
   match do |input_json|
-    schema_directory = "#{Dir.pwd}/spec/support/schema/"
-    schema_path = "#{schema_directory}/#{schema}.json"
+    schema_path = Rails.root.join('spec', 'support', 'schema', "#{schema}.json").to_s
     JSON::Validator.validate(schema_path, input_json, validate_schema: true)
   end
 end
