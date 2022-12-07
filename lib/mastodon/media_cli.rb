a888fe18bd78618d5a6a2d21b38124a19a88e623
Evan <35814742+evanphilip@users.noreply.github.com>
2022-12-07 18:29:44 +0100

    Change table name 'user' to plural as per Ruby convention

--- lib/mastodon/media_cli.rb
+++ lib/mastodon/media_cli.rb
@@ -209,7 +209,7 @@ module Mastodon
       purged_accounts = Concurrent::Set[]
       processed, aggregate = parallelize_with_progress(
         Account.where({ last_webfingered_at: Time.zone.at(0)..time_ago,
-          updated_at: Time.zone.at(0)..time_ago }).left_outer_joins(:user).where(user: { id: nil })
+          updated_at: Time.zone.at(0)..time_ago }).left_outer_joins(:user).where(users: { id: nil })
       ) do |account|
         next if account.local?
         next if account.avatar.blank? && account.header.blank?
