fde91378202763e319078986870694a72f352bac
Claire <claire.github-309c@sitedethib.com>
2022-12-13 15:17:09 +0100

    Add AUTHORIZED_FETCH=actors to require signature only for actors

--- app/helpers/authorized_fetch_helper.rb
+++ app/helpers/authorized_fetch_helper.rb
@@ -2,7 +2,7 @@
 
 module AuthorizedFetchHelper
   def authorized_fetch_mode?
-    ENV.fetch('AUTHORIZED_FETCH') { Setting.authorized_fetch && 'true' } == 'true' || Rails.configuration.x.mastodon.limited_federation_mode
+    %(true all).include?(ENV.fetch('AUTHORIZED_FETCH') { Setting.authorized_fetch && 'true' }) || Rails.configuration.x.mastodon.limited_federation_mode
   end
 
   def authorized_fetch_overridden?
