a173ee8aa0213d5315b72a6c90c3978c9316a621
Avdi Grimm <avdi@avdi.codes>
2022-12-13 19:21:43 +0000

    Improve devcontainer for running tests
    - Pull devcontainer post-create out into its own script
    - Add asset precompilation
    - Add test-mode asset precompilation (needed to run tests without error)

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e # Fail the whole script on first error
+
+# Fetch Ruby gem dependencies
+bundle install --path vendor/bundle --with='development test'
+
+# Fetch Javascript dependencies
+yarn install
+
+git checkout -- Gemfile.lock
+
+# [re]create, migrate, and seed the test database
+RAILS_ENV=test ./bin/rails db:setup
+
+# Precompile assets for development
+RAILS_ENV=development ./bin/rails assets:precompile
+
+# Precompile assets for test
+RAILS_ENV=test NODE_ENV=tests ./bin/rails assets:precompile
