f29663e1717466a9af31c3f027ac82ccb9e64134
Holden Foreman <hsforeman98@gmail.com>
2022-12-22 12:39:47 -0500

    Change hard-coded strings to translatable strings in post character count sr-only text

--- app/javascript/mastodon/features/compose/components/character_counter.js
+++ app/javascript/mastodon/features/compose/components/character_counter.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { defineMessages, injectIntl } from 'react-intl';
 import { length } from 'stringz';
 
-export default class CharacterCounter extends React.PureComponent {
+const messages = defineMessages({
+  remaining: { id: 'character_counter.remaining', defaultMessage: 'character remaining' },
+  remaining_plural: { id: 'character_counter.remaining_plural', defaultMessage: 'characters remaining' },
+  exceeded: { id: 'character_counter.exceeded', defaultMessage: 'You have exceeded the character limit by' },
+});
+
+export default @injectIntl
+class CharacterCounter extends React.PureComponent {
 
   static propTypes = {
     text: PropTypes.string.isRequired,
     max: PropTypes.number.isRequired,
+    intl: PropTypes.object.isRequired,
   };
 
-  checkRemainingText(diff) {
+  checkRemainingText(diff, intl) {
     if (diff < 0) {
       return (
         <>
@@ -17,7 +26,7 @@ export default class CharacterCounter extends React.PureComponent {
             {diff}
           </span>
           <span className='sr-only'>
-            {`You have exceeded the character limit by ${diff * -1}`}
+            {`${intl.formatMessage(messages.exceeded)} ${diff * -1}`}
           </span>
         </>
       );
@@ -29,15 +38,16 @@ export default class CharacterCounter extends React.PureComponent {
           {diff}
         </span>
         <span className='sr-only'>
-          {`${diff} character${diff === 1 ? '' : 's'} remaining`}
+          {`${diff} ${diff === 1 ? intl.formatMessage(messages.remaining) : intl.formatMessage(messages.remaining_plural)}`}
         </span>
       </>
     );
   }
 
   render() {
+    const { intl } = this.props;
     const diff = this.props.max - length(this.props.text);
-    return this.checkRemainingText(diff);
+    return this.checkRemainingText(diff, intl);
   }
 
 }
