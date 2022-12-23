09d93720b24a6dcb5387d60acb439ee2d4a66413
Jed Fox <git@jedfox.com>
2022-12-23 15:55:13 -0500

    Format API responses when viewed directly in a browser

--- lib/mastodon/rack_middleware.rb
+++ lib/mastodon/rack_middleware.rb
@@ -6,12 +6,24 @@ class Mastodon::RackMiddleware
   end
 
   def call(env)
-    @app.call(env)
+    status, headers, response = @app.call(env)
+    response = format_json(env, headers, response)
+    [status, headers, response]
   ensure
     clean_up_sockets!
   end
 
   private
+
+  # If the request is expecting HTML but the response is JSON,
+  # pretty-print the json so it can be more easily read
+  def format_json env, headers, response
+    if headers['Content-Type']&.include?('application/json') && env['HTTP_ACCEPT']&.include?('text/html')
+      [JSON.pretty_generate(JSON.parse(response.body))]
+    else
+      response
+    end
+  end
 
   def clean_up_sockets!
     clean_up_redis_socket!
