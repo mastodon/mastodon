380ad076bfb5b3922f1a93bda2d6e7187fdb1dcf
S-H-GAMELINKS <gamelinks007@gmail.com>
2022-12-24 18:13:53 +0900

    Resize local custom emoji

--- app/javascript/mastodon/actions/importer/normalizer.js
+++ app/javascript/mastodon/actions/importer/normalizer.js
@@ -1,5 +1,5 @@
 import escapeTextContentForBrowser from 'escape-html';
-import emojify from '../../features/emoji/emoji';
+import emojify, { emojifyStatus } from '../../features/emoji/emoji';
 import { unescapeHTML } from '../../utils/html';
 import { expandSpoilers } from '../../initial_state';
 
@@ -87,8 +87,11 @@ export function normalizeStatus(status, normalOldStatus) {
     const searchContent = ([spoilerText, status.content].concat((status.poll && status.poll.options) ? status.poll.options.map(option => option.title) : [])).concat(status.media_attachments.map(att => att.description)).join('\n\n').replace(/<br\s*\/?>/g, '\n').replace(/<\/p><p>/g, '\n\n');
     const emojiMap      = makeEmojiMap(normalStatus);
 
+    const uri = status.uri;
+    const isLocalCustomEmoji = uri.match(/gamelinks007\.net/) !== null;
+
     normalStatus.search_index = domParser.parseFromString(searchContent, 'text/html').documentElement.textContent;
-    normalStatus.contentHtml  = emojify(normalStatus.content, emojiMap);
+    normalStatus.contentHtml  = emojifyStatus(isLocalCustomEmoji, normalStatus.content, emojiMap);
     normalStatus.spoilerHtml  = emojify(escapeTextContentForBrowser(spoilerText), emojiMap);
     normalStatus.hidden       = expandSpoilers ? false : spoilerText.length > 0 || normalStatus.sensitive;
   }
