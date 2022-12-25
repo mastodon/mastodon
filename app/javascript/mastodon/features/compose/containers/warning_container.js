89b973a5571e45868e19e456278492b0b4aa3caa
S-H-GAMELINKS <gamelinks007@gmail.com>
2022-12-25 21:30:37 +0900

    Add nyan visibility warning

--- app/javascript/mastodon/features/compose/containers/warning_container.js
+++ app/javascript/mastodon/features/compose/containers/warning_container.js
@@ -33,10 +33,15 @@ const APPROX_HASHTAG_RE = buildHashtagRE();
 const mapStateToProps = state => ({
   needsLockWarning: state.getIn(['compose', 'privacy']) === 'private' && !state.getIn(['accounts', me, 'locked']),
   hashtagWarning: state.getIn(['compose', 'privacy']) !== 'public' && APPROX_HASHTAG_RE.test(state.getIn(['compose', 'text'])),
+  nyanWarning: state.getIn(['compose', 'privacy']) === 'nyan',
   directMessageWarning: state.getIn(['compose', 'privacy']) === 'direct',
 });
 
-const WarningWrapper = ({ needsLockWarning, hashtagWarning, directMessageWarning }) => {
+const WarningWrapper = ({ needsLockWarning, hashtagWarning, nyanWarning, directMessageWarning }) => {
+  if (nyanWarning) {
+    return <Warning message={<FormattedMessage id='compose_form.nyan_warning' defaultMessage='公開範囲が「にゃーん」になっています。ここで入力した内容はすべて「にゃーん」に置き換えられます' />} />;
+  }
+
   if (needsLockWarning) {
     return <Warning message={<FormattedMessage id='compose_form.lock_disclaimer' defaultMessage='Your account is not {locked}. Anyone can follow you to view your follower-only posts.' values={{ locked: <a href='/settings/profile'><FormattedMessage id='compose_form.lock_disclaimer.lock' defaultMessage='locked' /></a> }} />} />;
   }
