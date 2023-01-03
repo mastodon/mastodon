c88d2835fb52e0421c64839dc8cf12711d43c8de
Darius Kazemi <darius@meedan.com>
2023-01-03 09:39:31 -0800

    Add sign-in button to mobile view when logged out

--- app/javascript/mastodon/components/column_header.js
+++ app/javascript/mastodon/components/column_header.js
@@ -105,7 +105,7 @@ class ColumnHeader extends React.PureComponent {
       'active': !collapsed,
     });
 
-    let extraContent, pinButton, moveButtons, backButton, collapseButton;
+    let extraContent, pinButton, moveButtons, backButton, collapseButton, signInButton;
 
     if (children) {
       extraContent = (
@@ -146,6 +146,19 @@ class ColumnHeader extends React.PureComponent {
       collapsedContent.push(moveButtons);
     }
 
+    if (this.context.identity.signedIn) {
+      signInButton = (
+        <>
+        </>
+      );
+    } else {
+      signInButton = (
+        <div class="ui__header__links">
+          <a href='/auth/sign_in' className='button'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Sign in' /></a>
+        </div>
+      );
+    }
+
     if (this.context.identity.signedIn && (children || (multiColumn && this.props.onPin))) {
       collapseButton = (
         <button
@@ -180,6 +193,7 @@ class ColumnHeader extends React.PureComponent {
             {hasTitle && backButton}
             {extraButton}
             {collapseButton}
+            {signInButton}
           </div>
         </h1>
 
