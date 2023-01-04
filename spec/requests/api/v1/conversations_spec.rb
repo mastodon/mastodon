376c7fad100cee80fedc9621689cf8d7ca7ff68f
Kaspar Vollenweider <vok@panter.ch>
2023-01-04 15:35:56 +0100

    docs(api): /api/v1/conversations/*

--- spec/requests/api/v1/conversations_spec.rb
+++ spec/requests/api/v1/conversations_spec.rb
@@ -2,6 +2,12 @@
 require 'swagger_helper'
 
 RSpec.describe Api::V1::ConversationsController, type: :request do
+  let!(:other) { Fabricate(:user).account }
+  let!(:conversation_status) do
+    PostStatusService.new.call(other, text: "Hello @#{user.account.acct}", visibility: 'direct')
+  end
+  let!(:account_conversation) { AccountConversation.where(account: user.account).last }
+
   path '/api/v1/conversations/{id}/read' do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
@@ -10,12 +16,57 @@ RSpec.describe Api::V1::ConversationsController, type: :request do
       tags 'Api', 'V1', 'Conversations'
       operationId 'v1ConversationsReadConversation'
       rswag_auth_scope %w(write write:conversations)
+      description <<~MD
+        **Mark a conversation as read**
+        
+        Direct conversations with other participants.  
+        *Currently, just threads containing a post with "direct" visibility.*
 
-      include_context 'user token auth'
+      MD
 
       response(200, 'successful') do
-        let(:id) { '123' }
+        schema '$ref' => '#/components/schemas/Conversation'
+        let(:id) { account_conversation.id }
+        include_context 'user token auth'
 
+        rswag_add_examples!
+        run_test! do |response|
+          body = rswag_parse_body_sym(response)
+          expect(body).to include({
+            id: account_conversation.id.to_s,
+            accounts: [
+              include({
+                id: other.id.to_s,
+              }),
+            ],
+            last_status: include({
+              id: account_conversation.last_status_id.to_s,
+              account: include({ id: other.id.to_s }),
+              mentions: match_array(
+                [
+                  include({ id: user.account.id.to_s }),
+                ]
+              ),
+            }),
+            unread: false,
+          })
+        end
+      end
+
+      response(403, 'Outside token scope') do
+        schema '$ref' => '#/components/schemas/Error'
+        let(:id) { account_conversation.id }
+        include_context 'user token auth' do
+          let(:user_token_scopes) { 'read:bookmarks' }
+        end
+        rswag_add_examples!
+        run_test!
+      end
+
+      response(404, 'Not found') do
+        schema '$ref' => '#/components/schemas/Error'
+        let(:id) { 0 }
+        include_context 'user token auth'
         rswag_add_examples!
         run_test!
       end
@@ -26,11 +77,58 @@ RSpec.describe Api::V1::ConversationsController, type: :request do
     get('list conversations') do
       tags 'Api', 'V1', 'Conversations'
       operationId 'v1ConversationsListConversation'
-      rswag_auth_scope %w(read read:statuses)
+      description <<~MD
+        **List all conversations for current user**
 
+        Direct conversations with other participants.  
+        *Currently, just threads containing a post with "direct" visibility.*
+
+      MD
+      rswag_auth_scope %w(read read:statuses)
+      rswag_page_params limit_desc: <<~MD
+        Maximum number of results to return.  
+        Defaults to 20 conversations.  
+        Max 40 conversations.
+      MD
       include_context 'user token auth'
 
       response(200, 'successful') do
+        schema type: :array, items: { '$ref' => '#/components/schemas/Conversation' }
+        rswag_add_examples!
+        run_test! do |response|
+          body = rswag_parse_body_sym(response)
+          expect(body).to have_attributes(size: 1).and(
+            match_array(
+              [
+                include({
+                  id: account_conversation.id.to_s,
+                  accounts: [
+                    include({
+                      id: other.id.to_s,
+                    }),
+                  ],
+                  last_status: include({
+                    id: account_conversation.last_status_id.to_s,
+                    account: include({ id: other.id.to_s }),
+                    mentions: match_array(
+                      [
+                        include({ id: user.account.id.to_s }),
+                      ]
+                    ),
+                  }),
+                  unread: true,
+                }),
+              ]
+            )
+          )
+        end
+      end
+
+      response(403, 'Outside token scope') do
+        schema '$ref' => '#/components/schemas/Error'
+        include_context 'user token auth' do
+          let(:user_token_scopes) { 'read:bookmarks' }
+        end
         rswag_add_examples!
         run_test!
       end
@@ -38,31 +136,59 @@ RSpec.describe Api::V1::ConversationsController, type: :request do
   end
 
   path '/api/v1/conversations/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     delete('delete conversation') do
       tags 'Api', 'V1', 'Conversations'
       operationId 'v1ConversationsDeleteConversation'
+      description <<~MD
+        **Removes a conversation from your list of conversations**
+
+        Direct conversations with other participants.  
+        *Currently, just threads containing a post with "direct" visibility.*
+
+      MD
       rswag_auth_scope %w(write write:conversations)
 
-      include_context 'user token auth'
-      let(:account) { Fabricate(:account) }
-
-      before do
-        user.account.follow!(account)
-        account.follow!(user.account)
-
-        result = PostStatusService.new.call(account, text: 'Hey @alice', visibility: 'direct')
-      end
-
       response(200, 'successful') do
+        schema type: :object, properties: {}
         let(:id) { account_conversation.id }
+        include_context 'user token auth'
 
         rswag_add_examples!
-        run_test! do |response|
-          body = rswag_parse_body(response)
+        run_test! do
+          expect(AccountConversation.exists?(id: account_conversation.id)).to be(false)
         end
+      end
+
+      response(401, 'Unauthorized') do
+        schema '$ref' => '#/components/schemas/Error'
+        let(:id) { account_conversation.id }
+        include_context 'user token auth' do
+          let(:authorization) { 'Bearer token666invalid' }
+        end
+
+        rswag_add_examples!
+        run_test!
+      end
+
+      response(403, 'Outside token scope') do
+        schema '$ref' => '#/components/schemas/Error'
+        let(:id) { account_conversation.id }
+
+        include_context 'user token auth' do
+          let(:user_token_scopes) { 'read' }
+        end
+        rswag_add_examples!
+        run_test!
+      end
+
+      response(404, 'Not found') do
+        schema '$ref' => '#/components/schemas/Error'
+        let(:id) { 0 }
+        include_context 'user token auth'
+        rswag_add_examples!
+        run_test!
       end
     end
   end
--- spec/support/rswag_helper/extend.rb
+++ spec/support/rswag_helper/extend.rb
@@ -14,6 +14,34 @@ module RswagHelper
       rswag_json_endpoint
     end
 
+    # @param [Hash] opts the options
+    # @option opts [Boolean] :no_limit Do not add limit parameter
+    # @option opts [String] :limit_desc custom description for limit
+    # @option opts [Boolean] :no_max_id Do not add max_id parameter
+    # @option opts [String] :max_id_desc custom description for max_id
+    # @option opts [Boolean] :no_min_id Do not add min_id parameter
+    # @option opts [String] :min_id_desc custom description for min_id
+    # @option opts [Boolean] :no_since_id Do not add since_id parameter
+    # @option opts [String] :since_id_desc custom description for since_id
+    def rswag_page_params(opts = {})
+      unless opts.fetch(:no_limit, false)
+        parameter name: 'limit', in: :query, type: :integer, required: false,
+                  description: opts.fetch(:limit_desc, 'Maximum number of results to return.')
+      end
+      unless opts.fetch(:no_max_id, false)
+        parameter name: 'max_id', in: :query, required: false, type: :string,
+                  description: opts.fetch(:max_id_desc, '**Internal parameter**. Use HTTP Link header for pagination.')
+      end
+      unless opts.fetch(:no_min_id, false)
+        parameter name: 'min_id', in: :query, required: false, type: :string,
+                  description: opts.fetch(:min_id_desc, '**Internal parameter**. Use HTTP Link header for pagination.')
+      end
+      unless opts.fetch(:no_since_id, false)
+        parameter name: 'since_id', in: :query, required: false, type: :string,
+                  description: opts.fetch(:since_id_desc, '**Internal parameter**. Use HTTP Link header for pagination.')
+      end
+    end
+
     # Utility method, only for dev debug use
     def analyse_body_run_test!
       run_test! do |response|
