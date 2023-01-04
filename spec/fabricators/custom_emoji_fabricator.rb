1a32e3cf99f34f1c73d94bd029b8fb4cc406f537
Kaspar Vollenweider <vok@panter.ch>
2023-01-04 16:28:10 +0100

    docs(api): GET /api/v1/custom_emojis

--- spec/fabricators/custom_emoji_fabricator.rb
+++ spec/fabricators/custom_emoji_fabricator.rb
@@ -1,5 +1,7 @@
 Fabricator(:custom_emoji) do
-  shortcode 'coolcat'
-  domain    nil
-  image     { File.open(Rails.root.join('spec', 'fixtures', 'files', 'emojo.png')) }
+  shortcode         'coolcat'
+  domain            nil
+  disabled          false
+  visible_in_picker true
+  image             { Rails.root.join('spec/fixtures/files/emojo.png').open }
 end
--- spec/requests/api/v1/custom_emojis_spec.rb
+++ spec/requests/api/v1/custom_emojis_spec.rb
@@ -6,13 +6,42 @@ RSpec.describe Api::V1::CustomEmojisController do
     get('list custom_emojis') do
       tags 'Api', 'V1', 'CustomEmojis'
       operationId 'v1CustomemojisListCustomEmoji'
-      rswag_auth_scope
-
-      include_context 'user token auth'
+      description 'Returns custom emojis that are available on the server.'
+      rswag_json_endpoint
+      let!(:custom_emojis) do
+        [
+          Fabricate(:custom_emoji, shortcode: 'test_emoji1', category: CustomEmojiCategory.create(name: 'test_category1')),
+          Fabricate(:custom_emoji, shortcode: 'test_emoji2'),
+          Fabricate(:custom_emoji, shortcode: 'test_emoji3', disabled: true),
+          Fabricate(:custom_emoji, shortcode: 'test_emoji4', visible_in_picker: false),
+        ]
+      end
 
       response(200, 'successful') do
+        schema type: :array, items: { '$ref' => '#/components/schemas/CustomEmoji' }
         rswag_add_examples!
-        run_test!
+        run_test! do |response|
+          body = rswag_parse_body_sym(response)
+          expect(body).to have_attributes(size: 2).and(
+            match_array(
+              [
+                include({
+                  shortcode: 'test_emoji1',
+                  url: a_string_starting_with('http'),
+                  static_url: a_string_starting_with('http'),
+                  visible_in_picker: true,
+                  category: 'test_category1',
+                }),
+                include({
+                  shortcode: 'test_emoji2',
+                  url: a_string_starting_with('http'),
+                  static_url: a_string_starting_with('http'),
+                  visible_in_picker: true,
+                }),
+              ]
+            )
+          )
+        end
       end
     end
   end
