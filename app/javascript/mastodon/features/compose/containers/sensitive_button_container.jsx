abbcb39bf625dbb999bbabb35136086b189e5ea1
Claire <claire.github-309c@sitedethib.com>
2023-01-05 11:26:55 +0100

    Change compose form checkbox to native input with `appearance: none`

--- app/javascript/mastodon/features/compose/containers/sensitive_button_container.jsx
+++ app/javascript/mastodon/features/compose/containers/sensitive_button_container.jsx
@@ -54,8 +54,6 @@ class SensitiveButton extends React.PureComponent {
             disabled={disabled}
           />
 
-          <span className={classNames('checkbox', { active })} />
-
           <FormattedMessage
             id='compose_form.sensitive.hide'
             defaultMessage='{count, plural, one {Mark media as sensitive} other {Mark media as sensitive}}'
