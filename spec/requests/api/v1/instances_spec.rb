c50c2e1b2c7d12b2f2d8ff8e89d9b92335c86d5c
Kaspar Vollenweider <vok@panter.ch>
2023-01-05 02:33:59 +0100

    docs(api): /api/v1/instance

--- spec/requests/api/v1/instances_spec.rb
+++ spec/requests/api/v1/instances_spec.rb
@@ -4,13 +4,22 @@ require 'swagger_helper'
 RSpec.describe Api::V1::InstancesController do
   path '/api/v1/instance' do
     get('show instance') do
+      description <<~MD
+        Version history:
+        - 1.1.0 added
+        - 3.0.0 requires user token if instance is in whitelist mode
+        - 3.1.4 added invites_enabled to response
+        - 3.4.0 added rules
+        - 3.4.2 added configuration
+        - 4.0.0 deprecated. added configuration[accounts].
+      MD
       tags 'Api', 'V1', 'Instances'
       operationId 'v1InstancesShowInstance'
-      rswag_auth_scope
-
-      include_context 'user token auth'
+      deprecated true
+      rswag_json_endpoint
 
       response(200, 'successful') do
+        schema '$ref' => '#/components/schemas/V1Instance'
         rswag_add_examples!
         run_test!
       end
