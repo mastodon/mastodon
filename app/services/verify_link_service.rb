0c689b9d014324aba5b8751dacec4c0fc20b2038
Markus Unterwaditzer <markus@unterwaditzer.net>
2023-01-11 21:59:13 +0100

    fix: allow verification when page size exceeds 1MB (using HTML5 parser) (#22879)
    * fix: allow verification when page size exceeds 1MB
    Truncates the page after 1MB instead
    
    Closes #15316
    
    * switch to HTML5 parser, fix rubocop errors
    
    * undo rubocop fixes
    
    Co-authored-by: Chris Zubak-Skees <chriszs@gmail.com>

--- app/services/verify_link_service.rb
+++ app/services/verify_link_service.rb
@@ -26,7 +26,7 @@ class VerifyLinkService < BaseService
   def link_back_present?
     return false if @body.blank?
 
-    links = Nokogiri::HTML(@body).xpath('//a[contains(concat(" ", normalize-space(@rel), " "), " me ")]|//link[contains(concat(" ", normalize-space(@rel), " "), " me ")]')
+    links = Nokogiri::HTML5(@body).xpath('//a[contains(concat(" ", normalize-space(@rel), " "), " me ")]|//link[contains(concat(" ", normalize-space(@rel), " "), " me ")]')
 
     if links.any? { |link| link['href']&.downcase == @link_back.downcase }
       true
