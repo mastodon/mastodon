2b2f7059228092dfa21c9a41c5f19757c481fe7a
Dick Hardt <dick.hardt@hello.coop>
2023-01-10 17:52:27 -0800

    comment out super app setup

--- db/seeds/10_hello.rb
+++ db/seeds/10_hello.rb
@@ -1,23 +1,23 @@
-redirect_uri = 'https://wallet.hello.coop/oauth/response/mastodon/'
-if ENV['HELLO_MASTODON_BUILDER_REDIRECT_URI']
-  redirect_uri = ENV['HELLO_MASTODON_BUILDER_REDIRECT_URI']
-end
+# redirect_uri = 'https://wallet.hello.coop/oauth/response/mastodon/'
+# if ENV['HELLO_MASTODON_BUILDER_REDIRECT_URI']
+#   redirect_uri = ENV['HELLO_MASTODON_BUILDER_REDIRECT_URI']
+# end
 
-client_secret = nil
-if ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
-  client_secret = ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
-end
+# client_secret = nil
+# if ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
+#   client_secret = ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
+# end
 
-Doorkeeper::Application.create(
-  name: 'Hellō Mastodon Builder',
-  uid: 'hello-mastodon-builder',
-  secret: client_secret,
-  redirect_uri: redirect_uri,
-  scopes: 'read read:accounts read:follows read:statuses write write:accounts write:follows write:statuses',
-  website: 'https://hello.coop/',
-  owner_type: 'User',
-  owner_id: 1
-)
+# Doorkeeper::Application.create(
+#   name: 'Hellō Mastodon Builder',
+#   uid: 'hello-mastodon-builder',
+#   secret: client_secret,
+#   redirect_uri: redirect_uri,
+#   scopes: 'read read:accounts read:follows read:statuses write write:accounts write:follows write:statuses',
+#   website: 'https://hello.coop/',
+#   owner_type: 'User',
+#   owner_id: 1
+# )
 
 Rule.create(text: 'Be kind. Don''t be mean, nasty, abusive, sexist, racist, or offensive. No shitposting. No doxing. Engage in healthy debate if inclined.')
 Rule.create(text: 'Be legal. Don''t post material illigal in either the US, or where you are located. Abide by all applicable laws. We will cooperate with law enforcement when legally required to.')
