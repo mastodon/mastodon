6daca256d48a2197181761c2eb5cafee460ed8e3
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-01-11 14:08:16 -0800

    raise exception if HELLO_MASTODON_BUILDER_CLIENT_SECRET is not set

--- db/seeds/10_hello.rb
+++ db/seeds/10_hello.rb
@@ -6,6 +6,8 @@ end
 client_secret = nil
 if ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
   client_secret = ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
+else
+  raise "The HELLO_MASTODON_BUILDER_CLIENT_SECRET env var must be set"
 end
 
 Doorkeeper::Application.create(
