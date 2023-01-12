807ed15c8ded0c6ec11a402e79ca9a14075647e2
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-01-11 19:26:23 -0800

    disable raising exception for missing HELLO_MASTODON_BUILDER_CLIENT_SECRET

--- db/seeds/10_hello.rb
+++ db/seeds/10_hello.rb
@@ -7,7 +7,7 @@ client_secret = nil
 if ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
   client_secret = ENV['HELLO_MASTODON_BUILDER_CLIENT_SECRET']
 else
-  raise "The HELLO_MASTODON_BUILDER_CLIENT_SECRET env var must be set"
+  # raise "The HELLO_MASTODON_BUILDER_CLIENT_SECRET env var must be set"
 end
 
 Doorkeeper::Application.create(
