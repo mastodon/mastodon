398514bed84e001e454abe5a5ddd0a3a73fe9b7c
Marius Scurtescu <marius.scurtescu@gmail.com>
2023-01-19 14:06:20 -0800

    show intermediate page before redirecting to Mastodon Builder

--- app/controllers/hello/verified_controller.rb
+++ app/controllers/hello/verified_controller.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
-class Hello::VerifiedController < ActionController::Base
+class Hello::VerifiedController < ApplicationController
+  include WebAppControllerConcern
+
+  before_action :require_authenticated_user!
 
   def index
-    redirect_to Hello.mastodon_builder_url
+    @mastodon_builder_url = Hello.mastodon_builder_url
+    @account = current_account
+  end
+
+  def require_authenticated_user!
+    render json: { error: 'An authenticated user is required' }, status: 401 unless current_user
   end
 end
