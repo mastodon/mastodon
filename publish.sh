854b143c63f9dac1fe5843675bd768876e2bee6f
Dick Hardt <dick.hardt@hello.coop>
2023-01-21 11:03:19 -0800

    tag releases

--- publish.sh
+++ publish.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-docker login
 export HELLO_VERSION=$(./hello_version.sh) 
 echo HELLO_VERSION=${HELLO_VERSION}
+git tag ${HELLO_VERSION}
+git push
+git push --tags
+
+docker login
 docker build . -t hello-mastodon:latest -t hello-mastodon:${HELLO_VERSION} --build-arg HELLO_VERSION=${HELLO_VERSION}
 docker tag hello-mastodon:lastest docker.io/hellocoop/mastodon:${HELLO_VERSION}
 docker tag hello-mastodon:lastest docker.io/hellocoop/mastodon:latest
