e59709b4e5f27bb94e9e3826aeb34a2356630b66
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-01-24 22:07:58 +0900

    [Feature] Support defaul visibility to nyan and portfolio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -198,7 +198,7 @@ module ApplicationHelper
       text: [params[:title], params[:text], params[:url]].compact.join(' '),
     }
 
-    permit_visibilities = %w(public unlisted private direct)
+    permit_visibilities = %w(public portfolio unlisted private nyan direct)
     default_privacy     = current_account&.user&.setting_default_privacy
     permit_visibilities.shift(permit_visibilities.index(default_privacy) + 1) if default_privacy.present?
     state_params[:visibility] = params[:visibility] if permit_visibilities.include? params[:visibility]
