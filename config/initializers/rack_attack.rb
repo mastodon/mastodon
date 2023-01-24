177032c897db04e1f36d85c99c0765bf7f46699f
Byron Miller <mtheory@live.com>
2023-01-24 07:43:05 -0600

    Update rack_attack.rb
    Increase rate limit to avoid 429 errors

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -62,7 +62,7 @@ class Rack::Attack
     IpBlock.blocked?(req.remote_ip)
   end
 
-  throttle('throttle_authenticated_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_authenticated_api', limit: 900, period: 5.minutes) do |req|
     req.authenticated_user_id if req.api_request?
   end
 
@@ -82,7 +82,7 @@ class Rack::Attack
     req.throttleable_remote_ip if req.post? && req.path == '/api/v1/accounts'
   end
 
-  throttle('throttle_authenticated_paging', limit: 300, period: 15.minutes) do |req|
+  throttle('throttle_authenticated_paging', limit: 900, period: 15.minutes) do |req|
     req.authenticated_user_id if req.paging_request?
   end
 
