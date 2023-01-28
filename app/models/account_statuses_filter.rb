dda45809fb63e73e4a43b5c9cdb262608ab1b87d
Jeong Arm <kjwonmail@gmail.com>
2023-01-28 09:27:49 +0900

    Fix accounts feed including reblogs of blocked domains

--- app/models/account_statuses_filter.rb
+++ app/models/account_statuses_filter.rb
@@ -55,7 +55,14 @@ class AccountStatusesFilter
   end
 
   def filtered_reblogs_scope
-    Status.left_outer_joins(:reblog).where(reblog_of_id: nil).or(Status.where.not(reblogs_statuses: { account_id: current_account.excluded_from_timeline_account_ids }))
+    Status
+      .left_outer_joins(:reblog).where(reblog_of_id: nil)
+      .joins('LEFT OUTER JOIN accounts reblogs_accounts ON reblogs_accounts.id = reblogs_statuses.account_id')
+      .or(
+        Status
+          .where.not(reblogs_statuses: { account_id: current_account.excluded_from_timeline_account_ids })
+          .where.not(reblogs_accounts: { domain: current_account.excluded_from_timeline_domains })
+      )
   end
 
   def only_media_scope
