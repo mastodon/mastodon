fbb50145a568d1ee885d4a44b928ea4a53c033d4
Claire <claire.github-309c@sitedethib.com>
2023-02-01 21:34:02 +0100

    Add tests

---
+++ spec/services/backup_service_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BackupService, type: :service do
+  subject(:service_call) { described_class.new.call(backup) }
+
+  let!(:user)           { Fabricate(:user) }
+  let!(:attachment)     { Fabricate(:media_attachment, account: user.account) }
+  let!(:status)         { Fabricate(:status, account: user.account, text: 'Hello', visibility: :public, media_attachments: [attachment]) }
+  let!(:private_status) { Fabricate(:status, account: user.account, text: 'secret', visibility: :private) }
+  let!(:favourite)      { Fabricate(:favourite, account: user.account) }
+  let!(:backup)         { Fabricate(:backup, user: user) }
+
+  def read_tgz_file(backup, filename)
+    file = Paperclip.io_adapters.for(backup.dump)
+    Zlib::GzipReader.wrap(file) do |gz|
+      Gem::Package::TarReader.new(gz) do |tar|
+        tar.each do |entry|
+          next unless entry.full_name == filename
+          return entry.read
+        end
+      end
+    end
+  end
+
+  it 'marks the backup as processed' do
+    expect { service_call }.to change(backup, :processed).from(false).to(true)
+  end
+
+  it 'exports outbox.json as expected' do
+    service_call
+
+    json = Oj.load(read_tgz_file(backup, 'outbox.json'))
+    expect(json['type']).to eq 'OrderedCollection'
+    expect(json['totalItems']).to eq 2
+    expect(json['orderedItems'][0]).to include({
+      'type' => 'Create',
+      'object' => include({
+        'id' => ActivityPub::TagManager.instance.uri_for(status),
+        'content' => '<p>Hello</p>',
+      }),
+    })
+    expect(json['orderedItems'][1]).to include({
+      'type' => 'Create',
+      'object' => include({
+        'id' => ActivityPub::TagManager.instance.uri_for(private_status),
+        'content' => '<p>secret</p>',
+      }),
+    })
+  end
+
+  it 'exports likes.json as expected' do
+    service_call
+
+    json = Oj.load(read_tgz_file(backup, 'likes.json'))
+    expect(json['type']).to eq 'OrderedCollection'
+    expect(json['totalItems']).to eq 1
+    expect(json['orderedItems']).to eq [ActivityPub::TagManager.instance.uri_for(favourite.status)]
+  end
+end
