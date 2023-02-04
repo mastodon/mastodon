1f803b95886ecc66c0c3901b1fbcf4853707ccb6
Victor Lee <leevic31@gmail.com>
2023-02-04 16:28:37 -0500

    Added spec for poll model

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Poll, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    context "when valid" do
+      let(:subject) { Fabricate.build(:poll) }
+      it "is valid with valid attributes" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "when not valid" do
+      let(:subject) { Fabricate.build(:poll, expires_at: nil) }
+      it 'is invalid without an expire date' do
+        subject.valid?
+        expect(subject).to model_have_error_on_field(:expires_at)
+      end
+    end
+  end
 end
