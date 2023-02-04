67744ee779b97ea07e91dd076e1df25e362ff33d
Christian Schmidt <github@chsc.dk>
2023-02-04 16:34:21 +0100

    Spell check input fields (#23395)

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -242,6 +242,7 @@ class ComposeForm extends ImmutablePureComponent {
             id='cw-spoiler-input'
             className='spoiler-input__input'
             lang={this.props.lang}
+            spellCheck
           />
         </div>
 
