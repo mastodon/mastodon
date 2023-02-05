c427ed13ba5dffeb1e824f7f21e93fea0fff4bae
Victor Lee <leevic31@gmail.com>
2023-02-05 13:21:54 -0500

    Fixed code style

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Poll, type: :model do
+RSpec.describe Poll do
+  describe 'validations' do
+    context 'when valid' do
+      let(:poll) { Fabricate.build(:poll) }
 
-  describe "validations" do
-    context "when valid" do
-      let(:subject) { Fabricate.build(:poll) }
-      it "is valid with valid attributes" do
-        expect(subject).to be_valid
+      it 'is valid with valid attributes' do
+        expect(poll).to be_valid
       end
     end
 
-    context "when not valid" do
-      let(:subject) { Fabricate.build(:poll, expires_at: nil) }
+    context 'when not valid' do
+      let(:poll) { Fabricate.build(:poll, expires_at: nil) }
+
       it 'is invalid without an expire date' do
-        subject.valid?
-        expect(subject).to model_have_error_on_field(:expires_at)
+        poll.valid?
+        expect(poll).to model_have_error_on_field(:expires_at)
       end
     end
   end
