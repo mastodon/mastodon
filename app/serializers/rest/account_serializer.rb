be1caad9338157773ca91f7aee89d440347cf49b
Claire <claire.github-309c@sitedethib.com>
2023-02-09 21:02:09 +0100

    Fix REST API serializer for Account not including `moved` when the moved account has itself moved (#22483) (#23492)
    Instead of cutting immediately, cut after one recursion.

--- app/serializers/rest/account_serializer.rb
+++ app/serializers/rest/account_serializer.rb
@@ -15,6 +15,16 @@ class REST::AccountSerializer < ActiveModel::Serializer
   attribute :suspended, if: :suspended?
   attribute :silenced, key: :limited, if: :silenced?
 
+  class AccountDecorator < SimpleDelegator
+    def self.model_name
+      Account.model_name
+    end
+
+    def moved?
+      false
+    end
+  end
+
   class FieldSerializer < ActiveModel::Serializer
     include FormattingHelper
 
@@ -84,7 +94,7 @@ class REST::AccountSerializer < ActiveModel::Serializer
   end
 
   def moved_to_account
-    object.suspended? ? nil : object.moved_to_account
+    object.suspended? ? nil : AccountDecorator.new(object.moved_to_account)
   end
 
   def emojis
@@ -106,6 +116,6 @@ class REST::AccountSerializer < ActiveModel::Serializer
   delegate :suspended?, :silenced?, to: :object
 
   def moved_and_not_nested?
-    object.moved? && object.moved_to_account.moved_to_account_id.nil?
+    object.moved?
   end
 end
