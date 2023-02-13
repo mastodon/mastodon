c229c1cbdecfdbefbaee65b067b48aa3d8b6b436
Lina <git@lina.pizza>
2023-02-13 23:30:00 +0000

    is_exclusive -> exclusive

--- app/lib/feed_manager.rb
+++ app/lib/feed_manager.rb
@@ -354,10 +354,9 @@ class FeedManager
   def filter_from_home?(status, receiver_id, crutches, timeline_type = :home)
     return false if receiver_id == status.account_id
     return true  if status.reply? && (status.in_reply_to_id.nil? || status.in_reply_to_account_id.nil?)
-    # hometown: exclusive list rules
     unless timeline_type == :list
       # find all exclusive lists
-      lists = List.where(account_id: receiver_id, is_exclusive: true)
+      lists = List.where(account_id: receiver_id, exclusive: true)
       # is there a list the receiver owns with this account on it? if so, return true
       return true if ListAccount.where(list: lists, account_id: status.account_id).exists?
     end
--- db/schema.rb
+++ db/schema.rb
@@ -540,7 +540,7 @@ ActiveRecord::Schema.define(version: 2023_02_07_034905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "replies_policy", default: 0, null: false
-    t.boolean "is_exclusive", default: false
+    t.boolean "exclusive", default: false
     t.index ["account_id"], name: "index_lists_on_account_id"
   end
 
