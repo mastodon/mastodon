7e64eef73b62fcb1537181e66bcd3e5e25fcc665
Nick Schonning <nschonni@gmail.com>
2023-02-13 09:12:14 -0500

    [Glitch] Enable ESLint no-useless-escape
    Port db2c58d47ae0db8490a30cd3846f30e615c382b5 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/actions/push_notifications/registerer.js
+++ app/javascript/flavours/glitch/actions/push_notifications/registerer.js
@@ -6,7 +6,7 @@ import { setBrowserSupport, setSubscription, clearSubscription } from './setter'
 const urlBase64ToUint8Array = (base64String) => {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
+    .replace(/-/g, '+')
     .replace(/_/g, '/');
 
   const rawData = window.atob(base64);
