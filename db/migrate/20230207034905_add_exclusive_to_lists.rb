71fb640cfd0a639dc527184994683127109c2987
Lina <git@lina.pizza>
2023-02-14 21:02:20 +0000

    migration name change

--- db/migrate/20230207034905_add_exclusive_to_lists.rb
+++ db/migrate/20230207034905_add_exclusive_to_lists.rb
@@ -1,4 +1,4 @@
-class AddIsExclusiveToLists < ActiveRecord::Migration[6.1]
+class AddExclusiveToLists < ActiveRecord::Migration[6.1]
   def change
     add_column :lists, :exclusive, :boolean
     change_column_default :lists, :exclusive, false
