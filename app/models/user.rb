9d8b656c7b63a2099fd81f6bd160eea8043343f7
Claire <claire.github-309c@sitedethib.com>
2023-02-22 14:33:57 +0100

    Make sure users are counted as active the moment they are approved and confirmed

--- app/models/user.rb
+++ app/models/user.rb
@@ -507,6 +507,7 @@ class User < ApplicationRecord
   def prepare_new_user!
     BootstrapTimelineWorker.perform_async(account_id)
     ActivityTracker.increment('activity:accounts:local')
+    ActivityTracker.record('activity:logins', id)
     UserMailer.welcome(self).deliver_later
     TriggerWebhookWorker.perform_async('account.approved', 'Account', account_id)
   end
