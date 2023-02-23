92afddd64030381fb9dfdd266831819d5095bb8b
Matt Jankowski <matt@jankowski.online>
2023-02-23 17:14:13 -0500

    Add spec coverage for AdminMailer.new_pending_account mailer

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -43,4 +43,24 @@ RSpec.describe AdminMailer, type: :mailer do
       expect(mail.body.encoded).to match "#{appeal.account.username} is appealing a moderation decision by #{appeal.strike.account.username}"
     end
   end
+
+  describe '.new_pending_account' do
+    let(:recipient) { Fabricate(:account, username: "Barklums") }
+    let(:user) { Fabricate(:user) }
+    let(:mail)      { described_class.new_pending_account(recipient, user) }
+
+    before do
+      recipient.user.update(locale: :en)
+    end
+
+    it 'renders the headers' do
+      expect(mail.subject).to eq("New account up for review on cb6e6126.ngrok.io (#{user.account.username})")
+      expect(mail.to).to eq [recipient.user_email]
+      expect(mail.from).to eq ['notifications@localhost']
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match "The details of the new account are below. You can approve or reject this application."
+    end
+  end
 end
