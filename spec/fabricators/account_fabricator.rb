1e506ff1b7f5ad93850ef0b429d738acd1b0fd66
Claire <claire.github-309c@sitedethib.com>
2023-02-28 17:06:05 +0100

    Add tests

--- spec/fabricators/account_fabricator.rb
+++ spec/fabricators/account_fabricator.rb
@@ -5,13 +5,14 @@ public_key  = keypair.public_key.to_pem
 private_key = keypair.to_pem
 
 Fabricator(:account) do
-  transient :suspended, :silenced
+  transient :suspended, :silenced, :deleted
   username            { sequence(:username) { |i| "#{Faker::Internet.user_name(separators: %w(_))}#{i}" } }
   last_webfingered_at { Time.now.utc }
   public_key          { public_key }
   private_key         { private_key }
   suspended_at        { |attrs| attrs[:suspended] ? Time.now.utc : nil }
   silenced_at         { |attrs| attrs[:silenced] ? Time.now.utc : nil }
+  deleted_at          { |attrs| attrs[:deleted] ? Time.now.utc : nil }
   user                { |attrs| attrs[:domain].nil? ? Fabricate.build(:user, account: nil) : nil }
   uri                 { |attrs| attrs[:domain].nil? ? '' : "https://#{attrs[:domain]}/users/#{attrs[:username]}" }
   discoverable        true
--- spec/requests/activitypub/collections_spec.rb
+++ spec/requests/activitypub/collections_spec.rb
@@ -62,6 +62,33 @@ RSpec.describe 'ActivityPub Collections' do
               .to have_http_status(403)
           end
         end
+
+        context 'when account is permanently deleted' do
+          before do
+            account.mark_deleted!
+            account.deletion_request.destroy
+          end
+
+          it 'returns http gone' do
+            subject
+
+            expect(response)
+              .to have_http_status(410)
+          end
+        end
+
+        context 'when account is pending deletion' do
+          before do
+            account.mark_deleted!
+          end
+
+          it 'returns http forbidden' do
+            subject
+
+            expect(response)
+              .to have_http_status(403)
+          end
+        end
       end
 
       context 'with signature' do
