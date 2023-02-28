6c8b64906c9df5e3e8221f05b61b86284344043a
Matt Jankowski <matt@jankowski.online>
2023-02-28 14:17:13 -0500

    Add spec coverage for HomeHelper#account_link_to

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -8,4 +8,30 @@ RSpec.describe HomeHelper, type: :helper do
       expect(helper.default_props).to eq locale: I18n.locale
     end
   end
+
+  describe 'account_link_to' do
+    context 'with a missing account' do
+      let(:account) { nil }
+
+      it 'returns a button' do
+        result = helper.account_link_to(account)
+
+        expect(result).to match t('about.contact_missing')
+      end
+    end
+
+    context 'with a valid account' do
+      let(:account) { Fabricate(:account) }
+
+      it 'returns a link to the account' do
+        without_partial_double_verification do
+          allow(helper).to receive(:current_account).and_return(account)
+          allow(helper).to receive(:prefers_autoplay?).and_return(false)
+          result = helper.account_link_to(account)
+
+          expect(result).to match "@#{account.acct}"
+        end
+      end
+    end
+  end
 end
