29b3c00c8340e14f7d844b1ca015cd691e8f31a1
Claire <claire.github-309c@sitedethib.com>
2023-03-01 15:17:09 +0100

    Add migration to backfill accounts.deleted_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_145403) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_145507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
