7583cc845fcc1deb7d281f75ae9ea468db09659f
Matt Jankowski <matt@jankowski.online>
2023-03-01 18:06:58 -0500

    Add coverage for AccountPolicy#destroy?

--- spec/policies/account_policy_spec.rb
+++ spec/policies/account_policy_spec.rb
@@ -130,4 +130,30 @@ RSpec.describe AccountPolicy do
       end
     end
   end
+
+  permissions :destroy? do
+    context 'admin' do
+      context 'with a temporarily suspended account' do
+        before { allow(alice).to receive(:suspended_temporarily?).and_return(true) }
+
+        it 'permits' do
+          expect(subject).to permit(admin, alice)
+        end
+      end
+
+      context 'with a not temporarily suspended account' do
+        before { allow(alice).to receive(:suspended_temporarily?).and_return(false) }
+
+        it 'denies' do
+          expect(subject).to_not permit(admin, alice)
+        end
+      end
+    end
+
+    context 'not admin' do
+      it 'denies' do
+        expect(subject).to_not permit(john, alice)
+      end
+    end
+  end
 end
