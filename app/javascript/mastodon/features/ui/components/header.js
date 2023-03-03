b52746e64b5e150f05b6e55e7e9546f408719118
Claire <claire.github-309c@sitedethib.com>
2023-03-03 20:25:36 +0100

    Fix duplicate “Publish” button on mobile (#23804)

--- app/javascript/mastodon/features/ui/components/header.js
+++ app/javascript/mastodon/features/ui/components/header.js
@@ -22,8 +22,8 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-export default @connect(null, mapDispatchToProps)
-@withRouter
+export default @withRouter
+@connect(null, mapDispatchToProps)
 class Header extends React.PureComponent {
 
   static contextTypes = {
