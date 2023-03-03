897961afa4ea3e44bc6213b2be6a0adfaccdf5dd
Matt Jankowski <matt@jankowski.online>
2023-03-03 15:59:49 -0500

    Add basic coverage for some worker jobs

---
+++ spec/workers/push_update_worker_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe PushUpdateWorker do
+  let(:worker) { described_class.new }
+
+  describe 'perform' do
+    it 'runs without error for missing record' do
+      account_id = nil
+      status_id = nil
+
+      expect { worker.perform(account_id, status_id) }.to_not raise_error
+    end
+  end
+end
