1c2c81407ef470ec2c0265e24284f653a1a2bf28
Amri Toufali <amri@handsomemedia.com>
2023-03-06 14:20:40 -0800

    remove "Create account" signup button

--- app/javascript/mastodon/features/ui/components/sign_in_banner.js
+++ app/javascript/mastodon/features/ui/components/sign_in_banner.js
@@ -32,7 +32,7 @@ const SignInBanner = () => {
     <div className='sign-in-banner'>
       <p><FormattedMessage id='sign_in_banner.text' defaultMessage='Sign in to follow profiles or hashtags, favourite, share and reply to posts. You can also interact from your account on a different server.' /></p>
       <a href='/auth/sign_in' className='button button--block'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Sign in' /></a>
-      {signupButton}
+      {/* {signupButton} */}
     </div>
   );
 };
