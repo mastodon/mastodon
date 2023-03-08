85145d0f984e5ad8181a03e5abe2665857a30544
Claire <claire.github-309c@sitedethib.com>
2023-03-08 15:29:13 +0100

    Change sidekiq-bulk's batch size from 10,000 to 1,000 jobs in one Redis call

--- app/services/remove_status_service.rb
+++ app/services/remove_status_service.rb
@@ -88,7 +88,7 @@ class RemoveStatusService < BaseService
 
     status_reach_finder = StatusReachFinder.new(@status, unsafe: true)
 
-    ActivityPub::DeliveryWorker.push_bulk(status_reach_finder.inboxes) do |inbox_url|
+    ActivityPub::DeliveryWorker.push_bulk(status_reach_finder.inboxes, limit: 1_000) do |inbox_url|
       [signed_activity_json, @account.id, inbox_url]
     end
   end
