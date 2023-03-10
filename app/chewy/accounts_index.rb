a44f0711e61ba45561267c5f03ac8b7d66979775
Vyr Cossont <VyrCossont@users.noreply.github.com>
2023-03-10 11:35:59 -0800

    Remove index settings related to account full text search

--- app/chewy/accounts_index.rb
+++ app/chewy/accounts_index.rb
@@ -2,21 +2,6 @@
 
 class AccountsIndex < Chewy::Index
   settings index: { refresh_interval: '30s' }, analysis: {
-    filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
-      english_stemmer: {
-        type: 'stemmer',
-        language: 'english',
-      },
-      english_possessive_stemmer: {
-        type: 'stemmer',
-        language: 'possessive_english',
-      },
-    },
-
     analyzer: {
       content: {
         tokenizer: 'whitespace',
@@ -26,18 +11,6 @@ class AccountsIndex < Chewy::Index
       edge_ngram: {
         tokenizer: 'edge_ngram',
         filter: %w(lowercase asciifolding cjk_width),
-      },
-
-      text: {
-        tokenizer: 'uax_url_email',
-        filter: %w(
-          english_possessive_stemmer
-          lowercase
-          asciifolding
-          cjk_width
-          english_stop
-          english_stemmer
-        ),
       },
     },
 
