6cca21938d1739ee87a0d6765fdbcc6952ac12ee
Matt Jankowski <matt@jankowski.online>
2023-03-11 09:14:26 -0500

    Add spec for react component helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :helper
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Paperclip::Shoulda::Matchers
   config.include ActiveSupport::Testing::TimeHelpers
