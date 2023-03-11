11cde2ac459354c56d480698535f96142dd68cf6
Matt Jankowski <matt@jankowski.online>
2023-03-11 09:27:25 -0500

    Move react component helpers to own file

--- spec/helpers/react_helper_spec.rb
+++ spec/helpers/react_helper_spec.rb
@@ -3,6 +3,71 @@
 require 'rails_helper'
 
 describe ReactHelper do
+  describe 'render_video_component' do
+    it 'renders a react component for the video' do
+      media = Fabricate(:media_attachment, type: :video, status: Fabricate(:status))
+      without_partial_double_verification do
+        allow(helper).to receive(:current_account).and_return(media.account)
+      end
+      result = helper.render_video_component(media.status)
+      html = Nokogiri::Slop(result)
+
+      expect(html.div['data-component']).to eq('Video')
+    end
+  end
+
+  describe 'render_audio_component' do
+    it 'renders a react component for the audio' do
+      media = Fabricate(:media_attachment, type: :audio, status: Fabricate(:status))
+      without_partial_double_verification do
+        allow(helper).to receive(:current_account).and_return(media.account)
+      end
+      result = helper.render_audio_component(media.status)
+      html = Nokogiri::Slop(result)
+
+      expect(html.div['data-component']).to eq('Audio')
+    end
+  end
+
+  describe 'render_media_gallery_component' do
+    it 'renders a react component for the media gallery' do
+      media = Fabricate(:media_attachment, type: :audio, status: Fabricate(:status))
+      without_partial_double_verification do
+        allow(helper).to receive(:current_account).and_return(media.account)
+      end
+      result = helper.render_media_gallery_component(media.status)
+      html = Nokogiri::Slop(result)
+
+      expect(html.div['data-component']).to eq('MediaGallery')
+    end
+  end
+
+  describe 'render_card_component' do
+    it 'returns the correct react component markup' do
+      status = Fabricate(:status, preview_cards: [Fabricate(:preview_card)])
+      without_partial_double_verification do
+        allow(helper).to receive(:current_account).and_return(status.account)
+      end
+      result = helper.render_card_component(status)
+      html = Nokogiri::Slop(result)
+
+      expect(html.div['data-component']).to eq('Card')
+    end
+  end
+
+  describe 'render_poll_component' do
+    it 'returns the correct react component markup' do
+      status = Fabricate(:status, poll: Fabricate(:poll))
+      without_partial_double_verification do
+        allow(helper).to receive(:current_account).and_return(status.account)
+      end
+      result = helper.render_poll_component(status)
+      html = Nokogiri::Slop(result)
+
+      expect(html.div['data-component']).to eq('Poll')
+    end
+  end
+
   describe 'react_component' do
     context 'with no block passed in' do
       let(:result) { helper.react_component('name', { one: :two }) }
