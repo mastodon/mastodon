6a8005aa6139bc758a4c3fc54282d17898fab7c8
Jesse Karmani <jessekarmani@gmail.com>
2023-03-14 14:18:49 -0700

    Refactor Logo component

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,19 +1,30 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import Image from 'mastodon/components/image';
 
-const Logo = connect(state => ({
-  server: state.getIn(['server', 'server']),
-}))(({ server }) => {
-  if (server.getIn(['logo', 'url']) === null) {
-    return (<svg viewBox='0 0 376 102' className='logo' role='img'>
-    <title>decodon</title>
-    <use xlinkHref='#decodon-logo' />
-  </svg>);
-  } else {
-    return (<Image blurhash={server.getIn(['logo', 'blurhash'])} src={server.getIn(['logo', 'url'])} className='logo' />)
-  }
+const mapStateToProps = state => ({
+  server: state.getIn(['server', 'server'])
 });
+
+export default @connect(mapStateToProps)
+class Logo extends React.PureComponent {
+  static propTypes = {
+    server: PropTypes.object,
+  }
+
+  render() {
+    const { server } = this.props;
+    if (server.getIn(['logo', 'url']) === null) {
+      return (<svg viewBox='0 0 376 102' className='logo' role='img'>
+      <title>decodon</title>
+      <use xlinkHref='#decodon-logo' />
+    </svg>);
+    } else {
+      return (<Image blurhash={server.getIn(['logo', 'blurhash'])} src={server.getIn(['logo', 'url'])} className='logo' />)
+    }
+  }
+};
 
 // const Logo = () => (
 //   <svg viewBox='0 0 376 102' className='logo' role='img'>
@@ -22,4 +33,3 @@ const Logo = connect(state => ({
 //   </svg>
 // );
 
-export default Logo;
