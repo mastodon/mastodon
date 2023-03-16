7fa3ec0603eb924bcf60f6fc495fa4f53a8007b5
Matt Jankowski <matt@jankowski.online>
2023-03-16 13:09:44 -0400

    Coverage for cli `media usage`

--- spec/lib/mastodon/cli/media_spec.rb
+++ spec/lib/mastodon/cli/media_spec.rb
@@ -5,4 +5,12 @@ require 'mastodon/cli/media'
 
 describe Mastodon::CLI::Media do
   it_behaves_like 'A CLI Sub-Command'
+
+  describe 'usage' do
+    it 'calculates used disk space' do
+      expect { described_class.new.invoke(:usage) }.to output(
+        a_string_including('Settings')
+      ).to_stdout
+    end
+  end
 end
