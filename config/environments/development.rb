f0e727f958cd9428b2c56a3c6a65bbbf176bfa0d
Eugen Rochko <eugen@zeonfederated.com>
2023-03-16 02:55:54 +0100

    Add cache headers to static files served through Rails (#24120)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,12 +16,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
     config.cache_store = :redis_cache_store, REDIS_CACHE_PARAMS
-
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}",
-    }
   else
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
