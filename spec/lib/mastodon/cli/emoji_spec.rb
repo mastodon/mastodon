e3457540a584ca775e1e5066ed9b979c5a736484
Matt Jankowski <matt@jankowski.online>
2023-03-16 13:43:35 -0400

    Coverage for cli emoji purge

--- spec/lib/mastodon/cli/emoji_spec.rb
+++ spec/lib/mastodon/cli/emoji_spec.rb
@@ -5,4 +5,12 @@ require 'mastodon/cli/emoji'
 
 describe Mastodon::CLI::Emoji do
   it_behaves_like 'A CLI Sub-Command'
+
+  describe 'purge' do
+    it 'removes the emoji' do
+      expect { described_class.new.invoke(:purge) }.to output(
+        a_string_including('OK')
+      ).to_stdout
+    end
+  end
 end
