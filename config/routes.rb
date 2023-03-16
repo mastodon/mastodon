bd047acc356671727c112336bb237f979bba517d
Christian Schmidt <github@chsc.dk>
2023-03-16 11:07:24 +0100

    Replace `Status#translatable?` with language matrix in separate endpoint (#24037)

--- config/routes.rb
+++ config/routes.rb
@@ -546,6 +546,7 @@ Rails.application.routes.draw do
         resources :domain_blocks, only: [:index], controller: 'instances/domain_blocks'
         resource :privacy_policy, only: [:show], controller: 'instances/privacy_policies'
         resource :extended_description, only: [:show], controller: 'instances/extended_descriptions'
+        resource :translation_languages, only: [:show], controller: 'instances/translation_languages'
         resource :activity, only: [:show], controller: 'instances/activity'
       end
 
