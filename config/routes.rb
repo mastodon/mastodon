a6217bd035093a0e568ba10ed2d2f9f3c3946c19
Eugen Rochko <eugen@zeonfederated.com>
2023-03-16 22:46:52 +0100

    Change user backups to use expiring URLs for download when possible (#24136)

--- config/routes.rb
+++ config/routes.rb
@@ -219,6 +219,7 @@ Rails.application.routes.draw do
   resource :statuses_cleanup, controller: :statuses_cleanup, only: [:show, :update]
 
   get '/media_proxy/:id/(*any)', to: 'media_proxy#show', as: :media_proxy, format: false
+  get '/backups/:id/download', to: 'backups#download', as: :download_backup, format: false
 
   resource :authorize_interaction, only: [:show, :create]
   resource :share, only: [:show, :create]
