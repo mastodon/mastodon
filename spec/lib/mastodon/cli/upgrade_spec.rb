ccbe8a6a8263f7f1391c3acfba089ee53daaaeef
Matt Jankowski <matt@jankowski.online>
2023-03-16 13:27:53 -0400

    Coverage for cli `upgrade storage_schema`

--- spec/lib/mastodon/cli/upgrade_spec.rb
+++ spec/lib/mastodon/cli/upgrade_spec.rb
@@ -5,4 +5,12 @@ require 'mastodon/cli/upgrade'
 
 describe Mastodon::CLI::Upgrade do
   it_behaves_like 'A CLI Sub-Command'
+
+  describe 'storage-schema' do
+    it 'updates the schema for attachments' do
+      expect { described_class.new.invoke(:storage_schema) }.to output(
+        a_string_including('Upgraded')
+      ).to_stdout
+    end
+  end
 end
