7d9d2e0e0b8a028ab683648afe2d434586138a43
Vyr Cossont <VyrCossont@users.noreply.github.com>
2023-03-18 23:15:52 -0700

    Revert "Add `.distributable` variants of `status` hooks that get only public and unlisted posts"
    This reverts commit bc2793e92e8c5209c492f17808b9fc9947580cf8.

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -20,9 +20,7 @@ class Webhook < ApplicationRecord
     account.updated
     report.created
     status.created
-    status.created.distributable
     status.updated
-    status.updated.distributable
   ).freeze
 
   scope :enabled, -> { where(enabled: true) }
