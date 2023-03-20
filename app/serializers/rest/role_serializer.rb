24831687b9565062e8810674cc190d47b59e4fec
Vyr Cossont <VyrCossont@users.noreply.github.com>
2023-03-19 23:28:35 -0700

    Add missing role attrs to serializer
    position, created_at, and updated_at are documented as being present, but weren't actually being serialized.

--- app/serializers/rest/role_serializer.rb
+++ app/serializers/rest/role_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class REST::RoleSerializer < ActiveModel::Serializer
-  attributes :id, :name, :permissions, :color, :highlighted
+  attributes :id, :name, :permissions, :color, :highlighted, :position, :created_at, :updated_at
 
   def id
     object.id.to_s
