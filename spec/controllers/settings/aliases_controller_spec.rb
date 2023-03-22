f148883f7582ee547c5ec76105abe75ec7bfea4b
Matt Jankowski <matt@jankowski.online>
2023-03-22 17:11:50 -0400

    Add coverage for settings/aliases controller actions

--- spec/controllers/settings/aliases_controller_spec.rb
+++ spec/controllers/settings/aliases_controller_spec.rb
@@ -18,4 +18,45 @@ describe Settings::AliasesController do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid alias' do
+      before { stub_resolver }
+
+      it 'creates an alias for the user' do
+        post :create, params: { account_alias: { acct: 'new@example.com' } }
+
+        expect(response).to redirect_to(settings_aliases_path)
+      end
+    end
+
+    context 'with invalid alias' do
+      it 'creates an alias for the user' do
+        post :create, params: { account_alias: { acct: 'format-wrong' } }
+
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:account_alias) do
+      AccountAlias.new(account: user.account, acct: 'new@example.com').tap do |account_alias|
+        account_alias.save(validate: false)
+      end
+    end
+
+    it 'removes an alias' do
+      delete :destroy, params: { id: account_alias.id }
+
+      expect(response).to redirect_to(settings_aliases_path)
+    end
+  end
+
+  private
+
+  def stub_resolver
+    resolver = instance_double(ResolveAccountService, call: Fabricate(:account))
+    allow(ResolveAccountService).to receive(:new).and_return(resolver)
+  end
 end
