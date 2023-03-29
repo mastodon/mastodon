af6eb37c704e4114157fdc04e54f682a9de0e8d9
Robert R George <rgeorge@midnightweb.net>
2023-03-29 00:58:27 -0700

    Wrap db:setup with Chewy.strategy(:mastodon) (#24302)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-Dir[Rails.root.join('db', 'seeds', '*.rb')].sort.each do |seed|
-  load seed
+Chewy.strategy(:mastodon) do
+  Dir[Rails.root.join('db', 'seeds', '*.rb')].sort.each do |seed|
+    load seed
+  end
 end
