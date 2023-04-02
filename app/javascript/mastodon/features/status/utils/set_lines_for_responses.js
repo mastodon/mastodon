1fddcb709ce2f80174ba45e0d908c2ba8f899290
Roni Laukkarinen <roni@dude.fi>
2023-04-02 12:19:17 +0300

    Add proof of concept
    Based on https://github.com/mastodon/mastodon/pull/17282

---
+++ app/javascript/mastodon/features/status/utils/set_lines_for_responses.js
@@ -0,0 +1,82 @@
+const getSetter = () => ({
+  value: false,
+  blocked: false,
+  not() {
+    this.blocked = true;
+    this.value = false;
+  },
+  can() {
+    if (this.blocked === false) this.value = true;
+  },
+});
+
+const flatChildren = (list, level = 1, hiddenLevels = 0, lastChildX = undefined) => {
+  return list
+    .map(({ id, children }, index, array) => {
+      const getMode = () => {
+        const canOut = getSetter();
+        const canIn = getSetter();
+
+        if (children.length > 0) {
+          canOut.can();
+        }
+
+        if (level > 1) {
+          canIn.can();
+        }
+
+        if (canIn.value && canOut.value) {
+          return 'io';
+        }
+        if (canIn.value) {
+          return 'i';
+        }
+
+        if (canOut.value) {
+          return 'o';
+        }
+
+        return '';
+      };
+
+      const current = {
+        mode: getMode(),
+        level,
+        hiddenLevels,
+        lastChild: array[index + 1] !== undefined ? false : true,
+      };
+
+      return children.length
+        ? [
+          { [id]: current },
+          ...flatChildren(
+            children,
+            level + 1,
+            current.lastChild === true && level > 1 ? hiddenLevels + 1 : hiddenLevels,
+            current.lastChild || lastChildX,
+          ),
+        ]
+        : [{ [id]: current }];
+    })
+    .flat();
+};
+
+export function getLinesForResponses(responses) {
+  return flatChildren(responses).reduce((all, item) => {
+    return { ...all, [Object.keys(item)[0]]: Object.values(item)[0] };
+  }, {});
+}
+
+export const setResponsesWithLinesData = (responses) => {
+  const linesData = getLinesForResponses(responses);
+
+  return responses.map(addLinesToResponses(linesData));
+};
+
+const addLinesToResponses = (linesData) => (response) => {
+  return {
+    ...response,
+    lines: linesData[response.id],
+    children: response.children.map(addLinesToResponses(linesData)),
+  };
+};
