e04cf142b83c2a975bfc0ac66d64edd2f02e17a8
Robert George <rgeorge@midnightweb.net>
2023-04-14 15:05:40 -0700

    Added pagination to publishers api endpoint

--- app/controllers/api/v1/admin/trends/links/preview_card_providers_controller.rb
+++ app/controllers/api/v1/admin/trends/links/preview_card_providers_controller.rb
@@ -3,11 +3,16 @@
 class Api::V1::Admin::Trends::Links::PreviewCardProvidersController < Api::BaseController
   include Authorization
 
+  LIMIT = 100
+
   before_action -> { authorize_if_got_token! :'admin:read' }, only: :index
   before_action -> { authorize_if_got_token! :'admin:write' }, except: :index
   before_action :set_providers, only: :index
 
   after_action :verify_authorized
+  after_action :insert_pagination_headers, only: :index
+
+  PAGINATION_PARAMS = %i(limit).freeze
 
   def index
     authorize :preview_card_provider, :index?
@@ -38,6 +43,34 @@ class Api::V1::Admin::Trends::Links::PreviewCardProvidersController < Api::BaseC
   end
 
   def set_providers
-    @providers = PreviewCardProvider.all
+    @providers = PreviewCardProvider.all.to_a_paginated_by_id(limit_param(LIMIT), params_slice(:max_id, :since_id, :min_id))
+  end
+
+  def insert_pagination_headers
+    set_pagination_headers(next_path, prev_path)
+  end
+
+  def next_path
+    api_v1_admin_trends_links_preview_card_providers_url(pagination_params(max_id: pagination_max_id)) if records_continue?
+  end
+
+  def prev_path
+    api_v1_admin_trends_links_preview_card_providers_url(pagination_params(min_id: pagination_since_id)) unless @providers.empty?
+  end
+
+  def pagination_max_id
+    @providers.last.id
+  end
+
+  def pagination_since_id
+    @providers.first.id
+  end
+
+  def records_continue?
+    @providers.size == limit_param(LIMIT)
+  end
+
+  def pagination_params(core_params)
+    params.slice(*PAGINATION_PARAMS).permit(*PAGINATION_PARAMS).merge(core_params)
   end
 end
