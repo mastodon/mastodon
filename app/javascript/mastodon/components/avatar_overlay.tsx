9355c4c5c8649ac6f2527faa187e2e75c6e22ca8
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2023-04-15 17:34:14 +0900

    rewrite AvatarOverlay component with React hooks

--- app/javascript/mastodon/components/avatar_overlay.tsx
+++ app/javascript/mastodon/components/avatar_overlay.tsx
@@ -1,51 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import ImmutablePropTypes from 'react-immutable-proptypes';
-import { autoPlayGif } from '../initial_state';
-import Avatar from './avatar';
+import type { Account } from '../../types/resources';
+import { Avatar } from './avatar';
 
-export default class AvatarOverlay extends React.PureComponent {
+type Props = {
+  account: Account;
+  friend: Account;
+  size?: number;
+  baseSize?: number;
+  overlaySize?: number;
+};
 
-  static propTypes = {
-    account: ImmutablePropTypes.map.isRequired,
-    friend: ImmutablePropTypes.map.isRequired,
-    animate: PropTypes.bool,
-    size: PropTypes.number,
-    baseSize: PropTypes.number,
-    overlaySize: PropTypes.number,
-  };
+export const AvatarOverlay: React.FC<Props> = ({
+  account,
+  friend,
+  size = 46,
+  baseSize = 36,
+  overlaySize = 24,
+}) => (
+  <div className='account__avatar-overlay' style={{ width: size, height: size }}>
+    <div className='account__avatar-overlay-base'><Avatar account={account} size={baseSize} /></div>
+    <div className='account__avatar-overlay-overlay'><Avatar account={friend} size={overlaySize} /></div>
+  </div>
+);
 
-  static defaultProps = {
-    animate: autoPlayGif,
-    size: 46,
-    baseSize: 36,
-    overlaySize: 24,
-  };
-
-  state = {
-    hovering: false,
-  };
-
-  handleMouseEnter = () => {
-    if (this.props.animate) return;
-    this.setState({ hovering: true });
-  };
-
-  handleMouseLeave = () => {
-    if (this.props.animate) return;
-    this.setState({ hovering: false });
-  };
-
-  render() {
-    const { account, friend, animate, size, baseSize, overlaySize } = this.props;
-    const { hovering } = this.state;
-
-    return (
-      <div className='account__avatar-overlay' style={{ width: size, height: size }}>
-        <div className='account__avatar-overlay-base'><Avatar animate={hovering || animate} account={account} size={baseSize} /></div>
-        <div className='account__avatar-overlay-overlay'><Avatar animate={hovering || animate} account={friend} size={overlaySize} /></div>
-      </div>
-    );
-  }
-
-}
+export default AvatarOverlay;
