b1397f831958e559241ebf6d813868a141ea2a40
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2023-04-16 16:36:16 +0900

    Play both avatars when this component hoverd
    For playing same time, do not attach event handlers to inner elements.

--- app/javascript/mastodon/components/avatar_overlay.tsx
+++ app/javascript/mastodon/components/avatar_overlay.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Account } from '../../types/resources';
-import { Avatar } from './avatar';
+import { useHovering } from '../../hooks/useHovering';
+import { autoPlayGif } from '../initial_state';
 
 type Props = {
   account: Account;
@@ -16,11 +17,35 @@ export const AvatarOverlay: React.FC<Props> = ({
   size = 46,
   baseSize = 36,
   overlaySize = 24,
-}) => (
-  <div className='account__avatar-overlay' style={{ width: size, height: size }}>
-    <div className='account__avatar-overlay-base'><Avatar account={account} size={baseSize} /></div>
-    <div className='account__avatar-overlay-overlay'><Avatar account={friend} size={overlaySize} /></div>
-  </div>
-);
+}) => {
+  const { hovering, handleMouseEnter, handleMouseLeave } = useHovering(autoPlayGif);
+  const accountSrc = hovering ? account?.get('avatar') : account?.get('avatar_static');
+  const friendSrc = hovering ? friend?.get('avatar') : friend?.get('avatar_static');
+
+  return (
+    <div
+      className='account__avatar-overlay' style={{ width: size, height: size }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className='account__avatar-overlay-base'>
+        <div
+          className='account__avatar'
+          style={{ width: `${baseSize}px`, height: `${baseSize}px` }}
+        >
+          {accountSrc && <img src={accountSrc} alt={account?.get('acct')} />}
+        </div>
+      </div>
+      <div className='account__avatar-overlay-overlay'>
+        <div
+          className='account__avatar'
+          style={{ width: `${overlaySize}px`, height: `${overlaySize}px` }}
+        >
+          {friendSrc && <img src={friendSrc} alt={friend?.get('acct')} />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AvatarOverlay;
