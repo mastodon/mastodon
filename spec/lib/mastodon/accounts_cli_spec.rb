7f98988001b8bc92fa73ad7a2a6b46b9e7901f76
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-04-18 10:10:37 -0300

    add tests for Mastodon::AccountsCLI#modify

--- spec/lib/mastodon/accounts_cli_spec.rb
+++ spec/lib/mastodon/accounts_cli_spec.rb
@@ -140,4 +140,182 @@ RSpec.describe Mastodon::AccountsCLI do
       end
     end
   end
+
+  describe '#modify' do
+    context 'when given username is not found' do
+      let(:arguments) { ['non_existent_username'] }
+
+      it 'returns an error message' do
+        expect { described_class.new.invoke(:modify, arguments) }
+          .to output(
+            a_string_including('No user with such username')
+          ).to_stdout
+          .and raise_error(SystemExit)
+      end
+    end
+
+    context 'when given username is found' do
+      let(:user) { Fabricate(:user) }
+      let(:arguments) { [user.account.username] }
+
+      context 'when no option is provided' do
+        it 'returns successfull message' do
+          expect { described_class.new.invoke(:modify, arguments) }
+            .to output(
+              a_string_including('OK')
+            ).to_stdout
+        end
+
+        it 'does not modify the user' do
+          described_class.new.invoke(:modify, arguments)
+
+          user_after_modify = User.find_by(email: user.email)
+
+          expect(user).to eq(user_after_modify)
+        end
+      end
+
+      context 'with --role option' do
+        context 'when given role is not found' do
+          let(:options) { { role: '404' } }
+
+          it 'returns an error message' do
+            expect { described_class.new.invoke(:modify, arguments, options) }
+              .to output(
+                a_string_including('Cannot find user role with that name')
+              ).to_stdout
+              .and raise_error(SystemExit)
+          end
+        end
+
+        context 'when given role is found' do
+          let(:options) { { role: 'Owner' } }
+
+          it "updates the user's role" do
+            described_class.new.invoke(:modify, arguments, options)
+
+            role = user.reload.role
+
+            expect(role.name).to eq('Owner')
+          end
+        end
+      end
+
+      context 'with --remove-role option' do
+        let(:options) { { remove_role: true } }
+        let(:role) { Fabricate(:user_role) }
+        let(:user) { Fabricate(:user, role: role) }
+
+        it "removes user's role successfully" do
+          described_class.new.invoke(:modify, arguments, options)
+
+          role = user.reload.role
+
+          expect(role.name).to be_empty
+        end
+      end
+
+      context 'with --email option' do
+        let(:user) { Fabricate(:user, email: 'old_email@email.com') }
+        let(:options) { { email: 'new_email@email.com' } }
+
+        it "sets 'unconfirmed_email' with given email adress" do
+          described_class.new.invoke(:modify, arguments, options)
+
+          expect(user.reload.unconfirmed_email).to eq(options[:email])
+        end
+
+        it 'does not update original email address' do
+          described_class.new.invoke(:modify, arguments, options)
+
+          expect(user.reload.email).to eq('old_email@email.com')
+        end
+
+        context 'with --confirm option' do
+          let(:user) { Fabricate(:user, email: 'old_email@email.com', confirmed_at: nil) }
+          let(:options) { { email: 'new_email@email.com', confirm: true } }
+
+          it "updates the user's email address" do
+            described_class.new.invoke(:modify, arguments, options)
+
+            expect(user.reload.email).to eq(options[:email])
+          end
+
+          it "set user's email address as confirmed" do
+            described_class.new.invoke(:modify, arguments, options)
+
+            expect(user.reload.confirmed?).to be(true)
+          end
+        end
+      end
+
+      context 'with --confirm option' do
+        let(:user) { Fabricate(:user, confirmed_at: nil) }
+        let(:options) { { confirm: true } }
+
+        it "confirms user's email address" do
+          described_class.new.invoke(:modify, arguments, options)
+
+          expect(user.reload.confirmed?).to be(true)
+        end
+      end
+
+      context 'with --approve option' do
+        let(:user) { Fabricate(:user, approved: false) }
+        let(:options) { { approve: true } }
+
+        it 'approves user' do
+          described_class.new.invoke(:modify, arguments, options)
+
+          expect(user.reload.approved?).to be(true)
+        end
+      end
+
+      context 'with --disable option' do
+        let(:user) { Fabricate(:user, disabled: false) }
+        let(:options) { { disable: true } }
+
+        it 'disables the user' do
+          described_class.new.invoke(:modify, arguments, options)
+
+          expect(user.reload.disabled).to be(true)
+        end
+      end
+
+      context 'with --enable option' do
+        let(:user) { Fabricate(:user, disabled: true) }
+        let(:options) { { enable: true } }
+
+        it 'enables the user' do
+          described_class.new.invoke(:modify, arguments, options)
+
+          expect(user.reload.disabled).to be(false)
+        end
+      end
+
+      context 'with --reset-password option' do
+        let(:options) { { reset_password: true } }
+
+        it "outputs new user's password" do
+          allow(SecureRandom).to receive(:hex).and_return('new_password')
+
+          expect { described_class.new.invoke(:modify, arguments, options) }
+            .to output(
+              a_string_including('new_password')
+            ).to_stdout
+        end
+      end
+
+      context 'with --disable-2fa option' do
+        let(:user) { Fabricate(:user, otp_required_for_login: true) }
+        let(:options) { { disable_2fa: true } }
+
+        it "sets 'otp_required_for_login' to false" do
+          described_class.new.invoke(:modify, arguments, options)
+
+          expect(user.reload.otp_required_for_login).to be(false)
+        end
+      end
+    end
+  end
 end
