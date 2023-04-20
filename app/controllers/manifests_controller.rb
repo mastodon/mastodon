94bee0108e1c9760f7df78cb146a457d0e7a2a4e
Claire <claire.github-309c@sitedethib.com>
2023-04-20 13:34:53 +0200

    Fix some user-independent endpoints potentially reading session cookies

--- app/controllers/manifests_controller.rb
+++ app/controllers/manifests_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ManifestsController < ActionController::Base # rubocop:disable Rails/ApplicationController
+  # Prevent `active_model_serializer`'s `ActionController::Serialization` from calling `current_user`
+  # and thus re-issuing session cookies
+  serialization_scope nil
+
   def show
     expires_in 3.minutes, public: true
     render json: InstancePresenter.new, serializer: ManifestSerializer, root: 'instance'
