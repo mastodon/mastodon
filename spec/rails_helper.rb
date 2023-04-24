f45961aa98da629e8bdaaba1189b9a338c8a2a08
Claire <claire.github-309c@sitedethib.com>
2023-04-24 19:06:58 +0200

    Add feature test for OAuth access grant (#24624)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Paperclip::Shoulda::Matchers
   config.include ActiveSupport::Testing::TimeHelpers
   config.include Chewy::Rspec::Helpers
