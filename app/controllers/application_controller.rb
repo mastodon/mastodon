319dbcdab7d300f834e48d931302e6cb91aa8f5c
Claire <claire.github-309c@sitedethib.com>
2023-04-24 20:03:57 +0200

    Fix crash when SSO_ACCOUNT_SETTINGS is not defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -128,7 +128,7 @@ class ApplicationController < ActionController::Base
   end
 
   def sso_account_settings
-    ENV.fetch('SSO_ACCOUNT_SETTINGS')
+    ENV.fetch('SSO_ACCOUNT_SETTINGS', nil)
   end
 
   def current_account
