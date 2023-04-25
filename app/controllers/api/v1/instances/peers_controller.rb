1419f90ef2252e7ef87418b12749968a9eab3de9
Claire <claire.github-309c@sitedethib.com>
2023-04-25 22:14:44 +0200

    Fix some user-independent endpoints potentially reading session cookies (#24650)

--- app/controllers/api/v1/instances/peers_controller.rb
+++ app/controllers/api/v1/instances/peers_controller.rb
@@ -4,8 +4,14 @@ class Api::V1::Instances::PeersController < Api::BaseController
   before_action :require_enabled_api!
 
   skip_before_action :require_authenticated_user!, unless: :whitelist_mode?
+  skip_around_action :set_locale
 
   vary_by ''
+
+  # Override `current_user` to avoid reading session cookies unless in whitelist mode
+  def current_user
+    super if whitelist_mode?
+  end
 
   def index
     cache_even_if_authenticated!
