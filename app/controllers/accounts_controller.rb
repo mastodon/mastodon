38419ea4f9396e28350bf8c246d338f54342f64f
Claire <claire.github-309c@sitedethib.com>
2023-04-28 09:42:51 +0200

    Change logged-out WebUI HTML pages to be cached for a few seconds

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -17,7 +17,7 @@ class AccountsController < ApplicationController
   def show
     respond_to do |format|
       format.html do
-        expires_in 0, public: true unless user_signed_in?
+        expires_in(15.seconds, public: true, stale_while_revalidate: 30.seconds, stale_if_error: 1.hour) unless user_signed_in?
 
         @rss_url = rss_url
       end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -21,7 +21,7 @@ class TagsController < ApplicationController
   def show
     respond_to do |format|
       format.html do
-        expires_in 0, public: true unless user_signed_in?
+        expires_in(15.seconds, public: true, stale_while_revalidate: 30.seconds, stale_if_error: 1.hour) unless user_signed_in?
       end
 
       format.rss do
