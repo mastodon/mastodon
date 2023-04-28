ece38b9318891113b98cd4fe78820b1b38bb1b75
Matt Jankowski <matt@jankowski.online>
2023-04-28 00:35:25 -0400

    Remove duplicate react helper method definitions (#24700)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -155,18 +155,6 @@ module ApplicationHelper
     tag(:meta, content: content, property: property)
   end
 
-  def react_component(name, props = {}, &block)
-    if block.nil?
-      content_tag(:div, nil, data: { component: name.to_s.camelcase, props: Oj.dump(props) })
-    else
-      content_tag(:div, data: { component: name.to_s.camelcase, props: Oj.dump(props) }, &block)
-    end
-  end
-
-  def react_admin_component(name, props = {})
-    content_tag(:div, nil, data: { 'admin-component': name.to_s.camelcase, props: Oj.dump({ locale: I18n.locale }.merge(props)) })
-  end
-
   def body_classes
     output = body_class_string.split
     output << "theme-#{current_theme.parameterize}"
