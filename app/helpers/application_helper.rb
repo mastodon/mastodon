bdc74086e6e7224117cfb46b1f5e7540c4580485
Matt Jankowski <matt@jankowski.online>
2023-04-30 00:47:15 -0400

    Fix Rails/ContentTag cop (#24691)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -118,7 +118,7 @@ module ApplicationHelper
   end
 
   def check_icon
-    content_tag(:svg, tag(:path, 'fill-rule': 'evenodd', 'clip-rule': 'evenodd', d: 'M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z'), xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 20 20', fill: 'currentColor')
+    content_tag(:svg, tag.path('fill-rule': 'evenodd', 'clip-rule': 'evenodd', d: 'M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z'), xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 20 20', fill: 'currentColor')
   end
 
   def visibility_icon(status)
@@ -152,7 +152,7 @@ module ApplicationHelper
   end
 
   def opengraph(property, content)
-    tag(:meta, content: content, property: property)
+    tag.meta(content: content, property: property)
   end
 
   def body_classes
