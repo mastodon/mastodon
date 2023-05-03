8b636a29c6e1f9f09ea1b5ad0787b1e90fd902d5
Claire <claire.github-309c@sitedethib.com>
2023-05-03 10:31:40 +0200

    Improve automatic post cleanup worker performances (#24785)

--- spec/workers/scheduler/accounts_statuses_cleanup_scheduler_spec.rb
+++ spec/workers/scheduler/accounts_statuses_cleanup_scheduler_spec.rb
@@ -75,7 +75,7 @@ describe Scheduler::AccountsStatusesCleanupScheduler do
     end
   end
 
-  describe '#get_budget' do
+  describe '#compute_budget' do
     context 'on a single thread' do
       let(:process_set_stub) { [{ 'concurrency' => 1, 'queues' => %w(push default) }] }
 
@@ -129,6 +129,19 @@ describe Scheduler::AccountsStatusesCleanupScheduler do
           .and change { account1.statuses.count }
           .and change { account3.statuses.count }
           .and change { account5.statuses.count }
+      end
+
+      context 'when given a big budget' do
+        let(:process_set_stub) { [{ 'concurrency' => 400, 'queues' => %w(push default) }] }
+
+        before do
+          stub_const 'Scheduler::AccountsStatusesCleanupScheduler::MAX_BUDGET', 400
+        end
+
+        it 'correctly handles looping in a single run' do
+          expect(subject.compute_budget).to eq(400)
+          expect { subject.perform }.to change { Status.count }.by(-30)
+        end
       end
     end
   end
