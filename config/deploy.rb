618babc81d2285e8adf5aba9a77a466c75220ddd
Matt Jankowski <matt@jankowski.online>
2023-05-03 12:25:19 -0400

    Update capistrano config lock version to match bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-lock '3.17.1'
+lock '3.17.2'
 
 set :repo_url, ENV.fetch('REPO', 'https://github.com/mastodon/mastodon.git')
 set :branch, ENV.fetch('BRANCH', 'main')
