7641043b6a926ca697093c49d4a61ef94025bae8
Robert George <rgeorge@midnightweb.net>
2023-05-04 10:19:49 -0700

    Change default setting to block DMs from non-followed

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -48,7 +48,7 @@ class UserSettings
   namespace :interactions do
     setting :must_be_follower, default: false
     setting :must_be_following, default: false
-    setting :must_be_following_dm, default: false
+    setting :must_be_following_dm, default: true
   end
 
   def initialize(original_hash)
