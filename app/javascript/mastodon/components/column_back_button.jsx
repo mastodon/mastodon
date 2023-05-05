a2f81ade2ce497192661230b631629c986b37c84
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-05-05 14:19:25 +0900

    Fix column back button error

--- app/javascript/mastodon/components/column_back_button.jsx
+++ app/javascript/mastodon/components/column_back_button.jsx
@@ -23,7 +23,7 @@ export default class ColumnBackButton extends React.PureComponent {
       onClick();
     // Check if there is a previous page in the app to go back to per https://stackoverflow.com/a/70532858/9703201
     // When upgrading to V6, check `location.key !== 'default'` instead per https://github.com/remix-run/history/blob/main/docs/api-reference.md#location
-    } else if (router.location.key) {
+    } else if (router.route.location.key) {
       router.history.goBack();
     } else {
       router.history.push('/');
