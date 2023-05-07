4cabeb3ab7188f3d2f6753319b26de9c5ef94415
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2023-05-07 17:42:30 +0900

    Rewrite Domain component as function component

--- app/javascript/mastodon/components/domain.tsx
+++ app/javascript/mastodon/components/domain.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import IconButton from './icon_button';
 import { InjectedIntl, defineMessages, injectIntl } from 'react-intl';
-import ImmutablePureComponent from 'react-immutable-pure-component';
 
 const messages = defineMessages({
   unblockDomain: { id: 'account.unblock_domain', defaultMessage: 'Unblock domain {domain}' },
@@ -11,31 +10,25 @@ type Props = {
   domain: string;
   onUnblockDomain: (domain: string) => void;
   intl: InjectedIntl;
-}
-class Domain extends ImmutablePureComponent<Props> {
+};
+const _Domain: React.FC<Props> = ({ domain, onUnblockDomain, intl }) => {
+  const handleDomainUnblock = useCallback(() => {
+    onUnblockDomain(domain);
+  }, [domain, onUnblockDomain]);
 
-  handleDomainUnblock = () => {
-    this.props.onUnblockDomain(this.props.domain);
-  };
+  return (
+    <div className='domain'>
+      <div className='domain__wrapper'>
+        <span className='domain__domain-name'>
+          <strong>{domain}</strong>
+        </span>
 
-  render () {
-    const { domain, intl } = this.props;
-
-    return (
-      <div className='domain'>
-        <div className='domain__wrapper'>
-          <span className='domain__domain-name'>
-            <strong>{domain}</strong>
-          </span>
-
-          <div className='domain__buttons'>
-            <IconButton active icon='unlock' title={intl.formatMessage(messages.unblockDomain, { domain })} onClick={this.handleDomainUnblock} />
-          </div>
+        <div className='domain__buttons'>
+          <IconButton active icon='unlock' title={intl.formatMessage(messages.unblockDomain, { domain })} onClick={handleDomainUnblock} />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
-}
-
-export default injectIntl(Domain);
+export const Domain = injectIntl(_Domain);
