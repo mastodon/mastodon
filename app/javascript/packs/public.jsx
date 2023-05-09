mastodon/mastodon

commit 955179fc55f2db2694ab2d1e98d5ae82af430571

Dont use CommonJS (`require`, `module.exports`) anywhere (#24913)

app/javascript/packs/public.jsx
@@ -1,21 +1,30 @@
 import './public-path';
-import escapeTextContentForBrowser from 'escape-html';
 import loadPolyfills from '../mastodon/load_polyfills';
-import ready from '../mastodon/ready';
 import { start } from '../mastodon/common';
+
+import escapeTextContentForBrowser from 'escape-html';
+import ready from '../mastodon/ready';
 import loadKeyboardExtensions from '../mastodon/load_keyboard_extensions';
 import 'cocoon-js-vanilla';
 import axios from 'axios';
 import { throttle } from 'lodash';
 import { defineMessages } from 'react-intl';
+import * as IntlMessageFormat  from 'intl-messageformat';
+import { timeAgoString }  from '../mastodon/components/relative_timestamp';
+import { delegate }  from '@rails/ujs';
+import * as emojify  from '../mastodon/features/emoji/emoji';
+import { getLocale }  from '../mastodon/locales';
+import React  from 'react';
+import ReactDOM  from 'react-dom';
+import { createBrowserHistory }  from 'history';
+
+start();
 
 const messages = defineMessages({
   usernameTaken: { id: 'username.taken', defaultMessage: 'That username is taken. Try another' },
   passwordExceedsLength: { id: 'password_confirmation.exceeds_maxlength', defaultMessage: 'Password confirmation exceeds the maximum password length' },
   passwordDoesNotMatch: { id: 'password_confirmation.mismatching', defaultMessage: 'Password confirmation does not match' },
 });
-
-start();
 
 window.addEventListener('message', e => {
   const data = e.data || {};
@@ -33,16 +42,8 @@ window.addEventListener('message', e => {
   });
 });
 
-function main() {
-  const IntlMessageFormat = require('intl-messageformat').default;
-  const { timeAgoString } = require('../mastodon/components/relative_timestamp');
-  const { delegate } = require('@rails/ujs');
-  const emojify = require('../mastodon/features/emoji/emoji').default;
-  const { getLocale } = require('../mastodon/locales');
+function loaded() {
   const { localeData } = getLocale();
-  const React = require('react');
-  const ReactDOM = require('react-dom');
-  const { createBrowserHistory } = require('history');
 
   const scrollToDetailedStatus = () => {
     const history = createBrowserHistory();
@@ -339,6 +340,11 @@ function main() {
       }
     });
   });
+}
+
+
+function main() {
+  ready(loaded);
 }
 
 loadPolyfills()

.eslintrc.js
@@ -102,6 +102,7 @@ module.exports = {
       {
         vars: 'all',
         args: 'after-used',
+        destructuredArrayIgnorePattern: '^_',
         ignoreRestSiblings: true,
       },
     ],
@@ -208,6 +209,9 @@ module.exports = {
         ],
       },
     ],
+    'import/no-amd': 'error',
+    'import/no-commonjs': 'error',
+    'import/no-import-module-exports': 'error',
     'import/no-webpack-loader-syntax': 'error',
 
     'promise/always-return': 'off',
@@ -255,6 +259,7 @@ module.exports = {
         '*.config.js',
         '.*rc.js',
         'ide-helper.js',
+        'config/webpack/**/*',
       ],
 
       env: {
@@ -263,6 +268,10 @@ module.exports = {
 
       parserOptions: {
         sourceType: 'script',
+      },
+
+      rules: {
+        'import/no-commonjs': 'off',
       },
     },
     {
@@ -296,6 +305,14 @@ module.exports = {
 
       env: {
         jest: true,
+      },
+    },
+    {
+      files: [
+        'streaming/**/*',
+      ],
+      rules: {
+        'import/no-commonjs': 'off',
       },
     },
   ],

app/javascript/mastodon/common.js
@@ -1,7 +1,7 @@
 import Rails from '@rails/ujs';
+import 'font-awesome/css/font-awesome.css';
 
 export function start() {
-  require('font-awesome/css/font-awesome.css');
   require.context('../images/', true);
 
   try {

app/javascript/mastodon/features/emoji/emoji_compressed.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-commonjs --
+   We need to use CommonJS here due to preval */
 // @preval
 // http://www.unicode.org/Public/emoji/5.0/emoji-test.txt
 // This file contains the compressed version of the emoji data from

app/javascript/mastodon/features/emoji/emoji_mart_data_light.js
@@ -1,8 +1,10 @@
 // The output of this module is designed to mimic emoji-mart's
 // "data" object, such that we can use it for a light version of emoji-mart's
 // emojiIndex.search functionality.
-const { unicodeToUnifiedName } = require('./unicode_to_unified_name');
-const [ shortCodesToEmojiData, skins, categories, short_names ] = require('./emoji_compressed');
+import { unicodeToUnifiedName } from './unicode_to_unified_name';
+import emojiCompressed from './emoji_compressed';
+
+const [ shortCodesToEmojiData, skins, categories, short_names ] = emojiCompressed;
 
 const emojis = {};
 
@@ -33,7 +35,7 @@ Object.keys(shortCodesToEmojiData).forEach((shortCode) => {
   };
 });
 
-module.exports = {
+export {
   emojis,
   skins,
   categories,

app/javascript/mastodon/features/emoji/emoji_mart_search_light.js
@@ -1,7 +1,7 @@
 // This code is largely borrowed from:
 // https://github.com/missive/emoji-mart/blob/5f2ffcc/src/utils/emoji-index.js
 
-import data from './emoji_mart_data_light';
+import * as data from './emoji_mart_data_light';
 import { getData, getSanitizedData, uniq, intersect } from './emoji_utils';
 
 let originalPool = {};

app/javascript/mastodon/features/emoji/emoji_unicode_mapping_light.js
@@ -2,14 +2,17 @@
 // (i.e. the svg filename) and a shortCode intended to be shown
 // as a "title" attribute in an HTML element (aka tooltip).
 
+import emojiCompressed from './emoji_compressed';
+
+import { unicodeToFilename } from './unicode_to_filename';
+
 const [
   shortCodesToEmojiData,
-  skins, // eslint-disable-line @typescript-eslint/no-unused-vars
-  categories, // eslint-disable-line @typescript-eslint/no-unused-vars
-  short_names, // eslint-disable-line @typescript-eslint/no-unused-vars
+  _skins,
+  _categories,
+  _short_names,
   emojisWithoutShortCodes,
-] = require('./emoji_compressed');
-const { unicodeToFilename } = require('./unicode_to_filename');
+] = emojiCompressed;
 
 // decompress
 const unicodeMapping = {};
@@ -32,4 +35,4 @@ Object.keys(shortCodesToEmojiData).forEach((shortCode) => {
 });
 emojisWithoutShortCodes.forEach(emojiMapData => processEmojiMapData(emojiMapData));
 
-module.exports = unicodeMapping;
+export default unicodeMapping;

app/javascript/mastodon/features/emoji/emoji_utils.js
@@ -1,7 +1,7 @@
 // This code is largely borrowed from:
 // https://github.com/missive/emoji-mart/blob/5f2ffcc/src/utils/index.js
 
-import data from './emoji_mart_data_light';
+import * as data from './emoji_mart_data_light';
 
 const buildSearch = (data) => {
   const search = [];

commit 909f81f0cab22097ca18b6da4b666372335d3dd8

Fix javascript on moderation interface
Fixes a regression from #24913

app/javascript/packs/public.jsx
@@ -12,7 +12,7 @@ import { defineMessages } from 'react-intl';
 import * as IntlMessageFormat  from 'intl-messageformat';
 import { timeAgoString }  from '../mastodon/components/relative_timestamp';
 import { delegate }  from '@rails/ujs';
-import * as emojify  from '../mastodon/features/emoji/emoji';
+import emojify  from '../mastodon/features/emoji/emoji';
 import { getLocale }  from '../mastodon/locales';
 import React  from 'react';
 import ReactDOM  from 'react-dom';