d552f8f4f695beee799e31a5e52d02e9fc5c7480
Renaud Chaput <renchap@gmail.com>
2023-05-09 22:48:40 +0200

    Remove extra eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -260,7 +260,6 @@ module.exports = {
 
       extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:react/recommended',
