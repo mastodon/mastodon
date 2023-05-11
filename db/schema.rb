fca14bb671f9fecf3a385ee91f860ad17e6aa071
Lina <20880695+necropolina@users.noreply.github.com>
2023-05-10 23:10:25 -0400

    remove added extra line

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2023_05_10_155710) do
 
   # These are extensions that must be enabled in order to support this database
