11157822248d71f3a26954d2e8cf89f9c8987f3c
Matt Jankowski <matt@jankowski.online>
2023-05-12 09:52:21 -0400

    Replace i18n view spec with helper spec
    The problem we are trying to catch here is not specific to that view
    partial, and the set up there felt heavy for what we are actually trying
    to catch. This simplifies the spec and converts from view to helper
    spec.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -259,6 +259,36 @@ describe ApplicationHelper do
     end
   end
 
+  describe 'the built-in rails translate helper' do
+    subject { helper.t('generic.validation_errors', count: 1) }
+
+    context 'with the `en` locale which has `one` and `other` plural values' do
+      around do |example|
+        I18n.with_locale(:en) do
+          example.run
+        end
+      end
+
+      it 'translates to `en` without error' do
+        expect { subject }.to_not raise_error
+        expect(subject).to match(/the error below/)
+      end
+    end
+
+    context 'with the `my` locale which has only `other` plural value' do
+      around do |example|
+        I18n.with_locale(:my) do
+          example.run
+        end
+      end
+
+      it 'translates to `my` without error' do
+        expect { subject }.to_not raise_error
+        expect(subject).to match(/1/)
+      end
+    end
+  end
+
   describe 'visibility_icon' do
     it 'returns a globe icon for a public visible status' do
       result = helper.visibility_icon Status.new(visibility: 'public')
