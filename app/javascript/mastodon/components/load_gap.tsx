94d1d3061ba0cf6b27e759274606c5737ce7015d
taichi221228 <taichi221228@icloud.com>
2023-05-16 03:23:24 +0000

    Rename `load_gap.jsx` to tsx and FC

--- app/javascript/mastodon/components/load_gap.tsx
+++ app/javascript/mastodon/components/load_gap.tsx
@@ -1,17 +1,20 @@
 import React, { useCallback } from 'react';
-import { injectIntl, defineMessages, InjectedIntl } from 'react-intl';
+
+import type { InjectedIntl } from 'react-intl';
+import { injectIntl, defineMessages } from 'react-intl';
+
 import { Icon } from 'mastodon/components/icon';
 
 const messages = defineMessages({
   load_more: { id: 'status.load_more', defaultMessage: 'Load more' },
 });
 
-type Props = {
-  disabled: boolean,
-  maxId: string,
-  onClick: (maxId: string) => void,
-  intl: InjectedIntl
-};
+interface Props {
+  disabled: boolean;
+  maxId: string;
+  onClick: (maxId: string) => void;
+  intl: InjectedIntl;
+}
 
 const LoadGap: React.FC<Props> = ({ disabled, maxId, onClick, intl }) => {
   const handleClick = useCallback(() => {
@@ -19,7 +22,12 @@ const LoadGap: React.FC<Props> = ({ disabled, maxId, onClick, intl }) => {
   }, [maxId, onClick]);
 
   return (
-    <button className='load-more load-gap' disabled={disabled} onClick={handleClick} aria-label={intl.formatMessage(messages.load_more)}>
+    <button
+      className='load-more load-gap'
+      disabled={disabled}
+      onClick={handleClick}
+      aria-label={intl.formatMessage(messages.load_more)}
+    >
       <Icon id='ellipsis-h' />
     </button>
   );
