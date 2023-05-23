495c11dae6b8f1ac3599bb106c4a9912685228c2
Renaud Chaput <renchap@gmail.com>
2023-05-24 01:01:11 +0200

    Adjust formatjs formatted to match current behavior

--- config/formatjs-formatter.js
+++ config/formatjs-formatter.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
+const currentTranslations = require(path.join(__dirname, "../app/javascript/mastodon/locales/en.json"));
+
 exports.format = (msgs) => {
   const results = {};
   for (const [id, msg] of Object.entries(msgs)) {
-    results[id] = msg.defaultMessage;
+    results[id] = currentTranslations[id] || msg.defaultMessage;
   }
   return results;
 };
