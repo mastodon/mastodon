60a0aa056366e774437ff7b2d9cd5b4e5b8c7c2f
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-05-25 10:47:25 -0300

    use a before_action filter for param validation

--- app/controllers/api/v1/accounts/pictures_controller.rb
+++ app/controllers/api/v1/accounts/pictures_controller.rb
@@ -4,10 +4,9 @@ class Api::V1::Accounts::PicturesController < Api::BaseController
   before_action -> { doorkeeper_authorize! :write, :'write:accounts' }
   before_action :require_user!
   before_action :set_picture
+  before_action :validate_picture_param
 
   def destroy
-    raise(Mastodon::InvalidParameterError, 'Picture must be either "avatar" or "header"') unless valid_picture?
-
     @account = current_account
     UpdateAccountService.new.call(@account, { @picture => nil, "#{@picture}_remote_url" => '' }, raise_error: true)
     ActivityPub::UpdateDistributionWorker.perform_async(@account.id)
@@ -18,6 +17,10 @@ class Api::V1::Accounts::PicturesController < Api::BaseController
 
   def set_picture
     @picture = params[:picture]
+  end
+
+  def validate_picture_param
+    raise(Mastodon::InvalidParameterError, 'Picture must be either "avatar" or "header"') unless valid_picture?
   end
 
   def valid_picture?
