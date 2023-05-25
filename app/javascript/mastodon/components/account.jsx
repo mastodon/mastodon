6fb21d5ccf8a3dcb60a95fbc1aa3a352d0c6dbba
Claire <claire.github-309c@sitedethib.com>
2023-05-25 10:25:42 +0200

    Add aria-hidden to decorative separator in account component

--- app/javascript/mastodon/components/account.jsx
+++ app/javascript/mastodon/components/account.jsx
@@ -143,7 +143,7 @@ class Account extends ImmutablePureComponent {
     const firstVerifiedField = account.get('fields').find(item => !!item.get('verified_at'));
 
     if (firstVerifiedField) {
-      verification = <> <span className='separator'>·</span> <VerifiedBadge link={firstVerifiedField.get('value')} /></>;
+      verification = <> <span className='separator' aria-hidden='true'>·</span> <VerifiedBadge link={firstVerifiedField.get('value')} /></>;
     }
 
     return (
