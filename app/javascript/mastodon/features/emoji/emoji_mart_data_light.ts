2ec62fe86bd79531b73f7a2374deb9b813484dc2
taichi221228 <taichi221228@icloud.com>
2023-05-26 03:23:51 +0000

    Add type

--- app/javascript/mastodon/features/emoji/emoji_mart_data_light.ts
+++ app/javascript/mastodon/features/emoji/emoji_mart_data_light.ts
@@ -1,14 +1,45 @@
-/* eslint-disable */
-// @ts-nocheck
 // The output of this module is designed to mimic emoji-mart's
 // "data" object, such that we can use it for a light version of emoji-mart's
 // emojiIndex.search functionality.
-import emojiCompressed from './emoji_compressed';
+import emojiCompressed_ from './emoji_compressed';
 import { unicodeToUnifiedName } from './unicode_to_unified_name';
 
+type FilenameData = string[];
+type Native = string;
+type ShortName = string;
+type Search = string;
+type Unified = string;
+
+type SearchData = [Native, ShortName[], Search, Unified];
+
+interface ShortCodesToEmojiData {
+  [key: string]: [FilenameData, SearchData];
+}
+type Skins = null;
+interface Category {
+  id: string;
+  name: string;
+  emojis: string[];
+}
+
+type Emojis = {
+  [key in keyof ShortCodesToEmojiData]: {
+    native: Native;
+    search: Search;
+    short_names: ShortName[];
+    unified: Unified;
+  };
+};
+
+const emojiCompressed = emojiCompressed_ as [
+  ShortCodesToEmojiData,
+  Skins,
+  Category[],
+  ShortName[]
+];
 const [shortCodesToEmojiData, skins, categories, short_names] = emojiCompressed;
 
-const emojis = {};
+const emojis: Emojis = {};
 
 // decompress
 Object.keys(shortCodesToEmojiData).forEach((shortCode) => {
@@ -16,7 +47,7 @@ Object.keys(shortCodesToEmojiData).forEach((shortCode) => {
     filenameData, // eslint-disable-line @typescript-eslint/no-unused-vars
     searchData,
   ] = shortCodesToEmojiData[shortCode];
-  let [native, short_names, search, unified] = searchData;
+  let [native, short_names, search, unified] = searchData; // eslint-disable-line prefer-const
 
   if (!unified) {
     // unified name can be derived from unicodeToUnifiedName
