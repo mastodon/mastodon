374fcbe4e561250fbdd4083daa7c33399b22b913
Matt Jankowski <matt@jankowski.online>
2023-05-30 10:01:13 -0400

    Add coverage for admin/relays #new and #create actions

--- spec/controllers/admin/relays_controller_spec.rb
+++ spec/controllers/admin/relays_controller_spec.rb
@@ -18,4 +18,42 @@ describe Admin::RelaysController do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'GET #new' do
+    it 'returns http success and renders view' do
+      get :new
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid data' do
+      let(:inbox_url) { 'https://example.com/inbox' }
+
+      before do
+        stub_request(:post, inbox_url).to_return(status: 200)
+      end
+
+      it 'creates a new relay and redirects' do
+        expect do
+          post :create, params: { relay: { inbox_url: inbox_url } }
+        end.to change(Relay, :count).by(1)
+
+        expect(response).to redirect_to(admin_relays_path)
+      end
+    end
+
+    context 'with invalid data' do
+      it 'does not create new a relay and renders new' do
+        expect do
+          post :create, params: { relay: { inbox_url: 'invalid' } }
+        end.to_not change(Relay, :count)
+
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
