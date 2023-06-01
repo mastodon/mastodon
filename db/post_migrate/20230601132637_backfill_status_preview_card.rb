e3e074e3f2a9861ce51953f7f1f8084c26daa00f
Claire <claire.github-309c@sitedethib.com>
2023-06-01 16:22:27 +0200

    Backfill statuses.preview_card_id

---
+++ db/post_migrate/20230601132637_backfill_status_preview_card.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class BackfillStatusPreviewCard < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
+  class PreviewCardStatusJoin < ApplicationRecord
+    self.table_name = 'preview_cards_statuses'
+    self.primary_key = 'status_id'
+  end
+
+  class Status < ApplicationRecord; end
+
+  def up
+    PreviewCardStatusJoin.in_batches do |preview_card_status_join|
+      Status.where(id: preview_card_status_join.ids)
+            .update_all( # rubocop:disable Rails/SkipsModelValidations
+              'preview_card_id = (SELECT preview_card_id FROM preview_cards_statuses WHERE status_id = statuses.id LIMIT 1)'
+            )
+    end
+  end
+
+  def down; end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_01_125204) do
+ActiveRecord::Schema.define(version: 2023_06_01_132637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
