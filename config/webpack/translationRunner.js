aea67d448bc7974aa4c342f5c1987ec4ee2681ea
Nick Schonning <nschonni@gmail.com>
2023-06-02 14:01:36 -0400

    Cleanup old translationRunner (#25241)

--- config/webpack/translationRunner.js
+++
@@ -1,3 +0,0 @@
-console.error("The localisation functionality has been refactored, please see the Localisation section in the development documentation (https://docs.joinmastodon.org/dev/code/#localizations)");
-
-process.exit(1);
