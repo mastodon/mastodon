4103a9deb78e5f34d8238f19f4134f355d51ea2b
Claire <claire.github-309c@sitedethib.com>
2023-06-02 13:04:35 +0200

    Change update order to backfill newer statuses first

--- db/post_migrate/20230601132637_backfill_status_preview_card.rb
+++ db/post_migrate/20230601132637_backfill_status_preview_card.rb
@@ -11,7 +11,7 @@ class BackfillStatusPreviewCard < ActiveRecord::Migration[6.1]
   class Status < ApplicationRecord; end
 
   def up
-    PreviewCardStatusJoin.in_batches do |preview_card_status_join|
+    PreviewCardStatusJoin.in_batches(order: :desc) do |preview_card_status_join|
       Status.where(id: preview_card_status_join.ids)
             .update_all( # rubocop:disable Rails/SkipsModelValidations
               'preview_card_id = (SELECT preview_card_id FROM preview_cards_statuses WHERE status_id = statuses.id LIMIT 1)'
