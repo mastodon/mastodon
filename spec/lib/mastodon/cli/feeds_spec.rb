d5a2e2ee6e88dfdf07e546205920ff6e497ef583
Matt Jankowski <matt@jankowski.online>
2023-06-03 12:03:38 -0400

    Add coverage for `CLI::Feeds#clear` command

--- spec/lib/mastodon/cli/feeds_spec.rb
+++ spec/lib/mastodon/cli/feeds_spec.rb
@@ -4,9 +4,23 @@ require 'rails_helper'
 require 'mastodon/cli/feeds'
 
 describe Mastodon::CLI::Feeds do
+  let(:cli) { described_class.new }
+
   describe '.exit_on_failure?' do
     it 'returns true' do
       expect(described_class.exit_on_failure?).to be true
+    end
+  end
+
+  describe '#clear' do
+    before do
+      allow(redis).to receive(:del).with(redis.keys('feed:*'))
+    end
+
+    it 'clears the redis `feed:*` namespace' do
+      expect { cli.invoke(:clear) }.to output(
+        a_string_including('OK')
+      ).to_stdout
     end
   end
 end
