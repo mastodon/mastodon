f378f104042955ec807103e4283110b06ba191d7
Claire <claire.github-309c@sitedethib.com>
2023-06-07 01:53:50 +0200

    Fix compatibility of recent migration with PostgreSQL 10 (#25324)

--- db/schema.rb
+++ db/schema.rb
@@ -567,7 +567,7 @@ ActiveRecord::Schema.define(version: 2023_06_05_085710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "replies_policy", default: 0, null: false
-    t.boolean "exclusive", default: false
+    t.boolean "exclusive", default: false, null: false
     t.index ["account_id"], name: "index_lists_on_account_id"
   end
 
