ec6538dcefcc63483276798e557085beed09ffdb
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2023-06-10 18:24:37 +0200

    Fix logging of messages that are binary before closing their connection (#25361)

--- streaming/index.js
+++ streaming/index.js
@@ -1219,7 +1219,7 @@ const startWorker = async (workerId) => {
 
     ws.on('message', (data, isBinary) => {
       if (isBinary) {
-        log.debug('Received binary data, closing connection');
+        log.warn('socket', 'Received binary data, closing connection');
         ws.close(1003, 'The mastodon streaming server does not support binary messages');
         return;
       }
