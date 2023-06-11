e37f688093a7f0b66e41e0fbca67c9bbaf12b804
alfe <alfe10251@gmail.com>
2023-06-11 11:46:19 +0900

    Rewrite `<LoadingIndicator />` as FC, TS and isolate

---
+++ app/javascript/mastodon/components/loading_indicator.tsx
@@ -0,0 +1,9 @@
+import { CircularProgress } from './circular_progress';
+
+export const LoadingIndicator: React.FC = () => (
+  <div className='loading-indicator'>
+    <CircularProgress size={50} strokeWidth={6} />
+  </div>
+);
+
+export default LoadingIndicator;
