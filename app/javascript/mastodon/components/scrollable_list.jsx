dfaf59d99a2aa1f913608faee978bc3e1586a3ce
alfe <alfe10251@gmail.com>
2023-06-11 11:47:18 +0900

    Rewrite <LoadPending /> as FC and TS (#25363)

--- app/javascript/mastodon/components/scrollable_list.jsx
+++ app/javascript/mastodon/components/scrollable_list.jsx
@@ -16,7 +16,7 @@ import { attachFullscreenListener, detachFullscreenListener, isFullscreen } from
 import IntersectionObserverWrapper from '../features/ui/util/intersection_observer_wrapper';
 
 import { LoadMore } from './load_more';
-import LoadPending from './load_pending';
+import { LoadPending } from './load_pending';
 import LoadingIndicator from './loading_indicator';
 
 const MOUSE_IDLE_DELAY = 300;
