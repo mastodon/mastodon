cbb73d85044db06f198dc1cc1604ca3b11fa59af
taichi221228 <taichi221228@icloud.com>
2023-06-13 01:02:02 +0000

    Add `<LoadingIndicator />` type

--- app/javascript/mastodon/components/loading_indicator.tsx
+++ app/javascript/mastodon/components/loading_indicator.tsx
@@ -1,6 +1,6 @@
 import { CircularProgress } from './circle_progress';
 
-export const LoadingIndicator = () => (
+export const LoadingIndicator: React.FC = () => (
   <div className='loading-indicator'>
     <CircularProgress size={50} strokeWidth={6} />
   </div>
