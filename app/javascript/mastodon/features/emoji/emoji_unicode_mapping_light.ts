5f546a784e4fcc0ba4621ffeec7c9476c0b27312
taichi221228 <taichi221228@icloud.com>
2023-06-15 00:39:18 +0000

    Refactor `emoji_unicode_mapping_light.ts`

--- app/javascript/mastodon/features/emoji/emoji_unicode_mapping_light.ts
+++ app/javascript/mastodon/features/emoji/emoji_unicode_mapping_light.ts
@@ -1,12 +1,20 @@
-/* eslint-disable */
-// @ts-nocheck
-
 // A mapping of unicode strings to an object containing the filename
 // (i.e. the svg filename) and a shortCode intended to be shown
 // as a "title" attribute in an HTML element (aka tooltip).
 
+import type {
+  FilenameData,
+  ShortCodesToEmojiDataKey,
+} from './emoji_compressed';
 import emojiCompressed from './emoji_compressed';
 import { unicodeToFilename } from './unicode_to_filename';
+
+type UnicodeMapping = {
+  [key in FilenameData[number][0]]: {
+    shortCode: ShortCodesToEmojiDataKey;
+    filename: FilenameData[number][number];
+  };
+};
 
 const [
   shortCodesToEmojiData,
@@ -17,28 +25,36 @@ const [
 ] = emojiCompressed;
 
 // decompress
-const unicodeMapping = {};
+const unicodeMapping: UnicodeMapping = {};
 
-function processEmojiMapData(emojiMapData, shortCode) {
-  let [native, filename] = emojiMapData;
+function processEmojiMapData(
+  emojiMapData: FilenameData[number],
+  shortCode?: ShortCodesToEmojiDataKey
+) {
+  const [native, _filename] = emojiMapData;
+  let filename = emojiMapData[1];
   if (!filename) {
     // filename name can be derived from unicodeToFilename
     filename = unicodeToFilename(native);
   }
   unicodeMapping[native] = {
-    shortCode: shortCode,
-    filename: filename,
+    shortCode,
+    filename,
   };
 }
 
-Object.keys(shortCodesToEmojiData).forEach((shortCode) => {
-  let [filenameData] = shortCodesToEmojiData[shortCode];
-  filenameData.forEach((emojiMapData) =>
-    processEmojiMapData(emojiMapData, shortCode)
-  );
-});
+Object.keys(shortCodesToEmojiData).forEach(
+  (shortCode: ShortCodesToEmojiDataKey) => {
+    if (shortCode === undefined) return;
+    const [filenameData, _searchData] = shortCodesToEmojiData[shortCode];
+    filenameData.forEach((emojiMapData) => {
+      processEmojiMapData(emojiMapData, shortCode);
+    });
+  }
+);
+
 emojisWithoutShortCodes.forEach((emojiMapData) =>
   processEmojiMapData(emojiMapData)
 );
 
-export default unicodeMapping;
+export default unicodeMapping; // eslint-disable-line import/no-default-export
