a8c1c8bd377263677bfb654513a4160caeac77bb
Claire <claire.github-309c@sitedethib.com>
2023-06-22 17:54:43 +0200

    Fix j/k keyboard shortcuts on some status lists (#25554)

--- app/javascript/mastodon/features/favourited_statuses/index.jsx
+++ app/javascript/mastodon/features/favourited_statuses/index.jsx
@@ -15,13 +15,14 @@ import { fetchFavouritedStatuses, expandFavouritedStatuses } from 'mastodon/acti
 import ColumnHeader from 'mastodon/components/column_header';
 import StatusList from 'mastodon/components/status_list';
 import Column from 'mastodon/features/ui/components/column';
+import { getStatusList } from 'mastodon/selectors';
 
 const messages = defineMessages({
   heading: { id: 'column.favourites', defaultMessage: 'Favourites' },
 });
 
 const mapStateToProps = state => ({
-  statusIds: state.getIn(['status_lists', 'favourites', 'items']),
+  statusIds: getStatusList(state, 'favourites'),
   isLoading: state.getIn(['status_lists', 'favourites', 'isLoading'], true),
   hasMore: !!state.getIn(['status_lists', 'favourites', 'next']),
 });
