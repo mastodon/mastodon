42e2db4af6a68d7ac00c228b0efd2934e486d907
alfe <alfe10251@gmail.com>
2023-06-27 09:19:02 +0900

    fix: reflect reviews

--- app/javascript/mastodon/components/short_number.tsx
+++ app/javascript/mastodon/components/short_number.tsx
@@ -4,63 +4,41 @@ import { FormattedMessage, FormattedNumber } from 'react-intl';
 
 import { toShortNumber, pluralReady, DECIMAL_UNITS } from '../utils/numbers';
 
-/**
- * Custom renderer for numbers
- * @param {JSX.Element} displayNumber Number to display
- * @param {number} pluralReady Number used for pluralization
- * @returns {JSX.Element} Final render of number
- */
-interface ShortNumberRenderer {
-  (displayNumber: JSX.Element, pluralReady: number): JSX.Element;
-}
+type ShortNumberRenderer = (
+  displayNumber: JSX.Element,
+  pluralReady: number
+) => JSX.Element;
 
-/**
- * Component that renders short big number to a shorter version
- * @param {ShortNumberProps} props  Props for the component
- * @param {number} props.value value Number to display in short variant
- * @param {ShortNumberRenderer} [props.renderer]
- * Custom renderer for numbers, provided as a prop. If another renderer
- * passed as a child of this component, this prop won't be used.
- * @param {ShortNumberRenderer} [props.children]
- * Custom renderer for numbers, provided as a child. If another renderer
- * passed as a prop of this component, this one will be used instead.
- * @returns {JSX.Element} - Rendered number.
- */
 interface ShortNumberProps {
   value: number;
   renderer?: ShortNumberRenderer;
   children?: ShortNumberRenderer;
 }
+
 export const ShortNumberRenderer: React.FC<ShortNumberProps> = ({
   value,
   renderer,
   children,
-}): JSX.Element => {
+}) => {
   const shortNumber = toShortNumber(value);
   const [, division] = shortNumber;
 
-  if (children != null && renderer != null) {
+  if (children && renderer) {
     console.warn(
       'Both renderer prop and renderer as a child provided. This is a mistake and you really should fix that. Only renderer passed as a child will be used.'
     );
   }
 
-  const customRenderer = children != null ? children : renderer || null;
+  const customRenderer = children || renderer || null;
 
   const displayNumber = <ShortNumberCounter value={shortNumber} />;
 
-  return customRenderer != null
+  return customRenderer
     ? customRenderer(displayNumber, pluralReady(value, division))
     : displayNumber;
 };
 export const ShortNumber = memo(ShortNumberRenderer);
 
-/**
- * Renders short number into corresponding localizable react fragment
- * @param {ShortNumberCounterProps} props Props for the component
- * @param {number[]} props.value Number array: shorten number, unit of shorten number and maximum fraction digits
- * @returns {JSX.Element} FormattedMessage ready to be embedded in code
- */
 interface ShortNumberCounterProps {
   value: number[];
 }
