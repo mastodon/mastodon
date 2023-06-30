bc312c712c9a7a6cce287177242c8f4f139ce1c8
Claire <claire.github-309c@sitedethib.com>
2023-06-30 13:37:20 +0200

    Rewrite Firehose as a functional component

--- app/javascript/mastodon/features/firehose/index.jsx
+++ app/javascript/mastodon/features/firehose/index.jsx
@@ -1,220 +1,210 @@
 import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
+import { useRef, useCallback, useEffect } from 'react';
 
-import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
+import { useIntl, defineMessages, FormattedMessage } from 'react-intl';
 
 import { Helmet } from 'react-helmet';
 import { NavLink } from 'react-router-dom';
 
-import { connect } from 'react-redux';
-
 import { addColumn } from 'mastodon/actions/columns';
+import { changeSetting } from 'mastodon/actions/settings';
 import { connectPublicStream, connectCommunityStream } from 'mastodon/actions/streaming';
 import { expandPublicTimeline, expandCommunityTimeline } from 'mastodon/actions/timelines';
 import DismissableBanner from 'mastodon/components/dismissable_banner';
-import { domain } from 'mastodon/initial_state';
+import initialState, { domain } from 'mastodon/initial_state';
+import { useAppDispatch, useAppSelector } from 'mastodon/store';
 
 import Column from '../../components/column';
 import ColumnHeader from '../../components/column_header';
+import SettingToggle from '../notifications/components/setting_toggle';
 import StatusListContainer from '../ui/containers/status_list_container';
-
-import ColumnSettingsContainer from './containers/column_settings_container';
 
 const messages = defineMessages({
   title: { id: 'column.firehose', defaultMessage: 'Live feeds' },
 });
 
-const mapStateToProps = (state, { feedType }) => {
-  const onlyMedia = state.getIn(['settings', 'firehose', 'onlyMedia'], false);
+// TODO: use a proper React context later on
+const useIdentity = () => ({
+  signedIn: !!initialState.meta.me,
+  accountId: initialState.meta.me,
+  disabledAccountId: initialState.meta.disabled_account_id,
+  accessToken: initialState.meta.access_token,
+  permissions: initialState.role ? initialState.role.permissions : 0,
+});
 
-  return {
-    onlyMedia,
-    hasUnread: state.getIn(['timelines', `${feedType}${onlyMedia ? ':media' : ''}`, 'unread'], 0) > 0,
-  }
+const ColumnSettings = () => {
+  const dispatch = useAppDispatch();
+  const settings = useAppSelector((state) => state.getIn(['settings', 'firehose']));
+  const onChange = useCallback(
+    (key, checked) => dispatch(changeSetting(['firehose', ...key], checked)),
+    [dispatch],
+  );
+
+  return (
+    <div>
+      <div className='column-settings__row'>
+        <SettingToggle
+          settings={settings}
+          settingPath={['onlyMedia']}
+          onChange={onChange}
+          label={<FormattedMessage id='community.column_settings.media_only' defaultMessage='Media only' />}
+        />
+      </div>
+    </div>
+  );
 };
 
-class Firehose extends PureComponent {
+const Firehose = ({ feedType, multiColumn }) => {
+  const dispatch = useAppDispatch();
+  const intl = useIntl();
+  const { signedIn } = useIdentity();
+  const columnRef = useRef(null);
 
-  static contextTypes = {
-    router: PropTypes.object,
-    identity: PropTypes.object,
-  };
+  const onlyMedia = useAppSelector((state) => state.getIn(['settings', 'firehose', 'onlyMedia'], false));
+  const hasUnread = useAppSelector((state) => state.getIn(['timelines', `${feedType}${onlyMedia ? ':media' : ''}`, 'unread'], 0) > 0);
 
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    intl: PropTypes.object.isRequired,
-    multiColumn: PropTypes.bool,
-    hasUnread: PropTypes.bool,
-    onlyMedia: PropTypes.bool,
-    feedType: PropTypes.string, // TODO: type this properly
-  };
-
-  handlePin = () => {
-    const { dispatch, onlyMedia, feedType } = this.props;
-
-    switch(feedType) {
-    case 'community':
-      dispatch(addColumn('COMMUNITY', { other: { onlyMedia } }));
-      break;
-    case 'public':
-      dispatch(addColumn('PUBLIC', { other: { onlyMedia } }));
-      break;
-    case 'public:remote':
-      dispatch(addColumn('REMOTE', { other: { onlyMedia, onlyRemote: true } }));
-      break;
-    }
-  };
-
-  handleHeaderClick = () => {
-    this.column.scrollTop();
-  };
-
-  componentDidMount () {
-    this._connectFeed();
-  }
-
-  componentDidUpdate (prevProps) {
-    if (prevProps.onlyMedia !== this.props.onlyMedia || prevProps.feedType !== this.props.feedType) {
-      if (this.disconnect) {
-        this.disconnect();
+  const handlePin = useCallback(
+    () => {
+      switch(feedType) {
+      case 'community':
+        dispatch(addColumn('COMMUNITY', { other: { onlyMedia } }));
+        break;
+      case 'public':
+        dispatch(addColumn('PUBLIC', { other: { onlyMedia } }));
+        break;
+      case 'public:remote':
+        dispatch(addColumn('REMOTE', { other: { onlyMedia, onlyRemote: true } }));
+        break;
       }
+    },
+    [dispatch, onlyMedia, feedType],
+  );
 
-      this._connectFeed();
-    }
-  }
+  const handleLoadMore = useCallback(
+    (maxId) => {
+      switch(feedType) {
+      case 'community':
+        dispatch(expandCommunityTimeline({ onlyMedia }));
+        break;
+      case 'public':
+        dispatch(expandPublicTimeline({ maxId, onlyMedia }));
+        break;
+      case 'public:remote':
+        dispatch(expandPublicTimeline({ maxId, onlyMedia, onlyRemote: true }));
+        break;
+      }
+    },
+    [dispatch, onlyMedia, feedType],
+  );
 
-  _connectFeed = () => {
-    const { dispatch, onlyMedia, feedType } = this.props;
-    const { signedIn } = this.context.identity;
+  const handleHeaderClick = useCallback(() => columnRef.current?.scrollTop(), []);
+
+  useEffect(() => {
+    let disconnect;
 
     switch(feedType) {
     case 'community':
       dispatch(expandCommunityTimeline({ onlyMedia }));
       if (signedIn) {
-        this.disconnect = dispatch(connectCommunityStream({ onlyMedia }));
+        disconnect = dispatch(connectCommunityStream({ onlyMedia }));
       }
       break;
     case 'public':
       dispatch(expandPublicTimeline({ onlyMedia }));
       if (signedIn) {
-        this.disconnect = dispatch(connectPublicStream({ onlyMedia }));
+        disconnect = dispatch(connectPublicStream({ onlyMedia }));
       }
       break;
     case 'public:remote':
       dispatch(expandPublicTimeline({ onlyMedia, onlyRemote: true }));
       if (signedIn) {
-        this.disconnect = dispatch(connectPublicStream({ onlyMedia, onlyRemote: true }));
+        disconnect = dispatch(connectPublicStream({ onlyMedia, onlyRemote: true }));
       }
       break;
     }
-  }
 
-  componentWillUnmount () {
-    if (this.disconnect) {
-      this.disconnect();
-      this.disconnect = null;
-    }
-  }
+    return () => disconnect?.();
+  }, [dispatch, signedIn, feedType, onlyMedia]);
 
-  setRef = c => {
-    this.column = c;
-  };
-
-  handleLoadMore = maxId => {
-    const { dispatch, onlyMedia, feedType } = this.props;
-
-    switch(feedType) {
-    case 'community':
-      dispatch(expandCommunityTimeline({ onlyMedia }));
-      break;
-    case 'public':
-      dispatch(expandPublicTimeline({ maxId, onlyMedia }));
-      break;
-    case 'public:remote':
-      dispatch(expandPublicTimeline({ maxId, onlyMedia, onlyRemote: true }));
-      break;
-    }
-  };
-
-  render () {
-    const { intl, hasUnread, multiColumn, onlyMedia, feedType } = this.props;
-
-    const prependBanner = feedType === 'community' ? (
-      <DismissableBanner id='community_timeline'>
-        <FormattedMessage
-          id='dismissable_banner.community_timeline'
-          defaultMessage='These are the most recent public posts from people whose accounts are hosted by {domain}.'
-          values={{ domain }}
-        />
-      </DismissableBanner>
-    ) : (
-     <DismissableBanner id='public_timeline'>
-       <FormattedMessage
-         id='dismissable_banner.public_timeline'
-         defaultMessage='These are the most recent public posts from people on this and other servers of the decentralized network that this server knows about.'
-       />
-     </DismissableBanner>
-    );
-
-    const emptyMessage = feedType === 'community' ? (
+  const prependBanner = feedType === 'community' ? (
+    <DismissableBanner id='community_timeline'>
       <FormattedMessage
-        id='empty_column.community'
-        defaultMessage='The local timeline is empty. Write something publicly to get the ball rolling!'
+        id='dismissable_banner.community_timeline'
+        defaultMessage='These are the most recent public posts from people whose accounts are hosted by {domain}.'
+        values={{ domain }}
       />
-    ) : (
+    </DismissableBanner>
+  ) : (
+   <DismissableBanner id='public_timeline'>
      <FormattedMessage
-       id='empty_column.public'
-       defaultMessage='There is nothing here! Write something publicly, or manually follow users from other servers to fill it up'
+       id='dismissable_banner.public_timeline'
+       defaultMessage='These are the most recent public posts from people on this and other servers of the decentralized network that this server knows about.'
      />
-    );
+   </DismissableBanner>
+  );
 
-    return (
-      <Column bindToDocument={!multiColumn} ref={this.setRef} label={intl.formatMessage(messages.title)}>
-        <ColumnHeader
-          icon='globe'
-          active={hasUnread}
-          title={intl.formatMessage(messages.title)}
-          onPin={this.handlePin}
-          onClick={this.handleHeaderClick}
-          multiColumn={multiColumn}
-        >
-          <ColumnSettingsContainer />
-        </ColumnHeader>
+  const emptyMessage = feedType === 'community' ? (
+    <FormattedMessage
+      id='empty_column.community'
+      defaultMessage='The local timeline is empty. Write something publicly to get the ball rolling!'
+    />
+  ) : (
+   <FormattedMessage
+     id='empty_column.public'
+     defaultMessage='There is nothing here! Write something publicly, or manually follow users from other servers to fill it up'
+   />
+  );
 
-        <div className='scrollable scrollable--flex'>
-          <div className='account__section-headline'>
-            <NavLink exact to='/public/local'>
-              <FormattedMessage tagName='div' id='firehose.local' defaultMessage='Local' />
-            </NavLink>
+  return (
+    <Column bindToDocument={!multiColumn} ref={columnRef} label={intl.formatMessage(messages.title)}>
+      <ColumnHeader
+        icon='globe'
+        active={hasUnread}
+        title={intl.formatMessage(messages.title)}
+        onPin={handlePin}
+        onClick={handleHeaderClick}
+        multiColumn={multiColumn}
+      >
+        <ColumnSettings />
+      </ColumnHeader>
 
-            <NavLink exact to='/public/remote'>
-              <FormattedMessage tagName='div' id='firehose.remote' defaultMessage='Remote' />
-            </NavLink>
+      <div className='scrollable scrollable--flex'>
+        <div className='account__section-headline'>
+          <NavLink exact to='/public/local'>
+            <FormattedMessage tagName='div' id='firehose.local' defaultMessage='Local' />
+          </NavLink>
 
-            <NavLink exact to='/public'>
-              <FormattedMessage tagName='div' id='firehose.all' defaultMessage='All' />
-            </NavLink>
-          </div>
+          <NavLink exact to='/public/remote'>
+            <FormattedMessage tagName='div' id='firehose.remote' defaultMessage='Remote' />
+          </NavLink>
 
-          <StatusListContainer
-            prepend={prependBanner}
-            timelineId={`${feedType}${onlyMedia ? ':media' : ''}`}
-            onLoadMore={this.handleLoadMore}
-            trackScroll
-            scrollKey='firehose'
-            emptyMessage={emptyMessage}
-            bindToDocument={!multiColumn}
-          />
+          <NavLink exact to='/public'>
+            <FormattedMessage tagName='div' id='firehose.all' defaultMessage='All' />
+          </NavLink>
         </div>
 
-        <Helmet>
-          <title>{intl.formatMessage(messages.title)}</title>
-          <meta name='robots' content='noindex' />
-        </Helmet>
-      </Column>
-    );
-  }
+        <StatusListContainer
+          prepend={prependBanner}
+          timelineId={`${feedType}${onlyMedia ? ':media' : ''}`}
+          onLoadMore={handleLoadMore}
+          trackScroll
+          scrollKey='firehose'
+          emptyMessage={emptyMessage}
+          bindToDocument={!multiColumn}
+        />
+      </div>
 
+      <Helmet>
+        <title>{intl.formatMessage(messages.title)}</title>
+        <meta name='robots' content='noindex' />
+      </Helmet>
+    </Column>
+  );
 }
 
-export default connect(mapStateToProps)(injectIntl(Firehose));
+Firehose.propTypes = {
+  multiColumn: PropTypes.bool,
+  feedType: PropTypes.string,
+};
+
+export default Firehose;
