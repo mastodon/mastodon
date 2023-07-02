ba06a2f1044aa81a99eb5fc509611dbb1150d43d
Eugen Rochko <eugen@zeonfederated.com>
2023-07-02 11:14:22 +0200

    Revert "Rails 7 update" (#25667)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,12 +3,11 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << 'node_modules'
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w()
 
 Rails.application.config.assets.initialize_on_precompile = true
