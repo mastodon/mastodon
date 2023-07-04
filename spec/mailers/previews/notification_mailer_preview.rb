48d6c0729ea9a4567889b4d823730e389d30268d
Matt Jankowski <matt@jankowski.online>
2023-07-04 13:19:56 -0400

    Clean up NotificationMailer Preview class

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -5,31 +5,35 @@
 class NotificationMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/mention
   def mention
-    m = Mention.last
-    NotificationMailer.with(recipient: m.account).mention(Notification.find_by(activity: m))
+    activity = Mention.last
+    NotificationMailer.with(recipient: activity.account).mention(notification_for(activity))
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
   def follow
-    f = Follow.last
-    NotificationMailer.with(recipient: m.target_account).follow(Notification.find_by(activity: f))
+    activity = Follow.last
+    NotificationMailer.with(recipient: activity.target_account).follow(notification_for(activity))
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow_request
   def follow_request
-    f = Follow.last
-    NotificationMailer.with(recipient: f.target_account).follow_request(Notification.find_by(activity: f))
+    activity = Follow.last
+    NotificationMailer.with(recipient: activity.target_account).follow_request(notification_for(activity))
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/favourite
   def favourite
-    f = Favourite.last
-    NotificationMailer.with(recipient: f.status.account).favourite(Notification.find_by(activity: f))
+    activity = Favourite.last
+    NotificationMailer.with(recipient: activity.status.account).favourite(notification_for(activity))
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/reblog
   def reblog
-    r = Status.where.not(reblog_of_id: nil).first
-    NotificationMailer.with(recipient: r.reblog.account).reblog(Notification.find_by(activity: r))
+    activity = Status.where.not(reblog_of_id: nil).first
+    NotificationMailer.with(recipient: activity.reblog.account).reblog(notification_for(activity))
+  end
+
+  def notification_for(activity)
+    Notification.find_by(activity: activity)
   end
 end
