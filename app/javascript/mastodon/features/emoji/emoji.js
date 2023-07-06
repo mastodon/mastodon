5052a6b018fe316980a264f843cfaf70a48be673
MiJaJo <tasksystem.john@gmail.com>
2023-07-07 03:18:59 +0900

    ゼロ幅スペース対応によって単独絵文字判定に不具合が出たのを修正

--- app/javascript/mastodon/features/emoji/emoji.js
+++ app/javascript/mastodon/features/emoji/emoji.js
@@ -111,9 +111,9 @@ const emojify = (str, customEmojis = {}) => {
 
   emojifyNode(wrapper, customEmojis);
 
-  if((wrapper.innerHTML.match('h-card') || []).length==1&&(wrapper.innerHTML.match('emojione') || []).length==1&&wrapper.innerHTML.replace(/^<p><span class=.h-card.>.+<\/span>[ (<br>)]+<img [^<]+?><\/p>$/,'')==''){
+  if((wrapper.innerHTML.match('h-card') || []).length==1&&(wrapper.innerHTML.match('emojione') || []).length==1&&wrapper.innerHTML.replace(/^<p><span class=.h-card.>.+<\/span>[ (\u{200b})(<br>)]+<img [^<]+?>[ (\u{200b})(<br>)]?<\/p>$/u,'')==''){
     wrapper.innerHTML = wrapper.innerHTML.replace('emojione','emojione mcd__singleEmoji').replace('<img','<br><img');
-  }else if((wrapper.innerHTML.match('emojione') || []).length==1&&wrapper.innerHTML.replace(/^<p><img [^<]+?><\/p>$/,'')==''){
+  }else if((wrapper.innerHTML.match('emojione') || []).length==1&&wrapper.innerHTML.replace(/^<p>\u{200b}?<img [^<]+?>\u{200b}?<\/p>$/u,'')==''){
     wrapper.innerHTML = wrapper.innerHTML.replace('emojione','emojione mcd__singleEmoji');
   }
 
