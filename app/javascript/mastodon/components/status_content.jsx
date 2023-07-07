mastodon/mastodon

commit 182fd93a076592634d909cca1267cd25c6f87c29

fix read more button overlapping thread line bug (#25706)

app/javascript/styles/mastodon/components.scss
@@ -1127,6 +1127,8 @@ body > [data-popper-placement] {
   }
 
   &--in-thread {
+    $thread-margin: 46px + 10px;
+
     border-bottom: 0;
 
     .status__content,
@@ -1137,8 +1139,12 @@ body > [data-popper-placement] {
     .attachment-list,
     .picture-in-picture-placeholder,
     .status-card {
-      margin-inline-start: 46px + 10px;
-      width: calc(100% - (46px + 10px));
+      margin-inline-start: $thread-margin;
+      width: calc(100% - ($thread-margin));
+    }
+
+    .status__content__read-more-button {
+      margin-inline-start: $thread-margin;
     }
   }
 

commit 4f0b413bfdbe6d2a749b2f7be905c56fa886e2ed

Fix translate button position
Regression from #25706

app/javascript/mastodon/components/status_content.jsx
@@ -44,7 +44,7 @@ class TranslateButton extends PureComponent {
     }
 
     return (
-      <button className='status__content__read-more-button' onClick={onClick}>
+      <button className='status__content__translate-button' onClick={onClick}>
         <FormattedMessage id='status.translate' defaultMessage='Translate' />
       </button>
     );

app/javascript/styles/contrast/diff.scss
@@ -15,7 +15,8 @@
 .status__content a,
 .link-footer a,
 .reply-indicator__content a,
-.status__content__read-more-button {
+.status__content__read-more-button,
+.status__content__translate-button {
   text-decoration: underline;
 
   &:hover,

app/javascript/styles/mastodon/components.scss
@@ -981,7 +981,8 @@ body > [data-popper-placement] {
   max-height: 22px * 15; // 15 lines is roughly above 500 characters
 }
 
-.status__content__read-more-button {
+.status__content__read-more-button,
+.status__content__translate-button {
   display: block;
   font-size: 15px;
   line-height: 22px;