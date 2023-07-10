50c60ada46396d0b7567b132f589c644c68e4e3d
Matt Jankowski <matt@jankowski.online>
2023-07-10 14:39:33 -0400

    Move localized subject mailer shared example to separate file

---
+++ spec/support/examples/mailers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+shared_examples 'localized subject' do |*args, **kwrest|
+  it 'renders subject localized for the locale of the receiver' do
+    locale = :de
+    receiver.update!(locale: locale)
+    expect(mail.subject).to eq I18n.t(*args, **kwrest.merge(locale: locale))
+  end
+
+  it 'renders subject localized for the default locale if the locale of the receiver is unavailable' do
+    receiver.update!(locale: nil)
+    expect(mail.subject).to eq I18n.t(*args, **kwrest.merge(locale: I18n.default_locale))
+  end
+end
