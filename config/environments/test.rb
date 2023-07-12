84c043792a1cc1dcd8e4185603bc9b2dfcec05b9
Matt Jankowski <matt@jankowski.online>
2023-07-12 10:39:02 -0400

    Remove public_file_server config from test env in favor of application.rb config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,12 +20,6 @@ Rails.application.configure do
 
   config.assets_digest = false
 
-  # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
-  }
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
