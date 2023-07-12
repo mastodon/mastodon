d516d025a6296320e5dbb5c6fce7c23e57bf94ca
Claire <claire.github-309c@sitedethib.com>
2023-07-12 14:07:02 +0200

    Fix tests

--- spec/workers/activitypub/distribute_poll_update_worker_spec.rb
+++ spec/workers/activitypub/distribute_poll_update_worker_spec.rb
@@ -6,7 +6,7 @@ describe ActivityPub::DistributePollUpdateWorker do
   subject { described_class.new }
 
   let(:account)  { Fabricate(:account) }
-  let(:follower) { Fabricate(:account, protocol: :activitypub, inbox_url: 'http://example.com') }
+  let(:follower) { Fabricate(:account, protocol: :activitypub, inbox_url: 'http://example.com', domain: 'example.com') }
   let(:poll)     { Fabricate(:poll, account: account) }
   let!(:status)  { Fabricate(:status, account: account, poll: poll) }
 
