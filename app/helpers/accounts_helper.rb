mastodon/mastodon

commit ea10febd257b5b729a50aeb3218389763f5f4b97

fix buttons showing inconsistent styles (#25903)

app/helpers/accounts_helper.rb
@@ -22,7 +22,7 @@ module AccountsHelper
   def account_action_button(account)
     return if account.memorial? || account.moved?
 
-    link_to ActivityPub::TagManager.instance.url_for(account), class: 'button logo-button', target: '_new' do
+    link_to ActivityPub::TagManager.instance.url_for(account), class: 'button', target: '_new' do
       safe_join([logo_as_symbol, t('accounts.follow')])
     end
   end

app/javascript/styles/mastodon/statuses.scss
@@ -77,66 +77,6 @@
   }
 }
 
-.button.logo-button {
-  flex: 0 auto;
-  font-size: 14px;
-  background: darken($ui-highlight-color, 2%);
-  color: $primary-text-color;
-  text-transform: none;
-  line-height: 1.2;
-  height: auto;
-  min-height: 36px;
-  min-width: 88px;
-  white-space: normal;
-  overflow-wrap: break-word;
-  hyphens: auto;
-  padding: 0 15px;
-  border: 0;
-
-  svg {
-    width: 20px;
-    height: auto;
-    vertical-align: middle;
-    margin-inline-end: 5px;
-    fill: $primary-text-color;
-  }
-
-  &:active,
-  &:focus,
-  &:hover {
-    background: $ui-highlight-color;
-  }
-
-  &:disabled,
-  &.disabled {
-    &:active,
-    &:focus,
-    &:hover {
-      background: $ui-primary-color;
-    }
-  }
-
-  &.button--destructive {
-    &:active,
-    &:focus,
-    &:hover {
-      background: $error-red;
-    }
-  }
-
-  @media screen and (max-width: $no-gap-breakpoint) {
-    svg {
-      display: none;
-    }
-  }
-}
-
-a.button.logo-button {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-}
-
 .embed {
   .status__content[data-spoiler='folded'] {
     .e-content {

app/javascript/mastodon/features/account/components/header.jsx
@@ -264,14 +264,14 @@ class Header extends ImmutablePureComponent {
       if (signedIn && !account.get('relationship')) { // Wait until the relationship is loaded
         actionBtn = '';
       } else if (account.getIn(['relationship', 'requested'])) {
-        actionBtn = <Button className={classNames('logo-button', { 'button--with-bell': bellBtn !== '' })} text={intl.formatMessage(messages.cancel_follow_request)} title={intl.formatMessage(messages.requested)} onClick={this.props.onFollow} />;
+        actionBtn = <Button className={classNames({ 'button--with-bell': bellBtn !== '' })} text={intl.formatMessage(messages.cancel_follow_request)} title={intl.formatMessage(messages.requested)} onClick={this.props.onFollow} />;
       } else if (!account.getIn(['relationship', 'blocking'])) {
-        actionBtn = <Button disabled={account.getIn(['relationship', 'blocked_by'])} className={classNames('logo-button', { 'button--destructive': account.getIn(['relationship', 'following']), 'button--with-bell': bellBtn !== '' })} text={intl.formatMessage(account.getIn(['relationship', 'following']) ? messages.unfollow : messages.follow)} onClick={signedIn ? this.props.onFollow : this.props.onInteractionModal} />;
+        actionBtn = <Button disabled={account.getIn(['relationship', 'blocked_by'])} className={classNames({ 'button--destructive': account.getIn(['relationship', 'following']), 'button--with-bell': bellBtn !== '' })} text={intl.formatMessage(account.getIn(['relationship', 'following']) ? messages.unfollow : messages.follow)} onClick={signedIn ? this.props.onFollow : this.props.onInteractionModal} />;
       } else if (account.getIn(['relationship', 'blocking'])) {
-        actionBtn = <Button className='logo-button' text={intl.formatMessage(messages.unblock, { name: account.get('username') })} onClick={this.props.onBlock} />;
+        actionBtn = <Button text={intl.formatMessage(messages.unblock, { name: account.get('username') })} onClick={this.props.onBlock} />;
       }
     } else {
-      actionBtn = <Button className='logo-button' text={intl.formatMessage(messages.edit_profile)} onClick={this.openEditProfile} />;
+      actionBtn = <Button text={intl.formatMessage(messages.edit_profile)} onClick={this.openEditProfile} />;
     }
 
     if (account.get('moved') && !account.getIn(['relationship', 'following'])) {

app/javascript/mastodon/features/directory/components/account_card.jsx
@@ -160,16 +160,16 @@ class AccountCard extends ImmutablePureComponent {
       if (!account.get('relationship')) { // Wait until the relationship is loaded
         actionBtn = '';
       } else if (account.getIn(['relationship', 'requested'])) {
-        actionBtn = <Button className={classNames('logo-button')} text={intl.formatMessage(messages.cancel_follow_request)} title={intl.formatMessage(messages.requested)} onClick={this.handleFollow} />;
+        actionBtn = <Button  text={intl.formatMessage(messages.cancel_follow_request)} title={intl.formatMessage(messages.requested)} onClick={this.handleFollow} />;
       } else if (account.getIn(['relationship', 'muting'])) {
-        actionBtn = <Button className='logo-button' text={intl.formatMessage(messages.unmute)} onClick={this.handleMute} />;
+        actionBtn = <Button  text={intl.formatMessage(messages.unmute)} onClick={this.handleMute} />;
       } else if (!account.getIn(['relationship', 'blocking'])) {
-        actionBtn = <Button disabled={account.getIn(['relationship', 'blocked_by'])} className={classNames('logo-button', { 'button--destructive': account.getIn(['relationship', 'following']) })} text={intl.formatMessage(account.getIn(['relationship', 'following']) ? messages.unfollow : messages.follow)} onClick={this.handleFollow} />;
+        actionBtn = <Button disabled={account.getIn(['relationship', 'blocked_by'])} className={classNames({ 'button--destructive': account.getIn(['relationship', 'following']) })} text={intl.formatMessage(account.getIn(['relationship', 'following']) ? messages.unfollow : messages.follow)} onClick={this.handleFollow} />;
       } else if (account.getIn(['relationship', 'blocking'])) {
-        actionBtn = <Button className='logo-button' text={intl.formatMessage(messages.unblock)} onClick={this.handleBlock} />;
+        actionBtn = <Button  text={intl.formatMessage(messages.unblock)} onClick={this.handleBlock} />;
       }
     } else {
-      actionBtn = <Button className='logo-button' text={intl.formatMessage(messages.edit_profile)} onClick={this.handleEditProfile} />;
+      actionBtn = <Button  text={intl.formatMessage(messages.edit_profile)} onClick={this.handleEditProfile} />;
     }
 
     return (

app/javascript/styles/mastodon-light/diff.scss
@@ -627,14 +627,6 @@ html {
   }
 }
 
-.button.logo-button {
-  color: $white;
-
-  svg {
-    fill: $white;
-  }
-}
-
 .notification__filter-bar button.active::after,
 .account__section-headline a.active::after {
   border-color: transparent transparent $white;

app/javascript/styles/mastodon/components.scss
@@ -1670,10 +1670,6 @@ a.account__display-name {
   color: inherit;
 }
 
-.detailed-status .button.logo-button {
-  margin-bottom: 15px;
-}
-
 .detailed-status__display-name {
   color: $darker-text-color;
   display: flex;

commit aaf5734d9de4dc6eb22d6856377695c4b24888e2

Fix follow link style in embeds
Fix regression introduced by #25903

app/helpers/accounts_helper.rb
@@ -22,7 +22,7 @@ module AccountsHelper
   def account_action_button(account)
     return if account.memorial? || account.moved?
 
-    link_to ActivityPub::TagManager.instance.url_for(account), class: 'button', target: '_new' do
+    link_to ActivityPub::TagManager.instance.url_for(account), class: 'button logo-button', target: '_new' do
       safe_join([logo_as_symbol, t('accounts.follow')])
     end
   end

app/javascript/styles/mastodon/statuses.scss
@@ -77,6 +77,18 @@
   }
 }
 
+.button.logo-button svg {
+  width: 20px;
+  height: auto;
+  vertical-align: middle;
+  margin-inline-end: 5px;
+  fill: $primary-text-color;
+
+  @media screen and (max-width: $no-gap-breakpoint) {
+    display: none;
+  }
+}
+
 .embed {
   .status__content[data-spoiler='folded'] {
     .e-content {