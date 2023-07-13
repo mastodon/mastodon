ce43ed144cde8817c80620b44c038c1f353e0c20
Matt Jankowski <matt@jankowski.online>
2023-07-13 03:36:07 -0400

    Rails 7.0 update (#25668)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,11 +5,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << 'node_modules'
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w()
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.initialize_on_precompile = true
