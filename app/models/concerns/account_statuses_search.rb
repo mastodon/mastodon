8b7edfe48206c5c0be10b4e6480f3c04bfeea351
jsgoldstein <jakegoldstein95@gmail.com>
2023-07-14 10:11:25 -0400

    Try to add a chewy enabled check in the concern and add a comment about potential next steps if this solution does not scale

--- app/models/concerns/account_statuses_search.rb
+++ app/models/concerns/account_statuses_search.rb
@@ -4,6 +4,11 @@ module AccountStatusesSearch
   extend ActiveSupport::Concern
 
   def update_statuses_index!
+    return unless Chewy.enabled?
+
+    # This might not scale if a user has a TON of statuses.
+    # If that is the case, perhaps for users with many statuses, we should:
+    # (1) get all their statuses and (2) submit requests to ES in batches.
     Chewy.strategy(:sidekiq) do
       StatusesIndex.import(query: Status.where(account_id: id))
     end
