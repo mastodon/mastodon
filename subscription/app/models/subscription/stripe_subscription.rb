17fc3a6e7442686fe7764b87ec1444d8a715b60a
Jesse Karmani <jessekarmani@gmail.com>
2023-07-14 13:41:58 -0700

    Show invites for group plans

--- subscription/app/models/subscription/stripe_subscription.rb
+++ subscription/app/models/subscription/stripe_subscription.rb
@@ -5,5 +5,24 @@ module Subscription
     attr_accessor :subscription
 
     validates :status, presence: true
+    before_validation :set_invite
+
+    def retrieve
+      @stripe_sub ||= ::Stripe::Subscription.retrieve(subscription_id)
+    end
+
+    def description
+      @product ||= ::Stripe::Product.retrieve(retrieve[:plan][:product])
+      @product[:name]
+    end
+
+    def size
+      retrieve[:quantity]
+    end
+
+    private
+    def set_invite
+      self.invite = ::Invite.find_by(id: invite_id)
+    end
   end
 end
