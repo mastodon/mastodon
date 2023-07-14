42f2b4b1b49f191736e16c27133249c084fc8306
Jesse Karmani <jessekarmani@gmail.com>
2023-07-13 19:41:21 -0700

    Update webhook worker test for checkout completed event

--- subscription/app/workers/subscription/webhook_worker.rb
+++ subscription/app/workers/subscription/webhook_worker.rb
@@ -10,22 +10,19 @@ module Subscription
       event = ::Stripe::Event.retrieve(event_id)
       case event[:type]
       when 'checkout.session.completed'
-        session = ::Stripe::Checkout::Session.retrieve({
-          id: event['data']['object']['id'],
-          expand: ['subscription'],
-        })
-        subscription = ::Stripe::Subscription.retrieve(session.subscription)
-        invite = Invite.create!(user_id: InstancePresenter.new.contact.account.user.id, max_uses: subscription.quantity, autofollow: true)
+        session = ::Stripe::Checkout::Session.retrieve(event[:data][:object][:id])
+        subscription = ::Stripe::Subscription.retrieve(session[:subscription])
+        invite = Invite.create!(user_id: InstancePresenter.new.contact.account.user.id, max_uses: subscription[:quantity], autofollow: true)
         Subscription::StripeSubscription.create(
-          user_id: session.client_reference_id,
-          customer_id: session.customer,
-          subscription_id: session.subscription.id,
-          status: subscription.status,
+          user_id: session[:client_reference_id],
+          customer_id: session[:customer],
+          subscription_id: session[:subscription],
+          status: subscription[:status],
           invite_id: invite.id,
         )
 
-        if (session.client_reference_id.nil?)
-          customer = ::Stripe::Customer.retrieve(session.customer)
+        if (session[:client_reference_id].nil?)
+          customer = ::Stripe::Customer.retrieve(session[:customer])
           Subscription::ApplicationMailer.send_invite(customer[:email], invite).deliver_later
         end
       when 'customer.subscription.updated'
