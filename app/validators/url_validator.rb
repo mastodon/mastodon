d8d6eeb56c978a4d92c7260bea17c87f51d727d0
Matt Jankowski <matt@jankowski.online>
2023-07-17 13:24:40 -0400

    Revert "Memoize parsed_url value"
    This reverts commit c6dbf83891a1a891732329ca9497e0861684666a.

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -16,7 +16,7 @@ class URLValidator < ActiveModel::EachValidator
   end
 
   def parsed_url
-    @parsed_url ||= Addressable::URI.parse(@value)
+    Addressable::URI.parse(@value)
   rescue Addressable::URI::InvalidURIError
     false
   end
