b4b7b116e53f628d587449ba51e5a23ddb13281a
Claire <claire.github-309c@sitedethib.com>
2023-07-21 14:18:04 +0200

    Add check preventing Sidekiq workers from running with Makara configured (#25850)
    Co-authored-by: Eugen Rochko <eugen@zeonfederated.com>

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,11 @@
 require_relative '../../lib/mastodon/sidekiq_middleware'
 
 Sidekiq.configure_server do |config|
+  if Rails.configuration.database_configuration.dig('production', 'adapter') == 'postgresql_makara'
+    STDERR.puts 'ERROR: Database replication is not currently supported in Sidekiq workers. Check your configuration.'
+    exit 1
+  end
+
   config.redis = REDIS_SIDEKIQ_PARAMS
 
   config.server_middleware do |chain|
