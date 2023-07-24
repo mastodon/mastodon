8cd0d5abdf50f4651ae9468348817de9c6b249cf
Matt Jankowski <matt@jankowski.online>
2023-07-24 15:53:04 -0400

    Cover more cases

--- spec/requests/omniauth_callbacks_spec.rb
+++ spec/requests/omniauth_callbacks_spec.rb
@@ -4,14 +4,44 @@ require 'rails_helper'
 
 describe 'OmniAuth callbacks' do
   describe '#openid_connect', if: ENV['OIDC_ENABLED'] == 'true' && ENV['OIDC_SCOPE'].present? do
-    context 'with a user and an identify' do
+    context 'without a matching user' do
+      it 'creates a user and an identity and redirects to root path' do
+        expect { post user_openid_connect_omniauth_callback_path }
+          .to change(User, :count)
+          .by(1)
+          .and change(Identity, :count)
+          .by(1)
+        expect(User.last.email).to eq('user@host.example')
+        expect(Identity.find_by(user: User.last).uid).to eq('123')
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with a matching user and no matching identity' do
       before do
-        user = Fabricate(:user, email: 'user@host.example')
-        Fabricate(:identity, user: user, uid: '123')
+        Fabricate(:user, email: 'user@host.example')
       end
 
-      it 'redirects to root path' do
-        post user_openid_connect_omniauth_callback_path
+      it 'matches the existing user, creates an identity, and redirects to root path' do
+        expect { post user_openid_connect_omniauth_callback_path }
+          .to not_change(User, :count)
+          .and change(Identity, :count).by(1)
+
+        expect(Identity.find_by(user: User.last).uid).to eq('123')
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with a matching user and a matching identity' do
+      before do
+        user = Fabricate(:user, email: 'user@host.example')
+        Fabricate(:identity, user: user, uid: '123', provider: :openid_connect)
+      end
+
+      it 'matches the existing records and redirects to root path' do
+        expect { post user_openid_connect_omniauth_callback_path }
+          .to not_change(User, :count)
+          .and not_change(Identity, :count)
 
         expect(response).to redirect_to(root_path)
       end
