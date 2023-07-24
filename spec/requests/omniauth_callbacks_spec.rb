7e73edc223eed6cc4bf4b815390354456f456def
Matt Jankowski <matt@jankowski.online>
2023-07-24 15:38:01 -0400

    Improve wording on callback spec

--- spec/requests/omniauth_callbacks_spec.rb
+++ spec/requests/omniauth_callbacks_spec.rb
@@ -3,16 +3,18 @@
 require 'rails_helper'
 
 describe 'OmniAuth callbacks' do
-  context 'when openid_connect is enabled', if: ENV['OIDC_ENABLED'] == 'true' && ENV['OIDC_SCOPE'].present? do
-    before do
-      user = Fabricate(:user, email: 'user@host.example')
-      Fabricate(:identity, user: user, uid: '123')
-    end
+  describe '#openid_connect', if: ENV['OIDC_ENABLED'] == 'true' && ENV['OIDC_SCOPE'].present? do
+    context 'with a user and an identify' do
+      before do
+        user = Fabricate(:user, email: 'user@host.example')
+        Fabricate(:identity, user: user, uid: '123')
+      end
 
-    it 'responds' do
-      post user_openid_connect_omniauth_callback_path
+      it 'redirects to root path' do
+        post user_openid_connect_omniauth_callback_path
 
-      expect(response).to redirect_to(root_path)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
