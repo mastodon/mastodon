feaf3cf7806c69ffae563120fd176103f90e1150
Plastikmensch <Plastikmensch@users.noreply.github.com>
2023-07-29 08:37:29 +0200

    Fix firehose not indicating new toots in "All" (#2335)
    When "Show local-only toots in All" is enabled, the column never indicated there are new toots, since `hasUnread` was always false.
    
    Signed-off-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/glitch/features/firehose/index.jsx
+++ app/javascript/flavours/glitch/features/firehose/index.jsx
@@ -76,11 +76,11 @@ const Firehose = ({ feedType, multiColumn }) => {
   const { signedIn } = useIdentity();
   const columnRef = useRef(null);
 
-  const onlyMedia = useAppSelector((state) => state.getIn(['settings', 'firehose', 'onlyMedia'], false));
-  const hasUnread = useAppSelector((state) => state.getIn(['timelines', `${feedType}${onlyMedia ? ':media' : ''}`, 'unread'], 0) > 0);
-
   const allowLocalOnly = useAppSelector((state) => state.getIn(['settings', 'firehose', 'allowLocalOnly']));
   const regex = useAppSelector((state) => state.getIn(['settings', 'firehose', 'regex', 'body']));
+
+  const onlyMedia = useAppSelector((state) => state.getIn(['settings', 'firehose', 'onlyMedia'], false));
+  const hasUnread = useAppSelector((state) => state.getIn(['timelines', `${feedType}${feedType === 'public' && allowLocalOnly ? ':allow_local_only' : ''}${onlyMedia ? ':media' : ''}`, 'unread'], 0) > 0);
 
   const handlePin = useCallback(
     () => {
