560503edcdf23c26d2b7aaf5d8ee2adac5789372
Jeong Arm <kjwonmail@gmail.com>
2023-08-03 18:35:13 +0900

    Fix invites vacuum

--- app/lib/vacuum/invites_vacuum.rb
+++ app/lib/vacuum/invites_vacuum.rb
@@ -7,17 +7,17 @@ class Vacuum::InvitesVacuum
   end
 
   def perform
-    expire_invites! if retention_period?
+    expire_invites! if @retention_period.present?
   end
 
   private
 
   def expire_invites!
-    invites = Invite.where('created_at < ?', retention_period.ago)
-    invites = if max_uses.nil?
-                invites.where(max_uses: nil)
+    invites = Invite.where('created_at < ?', @retention_period.ago)
+    invites = if @max_uses.present?
+                invites.where('max_uses > ? OR max_uses IS NULL', @max_uses)
               else
-                invites.where('max_uses > ? OR max_uses IS NULL', max_uses)
+                invites.where(max_uses: nil)
               end
 
     invites.reorder(nil).in_batches(&:expire!)
