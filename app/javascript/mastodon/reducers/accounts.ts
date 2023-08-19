22a54145f9c9a0bcc04921a96035a8f8b4cbc70b
Dan <dan@aredherring.tech>
2023-08-19 18:04:25 +0000

    Do not modify the jsdoc types to account for store types
    The jsdoc is accurate, the modifications were incorrect - The
    modifications to the types pertained to properties that are only
    changed in the Redux store representation of the type.
    
    It therefore does not make sense to modify the type the rest of the
    application relies on.

--- app/javascript/mastodon/reducers/accounts.ts
+++ app/javascript/mastodon/reducers/accounts.ts
@@ -2,25 +2,42 @@ import { Map as ImmutableMap, fromJS } from 'immutable';
 
 import { ACCOUNT_REVEAL } from 'mastodon/actions/accounts';
 import { ACCOUNT_IMPORT, ACCOUNTS_IMPORT } from 'mastodon/actions/importer';
-import type { Account } from 'mastodon/initial_state';
+import type { Account as StateAccount } from 'mastodon/initial_state';
 
 // TODO(trinitroglycerin): the value of this map is actually a record type,
 // mapping keys of Account to those values in an immutable map.
 type State = ImmutableMap<string, ImmutableMap<string, unknown>>;
 const initialState: State = ImmutableMap();
 
+interface Status {
+  limited: boolean;
+  hidden: boolean;
+}
+
+interface Counters {
+  followers_count: number;
+  following_count: number;
+  statuses_count: number;
+}
+
+// This complicated type is used to allow for the deletion of the properties in Counters from the Account type.
+//
+// The properties in the Counters type are derived values and are tracked in the account_counters slice.
+//
+// TODO(trinitroglycerin): This additionally adds the derived hidden and limited properties, as these are not present
+// in the jsdoc types. However, there are a few instances where React components rely on these properties existing,
+// so it's possible that the hidden and limited properties should be graduated to the jsdoc types.
+type Account = Pick<StateAccount, Exclude<keyof StateAccount, keyof Counters>> &
+  Partial<Counters> &
+  Status;
+
 const normalizeAccount = (state: State, account: Account): State => {
   account = { ...account };
-
+  // The following properties are deleted from the account as they are tracked separately within a the account_counters slice.
   delete account.followers_count;
   delete account.following_count;
   delete account.statuses_count;
 
-  // TODO(trinitroglycerin): I'm not sure if these sections are needed, the type errors
-  // appear to be caused by the fact we're importing types defined in jsdoc.
-  //
-  // The linter doesn't appear to be "smart" enough to understand these are real types
-  // and assumes any symbol imported from `mastodon/initial_state` is `any`.
   const limited = account.limited ?? false;
   const hidden = state.getIn([account.id, 'hidden']) as boolean;
   account.hidden = hidden === false ? false : limited;
