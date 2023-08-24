582607f8782dd2bc77151abaf94c165e61a66415
Jaehong Kang <sinoru@me.com>
2023-08-24 17:58:15 +0900

    Revert "Workaround to load MASTODON_VERSION_* in Docker."
    This reverts commit 11c537e43c585add7e3457d7e4d99f3233135d3f.

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -17,11 +17,11 @@ module Mastodon
     end
 
     def flags
-      ENV.fetch('MASTODON_VERSION_FLAGS', nil).presence || '-beta2'
+      ENV.fetch('MASTODON_VERSION_FLAGS', '-beta2')
     end
 
     def suffix
-      ENV.fetch('MASTODON_VERSION_SUFFIX', nil).presence || ''
+      ENV.fetch('MASTODON_VERSION_SUFFIX', '')
     end
 
     def to_a
