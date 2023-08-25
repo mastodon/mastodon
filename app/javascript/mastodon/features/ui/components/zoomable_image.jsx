ccf5d4a1a28dd13c66b0e1969c6be3c6f50da917
Antonin Del Fabbro <30950182+AntoninDelFabbro@users.noreply.github.com>
2023-08-25 17:28:25 +0200

    Opted for the explicit `draggable="true"`

--- app/javascript/mastodon/features/ui/components/zoomable_image.jsx
+++ app/javascript/mastodon/features/ui/components/zoomable_image.jsx
@@ -442,6 +442,7 @@ class ZoomableImage extends PureComponent {
               transform: `scale(${scale}) translate(-${lockTranslate.x}px, -${lockTranslate.y}px)`,
               transformOrigin: '0 0',
             }}
+            draggable={true}
             onClick={this.handleClick}
             onMouseDown={this.handleMouseDown}
           />
