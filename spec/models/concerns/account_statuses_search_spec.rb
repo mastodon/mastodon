93d6806300d2385570fd1c45459a4409e064cc19
jsgoldstein <jakegoldstein95@gmail.com>
2023-08-28 15:19:11 -0400

    Adding a simple ES test

--- spec/models/concerns/account_statuses_search_spec.rb
+++ spec/models/concerns/account_statuses_search_spec.rb
@@ -63,4 +63,52 @@ describe AccountStatusesSearch do
       expect(worker).to have_received(:perform_async).with(account.id)
     end
   end
+
+  describe '#add_to_public_statuses_index!' do
+    let(:indexable) { true }
+
+    before do
+      PublicStatusesIndex.delete
+      PublicStatusesIndex.create
+    end
+
+    after do
+      PublicStatusesIndex.delete
+    end
+
+    it 'adds the statuses to the PublicStatusesIndex' do
+      expect(PublicStatusesIndex.filter(term: { account_id: account.id }).count).to eq(0)
+
+      status_1 = Fabricate(:status, account: account, text: 'status 1', visibility: :public)
+      status_2 = Fabricate(:status, account: account, text: 'status 2', visibility: :public)
+      account.add_to_public_statuses_index!
+
+      expect(PublicStatusesIndex.filter(term: { account_id: account.id }).count).to eq(2)
+    end
+  end
+
+  describe '#remove_from_public_statuses_index!' do
+    # indexable must be true in order for the statuses to start in the index
+    let(:indexable) { true }
+    let(:status_1) { Fabricate(:status, account: account, text: 'status 1') }
+    let(:status_2) { Fabricate(:status, account: account, text: 'status 2') }
+
+    before do
+      PublicStatusesIndex.delete
+      PublicStatusesIndex.create
+      PublicStatusesIndex.import([status_1, status_2])
+    end
+
+    after do
+      PublicStatusesIndex.delete
+    end
+
+    it 'removes the statuses from the PublicStatusesIndex' do
+      expect(PublicStatusesIndex.filter(term: { account_id: account.id }).count).to eq(2)
+
+      account.remove_from_public_statuses_index!
+
+      expect(PublicStatusesIndex.filter(term: { account_id: account.id }).count).to eq(0)
+    end
+  end
 end
