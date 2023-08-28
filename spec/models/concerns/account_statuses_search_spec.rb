d89a2d0215300f2ea8de9b7dcf2bcfdb8631bbce
jsgoldstein <jakegoldstein95@gmail.com>
2023-08-28 15:49:50 -0400

    Lint the tests

--- spec/models/concerns/account_statuses_search_spec.rb
+++ spec/models/concerns/account_statuses_search_spec.rb
@@ -79,8 +79,8 @@ describe AccountStatusesSearch do
     it 'adds the statuses to the PublicStatusesIndex' do
       expect(PublicStatusesIndex.filter(term: { account_id: account.id }).count).to eq(0)
 
-      status_1 = Fabricate(:status, account: account, text: 'status 1', visibility: :public)
-      status_2 = Fabricate(:status, account: account, text: 'status 2', visibility: :public)
+      Fabricate(:status, account: account, text: 'status 1', visibility: :public)
+      Fabricate(:status, account: account, text: 'status 2', visibility: :public)
       account.add_to_public_statuses_index!
 
       expect(PublicStatusesIndex.filter(term: { account_id: account.id }).count).to eq(2)
@@ -90,13 +90,13 @@ describe AccountStatusesSearch do
   describe '#remove_from_public_statuses_index!' do
     # indexable must be true in order for the statuses to start in the index
     let(:indexable) { true }
-    let(:status_1) { Fabricate(:status, account: account, text: 'status 1') }
-    let(:status_2) { Fabricate(:status, account: account, text: 'status 2') }
+    let(:status_one) { Fabricate(:status, account: account, text: 'status 1') }
+    let(:status_two) { Fabricate(:status, account: account, text: 'status 2') }
 
     before do
       PublicStatusesIndex.delete
       PublicStatusesIndex.create
-      PublicStatusesIndex.import([status_1, status_2])
+      PublicStatusesIndex.import([status_one, status_two])
     end
 
     after do
