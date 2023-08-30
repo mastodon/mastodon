a67dcdcccef1bf2c4c536f7af7b57101fec22600
LuckDev <53546539+RustyReact@users.noreply.github.com>
2023-08-30 20:17:16 +0000

    Update statuses.jsx

--- app/javascript/mastodon/features/explore/statuses.jsx
+++ app/javascript/mastodon/features/explore/statuses.jsx
@@ -52,10 +52,9 @@ class Statuses extends PureComponent {
 
     return (
       <>
-        <DismissableBanner id='explore/statuses'>
+        {/* <DismissableBanner id='explore/statuses'>
           <FormattedMessage id='dismissable_banner.explore_statuses' defaultMessage='These are posts from across the social web that are gaining traction today. Newer posts with more boosts and favorites are ranked higher.' />
-          {/* <FormattedMessage id='empty_column.community' defaultMessage='These are posts from across the social web that are gaining traction today. Newer posts with more boosts and favorites are ranked higher.' /> */}
-        </DismissableBanner>
+        </DismissableBanner> */}
 
         <div className='scrollable scrollable--flex'>
           <StatusListContainer
