ad7b5290110d59093f027f4d30ca0d1cd7e09842
LuckDev <53546539+RustyReact@users.noreply.github.com>
2023-08-30 14:58:14 +0000

    tags

--- app/models/trends/tags.rb
+++ app/models/trends/tags.rb
@@ -5,7 +5,7 @@ class Trends::Tags < Trends::Base
 
   self.default_options = {
     threshold: 5,
-    review_threshold: 3,
+    review_threshold: 0,
     max_score_cooldown: 2.days.freeze,
     max_score_halflife: 4.hours.freeze,
     decay_threshold: 1,
