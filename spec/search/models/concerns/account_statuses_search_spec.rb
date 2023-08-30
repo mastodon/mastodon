c9fe00c821b8efbfdd7664867ca24b861fa51019
jsgoldstein <jakegoldstein95@gmail.com>
2023-08-30 09:17:11 -0400

    Clean up the tests a bit

--- spec/search/models/concerns/account_statuses_search_spec.rb
+++ spec/search/models/concerns/account_statuses_search_spec.rb
@@ -4,23 +4,21 @@ require 'rails_helper'
 
 describe AccountStatusesSearch do
   let(:account) { Fabricate(:account, indexable: indexable) }
+  # indexable must be true in order for the statuses to end up in the index
+  let(:indexable) { true }
 
   before do
     allow(Chewy).to receive(:enabled?).and_return(true)
+
+    PublicStatusesIndex.delete
+    PublicStatusesIndex.create
+  end
+
+  after do
+    PublicStatusesIndex.delete
   end
 
   describe '#add_to_public_statuses_index!' do
-    let(:indexable) { true }
-
-    before do
-      PublicStatusesIndex.delete
-      PublicStatusesIndex.create
-    end
-
-    after do
-      PublicStatusesIndex.delete
-    end
-
     it 'adds the statuses to the PublicStatusesIndex' do
       expect(PublicStatusesIndex.filter(term: { account_id: account.id }).count).to eq(0)
 
@@ -33,19 +31,11 @@ describe AccountStatusesSearch do
   end
 
   describe '#remove_from_public_statuses_index!' do
-    # indexable must be true in order for the statuses to start in the index
-    let(:indexable) { true }
     let(:status_one) { Fabricate(:status, account: account, text: 'status 1') }
     let(:status_two) { Fabricate(:status, account: account, text: 'status 2') }
 
     before do
-      PublicStatusesIndex.delete
-      PublicStatusesIndex.create
       PublicStatusesIndex.import([status_one, status_two])
-    end
-
-    after do
-      PublicStatusesIndex.delete
     end
 
     it 'removes the statuses from the PublicStatusesIndex' do
