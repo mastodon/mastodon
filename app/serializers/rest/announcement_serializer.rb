bfc93fd3d7f09009e4fb81b2cde5c1fa8c9b4b36
Claire <claire.github-309c@sitedethib.com>
2023-08-31 11:31:39 +0200

    Change `/api/v1/announcements` to return regular `Status` entities

--- app/serializers/rest/announcement_serializer.rb
+++ app/serializers/rest/announcement_serializer.rb
@@ -9,7 +9,7 @@ class REST::AnnouncementSerializer < ActiveModel::Serializer
   attribute :read, if: :current_user?
 
   has_many :mentions
-  has_many :statuses
+  has_many :statuses, serializer: REST::StatusSerializer
   has_many :tags, serializer: REST::StatusSerializer::TagSerializer
   has_many :emojis, serializer: REST::CustomEmojiSerializer
   has_many :reactions, serializer: REST::ReactionSerializer
@@ -47,18 +47,6 @@ class REST::AnnouncementSerializer < ActiveModel::Serializer
 
     def acct
       object.pretty_acct
-    end
-  end
-
-  class StatusSerializer < ActiveModel::Serializer
-    attributes :id, :url
-
-    def id
-      object.id.to_s
-    end
-
-    def url
-      ActivityPub::TagManager.instance.url_for(object)
     end
   end
 end
