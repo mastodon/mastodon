6dddadefa6294c7691e4afd175e813cb0340e55b
Eugen Rochko <eugen@zeonfederated.com>
2023-09-02 01:17:11 +0200

    Fix some video encoding failing due to uneven dimensions

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -102,6 +102,7 @@ class MediaAttachment < ApplicationRecord
         'preset' => 'veryfast',
         'movflags' => 'faststart', # Move metadata to start of file so playback can begin before download finishes
         'pix_fmt' => 'yuv420p', # Ensure color space for cross-browser compatibility
+        'vf' => 'crop=floor(iw/2)*2:floor(ih/2)*2', # h264 requires width and height to be even. Crop instead of scale to avoid blurring
         'c:v' => 'h264',
         'c:a' => 'aac',
         'b:a' => '192k',
