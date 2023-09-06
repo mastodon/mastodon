79f28f4592e582970fcbf8f2ec18a70dcd3927a4
Eugen Rochko <eugen@zeonfederated.com>
2023-09-06 21:33:31 +0200

    Fix mute button and volume slider feeling disconnected in web UI

--- app/javascript/mastodon/features/video/index.jsx
+++ app/javascript/mastodon/features/video/index.jsx
@@ -501,8 +501,9 @@ class Video extends PureComponent {
 
   render () {
     const { preview, src, aspectRatio, onOpenVideo, onCloseVideo, intl, alt, lang, detailed, sensitive, editable, blurhash, autoFocus } = this.props;
-    const { currentTime, duration, volume, buffer, dragging, paused, fullscreen, hovered, muted, revealed } = this.state;
+    const { currentTime, duration, volume, buffer, dragging, paused, fullscreen, hovered, revealed } = this.state;
     const progress = Math.min((currentTime / duration) * 100, 100);
+    const muted = this.state.muted || volume === 0;
 
     let preload;
 
@@ -593,12 +594,12 @@ class Video extends PureComponent {
                 <button type='button' title={intl.formatMessage(muted ? messages.unmute : messages.mute)} aria-label={intl.formatMessage(muted ? messages.unmute : messages.mute)} className='player-button' onClick={this.toggleMute}><Icon id={muted ? 'volume-off' : 'volume-up'} fixedWidth /></button>
 
                 <div className={classNames('video-player__volume', { active: this.state.hovered })} onMouseDown={this.handleVolumeMouseDown} ref={this.setVolumeRef}>
-                  <div className='video-player__volume__current' style={{ width: `${volume * 100}%` }} />
+                  <div className='video-player__volume__current' style={{ width: `${muted ? 0 : volume * 100}%` }} />
 
                   <span
                     className={classNames('video-player__volume__handle')}
                     tabIndex={0}
-                    style={{ left: `${volume * 100}%` }}
+                    style={{ left: `${muted ? 0 : volume * 100}%` }}
                   />
                 </div>
 
