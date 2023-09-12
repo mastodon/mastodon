cf8b242c4c4ac25ed18864844f4d40df32314d5a
Claire <claire.github-309c@sitedethib.com>
2023-09-12 18:27:01 +0200

    [Glitch] Fix notification permissions being requested immediately after login
    Port 520b57047403f8819206c938bdc2c93a16ec5f16 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/actions/notifications.js
+++ app/javascript/flavours/glitch/actions/notifications.js
@@ -18,6 +18,7 @@ import {
   importFetchedStatuses,
 } from './importer';
 import { submitMarkers } from './markers';
+import { register as registerPushNotifications } from './push_notifications';
 import { saveSettings } from './settings';
 
 
@@ -384,6 +385,10 @@ export function requestBrowserPermission(callback = noOp) {
     requestNotificationPermission((permission) => {
       dispatch(setBrowserPermission(permission));
       callback(permission);
+
+      if (permission === 'granted') {
+        dispatch(registerPushNotifications());
+      }
     });
   };
 }
