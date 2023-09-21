4d6c2ab94acf6c1e259f0c5c8f0d06e573f5b125
noellabo <noel.yoshiba@gmail.com>
2023-09-21 17:55:01 +0900

    Add search configuration to instance api

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -77,6 +77,13 @@ class REST::InstanceSerializer < ActiveModel::Serializer
       translation: {
         enabled: TranslationService.configured?,
       },
+
+      search: {
+        enabled: Chewy.enabled?,
+        supported_prefix: SearchQueryTransformer::SUPPORTED_PREFIXES,
+        supported_properties: SearchQueryTransformer::SUPPORTED_PROPERTIES,
+        supported_operator: SearchQueryTransformer::SUPPORTED_OPERATOR,
+      },
     }
   end
 
