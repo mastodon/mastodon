3b7caa00e24a80f34fd0c85e73e2d2f3bd833b69
Renaud Chaput <renchap@gmail.com>
2023-09-26 11:25:01 +0200

    [Glitch] Add Typescript types for some API objects
    Port 57f592fed50747f3c97718a2761e17bafe6c8698 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

---
+++ app/javascript/flavours/glitch/api_types/accounts.ts
@@ -0,0 +1,45 @@
+import type { ApiCustomEmojiJSON } from './custom_emoji';
+
+export interface ApiAccountFieldJSON {
+  name: string;
+  value: string;
+  verified_at: string | null;
+}
+
+export interface ApiAccountRoleJSON {
+  color: string;
+  id: string;
+  name: string;
+}
+
+// See app/serializers/rest/account_serializer.rb
+export interface ApiAccountJSON {
+  acct: string;
+  avatar: string;
+  avatar_static: string;
+  bot: boolean;
+  created_at: string;
+  discoverable: boolean;
+  display_name: string;
+  emojis: ApiCustomEmojiJSON[];
+  fields: ApiAccountFieldJSON[];
+  followers_count: number;
+  following_count: number;
+  group: boolean;
+  header: string;
+  header_static: string;
+  id: string;
+  last_status_at: string;
+  locked: boolean;
+  noindex: boolean;
+  note: string;
+  roles: ApiAccountJSON[];
+  statuses_count: number;
+  uri: string;
+  url: string;
+  username: string;
+  moved?: ApiAccountJSON;
+  suspended?: boolean;
+  limited?: boolean;
+  memorial?: boolean;
+}
