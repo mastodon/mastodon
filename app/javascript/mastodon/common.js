cc45f7334d4d01035ed48b0cc570d25e0f499b5b
Lety Does Stuff <48048071+letydoesstuff@users.noreply.github.com>
2023-09-26 13:06:02 +0000

    Hide top and bottom bars when scrolling (ronilaukkarinen/mastodon@acf894f7676b293a4db6e0eb7c9f76fecba6a5bb, ronilaukkarinen/mastodon@6bc5a50a57d1d8142b6a8d5bf5ba45850779be3a, ronilaukkarinen/mastodon@cfcc34c1580abab34257e68fc6fe9c6d54fa8697, ronilaukkarinen/mastodon@e954555e6849f00dab01631591093dd6d8d8174d, ronilaukkarinen/mastodon@e633ae29cd1f5f179da8c7b6f9a3f8478bff5e03)

--- app/javascript/mastodon/common.js
+++ app/javascript/mastodon/common.js
@@ -10,3 +10,82 @@ export function start() {
     // If called twice
   }
 }
+
+// Hide the top bar when scrolling down, show it when scrolling up
+let lastScrollTop = 0;
+let lastScrollDirection = 0;
+let lastScrollTime = 0;
+let scrollTimeout = null;
+function scrollHandler() {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const scrollDirection = scrollTop > lastScrollTop ? 1 : -1;
+  const scrollTime = Date.now();
+  const scrollPositionFromTop = window.scrollY;
+  // If not mobile, bail
+  if (window.innerWidth > 890) {
+    // Remove scroll-up and scroll-down classes
+    document.body.classList.remove('scroll-up');
+    document.body.classList.remove('scroll-down');
+    return;
+  }
+  // When the scroll position from top touches .notification__filter-bar, add class
+  const notificationFilterBar = document.querySelector('.notification__filter-bar');
+  const notificationFilterBarHeight = notificationFilterBar ? notificationFilterBar.offsetHeight : 0;
+  const tabsBarWrapperHeight = document.querySelector('.tabs-bar__wrapper') ? document.querySelector('.tabs-bar__wrapper').offsetHeight : 0;
+  const topBarHeight = notificationFilterBarHeight + tabsBarWrapperHeight;
+  if (notificationFilterBar) {
+    const notificationFilterBarRect = notificationFilterBar.getBoundingClientRect();
+    if (scrollPositionFromTop > 146) {
+      notificationFilterBar.classList.add('notification__filter-bar--fixed');
+    }
+    // When the fixed item reaches the amount of its height from the top, remove class
+    if (scrollPositionFromTop <= topBarHeight) {
+      notificationFilterBar.classList.remove('notification__filter-bar--fixed');
+    }
+  }
+  // If scroll position from bottom is less than a certain amount, don't do anything
+  if (scrollPositionFromTop < 500) {
+    // Remove scroll-up and scroll-down classes
+    document.body.classList.remove('scroll-up');
+    document.body.classList.remove('scroll-down');
+    return;
+  }
+  if (scrollDirection !== lastScrollDirection) {
+    lastScrollDirection = scrollDirection;
+    lastScrollTime = scrollTime;
+  }
+  if (scrollTimeout) {
+    clearTimeout(scrollTimeout);
+  }
+  if (scrollDirection === 1) {
+    document.body.classList.remove('scroll-up');
+    document.body.classList.add('scroll-down');
+  } else {
+    document.body.classList.remove('scroll-down');
+    document.body.classList.add('scroll-up');
+    if (scrollTime - lastScrollTime > 100) {
+      document.body.classList.add('scroll-top');
+    }
+  }
+  scrollTimeout = setTimeout(() => {
+    document.body.classList.remove('scroll-top');
+  }
+  , 100);
+  lastScrollTop = scrollTop;
+}
+export function enableScrollHandler() {
+  window.addEventListener('scroll', scrollHandler);
+  // Remove scroll-down class on click and touch events
+  window.addEventListener('click', () => {
+    scrollHandler();
+    document.body.classList.remove('scroll-down');
+  });
+  // Each time div .columns-area__panels__main has changed, call scrollHandler
+  const columnsAreaPanelsMain = document.querySelector('.columns-area__panels__main');
+  if (columnsAreaPanelsMain) {
+    const observer = new MutationObserver(scrollHandler);
+    observer.observe(columnsAreaPanelsMain, { childList: true });
+  }
+}
+// Launch
+enableScrollHandler();
