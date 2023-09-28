08d9881530fd3292a1263d4806b830734be8e832
Lety Does Stuff <48048071+letydoesstuff@users.noreply.github.com>
2023-09-28 02:46:36 +0000

    Increase poll options to 15 and character length to 150 (ronilaukkarinen/mastodon@9f58a0d95172fe64d9b5c37576d88dd6e5aaa3f4)

--- app/validators/poll_validator.rb
+++ app/validators/poll_validator.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class PollValidator < ActiveModel::Validator
-  MAX_OPTIONS      = 4
-  MAX_OPTION_CHARS = 50
+  MAX_OPTIONS      = 15
+  MAX_OPTION_CHARS = 150
   MAX_EXPIRATION   = 1.month.freeze
   MIN_EXPIRATION   = 5.minutes.freeze
 
