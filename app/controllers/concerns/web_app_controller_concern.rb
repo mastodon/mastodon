fabf7101d27973f5861b74ff72a018f42930efd5
Claire <claire.github-309c@sitedethib.com>
2023-10-04 13:42:28 +0200

    Add explicit caching of logged-out permalink redirects

--- app/controllers/concerns/web_app_controller_concern.rb
+++ app/controllers/concerns/web_app_controller_concern.rb
@@ -22,7 +22,9 @@ module WebAppControllerConcern
     return if user_signed_in? && current_account.moved_to_account_id.nil?
 
     redirect_path = PermalinkRedirector.new(request.path).redirect_path
+    return if redirect_path.blank?
 
-    redirect_to(redirect_path) if redirect_path.present?
+    expires_in(15.seconds, public: true, stale_while_revalidate: 30.seconds, stale_if_error: 1.day) unless user_signed_in?
+    redirect_to(redirect_path)
   end
 end
