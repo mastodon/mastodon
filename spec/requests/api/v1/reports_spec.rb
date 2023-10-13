058f73a4f5f17fc33bb842115d1f69a4b2e9f740
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-10-13 10:30:40 -0300

    Add request specs for `/api/v1/reports` (#25475)

---
+++ spec/requests/api/v1/reports_spec.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Reports' do
+  let(:user)    { Fabricate(:user) }
+  let(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let(:scopes)  { 'write:reports' }
+  let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+
+  describe 'POST /api/v1/reports' do
+    subject do
+      post '/api/v1/reports', headers: headers, params: params
+    end
+
+    let!(:admin)         { Fabricate(:user, role: UserRole.find_by(name: 'Admin')) }
+    let(:status)         { Fabricate(:status) }
+    let(:target_account) { status.account }
+    let(:category)       { 'other' }
+    let(:forward)        { nil }
+    let(:rule_ids)       { nil }
+
+    let(:params) do
+      {
+        status_ids: [status.id],
+        account_id: target_account.id,
+        comment: 'reasons',
+        category: category,
+        rule_ids: rule_ids,
+        forward: forward,
+      }
+    end
+
+    it_behaves_like 'forbidden for wrong scope', 'read read:reports'
+
+    it 'returns http success' do
+      subject
+
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns the created report' do
+      subject
+
+      expect(body_as_json).to match(
+        a_hash_including(
+          status_ids: [status.id.to_s],
+          category: category,
+          comment: 'reasons'
+        )
+      )
+    end
+
+    it 'creates a report' do
+      subject
+
+      expect(target_account.targeted_reports).to_not be_empty
+    end
+
+    it 'sends e-mails to admins' do
+      perform_enqueued_jobs do
+        subject
+
+        expect(ActionMailer::Base.deliveries.first.to).to eq([admin.email])
+      end
+    end
+
+    context 'when a status does not belong to the reported account' do
+      let(:target_account) { Fabricate(:account) }
+
+      it 'returns http not found' do
+        subject
+
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context 'when a category is chosen' do
+      let(:category) { 'spam' }
+
+      it 'saves category' do
+        subject
+
+        expect(target_account.targeted_reports.first.spam?).to be true
+      end
+    end
+
+    context 'when violated rules are chosen' do
+      let(:rule)     { Fabricate(:rule) }
+      let(:category) { 'violation' }
+      let(:rule_ids) { [rule.id] }
+
+      it 'saves category' do
+        subject
+
+        expect(target_account.targeted_reports.first.violation?).to be true
+      end
+
+      it 'saves rule_ids' do
+        subject
+
+        expect(target_account.targeted_reports.first.rule_ids).to contain_exactly(rule.id)
+      end
+    end
+  end
+end
