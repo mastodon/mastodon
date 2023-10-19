24cd0ab6f29cce8c7ef896bf66032e15520dfd9d
zhoujunlin <>
2023-10-19 16:13:26 +0800

    no message

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -59,8 +59,11 @@ class Auth::RegistrationsController < Devise::RegistrationsController
 
   def configure_sign_up_params
     logger.debug("Auth::RegistrationsController::configure_sign_up_params")
+    # devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    #   user_params.permit({ account_attributes: [:username, :display_name], invite_request_attributes: [:text] }, :email, :password, :password_confirmation, :invite_code, :agreement, :website, :confirm_password)
+    # end
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit({ account_attributes: [:username, :display_name], invite_request_attributes: [:text] }, :email, :password, :password_confirmation, :invite_code, :agreement, :website, :confirm_password)
+        user_params.permit({ account_attributes: [:username, :display_name], invite_request_attributes: [:text] }, :email, :password, :invite_code, :website)
     end
   end
 
