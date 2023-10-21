9429d0bab8ea593a93d52661da16cbc020e870cc
Lety Does Stuff <48048071+letydoesstuff@users.noreply.github.com>
2023-10-21 19:11:13 +0000

    Spin off tootified English locale as new "English (Legacy)" locale

--- config/application.rb
+++ config/application.rb
@@ -84,6 +84,7 @@ module Mastodon
     # All translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [
+      :en-L,
       :af,
       :an,
       :ar,
