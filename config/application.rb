07d43800eafec7680fd0c9559334c09f170ac2e7
Matt Jankowski <matt@jankowski.online>
2023-10-23 16:24:44 -0400

    Remove system test update from application.rb

--- config/application.rb
+++ config/application.rb
@@ -84,9 +84,6 @@ module Mastodon
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-
     # All translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [
