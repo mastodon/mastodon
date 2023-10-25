4fe95a7dbb4e739991a83f86f3d4786ddefe715a
Matt Jankowski <matt@jankowski.online>
2023-10-25 09:28:25 -0400

    Move lib/sanitize_ext to config/initializers

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ require 'mail'
 Bundler.require(*Rails.groups)
 
 require_relative '../lib/mastodon'
-require_relative '../lib/sanitize_ext/sanitize_config'
 require_relative '../lib/redis/namespace_extensions'
 require_relative '../lib/paperclip/url_generator_extensions'
 require_relative '../lib/paperclip/attachment_extensions'
