6b4ac26c8c5c5206eaf036d6f5597e826be9e5e4
Matt Jankowski <matt@jankowski.online>
2023-10-25 08:42:20 -0400

    Move lib/linter/* for zeitwerk

--- config/application.rb
+++ config/application.rb
@@ -67,7 +67,7 @@ module Mastodon
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks templates generators))
+    config.autoload_lib(ignore: %w(assets tasks templates generators linter))
 
     # Configuration for the application, engines, and railties goes here.
     #
