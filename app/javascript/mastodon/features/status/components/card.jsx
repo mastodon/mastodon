2d8f759a342813c48e00412d17ec2517ed9ef761
Simon Rapilly <85184231+srapilly@users.noreply.github.com>
2023-10-26 16:34:15 -0400

    Add HTML lang attribute to preview card descriptions (#27503)

--- app/javascript/mastodon/features/status/components/card.jsx
+++ app/javascript/mastodon/features/status/components/card.jsx
@@ -143,7 +143,7 @@ export default class Card extends PureComponent {
 
         <strong className='status-card__title' title={card.get('title')} lang={language}>{card.get('title')}</strong>
 
-        {card.get('author_name').length > 0 ? <span className='status-card__author'><FormattedMessage id='link_preview.author' defaultMessage='By {name}' values={{ name: <strong>{card.get('author_name')}</strong> }} /></span> : <span className='status-card__description'>{card.get('description')}</span>}
+        {card.get('author_name').length > 0 ? <span className='status-card__author'><FormattedMessage id='link_preview.author' defaultMessage='By {name}' values={{ name: <strong>{card.get('author_name')}</strong> }} /></span> : <span className='status-card__description' lang={language}>{card.get('description')}</span>}
       </div>
     );
 
