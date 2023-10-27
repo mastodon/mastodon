d26c44920274a5525de6fc2c0d34bf5eb3d8cda6
Jonathan de Jong <jonathandejong02@gmail.com>
2023-10-27 16:55:00 +0200

    Have `Follow` activities bypass availability (#27586)
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- app/workers/activitypub/delivery_worker.rb
+++ app/workers/activitypub/delivery_worker.rb
@@ -13,9 +13,10 @@ class ActivityPub::DeliveryWorker
   HEADERS = { 'Content-Type' => 'application/activity+json' }.freeze
 
   def perform(json, source_account_id, inbox_url, options = {})
-    return unless DeliveryFailureTracker.available?(inbox_url)
-
     @options        = options.with_indifferent_access
+
+    return unless @options[:bypass_availability] || DeliveryFailureTracker.available?(inbox_url)
+
     @json           = json
     @source_account = Account.find(source_account_id)
     @inbox_url      = inbox_url
