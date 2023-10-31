f428fdd86753cc113467eac07dd1f664af13bf33
Shlee <github@shl.ee>
2023-10-31 21:04:38 +1030

    Update initial_state_serializer.rb

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -5,7 +5,7 @@ class InitialStateSerializer < ActiveModel::Serializer
 
   attributes :meta, :compose, :accounts,
              :media_attachments, :settings,
-             :languages
+             :languages, :max_post_chars
 
   attribute :critical_updates_pending, if: -> { object&.role&.can?(:view_devops) && SoftwareUpdate.check_enabled? }
 
