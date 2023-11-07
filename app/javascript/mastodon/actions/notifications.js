mastodon/mastodon

commit 3bf2a7296e6d80d5cd9d5d3cba1507af6ba83dfd

Use Immutable `Record` for accounts in Redux state (#26559)

app/javascript/mastodon/actions/notifications.js
@@ -18,10 +18,12 @@ import {
   importFetchedStatuses,
 } from './importer';
 import { submitMarkers } from './markers';
+import { notificationsUpdate } from "./notifications_typed";
 import { register as registerPushNotifications } from './push_notifications';
 import { saveSettings } from './settings';
 
-export const NOTIFICATIONS_UPDATE      = 'NOTIFICATIONS_UPDATE';
+export * from "./notifications_typed";
+
 export const NOTIFICATIONS_UPDATE_NOOP = 'NOTIFICATIONS_UPDATE_NOOP';
 
 export const NOTIFICATIONS_EXPAND_REQUEST = 'NOTIFICATIONS_EXPAND_REQUEST';
@@ -95,12 +97,8 @@ export function updateNotifications(notification, intlMessages, intlLocale) {
         dispatch(importFetchedAccount(notification.report.target_account));
       }
 
-      dispatch({
-        type: NOTIFICATIONS_UPDATE,
-        notification,
-        usePendingItems: preferPendingItems,
-        meta: (playSound && !filtered) ? { sound: 'boop' } : undefined,
-      });
+
+      dispatch(notificationsUpdate(notification, preferPendingItems, playSound && !filtered));
 
       fetchRelatedRelationships(dispatch, [notification]);
     } else if (playSound && !filtered) {

app/javascript/mastodon/reducers/notifications.js
@@ -1,12 +1,12 @@
 import { fromJS, Map as ImmutableMap, List as ImmutableList } from 'immutable';
 
-import { DOMAIN_BLOCK_SUCCESS } from 'mastodon/actions/domain_blocks';
+import { blockDomainSuccess } from 'mastodon/actions/domain_blocks';
 
 import {
-  ACCOUNT_BLOCK_SUCCESS,
-  ACCOUNT_MUTE_SUCCESS,
-  FOLLOW_REQUEST_AUTHORIZE_SUCCESS,
-  FOLLOW_REQUEST_REJECT_SUCCESS,
+  authorizeFollowRequestSuccess,
+  blockAccountSuccess,
+  muteAccountSuccess,
+  rejectFollowRequestSuccess,
 } from '../actions/accounts';
 import {
   focusApp,
@@ -16,7 +16,7 @@ import {
   MARKERS_FETCH_SUCCESS,
 } from '../actions/markers';
 import {
-  NOTIFICATIONS_UPDATE,
+  notificationsUpdate,
   NOTIFICATIONS_EXPAND_SUCCESS,
   NOTIFICATIONS_EXPAND_REQUEST,
   NOTIFICATIONS_EXPAND_FAIL,
@@ -274,19 +274,19 @@ export default function notifications(state = initialState, action) {
     return state.set('items', ImmutableList()).set('pendingItems', ImmutableList()).set('hasMore', true);
   case NOTIFICATIONS_SCROLL_TOP:
     return updateTop(state, action.top);
-  case NOTIFICATIONS_UPDATE:
-    return normalizeNotification(state, action.notification, action.usePendingItems);
+  case notificationsUpdate.type:
+    return normalizeNotification(state, action.payload.notification, action.payload.usePendingItems);
   case NOTIFICATIONS_EXPAND_SUCCESS:
     return expandNormalizedNotifications(state, action.notifications, action.next, action.isLoadingMore, action.isLoadingRecent, action.usePendingItems);
-  case ACCOUNT_BLOCK_SUCCESS:
-    return filterNotifications(state, [action.relationship.id]);
-  case ACCOUNT_MUTE_SUCCESS:
-    return action.relationship.muting_notifications ? filterNotifications(state, [action.relationship.id]) : state;
-  case DOMAIN_BLOCK_SUCCESS:
-    return filterNotifications(state, action.accounts);
-  case FOLLOW_REQUEST_AUTHORIZE_SUCCESS:
-  case FOLLOW_REQUEST_REJECT_SUCCESS:
-    return filterNotifications(state, [action.id], 'follow_request');
+  case blockAccountSuccess.type:
+    return filterNotifications(state, [action.payload.relationship.id]);
+  case muteAccountSuccess.type:
+    return action.relationship.muting_notifications ? filterNotifications(state, [action.payload.relationship.id]) : state;
+  case blockDomainSuccess.type:
+    return filterNotifications(state, action.payload.accounts);
+  case authorizeFollowRequestSuccess.type:
+  case rejectFollowRequestSuccess.type:
+    return filterNotifications(state, [action.payload.id], 'follow_request');
   case NOTIFICATIONS_CLEAR:
     return state.set('items', ImmutableList()).set('pendingItems', ImmutableList()).set('hasMore', false);
   case TIMELINE_DELETE:

commit 1584dfc849804dc9b521c99c8fd1ce3cafd52732

Fix the `notificationsUpdate` call
Issue was introduced in #26559

app/javascript/mastodon/actions/notifications.js
@@ -98,7 +98,7 @@ export function updateNotifications(notification, intlMessages, intlLocale) {
       }
 
 
-      dispatch(notificationsUpdate(notification, preferPendingItems, playSound && !filtered));
+      dispatch(notificationsUpdate({ notification, preferPendingItems, playSound: playSound && !filtered}));
 
       fetchRelatedRelationships(dispatch, [notification]);
     } else if (playSound && !filtered) {