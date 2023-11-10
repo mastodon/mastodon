0b6a3a6d4b5b9d2ddcf54fc383814f5b24a95189
casually-blue <73141854+casually-blue@users.noreply.github.com>
2023-11-09 19:46:22 -0600

    Use alnum instead of a-zA-Z0-9_

--- app/models/custom_emoji.rb
+++ app/models/custom_emoji.rb
@@ -26,7 +26,7 @@ class CustomEmoji < ApplicationRecord
 
   LIMIT = 256.kilobytes
 
-  SHORTCODE_RE_FRAGMENT = '[a-zA-Z0-9_]{2,}'
+  SHORTCODE_RE_FRAGMENT = '(([[:alnum:]])|_){2,}'
 
   SCAN_RE = /(?<=[^[:alnum:]:]|\n|^)
     :(#{SHORTCODE_RE_FRAGMENT}):
