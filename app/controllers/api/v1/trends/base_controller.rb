ae504ee7e42c37db23e510ef6ebe69d534393140
Matt Jankowski <matt@jankowski.online>
2023-11-11 18:29:19 -0500

    Move insert_pagination_headers into base controller

--- app/controllers/api/v1/trends/base_controller.rb
+++ app/controllers/api/v1/trends/base_controller.rb
@@ -2,4 +2,10 @@
 
 class Api::V1::Trends::BaseController < Api::BaseController
   after_action :insert_pagination_headers
+
+  private
+
+  def insert_pagination_headers
+    set_pagination_headers(next_path, prev_path)
+  end
 end
