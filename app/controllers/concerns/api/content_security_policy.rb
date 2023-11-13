a5552eccfdb544ec6c83869ebd9672cf20e47ce7
Matt Jankowski <matt@jankowski.online>
2023-11-13 16:05:11 -0500

    List policies in constants in API setup

--- app/controllers/concerns/api/content_security_policy.rb
+++ app/controllers/concerns/api/content_security_policy.rb
@@ -3,25 +3,35 @@
 module Api::ContentSecurityPolicy
   extend ActiveSupport::Concern
 
+  FALLBACK_DIRECTIVES = %i(
+    base_uri
+    font_src
+    img_src
+    style_src
+    media_src
+    frame_src
+    manifest_src
+    connect_src
+    script_src
+    child_src
+    worker_src
+  ).freeze
+
+  NON_FALLBACK_DIRECTIVES = %i(
+    default_src
+    frame_ancestors
+    form_action
+  ).freeze
+
   included do
     content_security_policy do |policy|
-      # Set every directive that does not have a fallback
-      policy.default_src :none
-      policy.frame_ancestors :none
-      policy.form_action :none
+      NON_FALLBACK_DIRECTIVES.each do |directive|
+        policy.send directive, :none
+      end
 
-      # Disable every directive with a fallback to cut on response size
-      policy.base_uri false
-      policy.font_src false
-      policy.img_src false
-      policy.style_src false
-      policy.media_src false
-      policy.frame_src false
-      policy.manifest_src false
-      policy.connect_src false
-      policy.script_src false
-      policy.child_src false
-      policy.worker_src false
+      FALLBACK_DIRECTIVES.each do |directive|
+        policy.send directive, false
+      end
     end
   end
 end
