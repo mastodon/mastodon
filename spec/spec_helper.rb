7c72944661c100118555045a87ced7d1fa7cc417
Matt Jankowski <matt@jankowski.online>
2023-11-15 04:11:02 -0500

    Use `Lcov` simplecov formatter on CI and `HTML` elsewhere (#27859)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,14 @@
 
 if ENV['DISABLE_SIMPLECOV'] != 'true'
   require 'simplecov'
-  require 'simplecov-lcov'
-  SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+
+  if ENV['CI']
+    require 'simplecov-lcov'
+    SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+    SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+  else
+    SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+  end
   SimpleCov.start 'rails' do
     enable_coverage :branch
     enable_coverage_for_eval
