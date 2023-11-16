1e98859a28128fb609d0de6e0442022f089395e3
Matt Jankowski <matt@jankowski.online>
2023-11-16 11:42:49 -0500

    Use matchers in admin/export_domain_allows

--- spec/controllers/admin/export_domain_allows_controller_spec.rb
+++ spec/controllers/admin/export_domain_allows_controller_spec.rb
@@ -13,7 +13,9 @@ RSpec.describe Admin::ExportDomainAllowsController do
     it 'returns http success' do
       get :new
 
-      expect(response).to have_http_status(200)
+      expect(response)
+        .to have_http_status(200)
+        .and render_template(:new)
     end
   end
 
@@ -23,8 +25,12 @@ RSpec.describe Admin::ExportDomainAllowsController do
       Fabricate(:domain_allow, domain: 'better.domain')
 
       get :export, params: { format: :csv }
-      expect(response).to have_http_status(200)
-      expect(response.body).to eq(File.read(File.join(file_fixture_path, 'domain_allows.csv')))
+
+      expect(response)
+        .to have_http_status(200)
+        .and have_attributes(
+          body: eq(domain_allows_csv_file)
+        )
     end
   end
 
@@ -35,18 +41,40 @@ RSpec.describe Admin::ExportDomainAllowsController do
       expect(response).to redirect_to(admin_instances_path)
 
       # Header should not be imported
-      expect(DomainAllow.where(domain: '#domain').present?).to be(false)
+      expect(header_domain_allow).to_not be_present
 
       # Domains should now be added
       get :export, params: { format: :csv }
-      expect(response).to have_http_status(200)
-      expect(response.body).to eq(File.read(File.join(file_fixture_path, 'domain_allows.csv')))
+
+      expect(response)
+        .to have_http_status(200)
+        .and have_attributes(
+          body: eq(domain_allows_csv_file)
+        )
     end
 
     it 'displays error on no file selected' do
       post :import, params: { admin_import: {} }
-      expect(response).to redirect_to(admin_instances_path)
-      expect(flash[:error]).to eq(I18n.t('admin.export_domain_allows.no_file'))
+
+      expect(response)
+        .to redirect_to(admin_instances_path)
+
+      expect(flash.to_h.symbolize_keys)
+        .to include(
+          error: eq(I18n.t('admin.export_domain_allows.no_file'))
+        )
     end
+
+    private
+
+    def header_domain_allow
+      DomainAllow.where(domain: '#domain')
+    end
+  end
+
+  private
+
+  def domain_allows_csv_file
+    File.read(File.join(file_fixture_path, 'domain_allows.csv'))
   end
 end
