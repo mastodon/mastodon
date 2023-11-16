7868a863ef0165767d5b59e64eef3c76ee858472
Matt Jankowski <matt@jankowski.online>
2023-11-16 11:01:36 -0500

    Use matchers in admin/action_logs

--- spec/controllers/admin/action_logs_controller_spec.rb
+++ spec/controllers/admin/action_logs_controller_spec.rb
@@ -9,19 +9,43 @@ describe Admin::ActionLogsController do
   let!(:account) { Fabricate(:account) }
 
   before do
-    _orphaned_logs = %w(
-      Account User UserRole Report DomainBlock DomainAllow
-      EmailDomainBlock UnavailableDomain Status AccountWarning
-      Announcement IpBlock Instance CustomEmoji CanonicalEmailBlock Appeal
-    ).map { |type| Admin::ActionLog.new(account: account, action: 'destroy', target_type: type, target_id: 1312).save! }
+    orphaned_log_types.map do |type|
+      Admin::ActionLog.new(account: account, action: 'destroy', target_type: type, target_id: 1312).save!
+    end
   end
 
   describe 'GET #index' do
+    before { sign_in Fabricate(:user, role: UserRole.find_by(name: 'Admin')) }
+
     it 'returns 200' do
-      sign_in Fabricate(:user, role: UserRole.find_by(name: 'Admin'))
       get :index, params: { page: 1 }
 
-      expect(response).to have_http_status(200)
+      expect(response)
+        .to have_http_status(200)
+        .and render_template(:index)
     end
+  end
+
+  private
+
+  def orphaned_log_types
+    %w(
+      Account
+      AccountWarning
+      Announcement
+      Appeal
+      CanonicalEmailBlock
+      CustomEmoji
+      DomainAllow
+      DomainBlock
+      EmailDomainBlock
+      Instance
+      IpBlock
+      Report
+      Status
+      UnavailableDomain
+      User
+      UserRole
+    )
   end
 end
