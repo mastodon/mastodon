45de618a319958cd1d5d97952d6114693e5fc791
Matt Jankowski <matt@jankowski.online>
2023-11-16 11:33:44 -0500

    Use matchers in admin/email_domain_blocks

--- spec/controllers/admin/email_domain_blocks_controller_spec.rb
+++ spec/controllers/admin/email_domain_blocks_controller_spec.rb
@@ -20,39 +20,51 @@ RSpec.describe Admin::EmailDomainBlocksController do
     it 'returns http success' do
       2.times { Fabricate(:email_domain_block) }
       get :index, params: { page: 2 }
-      expect(response).to have_http_status(200)
+
+      expect(response)
+        .to have_http_status(200)
+        .and render_template(:index)
     end
   end
 
   describe 'GET #new' do
     it 'returns http success' do
       get :new
-      expect(response).to have_http_status(200)
+
+      expect(response)
+        .to have_http_status(200)
+        .and render_template(:new)
     end
   end
 
   describe 'POST #create' do
+    let(:domain) { 'example.com' }
+
     context 'when resolve button is pressed' do
       before do
-        post :create, params: { email_domain_block: { domain: 'example.com' } }
+        post :create, params: { email_domain_block: { domain: domain } }
       end
 
       it 'renders new template' do
-        expect(response).to render_template(:new)
+        expect(response)
+          .to render_template(:new)
       end
     end
 
     context 'when save button is pressed' do
       before do
-        post :create, params: { email_domain_block: { domain: 'example.com' }, save: '' }
+        post :create, params: { email_domain_block: { domain: domain }, save: '' }
       end
 
-      it 'blocks the domain' do
-        expect(EmailDomainBlock.find_by(domain: 'example.com')).to_not be_nil
+      it 'blocks the domain and redirects' do
+        expect(created_domain_block).to_not be_nil
+
+        expect(response)
+          .to redirect_to(admin_email_domain_blocks_path)
       end
 
-      it 'redirects to e-mail domain blocks' do
-        expect(response).to redirect_to(admin_email_domain_blocks_path)
+      def created_domain_block
+        EmailDomainBlock.find_by(domain: domain)
       end
     end
   end
