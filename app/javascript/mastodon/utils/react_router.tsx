cfe278568dbc31baad152ac68a9b519882908153
Josh Goldberg <git@joshuakgoldberg.com>
2023-11-16 12:05:46 -0500

    Two missing files, thanks mdsubuntu

--- app/javascript/mastodon/utils/react_router.tsx
+++ app/javascript/mastodon/utils/react_router.tsx
@@ -1,8 +1,8 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import { __RouterContext } from "react-router";
+import { __RouterContext } from 'react-router';
 
-import hoistStatics from "hoist-non-react-statics";
+import hoistStatics from 'hoist-non-react-statics';
 
 export const WithRouterPropTypes = {
   match: PropTypes.object.isRequired,
@@ -16,31 +16,37 @@ export const WithOptionalRouterPropTypes = {
   history: PropTypes.object,
 };
 
+export interface OptionalRouterProps {
+  ref: unknown;
+  wrappedComponentRef: unknown;
+}
+
 // This is copied from https://github.com/remix-run/react-router/blob/v5.3.4/packages/react-router/modules/withRouter.js
 // but does not fail if called outside of a React Router context
-export function withOptionalRouter(Component) {
-  const displayName = `withRouter(${Component.displayName || Component.name})`;
-  const C = props => {
+export function withOptionalRouter<
+  ComponentType extends React.ComponentType<OptionalRouterProps>,
+>(Component: ComponentType) {
+  const displayName = `withRouter(${Component.displayName ?? Component.name})`;
+  const C = (props: React.ComponentProps<ComponentType>) => {
     const { wrappedComponentRef, ...remainingProps } = props;
 
     return (
       <__RouterContext.Consumer>
-        {context => {
-          if(context)
+        {(context) => {
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+          if (context) {
             return (
+              // @ts-expect-error - Dynamic covariant generic components are tough to type.
               <Component
                 {...remainingProps}
                 {...context}
                 ref={wrappedComponentRef}
               />
             );
-          else
-            return (
-              <Component
-                {...remainingProps}
-                ref={wrappedComponentRef}
-              />
-            );
+          } else {
+            // @ts-expect-error - Dynamic covariant generic components are tough to type.
+            return <Component {...remainingProps} ref={wrappedComponentRef} />;
+          }
         }}
       </__RouterContext.Consumer>
     );
@@ -53,8 +59,8 @@ export function withOptionalRouter(Component) {
     wrappedComponentRef: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.func,
-      PropTypes.object
-    ])
+      PropTypes.object,
+    ]),
   };
 
   return hoistStatics(C, Component);
