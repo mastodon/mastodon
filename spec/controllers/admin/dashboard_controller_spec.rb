a049a2f91629b611155ca62687cf2984c53a6024
Matt Jankowski <matt@jankowski.online>
2023-11-16 11:11:05 -0500

    Use matchers in admin/dashboard

--- spec/controllers/admin/dashboard_controller_spec.rb
+++ spec/controllers/admin/dashboard_controller_spec.rb
@@ -6,19 +6,29 @@ describe Admin::DashboardController do
   render_views
 
   describe 'GET #index' do
+    let(:admin_user) { Fabricate(:user, role: UserRole.find_by(name: 'Admin')) }
+
     before do
-      allow(Admin::SystemCheck).to receive(:perform).and_return([
-                                                                  Admin::SystemCheck::Message.new(:database_schema_check),
-                                                                  Admin::SystemCheck::Message.new(:rules_check, nil, admin_rules_path),
-                                                                  Admin::SystemCheck::Message.new(:sidekiq_process_check, 'foo, bar'),
-                                                                ])
-      sign_in Fabricate(:user, role: UserRole.find_by(name: 'Admin'))
+      allow(Admin::SystemCheck).to receive(:perform).and_return(system_check_messages)
+      sign_in admin_user
     end
 
     it 'returns 200' do
       get :index
 
-      expect(response).to have_http_status(200)
+      expect(response)
+        .to have_http_status(200)
+        .and render_template(:index)
+    end
+
+    private
+
+    def system_check_messages
+      [
+        Admin::SystemCheck::Message.new(:database_schema_check),
+        Admin::SystemCheck::Message.new(:rules_check, nil, admin_rules_path),
+        Admin::SystemCheck::Message.new(:sidekiq_process_check, 'foo, bar'),
+      ]
     end
   end
 end
