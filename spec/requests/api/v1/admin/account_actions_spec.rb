549e8e7baf62eb4ecb4dd039e301e6280889218d
Matt Jankowski <matt@jankowski.online>
2023-11-17 04:50:19 -0500

    Add `email_spec` and speedup/cleanup to `spec/mailers` (#27902)

--- spec/requests/api/v1/admin/account_actions_spec.rb
+++ spec/requests/api/v1/admin/account_actions_spec.rb
@@ -8,18 +8,12 @@ RSpec.describe 'Account actions' do
   let(:scopes)  { 'admin:write admin:write:accounts' }
   let(:token)   { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
   let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
-  let(:mailer)  { instance_double(ActionMailer::MessageDelivery, deliver_later!: nil) }
-
-  before do
-    allow(UserMailer).to receive(:warning).with(target_account.user, anything).and_return(mailer)
-  end
 
   shared_examples 'a successful notification delivery' do
     it 'notifies the user about the action taken' do
-      subject
-
-      expect(UserMailer).to have_received(:warning).with(target_account.user, anything).once
-      expect(mailer).to have_received(:deliver_later!).once
+      expect { subject }
+        .to have_enqueued_job(ActionMailer::MailDeliveryJob)
+        .with('UserMailer', 'warning', 'deliver_now!', args: [User, AccountWarning])
     end
   end
 
