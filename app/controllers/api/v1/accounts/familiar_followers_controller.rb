e00397a0e06ced48663205b14fd0eaa7f30d2ec0
Kevin Bongart <154600+KevinBongart@users.noreply.github.com>
2023-11-21 12:44:38 -0500

    Remove ordering of accounts in response

--- app/controllers/api/v1/accounts/familiar_followers_controller.rb
+++ app/controllers/api/v1/accounts/familiar_followers_controller.rb
@@ -12,15 +12,14 @@ class Api::V1::Accounts::FamiliarFollowersController < Api::BaseController
   private
 
   def set_accounts
-    account_ids = unique_account_ids_from_params
-    @accounts = Account.without_suspended.where(id: account_ids).select('id, hide_collections').index_by(&:id).values_at(*account_ids).compact
+    @accounts = Account.without_suspended.where(id: account_ids).select('id, hide_collections').compact
   end
 
   def familiar_followers
     FamiliarFollowersPresenter.new(@accounts, current_user.account_id)
   end
 
-  def unique_account_ids_from_params
+  def account_ids
     Array(params[:id]).map(&:to_i).uniq
   end
 end
--- app/controllers/api/v1/accounts/relationships_controller.rb
+++ app/controllers/api/v1/accounts/relationships_controller.rb
@@ -5,13 +5,9 @@ class Api::V1::Accounts::RelationshipsController < Api::BaseController
   before_action :require_user!
 
   def index
-    account_ids = unique_account_ids_from_params
-
     scope = Account.where(id: account_ids).select('id')
     scope.merge!(Account.without_suspended) unless truthy_param?(:with_suspended)
-    # .where doesn't guarantee that our results are in the same order
-    # we requested them, so return the "right" order to the requestor.
-    @accounts = scope.index_by(&:id).values_at(*account_ids).compact
+    @accounts = scope.compact
     render json: @accounts, each_serializer: REST::RelationshipSerializer, relationships: relationships
   end
 
@@ -21,7 +17,7 @@ class Api::V1::Accounts::RelationshipsController < Api::BaseController
     AccountRelationshipsPresenter.new(@accounts, current_user.account_id)
   end
 
-  def unique_account_ids_from_params
+  def account_ids
     Array(params[:id]).map(&:to_i).uniq
   end
 end
