67fd3187b3cf626d836506431169c627ec066730
Matt Jankowski <matt@jankowski.online>
2023-11-21 10:51:40 -0500

    Update rspec fixture path config to silence deprecation warning (#28018)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,7 +60,9 @@ RSpec.configure do |config|
   # By default, skip the elastic search integration specs
   config.filter_run_excluding search: true
 
-  config.fixture_path = Rails.root.join('spec', 'fixtures')
+  config.fixture_paths = [
+    Rails.root.join('spec', 'fixtures'),
+  ]
   config.use_transactional_fixtures = true
   config.order = 'random'
   config.infer_spec_type_from_file_location!
