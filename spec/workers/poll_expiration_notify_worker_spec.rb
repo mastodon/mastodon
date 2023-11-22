3b71e33cee9d09806c4229545981ac91a0805b6a
Matt Jankowski <matt@jankowski.online>
2023-11-22 16:26:02 -0500

    Consolidate configuration of `Sidekiq::Testing.fake!` setup

--- spec/workers/poll_expiration_notify_worker_spec.rb
+++ spec/workers/poll_expiration_notify_worker_spec.rb
@@ -10,13 +10,7 @@ describe PollExpirationNotifyWorker do
   let(:remote?) { false }
   let(:poll_vote) { Fabricate(:poll_vote, poll: poll) }
 
-  describe '#perform' do
-    around do |example|
-      Sidekiq::Testing.fake! do
-        example.run
-      end
-    end
-
+  describe '#perform', :sidekiq_fake do
     it 'runs without error for missing record' do
       expect { worker.perform(nil) }.to_not raise_error
     end
