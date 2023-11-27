5e75d7ee1f3058c79427633dc48162cd5f655cdc
Matt Jankowski <matt@jankowski.online>
2023-11-27 17:05:30 -0500

    Add `dependent: :destroy` to association Status#trend

--- app/models/status.rb
+++ app/models/status.rb
@@ -84,7 +84,7 @@ class Status < ApplicationRecord
   has_one :notification, as: :activity, dependent: :destroy
   has_one :status_stat, inverse_of: :status
   has_one :poll, inverse_of: :status, dependent: :destroy
-  has_one :trend, class_name: 'StatusTrend', inverse_of: :status
+  has_one :trend, class_name: 'StatusTrend', inverse_of: :status, dependent: :destroy
 
   validates :uri, uniqueness: true, presence: true, unless: :local?
   validates :text, presence: true, unless: -> { with_media? || reblog? }
