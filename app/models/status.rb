b410a8cf10236d9a53a2478354b67ff83bfa20cb
Matt Jankowski <matt@jankowski.online>
2023-11-27 17:06:54 -0500

    Add `dependent: :destroy` to association Status#status_stat

--- app/models/status.rb
+++ app/models/status.rb
@@ -82,7 +82,7 @@ class Status < ApplicationRecord
 
   has_one :preview_cards_status, inverse_of: :status # Because of a composite primary key, the dependent option cannot be used
   has_one :notification, as: :activity, dependent: :destroy
-  has_one :status_stat, inverse_of: :status
+  has_one :status_stat, inverse_of: :status, dependent: :destroy
   has_one :poll, inverse_of: :status, dependent: :destroy
   has_one :trend, class_name: 'StatusTrend', inverse_of: :status, dependent: :destroy
 
