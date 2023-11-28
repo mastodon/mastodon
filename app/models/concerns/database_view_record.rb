b75a150367bd14b47c77406fcddac23dd20d85fd
Matt Jankowski <matt@jankowski.online>
2023-11-28 13:22:15 -0500

    Extract scenic view model common methods to concern

---
+++ app/models/concerns/database_view_record.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module DatabaseViewRecord
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def refresh
+      Scenic
+        .database
+        .refresh_materialized_view(
+          table_name,
+          concurrently: true,
+          cascade: false
+        )
+    end
+  end
+
+  def readonly?
+    true
+  end
+end
