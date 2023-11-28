3a7f10c3f10e6a7ab154030a2abf5a1a68c1b8f9
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2023-11-28 19:20:10 +0100

    Converted hashtag.jsx to TypeScript (#27872)
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>
    Co-authored-by: Renaud Chaput <renchap@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -284,7 +284,6 @@ module.exports = defineConfig({
     'formatjs/no-id': 'off', // IDs are used for translation keys
     'formatjs/no-invalid-icu': 'error',
     'formatjs/no-literal-string-in-jsx': 'off', // Should be looked at, but mainly flagging punctuation outside of strings
-    'formatjs/no-multiple-plurals': 'off', // Only used by hashtag.jsx
     'formatjs/no-multiple-whitespaces': 'error',
     'formatjs/no-offset': 'error',
     'formatjs/no-useless-message': 'error',
@@ -354,7 +353,7 @@ module.exports = defineConfig({
         '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
         '@typescript-eslint/consistent-type-exports': 'error',
         '@typescript-eslint/consistent-type-imports': 'error',
-        "@typescript-eslint/prefer-nullish-coalescing": ['error', {ignorePrimitives: {boolean: true}}],
+        "@typescript-eslint/prefer-nullish-coalescing": ['error', { ignorePrimitives: { boolean: true } }],
 
         'jsdoc/require-jsdoc': 'off',
 
