8e58f1f72736fdedab23958f2c46e03929b822f1
Matt Jankowski <matt@jankowski.online>
2023-11-28 08:49:10 -0500

    Move add and remove index to private methods

--- db/migrate/20231018192110_add_index_to_webauthn_credentials_user_id_nickname.rb
+++ db/migrate/20231018192110_add_index_to_webauthn_credentials_user_id_nickname.rb
@@ -5,14 +5,22 @@ class AddIndexToWebauthnCredentialsUserIdNickname < ActiveRecord::Migration[7.0]
 
   def up
     deduplicate_records
-    add_index :webauthn_credentials, [:nickname, :user_id], unique: true, algorithm: :concurrently
+    add_index_to_table
   end
 
   def down
-    remove_index :webauthn_credentials, [:nickname, :user_id]
+    remove_index_from_table
   end
 
   private
+
+  def add_index_to_table
+    add_index :webauthn_credentials, [:nickname, :user_id], unique: true, algorithm: :concurrently
+  end
+
+  def remove_index_from_table
+    remove_index :webauthn_credentials, [:nickname, :user_id]
+  end
 
   def deduplicate_records
     safety_assured do
