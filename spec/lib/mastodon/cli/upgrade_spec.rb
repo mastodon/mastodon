08db7a01c035370f96f418e9e0180aa57b144f45
Matt Jankowski <matt@jankowski.online>
2023-12-01 11:22:35 -0500

    Add spec coverage for `CLI::Upgrade#storage_schema` command

--- spec/lib/mastodon/cli/upgrade_spec.rb
+++ spec/lib/mastodon/cli/upgrade_spec.rb
@@ -4,5 +4,24 @@ require 'rails_helper'
 require 'mastodon/cli/upgrade'
 
 describe Mastodon::CLI::Upgrade do
+  let(:cli) { described_class.new }
+
   it_behaves_like 'CLI Command'
+
+  describe '#storage_schema' do
+    context 'with records that dont need upgrading' do
+      let(:options) { {} }
+
+      before do
+        Fabricate(:account)
+        Fabricate(:media_attachment)
+      end
+
+      it 'does not upgrade storage for the attachments' do
+        expect { cli.invoke(:storage_schema, [], options) }.to output(
+          a_string_including('Upgraded storage schema of 0 records')
+        ).to_stdout
+      end
+    end
+  end
 end
