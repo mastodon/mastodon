528ee6b45e13de11b2430b923e98191138e06554
Matt Jankowski <matt@jankowski.online>
2023-12-01 10:23:21 -0500

    Add spec coverage for `CLI::Domains#purge` command

--- spec/lib/mastodon/cli/domains_spec.rb
+++ spec/lib/mastodon/cli/domains_spec.rb
@@ -4,5 +4,22 @@ require 'rails_helper'
 require 'mastodon/cli/domains'
 
 describe Mastodon::CLI::Domains do
+  let(:cli) { described_class.new }
+
   it_behaves_like 'CLI Command'
+
+  describe '#purge' do
+    context 'with accounts from the domain' do
+      let(:options) { {} }
+      let(:domain) { 'host.example' }
+      let!(:account) { Fabricate(:account, domain: domain) }
+
+      it 'removes the account' do
+        expect { cli.invoke(:purge, [domain], options) }.to output(
+          a_string_including('Removed 1 accounts')
+        ).to_stdout
+        expect { account.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 end
