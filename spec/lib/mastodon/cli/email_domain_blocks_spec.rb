639c7fd624192e77f23c24c57bc7112f6ee53f69
Matt Jankowski <matt@jankowski.online>
2023-12-01 15:17:14 -0500

    Add child record check

--- spec/lib/mastodon/cli/email_domain_blocks_spec.rb
+++ spec/lib/mastodon/cli/email_domain_blocks_spec.rb
@@ -10,12 +10,14 @@ describe Mastodon::CLI::EmailDomainBlocks do
 
   describe '#list' do
     context 'with email domain block records' do
-      let!(:email_domain_block) { Fabricate(:email_domain_block) }
+      let!(:parent_block) { Fabricate(:email_domain_block) }
+      let!(:child_block) { Fabricate(:email_domain_block, parent: parent_block) }
       let(:options) { {} }
 
       it 'lists the blocks' do
         expect { cli.invoke(:list, [], options) }.to output(
-          a_string_including(email_domain_block.domain)
+          a_string_including(parent_block.domain)
+          .and(a_string_including(child_block.domain))
         ).to_stdout
       end
     end
