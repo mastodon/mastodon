3bc437b99a19bba26c0c637b18ce6c7410d5a606
Matt Jankowski <matt@jankowski.online>
2023-12-01 11:00:44 -0500

    Fix `Style/RedundantParentheses` cop (#28176)

--- lib/mastodon/cli/media.rb
+++ lib/mastodon/cli/media.rb
@@ -48,8 +48,8 @@ module Mastodon::CLI
           next if account.avatar.blank? && account.header.blank?
           next if options[:remove_headers] && account.header.blank?
 
-          size = (account.header_file_size || 0)
-          size += (account.avatar_file_size || 0) if options[:prune_profiles]
+          size = account.header_file_size || 0
+          size += account.avatar_file_size || 0 if options[:prune_profiles]
 
           unless dry_run?
             account.header.destroy
