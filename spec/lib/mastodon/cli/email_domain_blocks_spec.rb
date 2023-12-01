49e37f8b5f0d89154482a079041b056dc30c4e1d
Matt Jankowski <matt@jankowski.online>
2023-12-01 15:12:01 -0500

    Add spec coverage for `CLI::EmailDomainBlocks#list` command

--- spec/lib/mastodon/cli/email_domain_blocks_spec.rb
+++ spec/lib/mastodon/cli/email_domain_blocks_spec.rb
@@ -4,5 +4,20 @@ require 'rails_helper'
 require 'mastodon/cli/email_domain_blocks'
 
 describe Mastodon::CLI::EmailDomainBlocks do
+  let(:cli) { described_class.new }
+
   it_behaves_like 'CLI Command'
+
+  describe '#list' do
+    context 'with email domain block records' do
+      let!(:email_domain_block) { Fabricate(:email_domain_block) }
+      let(:options) { {} }
+
+      it 'lists the blocks' do
+        expect { cli.invoke(:list, [], options) }.to output(
+          a_string_including(email_domain_block.domain)
+        ).to_stdout
+      end
+    end
+  end
 end
