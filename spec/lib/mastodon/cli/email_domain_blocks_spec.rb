041a12fac4052f0d5ea6ff8e97a53db5ae0964f4
Matt Jankowski <matt@jankowski.online>
2023-12-01 15:35:25 -0500

    Coverage for remove

--- spec/lib/mastodon/cli/email_domain_blocks_spec.rb
+++ spec/lib/mastodon/cli/email_domain_blocks_spec.rb
@@ -60,4 +60,42 @@ describe Mastodon::CLI::EmailDomainBlocks do
       end
     end
   end
+
+  describe '#remove' do
+    context 'without any options' do
+      let(:options) { {} }
+
+      it 'warns about usage and exits' do
+        expect { cli.invoke(:remove, [], options) }.to output(
+          a_string_including('No domain(s) given')
+        ).to_stdout.and raise_error(SystemExit)
+      end
+    end
+
+    context 'when blocks exist' do
+      let(:options) { {} }
+      let(:domain) { 'host.example' }
+
+      before { Fabricate(:email_domain_block, domain: domain) }
+
+      it 'removes the block' do
+        expect { cli.invoke(:remove, [domain], options) }.to output(
+          a_string_including('Removed 1')
+        ).to_stdout
+          .and(change(EmailDomainBlock, :count).by(-1))
+      end
+    end
+
+    context 'when no blocks exist' do
+      let(:options) { {} }
+      let(:domain) { 'host.example' }
+
+      it 'does not remove a block' do
+        expect { cli.invoke(:remove, [domain], options) }.to output(
+          a_string_including('is not yet blocked')
+        ).to_stdout
+          .and(not_change(EmailDomainBlock, :count))
+      end
+    end
+  end
 end
