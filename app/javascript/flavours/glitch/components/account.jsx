21df2a68ac7827b63f012fd8c28e7888f6830c39
Claire <claire.github-309c@sitedethib.com>
2023-12-02 17:16:21 +0100

    Hide followers count when hidden by instance or user

--- app/javascript/flavours/glitch/components/account.jsx
+++ app/javascript/flavours/glitch/components/account.jsx
@@ -160,7 +160,9 @@ class Account extends ImmutablePureComponent {
               <DisplayName account={account} inline />
               {!minimal && (
                 <div className='account__details'>
-                  <ShortNumber value={account.get('followers_count')} renderer={FollowersCounter} /> {verification} {muteTimeRemaining}
+                  {account.get('followers_count') !== -1 && (
+                    <ShortNumber value={account.get('followers_count')} renderer={FollowersCounter} />
+                  )} {verification} {muteTimeRemaining}
                 </div>
               )}
             </div>
