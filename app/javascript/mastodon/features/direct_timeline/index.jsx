2b57f9fc76b74b29a9b35138cce08fe27c1c2a71
Tod Beardsley <todb@packetfu.com>
2023-12-04 20:49:55 -0600

    Be explicit about user visibility in DMs
    See mastodon#20657
    
    Signed-off-by: Tod Beardsley <todb@packetfu.com>

--- app/javascript/mastodon/features/direct_timeline/index.jsx
+++ app/javascript/mastodon/features/direct_timeline/index.jsx
@@ -99,7 +99,7 @@ class DirectTimeline extends PureComponent {
           timelineId='direct'
           bindToDocument={!multiColumn}
           onLoadMore={this.handleLoadMore}
-          prepend={<div className='follow_requests-unlocked_explanation'><span><FormattedMessage id='compose_form.encryption_warning' defaultMessage='Posts on Mastodon are not end-to-end encrypted. Do not share any dangerous information over Mastodon.' /> <a href='/terms' target='_blank'><FormattedMessage id='compose_form.direct_message_warning_learn_more' defaultMessage='Learn more' /></a></span></div>}
+          prepend={<div className='follow_requests-unlocked_explanation'><span><FormattedMessage id='compose_form.encryption_warning' defaultMessage='Posts on Mastodon are not end-to-end encrypted, and mentioned users can see these posts. Do not share any dangerous information over Mastodon.' /> <a href='/terms' target='_blank'><FormattedMessage id='compose_form.direct_message_warning_learn_more' defaultMessage='Learn more' /></a></span></div>}
           alwaysPrepend
           emptyMessage={<FormattedMessage id='empty_column.direct' defaultMessage="You don't have any private mentions yet. When you send or receive one, it will show up here." />}
         />
