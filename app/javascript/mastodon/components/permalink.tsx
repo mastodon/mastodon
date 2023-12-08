7ac19aeb5330f18eb59c04b75263e6ae9690227c
lindwurm <lindwurm.q@gmail.com>
2023-12-08 16:36:52 +0900

    [maud] Replace old permalink.js with our new one
    * to fix external link
    
    Co-authored-by: MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

---
+++ app/javascript/mastodon/components/permalink.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+interface Props
+  extends React.PropsWithChildren<React.HTMLAttributes<HTMLAnchorElement>> {
+  className?: string;
+  href: string;
+  to: string;
+  onInterceptClick: () => boolean;
+}
+
+const Permalink: React.FC<Props> = (inProps) => {
+  const { className, href, to, onInterceptClick, children, ...props } = inProps;
+  const history = useHistory();
+
+  const linkClickHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (onInterceptClick && onInterceptClick()) {
+      e.preventDefault();
+      return;
+    }
+
+    if (e.button === 0 && !(e.ctrlKey || e.metaKey)) {
+      e.preventDefault();
+      history.push(to);
+    }
+  };
+
+  return (
+    <a
+      target="_blank"
+      href={href}
+      onClick={linkClickHandler}
+      {...props}
+      className={`permalink${className ? ' ' + className : ''}`}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Permalink;
