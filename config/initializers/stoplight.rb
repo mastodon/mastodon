afd13fcf5e707019f3ba7e8ebf21706ed62801f6
Matt Jankowski <matt@jankowski.online>
2023-12-14 10:37:15 -0500

    Set Stoplight options directly on class and not via `Stoplight::Light`

--- config/initializers/stoplight.rb
+++ config/initializers/stoplight.rb
@@ -3,6 +3,6 @@
 require 'stoplight'
 
 Rails.application.reloader.to_prepare do
-  Stoplight::Light.default_data_store = Stoplight::DataStore::Redis.new(RedisConfiguration.new.connection)
-  Stoplight::Light.default_notifiers  = [Stoplight::Notifier::Logger.new(Rails.logger)]
+  Stoplight.default_data_store = Stoplight::DataStore::Redis.new(RedisConfiguration.new.connection)
+  Stoplight.default_notifiers  = [Stoplight::Notifier::Logger.new(Rails.logger)]
 end
