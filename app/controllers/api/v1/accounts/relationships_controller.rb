7d9b209fe84b00eff348ea9d54905cbfffa79788
Claire <claire.github-309c@sitedethib.com>
2023-12-18 17:14:43 +0100

    Fix call to inefficient `delete_matched` cache method in domain blocks (#28374)

--- app/controllers/api/v1/accounts/relationships_controller.rb
+++ app/controllers/api/v1/accounts/relationships_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Accounts::RelationshipsController < Api::BaseController
   before_action :require_user!
 
   def index
-    @accounts = Account.where(id: account_ids).select('id')
+    @accounts = Account.where(id: account_ids).select(:id, :domain)
     @accounts.merge!(Account.without_suspended) unless truthy_param?(:with_suspended)
     render json: @accounts, each_serializer: REST::RelationshipSerializer, relationships: relationships
   end
