98817b5b9a3de61acca3b8c234acd03b9b621b03
Matt Jankowski <matt@jankowski.online>
2024-01-06 10:12:52 -0500

    Enable Rails 7.1 default setting for invalid cache expiration time

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -78,7 +78,7 @@ Rails.application.config.active_job.use_big_decimal_serializer = true
 # `write` are given an invalid `expires_at` or `expires_in` time.
 # Options are `true`, and `false`. If `false`, the exception will be reported
 # as `handled` and logged instead.
-# Rails.application.config.active_support.raise_on_invalid_cache_expiration_time = true
+Rails.application.config.active_support.raise_on_invalid_cache_expiration_time = true
 
 # Specify whether Query Logs will format tags using the SQLCommenter format
 # (https://open-telemetry.github.io/opentelemetry-sqlcommenter/), or using the legacy format.
