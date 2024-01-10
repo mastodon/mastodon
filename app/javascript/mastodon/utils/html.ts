b13e7c4fb82636421435c328efbd0e1a5304b2e5
Shlee <github@shl.ee>
2024-01-10 14:18:01 +1030

    lint

--- app/javascript/mastodon/utils/html.ts
+++ app/javascript/mastodon/utils/html.ts
@@ -2,9 +2,11 @@
 export const unescapeHTML = (html: string) => {
   const wrapper = document.createElement('div');
   const sanitizedHTML = new DOMParser().parseFromString(html, 'text/html');
-  wrapper.textContent = sanitizedHTML.body.textContent
-    .replace(/<br\s*\/?>/g, '\n')
-    .replace(/<\/p><p>/g, '\n\n')
-    .replace(/<[^>]*>/g, '');
+  if (sanitizedHTML !== null) {
+    wrapper.textContent = sanitizedHTML.body.textContent
+      .replace(/<br\s*\/?>/g, '\n')
+      .replace(/<\/p><p>/g, '\n\n')
+      .replace(/<[^>]*>/g, '');
   return wrapper.textContent;
+  }
 };
