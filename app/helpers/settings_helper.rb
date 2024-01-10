a3698f652fc1661d311b2375b9e44a7cfed83861
Matt Jankowski <matt@jankowski.online>
2024-01-10 15:36:46 -0500

    Clean up helper method

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -14,7 +14,7 @@ module SettingsHelper
       [
         t('simple_form.hints.featured_tag.name'),
         safe_join(
-          recently_used_tags.map { |tag| link_to("##{tag.display_name}", settings_featured_tags_path(featured_tag: { name: tag.name }), method: :post) },
+          links_for_featured_tags(recently_used_tags),
           ', '
         ),
       ],
@@ -40,5 +40,19 @@ module SettingsHelper
     link_to ActivityPub::TagManager.instance.url_for(account), class: 'name-tag', title: account.acct do
       safe_join([image_tag(account.avatar.url, width: 15, height: 15, alt: '', class: 'avatar'), content_tag(:span, account.acct, class: 'username')], ' ')
     end
+  end
+
+  private
+
+  def links_for_featured_tags(tags)
+    tags.map { |tag| post_link_to_featured_tag(tag) }
+  end
+
+  def post_link_to_featured_tag(tag)
+    link_to(
+      "##{tag.display_name}",
+      settings_featured_tags_path(featured_tag: { name: tag.name }),
+      method: :post
+    )
   end
 end
