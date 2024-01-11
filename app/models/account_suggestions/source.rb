e85c1d7e5088ea02c3947aa14b6cf055001fe440
Matt Jankowski <matt@jankowski.online>
2024-01-11 11:53:47 -0500

    Move followable by logic to suggestion class

--- app/models/account_suggestions/source.rb
+++ app/models/account_suggestions/source.rb
@@ -8,11 +8,31 @@ class AccountSuggestions::Source
   protected
 
   def base_account_scope(account)
-    Account.searchable
-           .followable_by(account)
-           .not_excluded_by_account(account)
-           .not_domain_blocked_by_account(account)
-           .where.not(id: account.id)
-           .joins("LEFT OUTER JOIN follow_recommendation_mutes ON follow_recommendation_mutes.target_account_id = accounts.id AND follow_recommendation_mutes.account_id = #{account.id}").where(follow_recommendation_mutes: { target_account_id: nil })
+    Account
+      .searchable
+      .where.not(follows_sql, id: account.id)
+      .where.not(follow_requests_sql, id: account.id)
+      .not_excluded_by_account(account)
+      .not_domain_blocked_by_account(account)
+      .where.not(id: account.id)
+      .where.not(follow_recommendation_mutes_sql, id: account.id)
+  end
+
+  def follows_sql
+    <<~SQL.squish
+      EXISTS (SELECT 1 FROM follows WHERE follows.target_account_id = accounts.id AND follows.account_id = :id)
+    SQL
+  end
+
+  def follow_requests_sql
+    <<~SQL.squish
+      EXISTS (SELECT 1 FROM follow_requests WHERE follow_requests.target_account_id = accounts.id AND follow_requests.account_id = :id)
+    SQL
+  end
+
+  def follow_recommendation_mutes_sql
+    <<~SQL.squish
+      EXISTS (SELECT 1 FROM follow_recommendation_mutes WHERE follow_recommendation_mutes.target_account_id = accounts.id AND follow_recommendation_mutes.account_id = :id)
+    SQL
   end
 end
