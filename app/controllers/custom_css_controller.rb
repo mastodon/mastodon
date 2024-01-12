7801db7ba463a71b8a11a2867486b012a2ec3972
Matt Jankowski <matt@jankowski.online>
2024-01-12 04:19:25 -0500

    Spec coverage for custom css endpoint (#28706)

--- app/controllers/custom_css_controller.rb
+++ app/controllers/custom_css_controller.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 class CustomCssController < ActionController::Base # rubocop:disable Rails/ApplicationController
+  before_action :set_user_roles
+
   def show
     expires_in 3.minutes, public: true
     render content_type: 'text/css'
+  end
+
+  private
+
+  def custom_css_styles
+    Setting.custom_css
+  end
+  helper_method :custom_css_styles
+
+  def set_user_roles
+    @user_roles = UserRole.where(highlighted: true).where.not(color: [nil, ''])
   end
 end
