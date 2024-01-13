12136cc16b8dfe782f8b0734536814bd42f1fecd
Matt Jankowski <matt@jankowski.online>
2024-01-13 14:50:46 -0500

    Add coverage for measure of active_users

--- spec/lib/admin/metrics/measure/active_users_measure_spec.rb
+++ spec/lib/admin/metrics/measure/active_users_measure_spec.rb
@@ -13,5 +13,34 @@ describe Admin::Metrics::Measure::ActiveUsersMeasure do
     it 'runs data query without error' do
       expect { measure.data }.to_not raise_error
     end
+
+    context 'with activity tracking records' do
+      before do
+        3.times do
+          travel_to 2.days.ago do
+            ActivityTracker.record('activity:logins', Fabricate(:user).id)
+          end
+        end
+        2.times do
+          travel_to 1.day.ago do
+            ActivityTracker.record('activity:logins', Fabricate(:user).id)
+          end
+        end
+        travel_to 0.days.ago do
+          ActivityTracker.record('activity:logins', Fabricate(:user).id)
+        end
+      end
+
+      it 'returns correct activity tracker counts' do
+        expect(measure.data.size)
+          .to eq(3)
+        expect(measure.data.map(&:symbolize_keys))
+          .to contain_exactly(
+            include(date: 2.days.ago.midnight.to_time, value: '3'),
+            include(date: 1.day.ago.midnight.to_time, value: '2'),
+            include(date: 0.days.ago.midnight.to_time, value: '1')
+          )
+      end
+    end
   end
 end
