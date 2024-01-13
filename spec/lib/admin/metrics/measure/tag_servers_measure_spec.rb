5aa55ed46ba3cf82740bc31a4b76014eb0e1c2bf
Matt Jankowski <matt@jankowski.online>
2024-01-13 16:48:41 -0500

    Add coverage for measure of tag_servers

--- spec/lib/admin/metrics/measure/tag_servers_measure_spec.rb
+++ spec/lib/admin/metrics/measure/tag_servers_measure_spec.rb
@@ -15,5 +15,39 @@ describe Admin::Metrics::Measure::TagServersMeasure do
     it 'runs data query without error' do
       expect { measure.data }.to_not raise_error
     end
+
+    context 'with tagged statuses' do
+      let(:alice) { Fabricate(:account, domain: 'alice.example') }
+      let(:bob) { Fabricate(:account, domain: 'bob.example') }
+
+      before do
+        3.times do
+          status_alice = Fabricate(:status, account: alice, created_at: 2.days.ago)
+          status_alice.tags << tag
+        end
+
+        2.times do
+          status_alice = Fabricate(:status, account: alice, created_at: 1.day.ago)
+          status_alice.tags << tag
+
+          status_bob = Fabricate(:status, account: bob, created_at: 1.day.ago)
+          status_bob.tags << tag
+        end
+
+        status_bob = Fabricate(:status, account: bob, created_at: 0.days.ago)
+        status_bob.tags << tag
+      end
+
+      it 'returns correct tag counts' do
+        expect(measure.data.size)
+          .to eq(3)
+        expect(measure.data.map(&:symbolize_keys))
+          .to contain_exactly(
+            include(date: 2.days.ago.midnight.to_time, value: '1'),
+            include(date: 1.day.ago.midnight.to_time, value: '2'),
+            include(date: 0.days.ago.midnight.to_time, value: '1')
+          )
+      end
+    end
   end
 end
