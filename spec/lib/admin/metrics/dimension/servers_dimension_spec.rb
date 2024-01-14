c62ea0e7ff038c052443bff339e8d57a3b3484fe
Matt Jankowski <matt@jankowski.online>
2024-01-14 14:20:15 -0500

    Use subject directly in dimension specs

--- spec/lib/admin/metrics/dimension/servers_dimension_spec.rb
+++ spec/lib/admin/metrics/dimension/servers_dimension_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe Admin::Metrics::Dimension::ServersDimension do
-  subject(:dimension) { described_class.new(start_at, end_at, limit, params) }
+  subject { described_class.new(start_at, end_at, limit, params) }
 
   let(:start_at) { 2.days.ago }
   let(:end_at) { Time.now.utc }
@@ -12,7 +12,7 @@ describe Admin::Metrics::Dimension::ServersDimension do
 
   describe '#data' do
     it 'runs data query without error' do
-      expect { dimension.data }.to_not raise_error
+      expect { subject.data }.to_not raise_error
     end
   end
 end
