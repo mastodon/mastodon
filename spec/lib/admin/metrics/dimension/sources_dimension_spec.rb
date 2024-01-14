6bd385ae3c9d82b5c9ee8d3093a8555e907adca1
Matt Jankowski <matt@jankowski.online>
2024-01-14 15:59:02 -0500

    Improve coverage for metrics/dimension classes

--- spec/lib/admin/metrics/dimension/sources_dimension_spec.rb
+++ spec/lib/admin/metrics/dimension/sources_dimension_spec.rb
@@ -11,8 +11,21 @@ describe Admin::Metrics::Dimension::SourcesDimension do
   let(:params) { ActionController::Parameters.new }
 
   describe '#data' do
-    it 'runs data query without error' do
-      expect { subject.data }.to_not raise_error
+    let(:app) { Fabricate(:application) }
+    let(:alice) { Fabricate(:user) }
+    let(:bob) { Fabricate(:user) }
+
+    before do
+      alice.update(created_by_application: app)
+    end
+
+    it 'returns OAuth applications with user counts' do
+      expect(subject.data.size)
+        .to eq(1)
+      expect(subject.data.map(&:symbolize_keys))
+        .to contain_exactly(
+          include(key: app.name, value: '1')
+        )
     end
   end
 end
