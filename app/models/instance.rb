ae2603803bc3865985a1f4d39c8fb41b28236595
Matt Jankowski <matt@jankowski.online>
2024-01-17 16:28:03 -0500

    Use `sanitize_sql_like` on `Instance.matches_domain` query

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -22,7 +22,7 @@ class Instance < ApplicationRecord
   end
 
   scope :searchable, -> { where.not(domain: DomainBlock.select(:domain)) }
-  scope :matches_domain, ->(value) { where(arel_table[:domain].matches("%#{value}%")) }
+  scope :matches_domain, ->(value) { where(arel_table[:domain].matches("%#{sanitize_sql_like(value)}%")) }
   scope :by_domain_and_subdomains, ->(domain) { where("reverse('.' || domain) LIKE reverse(?)", "%.#{domain}") }
 
   def self.refresh
