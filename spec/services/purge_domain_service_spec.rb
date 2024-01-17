ef3157e389bc2570063374b11972542cf211e699
Matt Jankowski <matt@jankowski.online>
2024-01-17 11:28:07 -0500

    Remove a bunch of `_old` names

--- spec/services/purge_domain_service_spec.rb
+++ spec/services/purge_domain_service_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe PurgeDomainService, type: :service do
   subject { described_class.new }
 
   let(:domain) { 'obsolete.org' }
-  let!(:old_account) { Fabricate(:account, domain: domain) }
-  let!(:old_status_plain) { Fabricate(:status, account: old_account) }
-  let!(:old_status_with_attachment) { Fabricate(:status, account: old_account) }
-  let!(:old_attachment) { Fabricate(:media_attachment, account: old_account, status: old_status_with_attachment, file: attachment_fixture('attachment.jpg')) }
+  let!(:account) { Fabricate(:account, domain: domain) }
+  let!(:status_plain) { Fabricate(:status, account: account) }
+  let!(:status_with_attachment) { Fabricate(:status, account: account) }
+  let!(:attachment) { Fabricate(:media_attachment, account: account, status: status_with_attachment, file: attachment_fixture('attachment.jpg')) }
 
   describe 'for a suspension' do
     it 'refreshes instance view and removes associated records' do
       expect { subject.call(domain) }
         .to change { domain_instance_exists }.from(true).to(false)
 
-      expect { old_account.reload }.to raise_exception ActiveRecord::RecordNotFound
-      expect { old_status_plain.reload }.to raise_exception ActiveRecord::RecordNotFound
-      expect { old_status_with_attachment.reload }.to raise_exception ActiveRecord::RecordNotFound
-      expect { old_attachment.reload }.to raise_exception ActiveRecord::RecordNotFound
+      expect { account.reload }.to raise_exception ActiveRecord::RecordNotFound
+      expect { status_plain.reload }.to raise_exception ActiveRecord::RecordNotFound
+      expect { status_with_attachment.reload }.to raise_exception ActiveRecord::RecordNotFound
+      expect { attachment.reload }.to raise_exception ActiveRecord::RecordNotFound
     end
 
     def domain_instance_exists
