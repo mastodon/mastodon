5ec724d13233b7398f6c50ac2692025aa0bcd5ca
Renaud Chaput <renchap@gmail.com>
2024-01-18 23:39:00 +0100

    Use the new way of typing Redux hooks

--- app/javascript/mastodon/store/typed_functions.ts
+++ app/javascript/mastodon/store/typed_functions.ts
@@ -1,12 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import type { TypedUseSelectorHook } from 'react-redux';
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { useDispatch, useSelector } from 'react-redux';
 
 import type { AppDispatch, RootState } from './store';
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
 
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
   state: RootState;
