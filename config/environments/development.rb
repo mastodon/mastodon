c8f59d2ca4d693f328d8f68a21749ab4efc8c820
Matt Jankowski <matt@jankowski.online>
2024-01-25 08:28:49 -0500

    Fix `Style/TernaryParentheses` cop (#28387)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   # If using a Heroku, Vagrant or generic remote development environment,
   # use letter_opener_web, accessible at  /letter_opener.
   # Otherwise, use letter_opener, which launches a browser window to view sent mail.
-  config.action_mailer.delivery_method = (ENV['HEROKU'] || ENV['VAGRANT'] || ENV['REMOTE_DEV']) ? :letter_opener_web : :letter_opener
+  config.action_mailer.delivery_method = ENV['HEROKU'] || ENV['VAGRANT'] || ENV['REMOTE_DEV'] ? :letter_opener_web : :letter_opener
 
   # We provide a default secret for the development environment here.
   # This value should not be used in production environments!
