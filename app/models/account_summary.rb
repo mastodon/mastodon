bb76af439aa0065243d123133ec561b6b235adee
Matt Jankowski <matt@jankowski.online>
2024-01-25 11:39:47 -0500

    Rubocop does not see `readonly?` when in concern

--- app/models/account_summary.rb
+++ app/models/account_summary.rb
@@ -15,7 +15,7 @@ class AccountSummary < ApplicationRecord
 
   self.primary_key = :account_id
 
-  has_many :follow_recommendation_suppressions, primary_key: :account_id, foreign_key: :account_id, inverse_of: false
+  has_many :follow_recommendation_suppressions, primary_key: :account_id, foreign_key: :account_id, inverse_of: false, dependent: nil
 
   scope :safe, -> { where(sensitive: false) }
   scope :localized, ->(locale) { order(Arel::Nodes::Case.new.when(arel_table[:language].eq(locale)).then(1).else(0).desc) }
