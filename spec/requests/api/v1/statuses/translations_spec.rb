d988c3bd0c8994bef30e9b10e0b8746a0ab912dc
Matt Jankowski <matt@jankowski.online>
2024-01-26 12:06:50 -0500

    Move `api/v1/statuses/*` to request spec

--- spec/requests/api/v1/statuses/translations_spec.rb
+++ spec/requests/api/v1/statuses/translations_spec.rb
@@ -2,19 +2,14 @@
 
 require 'rails_helper'
 
-describe Api::V1::Statuses::TranslationsController do
-  render_views
-
+describe 'API V1 Statuses Translations' do
   let(:user)  { Fabricate(:user) }
-  let(:app)   { Fabricate(:application, name: 'Test app', website: 'http://testapp.com') }
-  let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: 'read:statuses', application: app) }
+  let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, scopes: scopes) }
+  let(:scopes)  { 'read:statuses' }
+  let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
   context 'with an oauth token' do
-    before do
-      allow(controller).to receive(:doorkeeper_token) { token }
-    end
-
-    describe 'POST #create' do
+    describe 'POST /api/v1/statuses/:status_id/translate' do
       let(:status) { Fabricate(:status, account: user.account, text: 'Hola', language: 'es') }
 
       before do
@@ -22,7 +17,7 @@ describe Api::V1::Statuses::TranslationsController do
         service = instance_double(TranslationService::DeepL, translate: [translation])
         allow(TranslationService).to receive_messages(configured?: true, configured: service)
         Rails.cache.write('translation_service/languages', { 'es' => ['en'] })
-        post :create, params: { status_id: status.id }
+        post "/api/v1/statuses/#{status.id}/translate", headers: headers
       end
 
       it 'returns http success' do
