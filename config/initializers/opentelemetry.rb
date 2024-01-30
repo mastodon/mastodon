d749f14c799e83af8c1e8334a698c15ec08e0269
Juliano Costa <juliano.costa@datadoghq.com>
2024-01-30 14:00:28 +0100

    Add OTel Collector and OTel instrumentation to backend

---
+++ config/initializers/opentelemetry.rb
@@ -0,0 +1,45 @@
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/instrumentation/action_pack'
+require 'opentelemetry/instrumentation/action_view'
+require 'opentelemetry/instrumentation/active_job'
+require 'opentelemetry/instrumentation/active_model_serializers'
+require 'opentelemetry/instrumentation/active_record'
+require 'opentelemetry/instrumentation/active_support'
+require 'opentelemetry/instrumentation/concurrent_ruby'
+require 'opentelemetry/instrumentation/excon'
+require 'opentelemetry/instrumentation/faraday'
+require 'opentelemetry/instrumentation/http'
+require 'opentelemetry/instrumentation/http_client'
+require 'opentelemetry/instrumentation/net/http'
+require 'opentelemetry/instrumentation/pg'
+require 'opentelemetry/instrumentation/rack'
+require 'opentelemetry/instrumentation/rails'
+require 'opentelemetry/instrumentation/redis'
+require 'opentelemetry/instrumentation/sidekiq'
+
+OpenTelemetry::SDK.configure do |c|
+  c.use 'OpenTelemetry::Instrumentation::ActionPack'
+  c.use 'OpenTelemetry::Instrumentation::ActionView'
+  c.use 'OpenTelemetry::Instrumentation::ActiveJob'
+  c.use 'OpenTelemetry::Instrumentation::ActiveModelSerializers'
+  c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
+  c.use 'OpenTelemetry::Instrumentation::ActiveSupport'
+  c.use 'OpenTelemetry::Instrumentation::CuncurrentRuby'
+  c.use 'OpenTelemetry::Instrumentation::Excon'
+  c.use 'OpenTelemetry::Instrumentation::Faraday'
+  c.use 'OpenTelemetry::Instrumentation::HTTP'
+  c.use 'OpenTelemetry::Instrumentation::HttpClient'
+  c.use 'OpenTelemetry::Instrumentation::Net::HTTP'
+  c.use 'OpenTelemetry::Instrumentation::PG'
+  c.use 'OpenTelemetry::Instrumentation::Rack'
+  c.use 'OpenTelemetry::Instrumentation::Rails'
+  c.use 'OpenTelemetry::Instrumentation::Redis'
+  c.use 'OpenTelemetry::Instrumentation::Sidekiq'
+
+  c.service_name =  case $PROGRAM_NAME
+                    when /puma/ then 'mastodon/web'
+                    else
+                      "mastodon/" + $PROGRAM_NAME.split('/').last
+                    end
+end
