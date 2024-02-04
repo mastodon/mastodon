ddc579de6ed5fc06c3d5aeb0407493c0fda0f659
iLeoDo <iLeoDo@Gmail.com>
2024-02-04 17:15:48 +0000

    update frontend check

--- app/javascript/mastodon/features/compose/containers/upload_button_container.js
+++ app/javascript/mastodon/features/compose/containers/upload_button_container.js
@@ -4,7 +4,7 @@ import { uploadCompose } from '../../../actions/compose';
 import UploadButton from '../components/upload_button';
 
 const mapStateToProps = state => ({
-  disabled: state.getIn(['compose', 'poll']) !== null || state.getIn(['compose', 'is_uploading']) || (state.getIn(['compose', 'media_attachments']).size + state.getIn(['compose', 'pending_media_attachments']) > 3 || state.getIn(['compose', 'media_attachments']).some(m => ['video', 'audio'].includes(m.get('type')))),
+  disabled: state.getIn(['compose', 'poll']) !== null || state.getIn(['compose', 'is_uploading']) || (state.getIn(['compose', 'media_attachments']).size + state.getIn(['compose', 'pending_media_attachments']) > 3 || (!state.getIn(['server', 'server', 'configuration', 'media_attachments', 'allow_mix_media'], false) && state.getIn(['compose', 'media_attachments']).some(m => ['video', 'audio'].includes(m.get('type'))))),
   resetFileKey: state.getIn(['compose', 'resetFileKey']),
 });
 
