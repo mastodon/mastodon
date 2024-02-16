8afd74b026bb09a545a10ee62f93b05f9d43bc3e
Matt Jankowski <matt@jankowski.online>
2024-02-16 12:24:39 -0500

    Replace redis namespace stub with pipelined connection

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -469,7 +469,7 @@ RSpec.describe User do
     let!(:access_token) { Fabricate(:access_token, resource_owner_id: user.id) }
     let!(:web_push_subscription) { Fabricate(:web_push_subscription, access_token: access_token) }
 
-    let(:redis_pipeline_stub) { instance_double(Redis::Namespace, publish: nil) }
+    let(:redis_pipeline_stub) { instance_double(Redis::PipelinedConnection, publish: nil) }
 
     before do
       allow(redis).to receive(:pipelined).and_yield(redis_pipeline_stub)
