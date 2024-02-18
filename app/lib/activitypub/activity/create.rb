0b52fa3e6f4ad5abc5d6b9b22eabd570c95ff3e9
Sho Kusano <rosylilly@aduca.org>
2024-02-18 20:49:29 +0900

    Reject spammer

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -85,7 +85,15 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     ApplicationRecord.transaction do
       @status = Status.create!(@params)
       attach_tags(@status)
+
+      # Delete status on zero follower user and nearly created account with include some replies
+      if like_a_spam?
+        @status = nil
+        raise ActiveRecord::Rollback
+      end
     end
+
+    return if @status.nil?
 
     resolve_thread(@status)
     fetch_replies(@status)
@@ -429,5 +437,14 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
   rescue ActiveRecord::StaleObjectError
     poll.reload
     retry
+  end
+
+  def like_a_spam?
+    (
+      !@status.account.local? &&
+      @status.account.followers_count.zero? &&
+      @status.account.created_at > 1.day.ago &&
+      @mentions.count >= 2
+    )
   end
 end
