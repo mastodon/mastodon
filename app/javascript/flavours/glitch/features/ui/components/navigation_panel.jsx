f6903d9ab03f43468125e1998a8afd332c48d56b
Eugen Rochko <eugen@zeonfederated.com>
2024-02-20 10:10:44 +0100

    [Glitch] Change explore icon from hashtag to compass in web UI
    Port b8b2f20b16d92274fc3e576f493d19347f9421da to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/ui/components/navigation_panel.jsx
+++ app/javascript/flavours/glitch/features/ui/components/navigation_panel.jsx
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import { defineMessages, injectIntl } from 'react-intl';
 
 import BookmarksIcon from '@/material-icons/400-24px/bookmarks-fill.svg?react';
+import ExploreIcon from '@/material-icons/400-24px/explore.svg?react';
 import HomeIcon from '@/material-icons/400-24px/home-fill.svg?react';
 import ListAltIcon from '@/material-icons/400-24px/list_alt.svg?react';
 import MailIcon from '@/material-icons/400-24px/mail.svg?react';
@@ -13,7 +14,6 @@ import PublicIcon from '@/material-icons/400-24px/public.svg?react';
 import SearchIcon from '@/material-icons/400-24px/search.svg?react';
 import SettingsIcon from '@/material-icons/400-24px/settings-fill.svg?react';
 import StarIcon from '@/material-icons/400-24px/star-fill.svg?react';
-import TagIcon from '@/material-icons/400-24px/tag.svg?react';
 import { NavigationPortal } from 'flavours/glitch/components/navigation_portal';
 import { timelinePreview, trendsEnabled } from 'flavours/glitch/initial_state';
 import { transientSingleColumn } from 'flavours/glitch/is_mobile';
@@ -91,7 +91,7 @@ class NavigationPanel extends Component {
         )}
 
         {trendsEnabled ? (
-          <ColumnLink transparent to='/explore' icon='hashtag' iconComponent={TagIcon} text={intl.formatMessage(messages.explore)} />
+          <ColumnLink transparent to='/explore' icon='hashtag' iconComponent={ExploreIcon} text={intl.formatMessage(messages.explore)} />
         ) : (
           <ColumnLink transparent to='/search' icon='search' iconComponent={SearchIcon} text={intl.formatMessage(messages.search)} />
         )}
