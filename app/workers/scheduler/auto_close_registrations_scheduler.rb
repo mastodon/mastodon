9d04eee1699e1f5a727aff4b923e0db3b1da957f
Claire <claire.github-309c@sitedethib.com>
2024-02-21 15:05:17 +0100

    Add email notification to server admins when registrations get automatically switched to approval

--- app/workers/scheduler/auto_close_registrations_scheduler.rb
+++ app/workers/scheduler/auto_close_registrations_scheduler.rb
@@ -14,12 +14,20 @@ class Scheduler::AutoCloseRegistrationsScheduler
     return if Rails.configuration.x.email_domains_whitelist.present? || ENV['DISABLE_AUTOMATIC_SWITCHING_TO_APPROVED_REGISTRATIONS'] == 'true'
     return unless Setting.registrations_mode == 'open'
 
-    Setting.registrations_mode = 'approved' unless active_moderators?
+    switch_to_approval_mode! unless active_moderators?
   end
 
   private
 
   def active_moderators?
     User.those_who_can(:manage_reports).exists?(current_sign_in_at: OPEN_REGISTRATIONS_MODERATOR_THRESHOLD.ago...)
+  end
+
+  def switch_to_approval_mode!
+    Setting.registrations_mode = 'approved'
+
+    User.those_who_can(:view_devops).includes(:account).find_each do |user|
+      AdminMailer.with(recipient: user.account).auto_close_registrations.deliver_later
+    end
   end
 end
