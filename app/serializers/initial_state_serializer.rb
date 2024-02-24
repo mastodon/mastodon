47b5105e5dc95ba58f0b8964533acb85e7f54b4f
Claire <claire.github-309c@sitedethib.com>
2024-02-24 13:58:00 +0100

    Remove `max_toot_chars` from initial-state
    This is still in `/api/v1/instance` for compatibility with other clients

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -5,17 +5,13 @@ class InitialStateSerializer < ActiveModel::Serializer
 
   attributes :meta, :compose, :accounts,
              :media_attachments, :settings,
-             :max_toot_chars, :max_feed_hashtags, :poll_limits,
+             :max_feed_hashtags, :poll_limits,
              :languages
 
   attribute :critical_updates_pending, if: -> { object&.role&.can?(:view_devops) && SoftwareUpdate.check_enabled? }
 
   has_one :push_subscription, serializer: REST::WebPushSubscriptionSerializer
   has_one :role, serializer: REST::RoleSerializer
-
-  def max_toot_chars
-    StatusLengthValidator::MAX_CHARS
-  end
 
   def max_feed_hashtags
     TagFeed::LIMIT_PER_MODE
