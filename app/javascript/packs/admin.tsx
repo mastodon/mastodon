d4b7fb347d20b1234b0167f5afcb1ebd8643a592
Renaud Chaput <renchap@gmail.com>
2024-02-25 22:54:05 +0100

    Use an async function to mount React components

--- app/javascript/packs/admin.tsx
+++ app/javascript/packs/admin.tsx
@@ -260,6 +260,31 @@ Rails.delegate(
   },
 );
 
+async function mountReactComponent(element: Element) {
+  const componentName = element.getAttribute('data-admin-component');
+  const stringProps = element.getAttribute('data-props');
+
+  if (!stringProps) return;
+
+  const componentProps = JSON.parse(stringProps) as object;
+
+  const { default: AdminComponent } = await import(
+    '@/mastodon/containers/admin_component'
+  );
+
+  const { default: Component } = (await import(
+    `@/mastodon/components/admin/${componentName}`
+  )) as { default: React.ComponentType };
+
+  const root = createRoot(element);
+
+  root.render(
+    <AdminComponent>
+      <Component {...componentProps} />
+    </AdminComponent>,
+  );
+}
+
 ready(() => {
   const domainBlockSeverityInput = document.querySelector<HTMLInputElement>(
     'input#domain_block_severity',
@@ -336,31 +361,7 @@ ready(() => {
   }
 
   document.querySelectorAll('[data-admin-component]').forEach((element) => {
-    const componentName = element.getAttribute('data-admin-component');
-    const stringProps = element.getAttribute('data-props');
-    if (!stringProps) return;
-
-    const componentProps = JSON.parse(stringProps) as object;
-
-    import('../mastodon/containers/admin_component')
-      .then(({ default: AdminComponent }) => {
-        return import('../mastodon/components/admin/' + componentName).then(
-          ({ default: Component }) => {
-            const root = createRoot(element);
-
-            root.render(
-              <AdminComponent>
-                <Component {...componentProps} />
-              </AdminComponent>,
-            );
-
-            return undefined;
-          },
-        );
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    void mountReactComponent(element);
   });
 }).catch((reason) => {
   throw reason;
