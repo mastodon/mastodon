07e36555ad6fb6fd980a617d2c26bd45e6ef7b42
Renaud Chaput <renchap@gmail.com>
2024-03-03 22:49:44 +0100

    Convert small pack files to Typescript

--- app/javascript/packs/mailer.js
+++
@@ -1,3 +0,0 @@
-import '../styles/mailer.scss';
-
-require.context('../icons');
---
+++ app/javascript/packs/mailer.ts
@@ -0,0 +1,5 @@
+/* Placeholder to make the mailer assets compiled by Webpack */
+
+import '../styles/mailer.scss';
+
+require.context('../icons');
