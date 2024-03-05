1994667fbbc1f31ddfc19b1578998bbe975d10bc
Daniel M Brasil <danielmbrasil@protonmail.com>
2024-03-05 10:59:39 -0300

    add model specs

--- spec/models/ip_block_spec.rb
+++ spec/models/ip_block_spec.rb
@@ -18,6 +18,13 @@ describe IpBlock do
       expect(ip_block).to model_have_error_on_field(:severity)
     end
 
+    it 'validates severity inclusion', :aggregate_failures do
+      ip_block = described_class.new(ip: '127.0.0.1', severity: :invalid)
+
+      expect(ip_block).to_not be_valid
+      expect(ip_block).to model_have_error_on_field(:severity)
+    end
+
     it 'validates ip uniqueness', :aggregate_failures do
       described_class.create!(ip: '127.0.0.1', severity: :no_access)
 
