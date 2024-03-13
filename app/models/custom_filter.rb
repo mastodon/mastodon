01b624c4a0d17ec24f6acf56298fe9e5e8a6dd49
Matt Jankowski <matt@jankowski.online>
2024-03-13 04:50:21 -0400

    Use `normalizes` on `CustomFilter#context` value (#27602)

--- app/models/custom_filter.rb
+++ app/models/custom_filter.rb
@@ -41,7 +41,7 @@ class CustomFilter < ApplicationRecord
   validates :title, :context, presence: true
   validate :context_must_be_valid
 
-  before_validation :clean_up_contexts
+  normalizes :context, with: ->(context) { context.map(&:strip).filter_map(&:presence) }
 
   before_save :prepare_cache_invalidation!
   before_destroy :prepare_cache_invalidation!
@@ -113,10 +113,6 @@ class CustomFilter < ApplicationRecord
   end
 
   private
-
-  def clean_up_contexts
-    self.context = Array(context).map(&:strip).filter_map(&:presence)
-  end
 
   def context_must_be_valid
     errors.add(:context, I18n.t('filters.errors.invalid_context')) if invalid_context_value?
