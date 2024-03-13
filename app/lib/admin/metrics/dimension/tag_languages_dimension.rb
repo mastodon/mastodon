7e6eb64f1e89179f49ebdaddab7f96ac3f53ad05
Matt Jankowski <matt@jankowski.online>
2024-03-13 04:56:37 -0400

    Use full snowflake range in `admin/metrics` classes (#29416)

--- app/lib/admin/metrics/dimension/tag_languages_dimension.rb
+++ app/lib/admin/metrics/dimension/tag_languages_dimension.rb
@@ -40,11 +40,11 @@ class Admin::Metrics::Dimension::TagLanguagesDimension < Admin::Metrics::Dimensi
   end
 
   def earliest_status_id
-    Mastodon::Snowflake.id_at(@start_at, with_random: false)
+    Mastodon::Snowflake.id_at(@start_at.beginning_of_day, with_random: false)
   end
 
   def latest_status_id
-    Mastodon::Snowflake.id_at(@end_at, with_random: false)
+    Mastodon::Snowflake.id_at(@end_at.end_of_day, with_random: false)
   end
 
   def params
