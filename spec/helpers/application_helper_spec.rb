979b20df2f2209579148ca3993272d2cd2aa875e
Matt Jankowski <matt@jankowski.online>
2024-03-15 15:12:25 -0400

    Remove unused `active_nav_class` helper method

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,30 +3,6 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  describe 'active_nav_class' do
-    it 'returns active when on the current page' do
-      allow(helper).to receive(:current_page?).and_return(true)
-
-      result = helper.active_nav_class('/test')
-      expect(result).to eq 'active'
-    end
-
-    it 'returns active when on a current page' do
-      allow(helper).to receive(:current_page?).with('/foo').and_return(false)
-      allow(helper).to receive(:current_page?).with('/test').and_return(true)
-
-      result = helper.active_nav_class('/foo', '/test')
-      expect(result).to eq 'active'
-    end
-
-    it 'returns empty string when not on current page' do
-      allow(helper).to receive(:current_page?).and_return(false)
-
-      result = helper.active_nav_class('/test')
-      expect(result).to eq ''
-    end
-  end
-
   describe 'body_classes' do
     context 'with a body class string from a controller' do
       before { helper.extend controller_helpers }
