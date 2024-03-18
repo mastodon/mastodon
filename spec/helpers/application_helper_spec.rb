92855948a000563f1a6d715721892dfc18606baf
Matt Jankowski <matt@jankowski.online>
2024-03-18 04:55:54 -0400

    Remove unused `show_landing_strip?` helper method (#29618)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -99,36 +99,6 @@ describe ApplicationHelper do
     end
   end
 
-  describe 'show_landing_strip?', :without_verify_partial_doubles do
-    describe 'when signed in' do
-      before do
-        allow(helper).to receive(:user_signed_in?).and_return(true)
-      end
-
-      it 'does not show landing strip' do
-        expect(helper.show_landing_strip?).to be false
-      end
-    end
-
-    describe 'when signed out' do
-      before do
-        allow(helper).to receive(:user_signed_in?).and_return(false)
-      end
-
-      it 'does not show landing strip on single user instance' do
-        allow(helper).to receive(:single_user_mode?).and_return(true)
-
-        expect(helper.show_landing_strip?).to be false
-      end
-
-      it 'shows landing strip on multi user instance' do
-        allow(helper).to receive(:single_user_mode?).and_return(false)
-
-        expect(helper.show_landing_strip?).to be true
-      end
-    end
-  end
-
   describe 'available_sign_up_path' do
     context 'when registrations are closed' do
       before do
