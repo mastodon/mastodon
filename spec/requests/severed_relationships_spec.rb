10a5ae9fd13c4a140e2b5535c8295e4b4de9d5e8
Claire <claire.github-309c@sitedethib.com>
2024-03-20 18:26:31 +0100

    Fix the relationships controller spec, since it requires an extra model now

--- spec/requests/severed_relationships_spec.rb
+++ spec/requests/severed_relationships_spec.rb
@@ -7,11 +7,12 @@ describe 'Severed relationships page' do
 
   describe 'GET severed_relationships#index' do
     let(:user) { Fabricate(:user) }
+    let(:event) { Fabricate(:account_relationship_severance_event, account: user.account) }
 
     before do
       sign_in user
 
-      Fabricate(:severed_relationship, local_account: user.account)
+      Fabricate.times(3, :severed_relationship, local_account: user.account, relationship_severance_event: event.relationship_severance_event)
     end
 
     it 'returns http success' do
