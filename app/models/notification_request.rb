c435880be4d31a067c1cc43f9e873304bd64a385
Claire <claire.github-309c@sitedethib.com>
2024-03-20 17:36:42 +0100

    Fix error when attempting to delete posts that triggered a notification request

--- app/models/notification_request.rb
+++ app/models/notification_request.rb
@@ -7,7 +7,7 @@
 #  id                  :bigint(8)        not null, primary key
 #  account_id          :bigint(8)        not null
 #  from_account_id     :bigint(8)        not null
-#  last_status_id      :bigint(8)        not null
+#  last_status_id      :bigint(8)
 #  notifications_count :bigint(8)        default(0), not null
 #  dismissed           :boolean          default(FALSE), not null
 #  created_at          :datetime         not null
