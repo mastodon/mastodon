af1de2f791b40dad159f6f96a675709118efdf7a
Claire <claire.github-309c@sitedethib.com>
2024-03-20 17:44:13 +0100

    Fix error in severed relationships controller

--- app/models/account_relationship_severance_event.rb
+++ app/models/account_relationship_severance_event.rb
@@ -16,7 +16,7 @@ class AccountRelationshipSeveranceEvent < ApplicationRecord
   belongs_to :account
   belongs_to :relationship_severance_event
 
-  delegate :severed_relationships, :type, :target_name, :purged, to: :relationship_severance_event, prefix: false
+  delegate :severed_relationships, :type, :target_name, :purged?, to: :relationship_severance_event, prefix: false
 
   before_create :set_relationships_count!
 
