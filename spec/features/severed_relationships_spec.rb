296fa22d7fe2da6630d61f3578a927c261278cc3
Claire <claire.github-309c@sitedethib.com>
2024-03-22 17:12:18 +0100

    Fix severed relationships controller test

--- spec/features/severed_relationships_spec.rb
+++ spec/features/severed_relationships_spec.rb
@@ -6,19 +6,19 @@ describe 'Severed relationships page' do
   include ProfileStories
 
   describe 'GET severed_relationships#index' do
-    let(:event) { Fabricate(:account_relationship_severance_event, account: bob.account) }
-
     before do
       as_a_logged_in_user
 
-      Fabricate.times(3, :severed_relationship, local_account: event.account, relationship_severance_event: event.relationship_severance_event)
+      event = Fabricate(:relationship_severance_event)
+      Fabricate.times(3, :severed_relationship, local_account: bob.account, relationship_severance_event: event)
+      Fabricate(:account_relationship_severance_event, account: bob.account, relationship_severance_event: event)
     end
 
     it 'returns http success' do
       visit severed_relationships_path
 
       expect(page).to have_title(I18n.t('settings.severed_relationships'))
-      expect(page).to have_link(href: following_severed_relationship_path(event, format: :csv))
+      expect(page).to have_link(href: following_severed_relationship_path(AccountRelationshipSeveranceEvent.first, format: :csv))
     end
   end
 end
