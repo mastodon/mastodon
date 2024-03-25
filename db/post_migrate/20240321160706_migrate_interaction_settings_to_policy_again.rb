d6fd26c309692d011996968b6b4d668cdbcb3e94
Claire <claire.github-309c@sitedethib.com>
2024-03-25 19:09:54 +0100

    Use `upsert_all` and `insert_all` to reduce back-and-forth in costly migrations

--- db/post_migrate/20240321160706_migrate_interaction_settings_to_policy_again.rb
+++ db/post_migrate/20240321160706_migrate_interaction_settings_to_policy_again.rb
@@ -18,37 +18,44 @@ class MigrateInteractionSettingsToPolicyAgain < ActiveRecord::Migration[7.1]
   end
 
   def up
-    User.includes(account: :notification_policy).find_each do |user|
-      deserialized_settings = Oj.load(user.attributes_before_type_cast['settings'])
-
-      next if deserialized_settings.nil?
-
-      # If the user has configured a notification policy, don't override it
-      next if user.account.notification_policy.present?
-
-      policy = user.account.build_notification_policy
-      requires_new_policy = false
-
-      if deserialized_settings['interactions.must_be_follower']
-        policy.filter_not_followers = true
-        requires_new_policy = true
-      end
-
-      if deserialized_settings['interactions.must_be_following']
-        policy.filter_not_following = true
-        requires_new_policy = true
-      end
-
-      unless deserialized_settings['interactions.must_be_following_dm']
-        policy.filter_private_mentions = false
-        requires_new_policy = true
-      end
-
-      policy.save if requires_new_policy && policy.changed?
-    rescue ActiveRecord::RecordNotUnique
-      next
+    User.includes(account: :notification_policy).in_batches do |users|
+      NotificationPolicy.insert_all(users.filter_map { |user| policy_for_user(user) })
     end
   end
 
   def down; end
+
+  private
+
+  def policy_for_user(user)
+    deserialized_settings = Oj.load(user.attributes_before_type_cast['settings'])
+    return if deserialized_settings.nil?
+    return if user.account.notification_policy.present?
+
+    requires_new_policy = false
+
+    policy = {
+      account_id: user.account.id,
+      filter_not_followers: false,
+      filter_not_following: false,
+      filter_private_mentions: true,
+    }
+
+    if deserialized_settings['interactions.must_be_follower']
+      policy[:filter_not_followers] = true
+      requires_new_policy = true
+    end
+
+    if deserialized_settings['interactions.must_be_following']
+      policy[:filter_not_following] = true
+      requires_new_policy = true
+    end
+
+    unless deserialized_settings['interactions.must_be_following_dm']
+      policy[:filter_private_mentions] = false
+      requires_new_policy = true
+    end
+
+    policy if requires_new_policy
+  end
 end
