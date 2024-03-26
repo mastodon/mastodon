35088bdb158ebae9e8e66aa42ace495e4d50e58c
Matt Jankowski <matt@jankowski.online>
2024-03-26 10:31:49 -0400

    Rename `paperclip_processing` option to `attachment_processing`

--- spec/system/report_interface_spec.rb
+++ spec/system/report_interface_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe 'report interface', :paperclip_processing do
+describe 'report interface', :attachment_processing do
   include ProfileStories
 
   let(:email)               { 'admin@example.com' }
