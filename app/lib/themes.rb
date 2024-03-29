672c9f5f05bde788877ae0dcdb6668366c4c8941
Renaud Chaput <renchap@gmail.com>
2024-03-29 14:32:07 +0100

    Change the `theme-color` value automatically when using a built-in theme (#29795)

--- app/lib/themes.rb
+++ app/lib/themes.rb
@@ -6,6 +6,9 @@ require 'yaml'
 class Themes
   include Singleton
 
+  MASTODON_DARK_THEME_COLOR = '#191b22'
+  MASTODON_LIGHT_THEME_COLOR = '#f3f5f7'
+
   def initialize
     @conf = YAML.load_file(Rails.root.join('config', 'themes.yml'))
   end
