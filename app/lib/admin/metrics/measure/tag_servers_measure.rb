966d7f5bf9b6b7088f63cc7f8dca90496700ac05
Matt Jankowski <matt@jankowski.online>
2024-04-04 07:33:17 -0400

    Add missing snowflake range correction (#29841)

--- app/lib/admin/metrics/measure/tag_servers_measure.rb
+++ app/lib/admin/metrics/measure/tag_servers_measure.rb
@@ -46,11 +46,11 @@ class Admin::Metrics::Measure::TagServersMeasure < Admin::Metrics::Measure::Base
   end
 
   def earliest_status_id
-    Mastodon::Snowflake.id_at(@start_at, with_random: false)
+    Mastodon::Snowflake.id_at(@start_at.beginning_of_day, with_random: false)
   end
 
   def latest_status_id
-    Mastodon::Snowflake.id_at(@end_at, with_random: false)
+    Mastodon::Snowflake.id_at(@end_at.end_of_day, with_random: false)
   end
 
   def tag
