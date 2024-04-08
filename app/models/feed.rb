babbf6017d3f00e730f2c1e28542b3af3967acde
Claire <claire.github-309c@sitedethib.com>
2024-04-08 15:46:13 +0200

    Remove caching in `cache_collection` (#29862)

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -28,7 +28,7 @@ class Feed
       unhydrated = redis.zrangebyscore(key, "(#{min_id}", "(#{max_id}", limit: [0, limit], with_scores: true).map { |id| id.first.to_i }
     end
 
-    Status.where(id: unhydrated).cache_ids
+    Status.where(id: unhydrated)
   end
 
   def key
