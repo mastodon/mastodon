6df3006831a388a6517e6058efa9d90ec096fd78
MszPro <code_sign@mszpro.com>
2024-04-09 10:36:06 +0900

    Also removing webauthn credentials on 2fa reset with admin cli

--- lib/mastodon/cli/accounts.rb
+++ lib/mastodon/cli/accounts.rb
@@ -166,6 +166,7 @@ module Mastodon::CLI
       user.disabled = true if options[:disable]
       user.approved = true if options[:approve]
       user.otp_required_for_login = false if options[:disable_2fa]
+      user.webauthn_credentials = [] if options[:disable_2fa]
 
       if user.save
         user.confirm if options[:confirm]
