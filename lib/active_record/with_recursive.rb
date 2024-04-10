6fbacf9f16a44de708e844c6e59c6a0d27097ffe
Claire <claire.github-309c@sitedethib.com>
2024-04-10 11:52:18 +0200

    Change `QueryMethods#with_recursive` to match upcoming Rails' implementation

--- lib/active_record/with_recursive.rb
+++ lib/active_record/with_recursive.rb
@@ -2,15 +2,28 @@
 
 # Add support for writing recursive CTEs in ActiveRecord
 
-# From Lorin Thwaits (https://github.com/lorint) as per comment:
+# Initially from Lorin Thwaits (https://github.com/lorint) as per comment:
 # https://github.com/vlado/activerecord-cte/issues/16#issuecomment-1433043310
+
+# Modified from the above code to change the signature to
+# `with_recursive(hash)` and extending CTE hash values to also includes arrays
+# of values that get turned into UNION ALL expressions.
+
+# This implementation has been merged in Rails: https://github.com/rails/rails/pull/51601
 
 module ActiveRecord
   module QueryMethodsExtensions
-    def with_recursive(as, anchor, recursive)
-      @is_recursive = true
-      common_table = Arel.sql(anchor.arel.union(:all, recursive.arel).to_sql)
-      from(as).with(as => common_table)
+    def with_recursive(*args)
+      @with_is_recursive = true
+      check_if_method_has_arguments!(__callee__, args)
+      spawn.with_recursive!(*args)
+    end
+
+    # Like #with_recursive but modifies the relation in place.
+    def with_recursive!(*args) # :nodoc:
+      self.with_values += args
+      @with_is_recursive = true
+      self
     end
 
     private
@@ -25,7 +38,24 @@ module ActiveRecord
       end
 
       # Was:  arel.with(with_statements)
-      @is_recursive ? arel.with(:recursive, with_statements) : arel.with(with_statements)
+      @with_is_recursive ? arel.with(:recursive, with_statements) : arel.with(with_statements)
+    end
+
+    def build_with_value_from_hash(hash)
+      hash.map do |name, value|
+        Arel::Nodes::TableAlias.new(build_with_expression_from_value(value), name)
+      end
+    end
+
+    def build_with_expression_from_value(value)
+      case value
+      when Arel::Nodes::SqlLiteral then Arel::Nodes::Grouping.new(value)
+      when ActiveRecord::Relation then value.arel
+      when Arel::SelectManager then value
+      when Array then value.map { |e| build_with_expression_from_value(e) }.reduce { |result, value| Arel::Nodes::UnionAll.new(result, value) }
+      else
+        raise ArgumentError, "Unsupported argument type: `#{value}` #{value.class}"
+      end
     end
   end
 end
