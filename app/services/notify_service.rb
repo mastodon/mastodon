3339e2ff1d8991c8f9072d09780d2ea59f9c3b2f
Claire <claire.github-309c@sitedethib.com>
2024-04-10 12:12:20 +0200

    [WiP] Set notification group key when issuing notifications

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -179,6 +179,7 @@ class NotifyService < BaseService
     return if dismiss?
 
     @notification.filtered = filter?
+    @notification.group_key = notification_group_key
     @notification.save!
 
     # It's possible the underlying activity has been deleted
@@ -197,6 +198,12 @@ class NotifyService < BaseService
   end
 
   private
+
+  def notification_group_key
+    return nil if @notification.filtered || %i(favourite reblog).exclude?(@notification.type)
+
+    "#{@notification.type}-#{@notification.target_status.id}-#{@notification.activity.created_at.to_date}"
+  end
 
   def dismiss?
     DismissCondition.new(@notification).dismiss?
