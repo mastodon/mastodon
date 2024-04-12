ec5a0e0f5e14a512f8e247a22742ec0f85e14f5c
Matt Jankowski <matt@jankowski.online>
2024-04-12 05:18:35 -0400

    Expand coverage for admin/metrics/dimension/* classes (#29913)

--- spec/lib/admin/metrics/dimension/instance_languages_dimension_spec.rb
+++ spec/lib/admin/metrics/dimension/instance_languages_dimension_spec.rb
@@ -3,16 +3,30 @@
 require 'rails_helper'
 
 describe Admin::Metrics::Dimension::InstanceLanguagesDimension do
-  subject(:dimension) { described_class.new(start_at, end_at, limit, params) }
+  subject { described_class.new(start_at, end_at, limit, params) }
 
   let(:start_at) { 2.days.ago }
   let(:end_at) { Time.now.utc }
   let(:limit) { 10 }
-  let(:params) { ActionController::Parameters.new }
+  let(:params) { ActionController::Parameters.new(domain: domain) }
 
   describe '#data' do
-    it 'runs data query without error' do
-      expect { dimension.data }.to_not raise_error
+    let(:domain) { 'host.example' }
+    let(:alice) { Fabricate(:account, domain: domain) }
+    let(:bob) { Fabricate(:account) }
+
+    before do
+      Fabricate :status, account: alice, language: 'en'
+      Fabricate :status, account: bob, language: 'es'
+    end
+
+    it 'returns locales with status counts' do
+      expect(subject.data.size)
+        .to eq(1)
+      expect(subject.data.map(&:symbolize_keys))
+        .to contain_exactly(
+          include(key: 'en', value: '1')
+        )
     end
   end
 end
