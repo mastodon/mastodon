39d9265a823829359306b924943d9375d546d6be
Matt Jankowski <matt@jankowski.online>
2024-04-12 09:02:54 -0400

    Move alice and status to let

--- spec/views/statuses/show.html.haml_spec.rb
+++ spec/views/statuses/show.html.haml_spec.rb
@@ -10,9 +10,10 @@ describe 'statuses/show.html.haml', :without_verify_partial_doubles do
     assign(:instance_presenter, InstancePresenter.new)
   end
 
+  let(:alice) { Fabricate(:account, username: 'alice', display_name: 'Alice') }
+  let(:status) { Fabricate(:status, account: alice, text: 'Hello World') }
+
   it 'has valid opengraph tags' do
-    alice  = Fabricate(:account, username: 'alice', display_name: 'Alice')
-    status = Fabricate(:status, account: alice, text: 'Hello World')
     Fabricate(:media_attachment, account: alice, status: status, type: :video)
 
     assign(:status, status)
@@ -29,8 +30,6 @@ describe 'statuses/show.html.haml', :without_verify_partial_doubles do
   end
 
   it 'has twitter player tag' do
-    alice  = Fabricate(:account, username: 'alice', display_name: 'Alice')
-    status = Fabricate(:status, account: alice, text: 'Hello World')
     Fabricate(:media_attachment, account: alice, status: status, type: :video)
 
     assign(:status, status)
