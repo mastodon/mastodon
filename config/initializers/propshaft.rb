9e246d3d72c62a3cda715841aa4ea3e49707371d
Matt Jankowski <matt@jankowski.online>
2024-04-12 09:33:32 -0400

    Same comment style

--- config/initializers/propshaft.rb
+++ config/initializers/propshaft.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# SVG Icons
+# SVG icons
 Rails.application.config.assets.paths << Rails.root.join('app', 'javascript', 'images')
 
-# Tell propshaft where the material design icons are
+# Material Design icons
 Rails.application.config.assets.paths << Rails.root.join('app', 'javascript', 'material-icons')
