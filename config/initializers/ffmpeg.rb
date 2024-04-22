a6089cdfca1d8089318dd694edf44db8ce9bec59
Tim Rogers <rogers.timothy.john@gmail.com>
2024-04-22 04:00:24 -0500

    Fixed crash when supplying FFMPEG_BINARY environment variable (#30022)

--- config/initializers/ffmpeg.rb
+++ config/initializers/ffmpeg.rb
@@ -1,3 +1,6 @@
-if ENV['FFMPEG_BINARY'].present?
-    FFMPEG.ffmpeg_binary = ENV['FFMPEG_BINARY']
+# frozen_string_literal: true
+
+Rails.application.configure do
+  config.x.ffmpeg_binary = ENV['FFMPEG_BINARY'] || 'ffmpeg'
+  config.x.ffprobe_binary = ENV['FFPROBE_BINARY'] || 'ffprobe'
 end
