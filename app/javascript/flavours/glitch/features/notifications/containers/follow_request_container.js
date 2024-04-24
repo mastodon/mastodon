113c931cdafd06147fe443b5b94c802710e519f0
Claire <claire.github-309c@sitedethib.com>
2024-04-24 17:00:48 +0200

    Fix follow request notifications not being displayed (#2695)

--- app/javascript/flavours/glitch/features/notifications/containers/follow_request_container.js
+++ app/javascript/flavours/glitch/features/notifications/containers/follow_request_container.js
@@ -1,17 +1,28 @@
 import { connect } from 'react-redux';
 
 import { authorizeFollowRequest, rejectFollowRequest } from 'flavours/glitch/actions/accounts';
+import { makeGetAccount } from 'flavours/glitch/selectors';
 
 import FollowRequest from '../components/follow_request';
 
-const mapDispatchToProps = (dispatch, { account }) => ({
+const makeMapStateToProps = () => {
+  const getAccount = makeGetAccount();
+
+  const mapStateToProps = (state, props) => ({
+    account: getAccount(state, props.id),
+  });
+
+  return mapStateToProps;
+};
+
+const mapDispatchToProps = (dispatch, { id }) => ({
   onAuthorize () {
-    dispatch(authorizeFollowRequest(account.get('id')));
+    dispatch(authorizeFollowRequest(id));
   },
 
   onReject () {
-    dispatch(rejectFollowRequest(account.get('id')));
+    dispatch(rejectFollowRequest(id));
   },
 });
 
-export default connect(null, mapDispatchToProps)(FollowRequest);
+export default connect(makeMapStateToProps, mapDispatchToProps)(FollowRequest);
