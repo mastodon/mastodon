33830ae759add9bcbbe57f5ef957ec2028e66b7d
Matt Jankowski <matt@jankowski.online>
2024-04-25 12:22:00 -0400

    Extract `ActivityPubPayloadGeneration` model concern

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -16,6 +16,7 @@
 #
 
 class Follow < ApplicationRecord
+  include ActivityPubPayloadGeneration
   include Paginable
   include RelationshipCacheable
   include RateLimitable
@@ -42,7 +43,6 @@ class Follow < ApplicationRecord
     destroy!
   end
 
-  before_validation :set_uri, only: :create
   after_create :increment_cache_counters
   after_destroy :remove_endorsements
   after_destroy :decrement_cache_counters
@@ -50,10 +50,6 @@ class Follow < ApplicationRecord
   after_commit :invalidate_hash_cache
 
   private
-
-  def set_uri
-    self.uri = ActivityPub::TagManager.instance.generate_uri_for(self) if uri.nil?
-  end
 
   def remove_endorsements
     AccountPin.where(target_account_id: target_account_id, account_id: account_id).delete_all
