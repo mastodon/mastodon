f8d0d0e08ea08612059da26f3267d66a75f1412d
Claire <claire.github-309c@sitedethib.com>
2024-04-29 11:18:13 +0200

    Move `as_json` override to `ApplicationRecord`

--- app/models/concerns/no_implicit_serialization.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module NoImplicitSerialization
-  # Prevent implicit serialization in ActiveModel::Serializer or other code paths.
-  # This is a hardening step to avoid accidental leaking of attributes.
-  def as_json
-    raise NotImplementedError
-  end
-end
