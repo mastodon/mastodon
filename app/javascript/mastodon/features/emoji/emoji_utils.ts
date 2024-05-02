70e35f837c57c8becf89857b3c59baf1cb667f81
taichi221228 <taichi221228@icloud.com>
2024-05-02 15:40:06 +0900

    Remove measureScrollbar function from emoji_utils
    The measureScrollbar function has been deleted from the emoji_utils.ts file. This simplifies the module export list by removing a function that was unnecessary or out of context.

--- app/javascript/mastodon/features/emoji/emoji_utils.ts
+++ app/javascript/mastodon/features/emoji/emoji_utils.ts
@@ -218,28 +218,4 @@ function intersect(a: [], b: []) {
   return uniqA.filter((item) => uniqB.includes(item));
 }
 
-// https://github.com/sonicdoe/measure-scrollbar
-function measureScrollbar() {
-  const div = document.createElement('div');
-
-  div.style.width = '100px';
-  div.style.height = '100px';
-  div.style.overflow = 'scroll';
-  div.style.position = 'absolute';
-  div.style.top = '-9999px';
-
-  document.body.appendChild(div);
-  const scrollbarWidth = div.offsetWidth - div.clientWidth;
-  document.body.removeChild(div);
-
-  return scrollbarWidth;
-}
-
-export {
-  getData,
-  getSanitizedData,
-  uniq,
-  intersect,
-  unifiedToNative,
-  measureScrollbar,
-};
+export { getData, getSanitizedData, uniq, intersect, unifiedToNative };
