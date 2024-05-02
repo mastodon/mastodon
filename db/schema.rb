1a7add95c8d327abe4418622674cac316ea5d072
Matt Jankowski <matt@jankowski.online>
2024-05-02 15:43:15 -0400

    Remove Legacy OTP Secret code

--- db/schema.rb
+++ db/schema.rb
@@ -1146,9 +1146,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_082851) do
     t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
     t.string "locale"
-    t.string "encrypted_otp_secret"
-    t.string "encrypted_otp_secret_iv"
-    t.string "encrypted_otp_secret_salt"
     t.integer "consumed_timestep"
     t.boolean "otp_required_for_login", default: false, null: false
     t.datetime "last_emailed_at", precision: nil
