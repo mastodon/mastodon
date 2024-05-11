913a287de3d7bfc3b3f15ba401f3d8099ee91cd3
Nick Schonning <nschonni@gmail.com>
2024-05-11 15:27:37 -0400

    Add i18n-task rspec tests

--- spec/locales/i18n_spec.rb
+++ spec/locales/i18n_spec.rb
@@ -1,8 +1,41 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'i18n/tasks'
 
 describe 'I18n' do
+  # Copied from $(bundle exec i18n-tasks gem-path)/templates/rspec/i18n_spec.rb
+  describe I18n do
+    let(:i18n) { I18n::Tasks::BaseTask.new }
+    let(:missing_keys) { i18n.missing_keys }
+    let(:unused_keys) { i18n.unused_keys }
+    let(:inconsistent_interpolations) { i18n.inconsistent_interpolations }
+
+    it 'does not have missing keys' do
+      expect(missing_keys).to be_empty,
+                              "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+    end
+
+    it 'does not have unused keys' do
+      expect(unused_keys).to be_empty,
+                             "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+    end
+
+    it 'files are normalized' do
+      non_normalized = i18n.non_normalized_paths
+      error_message = "The following files need to be normalized:\n" \
+                      "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
+                      "Please run `i18n-tasks normalize' to fix"
+      expect(non_normalized).to be_empty, error_message
+    end
+
+    it 'does not have inconsistent interpolations' do
+      error_message = "#{inconsistent_interpolations.leaves.count} i18n keys have inconsistent interpolations.\n" \
+                      "Run `i18n-tasks check-consistent-interpolations' to show them"
+      expect(inconsistent_interpolations).to be_empty, error_message
+    end
+  end
+
   describe 'Pluralizing locale translations' do
     subject { I18n.t('generic.validation_errors', count: 1) }
 
