78d999308a847e4b53a085565515bbbdc8feacc9
Matt Jankowski <matt@jankowski.online>
2024-05-14 18:39:23 -0400

    Move simplecov config to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,31 @@
 
 ENV['RAILS_ENV'] ||= 'test'
 
+unless ENV['DISABLE_SIMPLECOV'] == 'true'
+  require 'simplecov'
+
+  SimpleCov.start 'rails' do
+    if ENV['CI']
+      require 'simplecov-lcov'
+      formatter SimpleCov::Formatter::LcovFormatter
+      formatter.config.report_with_single_file = true
+    else
+      formatter SimpleCov::Formatter::HTMLFormatter
+    end
+
+    enable_coverage :branch
+
+    add_filter 'lib/linter'
+
+    add_group 'Libraries', 'lib'
+    add_group 'Policies', 'app/policies'
+    add_group 'Presenters', 'app/presenters'
+    add_group 'Serializers', 'app/serializers'
+    add_group 'Services', 'app/services'
+    add_group 'Validators', 'app/validators'
+  end
+end
+
 # This needs to be defined before Rails is initialized
 STREAMING_PORT = ENV.fetch('TEST_STREAMING_PORT', '4020')
 ENV['STREAMING_API_BASE_URL'] = "http://localhost:#{STREAMING_PORT}"
