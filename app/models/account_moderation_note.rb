5fd56512de244263b4b0df998b8a83c303c3d1c5
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-05-15 15:38:36 +0200

    Improve Report Notes and Account Moderation Notes (#30288)

--- app/models/account_moderation_note.rb
+++ app/models/account_moderation_note.rb
@@ -13,7 +13,7 @@
 #
 
 class AccountModerationNote < ApplicationRecord
-  CONTENT_SIZE_LIMIT = 500
+  CONTENT_SIZE_LIMIT = 2_000
 
   belongs_to :account
   belongs_to :target_account, class_name: 'Account'
--- app/models/report_note.rb
+++ app/models/report_note.rb
@@ -13,7 +13,7 @@
 #
 
 class ReportNote < ApplicationRecord
-  CONTENT_SIZE_LIMIT = 500
+  CONTENT_SIZE_LIMIT = 2_000
 
   belongs_to :account
   belongs_to :report, inverse_of: :notes, touch: true
