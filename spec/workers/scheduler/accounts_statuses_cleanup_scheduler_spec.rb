f0cf6213af420f1f823d996604a5d768b3e6fe52
Matt Jankowski <matt@jankowski.online>
2024-05-17 09:10:04 -0400

    Fix `Rails/WhereRange` cop

--- spec/workers/scheduler/accounts_statuses_cleanup_scheduler_spec.rb
+++ spec/workers/scheduler/accounts_statuses_cleanup_scheduler_spec.rb
@@ -163,7 +163,7 @@ describe Scheduler::AccountsStatusesCleanupScheduler do
       def cleanable_statuses_count
         Status
           .where(account_id: [account_alice, account_chris, account_erin]) # Accounts with enabled policies
-          .where('created_at < ?', 2.weeks.ago) # Policy defaults is 2.weeks
+          .where(created_at: ...2.weeks.ago) # Policy defaults is 2.weeks
           .count
       end
     end
