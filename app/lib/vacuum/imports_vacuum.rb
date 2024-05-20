def6b686ff3ea8e4e77075a812bd463a3bf325c3
Matt Jankowski <matt@jankowski.online>
2024-05-20 05:37:36 -0400

    Fix `Rails/WhereRange` cop (#30343)

--- app/lib/vacuum/imports_vacuum.rb
+++ app/lib/vacuum/imports_vacuum.rb
@@ -9,10 +9,10 @@ class Vacuum::ImportsVacuum
   private
 
   def clean_unconfirmed_imports!
-    BulkImport.state_unconfirmed.where('created_at <= ?', 10.minutes.ago).reorder(nil).in_batches.delete_all
+    BulkImport.state_unconfirmed.where(created_at: ..10.minutes.ago).reorder(nil).in_batches.delete_all
   end
 
   def clean_old_imports!
-    BulkImport.where('created_at <= ?', 1.week.ago).reorder(nil).in_batches.delete_all
+    BulkImport.where(created_at: ..1.week.ago).reorder(nil).in_batches.delete_all
   end
 end
