696ff31c19557d59dbaf95763379c552534e9d35
Jeong Arm <kjwonmail@gmail.com>
2024-05-21 15:43:33 +0900

    Fix admin/accounts/:id page when user is deleted locally

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -6,6 +6,8 @@ module EmailHelper
   end
 
   def email_to_canonical_email(str)
+    return nil if str.nil?
+
     username, domain = str.downcase.split('@', 2)
     username, = username.delete('.').split('+', 2)
 
@@ -13,6 +15,8 @@ module EmailHelper
   end
 
   def email_to_canonical_email_hash(str)
+    return nil if str.nil?
+
     Digest::SHA2.new(256).hexdigest(email_to_canonical_email(str))
   end
 end
