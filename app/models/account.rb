4c65a4773b8c0428f17a1baeb73688e9b3a331a2
hinusif <161274975+hinusif@users.noreply.github.com>
2024-05-21 22:10:52 +0900

    Increase profile fields count to 16
    References
    https://github.com/uri-life/mastodon/commit/6b1e9059fbf072f832c8f17bbd108cc8f92bd347

--- app/models/account.rb
+++ app/models/account.rb
@@ -101,7 +101,7 @@ class Account < ApplicationRecord
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? && actor_type != 'Application' }
   validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
   validates :note, note_length: { maximum: 500 }, if: -> { local? && will_save_change_to_note? }
-  validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
+  validates :fields, length: { maximum: 16 }, if: -> { local? && will_save_change_to_fields? }
   validates :uri, absence: true, if: :local?, on: :create
   validates :inbox_url, absence: true, if: :local?, on: :create
   validates :shared_inbox_url, absence: true, if: :local?, on: :create
@@ -344,7 +344,7 @@ class Account < ApplicationRecord
     self[:fields] = fields
   end
 
-  DEFAULT_FIELDS_SIZE = 4
+  DEFAULT_FIELDS_SIZE = 16
 
   def build_fields
     return if fields.size >= DEFAULT_FIELDS_SIZE
