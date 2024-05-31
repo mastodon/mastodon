cd5cf4c50a522c56889b64b13380aa193498f664
fabiosammy <fabiosammy@gmail.com>
2024-05-31 10:58:04 -0300

    Do not compile the assets when you're using docker setup. Also install the bundler version based on Gemfile.lock

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,26 +2,17 @@
 
 set -e # Fail the whole script on first error
 
+gem install bundler -v $(tail -n 1 Gemfile.lock)
+
 # Fetch Ruby gem dependencies
 bundle config path 'vendor/bundle'
 bundle config with 'development test'
-bundle install
-
-# Make Gemfile.lock pristine again
-git checkout -- Gemfile.lock
+bundle check || bundle install
 
 # Fetch Javascript dependencies
 corepack prepare
 yarn install --immutable
 
-# [re]create, migrate, and seed the test database
-RAILS_ENV=test ./bin/rails db:setup
-
 # [re]create, migrate, and seed the development database
-RAILS_ENV=development ./bin/rails db:setup
+./bin/rails db:setup
 
-# Precompile assets for development
-RAILS_ENV=development ./bin/rails assets:precompile
-
-# Precompile assets for test
-RAILS_ENV=test ./bin/rails assets:precompile
