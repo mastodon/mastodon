b191f0e86757666ab7656f8c750bad789158aa73
Matt Jankowski <matt@jankowski.online>
2024-05-31 12:36:36 -0400

    Convert scope to symbol array outside yaml read

--- config/initializers/3_omniauth.rb
+++ config/initializers/3_omniauth.rb
@@ -27,9 +27,13 @@ Devise.setup do |config|
 
   # OpenID Connect Strategy
   if Rails.configuration.omniauth.oidc_enabled
+    oidc_options = Rails.configuration.omniauth.oidc
+
     config.omniauth(
       :openid_connect,
-      Rails.configuration.omniauth.oidc
+      oidc_options.merge(
+        scope: oidc_options[:scope].split(',').map(&:to_sym) # Convert scope to symbol array
+      )
     )
   end
 end
