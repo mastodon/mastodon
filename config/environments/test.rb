62e71a1ec8ba38908e52e6ca47c69a861454e459
Matt Jankowski <matt@jankowski.online>
2024-06-06 11:51:25 -0400

    Restore default mail from

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_options = { from: 'notifications@localhost' }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
