e02d23b5499318432981b16d8968e109ebeca18c
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-06-06 09:30:10 +0200

    Change `read:me` scope to `profile` scope (#30357)
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_041528) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_195202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
