eb2886c3433d56e45357c36a6430cefd6834c69d
Matt Jankowski <matt@jankowski.online>
2024-06-06 11:54:46 -0400

    format

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,9 @@ Rails.application.configure do
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :redis_cache_store, REDIS_CACHE_PARAMS
-    config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{2.days.to_i}" }
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}",
+    }
   else
     config.action_controller.perform_caching = false
 
