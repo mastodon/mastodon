7d09701ef6c33755d128f58928461e1c8dca5516
Matt Jankowski <matt@jankowski.online>
2024-06-06 11:52:33 -0400

    cache store is memory store in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
-  config.cache_store = :null_store
+  config.cache_store = :memory_store
 
   # Render exception templates for rescuable exceptions and raise for other exceptions.
   config.action_dispatch.show_exceptions = :rescuable
