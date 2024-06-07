7f5ed8bfa84ea64fd81b2734483e28c119ae8599
Matt Jankowski <matt@jankowski.online>
2024-06-07 08:46:34 -0400

    Enable AR encryption work around

--- config/initializers/active_record_encryption.rb
+++ config/initializers/active_record_encryption.rb
@@ -32,8 +32,8 @@ Rails.application.configure do
   config.active_record.encryption.deterministic_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY')
   config.active_record.encryption.key_derivation_salt = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT')
   config.active_record.encryption.primary_key = ENV.fetch('ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY')
-end
 
-# TODO: https://github.com/rails/rails/issues/50604#issuecomment-1880990392
-# Remove after updating to Rails 7.1.4
-ActiveRecord::Encryption.configure(**config.active_record.encryption)
+  # TODO: https://github.com/rails/rails/issues/50604#issuecomment-1880990392
+  # Remove after updating to Rails 7.1.4
+  ActiveRecord::Encryption.configure(**config.active_record.encryption)
+end
