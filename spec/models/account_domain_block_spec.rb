c49fd031dff87637b58abe21a9f79817e615cb18
Matt Jankowski <matt@jankowski.online>
2024-06-09 14:34:55 -0400

    Use `change` expectation to verify actual update

--- spec/models/account_domain_block_spec.rb
+++ spec/models/account_domain_block_spec.rb
@@ -8,18 +8,21 @@ RSpec.describe AccountDomainBlock do
   it 'removes blocking cache after creation' do
     Rails.cache.write("exclude_domains_for:#{account.id}", 'a.domain.already.blocked')
 
-    Fabricate(:account_domain_block, account: account, domain: 'a.domain.blocked.later')
-
-    expect(account_has_exclude_domains_cache?).to be false
+    expect do
+      Fabricate(
+        :account_domain_block,
+        account: account,
+        domain: 'a.domain.blocked.later'
+      )
+    end.to change { account_has_exclude_domains_cache? }.to(false)
   end
 
   it 'removes blocking cache after destruction' do
     block = Fabricate(:account_domain_block, account: account, domain: 'domain')
     Rails.cache.write("exclude_domains_for:#{account.id}", 'domain')
 
-    block.destroy!
-
-    expect(account_has_exclude_domains_cache?).to be false
+    expect { block.destroy! }
+      .to change { account_has_exclude_domains_cache? }.to(false)
   end
 
   def account_has_exclude_domains_cache?
