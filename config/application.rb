d0a938d7e4929c7e92762bf7d01abe9b3281e41f
Matt Jankowski <matt@jankowski.online>
2024-06-11 15:52:31 -0400

    Move hcaptcha service config to `config_for` yml

--- config/application.rb
+++ config/application.rb
@@ -112,6 +112,8 @@ module Mastodon
       end
     end
 
+    config.x.captcha = config_for(:captcha)
+
     config.to_prepare do
       Doorkeeper::AuthorizationsController.layout 'modal'
       Doorkeeper::AuthorizedApplicationsController.layout 'admin'
