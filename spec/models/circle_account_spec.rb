96e04cb48bdb3b7a22a40b48c43f821095b05634
Jesse Karmani <jessekarmani@gmail.com>
2024-06-18 16:44:35 -0700

    Add and remove accounts from corresponding list and circle at the same time

--- spec/models/circle_account_spec.rb
+++ spec/models/circle_account_spec.rb
@@ -1,4 +1,25 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe CircleAccount, type: :model do
+RSpec.describe CircleAccount do
+  it 'creates a list account after creation' do
+    circle = Fabricate(:circle)
+    account = Fabricate(:account)
+    Fabricate(:follow, account: account, target_account: circle.account)
+    Fabricate(:follow, account: circle.account, target_account: account)
+    Fabricate(:circle_account, circle: circle, account: account)
+    expect(circle.list.accounts).to include(account)
+  end
+
+  it 'removes a list account after destruction' do
+    circle = Fabricate(:circle)
+    account = Fabricate(:account)
+    Fabricate(:follow, account: account, target_account: circle.account)
+    Fabricate(:follow, account: circle.account, target_account: account)
+    circle_account = Fabricate(:circle_account, circle: circle, account: account)
+
+    circle_account.destroy
+    expect(circle.list.accounts).to_not include(account)
+  end
 end
