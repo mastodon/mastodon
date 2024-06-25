93c400ac296226bbee4d82029e81eb5d681c18a2
Matt Jankowski <matt@jankowski.online>
2024-06-25 11:25:37 -0400

    Fix `Rails/ReversibleMigration` cop for `remove_index`

--- db/migrate/20161122163057_remove_unneeded_indexes.rb
+++ db/migrate/20161122163057_remove_unneeded_indexes.rb
@@ -2,8 +2,8 @@
 
 class RemoveUnneededIndexes < ActiveRecord::Migration[5.0]
   def change
-    remove_index :notifications, name: 'index_notifications_on_account_id'
-    remove_index :settings, name: 'index_settings_on_target_type_and_target_id'
-    remove_index :statuses_tags, name: 'index_statuses_tags_on_tag_id'
+    remove_index :notifications, :account_id, name: 'index_notifications_on_account_id'
+    remove_index :settings, [:target_type, :target_id], name: 'index_settings_on_target_type_and_target_id'
+    remove_index :statuses_tags, :tag_id, name: 'index_statuses_tags_on_tag_id'
   end
 end
