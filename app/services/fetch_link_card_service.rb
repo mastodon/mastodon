f5f8e61e4365a0bef0f1a7e44960f3c769168eca
David Roetzel <david@roetzel.de>
2024-06-27 11:54:24 +0200

    Catch encoding errors when creating link previews.
    This skips creation of link previews if any encoding
    errors occurr. See #30792

--- app/services/fetch_link_card_service.rb
+++ app/services/fetch_link_card_service.rb
@@ -29,7 +29,7 @@ class FetchLinkCardService < BaseService
     end
 
     attach_card if @card&.persisted?
-  rescue HTTP::Error, OpenSSL::SSL::SSLError, Addressable::URI::InvalidURIError, Mastodon::HostValidationError, Mastodon::LengthValidationError => e
+  rescue HTTP::Error, OpenSSL::SSL::SSLError, Addressable::URI::InvalidURIError, Mastodon::HostValidationError, Mastodon::LengthValidationError, Encoding::UndefinedConversionError => e
     Rails.logger.debug { "Error fetching link #{@original_url}: #{e}" }
     nil
   end
