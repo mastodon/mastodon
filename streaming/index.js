f429b03f7141ba6b1e7678f32e74c87c1d4807dc
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-07-10 00:12:05 +0200

    Streaming: Fix incorrect type definitions for WebSocket

--- streaming/index.js
+++ streaming/index.js
@@ -899,7 +899,7 @@ const startServer = async () => {
 
   /**
    * @param {http.IncomingMessage} req
-   * @param {WebSocket} ws
+   * @param {import('ws').WebSocket} ws
    * @param {string[]} streamName
    * @returns {function(string, string): void}
    */
@@ -1135,7 +1135,7 @@ const startServer = async () => {
 
   /**
    * @typedef WebSocketSession
-   * @property {WebSocket & { isAlive: boolean}} websocket
+   * @property {import('ws').WebSocket & { isAlive: boolean}} websocket
    * @property {http.IncomingMessage & ResolvedAccount} request
    * @property {import('pino').Logger} logger
    * @property {Object.<string, WebSocketSubscription>} subscriptions
@@ -1264,7 +1264,7 @@ const startServer = async () => {
   };
 
   /**
-   * @param {WebSocket & { isAlive: boolean }} ws
+   * @param {import('ws').WebSocket & { isAlive: boolean }} ws
    * @param {http.IncomingMessage & ResolvedAccount} req
    * @param {import('pino').Logger} log
    */
