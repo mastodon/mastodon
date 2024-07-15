c46c9e4e78cc316aec66c270d02bb2c215f025c1
Eugen Rochko <eugen@zeonfederated.com>
2024-07-15 16:13:16 +0200

    Change style of private mentions in notifications

--- app/javascript/mastodon/features/notifications_v2/components/notification_with_status.tsx
+++ app/javascript/mastodon/features/notifications_v2/components/notification_with_status.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import type { IconProp } from 'mastodon/components/icon';
 import { Icon } from 'mastodon/components/icon';
 import Status from 'mastodon/containers/status_container';
+import { useAppSelector } from 'mastodon/store';
 
 import { NamesList } from './names_list';
 import type { LabelRenderer } from './notification_group_with_status';
@@ -36,12 +37,19 @@ export const NotificationWithStatus: React.FC<{
     [labelRenderer, accountIds, count],
   );
 
+  const isPrivateMention = useAppSelector(
+    (state) => state.statuses.getIn([statusId, 'visibility']) === 'direct',
+  );
+
   return (
     <div
       role='button'
       className={classNames(
         `notification-ungrouped focusable notification-ungrouped--${type}`,
-        { 'notification-ungrouped--unread': unread },
+        {
+          'notification-ungrouped--unread': unread,
+          'notification-ungrouped--direct': isPrivateMention,
+        },
       )}
       tabIndex={0}
     >
