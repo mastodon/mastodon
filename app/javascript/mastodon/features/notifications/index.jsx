9d0bce40724eb77b7cc3917091b62e4ab159e29e
David Roetzel <david@roetzel.de>
2024-07-15 15:42:47 +0200

    Move filtered notifications bar in scrollable area (#30996)
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/mastodon/features/notifications/index.jsx
+++ app/javascript/mastodon/features/notifications/index.jsx
@@ -223,6 +223,13 @@ class Notifications extends PureComponent {
 
     let scrollContainer;
 
+    const prepend = (
+      <>
+        {needsNotificationPermission && <NotificationsPermissionBanner />}
+        <FilteredNotificationsBanner />
+      </>
+    );
+
     if (signedIn) {
       scrollContainer = (
         <ScrollableList
@@ -232,7 +239,7 @@ class Notifications extends PureComponent {
           showLoading={isLoading && notifications.size === 0}
           hasMore={hasMore}
           numPending={numPending}
-          prepend={needsNotificationPermission && <NotificationsPermissionBanner />}
+          prepend={prepend}
           alwaysPrepend
           emptyMessage={emptyMessage}
           onLoadMore={this.handleLoadOlder}
@@ -281,8 +288,6 @@ class Notifications extends PureComponent {
         </ColumnHeader>
 
         {filterBarContainer}
-
-        <FilteredNotificationsBanner />
 
         {scrollContainer}
 
