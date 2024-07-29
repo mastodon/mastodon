8967af338ca70e0a6965ac6286242985256bab17
Matt Jankowski <matt@jankowski.online>
2024-07-29 16:44:20 -0400

    Add private method declaration

--- app/models/concerns/canonical_email.rb
+++ app/models/concerns/canonical_email.rb
@@ -4,19 +4,21 @@ module CanonicalEmail
   extend ActiveSupport::Concern
 
   class_methods do
-    def email_to_canonical_email(str)
-      username, domain = str.downcase.split('@', 2)
-      username, = username.delete('.').split('+', 2)
-
-      "#{username}@#{domain}"
-    end
-
     def email_to_canonical_email_hash(str)
       Digest::SHA2
         .new(256)
         .hexdigest(
           email_to_canonical_email(str)
         )
+    end
+
+    private
+
+    def email_to_canonical_email(str)
+      username, domain = str.downcase.split('@', 2)
+      username, = username.delete('.').split('+', 2)
+
+      "#{username}@#{domain}"
     end
   end
 end
