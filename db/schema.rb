e06e7812367b4555b2cbfe7eab4a1c0f820c0c80
Claire <claire.github-309c@sitedethib.com>
2024-08-08 14:51:57 +0200

    Add data migration from old notifications policy version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_08_114841) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_08_124339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
