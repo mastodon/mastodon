95ba36d2dcd68f84b093cf57ac8c768f66a38438
Claire <claire.github-309c@sitedethib.com>
2024-08-08 15:10:33 +0200

    Add `/api/v2/notifications/policy`

--- app/serializers/rest/notification_policy_serializer.rb
+++ app/serializers/rest/notification_policy_serializer.rb
@@ -3,10 +3,11 @@
 class REST::NotificationPolicySerializer < ActiveModel::Serializer
   # Please update `app/javascript/mastodon/api_types/notification_policies.ts` when making changes to the attributes
 
-  attributes :filter_not_following,
-             :filter_not_followers,
-             :filter_new_accounts,
-             :filter_private_mentions,
+  attributes :for_not_following,
+             :for_not_followers,
+             :for_new_accounts,
+             :for_private_mentions,
+             :for_limited_accounts,
              :summary
 
   def summary
@@ -14,21 +15,5 @@ class REST::NotificationPolicySerializer < ActiveModel::Serializer
       pending_requests_count: object.pending_requests_count.to_i,
       pending_notifications_count: object.pending_notifications_count.to_i,
     }
-  end
-
-  def filter_not_following
-    !object.accept_not_following?
-  end
-
-  def filter_not_followers
-    !object.accept_not_followers?
-  end
-
-  def filter_new_accounts
-    !object.accept_new_accounts?
-  end
-
-  def filter_private_mentions
-    !object.accept_private_mentions?
   end
 end
