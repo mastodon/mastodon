8a5b57f66874dd936bcaa2532f0fb9d3fabc6e5c
Claire <claire.github-309c@sitedethib.com>
2024-08-09 15:48:54 +0200

    Revert "Support JSON-LD named graph (#31288)" (#31355)

--- app/lib/link_details_extractor.rb
+++ app/lib/link_details_extractor.rb
@@ -101,9 +101,7 @@ class LinkDetailsExtractor
     end
 
     def json
-      @json ||= root_array(Oj.load(@data))
-                .map { |node| JSON::LD::API.compact(node, 'https://schema.org') }
-                .find { |node| SUPPORTED_TYPES.include?(node['type']) } || {}
+      @json ||= root_array(Oj.load(@data)).compact.find { |obj| SUPPORTED_TYPES.include?(obj['@type']) } || {}
     end
   end
 
