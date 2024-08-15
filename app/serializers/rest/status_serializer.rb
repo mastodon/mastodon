49021ad562d4dde10fb451fafcdf86760fd3408b
Scobiform <datenkrake@scobiform.com>
2024-08-15 23:17:13 +0200

    Markdown status

--- app/serializers/rest/status_serializer.rb
+++ app/serializers/rest/status_serializer.rb
@@ -76,7 +76,7 @@ class REST::StatusSerializer < ActiveModel::Serializer
   end
 
   def content
-    markdown.render(text)
+    markdown.render(status_content_format(object.text, domain: Rails.configuration.x.local_domain))
   end
 
   def url
