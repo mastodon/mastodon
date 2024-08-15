4545b8d6cbab59591135ce8d0dfc75ad84c46e73
Renaud Chaput <renchap@gmail.com>
2024-08-15 08:44:13 +0200

    Add the ability to ignore specific browser errors for a spec (#31436)

--- spec/support/browser_errors.rb
+++ spec/support/browser_errors.rb
@@ -1,23 +1,36 @@
 # frozen_string_literal: true
 
+module BrowserErrorsHelpers
+  def ignore_js_error(error)
+    @ignored_js_errors_for_spec << error
+  end
+end
+
 RSpec.configure do |config|
+  config.include BrowserErrorsHelpers, :js, type: :system
+
+  config.before(:each, :js, type: :system) do
+    @ignored_js_errors_for_spec = []
+  end
+
   config.after(:each, :js, type: :system) do
     # Classes of intermittent ignorable errors
     ignored_errors = [
       /Error while trying to use the following icon from the Manifest/, # https://github.com/mastodon/mastodon/pull/30793
       /Manifest: Line: 1, column: 1, Syntax error/, # Similar parsing/interruption issue as above
-    ]
+    ].concat(@ignored_js_errors_for_spec)
+
     errors = page.driver.browser.logs.get(:browser).reject do |error|
       ignored_errors.any? { |pattern| pattern.match(error.message) }
     end
 
     if errors.present?
-      aggregate_failures 'javascript errrors' do
+      aggregate_failures 'browser errrors' do
         errors.each do |error|
           expect(error.level).to_not eq('SEVERE'), error.message
           next unless error.level == 'WARNING'
 
-          warn 'WARN: javascript warning'
+          warn 'WARN: browser warning'
           warn error.message
         end
       end
