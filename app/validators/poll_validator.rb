c1c16d0c1dfa5de2f4e3bbbd4b3be412986e283a
Scobiform <datenkrake@scobiform.com>
2024-08-16 15:02:45 +0200

    new poll option 365 days - MAX_EXPIRATION = 2.years.freeze

--- app/validators/poll_validator.rb
+++ app/validators/poll_validator.rb
@@ -3,7 +3,7 @@
 class PollValidator < ActiveModel::Validator
   MAX_OPTIONS      = 77
   MAX_OPTION_CHARS = 500
-  MAX_EXPIRATION   = 1.month.freeze
+  MAX_EXPIRATION   = 2.years.freeze
   MIN_EXPIRATION   = 5.minutes.freeze
 
   def validate(poll)
