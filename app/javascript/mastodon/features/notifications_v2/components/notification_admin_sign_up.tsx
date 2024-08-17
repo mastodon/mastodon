7140340f1db046a1495941fb1f337a5ce1db506e
mogaminsk <mgmnjp@icloud.com>
2024-08-17 19:55:35 +0900

    Update admin.sign-up

--- app/javascript/mastodon/features/notifications_v2/components/notification_admin_sign_up.tsx
+++ app/javascript/mastodon/features/notifications_v2/components/notification_admin_sign_up.tsx
@@ -1,31 +1,59 @@
+import { useMemo } from 'react';
+
 import { FormattedMessage } from 'react-intl';
 
 import PersonAddIcon from '@/material-icons/400-24px/person_add-fill.svg?react';
 import type { NotificationGroupAdminSignUp } from 'mastodon/models/notification_group';
 
-import type { LabelRenderer } from './notification_group_with_status';
 import { NotificationGroupWithStatus } from './notification_group_with_status';
-
-const labelRenderer: LabelRenderer = (values) => (
-  <FormattedMessage
-    id='notification.admin.sign_up'
-    defaultMessage='{name} signed up'
-    values={values}
-  />
-);
+import { DisplayedName } from './displayed_name';
 
 export const NotificationAdminSignUp: React.FC<{
   notification: NotificationGroupAdminSignUp;
   unread: boolean;
-}> = ({ notification, unread }) => (
-  <NotificationGroupWithStatus
-    type='admin-sign-up'
-    icon={PersonAddIcon}
-    iconId='person-add'
-    accountIds={notification.sampleAccountIds}
-    timestamp={notification.latest_page_notification_at}
-    count={notification.notifications_count}
-    labelRenderer={labelRenderer}
-    unread={unread}
-  />
-);
+}> = ({ notification, unread }) => {
+  const displayedName = (
+    <DisplayedName
+      accountIds={notification.sampleAccountIds}
+    />
+  );
+
+  const count = notification.notifications_count;
+
+  const label = useMemo(
+    () => {
+      if (count === 1)
+        return (
+          <FormattedMessage
+            id='notification.admin.sign_up'
+            defaultMessage='{name} signed up'
+            values={{ name: displayedName }}
+          />
+        );
+    
+      return (
+        <FormattedMessage
+          id='notification.admin.sign_up'
+          defaultMessage='{name} and {count, plural, one {# other} other {# others}} signed up'
+          values={{
+            name: displayedName,
+            count: count - 1,
+          }}
+        />
+      );
+    },
+    [displayedName, count],
+  );
+
+  return (
+    <NotificationGroupWithStatus
+      type='admin-sign-up'
+      icon={PersonAddIcon}
+      iconId='person-add'
+      accountIds={notification.sampleAccountIds}
+      timestamp={notification.latest_page_notification_at}
+      label={label}
+      unread={unread}
+    />
+  )
+};
