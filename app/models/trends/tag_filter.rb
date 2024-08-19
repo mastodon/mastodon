5f9f35982b12fedfa657b38b399203f0c7a5806a
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-08-19 15:09:12 +0200

    Update app/models/trends/tag_filter.rb
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- app/models/trends/tag_filter.rb
+++ app/models/trends/tag_filter.rb
@@ -14,7 +14,7 @@ class Trends::TagFilter
 
   def results
     scope = if params[:status] == 'pending_review'
-              Tag.unscoped.order(created_at: :desc)
+              Tag.unscoped.order(id: :desc)
             else
               trending_scope
             end
