5a953d4500e9f91ec20239af30124818e34dcc06
Eugen Rochko <eugen@zeonfederated.com>
2024-08-21 00:10:09 +0200

    Change hints for missing remote content in web UI

--- app/javascript/mastodon/components/timeline_hint.tsx
+++ app/javascript/mastodon/components/timeline_hint.tsx
@@ -1,28 +1,23 @@
-import { FormattedMessage } from 'react-intl';
-
 import classNames from 'classnames';
 
 interface Props {
-  resource: JSX.Element;
+  message: React.ReactNode;
+  label: React.ReactNode;
   url: string;
   className?: string;
 }
 
-export const TimelineHint: React.FC<Props> = ({ className, resource, url }) => (
+export const TimelineHint: React.FC<Props> = ({
+  className,
+  message,
+  label,
+  url,
+}) => (
   <div className={classNames('timeline-hint', className)}>
-    <strong>
-      <FormattedMessage
-        id='timeline_hint.remote_resource_not_displayed'
-        defaultMessage='{resource} from other servers are not displayed.'
-        values={{ resource }}
-      />
-    </strong>
-    <br />
+    <p>{message}</p>
+
     <a href={url} target='_blank' rel='noopener noreferrer'>
-      <FormattedMessage
-        id='account.browse_more_on_origin_server'
-        defaultMessage='Browse more on the original profile'
-      />
+      {label}
     </a>
   </div>
 );
