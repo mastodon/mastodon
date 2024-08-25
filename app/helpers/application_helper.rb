a6934e51923170e9c1e7bac9e75aa1dc5f70342d
Matt Jankowski <matt@jankowski.online>
2024-08-25 17:24:09 -0400

    Fix `Rails/CompactBlank` cop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -86,7 +86,7 @@ module ApplicationHelper
   def html_title
     safe_join(
       [content_for(:page_title).to_s.chomp, title]
-      .select(&:present?),
+      .compact_blank,
       ' - '
     )
   end
