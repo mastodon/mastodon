5ce7a904d4576c1e3268a98c1c34b05f812dd8ca
Matt Jankowski <matt@jankowski.online>
2024-08-26 12:43:16 -0400

    Convert `Settings::Exports::MutedAccounts` controller spec to request spec

---
+++ spec/requests/settings/exports/muted_accounts_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Settings / Exports / Muted Accounts' do
+  describe 'GET /settings/exports/mutes' do
+    context 'with a signed in user who has muted accounts' do
+      let(:user) { Fabricate :user }
+
+      before do
+        Fabricate(
+          :mute,
+          account: user.account,
+          target_account: Fabricate(:account, username: 'username', domain: 'domain')
+        )
+        sign_in user
+      end
+
+      it 'returns a CSV with the muted accounts' do
+        get '/settings/exports/mutes.csv'
+
+        expect(response)
+          .to have_http_status(200)
+        expect(response.body)
+          .to eq(<<~CSV)
+            Account address,Hide notifications
+            username@domain,true
+          CSV
+      end
+    end
+  end
+end
