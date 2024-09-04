246fbd245f54f26834edab0123acb88bf201a6f2
Matt Jankowski <matt@jankowski.online>
2024-09-04 14:45:32 -0400

    Check private cache in tags spec

--- spec/system/tags_spec.rb
+++ spec/system/tags_spec.rb
@@ -6,11 +6,15 @@ RSpec.describe 'Tags' do
   describe 'Viewing a tag' do
     let(:tag) { Fabricate(:tag, name: 'test') }
 
+    before { sign_in Fabricate(:user) }
+
     it 'visits the tag page and renders the web app' do
       visit tag_path(tag)
 
       expect(page)
         .to have_css('noscript', text: /Mastodon/)
+      expect(page.response_headers)
+        .to include('Cache-Control' => 'private, no-store')
     end
   end
 end
