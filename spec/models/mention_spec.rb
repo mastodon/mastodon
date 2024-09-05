d6604252da7ac09ef70bd2f09610a9fd4348eb5c
Matt Jankowski <matt@jankowski.online>
2024-09-05 13:45:13 -0400

    mention

--- spec/models/mention_spec.rb
+++ spec/models/mention_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Mention do
-  describe 'validations' do
+  describe 'Associations' do
     it { is_expected.to belong_to(:account).required }
     it { is_expected.to belong_to(:status).required }
   end
