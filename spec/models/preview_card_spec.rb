8f3ea2c682fea25d816d7c110abe2fc4e7c8be82
Matt Jankowski <matt@jankowski.online>
2024-09-05 13:45:28 -0400

    preview card

--- spec/models/preview_card_spec.rb
+++ spec/models/preview_card_spec.rb
@@ -9,26 +9,10 @@ RSpec.describe PreviewCard do
     end
   end
 
-  describe 'validations' do
-    describe 'urls' do
-      it 'allows http schemes' do
-        record = described_class.new(url: 'http://example.host/path')
-
-        expect(record).to be_valid
-      end
-
-      it 'allows https schemes' do
-        record = described_class.new(url: 'https://example.host/path')
-
-        expect(record).to be_valid
-      end
-
-      it 'does not allow javascript: schemes' do
-        record = described_class.new(url: 'javascript:alert()')
-
-        expect(record).to_not be_valid
-        expect(record).to model_have_error_on_field(:url)
-      end
+  describe 'Validations' do
+    describe 'url' do
+      it { is_expected.to allow_values('http://example.host/path', 'https://example.host/path').for(:url) }
+      it { is_expected.to_not allow_value('javascript:alert()').for(:url) }
     end
   end
 end
