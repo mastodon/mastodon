1190cbb199be4061db40e055d3e350997fbdeea9
Eugen Rochko <eugen@zeonfederated.com>
2024-09-05 09:32:15 +0200

    Change embedded posts to use web UI

--- public/embed.js
+++ public/embed.js
@@ -19,44 +19,65 @@
   };
 
   ready(function () {
-    /** @type {Map<number, HTMLIFrameElement>} */
-    var iframes = new Map();
+    /** @type {Map<number, HTMLQuoteElement>} */
+    var embeds = new Map();
 
     window.addEventListener('message', function (e) {
       var data = e.data || {};
 
-      if (typeof data !== 'object' || data.type !== 'setHeight' || !iframes.has(data.id)) {
+      if (typeof data !== 'object' || data.type !== 'setHeight' || !embeds.has(data.id)) {
         return;
       }
 
-      var iframe = iframes.get(data.id);
+      var container = embeds.get(data.id);
 
-      if(!iframe) return;
+      if (!container) return;
 
-      if ('source' in e && iframe.contentWindow !== e.source) {
+      var iframe = container.querySelector('iframe');
+
+      if (!iframe || ('source' in e && iframe.contentWindow !== e.source)) {
         return;
       }
 
       iframe.height = data.height;
+
+      var placeholder = container.querySelector('a');
+
+      if (!placeholder) return;
+
+      container.removeChild(placeholder);
     });
 
-    document.querySelectorAll('iframe.mastodon-embed').forEach(iframe => {
-      // select unique id for each iframe
+    document.querySelectorAll('blockquote.mastodon-embed').forEach(container => {
+      // Select unique id for each iframe
       var id = 0, failCount = 0, idBuffer = new Uint32Array(1);
-      while (id === 0 || iframes.has(id)) {
+
+      while (id === 0 || embeds.has(id)) {
         id = crypto.getRandomValues(idBuffer)[0];
         failCount++;
+
         if (failCount > 100) {
           // give up and assign (easily guessable) unique number if getRandomValues is broken or no luck
-          id = -(iframes.size + 1);
+          id = -(embeds.size + 1);
           break;
         }
       }
 
-      iframes.set(id, iframe);
+      embeds.set(id, container);
 
+      var iframe = document.createElement('iframe');
+      var embedUrl = new URL(container.getAttribute('data-embed-url'));
+
+      if (embedUrl.protocol !== 'https:') return;
+
+      iframe.src = embedUrl.toString();
+      iframe.width = container.clientWidth;
+      iframe.height = 0;
       iframe.scrolling = 'no';
+      iframe.allowfullscreen = true;
+      iframe.style.border = 0;
       iframe.style.overflow = 'hidden';
+      iframe.style.display = 'block';
 
       iframe.onload = function () {
         iframe.contentWindow.postMessage({
@@ -65,7 +86,7 @@
         }, '*');
       };
 
-      iframe.onload();
+      container.appendChild(iframe);
     });
   });
 })();
