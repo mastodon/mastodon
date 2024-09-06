4db23bd6d3532e5b3174379d7865de5140b77439
Matt Jankowski <matt@jankowski.online>
2024-09-06 16:30:09 -0400

    Add `ScheduledStatus` validation logic

--- app/models/scheduled_status.rb
+++ app/models/scheduled_status.rb
@@ -20,21 +20,39 @@ class ScheduledStatus < ApplicationRecord
   belongs_to :account, inverse_of: :scheduled_statuses
   has_many :media_attachments, inverse_of: :scheduled_status, dependent: :nullify
 
-  validate :validate_future_date
+  scope :scheduled_on_day, ->(date) { where(scheduled_at: date.all_day) }
+
+  validate :validate_future_date, if: :scheduled_at?
   validate :validate_total_limit
   validate :validate_daily_limit
 
   private
 
   def validate_future_date
-    errors.add(:scheduled_at, I18n.t('scheduled_statuses.too_soon')) if scheduled_at.present? && scheduled_at <= Time.now.utc + MINIMUM_OFFSET
+    errors.add(:scheduled_at, I18n.t('scheduled_statuses.too_soon')) if scheduled_too_early?
   end
 
   def validate_total_limit
-    errors.add(:base, I18n.t('scheduled_statuses.over_total_limit', limit: TOTAL_LIMIT)) if account.scheduled_statuses.count >= TOTAL_LIMIT
+    errors.add(:base, I18n.t('scheduled_statuses.over_total_limit', limit: TOTAL_LIMIT)) if over_total_limit?
   end
 
   def validate_daily_limit
-    errors.add(:base, I18n.t('scheduled_statuses.over_daily_limit', limit: DAILY_LIMIT)) if account.scheduled_statuses.where('scheduled_at::date = ?::date', scheduled_at).count >= DAILY_LIMIT
+    errors.add(:base, I18n.t('scheduled_statuses.over_daily_limit', limit: DAILY_LIMIT)) if over_daily_limit?
+  end
+
+  def over_daily_limit?
+    account.scheduled_statuses.scheduled_on_day(scheduled_at).count >= DAILY_LIMIT
+  end
+
+  def over_total_limit?
+    account.scheduled_statuses.count >= TOTAL_LIMIT
+  end
+
+  def scheduled_too_early?
+    scheduled_at <= earliest_valid_scheduled_at
+  end
+
+  def earliest_valid_scheduled_at
+    Time.now.utc + MINIMUM_OFFSET
   end
 end
