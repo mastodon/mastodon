62931c1628cda5085a638c1271dbc005a3d79e8b
Matt Jankowski <matt@jankowski.online>
2024-09-06 14:17:52 -0400

    Move body class to shared partial for web app controller concern views

--- app/controllers/concerns/web_app_controller_concern.rb
+++ app/controllers/concerns/web_app_controller_concern.rb
@@ -7,7 +7,6 @@ module WebAppControllerConcern
     vary_by 'Accept, Accept-Language, Cookie'
 
     before_action :redirect_unauthenticated_to_permalinks!
-    before_action :set_app_body_class
 
     content_security_policy do |p|
       policy = ContentSecurityPolicy.new
@@ -22,10 +21,6 @@ module WebAppControllerConcern
 
   def skip_csrf_meta_tags?
     !(ENV['ONE_CLICK_SSO_LOGIN'] == 'true' && ENV['OMNIAUTH_ONLY'] == 'true' && Devise.omniauth_providers.length == 1) && current_user.nil?
-  end
-
-  def set_app_body_class
-    @body_classes = 'app-body'
   end
 
   def redirect_unauthenticated_to_permalinks!
--- spec/system/privacy_spec.rb
+++ spec/system/privacy_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe 'Privacy policy page' do
 
     expect(page)
       .to have_css('noscript', text: /Mastodon/)
+      .and have_css('body', class: 'app-body')
   end
 end
