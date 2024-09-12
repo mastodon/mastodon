17c57c46e7f54f32ad7b17b86c7b936c789ca799
Matt Jankowski <matt@jankowski.online>
2024-09-12 09:25:23 -0400

    Add coverage for title/limit validations in `List` model (#31869)

--- app/models/list.rb
+++ app/models/list.rb
@@ -20,20 +20,22 @@ class List < ApplicationRecord
 
   enum :replies_policy, { list: 0, followed: 1, none: 2 }, prefix: :show
 
-  belongs_to :account, optional: true
+  belongs_to :account
 
   has_many :list_accounts, inverse_of: :list, dependent: :destroy
   has_many :accounts, through: :list_accounts
 
   validates :title, presence: true
 
-  validates_each :account_id, on: :create do |record, _attr, value|
-    record.errors.add(:base, I18n.t('lists.errors.limit')) if List.where(account_id: value).count >= PER_ACCOUNT_LIMIT
-  end
+  validate :validate_account_lists_limit, on: :create
 
   before_destroy :clean_feed_manager
 
   private
+
+  def validate_account_lists_limit
+    errors.add(:base, I18n.t('lists.errors.limit')) if account.lists.count >= PER_ACCOUNT_LIMIT
+  end
 
   def clean_feed_manager
     FeedManager.instance.clean_feeds!(:list, [id])
