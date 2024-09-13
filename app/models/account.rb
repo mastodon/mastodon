6f43bd0a2e3926f7d42cb92dcd6ecd78154c0fe7
Matt Jankowski <matt@jankowski.online>
2024-09-13 11:30:33 -0400

    Use rails built in attribute default for account trendable

--- app/models/account.rb
+++ app/models/account.rb
@@ -181,6 +181,8 @@ class Account < ApplicationRecord
 
   update_index('accounts', :self)
 
+  attribute :trendable, default: -> { Setting.trendable_by_default }
+
   def local?
     domain.nil?
   end
@@ -245,10 +247,6 @@ class Account < ApplicationRecord
 
   def memorialize!
     update!(memorial: true)
-  end
-
-  def trendable?
-    boolean_with_default('trendable', Setting.trendable_by_default)
   end
 
   def sign?
