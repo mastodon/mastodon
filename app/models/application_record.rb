b3c08974bf25c0f586c4790d6545e0bc9df0397b
Matt Jankowski <matt@jankowski.online>
2024-09-13 11:31:09 -0400

    Remove unused `boolean_with_default` method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -13,16 +13,6 @@ class ApplicationRecord < ActiveRecord::Base
     end
   end
 
-  def boolean_with_default(key, default_value)
-    value = attributes[key]
-
-    if value.nil?
-      default_value
-    else
-      value
-    end
-  end
-
   # Prevent implicit serialization in ActiveModel::Serializer or other code paths.
   # This is a hardening step to avoid accidental leaking of attributes.
   def as_json
