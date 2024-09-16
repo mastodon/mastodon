dc24d3c2beec00108543a8fd46beca42f4a5ba41
Claire <claire.github-309c@sitedethib.com>
2024-09-16 13:27:50 +0200

    Re-introduce v2_alpha for compat

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -316,6 +316,21 @@ namespace :api, format: false do
     end
   end
 
+  concern :grouped_notifications do
+    resources :notifications, param: :group_key, only: [:index, :show] do
+      collection do
+        post :clear
+        get :unread_count
+      end
+
+      member do
+        post :dismiss
+      end
+
+      resources :accounts, only: [:index], module: :notifications
+    end
+  end
+
   namespace :v2 do
     get '/search', to: 'search#index', as: :search
 
@@ -342,18 +357,11 @@ namespace :api, format: false do
       resource :policy, only: [:show, :update]
     end
 
-    resources :notifications, param: :group_key, only: [:index, :show] do
-      collection do
-        post :clear
-        get :unread_count
-      end
+    concerns :grouped_notifications
+  end
 
-      member do
-        post :dismiss
-      end
-
-      resources :accounts, only: [:index], module: :notifications
-    end
+  namespace :v2_alpha, module: 'v2' do
+    concerns :grouped_notifications
   end
 
   namespace :web do
