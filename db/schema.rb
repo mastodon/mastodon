7c79bf59c9bc7de78a6e9569df262549aee546a0
Matt Jankowski <matt@jankowski.online>
2024-09-16 15:19:50 -0400

    Remove `crypto` values from doorkeeper application/token `scopes`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_014637) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_16_190140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
