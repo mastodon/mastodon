5e0b959660bc80cf3ddbd4ae35b53361a99b2066
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-09-18 21:49:57 -0700

    accept review comments https://github.com/NeuromatchAcademy/mastodon/pull/44\#discussion_r1766143286 and https://github.com/NeuromatchAcademy/mastodon/pull/44\#discussion_r1766148179
    Signed-off-by: sneakers-the-rat <sneakers-the-rat@protonmail.com>

--- db/migrate/20240918233930_add_fetched_replies_at_to_status.rb
+++ db/migrate/20240918233930_add_fetched_replies_at_to_status.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class AddFetchedRepliesAtToStatus < ActiveRecord::Migration[7.1]
-  disable_ddl_transaction!
-
   def change
     add_column :statuses, :fetched_replies_at, :datetime, null: true
   end
