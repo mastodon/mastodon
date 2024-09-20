7b3047f0a86be359e48cc060a23b07ba01e092e3
Matt Jankowski <matt@jankowski.online>
2024-09-20 12:17:46 -0400

    Better nesting in req spec

--- spec/requests/settings/applications_spec.rb
+++ spec/requests/settings/applications_spec.rb
@@ -3,13 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Settings / Exports' do
-  context 'when user does not own application' do
-    let!(:application) { Fabricate :application }
-    let(:user) { Fabricate :user }
+  let(:user) { Fabricate :user }
 
-    before { sign_in user }
+  before { sign_in user }
 
-    describe 'GET /settings/application/:id' do
+  describe 'GET /settings/application/:id' do
+    context 'when user does not own application' do
+      let(:application) { Fabricate :application }
+
       it 'returns http missing' do
         get settings_application_path(application)
 
@@ -17,27 +18,27 @@ RSpec.describe 'Settings / Exports' do
           .to have_http_status(404)
       end
     end
+  end
 
-    describe 'POST /settings/applications' do
-      subject { post '/settings/applications', params: params }
+  describe 'POST /settings/applications' do
+    subject { post '/settings/applications', params: params }
 
-      let(:params) do
-        {
-          doorkeeper_application: {
-            name: 'My New App',
-            redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-            website: 'http://google.com',
-            scopes: 'read write follow',
-          },
-        }
-      end
+    let(:params) do
+      {
+        doorkeeper_application: {
+          name: 'My New App',
+          redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+          website: 'http://google.com',
+          scopes: 'read write follow',
+        },
+      }
+    end
 
-      it 'supports passing scope values as string' do
-        expect { subject }
-          .to change(Doorkeeper::Application, :count).by(1)
-        expect(response)
-          .to redirect_to(settings_applications_path)
-      end
+    it 'supports passing scope values as string' do
+      expect { subject }
+        .to change(Doorkeeper::Application, :count).by(1)
+      expect(response)
+        .to redirect_to(settings_applications_path)
     end
   end
 end
