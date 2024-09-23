1833f9082be0cd0c0aac4b5f94bea0209ed6c4cb
Matt Jankowski <matt@jankowski.online>
2024-09-23 14:28:33 -0400

    Reduce factory creation (2 -> 1) in `PublishScheduledStatusWorker` spec

--- spec/workers/publish_scheduled_status_worker_spec.rb
+++ spec/workers/publish_scheduled_status_worker_spec.rb
@@ -5,19 +5,19 @@ require 'rails_helper'
 RSpec.describe PublishScheduledStatusWorker do
   subject { described_class.new }
 
-  let(:scheduled_status) { Fabricate(:scheduled_status, params: { text: 'Hello world, future!' }) }
+  let(:scheduled_status) { Fabricate(:scheduled_status, params: { text: text }) }
+  let(:text) { 'Hello world, future!' }
 
   describe 'perform' do
-    before do
-      subject.perform(scheduled_status.id)
-    end
+    it 'creates a status and removes the scheduled status' do
+      expect { subject.perform(scheduled_status.id) }
+        .to change { scheduled_status.account.statuses.count }.from(0).to(1)
 
-    it 'creates a status' do
-      expect(scheduled_status.account.statuses.first.text).to eq 'Hello world, future!'
-    end
+      expect(scheduled_status.account.statuses.first.text)
+        .to eq(text)
 
-    it 'removes the scheduled status' do
-      expect(ScheduledStatus.find_by(id: scheduled_status.id)).to be_nil
+      expect { scheduled_status.reload }
+        .to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
