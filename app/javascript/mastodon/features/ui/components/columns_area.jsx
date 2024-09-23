7fb2e40ecad6483e7eb0daa7bd4d38af55480ebc
Claire <claire.github-309c@sitedethib.com>
2024-09-23 13:59:18 +0200

    Revert "Increase no-gap-breakpoint by 32px to fix overflow (#31889)"
    This reverts commit d2f907507857fb0d256c45dda34403ae1fe95cd8.

--- app/javascript/mastodon/features/ui/components/columns_area.jsx
+++ app/javascript/mastodon/features/ui/components/columns_area.jsx
@@ -61,8 +61,8 @@ export default class ColumnsArea extends ImmutablePureComponent {
     children: PropTypes.node,
   };
 
-  // Corresponds to (max-width: $no-gap-breakpoint - 1px) in SCSS
-  mediaQuery = 'matchMedia' in window && window.matchMedia('(max-width: 1206px)');
+  // Corresponds to (max-width: $no-gap-breakpoint + 285px - 1px) in SCSS
+  mediaQuery = 'matchMedia' in window && window.matchMedia('(max-width: 1174px)');
 
   state = {
     renderComposePanel: !(this.mediaQuery && this.mediaQuery.matches),
