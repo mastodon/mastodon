c709a8022ab45a621d3d352dea1a9a8af053b17e
Matt Jankowski <matt@jankowski.online>
2024-09-23 14:28:33 -0400

    Reduce factory creation (2 -> 1) in `PublishScheduledStatusWorker` spec

--- spec/workers/publish_scheduled_status_worker_spec.rb
+++ spec/workers/publish_scheduled_status_worker_spec.rb
@@ -5,32 +5,30 @@ require 'rails_helper'
 RSpec.describe PublishScheduledStatusWorker do
   subject { described_class.new }
 
-  let(:scheduled_status) { Fabricate(:scheduled_status, params: { text: 'Hello world, future!' }) }
+  let(:scheduled_status) { Fabricate(:scheduled_status, params: { text: text }) }
+  let(:text) { 'Hello world, future!' }
 
   describe 'perform' do
-    before do
-      subject.perform(scheduled_status.id)
-    end
+    before { subject.perform(scheduled_status.id) }
 
     context 'when the account is not disabled' do
-      it 'creates a status' do
-        expect(scheduled_status.account.statuses.first.text).to eq 'Hello world, future!'
-      end
+      it 'creates a new status and removes the scheduled' do
+        expect(scheduled_status.account.statuses.first.text)
+          .to eq 'Hello world, future!'
 
-      it 'removes the scheduled status' do
-        expect(ScheduledStatus.find_by(id: scheduled_status.id)).to be_nil
+        expect { scheduled_status.reload }
+          .to raise_error(ActiveRecord::RecordNotFound)
       end
     end
 
     context 'when the account is disabled' do
       let(:scheduled_status) { Fabricate(:scheduled_status, account: Fabricate(:account, user: Fabricate(:user, disabled: true))) }
 
-      it 'does not create a status' do
-        expect(Status.count).to eq 0
-      end
-
-      it 'removes the scheduled status' do
-        expect(ScheduledStatus.find_by(id: scheduled_status.id)).to be_nil
+      it 'does not create a new status and removes the scheduled status' do
+        expect(Status.count)
+          .to eq 0
+        expect { scheduled_status.reload }
+          .to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
