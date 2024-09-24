87f485051ea5f88a03fe0e232ff715562774d24e
Matt Jankowski <matt@jankowski.online>
2024-09-24 11:44:07 -0400

    Tagging model specs?

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -3,6 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Tagging do
+  describe 'Associations' do
+    it { is_expected.to belong_to(:tag).required }
+    it { is_expected.to belong_to(:taggable).required }
+  end
+
   describe 'Validations' do
     it 'validates presence of something' do
       expect(subject).to_not be_valid
