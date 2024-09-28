61c4ce0f537c3a291f38b262780ca6d452367597
Matt Jankowski <matt@jankowski.online>
2024-09-28 16:29:38 -0400

    Adjust alignment of "recommended" form badges

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -15,7 +15,7 @@ module RecommendedComponent
     return unless options[:recommended]
 
     key = options[:recommended].is_a?(Symbol) ? options[:recommended] : :recommended
-    options[:label_text] = ->(raw_label_text, _required_label_text, _label_present) { safe_join([raw_label_text, ' ', content_tag(:span, I18n.t(key, scope: 'simple_form'), class: key)]) }
+    options[:label_text] = ->(raw_label_text, _required_label_text, _label_present) { safe_join([tag.span(raw_label_text), tag.span(I18n.t(key, scope: 'simple_form'), class: key)]) }
 
     nil
   end
