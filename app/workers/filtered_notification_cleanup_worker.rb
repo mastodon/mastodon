ed8fa9929d3aab54c28d1ba8d221f39ed0798043
Matt Jankowski <matt@jankowski.online>
2024-09-30 16:25:57 -0400

    Remove unneeded `reorder(nil)` conditions

--- app/workers/filtered_notification_cleanup_worker.rb
+++ app/workers/filtered_notification_cleanup_worker.rb
@@ -4,6 +4,6 @@ class FilteredNotificationCleanupWorker
   include Sidekiq::Worker
 
   def perform(account_id, from_account_id)
-    Notification.where(account_id: account_id, from_account_id: from_account_id, filtered: true).reorder(nil).in_batches(order: :desc).delete_all
+    Notification.where(account_id: account_id, from_account_id: from_account_id, filtered: true).in_batches(order: :desc).delete_all
   end
 end
