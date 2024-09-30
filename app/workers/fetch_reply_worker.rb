895583b56ee1267dec80b74da1288da74c5486c7
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-09-29 23:24:04 -0700

    Remove recursion, separate out into separate workers/services, add limit to global maximum statuses fetched (untested, this might not work yet)
    Signed-off-by: sneakers-the-rat <sneakers-the-rat@protonmail.com>

--- app/workers/fetch_reply_worker.rb
+++ app/workers/fetch_reply_worker.rb
@@ -8,27 +8,6 @@ class FetchReplyWorker
   sidekiq_options queue: 'pull', retry: 3
 
   def perform(child_url, options = {})
-    all_replies = options.delete('all_replies')
-
-    status = FetchRemoteStatusService.new.call(child_url, **options.deep_symbolize_keys)
-
-    # asked to fetch replies recursively - do the second-level calls async
-    if all_replies && status
-      json_status = fetch_resource(status.uri, true)
-
-      collection = json_status['replies']
-      unless collection.nil?
-        # if expanding replies recursively, spread out the recursive calls
-        ActivityPub::FetchRepliesWorker.perform_in(
-          rand(1..30).seconds,
-          status.id,
-          collection,
-          {
-            allow_synchronous_requests: true,
-            all_replies: true,
-          }
-        )
-      end
-    end
+    FetchRemoteStatusService.new.call(child_url, **options.deep_symbolize_keys)
   end
 end
