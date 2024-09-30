fbbd4357f5dd71085aa41b7bdc6020e80867750e
Claire <claire.github-309c@sitedethib.com>
2024-09-30 11:29:40 +0200

    Change regexp timeout feature to be opt-in

--- config/initializers/regexp.rb
+++ config/initializers/regexp.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-# 2s is a fairly high default, but that should account for slow servers under load
-Regexp.timeout = ENV.fetch('REGEXP_TIMEOUT', 2).to_f if Regexp.respond_to?(:timeout=)
+Regexp.timeout = ENV['REGEXP_TIMEOUT'].to_f if Regexp.respond_to?(:timeout=) && ENV['REGEXP_TIMEOUT']
