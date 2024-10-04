dd4f2d3ee6318f8c78aab8bfdab6b722a47c0b83
Claire <claire.github-309c@sitedethib.com>
2024-10-04 16:55:44 +0200

    Fix media gallery items having incorrect borders when hidden (#32257)

--- app/javascript/mastodon/components/media_gallery.jsx
+++ app/javascript/mastodon/components/media_gallery.jsx
@@ -336,13 +336,13 @@ class MediaGallery extends PureComponent {
 
     return (
       <div className={`media-gallery media-gallery--layout-${size}`} style={style} ref={this.handleRef}>
+        {children}
+
         {(!visible || uncached) && (
           <div className={classNames('spoiler-button', { 'spoiler-button--click-thru': uncached })}>
             {spoilerButton}
           </div>
         )}
-
-        {children}
 
         {(visible && !uncached) && (
           <div className='media-gallery__actions'>
