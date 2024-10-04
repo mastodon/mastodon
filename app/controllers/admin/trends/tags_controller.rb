4fe7f213a64e22a2912f13a8e5d6ac415125b0a1
Matt Jankowski <matt@jankowski.online>
2024-10-04 08:29:43 -0400

    Use `async_count` in more view locations (#32086)

--- app/controllers/admin/trends/tags_controller.rb
+++ app/controllers/admin/trends/tags_controller.rb
@@ -4,6 +4,7 @@ class Admin::Trends::TagsController < Admin::BaseController
   def index
     authorize :tag, :review?
 
+    @pending_tags_count = Tag.pending_review.async_count
     @tags = filtered_tags.page(params[:page])
     @form = Trends::TagBatch.new
   end
