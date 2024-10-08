729bbcaca79dfe668c0c413f38a04ccf1c3c803c
Christian Schmidt <github@chsc.dk>
2024-10-08 19:40:32 +0200

    Performance

--- app/services/verify_link_service.rb
+++ app/services/verify_link_service.rb
@@ -26,7 +26,7 @@ class VerifyLinkService < BaseService
   def link_back_present?
     return false if @body.blank?
 
-    links = Nokogiri::HTML5(@body).xpath('(//a|//link)[nokogiri:link_rel_include(@rel, "me")]', NokogiriHandler)
+    links = Nokogiri::HTML5(@body).xpath('(//a|//link)[@rel][nokogiri:link_rel_include(@rel, "me")]', NokogiriHandler)
 
     if links.any? { |link| link['href']&.downcase == @link_back.downcase }
       true
