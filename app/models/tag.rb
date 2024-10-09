0d9812792b427fa58dc6a9ff85e477f31a4ed7fc
Matt Jankowski <matt@jankowski.online>
2024-10-09 18:07:29 -0400

    Add `Tag.discoverable_public_status_accounts` association for use in welcome mailer

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -32,6 +32,8 @@ class Tag < ApplicationRecord
   has_many :featured_tags, dependent: :destroy, inverse_of: :tag
   has_many :followers, through: :passive_relationships, source: :account
 
+  has_many :discoverable_public_status_accounts, -> { merge(Status.public_visibility).merge(Account.discoverable) }, through: :statuses, source: :account
+
   HASHTAG_SEPARATORS = "_\u00B7\u30FB\u200c"
   HASHTAG_FIRST_SEQUENCE_CHUNK_ONE = "[[:word:]_][[:word:]#{HASHTAG_SEPARATORS}]*[[:alpha:]#{HASHTAG_SEPARATORS}]"
   HASHTAG_FIRST_SEQUENCE_CHUNK_TWO = "[[:word:]#{HASHTAG_SEPARATORS}]*[[:word:]_]"
