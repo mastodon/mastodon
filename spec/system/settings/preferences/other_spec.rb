0b790f24c72cd7287a1e6575633863373f3746f6
Matt Jankowski <matt@jankowski.online>
2024-10-10 11:23:32 -0400

    Convert `settings/preferences/*` controller specs to system specs

---
+++ spec/system/settings/preferences/other_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Settings preferences other page' do
+  let(:user) { Fabricate :user }
+
+  before { sign_in user }
+
+  it 'Views and updates user prefs' do
+    visit settings_preferences_other_path
+
+    expect(page)
+      .to have_private_cache_control
+
+    check LanguagesHelper::SUPPORTED_LOCALES[:es].last
+    check LanguagesHelper::SUPPORTED_LOCALES[:fr].last
+    check mark_sensitive_field
+
+    expect { save_changes }
+      .to change { user.reload.chosen_languages }.to(%w(es fr))
+      .and(change { user.reload.settings.default_sensitive }.to(true))
+    expect(page)
+      .to have_title(I18n.t('settings.preferences'))
+  end
+
+  def save_changes
+    within('form') { click_on submit_button }
+  end
+
+  def mark_sensitive_field
+    I18n.t('simple_form.labels.defaults.setting_default_sensitive')
+  end
+end
