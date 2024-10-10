c0d100540d24f2bfb24a7ac48ae614dcac8c0fa1
Matt Jankowski <matt@jankowski.online>
2024-10-10 16:26:41 -0400

    Combine examples

--- spec/helpers/formatting_helper_spec.rb
+++ spec/helpers/formatting_helper_spec.rb
@@ -6,19 +6,15 @@ RSpec.describe FormattingHelper do
   include Devise::Test::ControllerHelpers
 
   describe '#rss_status_content_format' do
+    subject { helper.rss_status_content_format(status) }
+
     let(:status) { Fabricate(:status, text: 'Hello world<>', spoiler_text: 'This is a spoiler<>', poll: Fabricate(:poll, options: %w(Yes<> No))) }
-    let(:html) { helper.rss_status_content_format(status) }
 
-    it 'renders the spoiler text' do
-      expect(html).to include('<p>This is a spoiler&lt;&gt;</p><hr>')
-    end
-
-    it 'renders the status text' do
-      expect(html).to include('<p>Hello world&lt;&gt;</p>')
-    end
-
-    it 'renders the poll' do
-      expect(html).to include('<radio disabled="disabled">Yes&lt;&gt;</radio><br>')
+    it 'renders the formatted elements' do
+      expect(subject)
+        .to include('<p>This is a spoiler&lt;&gt;</p><hr>')
+        .and include('<p>Hello world&lt;&gt;</p>')
+        .and include('<radio disabled="disabled">Yes&lt;&gt;</radio><br>')
     end
   end
 end
