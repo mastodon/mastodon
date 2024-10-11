30522d6b84195685fc61697f8873af9b76f602bc
Matt Jankowski <matt@jankowski.online>
2024-10-10 20:35:35 -0400

    Remove remnants of embed views

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 module StatusesHelper
-  EMBEDDED_CONTROLLER = 'statuses'
-  EMBEDDED_ACTION = 'embed'
-
   VISIBLITY_ICONS = {
     public: 'globe',
     unlisted: 'lock_open',
@@ -60,16 +57,8 @@ module StatusesHelper
     components.compact_blank.join("\n\n")
   end
 
-  def stream_link_target
-    embedded_view? ? '_blank' : nil
-  end
-
   def visibility_icon(status)
     VISIBLITY_ICONS[status.visibility.to_sym]
-  end
-
-  def embedded_view?
-    params[:controller] == EMBEDDED_CONTROLLER && params[:action] == EMBEDDED_ACTION
   end
 
   def prefers_autoplay?
--- spec/helpers/accounts_helper_spec.rb
+++ spec/helpers/accounts_helper_spec.rb
@@ -3,16 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe AccountsHelper do
-  def set_not_embedded_view
-    params[:controller] = "not_#{StatusesHelper::EMBEDDED_CONTROLLER}"
-    params[:action] = "not_#{StatusesHelper::EMBEDDED_ACTION}"
-  end
-
-  def set_embedded_view
-    params[:controller] = StatusesHelper::EMBEDDED_CONTROLLER
-    params[:action] = StatusesHelper::EMBEDDED_ACTION
-  end
-
   describe '#display_name' do
     it 'uses the display name when it exists' do
       account = Account.new(display_name: 'Display', username: 'Username')
@@ -28,9 +18,8 @@ RSpec.describe AccountsHelper do
   end
 
   describe '#acct' do
-    it 'is fully qualified for embedded local accounts' do
+    it 'is fully qualified for local accounts' do
       allow(Rails.configuration.x).to receive(:local_domain).and_return('local_domain')
-      set_embedded_view
       account = Account.new(domain: nil, username: 'user')
 
       acct = helper.acct(account)
@@ -38,32 +27,12 @@ RSpec.describe AccountsHelper do
       expect(acct).to eq '@user@local_domain'
     end
 
-    it 'is fully qualified for embedded foreign accounts' do
-      set_embedded_view
+    it 'is fully qualified for remote accounts' do
       account = Account.new(domain: 'foreign_server.com', username: 'user')
 
       acct = helper.acct(account)
 
       expect(acct).to eq '@user@foreign_server.com'
-    end
-
-    it 'is fully qualified for non embedded foreign accounts' do
-      set_not_embedded_view
-      account = Account.new(domain: 'foreign_server.com', username: 'user')
-
-      acct = helper.acct(account)
-
-      expect(acct).to eq '@user@foreign_server.com'
-    end
-
-    it 'is fully qualified for non embedded local accounts' do
-      allow(Rails.configuration.x).to receive(:local_domain).and_return('local_domain')
-      set_not_embedded_view
-      account = Account.new(domain: nil, username: 'user')
-
-      acct = helper.acct(account)
-
-      expect(acct).to eq '@user@local_domain'
     end
   end
 end
