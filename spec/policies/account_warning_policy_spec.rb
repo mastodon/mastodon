46e6389007380defc4c58c21701ab4bba6734081
Matt Jankowski <matt@jankowski.online>
2024-10-11 11:12:02 -0400

    Move require to rails helper

--- spec/policies/account_warning_policy_spec.rb
+++ spec/policies/account_warning_policy_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'pundit/rspec'
 
 RSpec.describe AccountWarningPolicy do
   subject { described_class }
--- spec/policies/admin/status_policy_spec.rb
+++ spec/policies/admin/status_policy_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'pundit/rspec'
 
 RSpec.describe Admin::StatusPolicy do
   let(:policy) { described_class }
