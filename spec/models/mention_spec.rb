93b61f8468fac59b2b40c7d1da1e079c15d272d7
Matt Jankowski <matt@jankowski.online>
2024-10-12 19:40:16 -0400

    Add coverage for account uniqueness

--- spec/models/mention_spec.rb
+++ spec/models/mention_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Mention do
     it { is_expected.to belong_to(:account).required }
     it { is_expected.to belong_to(:status).required }
   end
+
+  describe 'Validations' do
+    subject { Fabricate.build :mention }
+
+    it { is_expected.to validate_uniqueness_of(:account_id).scoped_to(:status_id) }
+  end
 end
