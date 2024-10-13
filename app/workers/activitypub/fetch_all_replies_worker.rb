f803230ab3a4917fb60591cc46a990f3c8e44a46
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-10-12 21:02:31 -0700

    Working version of fetch all replies service with global maximum on fetching
    Signed-off-by: sneakers-the-rat <sneakers-the-rat@protonmail.com>

--- app/workers/activitypub/fetch_all_replies_worker.rb
+++ app/workers/activitypub/fetch_all_replies_worker.rb
@@ -7,29 +7,39 @@
 class ActivityPub::FetchAllRepliesWorker
   include Sidekiq::Worker
   include ExponentialBackoff
+  include JsonLdHelper
 
   sidekiq_options queue: 'pull', retry: 3
 
   # Global max replies to fetch per request
   MAX_REPLIES = 1000
 
-  def perform(parent_status_id, options = {})
+  def perform(parent_status_id, current_account_id = nil, options = {})
     @parent_status = Status.find(parent_status_id)
-    @current_account_id = options.fetch(:current_account_id, nil)
-    @current_account = @current_account_id.nil? ? nil : Account.find(id: @current_account_id)
+    @current_account_id = current_account_id
+    @current_account = @current_account_id.nil? ? nil : Account.find(@current_account_id)
+    Rails.logger.debug { "FetchAllRepliesWorker - #{parent_status_id}: Fetching all replies for status: #{@parent_status}" }
 
-    all_replies = get_replies(@parent_status.uri)
+    all_replies = get_replies(@parent_status.uri, options)
     got_replies = all_replies.length
     until all_replies.empty? || got_replies >= MAX_REPLIES
-      new_replies = get_replies(all_replies.pop)
+      next_reply = all_replies.pop
+      next if next_reply.nil?
+
+      new_replies = get_replies(next_reply, options)
+      next if new_replies.nil?
+
       got_replies += new_replies.length
-      all_replies << new_replies
+      all_replies.concat(new_replies)
     end
+
+    Rails.logger.debug { "FetchAllRepliesWorker - #{parent_status_id}: fetched #{got_replies} replies" }
+    got_replies
   end
 
   private
 
-  def get_replies(status_uri)
+  def get_replies(status_uri, options = {})
     replies_uri = get_replies_uri(status_uri)
     return if replies_uri.nil?
 
@@ -37,9 +47,14 @@ class ActivityPub::FetchAllRepliesWorker
   end
 
   def get_replies_uri(parent_status_uri)
-    json_status = fetch_resource(parent_status_uri, true, @current_account)
-    replies_uri = json_status['replies']
-    Rails.logger.debug { "Could not find replies uri for status URI: #{parent_status_uri}" } if replies_uri.nil?
-    replies_uri
+    begin
+      json_status = fetch_resource(parent_status_uri, true, @current_account)
+      replies_uri = json_status['replies']
+      Rails.logger.debug { "FetchAllRepliesWorker - #{@parent_status_id}: replies URI was nil" } if replies_uri.nil?
+      replies_uri
+    rescue => e
+      Rails.logger.error { "FetchAllRepliesWorker - #{@parent_status_id}: Got exception while resolving replies URI: #{e} - #{e.message}" }
+      nil
+    end
   end
 end
