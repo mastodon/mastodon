6a1c0af9ef86fd07a2bcd3d67865e7c197ac82e3
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-10-12 21:02:31 -0700

    Working version of fetch all replies service with global maximum on fetching
    Signed-off-by: sneakers-the-rat <sneakers-the-rat@protonmail.com>

--- app/models/concerns/status/fetch_replies_concern.rb
+++ app/models/concerns/status/fetch_replies_concern.rb
@@ -20,7 +20,7 @@ module Status::FetchRepliesConcern
 
   def should_fetch_replies?
     # we aren't brand new, and we haven't fetched replies since the debounce window
-    !local? && created_at <= 10.minutes.ago && (
+    !local? && created_at <= CREATED_RECENTLY_DEBOUNCE.ago && (
       fetched_replies_at.nil? || fetched_replies_at <= FETCH_REPLIES_DEBOUNCE.ago
     )
   end
