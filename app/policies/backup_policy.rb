d2460c347b5a58f6d36206d9622a6ce0a57660b8
Matt Jankowski <matt@jankowski.online>
2024-10-15 12:38:34 -0400

    Extract private method in backup policy

--- app/policies/backup_policy.rb
+++ app/policies/backup_policy.rb
@@ -4,6 +4,16 @@ class BackupPolicy < ApplicationPolicy
   MIN_AGE = 6.days
 
   def create?
-    user_signed_in? && current_user.backups.where(created_at: MIN_AGE.ago..).count.zero?
+    user_signed_in? && eligible_for_backup?
+  end
+
+  private
+
+  def eligible_for_backup?
+    current_user
+      .backups
+      .where(created_at: MIN_AGE.ago..)
+      .count
+      .zero?
   end
 end
