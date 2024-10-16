d01391c358a0b50f5824f9624c7830782ca026b0
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2024-10-16 21:21:11 +0200

    Add userinfo_endpoint to .well-known/oauth-authorization-server metadata

--- app/serializers/oauth_metadata_serializer.rb
+++ app/serializers/oauth_metadata_serializer.rb
@@ -2,7 +2,7 @@
 
 class OauthMetadataSerializer < ActiveModel::Serializer
   attributes :issuer, :authorization_endpoint, :token_endpoint,
-             :revocation_endpoint, :scopes_supported,
+             :revocation_endpoint, :userinfo_endpoint, :scopes_supported,
              :response_types_supported, :response_modes_supported,
              :grant_types_supported, :token_endpoint_auth_methods_supported,
              :code_challenge_methods_supported,
