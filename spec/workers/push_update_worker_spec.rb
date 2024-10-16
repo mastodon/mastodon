1663078e12b1e28a927699dc4d259d46d12babd2
Matt Jankowski <matt@jankowski.online>
2024-10-16 08:14:44 -0400

    worker spec - push update

--- spec/workers/push_update_worker_spec.rb
+++ spec/workers/push_update_worker_spec.rb
@@ -6,11 +6,31 @@ RSpec.describe PushUpdateWorker do
   let(:worker) { described_class.new }
 
   describe 'perform' do
-    it 'runs without error for missing record' do
-      account_id = nil
-      status_id = nil
+    context 'with missing values' do
+      it 'runs without error' do
+        expect { worker.perform(nil, nil) }
+          .to_not raise_error
+      end
+    end
 
-      expect { worker.perform(account_id, status_id) }.to_not raise_error
+    context 'with valid records' do
+      let(:account) { Fabricate :account }
+      let(:status) { Fabricate :status }
+
+      before { allow(redis).to receive(:publish) }
+
+      it 'pushes message to timeline' do
+        expect { worker.perform(account.id, status.id) }
+          .to_not raise_error
+
+        expect(redis)
+          .to have_received(:publish)
+          .with(redis_key, anything)
+      end
+
+      def redis_key
+        "timeline:#{account.id}"
+      end
     end
   end
 end
