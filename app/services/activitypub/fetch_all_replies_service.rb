70968032e359b71eca1ec8665c4cd86be736359c
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-10-21 12:12:24 -0700

    apply changes from patches that couldnt be auto-applied

--- app/services/activitypub/fetch_all_replies_service.rb
+++ app/services/activitypub/fetch_all_replies_service.rb
@@ -4,11 +4,12 @@ class ActivityPub::FetchAllRepliesService < ActivityPub::FetchRepliesService
   include JsonLdHelper
 
   # Limit of replies to fetch per status
-  MAX_REPLIES = 500
+  MAX_REPLIES = (ENV['FETCH_REPLIES_MAX_SINGLE'] || 500).to_i
 
   def call(collection_or_uri, allow_synchronous_requests: true, request_id: nil)
     @allow_synchronous_requests = allow_synchronous_requests
     @filter_by_host = false
+    @collection_or_uri = collection_or_uri
 
     @items = collection_items(collection_or_uri)
     @items = filtered_replies
@@ -40,6 +41,9 @@ class ActivityPub::FetchAllRepliesService < ActivityPub::FetchRepliesService
     Status.where(uri: uris).should_fetch_replies.touch_all(:fetched_replies_at)
 
     # Reject all statuses that we already have in the db
-    uris.reject { |uri| dont_update.include?(uri) }.take(MAX_REPLIES)
+    uris = uris.reject { |uri| dont_update.include?(uri) }.take(MAX_REPLIES)
+
+    Rails.logger.debug { "FetchAllRepliesService - #{@collection_or_uri}: Fetching filtered statuses: #{uris}" }
+    uris
   end
 end
