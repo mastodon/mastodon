a3bcbba3d8409f534ed5710a6891061e8222ac93
Matt Jankowski <matt@jankowski.online>
2024-10-21 10:24:47 -0400

    Prefer custom middleware

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,9 +19,6 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV['CI'].present?
 
-  # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{1.hour.to_i}" }
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
