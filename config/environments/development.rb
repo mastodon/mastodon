9cebf547ae11b34647b5e25f8dddb57a708fffa4
Matt Jankowski <matt@jankowski.online>
2024-10-21 10:23:07 -0400

    Remove dupe value

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,8 +50,6 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_options = { from: 'notifications@localhost' }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -74,7 +72,7 @@ Rails.application.configure do
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  config.action_view.annotate_rendered_view_with_filenames = true
+  # config.action_view.annotate_rendered_view_with_filenames = true
 
   config.action_mailer.default_options = { from: 'notifications@localhost' }
 
