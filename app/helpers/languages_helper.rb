02addb3b9641f4590c9d549352a79df89dc77f94
Eugene Alvin Villar <seav80@gmail.com>
2024-10-22 16:41:40 +0800

    Fix tl language native name (#32606)

--- app/helpers/languages_helper.rb
+++ app/helpers/languages_helper.rb
@@ -161,7 +161,7 @@ module LanguagesHelper
     th: ['Thai', 'ไทย'].freeze,
     ti: ['Tigrinya', 'ትግርኛ'].freeze,
     tk: ['Turkmen', 'Türkmen'].freeze,
-    tl: ['Tagalog', 'Wikang Tagalog'].freeze,
+    tl: ['Tagalog', 'Tagalog'].freeze,
     tn: ['Tswana', 'Setswana'].freeze,
     to: ['Tonga', 'faka Tonga'].freeze,
     tr: ['Turkish', 'Türkçe'].freeze,
