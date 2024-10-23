d346f6c39b45956ba708b13c265d7c0ef89a9bc8
Matt Jankowski <matt@jankowski.online>
2024-10-23 13:44:28 -0400

    Remove unused `ApplicationHelper#visibility_icon` helper method

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -227,28 +227,6 @@ RSpec.describe ApplicationHelper do
     end
   end
 
-  describe 'visibility_icon' do
-    it 'returns a globe icon for a public visible status' do
-      result = helper.visibility_icon Status.new(visibility: 'public')
-      expect(result).to match(/globe/)
-    end
-
-    it 'returns an unlock icon for a unlisted visible status' do
-      result = helper.visibility_icon Status.new(visibility: 'unlisted')
-      expect(result).to match(/lock_open/)
-    end
-
-    it 'returns a lock icon for a private visible status' do
-      result = helper.visibility_icon Status.new(visibility: 'private')
-      expect(result).to match(/lock/)
-    end
-
-    it 'returns an at icon for a direct visible status' do
-      result = helper.visibility_icon Status.new(visibility: 'direct')
-      expect(result).to match(/alternate_email/)
-    end
-  end
-
   describe 'title' do
     it 'returns site title on production environment' do
       Setting.site_title = 'site title'
