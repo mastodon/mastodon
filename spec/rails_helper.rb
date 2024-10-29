df3b9547202c07ec7ec7f6af8295deb272c6f5ca
Matt Jankowski <matt@jankowski.online>
2024-10-29 11:35:25 -0400

    Add `DomainHelpers` spec support module for DNS/MX stub (#32690)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -113,6 +113,7 @@ RSpec.configure do |config|
   config.include ActiveSupport::Testing::TimeHelpers
   config.include Chewy::Rspec::Helpers
   config.include Redisable
+  config.include DomainHelpers
   config.include ThreadingHelpers
   config.include SignedRequestHelpers, type: :request
   config.include CommandLineHelpers, type: :cli
