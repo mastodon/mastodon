daeb2b67f5c64b0f133b82a4ec05192ec4f3c214
Matt Jankowski <matt@jankowski.online>
2024-10-29 09:29:22 -0400

    Add `locales` class method to `RankedTrend` concern

--- spec/models/status_trend_spec.rb
+++ spec/models/status_trend_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe StatusTrend do
     it { is_expected.to belong_to(:account).required }
     it { is_expected.to belong_to(:status).required }
   end
+
+  describe '.locales' do
+    before do
+      Fabricate :status_trend, language: 'en'
+      Fabricate :status_trend, language: 'en'
+      Fabricate :status_trend, language: 'es'
+    end
+
+    it 'returns unique set of languages' do
+      expect(described_class.locales)
+        .to eq(['en', 'es'])
+    end
+  end
 end
