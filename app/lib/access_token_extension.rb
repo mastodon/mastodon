03e681d224bfacb78a834dc346d0b727df642023
Matt Jankowski <matt@jankowski.online>
2024-10-30 15:37:58 -0400

    Replace `Oj` with plain `JSON` across app/

--- app/lib/access_token_extension.rb
+++ app/lib/access_token_extension.rb
@@ -24,6 +24,6 @@ module AccessTokenExtension
   end
 
   def push_to_streaming_api
-    redis.publish("timeline:access_token:#{id}", Oj.dump(event: :kill)) if revoked? || destroyed?
+    redis.publish("timeline:access_token:#{id}", JSON.dump(event: :kill)) if revoked? || destroyed?
   end
 end
