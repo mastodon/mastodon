b072186e2376c69639fb825c163c6b4138233866
Matt Jankowski <matt@jankowski.online>
2024-11-03 09:28:44 -0500

    Add coverage for `ActivityPub::RemoveSerializer`

---
+++ spec/serializers/activitypub/remove_serializer_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ActivityPub::RemoveSerializer do
+  describe '.serializer_for' do
+    subject { described_class.serializer_for(model, {}) }
+
+    context 'with a Status model' do
+      let(:model) { Status.new }
+
+      it { is_expected.to eq(described_class::UriSerializer) }
+    end
+
+    context 'with a FeaturedTag model' do
+      let(:model) { FeaturedTag.new }
+
+      it { is_expected.to eq(ActivityPub::HashtagSerializer) }
+    end
+
+    context 'with an Array' do
+      let(:model) { [] }
+
+      it { is_expected.to eq(ActiveModel::Serializer::CollectionSerializer) }
+    end
+  end
+end
