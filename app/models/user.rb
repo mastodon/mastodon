77ebb32f6a4b1ba74c20225ea5b6f2abdaf6fb6b
Matt Jankowski <matt@jankowski.online>
2024-11-12 11:20:09 -0500

    Extract `User::Confirmation` concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -188,6 +188,20 @@ class User < ApplicationRecord
     account_id
   end
 
+  def confirm
+    wrap_email_confirmation do
+      super
+    end
+  end
+
+  # Mark current email as confirmed, bypassing Devise
+  def mark_email_as_confirmed!
+    wrap_email_confirmation do
+      skip_confirmation!
+      save!
+    end
+  end
+
   def update_sign_in!(new_sign_in: false)
     old_current = current_sign_in_at
     new_current = Time.now.utc
@@ -399,6 +413,25 @@ class User < ApplicationRecord
   def grant_approval_on_confirmation?
     # Re-check approval on confirmation if the server has switched to open registrations
     open_registrations? && !sign_up_from_ip_requires_approval? && !sign_up_email_requires_approval?
+  end
+
+  def wrap_email_confirmation
+    new_user      = !confirmed?
+    self.approved = true if grant_approval_on_confirmation?
+
+    yield
+
+    if new_user
+      # Avoid extremely unlikely race condition when approving and confirming
+      # the user at the same time
+      reload unless approved?
+
+      if approved?
+        prepare_new_user!
+      else
+        notify_staff_about_pending_account!
+      end
+    end
   end
 
   def sign_up_from_ip_requires_approval?
