ae4b6ce8a4e41534f65d8a9e5fc8208a4772c4ed
Matt Jankowski <matt@jankowski.online>
2024-11-15 18:04:06 -0500

    Add `url` var

--- spec/workers/activitypub/delivery_worker_spec.rb
+++ spec/workers/activitypub/delivery_worker_spec.rb
@@ -5,24 +5,25 @@ require 'rails_helper'
 RSpec.describe ActivityPub::DeliveryWorker do
   include RoutingHelper
 
-  let(:sender)  { Fabricate(:account) }
+  let(:sender) { Fabricate(:account) }
   let(:payload) { 'test' }
+  let(:url) { 'https://example.com/api' }
 
   before do
-    allow(sender).to receive(:remote_followers_hash).with('https://example.com/api').and_return('somehash')
+    allow(sender).to receive(:remote_followers_hash).with(url).and_return('somehash')
     allow(Account).to receive(:find).with(sender.id).and_return(sender)
   end
 
   describe 'perform' do
     it 'performs a request' do
-      stub_request(:post, 'https://example.com/api').to_return(status: 200)
-      subject.perform(payload, sender.id, 'https://example.com/api', { synchronize_followers: true })
-      expect(a_request(:post, 'https://example.com/api').with(headers: { 'Collection-Synchronization' => "collectionId=\"#{account_followers_url(sender)}\", digest=\"somehash\", url=\"#{account_followers_synchronization_url(sender)}\"" })).to have_been_made.once
+      stub_request(:post, url).to_return(status: 200)
+      subject.perform(payload, sender.id, url, { synchronize_followers: true })
+      expect(a_request(:post, url).with(headers: { 'Collection-Synchronization' => "collectionId=\"#{account_followers_url(sender)}\", digest=\"somehash\", url=\"#{account_followers_synchronization_url(sender)}\"" })).to have_been_made.once
     end
 
     it 'raises when request fails' do
-      stub_request(:post, 'https://example.com/api').to_return(status: 500)
-      expect { subject.perform(payload, sender.id, 'https://example.com/api') }.to raise_error Mastodon::UnexpectedResponseError
+      stub_request(:post, url).to_return(status: 500)
+      expect { subject.perform(payload, sender.id, url) }.to raise_error Mastodon::UnexpectedResponseError
     end
   end
 end
