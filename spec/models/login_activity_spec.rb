22e9556de24652e592d885d20b6c6ee0208677b4
Matt Jankowski <matt@jankowski.online>
2024-11-18 12:28:11 -0500

    Add basic coverage for `LoginActivity` model

---
+++ spec/models/login_activity_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe LoginActivity do
+  describe 'Associations' do
+    it { is_expected.to belong_to(:user).required }
+  end
+
+  describe 'Validations' do
+    subject { Fabricate.build :login_activity }
+
+    it { is_expected.to define_enum_for(:authentication_method).backed_by_column_of_type(:string) }
+  end
+end
