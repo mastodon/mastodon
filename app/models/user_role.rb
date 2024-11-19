dd1b64a93ef39b5c8adb24838218cab1557be35a
Matt Jankowski <matt@jankowski.online>
2024-11-19 08:36:47 -0500

    Add `UserRole#enables_delivery?` method for notification check

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -142,6 +142,10 @@ class UserRole < ApplicationRecord
     other_role.nil? || position > other_role.position
   end
 
+  def enables_delivery?(role)
+    overrides?(role) && highlighted? && can?(*Flags::CATEGORIES[:moderation])
+  end
+
   def computed_permissions
     # If called on the everyone role, no further computation needed
     return permissions if everyone?
