3d1eb5e294f3b7b966b72ee27896d98873e0a51a
Christian Schmidt <github@chsc.dk>
2024-11-22 18:12:39 +0100

    Rename "Import and export"

--- config/navigation.rb
+++ config/navigation.rb
@@ -34,9 +34,9 @@ SimpleNavigation::Configuration.run do |navigation|
       s.item :authorized_apps, safe_join([material_symbol('list_alt'), t('settings.authorized_apps')]), oauth_authorized_applications_path, if: -> { !self_destruct }
     end
 
-    n.item :data, safe_join([material_symbol('cloud_sync'), t('settings.import_and_export')]), settings_export_path do |s|
-      s.item :import, safe_join([material_symbol('cloud_upload'), t('settings.import')]), settings_imports_path, highlights_on: %r{/settings/imports}, if: -> { current_user.functional? && !self_destruct }
+    n.item :data, safe_join([material_symbol('cloud_sync'), t('settings.export_and_import')]), settings_export_path do |s|
       s.item :export, safe_join([material_symbol('cloud_download'), t('settings.export')]), settings_export_path
+      s.item :import, safe_join([material_symbol('cloud_upload'), t('settings.import')]), settings_imports_path, highlights_on: %r{/settings/imports}, if: -> { current_user.functional? && !self_destruct }
     end
 
     n.item :user_invites, safe_join([material_symbol('person_add'), t('invites.title')]), invites_path, if: -> { current_user.can?(:invite_users) && current_user.functional? && !self_destruct }
