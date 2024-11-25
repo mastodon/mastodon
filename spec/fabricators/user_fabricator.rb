f51f9717fa57c1525a3d1e90a2116a95f795f850
Claire <claire.github-309c@sitedethib.com>
2024-11-25 16:54:18 +0100

    Fix direct inbox delivery pushing posts into inactive followers' timelines (#33067)

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -3,5 +3,6 @@ Fabricator(:user) do
   email        { sequence(:email) { |i| "#{i}#{Faker::Internet.email}" } }
   password     "123456789"
   confirmed_at { Time.zone.now }
+  current_sign_in_at { Time.zone.now }
   agreement    true
 end
