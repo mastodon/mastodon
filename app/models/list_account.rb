48f3ed738f6688e75bfcb24430f0a26a5ef0d6e5
Eugen Rochko <eugen@zeonfederated.com>
2024-11-28 11:13:06 +0100

    Fix list error messages (#33082)

--- app/models/list_account.rb
+++ app/models/list_account.rb
@@ -25,17 +25,16 @@ class ListAccount < ApplicationRecord
   private
 
   def set_follow
-    self.follow = Follow.find_by!(account_id: list.account_id, target_account_id: account.id)
-  rescue ActiveRecord::RecordNotFound
-    self.follow_request = FollowRequest.find_by!(account_id: list.account_id, target_account_id: account.id)
+    self.follow = Follow.find_by(account_id: list.account_id, target_account_id: account.id)
+    self.follow_request = FollowRequest.find_by(account_id: list.account_id, target_account_id: account.id) if follow.nil?
   end
 
   def validate_relationship
-    return if list.account_id == account_id
+    return if list_owner_account_is_account?
 
-    errors.add(:account_id, 'follow relationship missing') if follow_id.nil? && follow_request_id.nil?
-    errors.add(:follow, 'mismatched accounts') if follow_id.present? && follow.target_account_id != account_id
-    errors.add(:follow_request, 'mismatched accounts') if follow_request_id.present? && follow_request.target_account_id != account_id
+    errors.add(:account_id, :must_be_following) if follow_id.nil? && follow_request_id.nil?
+    errors.add(:follow, :invalid) if follow_id.present? && follow.target_account_id != account_id
+    errors.add(:follow_request, :invalid) if follow_request_id.present? && follow_request.target_account_id != account_id
   end
 
   def list_owner_account_is_account?
