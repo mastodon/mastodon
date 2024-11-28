093f8a06ca9af38d5e0ecc44f2eb4e976712d1a6
Nik Clayton <nik@ngo.org.uk>
2024-11-28 13:55:52 +0100

    Revert change to account_serializer.rb

--- app/serializers/rest/account_serializer.rb
+++ app/serializers/rest/account_serializer.rb
@@ -93,7 +93,7 @@ class REST::AccountSerializer < ActiveModel::Serializer
   end
 
   def last_status_at
-    object.last_status_at&.to_date&.rfc3339
+    object.last_status_at&.to_date&.iso8601
   end
 
   def display_name
