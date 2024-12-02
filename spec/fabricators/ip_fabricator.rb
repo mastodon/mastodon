acddde2eb99fa3f50e7c9a6f81d161bb0da4d578
Nik Clayton <nik@ngo.org.uk>
2024-12-02 15:17:23 +0100

    Construct UserIp directly, ditch fabricator

--- spec/fabricators/ip_fabricator.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Fabricator(:ip) do
-  ip { '10.0.0.1' }
-  used_at { DateTime.new(2024, 11, 28, 16, 20, 0) }
-end
--- spec/serializers/rest/admin/ip_serializer_spec.rb
+++ spec/serializers/rest/admin/ip_serializer_spec.rb
@@ -5,9 +5,9 @@ require 'rails_helper'
 RSpec.describe REST::Admin::IpSerializer do
   subject { serialized_record_json(record, described_class) }
 
-  let(:record) { Fabricate(:ip) }
+  let(:record) { UserIp.new(used_at: 3.days.ago) }
 
-  context 'when used_at is populated', pending: 'No class found for ip' do
+  context 'when used_at is populated' do
     it 'parses as RFC 3339 datetime' do
       expect { DateTime.rfc3339(subject['used_at']) }.to_not raise_error
     end
