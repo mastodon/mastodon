067add75876ec1c3c83abade358b301e9bc08c97
Nik Clayton <nik@ngo.org.uk>
2024-12-02 21:56:08 +0100

    Remove unnecessary admin_report_fabricator.rb

--- spec/fabricators/admin_report_fabricator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Fabricator(:admin_report) do
-  account { Fabricate.build(:account) }
-  target_account  { Fabricate.build(:account) }
-  comment         'You nasty'
-  action_taken_at nil
-end
