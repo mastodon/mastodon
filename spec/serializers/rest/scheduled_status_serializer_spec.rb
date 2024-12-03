db002824a3f1a8f9e339755312aa4a8925996c0d
Matt Jankowski <matt@jankowski.online>
2024-12-03 14:15:46 -0500

    Fix application_id erroneous inclusion in scheduled status serializer

--- spec/serializers/rest/scheduled_status_serializer_spec.rb
+++ spec/serializers/rest/scheduled_status_serializer_spec.rb
@@ -11,11 +11,18 @@ RSpec.describe REST::ScheduledStatusSerializer do
   end
 
   let(:account) { Fabricate(:account) }
-  let(:scheduled_status) { Fabricate.build(:scheduled_status, scheduled_at: 4.minutes.from_now, account: account) }
+  let(:scheduled_status) { Fabricate.build(:scheduled_status, scheduled_at: 4.minutes.from_now, account: account, params: { application_id: 123 }) }
 
-  context 'with scheduled_at' do
+  describe 'serialization' do
     it 'is serialized as RFC 3339 datetime' do
-      expect { DateTime.rfc3339(subject['scheduled_at']) }.to_not raise_error
+      expect { DateTime.rfc3339(subject['scheduled_at']) }
+        .to_not raise_error
+    end
+
+    it 'returns expected values and removes application_id from params' do
+      expect(subject.deep_symbolize_keys)
+        .to include(scheduled_at: be_a(String))
+        .and include(params: not_include(:application_id))
     end
   end
 end
