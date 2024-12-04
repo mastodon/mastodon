4934242acaa581c2253252d5e7275c1de6f691cb
Matt Jankowski <matt@jankowski.online>
2024-12-04 12:55:56 -0500

    Update schema/annotations after tag trend db/redis migrate (#33173)

--- app/models/tag_trend.rb
+++ app/models/tag_trend.rb
@@ -5,11 +5,11 @@
 # Table name: tag_trends
 #
 #  id       :bigint(8)        not null, primary key
-#  tag_id   :bigint(8)        not null
-#  score    :float            default(0.0), not null
-#  rank     :integer          default(0), not null
 #  allowed  :boolean          default(FALSE), not null
-#  language :string
+#  language :string           default(""), not null
+#  rank     :integer          default(0), not null
+#  score    :float            default(0.0), not null
+#  tag_id   :bigint(8)        not null
 #
 class TagTrend < ApplicationRecord
   include RankedTrend
