52ab9645bef83b3adbb3411643b171a74d76ad76
Nick Schonning <nschonni@gmail.com>
2024-12-06 12:11:42 -0500

    Hoist ESLint config in streaming

--- .eslintrc.js
+++ .eslintrc.js
@@ -362,6 +362,41 @@ module.exports = defineConfig({
       env: {
         jest: true,
       },
+    },
+    {
+      files: ['streaming/*.js'],
+      env: {
+        browser: false,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: 'streaming/',
+        ecmaFeatures: {
+          jsx: false,
+        },
+        ecmaVersion: 2021,
+      },
+      rules: {
+        // In the streaming server we need to delete some variables to ensure
+        // garbage collection takes place on the values referenced by those objects;
+        // The alternative is to declare the variable as nullable, but then we need
+        // to assert it's in existence before every use, which becomes much harder
+        // to maintain.
+        'no-delete-var': 'off',
+
+        // This overrides the base configuration for this rule to pick up
+        // dependencies for the streaming server from the correct package.json file.
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            optionalDependencies: false,
+            peerDependencies: false,
+            includeTypes: true,
+            packageDir: 'streaming',
+          },
+        ],
+        'import/extensions': ['error', 'always'],
+      },
     }
   ],
 });
