3cccb146c6175869532accdccfd07e3140de9817
Bryan <ramirezgomero.bryan@gmail.com>
2024-12-06 14:03:41 -0500

    Act. Issues #32770

--- app/javascript/mastodon/features/preferences/index.jsx
+++ app/javascript/mastodon/features/preferences/index.jsx
@@ -1,46 +1,36 @@
 import React from 'react';
 
-import { FormattedMessage } from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
 
+import ImmutablePureComponent from 'react-immutable-pure-component';
+
+import Column from '../../components/column';
+import ColumnBackButton from '../../components/column_back_button';
 import LocalThemeSettings from '../local_themes';
-import Column from '../ui/components/column.jsx';
-import ColumnBackButton from '../../components/column_back_button.jsx'; // Importación corregida
 
-const Preferences = () => {
-  return (
-    <Column>
-      <ColumnBackButton />
-      <div className='scrollable'>
-        <div className='column-header'>
-          <h1>
-            <FormattedMessage
-              id='column.preferences'
-              defaultMessage='Preferencias'
-            />
-          </h1>
+class Preferences extends ImmutablePureComponent {
+  render() {
+    return (
+      <Column>
+        <ColumnBackButton />
+        <div className='scrollable'>
+          <div className='column-header'>
+            <h1><FormattedMessage id='column.preferences' defaultMessage='Preferencias' /></h1>
+          </div>
+
+          <div className='column-section'>
+            <h2>
+              <FormattedMessage
+                id='preferences.local_themes'
+                defaultMessage='Temas Locales'
+              />
+            </h2>
+            <LocalThemeSettings />
+          </div>
         </div>
+      </Column>
+    );
+  }
+}
 
-        {/* Otras secciones de preferencias */}
-
-        <div className='column-section'>
-          <h2>
-            <FormattedMessage
-              id='preferences.local_themes'
-              defaultMessage='Temas Locales'
-            />
-          </h2>
-          <p className='column-section__description'>
-            <FormattedMessage
-              id='preferences.local_themes_description'
-              defaultMessage='Personaliza la apariencia de Mastodon con tus propios estilos CSS'
-            />
-          </p>
-
-          <LocalThemeSettings />
-        </div>
-      </div>
-    </Column>
-  );
-};
-
-export default Preferences;
+export default injectIntl(Preferences);
