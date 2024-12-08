57062dc010caf4fe5146497efa272e42449d1686
Nik Clayton <nik@ngo.org.uk>
2024-12-08 14:07:16 +0100

    Note the external API commitment

--- config/initializers/time_with_zone.rb
+++ config/initializers/time_with_zone.rb
@@ -2,6 +2,9 @@
 
 # Ensure TimeWithZone are represented as RFC3339 DateTime with 3 digits of
 # precision for the fractional part of the second.
+#
+# This matches the external API commitment documented at
+# https://docs.joinmastodon.org/api/datetime-format/#datetime.
 class ActiveSupport::TimeWithZone
   def as_json(_options = {})
     rfc3339(3)
