6c276e9915eed8fa93e8b6b69132ec17a80fc533
Matt Jankowski <matt@jankowski.online>
2024-12-10 11:49:50 -0500

    Add coverage for admin/terms/drafts#update

--- spec/controllers/admin/terms_of_service/drafts_controller_spec.rb
+++ spec/controllers/admin/terms_of_service/drafts_controller_spec.rb
@@ -18,4 +18,49 @@ RSpec.describe Admin::TermsOfService::DraftsController do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'PUT #update' do
+    subject { put :update, params: params }
+
+    let!(:terms) { Fabricate :terms_of_service, published_at: nil }
+
+    context 'with publishing params' do
+      let(:params) { { terms_of_service: { text: 'new' }, action_type: 'publish' } }
+
+      it 'publishes the record' do
+        expect { subject }
+          .to change(Admin::ActionLog, :count).by(1)
+
+        expect(response)
+          .to redirect_to(admin_terms_of_service_index_path)
+        expect(terms.reload.published_at)
+          .to_not be_nil
+      end
+    end
+
+    context 'with non publishing params' do
+      let(:params) { { terms_of_service: { text: 'new' }, action_type: 'save_draft' } }
+
+      it 'updates but does not publish the record' do
+        expect { subject }
+          .to_not change(Admin::ActionLog, :count)
+
+        expect(response)
+          .to redirect_to(admin_terms_of_service_draft_path)
+        expect(terms.reload.published_at)
+          .to be_nil
+      end
+    end
+
+    context 'with invalid params' do
+      let(:params) { { terms_of_service: { text: '' }, action_type: 'save_draft' } }
+
+      it 'does not update the record' do
+        subject
+
+        expect(response)
+          .to have_http_status(:success)
+      end
+    end
+  end
 end
