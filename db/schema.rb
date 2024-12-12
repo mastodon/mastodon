8a15b495511b43270716181152faa8eacbba066c
Matt Jankowski <matt@jankowski.online>
2024-12-12 10:27:14 -0500

    Add `NOT NULL` requirement to columns on `account_deletion_requests`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_12_152158) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_12_152618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_12_152158) do
   end
 
   create_table "account_deletion_requests", force: :cascade do |t|
-    t.bigint "account_id"
+    t.bigint "account_id", null: false
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.index ["account_id"], name: "index_account_deletion_requests_on_account_id"
