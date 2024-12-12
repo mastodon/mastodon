c15564eedcb750455bc11e36c67e3c5f68f8335e
Matt Jankowski <matt@jankowski.online>
2024-12-12 09:36:29 -0500

    Move anonymous controller ignore to inline disable

--- spec/controllers/concerns/api/rate_limit_headers_spec.rb
+++ spec/controllers/concerns/api/rate_limit_headers_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Api::RateLimitHeaders do
   controller(ApplicationController) do
-    include Api::RateLimitHeaders
+    include Api::RateLimitHeaders # rubocop:disable RSpec/DescribedClass
 
     def show
       head 200
--- spec/controllers/concerns/challengable_concern_spec.rb
+++ spec/controllers/concerns/challengable_concern_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe ChallengableConcern do
   controller(ApplicationController) do
-    include ChallengableConcern
+    include ChallengableConcern # rubocop:disable RSpec/DescribedClass
 
     before_action :require_challenge!
 
