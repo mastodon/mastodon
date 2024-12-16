8ac58e6ba2317f2c0753970abad747af0fc70af3
David Roetzel <david@roetzel.de>
2024-12-16 15:50:59 +0100

    Handle full content lifecycle for subscribed fasps

--- app/models/concerns/status/fasp_concern.rb
+++ app/models/concerns/status/fasp_concern.rb
@@ -5,12 +5,22 @@ module Status::FaspConcern
 
   included do
     after_commit :announce_new_content_to_subscribed_fasp, on: :create
+    after_commit :announce_updated_content_to_subscribed_fasp, on: :update
+    after_commit :announce_deleted_content_to_subscribed_fasp, on: :destroy
   end
 
   private
 
   def announce_new_content_to_subscribed_fasp
     store_uri unless uri # TODO: solve this more elegantly
-    Fasp::AnnounceNewContentWorker.perform_async(uri)
+    Fasp::AnnounceContentLifecycleEventWorker.perform_async(uri, 'new')
+  end
+
+  def announce_updated_content_to_subscribed_fasp
+    Fasp::AnnounceContentLifecycleEventWorker.perform_async(uri, 'update')
+  end
+
+  def announce_deleted_content_to_subscribed_fasp
+    Fasp::AnnounceContentLifecycleEventWorker.perform_async(uri, 'delete')
   end
 end
