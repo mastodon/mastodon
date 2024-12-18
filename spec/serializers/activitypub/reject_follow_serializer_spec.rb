4be0487a84336e911b92982e06d9fb9c7f5537e1
Matt Jankowski <matt@jankowski.online>
2024-12-18 11:01:25 -0500

    Add serializer spec for AP Reject/Accept Follow serializers

---
+++ spec/serializers/activitypub/reject_follow_serializer_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ActivityPub::RejectFollowSerializer do
+  subject { serialized_record_json(record, described_class, adapter: ActivityPub::Adapter) }
+
+  describe 'serializing an object' do
+    let(:record) { Fabricate :follow_request }
+
+    it 'returns expected attributes' do
+      expect(subject.deep_symbolize_keys)
+        .to include(
+          actor: match(record.target_account.username),
+          id: match("#rejects/follows/#{record.id}"),
+          object: include(type: 'Follow'),
+          type: 'Reject'
+        )
+    end
+  end
+end
