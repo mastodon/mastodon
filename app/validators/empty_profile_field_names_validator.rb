80f14ca2be93dea3105e6cc422aadacee293ed14
Tim Rogers <rogers.timothy.john@gmail.com>
2025-01-03 16:49:14 -0600

    Updated field label validator to use activerecord key lookup and field validation error message to be consistent with UI naming

--- app/validators/empty_profile_field_names_validator.rb
+++ app/validators/empty_profile_field_names_validator.rb
@@ -4,7 +4,7 @@ class EmptyProfileFieldNamesValidator < ActiveModel::Validator
   def validate(account)
     return if account.fields.empty?
 
-    account.errors.add(:fields, I18n.t('edit_profile.errors.fields_with_values_missing_names')) if fields_with_values_missing_names?(account)
+    account.errors.add(:fields, :fields_with_values_missing_labels) if fields_with_values_missing_names?(account)
   end
 
   private
