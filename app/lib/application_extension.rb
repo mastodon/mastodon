b3243ef41c5d992afa87b60147b459c725931b55
Matt Jankowski <matt@jankowski.online>
2025-01-07 11:28:54 -0500

    Refer to constant values from `api/v1/apps` request spec (#33488)

--- app/lib/application_extension.rb
+++ app/lib/application_extension.rb
@@ -3,14 +3,18 @@
 module ApplicationExtension
   extend ActiveSupport::Concern
 
+  APP_NAME_LIMIT = 60
+  APP_REDIRECT_URI_LIMIT = 2_000
+  APP_WEBSITE_LIMIT = 2_000
+
   included do
     include Redisable
 
     has_many :created_users, class_name: 'User', foreign_key: 'created_by_application_id', inverse_of: :created_by_application
 
-    validates :name, length: { maximum: 60 }
-    validates :website, url: true, length: { maximum: 2_000 }, if: :website?
-    validates :redirect_uri, length: { maximum: 2_000 }
+    validates :name, length: { maximum: APP_NAME_LIMIT }
+    validates :redirect_uri, length: { maximum: APP_REDIRECT_URI_LIMIT }
+    validates :website, url: true, length: { maximum: APP_WEBSITE_LIMIT }, if: :website?
 
     # The relationship used between Applications and AccessTokens is using
     # dependent: delete_all, which means the ActiveRecord callback in
