22340a80352c71a34f5b67c6cd3199a57bd0e66f
Leni Kadali <52788034+lenikadali@users.noreply.github.com>
2025-01-09 15:46:16 +0300

    Initial commit for adding mCaptcha support
    Initial commit for adding mCaptcha support
    
    The implementation will need cleaning up
    as verification of the behaviour being what we want
    requires testing in a staging environment as
    the development environment doesn't provide a user
    sign up flow where a user would solve a CAPTCHA
    (if it is enabled on the server) like it would
    on production

--- app/controllers/concerns/auth/captcha_concern.rb
+++ app/controllers/concerns/auth/captcha_concern.rb
@@ -4,13 +4,22 @@ module Auth::CaptchaConcern
   extend ActiveSupport::Concern
 
   include Hcaptcha::Adapters::ViewMethods
+  include Mcaptcha::Adapters::ViewMethods
 
   included do
     helper_method :render_captcha
   end
 
   def captcha_available?
-    Rails.configuration.x.captcha.secret_key.present? && Rails.configuration.x.captcha.site_key.present?
+    if Rails.configuration.x.captcha.hcaptcha_secret_key.present? && Rails.configuration.x.captcha.hcaptcha_site_key.present?
+      @hcaptcha_enabled = true
+      true
+    elsif Rails.configuration.x.captcha.mcaptcha_secret_key.present? && Rails.configuration.x.captcha.mcaptcha_site_key.present?
+      @mcaptcha_enabled = true
+      true
+    else
+      false
+    end
   end
 
   def captcha_enabled?
@@ -25,7 +34,7 @@ module Auth::CaptchaConcern
     captcha_enabled? && !captcha_user_bypass?
   end
 
-  def check_captcha!
+  def check_hcaptcha!
     return true unless captcha_required?
 
     if verify_hcaptcha
@@ -41,6 +50,22 @@ module Auth::CaptchaConcern
     end
   end
 
+  def check_mcaptcha!
+    return true unless captcha_required?
+
+    if verify_mcaptcha
+      true
+    else
+      if block_given?
+        message = flash[:mcaptcha_error]
+        flash.delete(:mcaptcha_error)
+        yield message
+      end
+
+      false
+    end
+  end
+
   def extend_csp_for_captcha!
     policy = request.content_security_policy&.clone
 
@@ -49,8 +74,13 @@ module Auth::CaptchaConcern
     %w(script_src frame_src style_src connect_src).each do |directive|
       values = policy.send(directive)
 
-      values << 'https://hcaptcha.com' unless values.include?('https://hcaptcha.com') || values.include?('https:')
-      values << 'https://*.hcaptcha.com' unless values.include?('https://*.hcaptcha.com') || values.include?('https:')
+      if @hcaptcha_enabled
+        values << 'https://hcaptcha.com' unless values.include?('https://hcaptcha.com') || values.include?('https:')
+        values << 'https://*.hcaptcha.com' unless values.include?('https://*.hcaptcha.com') || values.include?('https:')
+      elsif @mcaptcha_enabled
+        values << 'https://mcaptcha.org' unless values.include?('https://mcaptcha.org') || values.include?('https:')
+        values << 'https://mcaptcha.*.*' unless values.include?('https://mcaptcha.*.*') || values.include?('https:')
+      end
 
       policy.send(directive, *values)
     end
@@ -61,6 +91,10 @@ module Auth::CaptchaConcern
   def render_captcha
     return unless captcha_required?
 
-    hcaptcha_tags
+    if @hcaptcha_enabled
+      hcaptcha_tags
+    elsif @mcaptcha_enabled
+      mcaptcha_tags
+    end
   end
 end
