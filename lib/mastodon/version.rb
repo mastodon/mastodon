73b5b03785c41bc677d93e540166dd08707b1d98
Matt Jankowski <matt@jankowski.online>
2025-01-10 10:24:22 -0500

    Use `config_for` for `Mastodon::Version` metadata/prerelease values

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,11 +21,11 @@ module Mastodon
     end
 
     def prerelease
-      ENV['MASTODON_VERSION_PRERELEASE'].presence || default_prerelease
+      version_configuration[:prerelease].presence || default_prerelease
     end
 
     def build_metadata
-      ENV.fetch('MASTODON_VERSION_METADATA', nil)
+      version_configuration[:metadata]
     end
 
     def to_a
@@ -76,6 +76,10 @@ module Mastodon
 
     def user_agent
       @user_agent ||= "Mastodon/#{Version} (#{HTTP::Request::USER_AGENT}; +http#{Rails.configuration.x.use_https ? 's' : ''}://#{Rails.configuration.x.web_domain}/)"
+    end
+
+    def version_configuration
+      Rails.configuration.x.mastodon.version
     end
   end
 end
