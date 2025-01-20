d26219d1ed5190f031ee609a350f0156b9cea289
Claire <claire.github-309c@sitedethib.com>
2025-01-20 16:50:33 +0100

    Add `quote_approval_policy` to Status

--- app/models/status.rb
+++ app/models/status.rb
@@ -28,6 +28,7 @@
 #  trendable                    :boolean
 #  ordered_media_attachment_ids :bigint(8)        is an Array
 #  fetched_replies_at           :datetime
+#  quote_approval_policy        :integer          default(0), not null
 #
 
 class Status < ApplicationRecord
@@ -43,6 +44,13 @@ class Status < ApplicationRecord
   include Status::Visibility
 
   MEDIA_ATTACHMENTS_LIMIT = 4
+
+  QUOTE_APPROVAL_POLICY_FLAGS = {
+    public: (1 << 0),
+    followers: (1 << 1),
+    followed: (1 << 2),
+    unknown: (1 << 3),
+  }.freeze
 
   rate_limit by: :account, family: :statuses
 
