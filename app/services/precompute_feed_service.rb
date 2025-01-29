74cffada37ac0a98fa7449feef75bf0398448051
Claire <claire.github-309c@sitedethib.com>
2025-01-29 19:36:31 +0100

    Fix `tootctl feeds build` not building list timelines

--- app/services/precompute_feed_service.rb
+++ app/services/precompute_feed_service.rb
@@ -5,6 +5,10 @@ class PrecomputeFeedService < BaseService
 
   def call(account)
     FeedManager.instance.populate_home(account)
+
+    account.owned_lists.each do |list|
+      FeedManager.instance.populate_list(list)
+    end
   ensure
     redis.del("account:#{account.id}:regeneration")
   end
