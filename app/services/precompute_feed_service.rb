9e164c532f21df3eb0a517e560b7fc8b58737c74
Claire <claire.github-309c@sitedethib.com>
2025-02-06 10:38:32 +0100

    Add `--skip-filled-timeline` option to `tootctl feed build` to skip half-filled feeds (#33844)

--- app/services/precompute_feed_service.rb
+++ app/services/precompute_feed_service.rb
@@ -3,13 +3,21 @@
 class PrecomputeFeedService < BaseService
   include Redisable
 
-  def call(account)
-    FeedManager.instance.populate_home(account)
+  def call(account, skip_filled_timelines: false)
+    @skip_filled_timelines = skip_filled_timelines
+
+    FeedManager.instance.populate_home(account) unless skip_timeline?(:home, account.id)
 
     account.owned_lists.each do |list|
-      FeedManager.instance.populate_list(list)
+      FeedManager.instance.populate_list(list) unless skip_timeline?(:list, list.id)
     end
   ensure
     redis.del("account:#{account.id}:regeneration")
+  end
+
+  private
+
+  def skip_timeline?(type, id)
+    @skip_filled_timelines && FeedManager.instance.timeline_size(type, id) * 2 > FeedManager::MAX_ITEMS
   end
 end
