f511bd65d1eea31a05ee5e2b6079258f843bb389
Matt Jankowski <matt@jankowski.online>
2025-02-10 11:31:49 -0500

    Convert `settings/featured_tags` controller->request/system spec

--- spec/requests/settings/featured_tags_spec.rb
+++ spec/requests/settings/featured_tags_spec.rb
@@ -2,15 +2,23 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Settings Aliases' do
+RSpec.describe 'Settings Featured Tags' do
   describe 'POST /settings/featured_tags' do
-    before { sign_in Fabricate(:user) }
+    context 'when signed in' do
+      before { sign_in Fabricate(:user) }
 
-    it 'gracefully handles invalid nested params' do
-      post settings_featured_tags_path(featured_tag: 'invalid')
+      it 'gracefully handles invalid nested params' do
+        post settings_featured_tags_path(featured_tag: 'invalid')
 
-      expect(response)
-        .to have_http_status(400)
+        expect(response)
+          .to have_http_status(400)
+      end
+    end
+
+    context 'when not signed in' do
+      subject { post settings_featured_tags_path }
+
+      it { is_expected.to redirect_to new_user_session_path }
     end
   end
 end
