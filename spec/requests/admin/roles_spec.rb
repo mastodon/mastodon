980104cddf2b119c7a05e1e5306c961c5657685e
Matt Jankowski <matt@jankowski.online>
2025-02-17 12:51:36 -0500

    Move the rest to request spec

--- spec/requests/admin/roles_spec.rb
+++ spec/requests/admin/roles_spec.rb
@@ -97,6 +97,38 @@ RSpec.describe 'Admin Roles' do
     end
   end
 
+  context 'when attempting to add permissions the user does not have' do
+    let(:user_role) { Fabricate :user_role, permissions: UserRole::FLAGS[:manage_roles], position: 5 }
+
+    before { sign_in Fabricate(:user, role: user_role) }
+
+    describe 'POST /admin/roles' do
+      subject { post admin_roles_path, params: { user_role: { name: 'Bar', position: 2, permissions_as_keys: %w(manage_roles manage_users manage_reports) } } }
+
+      it 'does not create role' do
+        expect { subject }
+          .to_not change(UserRole, :count)
+
+        expect(response.body)
+          .to include(I18n.t('admin.roles.add_new'))
+      end
+    end
+
+    describe 'PUT /admin/roles/:id' do
+      subject { put admin_role_path(role), params: { user_role: { position: 2, permissions_as_keys: %w(manage_roles manage_users manage_reports) } } }
+
+      let(:role) { Fabricate :user_role, name: 'Bar' }
+
+      it 'does not create role' do
+        expect { subject }
+          .to_not(change { role.reload.permissions })
+
+        expect(response.parsed_body.title)
+          .to match(I18n.t('admin.roles.edit', name: 'Bar'))
+      end
+    end
+  end
+
   context 'when signed in as admin' do
     before { sign_in Fabricate(:admin_user) }
 
