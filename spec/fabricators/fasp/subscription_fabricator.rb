c2102aa95cc68ae82954080f78351b2fee87775f
David Roetzel <david@roetzel.de>
2025-02-25 11:01:07 +0100

    Fix autogenerated fabricators

--- spec/fabricators/fasp/subscription_fabricator.rb
+++ spec/fabricators/fasp/subscription_fabricator.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-Fabricator('Fasp::Subscription') do
-  category            'MyString'
-  subscription_type   'MyString'
-  max_batch_size      1
-  threshold_timeframe 1
-  threshold_shares    1
-  threshold_likes     1
-  threshold_replies   1
-  fasp_provider       nil
+Fabricator(:fasp_subscription, from: 'Fasp::Subscription') do
+  category            'content'
+  subscription_type   'lifecycle'
+  max_batch_size      10
+  fasp_provider
 end
