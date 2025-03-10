597f3044daeafd206a457eec75cc69ca9489cdc3
Matt Jankowski <matt@jankowski.online>
2025-03-10 10:04:14 -0400

    Remove faker usage from fabricators

--- spec/fabricators/canonical_email_block_fabricator.rb
+++ spec/fabricators/canonical_email_block_fabricator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Fabricator(:canonical_email_block) do
-  email { sequence(:email) { |i| "#{i}#{Faker::Internet.email}" } }
+  email { sequence(:email) { |i| "email_#{i}@host.example" } }
   reference_account { Fabricate.build(:account) }
 end
--- spec/fabricators/email_domain_block_fabricator.rb
+++ spec/fabricators/email_domain_block_fabricator.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Fabricator(:email_domain_block) do
-  domain { sequence(:domain) { |i| "#{i}#{Faker::Internet.domain_name}" } }
+  domain { sequence(:domain) { |i| "host-#{i}-name.example" } }
 end
