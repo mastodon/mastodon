4cb3fe35beb5f85186d68f174179e9d67c704113
Claire <claire.github-309c@sitedethib.com>
2025-03-18 15:50:41 +0100

    Fix handling of malformed/unusual HTML (#34201)

--- app/models/account/field.rb
+++ app/models/account/field.rb
@@ -73,7 +73,14 @@ class Account::Field < ActiveModelSerializers::Model
   end
 
   def extract_url_from_html
-    doc = Nokogiri::HTML5.fragment(value)
+    begin
+      doc = Nokogiri::HTML5.fragment(value)
+    rescue ArgumentError
+      # This can happen if one of the Nokogumbo limits is encountered
+      # Unfortunately, it does not use a more precise error class
+      # nor allows more graceful handling
+      return
+    end
 
     return if doc.nil?
     return if doc.children.size != 1
