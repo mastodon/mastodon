c77fdf212e0dc6b3cd0f2948afb9d80c22af95cd
David Roetzel <david@roetzel.de>
2025-03-21 14:26:40 +0100

    Fix autogenerated fabricators

--- spec/fabricators/fasp/provider_fabricator.rb
+++ spec/fabricators/fasp/provider_fabricator.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
-Fabricator('Fasp::Provider') do
-  name                    'MyString'
-  base_url                'MyString'
-  sign_in_url             'MyString'
+Fabricator(:fasp_provider, from: 'Fasp::Provider') do
+  name                    { Faker::App.name }
+  base_url                { Faker::Internet.unique.url }
+  sign_in_url             { Faker::Internet.url }
   remote_identifier       'MyString'
   provider_public_key_pem 'MyString'
-  server_private_key_pem  'MyString'
-  capabilities            ''
-  privacy_policy          ''
-  contact_email           'MyString'
-  fediverse_account       'MyString'
+  capabilities            []
 end
