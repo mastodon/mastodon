7f8c6b58757e4ec8a9abce2b235d7bb02b160b87
Matt Jankowski <matt@jankowski.online>
2025-03-25 14:27:39 -0400

    Convert `settings/deletes` spec controller->request/system

--- spec/requests/settings/deletes_spec.rb
+++ spec/requests/settings/deletes_spec.rb
@@ -4,13 +4,65 @@ require 'rails_helper'
 
 RSpec.describe 'Settings Deletes' do
   describe 'DELETE /settings/delete' do
-    before { sign_in Fabricate(:user) }
+    context 'when signed in' do
+      before { sign_in(user) }
 
-    it 'gracefully handles invalid nested params' do
-      delete settings_delete_path(form_delete_confirmation: 'invalid')
+      let(:user) { Fabricate(:user) }
 
-      expect(response)
-        .to have_http_status(400)
+      it 'gracefully handles invalid nested params' do
+        delete settings_delete_path(form_delete_confirmation: 'invalid')
+
+        expect(response)
+          .to have_http_status(400)
+      end
+
+      context 'when suspended' do
+        let(:user) { Fabricate(:user, account_attributes: { suspended_at: Time.now.utc }) }
+
+        it 'returns http forbidden' do
+          delete settings_delete_path
+
+          expect(response)
+            .to have_http_status(403)
+        end
+      end
+    end
+
+    context 'when not signed in' do
+      it 'redirects to sign in' do
+        delete settings_delete_path
+
+        expect(response)
+          .to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'GET /settings/delete' do
+    context 'when signed in' do
+      before { sign_in(user) }
+
+      context 'when suspended' do
+        let(:user) { Fabricate(:user, account_attributes: { suspended_at: Time.now.utc }) }
+
+        it 'returns http forbidden with private cache control headers' do
+          get settings_delete_path
+
+          expect(response)
+            .to have_http_status(403)
+          expect(response.headers['Cache-Control'])
+            .to include('private, no-store')
+        end
+      end
+    end
+
+    context 'when not signed in' do
+      it 'redirects to sign in' do
+        get settings_delete_path
+
+        expect(response)
+          .to redirect_to(new_user_session_path)
+      end
     end
   end
 end
