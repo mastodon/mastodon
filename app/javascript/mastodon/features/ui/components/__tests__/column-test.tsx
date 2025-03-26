4c12edbdb4967a29f87b76c5e5ee13d5b0892a4b
scarf <greenscarf005@gmail.com>
2025-03-27 02:02:49 +0900

    refactor: column test

--- app/javascript/mastodon/features/ui/components/__tests__/column-test.tsx
+++ app/javascript/mastodon/features/ui/components/__tests__/column-test.tsx
@@ -9,17 +9,29 @@ describe('<Column />', () => {
     it('runs the scroll animation if the column contains scrollable content', () => {
       const scrollToMock = jest.fn();
       const { container } = render(
-        <Column heading='notifications' icon='notifications' iconComponent={fakeIcon}>
+        <Column
+          heading='notifications'
+          icon='notifications'
+          iconComponent={fakeIcon}
+        >
           <div className='scrollable' />
         </Column>,
       );
-      container.querySelector('.scrollable').scrollTo = scrollToMock;
+      const scrollable = container.querySelector('.scrollable');
+      if (scrollable?.scrollTo) scrollable.scrollTo = scrollToMock;
+
       fireEvent.click(screen.getByText('notifications'));
       expect(scrollToMock).toHaveBeenCalledWith({ behavior: 'smooth', top: 0 });
     });
 
     it('does not try to scroll if there is no scrollable content', () => {
-      render(<Column heading='notifications' icon='notifications' iconComponent={fakeIcon} />);
+      render(
+        <Column
+          heading='notifications'
+          icon='notifications'
+          iconComponent={fakeIcon}
+        />,
+      );
       fireEvent.click(screen.getByText('notifications'));
     });
   });
