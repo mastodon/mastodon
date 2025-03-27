f9b6ffedff6ad3c416c09ef97f32465b773bf715
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-03-27 16:58:28 +0100

    refactor Poll component to TS and use react-spring

--- app/javascript/mastodon/containers/media_container.jsx
+++ app/javascript/mastodon/containers/media_container.jsx
@@ -7,7 +7,7 @@ import { fromJS } from 'immutable';
 import { ImmutableHashtag as Hashtag } from 'mastodon/components/hashtag';
 import MediaGallery from 'mastodon/components/media_gallery';
 import ModalRoot from 'mastodon/components/modal_root';
-import Poll from 'mastodon/components/poll';
+import { Poll } from 'mastodon/components/poll';
 import Audio from 'mastodon/features/audio';
 import Card from 'mastodon/features/status/components/card';
 import MediaModal from 'mastodon/features/ui/components/media_modal';
