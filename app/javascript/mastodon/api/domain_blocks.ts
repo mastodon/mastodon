cd2e22544ad659acaa71fa5e724d4811ba354efb
Eugen Rochko <eugen@zeonfederated.com>
2025-04-02 12:26:40 +0200

    Apply suggestions from code review
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/mastodon/api/domain_blocks.ts
+++ app/javascript/mastodon/api/domain_blocks.ts
@@ -3,7 +3,7 @@ import api, { getLinks } from 'mastodon/api';
 export const apiGetDomainBlocks = async (url?: string) => {
   const response = await api().request<string[]>({
     method: 'GET',
-    url: url ?? 'api/v1/domain_blocks',
+    url: url ?? '/api/v1/domain_blocks',
   });
 
   return {
