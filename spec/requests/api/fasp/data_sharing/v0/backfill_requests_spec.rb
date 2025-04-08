64b85e7a5444f38ce779d46ac3e671f710208179
David Roetzel <david@roetzel.de>
2025-04-08 16:43:13 +0200

    Add request specs

--- spec/requests/api/fasp/data_sharing/v0/backfill_requests_spec.rb
+++ spec/requests/api/fasp/data_sharing/v0/backfill_requests_spec.rb
@@ -2,8 +2,40 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Api::Fasp::DataSharing::V0::BackfillRequests' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::Fasp::DataSharing::V0::BackfillRequests', feature: :fasp do
+  include ProviderRequestHelper
+
+  describe 'POST /api/fasp/data_sharing/v0/backfill_requests' do
+    let(:provider) { Fabricate(:fasp_provider) }
+
+    context 'with valid parameters' do
+      it 'creates a new backfill request' do
+        params = { category: 'content', maxCount: 10 }
+        headers = request_authentication_headers(provider,
+                                                 url: api_fasp_data_sharing_v0_backfill_requests_url,
+                                                 method: :post,
+                                                 body: params)
+
+        expect do
+          post api_fasp_data_sharing_v0_backfill_requests_path, headers:, params:, as: :json
+        end.to change(Fasp::BackfillRequest, :count).by(1)
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a backfill request' do
+        params = { category: 'unknown', maxCount: 10 }
+        headers = request_authentication_headers(provider,
+                                                 url: api_fasp_data_sharing_v0_backfill_requests_url,
+                                                 method: :post,
+                                                 body: params)
+
+        expect do
+          post api_fasp_data_sharing_v0_backfill_requests_path, headers:, params:, as: :json
+        end.to_not change(Fasp::BackfillRequest, :count)
+        expect(response).to have_http_status(422)
+      end
+    end
   end
 end
--- spec/requests/api/fasp/data_sharing/v0/continuations_spec.rb
+++ spec/requests/api/fasp/data_sharing/v0/continuations_spec.rb
@@ -2,8 +2,21 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Api::Fasp::DataSharing::V0::Continuations' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::Fasp::DataSharing::V0::Continuations', feature: :fasp do
+  include ProviderRequestHelper
+
+  describe 'POST /api/fasp/data_sharing/v0/backfill_requests/:id/continuations' do
+    let(:backfill_request) { Fabricate(:fasp_backfill_request) }
+    let(:provider) { backfill_request.fasp_provider }
+
+    it 'queues a job to continue the given backfill request' do
+      headers = request_authentication_headers(provider,
+                                               url: api_fasp_data_sharing_v0_backfill_request_continuation_url(backfill_request),
+                                               method: :post)
+
+      post api_fasp_data_sharing_v0_backfill_request_continuation_path(backfill_request), headers:, as: :json
+      expect(response).to have_http_status(204)
+      expect(Fasp::BackfillWorker).to have_enqueued_sidekiq_job(backfill_request.id)
+    end
   end
 end
