102ba8326b4fa9af47181be8de3659349acaf55d
Claire <claire.github-309c@sitedethib.com>
2025-04-09 14:49:03 +0200

    [WiP] Quote post filtering

--- app/serializers/rest/base_quote_serializer.rb
+++ app/serializers/rest/base_quote_serializer.rb
@@ -4,6 +4,12 @@ class REST::BaseQuoteSerializer < ActiveModel::Serializer
   attributes :state
 
   def quoted_status
-    object.quoted_status if object.accepted?
+    return unless object.accepted?
+
+    quoted_status = object.quoted_status
+    return if quoted_status.nil?
+
+    status_filter = StatusFilter.new(quoted_status, current_user&.account, instance_options[:relationships] || {})
+    quoted_status unless status_filter.filtered?
   end
 end
