ff0990ec9f9dfb6da8706157dab636ad5f955933
Eugen Rochko <eugen@zeonfederated.com>
2025-04-16 07:39:20 +0200

    Add REST API for fetching an account's endorsed accounts (#34421)

--- spec/requests/api/v1/accounts/endorsements_spec.rb
+++ spec/requests/api/v1/accounts/endorsements_spec.rb
@@ -13,8 +13,30 @@ RSpec.describe 'Accounts Pins API' do
     kevin.account.followers << user.account
   end
 
-  describe 'POST /api/v1/accounts/:account_id/pin' do
-    subject { post "/api/v1/accounts/#{kevin.account.id}/pin", headers: headers }
+  describe 'GET /api/v1/accounts/:account_id/endorsements' do
+    subject { get "/api/v1/accounts/#{user.account.id}/endorsements", headers: headers }
+
+    let(:scopes) { 'read:accounts' }
+
+    before do
+      user.account.endorsed_accounts << kevin.account
+    end
+
+    it 'returns the expected accounts', :aggregate_failures do
+      subject
+
+      expect(response).to have_http_status(200)
+      expect(response.content_type)
+        .to start_with('application/json')
+      expect(response.parsed_body)
+        .to contain_exactly(
+          hash_including(id: kevin.account_id.to_s)
+        )
+    end
+  end
+
+  describe 'POST /api/v1/accounts/:account_id/endorse' do
+    subject { post "/api/v1/accounts/#{kevin.account.id}/endorse", headers: headers }
 
     it 'creates account_pin', :aggregate_failures do
       expect do
@@ -26,8 +48,8 @@ RSpec.describe 'Accounts Pins API' do
     end
   end
 
-  describe 'POST /api/v1/accounts/:account_id/unpin' do
-    subject { post "/api/v1/accounts/#{kevin.account.id}/unpin", headers: headers }
+  describe 'POST /api/v1/accounts/:account_id/unendorse' do
+    subject { post "/api/v1/accounts/#{kevin.account.id}/unendorse", headers: headers }
 
     before do
       Fabricate(:account_pin, account: user.account, target_account: kevin.account)
