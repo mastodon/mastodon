bf7b4116ff1381c36b382658cdbbe1ba87deb2ff
diondiondion <mail@diondiondion.com>
2025-05-09 17:33:06 +0200

    feat: Add placeholder component for familiar followers

---
+++ app/javascript/mastodon/features/account_timeline/components/familiar_followers.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { useIntl, defineMessages } from 'react-intl';
+
+import { fetchAccountsFamiliarFollowers } from '@/mastodon/actions/accounts_familiar_followers';
+import { getAccountFamiliarFollowers } from '@/mastodon/selectors/accounts';
+import { useAppDispatch, useAppSelector } from '@/mastodon/store';
+
+const messages = defineMessages({
+  familiar_followers: {
+    id: 'account.familiar_followers',
+    defaultMessage: 'Followed by:',
+  },
+});
+
+export const FamiliarFollowers: React.FC<{ accountId: string }> = ({
+  accountId,
+}) => {
+  const intl = useIntl();
+  const dispatch = useAppDispatch();
+  const familiarFollowers = useAppSelector((state) =>
+    getAccountFamiliarFollowers(state, accountId),
+  );
+
+  useEffect(() => {
+    void dispatch(fetchAccountsFamiliarFollowers({ id: accountId }));
+  }, [dispatch, accountId]);
+
+  if (familiarFollowers.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      {intl.formatMessage(messages.familiar_followers)}
+      {familiarFollowers.map((fellow) => (
+        <div key={fellow.id}>{fellow.display_name}</div>
+      ))}
+    </>
+  );
+};
