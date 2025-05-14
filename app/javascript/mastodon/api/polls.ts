a38786dd43b0e33cd0562d5621a0c5bd93281580
diondiondion <mail@diondiondion.com>
2025-05-14 10:40:20 +0200

    refactor: Prevent leading slashes in API urls

--- app/javascript/mastodon/api/polls.ts
+++ app/javascript/mastodon/api/polls.ts
@@ -2,9 +2,9 @@ import { apiRequestGet, apiRequestPost } from 'mastodon/api';
 import type { ApiPollJSON } from 'mastodon/api_types/polls';
 
 export const apiGetPoll = (pollId: string) =>
-  apiRequestGet<ApiPollJSON>(`/v1/polls/${pollId}`);
+  apiRequestGet<ApiPollJSON>(`v1/polls/${pollId}`);
 
 export const apiPollVote = (pollId: string, choices: string[]) =>
-  apiRequestPost<ApiPollJSON>(`/v1/polls/${pollId}/votes`, {
+  apiRequestPost<ApiPollJSON>(`v1/polls/${pollId}/votes`, {
     choices,
   });
