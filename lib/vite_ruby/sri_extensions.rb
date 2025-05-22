829ee569bcadfc9054e502f8820327a07086fc45
Renaud Chaput <renchap@gmail.com>
2025-05-22 12:23:18 +0200

    Use a default value for `crossorigin`

--- lib/vite_ruby/sri_extensions.rb
+++ lib/vite_ruby/sri_extensions.rb
@@ -120,12 +120,12 @@ module ViteRails::TagHelpers::IntegrityExtension
     # Ignore this error, it is not critical if the file is not preloaded
   end
 
-  def vite_polyfills_tag(**)
+  def vite_polyfills_tag(crossorigin: 'anonymous', **)
     return if ViteRuby.instance.dev_server_running?
 
     entry = vite_manifest.path_and_integrity_for('polyfills', type: :virtual)
 
-    javascript_include_tag(entry[:path], type: 'module', integrity: entry[:integrity], **)
+    javascript_include_tag(entry[:path], type: 'module', integrity: entry[:integrity], crossorigin: crossorigin, **)
   end
 end
 
