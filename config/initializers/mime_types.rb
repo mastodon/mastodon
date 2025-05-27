5059772b044348589e12d5db3f2b1cfd50c92054
Matt Jankowski <matt@jankowski.online>
2025-05-27 06:58:36 -0400

    Remove mime type def for webp (in rack)

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,6 +2,3 @@
 
 Mime::Type.register 'application/json', :json, %w(text/x-json application/jsonrequest application/jrd+json application/activity+json application/ld+json)
 Mime::Type.register 'text/xml',         :xml,  %w(application/xml application/atom+xml application/xrd+xml)
-
-# WebP is not defined in Rack 2.2.
-Rack::Mime::MIME_TYPES['.webp'] = 'image/webp'
