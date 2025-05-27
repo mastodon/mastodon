64e0dde9a516d97777418f00c53b48eaa5af9a3d
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-05-27 21:20:23 +0200

    Extend client_credentials_token fabricator from accessible_access_token fabricator

--- spec/fabricators/client_credentials_token_fabricator.rb
+++ spec/fabricators/client_credentials_token_fabricator.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-Fabricator :client_credentials_token, from: :access_token do
+Fabricator :client_credentials_token, from: :accessible_access_token do
   resource_owner_id { nil }
-  expires_in { nil }
-  revoked_at { nil }
 end
