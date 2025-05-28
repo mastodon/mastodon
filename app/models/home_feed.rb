b195956ecb07e9a3e47123a5bad95a664b257ffc
David Roetzel <david@roetzel.de>
2025-05-28 14:34:37 +0200

    Encapsulate redis key usage (#34840)

--- app/models/home_feed.rb
+++ app/models/home_feed.rb
@@ -9,4 +9,12 @@ class HomeFeed < Feed
   def regenerating?
     redis.exists?("account:#{@account.id}:regeneration")
   end
+
+  def regeneration_in_progress!
+    redis.set("account:#{@account.id}:regeneration", true, nx: true, ex: 1.day.seconds)
+  end
+
+  def regeneration_finished!
+    redis.del("account:#{@account.id}:regeneration")
+  end
 end
