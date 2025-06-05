375add0c83651771b08c5c450353f1b7f313bb04
Claire <claire.github-309c@sitedethib.com>
2025-06-05 10:49:26 +0200

    Fix account note textarea being interactable before the relationship gets fetched (#34932)

--- app/javascript/mastodon/features/account/components/account_note.jsx
+++ app/javascript/mastodon/features/account/components/account_note.jsx
@@ -8,6 +8,8 @@ import ImmutablePureComponent from 'react-immutable-pure-component';
 
 import Textarea from 'react-textarea-autosize';
 
+import { LoadingIndicator } from '@/mastodon/components/loading_indicator';
+
 const messages = defineMessages({
   placeholder: { id: 'account_note.placeholder', defaultMessage: 'Click to add a note' },
 });
@@ -153,17 +155,23 @@ class AccountNote extends ImmutablePureComponent {
           <FormattedMessage id='account.account_note_header' defaultMessage='Personal note' /> <InlineAlert show={saved} />
         </label>
 
-        <Textarea
-          id={`account-note-${accountId}`}
-          className='account__header__account-note__content'
-          disabled={this.props.value === null || value === null}
-          placeholder={intl.formatMessage(messages.placeholder)}
-          value={value || ''}
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
-          onBlur={this.handleBlur}
-          ref={this.setTextareaRef}
-        />
+        {this.props.value === undefined ? (
+          <div className='account__header__account-note__loading-indicator-wrapper'>
+            <LoadingIndicator />
+          </div>
+        ) : (
+          <Textarea
+            id={`account-note-${accountId}`}
+            className='account__header__account-note__content'
+            disabled={value === null}
+            placeholder={intl.formatMessage(messages.placeholder)}
+            value={value || ''}
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+            onBlur={this.handleBlur}
+            ref={this.setTextareaRef}
+          />
+        )}
       </div>
     );
   }
