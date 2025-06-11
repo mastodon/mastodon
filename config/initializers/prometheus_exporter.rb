932954179a45af950836967f8d358b1ab638e1d1
David Roetzel <david@roetzel.de>
2025-06-11 11:02:47 +0200

    Start local prometheus_exporter...
    ... only on puma and sidekiq startup, so it will not be started
    in a rails console, a `rails runner` command, rake task etc.

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -8,13 +8,17 @@ if ENV['MASTODON_PROMETHEUS_EXPORTER_ENABLED'] == 'true'
     require 'prometheus_exporter/server'
     require 'prometheus_exporter/client'
 
-    # bind is the address, on which the webserver will listen
-    # port is the port that will provide the /metrics route
-    server = PrometheusExporter::Server::WebServer.new bind: ENV.fetch('MASTODON_PROMETHEUS_EXPORTER_HOST', 'localhost'), port: ENV.fetch('MASTODON_PROMETHEUS_EXPORTER_PORT', '9394').to_i
-    server.start
+    module Mastodon::PrometheusExporterLocalServer
+      def self.setup!
+        # bind is the address, on which the webserver will listen
+        # port is the port that will provide the /metrics route
+        server = PrometheusExporter::Server::WebServer.new bind: ENV.fetch('MASTODON_PROMETHEUS_EXPORTER_HOST', 'localhost'), port: ENV.fetch('MASTODON_PROMETHEUS_EXPORTER_PORT', '9394').to_i
+        server.start
 
-    # wire up a default local client
-    PrometheusExporter::Client.default = PrometheusExporter::LocalClient.new(collector: server.collector)
+        # wire up a default local client
+        PrometheusExporter::Client.default = PrometheusExporter::LocalClient.new(collector: server.collector)
+      end
+    end
   end
 
   if ENV['MASTODON_PROMETHEUS_EXPORTER_WEB_DETAILED_METRICS'] == 'true'
