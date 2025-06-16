9384b68e092b290172052215c173aac8935a040d
Matt Jankowski <matt@jankowski.online>
2025-06-16 10:36:23 -0400

    Move single user mode config to `x.mastodon` area

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -122,7 +122,7 @@ class REST::InstanceSerializer < ActiveModel::Serializer
   private
 
   def registrations_enabled?
-    Setting.registrations_mode != 'none' && !Rails.configuration.x.single_user_mode
+    Setting.registrations_mode != 'none' && !Rails.configuration.x.mastodon.single_user_mode
   end
 
   def registrations_message
