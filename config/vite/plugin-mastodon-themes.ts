c269c6b43eef0a40b41e77c9ef573464720c6e72
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-06-16 15:43:08 +0200

    adds HMR again for theme files

--- config/vite/plugin-mastodon-themes.ts
+++ config/vite/plugin-mastodon-themes.ts
@@ -7,8 +7,11 @@ import path from 'node:path';
 import yaml from 'js-yaml';
 import type { Plugin } from 'vite';
 
+type Themes = Record<string, string>;
+
 export function MastodonThemes(): Plugin {
-  const themes: Record<string, string> = {};
+  let projectRoot = '';
+  let jsRoot = '';
 
   return {
     name: 'mastodon-themes',
@@ -16,40 +19,16 @@ export function MastodonThemes(): Plugin {
       if (!userConfig.root || !userConfig.envDir) {
         throw new Error('Unknown project directory');
       }
+      projectRoot = userConfig.envDir;
+      jsRoot = userConfig.root;
 
       const entrypoints: Record<string, string> = {};
 
       // Get all files mentioned in the themes.yml file.
-      const themesFile = path.resolve(userConfig.envDir, 'config/themes.yml');
-      if (!themesFile) {
-        throw new Error('Themes file must be defined.');
-      }
-      const themesString = await fs.readFile(themesFile, 'utf8');
-      const themesObject = yaml.load(themesString, {
-        filename: 'themes.yml',
-        schema: yaml.FAILSAFE_SCHEMA,
-      });
+      const themes = await loadThemesFromConfig(projectRoot);
 
-      if (!themesObject || typeof themes !== 'object') {
-        throw new Error('Invalid themes.yml file');
-      }
-
-      for (const [themeName, themePath] of Object.entries(themesObject)) {
-        if (
-          typeof themePath !== 'string' ||
-          themePath.split('.').length !== 2 || // Ensure it has exactly one period
-          !themePath.endsWith('css')
-        ) {
-          console.warn(
-            `Invalid theme path "${themePath}" in themes.yml, skipping`,
-          );
-          continue;
-        }
-        themes[themeName] = themePath;
-        entrypoints[`themes/${themeName}`] = path.resolve(
-          userConfig.root,
-          themePath,
-        );
+      for (const [themeName, themePath] of Object.entries(themes)) {
+        entrypoints[`themes/${themeName}`] = path.resolve(jsRoot, themePath);
       }
 
       return {
@@ -60,18 +39,116 @@ export function MastodonThemes(): Plugin {
         },
       };
     },
-    configureServer(server) {
+    async configureServer(server) {
+      const themes = await loadThemesFromConfig(projectRoot);
       server.middlewares.use((req, res, next) => {
-        const basename = path.basename(req.url ?? '');
+        if (!req.url?.startsWith('/packs-dev/themes/')) {
+          next();
+          return;
+        }
+
         // Rewrite the URL to the entrypoint if it matches a theme.
-        if (
-          req.url?.startsWith('/packs-dev/themes/') &&
-          Object.hasOwn(themes, basename)
-        ) {
-          req.url = `/packs-dev/${themes[basename]}`;
+        if (isThemeFile(req.url ?? '', themes)) {
+          const themeName = pathToThemeName(req.url ?? '');
+          req.url = `/packs-dev/${themes[themeName]}`;
         }
         next();
       });
     },
+    async handleHotUpdate({ modules, server }) {
+      if (modules.length === 0) {
+        return;
+      }
+      const themes = await loadThemesFromConfig(projectRoot);
+      const themePathToName = new Map(
+        Object.entries(themes).map(([themeName, themePath]) => [
+          path.resolve(jsRoot, themePath),
+          `/themes/${themeName}`,
+        ]),
+      );
+      const themeNames = new Set<string>();
+
+      const addIfMatches = (file: string | null) => {
+        if (!file) {
+          return false;
+        }
+        const themeName = themePathToName.get(file);
+        if (themeName) {
+          themeNames.add(themeName);
+          return true;
+        }
+        return false;
+      };
+
+      for (const module of modules) {
+        if (module.type !== 'css') {
+          continue;
+        }
+        if (!addIfMatches(module.file)) {
+          for (const importer of module.importers) {
+            addIfMatches(importer.file);
+          }
+        }
+      }
+
+      if (themeNames.size > 0) {
+        server.ws.send({
+          type: 'update',
+          updates: Array.from(themeNames).map((themeName) => ({
+            type: 'css-update',
+            path: themeName,
+            acceptedPath: themeName,
+            timestamp: Date.now(),
+          })),
+        });
+      }
+    },
   };
+}
+
+async function loadThemesFromConfig(root: string) {
+  const themesFile = path.resolve(root, 'config/themes.yml');
+  const themes: Themes = {};
+
+  const themesString = await fs.readFile(themesFile, 'utf8');
+  const themesObject = yaml.load(themesString, {
+    filename: 'themes.yml',
+    schema: yaml.FAILSAFE_SCHEMA,
+  });
+
+  if (!themesObject || typeof themes !== 'object') {
+    throw new Error('Invalid themes.yml file');
+  }
+
+  for (const [themeName, themePath] of Object.entries(themesObject)) {
+    if (
+      typeof themePath !== 'string' ||
+      themePath.split('.').length !== 2 || // Ensure it has exactly one period
+      !themePath.endsWith('css')
+    ) {
+      console.warn(`Invalid theme path "${themePath}" in themes.yml, skipping`);
+      continue;
+    }
+    themes[themeName] = themePath;
+  }
+
+  if (Object.keys(themes).length === 0) {
+    throw new Error('No valid themes found in themes.yml');
+  }
+
+  return themes;
+}
+
+function pathToThemeName(file: string) {
+  const basename = path.basename(file);
+  return basename.split(/[.?]/)[0] ?? '';
+}
+
+function isThemeFile(file: string, themes: Themes) {
+  if (!file.includes('/themes/')) {
+    return false;
+  }
+
+  const basename = pathToThemeName(file);
+  return basename in themes;
 }
