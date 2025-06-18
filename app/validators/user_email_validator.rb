94d4afac962369ff71aa2c3fe280729a5d04b505
Matt Jankowski <matt@jankowski.online>
2025-06-18 09:44:45 -0400

    Use `Regexp.escape` on domain values

--- app/validators/user_email_validator.rb
+++ app/validators/user_email_validator.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UserEmailValidator < ActiveModel::Validator
+  SEPARATOR = '|'
+
   def validate(user)
     return if user.valid_invitation? || user.email.blank?
 
@@ -25,19 +27,33 @@ class UserEmailValidator < ActiveModel::Validator
   def not_allowed_through_configuration?(email)
     return false if allowed_email_domains.blank?
 
-    domains = allowed_email_domains.gsub('.', '\.')
-    regexp  = Regexp.new("@(.+\\.)?(#{domains})$", true)
+    domains = escaped_domains(allowed_email_domains)
 
-    email !~ regexp
+    email !~ allowed_domain_pattern(domains)
   end
 
   def disallowed_through_configuration?(email)
     return false if denied_email_domains.blank?
 
-    domains = denied_email_domains.gsub('.', '\.')
-    regexp  = Regexp.new("@(.+\\.)?(#{domains})", true)
+    domains = escaped_domains(denied_email_domains)
 
-    regexp.match?(email)
+    denied_domain_pattern(domains).match?(email)
+  end
+
+  def allowed_domain_pattern(domains)
+    Regexp.new("@(.+\\.)?(#{domains})$", true)
+  end
+
+  def denied_domain_pattern(domains)
+    Regexp.new("@(.+\\.)?(#{domains})", true)
+  end
+
+  def escaped_domains(domains)
+    domains
+      .split(SEPARATOR)
+      .map { |domain| Regexp.escape(domain) }
+      .join(SEPARATOR)
+      .to_s
   end
 
   def allowed_email_domains
