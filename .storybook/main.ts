e86ca6223927c8c05c1047f7dad740f124c86fb7
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-06-18 17:11:57 +0200

    just use static for now

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,7 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
-  staticDirs: ['../public', './static'],
+  staticDirs: ['./static'],
 };
 
 export default config;
