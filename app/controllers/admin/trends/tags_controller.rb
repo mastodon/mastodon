94bcf27819f0710e1203577fb427cfd5ac0276f0
David Roetzel <david@roetzel.de>
2025-06-23 10:39:41 +0200

    Make displayed count and records consistent
    Fixes #35085
    
    Use the same base query to count and fetch records, so the count
    and the actual records displayed are consistent.

--- app/controllers/admin/trends/tags_controller.rb
+++ app/controllers/admin/trends/tags_controller.rb
@@ -4,7 +4,7 @@ class Admin::Trends::TagsController < Admin::BaseController
   def index
     authorize :tag, :review?
 
-    @pending_tags_count = Tag.pending_review.async_count
+    @pending_tags_count = pending_tags.async_count
     @tags = filtered_tags.page(params[:page])
     @form = Trends::TagBatch.new
   end
@@ -24,6 +24,10 @@ class Admin::Trends::TagsController < Admin::BaseController
 
   def filtered_tags
     Trends::TagFilter.new(filter_params).results
+  end
+
+  def pending_tags
+    Trends::TagFilter.new(status: :pending_review).results
   end
 
   def filter_params
