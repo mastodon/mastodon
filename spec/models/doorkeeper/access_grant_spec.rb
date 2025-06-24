7e27e1cb9d843156cf3c451022a7468dc60dd220
Matt Jankowski <matt@jankowski.online>
2025-06-24 16:41:30 -0400

    Add coverage for doorkeeper models

--- spec/models/doorkeeper/access_grant_spec.rb
+++ spec/models/doorkeeper/access_grant_spec.rb
@@ -11,4 +11,32 @@ RSpec.describe Doorkeeper::AccessGrant do
     it { is_expected.to validate_presence_of(:redirect_uri) }
     it { is_expected.to validate_presence_of(:token) }
   end
+
+  describe 'Scopes' do
+    describe '.expired' do
+      let!(:unexpired) { Fabricate :access_grant, expires_in: 10.hours }
+      let!(:expired) do
+        travel_to 10.minutes.ago do
+          Fabricate :access_grant, expires_in: 5.minutes
+        end
+      end
+
+      it 'returns records past their expired time' do
+        expect(described_class.expired)
+          .to include(expired)
+          .and not_include(unexpired)
+      end
+    end
+
+    describe '.revoked' do
+      let!(:revoked) { Fabricate :access_grant, revoked_at: 10.minutes.ago }
+      let!(:unrevoked) { Fabricate :access_grant, revoked_at: 10.minutes.from_now }
+
+      it 'returns records past their expired time' do
+        expect(described_class.revoked)
+          .to include(revoked)
+          .and not_include(unrevoked)
+      end
+    end
+  end
 end
