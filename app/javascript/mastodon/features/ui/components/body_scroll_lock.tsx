35e9f9b92d93064fb855cd2077c5b50191911fff
diondiondion <mail@diondiondion.com>
2025-06-25 20:56:33 +0200

    feat: Prevent scrolling behind menus and modals on Safari iOS

--- app/javascript/mastodon/features/ui/components/body_scroll_lock.tsx
+++ app/javascript/mastodon/features/ui/components/body_scroll_lock.tsx
@@ -1,7 +1,6 @@
-import { useLayoutEffect, useEffect, useState } from 'react';
+import { useLayoutEffect } from 'react';
 
 import { createAppSelector, useAppSelector } from 'mastodon/store';
-import { getScrollbarWidth } from 'mastodon/utils/scrollbar';
 
 const getShouldLockBodyScroll = createAppSelector(
   [
@@ -13,58 +12,19 @@ const getShouldLockBodyScroll = createAppSelector(
 );
 
 /**
- * This component locks scrolling on the `body` element when
+ * This component locks scrolling on the body when
  * `getShouldLockBodyScroll` returns true.
- *
- * The scrollbar width is taken into account and written to
- * a CSS custom property `--root-scrollbar-width`
  */
 
 export const BodyScrollLock: React.FC = () => {
   const shouldLockBodyScroll = useAppSelector(getShouldLockBodyScroll);
 
   useLayoutEffect(() => {
-    document.body.classList.toggle('with-modals--active', shouldLockBodyScroll);
+    document.documentElement.classList.toggle(
+      'has-modal',
+      shouldLockBodyScroll,
+    );
   }, [shouldLockBodyScroll]);
-
-  const [scrollbarWidth, setScrollbarWidth] = useState(() =>
-    getScrollbarWidth(),
-  );
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScrollbarWidth(getScrollbarWidth());
-    };
-    window.addEventListener('resize', handleResize, { passive: true });
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  // Inject style element to make scrollbar width available
-  // as CSS custom property
-  useLayoutEffect(() => {
-    const nonce = document
-      .querySelector('meta[name=style-nonce]')
-      ?.getAttribute('content');
-
-    if (nonce) {
-      const styleEl = document.createElement('style');
-      styleEl.nonce = nonce;
-      styleEl.innerHTML = `
-        :root {
-          --root-scrollbar-width: ${scrollbarWidth}px;
-        }
-      `;
-      document.head.appendChild(styleEl);
-
-      return () => {
-        document.head.removeChild(styleEl);
-      };
-    }
-
-    return () => '';
-  }, [scrollbarWidth]);
 
   return null;
 };
