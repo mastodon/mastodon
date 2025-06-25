241332f37c69a31e7b755ef169b1464f2c17dbcb
diondiondion <mail@diondiondion.com>
2025-06-25 13:11:34 +0200

    fix: Return mobile bailout because Safari iOS is silly

--- app/javascript/mastodon/utils/scrollbar.ts
+++ app/javascript/mastodon/utils/scrollbar.ts
@@ -1,4 +1,9 @@
+import { isMobile } from '../is_mobile';
+
 export const getScrollbarWidth = () => {
+  if (isMobile(window.innerWidth)) {
+    return 0;
+  }
   const outer = document.createElement('div');
   outer.style.visibility = 'hidden';
   outer.style.overflow = 'scroll';
