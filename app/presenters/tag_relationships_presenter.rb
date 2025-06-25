b8c8423e0b837415e2ce33df0a6a8587f1afa303
Matt Jankowski <matt@jankowski.online>
2025-06-25 13:36:27 -0400

    Remove unused `options` arg

--- app/presenters/tag_relationships_presenter.rb
+++ app/presenters/tag_relationships_presenter.rb
@@ -3,13 +3,13 @@
 class TagRelationshipsPresenter
   attr_reader :following_map, :featuring_map
 
-  def initialize(tags, current_account_id = nil, **options)
+  def initialize(tags, current_account_id = nil)
     if current_account_id.nil?
       @following_map = {}
       @featuring_map = {}
     else
-      @following_map = TagFollow.select(:tag_id).where(tag_id: tags.map(&:id), account_id: current_account_id).each_with_object({}) { |f, h| h[f.tag_id] = true }.merge(options[:following_map] || {})
-      @featuring_map = FeaturedTag.select(:tag_id).where(tag_id: tags.map(&:id), account_id: current_account_id).each_with_object({}) { |f, h| h[f.tag_id] = true }.merge(options[:featuring_map] || {})
+      @following_map = TagFollow.select(:tag_id).where(tag_id: tags.map(&:id), account_id: current_account_id).each_with_object({}) { |f, h| h[f.tag_id] = true }
+      @featuring_map = FeaturedTag.select(:tag_id).where(tag_id: tags.map(&:id), account_id: current_account_id).each_with_object({}) { |f, h| h[f.tag_id] = true }
     end
   end
 end
