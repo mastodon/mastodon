adfd608ee213fb9e182763ea00fb9c05f815a45b
Matt Jankowski <matt@jankowski.online>
2025-06-26 11:58:32 -0400

    Move `x.single_user_mode` config into `mastodon.x` area

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -148,7 +148,7 @@ class ApplicationController < ActionController::Base
   end
 
   def single_user_mode?
-    @single_user_mode ||= Rails.configuration.x.single_user_mode && Account.without_internal.exists?
+    @single_user_mode ||= Rails.configuration.x.mastodon.single_user_mode && Account.without_internal.exists?
   end
 
   def use_seamless_external_login?
