57de04ea46cdf11705e60dcb81b83a4ad6a59b3d
Eugen Rochko <eugen@zeonfederated.com>
2025-06-29 20:20:03 +0200

    Add automatic loading of new replies in web UI

--- app/javascript/mastodon/actions/statuses_typed.ts
+++ app/javascript/mastodon/actions/statuses_typed.ts
@@ -1,3 +1,5 @@
+import { createAction } from '@reduxjs/toolkit';
+
 import { apiGetContext } from 'mastodon/api/statuses';
 import { createDataLoadingThunk } from 'mastodon/store/typed_functions';
 
@@ -6,13 +8,18 @@ import { importFetchedStatuses } from './importer';
 export const fetchContext = createDataLoadingThunk(
   'status/context',
   ({ statusId }: { statusId: string }) => apiGetContext(statusId),
-  (context, { dispatch }) => {
+  ({ context, refresh }, { dispatch }) => {
     const statuses = context.ancestors.concat(context.descendants);
 
     dispatch(importFetchedStatuses(statuses));
 
     return {
       context,
+      refresh,
     };
   },
+);
+
+export const completeContextRefresh = createAction<{ statusId: string }>(
+  'status/context/complete',
 );
