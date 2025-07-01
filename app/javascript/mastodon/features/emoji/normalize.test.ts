1bdd7288d291700ec76a45b8011c99593b85ee3f
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-07-01 14:37:22 +0200

    add function to convert from Twemoji to Unicode standard

--- app/javascript/mastodon/features/emoji/normalize.test.ts
+++ app/javascript/mastodon/features/emoji/normalize.test.ts
@@ -3,37 +3,70 @@ import { basename, resolve } from 'path';
 
 import unicodeEmojis from 'emojibase-data/en/data.json';
 
-import { unicodeToTwemojiHex } from './normalize';
+import { twemojiToUnicodeInfo, unicodeToTwemojiHex } from './normalize';
+
+const emojiSVGFiles = await readdir(
+  // This assumes tests are run from project root
+  resolve(process.cwd(), 'public/emoji'),
+  {
+    withFileTypes: true,
+  },
+);
+const svgFileNames = emojiSVGFiles
+  .filter(
+    (file) =>
+      file.isFile() &&
+      file.name.endsWith('.svg') &&
+      !file.name.endsWith('_border.svg'),
+  )
+  .map((file) => basename(file.name, '.svg').toUpperCase());
 
 describe('normalizeEmoji', () => {
   describe('unicodeToSVGName', () => {
-    test.concurrent.each(
+    test.concurrent.for(
       unicodeEmojis
         // Our version of Twemoji only supports up to version 15.1
         .filter((emoji) => emoji.version < 16)
         .map((emoji) => [emoji.hexcode, emoji.label] as [string, string]),
-    )('verifying an emoji exists for %s (%s)', (hexcode) => {
+    )('verifying an emoji exists for %s (%s)', ([hexcode], { expect }) => {
       const result = unicodeToTwemojiHex(hexcode);
       expect(svgFileNames).toContain(result);
     });
   });
 
-  let svgFileNames: string[];
-  beforeAll(async () => {
-    const emojiSVGFiles = await readdir(
-      // This assumes tests are run from project root
-      resolve(process.cwd(), 'public/emoji'),
-      {
-        withFileTypes: true,
+  describe('twemojiToUnicodeInfo', () => {
+    const unicodeMap = new Map(
+      unicodeEmojis.flatMap((emoji) => {
+        const base: [string, string][] = [[emoji.hexcode, emoji.label]];
+        if (emoji.skins) {
+          base.push(
+            ...emoji.skins.map(
+              (skin) => [skin.hexcode, skin.label] as [string, string],
+            ),
+          );
+        }
+        return base;
+      }),
+    );
+
+    test.concurrent.for(svgFileNames)(
+      'verifying SVG file %s maps to Unicode emoji',
+      (svgFileName, { expect }) => {
+        assert(!!svgFileName);
+        const result = twemojiToUnicodeInfo(svgFileName);
+        const hexcode =
+          typeof result === 'string' ? result : result.unqualified;
+        if (!hexcode) {
+          // No hexcode means this is a special case like the Shibuya 109 emoji
+          expect(result).toHaveProperty('label');
+          return;
+        }
+        assert(!!hexcode);
+        expect(
+          unicodeMap.has(hexcode),
+          `${hexcode} (${svgFileName}) not found`,
+        ).toBeTruthy();
       },
     );
-    svgFileNames = emojiSVGFiles
-      .filter(
-        (file) =>
-          file.isFile() &&
-          file.name.endsWith('.svg') &&
-          !file.name.endsWith('_border.svg'),
-      )
-      .map((file) => basename(file.name, '.svg').toUpperCase());
   });
 });
--- app/javascript/mastodon/features/emoji/normalize.ts
+++ app/javascript/mastodon/features/emoji/normalize.ts
@@ -1,10 +1,31 @@
-const VARIATION_SELECTOR = 0xfe0f;
-const KEYCAP_MARK = 0x20e3;
+// Utility codes
+const VARIATION_SELECTOR_CODE = 0xfe0f;
+const KEYCAP_CODE = 0x20e3;
+
+// Gender codes
+const GENDER_FEMALE_CODE = 0x2640;
+const GENDER_MALE_CODE = 0x2642;
+
+// Skin tone codes
+const SKIN_TONE_CODES = [
+  0x1f3fb, // Light skin tone
+  0x1f3fc, // Medium-light skin tone
+  0x1f3fd, // Medium skin tone
+  0x1f3fe, // Medium-dark skin tone
+  0x1f3ff, // Dark skin tone
+] as const;
+
+// Misc codes that have special handling
+const SKIER_CODE = 0x26f7;
+const CHRISTMAS_TREE_CODE = 0x1f384;
+const MR_CLAUS_CODE = 0x1f385;
 const EYE_CODE = 0x1f441;
+const LEVITATING_PERSON_CODE = 0x1f574;
 const SPEECH_BUBBLE_CODE = 0x1f5e8;
+const MS_CLAUS_CODE = 0x1f936;
 
 export function unicodeToTwemojiHex(unicodeHex: string): string {
-  const codes = unicodeHex.split('-').map((code) => Number.parseInt(code, 16));
+  const codes = hexStringToNumbers(unicodeHex);
   const normalizedCodes: string[] = [];
   for (let i = 0; i < codes.length; i++) {
     const code = codes[i];
@@ -12,9 +33,9 @@ export function unicodeToTwemojiHex(unicodeHex: string): string {
       continue;
     }
     // Some emoji have their variation selector removed
-    if (code === VARIATION_SELECTOR) {
+    if (code === VARIATION_SELECTOR_CODE) {
       // Key emoji
-      if (i === 1 && codes.at(-1) === KEYCAP_MARK) {
+      if (i === 1 && codes.at(-1) === KEYCAP_CODE) {
         continue;
       }
       // Eye in speech bubble
@@ -27,4 +48,88 @@ export function unicodeToTwemojiHex(unicodeHex: string): string {
   }
 
   return normalizedCodes.join('-');
+}
+
+interface TwemojiSpecificEmoji {
+  unqualified?: string;
+  gender?: number;
+  skin?: number;
+  label?: string;
+}
+
+// Normalize man/woman to male/female
+const GENDER_CODES_MAP: Record<number, number> = {
+  [GENDER_FEMALE_CODE]: GENDER_FEMALE_CODE,
+  [GENDER_MALE_CODE]: GENDER_MALE_CODE,
+  // These are man/woman markers, but are used for gender sometimes.
+  [0x1f468]: GENDER_MALE_CODE,
+  [0x1f469]: GENDER_FEMALE_CODE,
+};
+
+const TWEMOJI_SPECIAL_CASES: Record<string, string | TwemojiSpecificEmoji> = {
+  '1F441-200D-1F5E8': '1F441-FE0F-200D-1F5E8-FE0F', // Eye in speech bubble
+  // An emoji that was never ported to the Unicode standard.
+  // See: https://emojipedia.org/shibuya
+  E50A: { label: 'Shibuya 109' },
+};
+
+export function twemojiToUnicodeInfo(
+  twemojiHex: string,
+): TwemojiSpecificEmoji | string {
+  const specialCase = TWEMOJI_SPECIAL_CASES[twemojiHex.toUpperCase()];
+  if (specialCase) {
+    return specialCase;
+  }
+  const codes = hexStringToNumbers(twemojiHex);
+  let gender: undefined | number;
+  let skin: undefined | number;
+  for (const code of codes) {
+    if (code in GENDER_CODES_MAP) {
+      gender = GENDER_CODES_MAP[code];
+    } else if (code in SKIN_TONE_CODES) {
+      skin = code;
+    }
+  }
+
+  let mappedCodes: unknown[] = codes;
+
+  if (codes.at(-1) === CHRISTMAS_TREE_CODE && codes.length >= 3) {
+    // Twemoji uses the christmas tree with a ZWJ for Mr. and Mrs. Claus,
+    // but in Unicode that only works for Mx. Claus.
+    const START_CODE =
+      gender === GENDER_FEMALE_CODE ? MS_CLAUS_CODE : MR_CLAUS_CODE;
+    mappedCodes = [START_CODE, skin];
+  } else if (codes.at(-1) === KEYCAP_CODE && codes.length === 2) {
+    // For key emoji, insert the variation selector
+    mappedCodes = [codes[0], VARIATION_SELECTOR_CODE, KEYCAP_CODE];
+  } else if (
+    codes.at(0) === SKIER_CODE ||
+    codes.at(0) === LEVITATING_PERSON_CODE
+  ) {
+    // Twemoji offers more gender and skin options for the skier and levitating person emoji.
+    return {
+      unqualified: hexNumbersToString([codes.at(0)]),
+      skin,
+      gender,
+    };
+  }
+
+  return hexNumbersToString(mappedCodes);
+}
+
+function hexStringToNumbers(hexString: string): number[] {
+  return hexString
+    .split('-')
+    .map((code) => Number.parseInt(code, 16))
+    .filter((code) => !Number.isNaN(code));
+}
+
+function hexNumbersToString(codes: unknown[]): string {
+  return codes
+    .filter(
+      (code): code is number =>
+        typeof code === 'number' && code > 0 && !Number.isNaN(code),
+    )
+    .map((code) => code.toString(16).padStart(4, '0').toUpperCase())
+    .join('-');
 }
