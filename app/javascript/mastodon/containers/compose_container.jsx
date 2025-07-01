d2f8a3888706471f01ea4e1339111435640f7483
Claire <claire.github-309c@sitedethib.com>
2025-07-01 16:18:25 +0200

    Fix `/share` not using server-set characters limit (#33459)

--- app/javascript/mastodon/containers/compose_container.jsx
+++ app/javascript/mastodon/containers/compose_container.jsx
@@ -3,18 +3,19 @@ import { PureComponent } from 'react';
 import { Provider } from 'react-redux';
 
 import { fetchCustomEmojis } from '../actions/custom_emojis';
+import { fetchServer } from '../actions/server';
 import { hydrateStore } from '../actions/store';
 import Compose from '../features/standalone/compose';
 import initialState from '../initial_state';
 import { IntlProvider } from '../locales';
 import { store } from '../store';
 
-
 if (initialState) {
   store.dispatch(hydrateStore(initialState));
 }
 
 store.dispatch(fetchCustomEmojis());
+store.dispatch(fetchServer());
 
 export default class ComposeContainer extends PureComponent {
 
