ca246710c5001244fd7bfd2a4d4d9b91a0e76d0f
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-07-03 13:29:31 +0200

    moves codes to constants file and adds more normalization functions

--- app/javascript/mastodon/features/emoji/normalize.test.ts
+++ app/javascript/mastodon/features/emoji/normalize.test.ts
@@ -1,9 +1,16 @@
 import { readdir } from 'fs/promises';
 import { basename, resolve } from 'path';
 
-import unicodeEmojis from 'emojibase-data/en/data.json';
+import { flattenEmojiData } from 'emojibase';
+import unicodeRawEmojis from 'emojibase-data/en/data.json';
 
-import { twemojiToUnicodeInfo, unicodeToTwemojiHex } from './normalize';
+import {
+  twemojiHasBorder,
+  twemojiToUnicodeInfo,
+  unicodeToTwemojiHex,
+  CODES_WITH_DARK_BORDER,
+  CODES_WITH_LIGHT_BORDER,
+} from './normalize';
 
 const emojiSVGFiles = await readdir(
   // This assumes tests are run from project root
@@ -13,60 +20,65 @@ const emojiSVGFiles = await readdir(
   },
 );
 const svgFileNames = emojiSVGFiles
-  .filter(
-    (file) =>
-      file.isFile() &&
-      file.name.endsWith('.svg') &&
-      !file.name.endsWith('_border.svg'),
-  )
+  .filter((file) => file.isFile() && file.name.endsWith('.svg'))
   .map((file) => basename(file.name, '.svg').toUpperCase());
+const svgFileNamesWithoutBorder = svgFileNames.filter(
+  (fileName) => !fileName.endsWith('_BORDER'),
+);
 
-describe('normalizeEmoji', () => {
-  describe('unicodeToSVGName', () => {
-    test.concurrent.for(
-      unicodeEmojis
-        // Our version of Twemoji only supports up to version 15.1
-        .filter((emoji) => emoji.version < 16)
-        .map((emoji) => [emoji.hexcode, emoji.label] as [string, string]),
-    )('verifying an emoji exists for %s (%s)', ([hexcode], { expect }) => {
-      const result = unicodeToTwemojiHex(hexcode);
-      expect(svgFileNames).toContain(result);
-    });
+const unicodeEmojis = flattenEmojiData(unicodeRawEmojis);
+
+describe('unicodeToTwemojiHex', () => {
+  test.concurrent.for(
+    unicodeEmojis
+      // Our version of Twemoji only supports up to version 15.1
+      .filter((emoji) => emoji.version < 16)
+      .map((emoji) => [emoji.hexcode, emoji.label] as [string, string]),
+  )('verifying an emoji exists for %s (%s)', ([hexcode], { expect }) => {
+    const result = unicodeToTwemojiHex(hexcode);
+    expect(svgFileNamesWithoutBorder).toContain(result);
   });
+});
 
-  describe('twemojiToUnicodeInfo', () => {
-    const unicodeMap = new Map(
-      unicodeEmojis.flatMap((emoji) => {
-        const base: [string, string][] = [[emoji.hexcode, emoji.label]];
-        if (emoji.skins) {
-          base.push(
-            ...emoji.skins.map(
-              (skin) => [skin.hexcode, skin.label] as [string, string],
-            ),
-          );
-        }
-        return base;
+describe('twemojiHasBorder', () => {
+  test.concurrent.for(
+    svgFileNames
+      .filter((file) => file.endsWith('_BORDER'))
+      .map((file) => {
+        const hexCode = file.replace('_BORDER', '');
+        return [
+          hexCode,
+          CODES_WITH_LIGHT_BORDER.includes(hexCode),
+          CODES_WITH_DARK_BORDER.includes(hexCode),
+        ] as const;
       }),
-    );
-
-    test.concurrent.for(svgFileNames)(
-      'verifying SVG file %s maps to Unicode emoji',
-      (svgFileName, { expect }) => {
-        assert(!!svgFileName);
-        const result = twemojiToUnicodeInfo(svgFileName);
-        const hexcode =
-          typeof result === 'string' ? result : result.unqualified;
-        if (!hexcode) {
-          // No hexcode means this is a special case like the Shibuya 109 emoji
-          expect(result).toHaveProperty('label');
-          return;
-        }
-        assert(!!hexcode);
-        expect(
-          unicodeMap.has(hexcode),
-          `${hexcode} (${svgFileName}) not found`,
-        ).toBeTruthy();
-      },
-    );
+  )('twemojiHasBorder for %s', ([hexCode, isLight, isDark]) => {
+    const result = twemojiHasBorder(hexCode);
+    expect(result).toHaveProperty('hexCode', hexCode);
+    expect(result).toHaveProperty('hasLightBorder', isLight);
+    expect(result).toHaveProperty('hasDarkBorder', isDark);
   });
+});
+
+describe.only('twemojiToUnicodeInfo', () => {
+  const unicodeCodeSet = new Set(unicodeEmojis.map((emoji) => emoji.hexcode));
+
+  test.concurrent.for(svgFileNamesWithoutBorder)(
+    'verifying SVG file %s maps to Unicode emoji',
+    (svgFileName, { expect }) => {
+      assert(!!svgFileName);
+      const result = twemojiToUnicodeInfo(svgFileName);
+      const hexcode = typeof result === 'string' ? result : result.unqualified;
+      if (!hexcode) {
+        // No hexcode means this is a special case like the Shibuya 109 emoji
+        expect(result).toHaveProperty('label');
+        return;
+      }
+      assert(!!hexcode);
+      expect(
+        unicodeCodeSet.has(hexcode),
+        `${hexcode} (${svgFileName}) not found`,
+      ).toBeTruthy();
+    },
+  );
 });
