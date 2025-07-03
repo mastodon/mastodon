fd09cfc608cbe84a0569d8c38f810e86fd25ef2d
Matt Jankowski <matt@jankowski.online>
2025-07-03 10:03:20 -0400

    Separate the status vs id methods

--- app/lib/annual_report/top_statuses.rb
+++ app/lib/annual_report/top_statuses.rb
@@ -2,9 +2,9 @@
 
 class AnnualReport::TopStatuses < AnnualReport::Source
   def generate
-    most_reblogged_status_id = base_scope.order(reblogs_count: :desc).first&.id
-    most_favourited_status_id = base_scope.where.not(id: most_reblogged_status_id).order(favourites_count: :desc).first&.id
-    most_replied_status_id = base_scope.where.not(id: [most_reblogged_status_id, most_favourited_status_id]).order(replies_count: :desc).first&.id
+    most_reblogged_status_id = most_reblogged_status&.id
+    most_favourited_status_id = most_favourited_status&.id
+    most_replied_status_id = most_replied_status&.id
 
     {
       top_statuses: {
@@ -16,6 +16,18 @@ class AnnualReport::TopStatuses < AnnualReport::Source
   end
 
   private
+
+  def most_reblogged_status
+    base_scope.order(reblogs_count: :desc).first
+  end
+
+  def most_favourited_status
+    base_scope.excluding(most_reblogged_status).order(favourites_count: :desc).first
+  end
+
+  def most_replied_status
+    base_scope.excluding(most_reblogged_status, most_favourited_status).order(replies_count: :desc).first
+  end
 
   def base_scope
     report_statuses.public_visibility.joins(:status_stat)
