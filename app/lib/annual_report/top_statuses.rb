6d6d50aadcff3f267fc0566cdd6985d09bac69fc
Matt Jankowski <matt@jankowski.online>
2025-07-03 10:17:12 -0400

    Wrap up format approach

--- app/lib/annual_report/top_statuses.rb
+++ app/lib/annual_report/top_statuses.rb
@@ -4,40 +4,42 @@ class AnnualReport::TopStatuses < AnnualReport::Source
   def generate
     {
       top_statuses: {
-        by_reblogs: most_reblogged_status_id,
-        by_favourites: most_favourited_status_id,
-        by_replies: most_replied_status_id,
+        by_reblogs: formatted_status(most_reblogged_status),
+        by_favourites: formatted_status(most_favourited_status),
+        by_replies: formatted_status(most_replied_status),
       },
     }
   end
 
   private
 
-  def most_reblogged_status_id
-    most_reblogged_status&.id&.to_s
-  end
-
-  def most_favourited_status_id
-    most_favourited_status&.id&.to_s
-  end
-
-  def most_replied_status_id
-    most_replied_status&.id&.to_s
+  def formatted_status(status)
+    status.id.to_s if status.present?
   end
 
   def most_reblogged_status
-    base_scope.order(reblogs_count: :desc).first
+    base_scope
+      .order(reblogs_count: :desc)
+      .first
   end
 
   def most_favourited_status
-    base_scope.excluding(most_reblogged_status).order(favourites_count: :desc).first
+    base_scope
+      .excluding(most_reblogged_status)
+      .order(favourites_count: :desc)
+      .first
   end
 
   def most_replied_status
-    base_scope.excluding(most_reblogged_status, most_favourited_status).order(replies_count: :desc).first
+    base_scope
+      .excluding(most_reblogged_status, most_favourited_status)
+      .order(replies_count: :desc)
+      .first
   end
 
   def base_scope
-    report_statuses.public_visibility.joins(:status_stat)
+    report_statuses
+      .public_visibility
+      .joins(:status_stat)
   end
 end
