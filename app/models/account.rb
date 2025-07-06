a7e10347096c26f32a94cca41f540b79a8adc422
Matt Jankowski <matt@jankowski.online>
2025-07-06 17:08:05 -0400

    Remove unused `Account.alphabetic` scope
    Originally used in admin/accounts controller, but since replaced by the
    filter class which uses other scopes for ordering.

--- app/models/account.rb
+++ app/models/account.rb
@@ -141,7 +141,6 @@ class Account < ApplicationRecord
   scope :bots, -> { where(actor_type: AUTOMATED_ACTOR_TYPES) }
   scope :non_automated, -> { where.not(actor_type: AUTOMATED_ACTOR_TYPES) }
   scope :groups, -> { where(actor_type: 'Group') }
-  scope :alphabetic, -> { order(domain: :asc, username: :asc) }
   scope :matches_uri_prefix, ->(value) { where(arel_table[:uri].matches("#{sanitize_sql_like(value)}/%", false, true)).or(where(uri: value)) }
   scope :matches_username, ->(value) { where('lower((username)::text) LIKE lower(?)', "#{value}%") }
   scope :matches_display_name, ->(value) { where(arel_table[:display_name].matches("#{value}%")) }
