b7e60a936afbcbc3b1c792f7e69ef052f2d4fc44
Claire <claire.github-309c@sitedethib.com>
2025-07-09 10:27:28 +0200

    Add more special characters to SMTP_PASSWORD test

--- spec/configuration/email_spec.rb
+++ spec/configuration/email_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'Configuration for email', type: :feature do
   context 'with special characters in SMTP_PASSWORD env variable' do
-    let(:password) { ']]123456789[[' }
+    let(:password) { ']]123456789[["!:@<>/\\=' }
 
     around do |example|
       ClimateControl.modify SMTP_PASSWORD: password do
