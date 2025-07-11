9c9442ab84659392a34b39f326c17f66b730a30f
Thomas Steiner <tomac@google.com>
2025-07-11 19:17:52 +0200

    Remove unused imports

--- app/javascript/mastodon/features/compose/components/compose_form.jsx
+++ app/javascript/mastodon/features/compose/components/compose_form.jsx
@@ -15,14 +15,12 @@ import { missingAltTextModal } from 'mastodon/initial_state';
 import AutosuggestInput from 'mastodon/components/autosuggest_input';
 import AutosuggestTextarea from 'mastodon/components/autosuggest_textarea';
 import { Button } from 'mastodon/components/button';
-import { LoadingIndicator } from 'mastodon/components/loading_indicator';
 import EmojiPickerDropdown from '../containers/emoji_picker_dropdown_container';
 import PollButtonContainer from '../containers/poll_button_container';
 import PrivacyDropdownContainer from '../containers/privacy_dropdown_container';
 import SpoilerButtonContainer from '../containers/spoiler_button_container';
 import UploadButtonContainer from '../containers/upload_button_container';
 import { countableText } from '../util/counter';
-import { debouncedGuess, countLetters } from '../util/language_detection';
 
 import { CharacterCounter } from './character_counter';
 import { EditIndicator } from './edit_indicator';
@@ -32,8 +30,6 @@ import { PollForm } from "./poll_form";
 import { ReplyIndicator } from './reply_indicator';
 import { UploadForm } from './upload_form';
 import { Warning } from './warning';
-
-import { connect } from 'react-redux';
 
 const allowedAroundShortCode = '><\u0085\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029\u0009\u000a\u000b\u000c\u000d';
 
