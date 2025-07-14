bd3c8df9c8dbc971fe195016285b9a987845ecea
Matt Jankowski <matt@jankowski.online>
2025-07-14 13:26:55 -0400

    Extract method for cache key calc

--- app/models/concerns/recommendation_maintenance.rb
+++ app/models/concerns/recommendation_maintenance.rb
@@ -3,6 +3,8 @@
 module RecommendationMaintenance
   extend ActiveSupport::Concern
 
+  FOLLOW_RECOMMENDATIONS_SCOPE = 'follow_recommendations'
+
   included do
     after_commit :invalidate_follow_recommendations_cache
   end
@@ -10,6 +12,10 @@ module RecommendationMaintenance
   private
 
   def invalidate_follow_recommendations_cache
-    Rails.cache.delete("follow_recommendations/#{account_id}")
+    Rails.cache.delete(follow_recommendations_cache_key)
+  end
+
+  def follow_recommendations_cache_key
+    [FOLLOW_RECOMMENDATIONS_SCOPE, account_id].join('/')
   end
 end
