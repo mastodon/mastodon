125b39af3d8cb56a96a28aa7ae33d8b982f878d5
diondiondion <mail@diondiondion.com>
2025-07-15 09:52:34 +0200

    fix: Improve `Dropdown` component accessibility (#35373)

--- app/javascript/mastodon/components/icon_button.tsx
+++ app/javascript/mastodon/components/icon_button.tsx
@@ -14,7 +14,6 @@ interface Props {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   onMouseDown?: React.MouseEventHandler<HTMLButtonElement>;
   onKeyDown?: React.KeyboardEventHandler<HTMLButtonElement>;
-  onKeyPress?: React.KeyboardEventHandler<HTMLButtonElement>;
   active?: boolean;
   expanded?: boolean;
   style?: React.CSSProperties;
@@ -45,7 +44,6 @@ export const IconButton = forwardRef<HTMLButtonElement, Props>(
       activeStyle,
       onClick,
       onKeyDown,
-      onKeyPress,
       onMouseDown,
       active = false,
       disabled = false,
@@ -84,16 +82,6 @@ export const IconButton = forwardRef<HTMLButtonElement, Props>(
       },
       [disabled, onClick],
     );
-
-    const handleKeyPress: React.KeyboardEventHandler<HTMLButtonElement> =
-      useCallback(
-        (e) => {
-          if (!disabled) {
-            onKeyPress?.(e);
-          }
-        },
-        [disabled, onKeyPress],
-      );
 
     const handleMouseDown: React.MouseEventHandler<HTMLButtonElement> =
       useCallback(
@@ -161,7 +149,6 @@ export const IconButton = forwardRef<HTMLButtonElement, Props>(
         onClick={handleClick}
         onMouseDown={handleMouseDown}
         onKeyDown={handleKeyDown}
-        onKeyPress={handleKeyPress} // eslint-disable-line @typescript-eslint/no-deprecated
         style={buttonStyle}
         tabIndex={tabIndex}
         disabled={disabled}
