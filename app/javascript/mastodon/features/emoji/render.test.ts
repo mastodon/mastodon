587066b91b54954f3bf1ee7bfcc338ae6bcb61d2
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-07-16 16:01:15 +0200

    starts work on rendering at last

---
+++ app/javascript/mastodon/features/emoji/render.test.ts
@@ -0,0 +1,105 @@
+import { EMOJI_MODE_NATIVE, EMOJI_MODE_NATIVE_WITH_FLAGS } from './constants';
+import { emojifyElement, tokenizeText } from './render';
+
+describe('emojifyElement', () => {
+  const testElement = document.createElement('div');
+  testElement.innerHTML = '<p>Hello 😊🇪🇺!</p><p>:custom:</p>';
+
+  test('emojifies custom emoji in native mode', () => {
+    const emojifiedElement = emojifyElement(testElement, EMOJI_MODE_NATIVE);
+    expect(emojifiedElement.innerHTML).toBe(
+      '<p>Hello 😊🇪🇺!</p>' +
+        '<p><img draggable="false" class="emojione custom-emoji" alt=":custom:" title=":custom:"></p>',
+    );
+  });
+
+  test('emojifies flag emoji in native-with-flags mode', () => {
+    const emojifiedElement = emojifyElement(
+      testElement,
+      EMOJI_MODE_NATIVE_WITH_FLAGS,
+    );
+    expect(emojifiedElement.innerHTML).toBe(
+      '<p>Hello 😊<img draggable="false" class="emojione" alt="🇪🇺" data-code="1F1EA-1F1FA">!</p>' +
+        '<p><img draggable="false" class="emojione custom-emoji" alt=":custom:" title=":custom:"></p>',
+    );
+  });
+
+  test('emojifies everything in twemoji mode', () => {
+    const emojifiedElement = emojifyElement(testElement, 'twemoji');
+    expect(emojifiedElement.innerHTML).toBe(
+      '<p>Hello <img draggable="false" class="emojione" alt="😊" data-code="1F60A">' +
+        '<img draggable="false" class="emojione" alt="🇪🇺" data-code="1F1EA-1F1FA">!</p>' +
+        '<p><img draggable="false" class="emojione custom-emoji" alt=":custom:" title=":custom:"></p>',
+    );
+  });
+});
+
+describe('tokenizeText', () => {
+  test('returns empty array for string with only whitespace', () => {
+    expect(tokenizeText('   \n')).toEqual([]);
+  });
+
+  test('returns an array of text to be a single token', () => {
+    expect(tokenizeText('Hello')).toEqual(['Hello']);
+  });
+
+  test('returns tokens for text with emoji', () => {
+    expect(tokenizeText('Hello 😊 🇿🇼!!')).toEqual([
+      'Hello ',
+      {
+        type: 'unicode',
+        code: '😊',
+      },
+      ' ',
+      {
+        type: 'unicode',
+        code: '🇿🇼',
+      },
+      '!!',
+    ]);
+  });
+
+  test('returns tokens for text with custom emoji', () => {
+    expect(tokenizeText('Hello :smile:!!')).toEqual([
+      'Hello ',
+      {
+        type: 'custom',
+        code: 'smile',
+      },
+      '!!',
+    ]);
+  });
+
+  test('handles custom emoji with underscores and numbers', () => {
+    expect(tokenizeText('Hello :smile_123:!!')).toEqual([
+      'Hello ',
+      {
+        type: 'custom',
+        code: 'smile_123',
+      },
+      '!!',
+    ]);
+  });
+
+  test('returns tokens for text with mixed emoji', () => {
+    expect(tokenizeText('Hello 😊 :smile:!!')).toEqual([
+      'Hello ',
+      {
+        type: 'unicode',
+        code: '😊',
+      },
+      ' ',
+      {
+        type: 'custom',
+        code: 'smile',
+      },
+      '!!',
+    ]);
+  });
+
+  test('does not capture custom emoji with invalid characters', () => {
+    expect(tokenizeText('Hello :smile-123:!!')).toEqual([
+      'Hello :smile-123:!!',
+    ]);
+  });
+});
