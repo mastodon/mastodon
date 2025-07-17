16b19f48cc8decab9a2e62ef5746a19b79c38e39
Fredys Fonseca <alkemist17@gmail.com>
2025-07-17 06:36:28 +0000

    Add admin MFA enforcement feature

--- spec/helpers/flashes_helper_spec.rb
+++ spec/helpers/flashes_helper_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe FlashesHelper do
       flash[:error] = 'an error'
       flash[:notice] = 'a notice'
       flash[:success] = 'a success'
+      flash[:warning] = 'a warning'
       flash[:not_user_facing] = 'a not user facing flash'
       # rubocop:enable Rails/I18nLocaleTexts
     end
@@ -19,7 +20,8 @@ RSpec.describe FlashesHelper do
         'alert' => 'an alert',
         'error' => 'an error',
         'notice' => 'a notice',
-        'success' => 'a success'
+        'success' => 'a success',
+        'warning' => 'a warning'
       )
     end
   end
