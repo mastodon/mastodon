33ed884ed03dcd6f0da708136ecf695acb4b07f7
diondiondion <mail@diondiondion.com>
2025-07-18 21:37:45 +0200

    Implement Konami code

--- app/javascript/mastodon/components/hotkeys/index.tsx
+++ app/javascript/mastodon/components/hotkeys/index.tsx
@@ -112,6 +112,19 @@ const hotkeyMatcherMap = {
   goToBlocked: sequence('g', 'b'),
   goToMuted: sequence('g', 'm'),
   goToRequests: sequence('g', 'r'),
+  donate: sequence(
+    'up',
+    'up',
+    'down',
+    'down',
+    'left',
+    'right',
+    'left',
+    'right',
+    'b',
+    'a',
+    'enter',
+  ),
 } as const;
 
 type HotkeyName = keyof typeof hotkeyMatcherMap;
