023a90d328bf30cc5cc11bf45d8f53e488bc7dc8
Matt Jankowski <matt@jankowski.online>
2025-07-20 13:26:58 -0400

    Move csv row generation for settings/imports#failures to `BulkImportRow`

--- app/models/bulk_import_row.rb
+++ app/models/bulk_import_row.rb
@@ -12,4 +12,27 @@
 #
 class BulkImportRow < ApplicationRecord
   belongs_to :bulk_import
+
+  def to_csv
+    case bulk_import.type.to_sym
+    when :following
+      [data['acct'], data.fetch('show_reblogs', true), data.fetch('notify', false), language_list]
+    when :blocking
+      [data['acct']]
+    when :muting
+      [data['acct'], data.fetch('hide_notifications', true)]
+    when :domain_blocking
+      [data['domain']]
+    when :bookmarks
+      [data['uri']]
+    when :lists
+      [data['list_name'], data['acct']]
+    end
+  end
+
+  private
+
+  def language_list
+    data['languages']&.join(', ')
+  end
 end
