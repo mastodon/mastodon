8998b6f94f1d0652c21a3da4a49e239028e9d523
Matt Jankowski <matt@jankowski.online>
2025-07-20 14:13:27 -0400

    Move list owner check to declaration in `ListAccount`

--- app/models/list_account.rb
+++ app/models/list_account.rb
@@ -18,11 +18,16 @@ class ListAccount < ApplicationRecord
   belongs_to :follow_request, optional: true
 
   validates :account_id, uniqueness: { scope: :list_id }
-  validate :validate_relationship
+
+  with_options unless: :list_owner_account_is_account? do
+    before_validation :set_follow
+
+    validate :verify_follow_or_follow_request_presence
+    validate :verify_follow_target_account, if: :follow_id?
+    validate :verify_follow_request_target_account, if: :follow_request_id?
+  end
 
   scope :active, -> { where.not(follow_id: nil) }
-
-  before_validation :set_follow, unless: :list_owner_account_is_account?
 
   private
 
@@ -31,12 +36,16 @@ class ListAccount < ApplicationRecord
     self.follow_request = FollowRequest.find_by(account_id: list.account_id, target_account_id: account.id) if follow.nil?
   end
 
-  def validate_relationship
-    return if list_owner_account_is_account?
-
+  def verify_follow_or_follow_request_presence
     errors.add(:account_id, :must_be_following) if follow_id.nil? && follow_request_id.nil?
-    errors.add(:follow, :invalid) if follow_id.present? && follow.target_account_id != account_id
-    errors.add(:follow_request, :invalid) if follow_request_id.present? && follow_request.target_account_id != account_id
+  end
+
+  def verify_follow_target_account
+    errors.add(:follow, :invalid) if follow.target_account_id != account_id
+  end
+
+  def verify_follow_request_target_account
+    errors.add(:follow_request, :invalid) if follow_request.target_account_id != account_id
   end
 
   def list_owner_account_is_account?
