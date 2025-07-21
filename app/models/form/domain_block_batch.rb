b0a54941b5956142611811b4c9353188f7cd1566
Matt Jankowski <matt@jankowski.online>
2025-07-21 16:45:29 -0400

    Add `Form::BaseBatch` class for "batch form update" objects

--- app/models/form/domain_block_batch.rb
+++ app/models/form/domain_block_batch.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-class Form::DomainBlockBatch
-  include ActiveModel::Model
-  include Authorization
-  include AccountableConcern
-
-  attr_accessor :domain_blocks_attributes, :action, :current_account
+class Form::DomainBlockBatch < Form::BaseBatch
+  attr_accessor :domain_blocks_attributes
 
   def save
     case action
