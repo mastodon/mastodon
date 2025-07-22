54f9f26ec74f984d925e446dea12f76609dbc6dc
Matt Jankowski <matt@jankowski.online>
2025-07-22 10:11:08 -0400

    Use to_label in view

--- app/controllers/admin/account_actions_controller.rb
+++ app/controllers/admin/account_actions_controller.rb
@@ -7,16 +7,13 @@ module Admin
     def new
       authorize @account, :show?
 
-      @account_action  = Admin::AccountAction.new(type: params[:type], report_id: params[:report_id], send_email_notification: true, include_statuses: true)
-      @warning_presets = AccountWarningPreset.all
+      @account_action = Admin::AccountAction.new(type: params[:type], report_id: params[:report_id], send_email_notification: true, include_statuses: true)
     end
 
     def create
       authorize @account, :show?
 
-      @account_action                 = Admin::AccountAction.new(resource_params)
-      @account_action.target_account  = @account
-      @account_action.current_account = current_account
+      @account_action = Admin::AccountAction.new(resource_params.merge(target_account: @account, current_account:))
 
       if @account_action.save
         if @account_action.with_report?
@@ -25,8 +22,6 @@ module Admin
           redirect_to admin_account_path(@account.id)
         end
       else
-        @warning_presets = AccountWarningPreset.all
-
         render :new
       end
     end
