53d975f0c8f569464614d177605b4c66ed45bb14
Claire <claire.github-309c@sitedethib.com>
2025-07-24 17:22:13 +0200

    Simplify test and avoid flakiness

--- spec/models/trends/statuses_spec.rb
+++ spec/models/trends/statuses_spec.rb
@@ -111,17 +111,16 @@ RSpec.describe Trends::Statuses do
     let!(:yesterday) { today - 1.day }
 
     let!(:status_foo) { Fabricate(:status, text: 'Foo', language: 'en', trendable: true, created_at: yesterday) }
-    let!(:status_bar) { Fabricate(:status, text: 'Bar', language: 'en', trendable: true, created_at: today) }
+    let!(:status_bar) { Fabricate(:status, text: 'Bar', language: 'en', trendable: true, created_at: today, quote: Quote.new(state: :accepted, quoted_status: status_foo)) }
     let!(:status_baz) { Fabricate(:status, text: 'Baz', language: 'en', trendable: true, created_at: today) }
     let!(:untrendable) { Fabricate(:status, text: 'Untrendable', language: 'en', trendable: true, visibility: :unlisted) }
-    let!(:quote_post) { Fabricate(:status, text: 'Quote!', language: 'en', trendable: true, created_at: today, quote: Quote.new(state: :accepted, quoted_status: status_foo)) }
     let!(:untrendable_quote) { Fabricate(:status, text: 'Untrendable quote!', language: 'en', trendable: true, created_at: today, quote: Quote.new(state: :accepted, quoted_status: untrendable)) }
 
     before do
       default_threshold_value.times { reblog(status_foo, today) }
       default_threshold_value.times { reblog(status_bar, today) }
       default_threshold_value.times { reblog(untrendable, today) }
-      default_threshold_value.times { reblog(quote_post, today) }
+      default_threshold_value.times { reblog(untrendable_quote, today) }
       (default_threshold_value - 1).times { reblog(status_baz, today) }
     end
 
@@ -133,7 +132,7 @@ RSpec.describe Trends::Statuses do
       it 'returns correct statuses from query' do
         results = subject.query.limit(10).to_a
 
-        expect(results).to eq [quote_post, status_bar, status_foo]
+        expect(results).to eq [status_bar, status_foo]
         expect(results).to_not include(status_baz, untrendable, untrendable_quote)
       end
     end
