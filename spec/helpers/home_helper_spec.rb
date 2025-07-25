960f6932194ed2d0dc19909b302e94d8eb1c7d88
Matt Jankowski <matt@jankowski.online>
2025-07-25 03:21:08 -0400

    Use `field` partial in admin account show view (#35503)

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -75,23 +75,19 @@ RSpec.describe HomeHelper do
     end
   end
 
-  describe 'custom_field_classes' do
-    context 'with a verified field' do
-      let(:field) { instance_double(Account::Field, verified?: true) }
+  describe 'field_verified_class' do
+    subject { helper.field_verified_class(verified) }
 
-      it 'returns verified string' do
-        result = helper.custom_field_classes(field)
-        expect(result).to eq 'verified'
-      end
+    context 'with a verified field' do
+      let(:verified) { true }
+
+      it { is_expected.to eq('verified') }
     end
 
     context 'with a non-verified field' do
-      let(:field) { instance_double(Account::Field, verified?: false) }
+      let(:verified) { false }
 
-      it 'returns verified string' do
-        result = helper.custom_field_classes(field)
-        expect(result).to eq 'emojify'
-      end
+      it { is_expected.to eq('emojify') }
     end
   end
 
