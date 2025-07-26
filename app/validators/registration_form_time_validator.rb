aee0fa41a9c37be86500a24a960868c4044b2342
Matt Jankowski <matt@jankowski.online>
2025-07-26 17:03:07 -0400

    Extract `User::Registration` concern

--- app/validators/registration_form_time_validator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RegistrationFormTimeValidator < ActiveModel::Validator
-  REGISTRATION_FORM_MIN_TIME = 3.seconds.freeze
-
-  def validate(user)
-    user.errors.add(:base, I18n.t('auth.too_fast')) if user.registration_form_time.present? && user.registration_form_time > REGISTRATION_FORM_MIN_TIME.ago
-  end
-end
