94e97257189dd4bd91106ea26e47d07b9494ab3b
Matt Jankowski <matt@jankowski.online>
2025-08-01 17:12:31 -0400

    Add `StatusDescriptionPresenter` to summarize status detail

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -14,47 +14,8 @@ module StatusesHelper
     end
   end
 
-  def media_summary(status)
-    attachments = { image: 0, video: 0, audio: 0 }
-
-    status.ordered_media_attachments.each do |media|
-      if media.video?
-        attachments[:video] += 1
-      elsif media.audio?
-        attachments[:audio] += 1
-      else
-        attachments[:image] += 1
-      end
-    end
-
-    text = attachments.to_a.reject { |_, value| value.zero? }.map { |key, value| I18n.t("statuses.attached.#{key}", count: value) }.join(' · ')
-
-    return if text.blank?
-
-    I18n.t('statuses.attached.description', attached: text)
-  end
-
-  def status_text_summary(status)
-    return if status.spoiler_text.blank?
-
-    I18n.t('statuses.content_warning', warning: status.spoiler_text)
-  end
-
-  def poll_summary(status)
-    return unless status.preloadable_poll
-
-    status.preloadable_poll.options.map { |o| "[ ] #{o}" }.join("\n")
-  end
-
   def status_description(status)
-    components = [[media_summary(status), status_text_summary(status)].compact_blank.join(' · ')]
-
-    if status.spoiler_text.blank?
-      components << status.text
-      components << poll_summary(status)
-    end
-
-    components.compact_blank.join("\n\n")
+    StatusDescriptionPresenter.new(status).description
   end
 
   def visibility_icon(status)
--- spec/helpers/statuses_helper_spec.rb
+++ spec/helpers/statuses_helper_spec.rb
@@ -3,39 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe StatusesHelper do
-  describe 'status_text_summary' do
-    context 'with blank text' do
-      let(:status) { Status.new(spoiler_text: '') }
-
-      it 'returns immediately with nil' do
-        result = helper.status_text_summary(status)
-        expect(result).to be_nil
-      end
-    end
-
-    context 'with present text' do
-      let(:status) { Status.new(spoiler_text: 'SPOILERS!!!') }
-
-      it 'returns the content warning' do
-        result = helper.status_text_summary(status)
-        expect(result).to eq(I18n.t('statuses.content_warning', warning: 'SPOILERS!!!'))
-      end
-    end
-  end
-
-  describe '#media_summary' do
-    it 'describes the media on a status' do
-      status = Fabricate :status
-      Fabricate :media_attachment, status: status, type: :video
-      Fabricate :media_attachment, status: status, type: :audio
-      Fabricate :media_attachment, status: status, type: :image
-
-      result = helper.media_summary(status)
-
-      expect(result).to eq('Attached: 1 image · 1 video · 1 audio')
-    end
-  end
-
   describe 'visibility_icon' do
     context 'with a status that is public' do
       let(:status) { Status.new(visibility: 'public') }
