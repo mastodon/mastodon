d915eb570e9d43ff60d7b1a1ee88d4e016507644
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-08-02 03:30:55 +0200

    Support multiple choice polls in status history modal

--- spec/fabricators/status_edit_fabricator.rb
+++ spec/fabricators/status_edit_fabricator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Fabricator(:status_edit) do
+  account { Fabricate.build(:account) }
   status { Fabricate.build(:status) }
+  poll_options { |attrs| attrs[:poll_options] }
 end
