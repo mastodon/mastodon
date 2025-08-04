77740f80e69d0b7b0bca56cd28b3e23c49851106
Claire <claire.github-309c@sitedethib.com>
2025-08-04 14:19:28 +0200

    Add quote notifications to WebUI

--- app/javascript/mastodon/features/notifications_v2/components/notification_group.tsx
+++ app/javascript/mastodon/features/notifications_v2/components/notification_group.tsx
@@ -15,6 +15,7 @@ import { NotificationFollowRequest } from './notification_follow_request';
 import { NotificationMention } from './notification_mention';
 import { NotificationModerationWarning } from './notification_moderation_warning';
 import { NotificationPoll } from './notification_poll';
+import { NotificationQuote } from './notification_quote';
 import { NotificationReblog } from './notification_reblog';
 import { NotificationSeveredRelationships } from './notification_severed_relationships';
 import { NotificationStatus } from './notification_status';
@@ -89,6 +90,11 @@ export const NotificationGroup: React.FC<{
     case 'mention':
       content = (
         <NotificationMention unread={unread} notification={notificationGroup} />
+      );
+      break;
+    case 'quote':
+      content = (
+        <NotificationQuote unread={unread} notification={notificationGroup} />
       );
       break;
     case 'follow':
