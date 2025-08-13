571bca69fa02260dfc9918d4d24c04595aeeaa86
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-08-13 18:19:09 +0200

    update api function to use new endpoint in #35769

--- app/javascript/mastodon/api/statuses.ts
+++ app/javascript/mastodon/api/statuses.ts
@@ -1,12 +1,7 @@
-import api, {
-  apiRequestGet,
-  apiRequestPut,
-  getAsyncRefreshHeader,
-} from 'mastodon/api';
+import api, { apiRequestPut, getAsyncRefreshHeader } from 'mastodon/api';
 import type {
   ApiContextJSON,
   ApiStatusJSON,
-  ApiStatusSourceJSON,
 } from 'mastodon/api_types/statuses';
 
 import type { ApiQuotePolicy } from '../api_types/quotes';
@@ -27,11 +22,10 @@ export const apiSetQuotePolicy = async (
   statusId: string,
   policy: ApiQuotePolicy,
 ) => {
-  const { text } = await apiRequestGet<ApiStatusSourceJSON>(
-    `v1/statuses/${statusId}/source`,
+  return apiRequestPut<ApiStatusJSON>(
+    `v1/statuses/${statusId}/interaction_policy`,
+    {
+      quote_approval_policy: policy,
+    },
   );
-  return apiRequestPut<ApiStatusJSON>(`v1/statuses/${statusId}`, {
-    quote_approval_policy: policy,
-    status: text,
-  });
 };
