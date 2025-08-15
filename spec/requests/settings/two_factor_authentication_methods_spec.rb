c5a075b6c2fc8c98ac58449b59ec7ac36fcf82ce
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-08-15 14:38:08 -0300

    test: update specs to not require OTP for enabling WebAuthn as 2FA
    Co-authored-by: Santiago Rodriguez <santiago.rodriguez@cedarcode.com>

--- spec/requests/settings/two_factor_authentication_methods_spec.rb
+++ spec/requests/settings/two_factor_authentication_methods_spec.rb
@@ -13,23 +13,4 @@ RSpec.describe 'Settings TwoFactorAuthenticationMethods' do
       end
     end
   end
-
-  context 'when signed in' do
-    let(:user) { Fabricate(:user) }
-
-    before { sign_in user }
-
-    describe 'GET to /settings/two_factor_authentication_methods' do
-      describe 'when user has not enabled otp' do
-        before { user.update(otp_required_for_login: false) }
-
-        it 'redirects to enable otp' do
-          get settings_two_factor_authentication_methods_path
-
-          expect(response)
-            .to redirect_to(settings_otp_authentication_path)
-        end
-      end
-    end
-  end
 end
