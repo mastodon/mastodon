ee06c423ce7397ccc2374c4101511fa7aedc2b10
Matt Jankowski <matt@jankowski.online>
2025-08-16 14:52:51 -0400

    Add coverage for when requested status is itself a reply

--- spec/requests/api/v1/statuses/contexts_spec.rb
+++ spec/requests/api/v1/statuses/contexts_spec.rb
@@ -3,39 +3,66 @@
 require 'rails_helper'
 
 RSpec.describe 'API V1 Statuses Contexts' do
-  context 'with an oauth token' do
-    let(:user) { Fabricate(:user) }
-    let(:client_app) { Fabricate(:application, name: 'Test app', website: 'http://testapp.com') }
-    let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, application: client_app, scopes: scopes) }
-    let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
+  describe 'GET /api/v1/statuses/:status_id/context' do
+    context 'with an oauth token' do
+      let(:user) { Fabricate(:user) }
+      let(:client_app) { Fabricate(:application, name: 'Test app', website: 'http://testapp.com') }
+      let(:token) { Fabricate(:accessible_access_token, resource_owner_id: user.id, application: client_app, scopes: scopes) }
+      let(:headers) { { 'Authorization' => "Bearer #{token.token}" } }
 
-    describe 'GET /api/v1/statuses/:status_id/context' do
       let(:scopes) { 'read:statuses' }
-      let(:status) { Fabricate(:status, account: user.account) }
 
-      before do
-        Fabricate(:status, account: user.account, thread: status)
+      context 'with a public status' do
+        let(:status) { Fabricate(:status, account: user.account) }
+
+        before { Fabricate(:status, account: user.account, thread: status) }
+
+        it 'returns http success' do
+          get "/api/v1/statuses/#{status.id}/context", headers: headers
+
+          expect(response)
+            .to have_http_status(200)
+          expect(response.content_type)
+            .to start_with('application/json')
+        end
       end
 
-      it 'returns http success' do
-        get "/api/v1/statuses/#{status.id}/context", headers: headers
+      context 'with a public status that is a reply' do
+        let(:status) { Fabricate(:status, account: user.account, thread: Fabricate(:status)) }
 
-        expect(response)
-          .to have_http_status(200)
-        expect(response.content_type)
-          .to start_with('application/json')
+        before { Fabricate(:status, account: user.account, thread: status) }
+
+        it 'returns http success' do
+          get "/api/v1/statuses/#{status.id}/context", headers: headers
+
+          expect(response)
+            .to have_http_status(200)
+          expect(response.content_type)
+            .to start_with('application/json')
+        end
       end
     end
-  end
 
-  context 'without an oauth token' do
-    context 'with a public status' do
-      let(:status) { Fabricate(:status, visibility: :public) }
+    context 'without an oauth token' do
+      context 'with a public status' do
+        let(:status) { Fabricate(:status, visibility: :public) }
 
-      describe 'GET /api/v1/statuses/:status_id/context' do
-        before do
-          Fabricate(:status, thread: status)
+        before { Fabricate(:status, thread: status) }
+
+        it 'returns http success' do
+          get "/api/v1/statuses/#{status.id}/context"
+
+          expect(response)
+            .to have_http_status(200)
+          expect(response.content_type)
+            .to start_with('application/json')
         end
+      end
+
+      context 'with a public status that is a reply' do
+        let(:status) { Fabricate(:status, visibility: :public, thread: Fabricate(:status)) }
+
+        before { Fabricate(:status, thread: status) }
 
         it 'returns http success' do
           get "/api/v1/statuses/#{status.id}/context"
