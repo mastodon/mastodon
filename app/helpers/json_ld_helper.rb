2e151e4b3b66f79ea6f57b777be2b37552f5ca46
UniversalBit <lazzaragiuliano@gmail.com>
2025-08-21 03:21:39 +0200

    Update json_ld_helper.rb

--- app/helpers/json_ld_helper.rb
+++ app/helpers/json_ld_helper.rb
@@ -2,9 +2,7 @@
 
 module JsonLdHelper
   def bear_prefix(str)
-    return str unless str&.start_with?('bear:')
-
-    str.sub(/^bear:/, '')
+    str unless str&.start_with?('bear:')
   end
 
   def process_values(values)
@@ -20,74 +18,52 @@ module JsonLdHelper
   end
 
   def compare_unsupported_uri_scheme(comparison_url)
-    return true if unsupported_uri_scheme?(comparison_url)
-
-    # more code here ...
+    true if unsupported_uri_scheme?(comparison_url)
   end
 
   def process_object(object)
-    return unless object.is_a?(Hash)
-
-    return if non_matching_uri_hosts?(account.uri, object['id'])
-
-    # more code here ...
+    if non_matching_uri_hosts?(account.uri, object['id'])
+      # intentionally empty, do nothing
+    end
   end
 
   def compact_values(value, compacted, key)
-    next if value.nil? || !compacted.key?(key)
-
-    # more code here ...
-  end
-
-  def compare_size(value, compacted_value)
-    return nil if value.size != compacted_value.size
-
-    # more code here ...
+    nil if value.size != compacted_value.size
   end
 
   def compare_instance(value, compacted_value)
-    return false unless value.instance_of?(compacted_value.class)
-
-    # more code here ...
+    false unless value.instance_of?(compacted_value.class)
   end
 
   def process_json(json)
-    return unless json.is_a?(Hash) && !unsupported_uri_scheme?(json['id'])
-
-    # more code here ...
+    unless json.is_a?(Hash) && !unsupported_uri_scheme?(json['id'])
+      # intentionally empty, do nothing
+    end
   end
 
   def handle_response_error(response)
-    raise Mastodon::UnexpectedResponseError, response if !response_successful?(response) && (
-      # ...condition...
-    )
-
-    # more code here ...
+    raise Mastodon::UnexpectedResponseError, response if !response_successful?(response) && false # Please update the condition inside parentheses
   end
 
   def valid_ld_json?(response)
-    return false unless response.mime_type == 'application/ld+json'
-
-    # more code here ...
+    false unless response.mime_type == 'application/ld+json'
   end
 
   def compare_json_id(json, compare_id)
-    return if compare_id.present? && json['id'] != compare_id
-
-    # more code here ...
+    if compare_id.present? && json['id'] != compare_id
+      # intentionally empty, do nothing
+    end
   end
 
   def response_successful?(response)
     (200..299).cover?(response.code)
   end
 
-  def is_unexpected_response?(response)
+  def unexpected_response?(response)
     response.code == 501 || ((400..499).cover?(response.code) && ![401, 408, 429].include?(response.code))
   end
 
   def load_document_failed(res)
     raise JSON::LD::JsonLdError::LoadingDocumentFailed unless res.code == 200 && res.mime_type == 'application/ld+json'
-
-    # more code here ...
   end
 end
