6db8db12469ff9435aeea189028fe29e404b22a4
diondiondion <mail@diondiondion.com>
2025-08-26 18:34:14 +0200

    Fix error alerts for deleted quotes (#35918)

--- app/javascript/mastodon/features/status/components/detailed_status.tsx
+++ app/javascript/mastodon/features/status/components/detailed_status.tsx
@@ -381,7 +381,10 @@ export const DetailedStatus: React.FC<{
             {hashtagBar}
 
             {status.get('quote') && (
-              <QuotedStatus quote={status.get('quote')} />
+              <QuotedStatus
+                quote={status.get('quote')}
+                parentQuotePostId={status.get('id')}
+              />
             )}
           </>
         )}
