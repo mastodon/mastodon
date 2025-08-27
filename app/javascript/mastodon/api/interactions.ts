81ab954c598c07a5aaf1dd29318a4b8e9aaa62ae
Claire <claire.github-309c@sitedethib.com>
2025-08-27 10:13:34 +0200

    Fix API return types for interaction API helpers (#35915)

--- app/javascript/mastodon/api/interactions.ts
+++ app/javascript/mastodon/api/interactions.ts
@@ -1,10 +1,11 @@
 import { apiRequestPost } from 'mastodon/api';
-import type { Status, StatusVisibility } from 'mastodon/models/status';
+import type { ApiStatusJSON } from 'mastodon/api_types/statuses';
+import type { StatusVisibility } from 'mastodon/models/status';
 
 export const apiReblog = (statusId: string, visibility: StatusVisibility) =>
-  apiRequestPost<{ reblog: Status }>(`v1/statuses/${statusId}/reblog`, {
+  apiRequestPost<{ reblog: ApiStatusJSON }>(`v1/statuses/${statusId}/reblog`, {
     visibility,
   });
 
 export const apiUnreblog = (statusId: string) =>
-  apiRequestPost<Status>(`v1/statuses/${statusId}/unreblog`);
+  apiRequestPost<ApiStatusJSON>(`v1/statuses/${statusId}/unreblog`);
