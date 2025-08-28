305db449e513d2e2db36954141f6161fd6b628d5
Jesse Karmani <jessekarmani@gmail.com>
2025-08-28 15:40:17 -0700

    Populute parent status and account info for new conversations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_05_075010) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_222741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -359,6 +359,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_05_075010) do
     t.string "uri"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.bigint "parent_status_id"
+    t.bigint "parent_account_id"
     t.index ["uri"], name: "index_conversations_on_uri", unique: true, opclass: :text_pattern_ops, where: "(uri IS NOT NULL)"
   end
 
