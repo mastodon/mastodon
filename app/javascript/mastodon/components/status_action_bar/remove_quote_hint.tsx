4a9c94cbb0b55a4467d71608504e0d91648a2bbf
diondiondion <mail@diondiondion.com>
2025-09-03 14:50:41 +0200

    Fix tooltip background blur

--- app/javascript/mastodon/components/status_action_bar/remove_quote_hint.tsx
+++ app/javascript/mastodon/components/status_action_bar/remove_quote_hint.tsx
@@ -42,49 +42,48 @@ export const RemoveQuoteHint: React.FC<{
         container={anchorRef.current}
       >
         {({ props, placement }) => (
-          <div {...props}>
-            <div
-              className={classNames(
-                'info-tooltip info-tooltip--solid dropdown-animation',
-                placement,
-              )}
+          <div
+            {...props}
+            className={classNames(
+              'info-tooltip info-tooltip--solid dropdown-animation',
+              placement,
+            )}
+          >
+            <h4>
+              <FormattedMessage
+                id='remove_quote_hint.title'
+                defaultMessage='Want to remove your quoted post?'
+              />
+            </h4>
+            <FormattedMessage
+              id='remove_quote_hint.message'
+              defaultMessage='You can do so from the {icon} options menu.'
+              values={{
+                icon: (
+                  <Icon
+                    id='ellipsis-h'
+                    icon={MoreHorizIcon}
+                    aria-label={intl.formatMessage({
+                      id: 'status.more',
+                      defaultMessage: 'More',
+                    })}
+                    style={{ verticalAlign: 'middle' }}
+                  />
+                ),
+              }}
             >
-              <h4>
-                <FormattedMessage
-                  id='remove_quote_hint.title'
-                  defaultMessage='Want to remove your quoted post?'
-                />
-              </h4>
-              <FormattedMessage
-                id='remove_quote_hint.message'
-                defaultMessage='You can do so from the {icon} options menu.'
-                values={{
-                  icon: (
-                    <Icon
-                      id='ellipsis-h'
-                      icon={MoreHorizIcon}
-                      aria-label={intl.formatMessage({
-                        id: 'status.more',
-                        defaultMessage: 'More',
-                      })}
-                      style={{ verticalAlign: 'middle' }}
-                    />
-                  ),
-                }}
-              >
-                {(text) => <p>{text}</p>}
-              </FormattedMessage>
-              <FormattedMessage
-                id='remove_quote_hint.button_label'
-                defaultMessage='Got it'
-              >
-                {(text) => (
-                  <Button compact onClick={dismiss}>
-                    {text}
-                  </Button>
-                )}
-              </FormattedMessage>
-            </div>
+              {(text) => <p>{text}</p>}
+            </FormattedMessage>
+            <FormattedMessage
+              id='remove_quote_hint.button_label'
+              defaultMessage='Got it'
+            >
+              {(text) => (
+                <Button compact onClick={dismiss}>
+                  {text}
+                </Button>
+              )}
+            </FormattedMessage>
           </div>
         )}
       </Overlay>
