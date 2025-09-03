779471b2c63365040289fbd5456cf637b92655cb
Matt Jankowski <matt@jankowski.online>
2025-09-03 09:40:30 -0400

    Add `application_scope_groups` helper method for form to use

--- spec/helpers/settings_helper_spec.rb
+++ spec/helpers/settings_helper_spec.rb
@@ -3,6 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe SettingsHelper do
+  describe '#application_scope_groups' do
+    subject { helper.application_scope_groups }
+
+    before { allow(Doorkeeper.configuration).to receive(:scopes).and_return(scopes) }
+
+    context 'with configured scopes' do
+      let(:scopes) { %w(read read:accounts profile write write:accounts) }
+
+      it { is_expected.to eq [%w(read read:accounts), %w(profile), %w(write write:accounts)] }
+    end
+
+    context 'with empty scopes' do
+      let(:scopes) { [] }
+
+      it { is_expected.to be_empty }
+    end
+  end
+
   describe 'session_device_icon' do
     context 'with a mobile device' do
       let(:session) { SessionActivation.new(user_agent: 'Mozilla/5.0 (iPhone)') }
