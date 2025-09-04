0a24e487ffb9ddaaa20787b84748385e7a058c5a
Matt Jankowski <matt@jankowski.online>
2025-09-04 12:15:34 -0400

    Use date attribute for date of birth

--- app/inputs/date_of_birth_input.rb
+++ app/inputs/date_of_birth_input.rb
@@ -2,25 +2,25 @@
 
 class DateOfBirthInput < SimpleForm::Inputs::Base
   OPTIONS = [
-    { autocomplete: 'bday-day', maxlength: 2, pattern: '[0-9]+', placeholder: 'DD' }.freeze,
-    { autocomplete: 'bday-month', maxlength: 2, pattern: '[0-9]+', placeholder: 'MM' }.freeze,
     { autocomplete: 'bday-year', maxlength: 4, pattern: '[0-9]+', placeholder: 'YYYY' }.freeze,
+    { autocomplete: 'bday-month', maxlength: 2, pattern: '[0-9]+', placeholder: 'MM' }.freeze,
+    { autocomplete: 'bday-day', maxlength: 2, pattern: '[0-9]+', placeholder: 'DD' }.freeze,
   ].freeze
 
   def input(wrapper_options = nil)
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
     merged_input_options[:inputmode] = 'numeric'
 
-    values = (object.public_send(attribute_name) || '').split('.')
+    values = (object.public_send(attribute_name) || '').to_s.split('-')
 
-    safe_join(Array.new(3) do |index|
+    safe_join(2.downto(0).map do |index|
       options = merged_input_options.merge(OPTIONS[index]).merge id: generate_id(index), 'aria-label': I18n.t("simple_form.labels.user.date_of_birth_#{index + 1}i"), value: values[index]
       @builder.text_field("#{attribute_name}(#{index + 1}i)", options)
     end)
   end
 
   def label_target
-    "#{attribute_name}_1i"
+    "#{attribute_name}_3i"
   end
 
   private
