86a549a0a245f4d4f4eac95af62edb146adacda9
Matt Jankowski <matt@jankowski.online>
2025-09-08 08:33:00 -0400

    dob cleanup

--- spec/validators/date_of_birth_validator_spec.rb
+++ spec/validators/date_of_birth_validator_spec.rb
@@ -8,18 +8,20 @@ RSpec.describe DateOfBirthValidator do
   before { Setting.min_age = 16 }
 
   context 'with an invalid date' do
-    it { is_expected.to_not allow_values('76.830.10').for(:date_of_birth).with_message(:invalid) }
+    let(:invalid_date) { '76.830.10' }
+
+    it { is_expected.to_not allow_values(invalid_date).for(:date_of_birth).with_message(:invalid) }
   end
 
-  context 'with a date below age limit' do
+  context 'with a date below the age limit' do
     let(:too_young) { 13.years.ago.strftime('%d.%m.%Y') }
 
     it { is_expected.to_not allow_values(too_young).for(:date_of_birth).with_message(:below_limit) }
   end
 
-  context 'with a date above age limit' do
-    let(:exact_age) { 16.years.ago.strftime('%d.%m.%Y') }
+  context 'with a date above the age limit' do
+    let(:old_enough) { 16.years.ago.strftime('%d.%m.%Y') }
 
-    it { is_expected.to allow_values(exact_age).for(:date_of_birth) }
+    it { is_expected.to allow_values(old_enough).for(:date_of_birth) }
   end
 end
