68daf669233eae8e30d8bf433a2d5ff5d46e960c
Matt Jankowski <matt@jankowski.online>
2025-09-08 08:47:20 -0400

    cleanup user email

--- spec/validators/user_email_validator_spec.rb
+++ spec/validators/user_email_validator_spec.rb
@@ -5,21 +5,23 @@ require 'rails_helper'
 RSpec.describe UserEmailValidator do
   subject { Fabricate.build :user, confirmed_at: nil }
 
-  context 'when email provider is blocked' do
-    before { Fabricate :email_domain_block, domain: 'mail.com' }
+  let(:email_address) { 'info@host.example' }
 
-    it { is_expected.to_not allow_value('info@mail.com').for(:email).with_message(:blocked) }
+  context 'when email provider is blocked' do
+    before { Fabricate :email_domain_block, domain: 'host.example' }
+
+    it { is_expected.to_not allow_value(email_address).for(:email).with_message(:blocked) }
   end
 
   context 'when email provider is not blocked' do
-    it { is_expected.to allow_value('info@mail.com').for(:email) }
+    it { is_expected.to allow_value(email_address).for(:email) }
   end
 
   context 'when canonical email address is blocked' do
-    let(:other_user) { Fabricate(:user, email: 'i.n.f.o@mail.com') }
+    let(:other_user) { Fabricate(:user, email: 'i.n.f.o@host.example') }
 
     before { other_user.account.suspend! }
 
-    it { is_expected.to_not allow_value('info@mail.com').for(:email).with_message(:taken) }
+    it { is_expected.to_not allow_value(email_address).for(:email).with_message(:taken) }
   end
 end
