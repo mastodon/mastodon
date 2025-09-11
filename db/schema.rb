f7289b251f8c0edc115f37013e88f6ae93de46dd
Claire <claire.github-309c@sitedethib.com>
2025-09-11 12:11:31 +0200

    Add migration to fill unset default quote policy based on default post privacy (#36041)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_09_100506) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_11_163952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
