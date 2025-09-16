563df2dd58a17cbe35389c1e2e56a7624c367496
diondiondion <mail@diondiondion.com>
2025-09-16 17:13:07 +0200

    Create reusable Alert/Snackbar component

---
+++ app/javascript/mastodon/components/alert/index.tsx
@@ -0,0 +1,65 @@
+import { useIntl } from 'react-intl';
+
+import classNames from 'classnames';
+
+import CloseIcon from '@/material-icons/400-24px/close.svg?react';
+
+import { IconButton } from '../icon_button';
+
+/**
+ * Snackbar/Toast-style notification component.
+ */
+export const Alert: React.FC<{
+  isActive?: boolean;
+  animateFrom?: 'start' | 'below';
+  title?: string;
+  message: string;
+  action?: string;
+  onActionClick?: () => void;
+  onDismiss?: () => void;
+}> = ({
+  isActive,
+  animateFrom = 'start',
+  title,
+  message,
+  action,
+  onActionClick,
+  onDismiss,
+}) => {
+  const intl = useIntl();
+
+  const hasAction = Boolean(action && onActionClick);
+
+  return (
+    <div
+      className={classNames('notification-bar', {
+        'notification-bar-active': isActive,
+        'from-start': animateFrom === 'start',
+        'from-below': animateFrom === 'below',
+      })}
+    >
+      {!!title && <span className='notification-bar-title'>{title}</span>}
+
+      <span className='notification-bar-message'>{message}</span>
+
+      {hasAction && (
+        <button className='notification-bar-action' onClick={onActionClick}>
+          {action}
+        </button>
+      )}
+
+      {!!onDismiss && (
+        <IconButton
+          title={intl.formatMessage({
+            id: 'alerts.dismiss',
+            defaultMessage: 'Dismiss',
+          })}
+          icon='times'
+          iconComponent={CloseIcon}
+          className='notification-bar-dismiss-button'
+          onClick={onDismiss}
+        />
+      )}
+    </div>
+  );
+};
