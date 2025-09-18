a892df127313ba228787df00c2553fcb11d82a36
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-09-18 14:42:38 +0200

    breaks out function to clean extra emoji

--- app/javascript/mastodon/components/html_contents/html_block.tsx
+++ app/javascript/mastodon/components/html_contents/html_block.tsx
@@ -1,6 +1,8 @@
 import type { FC, ReactNode } from 'react';
 import { useMemo } from 'react';
 
+import { cleanExtraEmojis } from '@/mastodon/features/emoji/normalize';
+import type { CustomEmojiMapArg } from '@/mastodon/features/emoji/types';
 import { createLimitedCache } from '@/mastodon/utils/cache';
 
 import emojify from '../../features/emoji/emoji';
@@ -9,23 +11,43 @@ import { htmlStringToComponents } from '../../utils/html';
 // Use a module-level cache to avoid re-rendering the same HTML multiple times.
 const cache = createLimitedCache<ReactNode>({ maxSize: 1000 });
 
-export const HTMLBlock: FC<{
+interface HTMLBlockProps {
   contents: string;
-}> = ({ contents: raw }) => {
+  extraEmojis?: CustomEmojiMapArg;
+}
+
+export const HTMLBlock: FC<HTMLBlockProps> = ({
+  contents: raw,
+  extraEmojis,
+}) => {
+  const customEmojis = useMemo(
+    () => cleanExtraEmojis(extraEmojis),
+    [extraEmojis],
+  );
   const contents = useMemo(() => {
-    if (cache.has(raw)) {
-      return cache.get(raw);
+    const key = JSON.stringify({ raw, customEmojis });
+    if (cache.has(key)) {
+      return cache.get(key);
     }
     const rendered = htmlStringToComponents(raw, {
       onText,
+      extraArgs: { customEmojis },
     });
-    cache.set(raw, rendered);
+
+    cache.set(key, rendered);
     return rendered;
-  }, [raw]);
+  }, [raw, customEmojis]);
 
   return contents;
 };
 
-function onText(text: string) {
-  return emojify(text);
+function onText(
+  text: string,
+  { customEmojis }: { customEmojis: CustomEmojiMapArg },
+) {
+  const result = emojify(text, customEmojis);
+  const components = htmlStringToComponents(result);
+
+  // TODO: Wire up new emoji rendering when it's sync.
+  return components;
 }
