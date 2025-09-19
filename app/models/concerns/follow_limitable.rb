155b1320a39f64d1be3173608c6fc151cf66da5c
Matt Jankowski <matt@jankowski.online>
2025-09-19 10:27:16 -0400

    Move shared callback to concern

--- app/models/concerns/follow_limitable.rb
+++ app/models/concerns/follow_limitable.rb
@@ -4,16 +4,21 @@ module FollowLimitable
   extend ActiveSupport::Concern
 
   included do
-    validates_with FollowLimitValidator, on: :create, unless: :bypass_follow_limit
+    rate_limit by: :account, family: :follows
 
     attribute :bypass_follow_limit, :boolean, default: false
+    validates_with FollowLimitValidator, on: :create, unless: :bypass_follow_limit
 
-    rate_limit by: :account, family: :follows
+    before_validation :set_uri, only: :create
 
     after_commit :invalidate_follow_recommendations_cache
   end
 
   private
+
+  def set_uri
+    self.uri = ActivityPub::TagManager.instance.generate_uri_for(self) if uri.nil?
+  end
 
   def invalidate_follow_recommendations_cache
     Rails.cache.delete("follow_recommendations/#{account_id}")
