11c72f6690eb8ed5da91e42b61a21483ba82f3f4
Daniel King <git@kin.gy>
2025-09-20 11:27:12 +0100

    Add flag to preserve cached media on cleanup
    Adding the flag --keep-interacted to the "media remove" tootctl command
    to preserve media cached from other servers if the related status has
    interactions from local accounts.
    
    For example, you may wish to delete all cached media from other servers
    older than 3 months to save disk space on the local server. However, if
    the status was bookmarked by a user they likely want to keep a copy
    locally, preserving the media in case the remote server is closed or has
    decided themselves to clear out all old user media.
    
    This flag was proposed in issue #30449.
    
    The change does not impact the local content retention setting, which
    manages automated cleanup. A new setting would need to be added there in
    a future improvement.
    
    This means to make use of this change a server admin would have to set a
    long content retention setting beyond which all remote media would be
    cleared, and then use a shorter range with the tootctl command where
    interacted media would be preserved with this new flag.
    
    The flag is only to preserve media attached to statuses, and makes no
    difference to the logic removing cached avatars and headers.

--- app/models/status.rb
+++ app/models/status.rb
@@ -90,6 +90,7 @@ class Status < ApplicationRecord
   has_many :local_favorited, -> { merge(Account.local) }, through: :favourites, source: :account
   has_many :local_reblogged, -> { merge(Account.local) }, through: :reblogs, source: :account
   has_many :local_bookmarked, -> { merge(Account.local) }, through: :bookmarks, source: :account
+  has_many :local_replied, -> { merge(Account.local) }, through: :replies, source: :account
 
   has_and_belongs_to_many :tags # rubocop:disable Rails/HasAndBelongsToMany
 
@@ -137,6 +138,12 @@ class Status < ApplicationRecord
     where('NOT EXISTS (SELECT * FROM statuses_tags forbidden WHERE forbidden.status_id = statuses.id AND forbidden.tag_id IN (?))', tag_ids)
   }
   scope :without_empty_attachments, -> { where(ordered_media_attachment_ids: nil).or(where.not(ordered_media_attachment_ids: [])) }
+  scope :with_local_interaction, lambda {
+    Status.where(id: Status.joins(:local_favorited).select(:id))
+          .or(Status.where(id: Status.joins(:local_bookmarked).select(:id)))
+          .or(Status.where(id: Status.joins(:local_replied).select(:id)))
+          .or(Status.where(id: Status.joins(:local_reblogged).select(:id)))
+  }
 
   after_create_commit :trigger_create_webhooks
   after_update_commit :trigger_update_webhooks
