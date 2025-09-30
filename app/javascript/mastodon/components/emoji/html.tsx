db6732e83aa5b54ce3bdde7348d1c261b716e281
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-09-30 16:55:11 +0200

    leverage new polymorphicForwardRef helper

--- app/javascript/mastodon/components/emoji/html.tsx
+++ app/javascript/mastodon/components/emoji/html.tsx
@@ -1,60 +1,72 @@
 import { useMemo } from 'react';
-import type { ComponentPropsWithoutRef, ElementType } from 'react';
 
 import classNames from 'classnames';
 
 import type { CustomEmojiMapArg } from '@/mastodon/features/emoji/types';
 import { isModernEmojiEnabled } from '@/mastodon/utils/environment';
 import { htmlStringToComponents } from '@/mastodon/utils/html';
+import { polymorphicForwardRef } from '@/types/polymorphic';
 
 import { AnimateEmojiProvider, CustomEmojiProvider } from './context';
 import { textToEmojis } from './index';
 
-type EmojiHTMLProps<Element extends ElementType = 'div'> = Omit<
-  ComponentPropsWithoutRef<Element>,
-  'dangerouslySetInnerHTML' | 'className'
-> & {
+interface EmojiHTMLProps {
   htmlString: string;
   extraEmojis?: CustomEmojiMapArg;
-  as?: Element;
   className?: string;
-};
+}
 
-export const ModernEmojiHTML = ({
-  extraEmojis,
-  htmlString,
-  as: asProp = 'div', // Rename for syntax highlighting
-  shallow,
-  className = '',
-  ...props
-}: EmojiHTMLProps<ElementType>) => {
-  const contents = useMemo(
-    () => htmlStringToComponents(htmlString, { onText: textToEmojis }),
-    [htmlString],
-  );
+export const ModernEmojiHTML = polymorphicForwardRef<'div', EmojiHTMLProps>(
+  (
+    {
+      extraEmojis,
+      htmlString,
+      as: asProp = 'div', // Rename for syntax highlighting
+      className = '',
+      ...props
+    },
+    ref,
+  ) => {
+    const contents = useMemo(
+      () => htmlStringToComponents(htmlString, { onText: textToEmojis }),
+      [htmlString],
+    );
 
-  return (
-    <CustomEmojiProvider emojis={extraEmojis}>
-      <AnimateEmojiProvider {...props} as={asProp} className={className}>
-        {contents}
-      </AnimateEmojiProvider>
-    </CustomEmojiProvider>
-  );
-};
+    return (
+      <CustomEmojiProvider emojis={extraEmojis}>
+        <AnimateEmojiProvider
+          {...props}
+          as={asProp}
+          className={className}
+          ref={ref}
+        >
+          {contents}
+        </AnimateEmojiProvider>
+      </CustomEmojiProvider>
+    );
+  },
+);
 
-export const LegacyEmojiHTML = <Element extends ElementType>(
-  props: EmojiHTMLProps<Element>,
-) => {
-  const { as: asElement, htmlString, extraEmojis, className, ...rest } = props;
-  const Wrapper = asElement ?? 'div';
-  return (
-    <Wrapper
-      {...rest}
-      dangerouslySetInnerHTML={{ __html: htmlString }}
-      className={classNames(className, 'animate-parent')}
-    />
-  );
-};
+export const LegacyEmojiHTML = polymorphicForwardRef<'div', EmojiHTMLProps>(
+  (props, ref) => {
+    const {
+      as: asElement,
+      htmlString,
+      extraEmojis,
+      className,
+      ...rest
+    } = props;
+    const Wrapper = asElement ?? 'div';
+    return (
+      <Wrapper
+        {...rest}
+        ref={ref}
+        dangerouslySetInnerHTML={{ __html: htmlString }}
+        className={classNames(className, 'animate-parent')}
+      />
+    );
+  },
+);
 
 export const EmojiHTML = isModernEmojiEnabled()
   ? ModernEmojiHTML
