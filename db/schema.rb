975e23f96d3c48489a3f9a9c5a8b8a5486ea9bea
Claire <claire.github-309c@sitedethib.com>
2025-10-02 16:20:46 +0200

    Split `timeline_preview` boolean setting in 4 settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_24_170259) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_02_140103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
