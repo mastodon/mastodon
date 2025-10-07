48e40e918c0b681b52fa02d0c587053bf3642e97
Claire <claire.github-309c@sitedethib.com>
2025-10-07 15:14:38 +0200

    Fix `"quote": { "type": "Tombstone" }` being ignored

--- app/services/activitypub/process_status_update_service.rb
+++ app/services/activitypub/process_status_update_service.rb
@@ -298,7 +298,7 @@ class ActivityPub::ProcessStatusUpdateService < BaseService
   def update_quote!
     quote_uri = @status_parser.quote_uri
 
-    if quote_uri.present?
+    if @status_parser.quote?
       approval_uri = @status_parser.quote_approval_uri
       approval_uri = nil if unsupported_uri_scheme?(approval_uri) || TagManager.instance.local_url?(approval_uri)
 
