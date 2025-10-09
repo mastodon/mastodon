5a8bb78e907fa532eeb6cf9c10957c440d65a715
Echo <ChaosExAnima@users.noreply.github.com>
2025-10-09 15:26:36 +0200

    Update app/javascript/mastodon/components/status/handled_link.tsx
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/mastodon/components/status/handled_link.tsx
+++ app/javascript/mastodon/components/status/handled_link.tsx
@@ -38,7 +38,7 @@ export const HandledLink: FC<HandledLinkProps & ComponentProps<'a'>> = ({
         {children}
       </Link>
     );
-  } else if ((text.startsWith('@') || prevText?.endsWith('#')) && mention) {
+  } else if ((text.startsWith('@') || prevText?.endsWith('@')) && mention) {
     // Handle mentions
     return (
       <Link
