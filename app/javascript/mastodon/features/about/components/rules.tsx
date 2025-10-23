0248cbec4ba450ef1a75022e63f83c512b52ff4d
diondiondion <mail@diondiondion.com>
2025-10-23 12:44:52 +0200

    Move select icon to dedicated class

--- app/javascript/mastodon/features/about/components/rules.tsx
+++ app/javascript/mastodon/features/about/components/rules.tsx
@@ -104,17 +104,19 @@ export const RulesSection: FC<RulesSectionProps> = ({ isLoading = false }) => {
               defaultMessage='Language'
             />
           </label>
-          <select onChange={handleLocaleChange} id='language-select'>
-            {localeOptions.map((option) => (
-              <option
-                key={option.value}
-                value={option.value}
-                selected={option.value === selectedLocale}
-              >
-                {option.text}
-              </option>
-            ))}
-          </select>
+          <div className='select-wrapper'>
+            <select onChange={handleLocaleChange} id='language-select'>
+              {localeOptions.map((option) => (
+                <option
+                  key={option.value}
+                  value={option.value}
+                  selected={option.value === selectedLocale}
+                >
+                  {option.text}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       )}
     </Section>
