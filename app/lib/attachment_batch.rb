40c4f903759d2b87f78c414db064392be24f38d1
Claire <claire.github-309c@sitedethib.com>
2025-11-24 14:07:47 +0100

    Revert "Increase HTTP read timeout for expensive S3 batch delete operation (#36971)"
    This reverts commit a54334b7143a4483eb35b00e2331ccfcfb00b833.

--- app/lib/attachment_batch.rb
+++ app/lib/attachment_batch.rb
@@ -112,17 +112,10 @@ class AttachmentBatch
     keys.each_slice(LIMIT) do |keys_slice|
       logger.debug { "Deleting #{keys_slice.size} objects" }
 
-      bucket.delete_objects(
-        {
-          delete: {
-            objects: keys_slice.map { |key| { key: key } },
-            quiet: true,
-          },
-        },
-        {
-          http_read_timeout: [Paperclip::Attachment.default_options[:s3_options][:http_read_timeout], 120].max,
-        }
-      )
+      bucket.delete_objects(delete: {
+        objects: keys_slice.map { |key| { key: key } },
+        quiet: true,
+      })
     rescue => e
       retries += 1
 
