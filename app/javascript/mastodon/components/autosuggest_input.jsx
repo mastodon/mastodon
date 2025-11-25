86e463c0e826bedf71b41822d9b1d1fe4be3808a
Claire <claire.github-309c@sitedethib.com>
2025-11-25 14:17:44 +0100

    Fix compose autosuggest always lowercasing token (#36995)

--- app/javascript/mastodon/components/autosuggest_input.jsx
+++ app/javascript/mastodon/components/autosuggest_input.jsx
@@ -28,7 +28,7 @@ const textAtCursorMatchesToken = (str, caretPosition, searchTokens) => {
     return [null, null];
   }
 
-  word = word.trim().toLowerCase();
+  word = word.trim();
 
   if (word.length > 0) {
     return [left + 1, word];
