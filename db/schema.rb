28e9f450f6b4163c8574ec5950ce0fe147e4f101
Claire <claire.github-309c@sitedethib.com>
2025-11-28 12:17:09 +0100

    Add shareable wrapstodon links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_19_093332) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_28_110424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -615,6 +615,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_19_093332) do
     t.datetime "viewed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "share_key"
     t.index ["account_id", "year"], name: "index_generated_annual_reports_on_account_id_and_year", unique: true
   end
 
