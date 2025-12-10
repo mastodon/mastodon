17e3fab924c64fdc229a32ff754895153e7b9b28
David Roetzel <david@roetzel.de>
2025-12-10 15:13:20 +0100

    Add basic service to add account to a collection
    This also includes the first time to check for an account's
    preferences with regards to discoverability. As such, only
    local accounts are supported for now until we federate these
    preferences.

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -32,11 +32,21 @@ class CollectionItem < ApplicationRecord
   validates :account, presence: true, if: :accepted?
   validates :object_uri, presence: true, if: -> { account.nil? }
 
+  before_validation :set_position, on: :create
+
   scope :ordered, -> { order(position: :asc) }
   scope :with_accounts, -> { includes(account: [:account_stat, :user]) }
   scope :not_blocked_by, ->(account) { where.not(accounts: { id: account.blocking }) }
 
   def local_item_with_remote_account?
     local? && account&.remote?
+  end
+
+  private
+
+  def set_position
+    return if position_changed?
+
+    self.position = self.class.where(collection_id:).maximum(:position).to_i + 1
   end
 end
