b7ff957ca976138062cef2ed7b6ee63f51a3fbbe
diondiondion <mail@diondiondion.com>
2025-12-11 15:09:43 +0100

    Fix scrolling not working on iOS

--- app/javascript/mastodon/features/annual_report/modal.tsx
+++ app/javascript/mastodon/features/annual_report/modal.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 import classNames from 'classnames';
+
+import { closeModal } from '@/mastodon/actions/modal';
+import { useAppDispatch } from '@/mastodon/store';
 
 import { AnnualReport } from '.';
 import styles from './index.module.scss';
@@ -12,13 +15,24 @@ const AnnualReportModal: React.FC<{
     onChangeBackgroundColor('var(--color-bg-media-base)');
   }, [onChangeBackgroundColor]);
 
+  const dispatch = useAppDispatch();
+  const handleCloseModal = useCallback(() => {
+    dispatch(closeModal({ modalType: 'ANNUAL_REPORT', ignoreFocus: false }));
+  }, [dispatch]);
+
   return (
+    // It's fine not to provide a keyboard handler here since there is a global
+    // [Esc] key listener that will close open modals.
+    // This onClick handler is needed since the modalWrapper styles overlap the
+    // default modal backdrop, preventing clicks to pass through.
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div
       className={classNames(
         'modal-root__modal',
         styles.modalWrapper,
         'theme-dark',
       )}
+      onClick={handleCloseModal}
     >
       <AnnualReport context='modal' />
     </div>
