9dabd292770798375776b12a6db74f6c38eec05f
Shlee <github@shl.ee>
2025-12-13 23:16:41 +1030

    Update mail_delivery_job.rb

--- config/initializers/mail_delivery_job.rb
+++ config/initializers/mail_delivery_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActionMailer::MailDeliveryJob.class_eval do
-  discard_on ActiveJob::DeserializationError do |job, error|
+  discard_on ActiveJob::DeserializationError do |_job, error|
     raise error unless error.cause.is_a?(ActiveRecord::RecordNotFound)
   end
 end
