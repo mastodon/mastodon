7230c2059f1599809f812feeaad94c77aebf113e
Matt Jankowski <matt@jankowski.online>
2025-12-16 05:24:27 -0500

    Add coverage for "domain variants" consumers (#35995)

--- spec/models/email_domain_block_spec.rb
+++ spec/models/email_domain_block_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe EmailDomainBlock do
   describe 'block?' do
+    subject { described_class.block?(input) }
+
     let(:input) { nil }
 
     context 'when given an e-mail address' do
@@ -14,12 +16,12 @@ RSpec.describe EmailDomainBlock do
 
         it 'returns true if the domain is blocked' do
           Fabricate(:email_domain_block, domain: 'example.com')
-          expect(described_class.block?(input)).to be true
+          expect(subject).to be true
         end
 
         it 'returns false if the domain is not blocked' do
           Fabricate(:email_domain_block, domain: 'other-example.com')
-          expect(described_class.block?(input)).to be false
+          expect(subject).to be false
         end
       end
 
@@ -28,7 +30,7 @@ RSpec.describe EmailDomainBlock do
 
         it 'returns true if it is a subdomain of a blocked domain' do
           Fabricate(:email_domain_block, domain: 'example.com')
-          expect(described_class.block?(input)).to be true
+          expect(subject).to be true
         end
       end
     end
@@ -38,8 +40,40 @@ RSpec.describe EmailDomainBlock do
 
       it 'returns true if the domain is blocked' do
         Fabricate(:email_domain_block, domain: 'mail.foo.com')
-        expect(described_class.block?(input)).to be true
+        expect(subject).to be true
       end
+    end
+
+    context 'when given nil' do
+      it { is_expected.to be false }
+    end
+
+    context 'when given empty string' do
+      let(:input) { '' }
+
+      it { is_expected.to be true }
+    end
+  end
+
+  describe '.requires_approval?' do
+    subject { described_class.requires_approval?(input) }
+
+    let(:input) { nil }
+
+    context 'with a matching block requiring approval' do
+      before { Fabricate :email_domain_block, domain: input, allow_with_approval: true }
+
+      let(:input) { 'host.example' }
+
+      it { is_expected.to be true }
+    end
+
+    context 'with a matching block not requiring approval' do
+      before { Fabricate :email_domain_block, domain: input, allow_with_approval: false }
+
+      let(:input) { 'host.example' }
+
+      it { is_expected.to be false }
     end
   end
 end
