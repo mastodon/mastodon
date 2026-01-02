c46dc5f4c4f8d3a3d189a47fb45423ba57af0f47
Shlee <github@shl.ee>
2026-01-02 23:39:40 +0700

    Update grouped_notifications_presenter.rb

--- app/presenters/grouped_notifications_presenter.rb
+++ app/presenters/grouped_notifications_presenter.rb
@@ -13,15 +13,15 @@ class GroupedNotificationsPresenter < ActiveModelSerializers::Model
   end
 
   def statuses
-    @grouped_notifications.filter_map(&:target_status).uniq(&:id)
+    @grouped_notifications.filter_map(&:target_status).compact.uniq(&:id)
   end
 
   def accounts
     @accounts ||= begin
       if partial_avatars?
-        @grouped_notifications.map { |group| group.sample_accounts.first }.uniq(&:id)
+        @grouped_notifications.map { |group| group.sample_accounts.first }.compact.uniq(&:id)
       else
-        @grouped_notifications.flat_map(&:sample_accounts).uniq(&:id)
+        @grouped_notifications.flat_map(&:sample_accounts).compact.uniq(&:id)
       end
     end
   end
@@ -29,7 +29,7 @@ class GroupedNotificationsPresenter < ActiveModelSerializers::Model
   def partial_accounts
     @partial_accounts ||= begin
       if partial_avatars?
-        @grouped_notifications.flat_map { |group| group.sample_accounts[1...] }.uniq(&:id).filter { |account| accounts.exclude?(account) }
+        @grouped_notifications.flat_map { |group| group.sample_accounts[1...] }.compact.uniq(&:id).filter { |account| accounts.exclude?(account) }
       else
         []
       end
