9b68ad2bdf078a27d59005a01e041460e728eba8
Claire <claire.github-309c@sitedethib.com>
2026-01-12 17:07:42 +0100

    Use data attributes for JS-set theme toggles

--- app/javascript/inline/theme-selection.js
+++ app/javascript/inline/theme-selection.js
@@ -2,6 +2,5 @@ const {userTheme} = document.documentElement.dataset;
 const useDarkMode = userTheme !== 'mastodon-light' || userTheme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const useHighContrast = userTheme === 'contrast' || window.matchMedia('(prefers-contrast: more)').matches;
 
-document.documentElement.classList.toggle('theme-dark', useDarkMode);
-document.documentElement.classList.toggle('theme-light', !useDarkMode);
-document.documentElement.classList.toggle('theme-high-contrast', useHighContrast);
+document.documentElement.dataset.mode = useDarkMode ? 'dark' : 'light';
+document.documentElement.dataset.contrast = useHighContrast ? 'high' : 'default';
