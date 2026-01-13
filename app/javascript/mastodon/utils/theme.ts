41639655ffaa9914632fe937c9583b901b36f5cc
diondiondion <mail@diondiondion.com>
2026-01-13 12:06:54 +0100

    Fix `isDarkMode` utility (#37470)

--- app/javascript/mastodon/utils/theme.ts
+++ app/javascript/mastodon/utils/theme.ts
@@ -5,9 +5,7 @@ export function getUserTheme() {
 
 export function isDarkMode() {
   const { userTheme } = document.documentElement.dataset;
-  return (
-    (userTheme === 'system' &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches) ||
-    userTheme !== 'mastodon-light'
-  );
+  return userTheme === 'system'
+    ? window.matchMedia('(prefers-color-scheme: dark)').matches
+    : userTheme !== 'mastodon-light';
 }
