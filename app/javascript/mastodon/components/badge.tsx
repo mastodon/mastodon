5612237e4c8afd4b52678a8bc37d95e7f1b5b9e6
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-01-20 14:17:16 +0100

    converts Badge component to TS

--- app/javascript/mastodon/components/badge.tsx
+++ app/javascript/mastodon/components/badge.tsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import type { FC, ReactNode } from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
@@ -6,8 +6,12 @@ import GroupsIcon from '@/material-icons/400-24px/group.svg?react';
 import PersonIcon from '@/material-icons/400-24px/person.svg?react';
 import SmartToyIcon from '@/material-icons/400-24px/smart_toy.svg?react';
 
-
-export const Badge = ({ icon = <PersonIcon />, label, domain, roleId }) => (
+export const Badge: FC<{
+  label: ReactNode;
+  icon?: ReactNode;
+  domain?: ReactNode;
+  roleId?: string;
+}> = ({ icon = <PersonIcon />, label, domain, roleId }) => (
   <div className='account-role' data-account-role-id={roleId}>
     {icon}
     {label}
@@ -15,17 +19,20 @@ export const Badge = ({ icon = <PersonIcon />, label, domain, roleId }) => (
   </div>
 );
 
-Badge.propTypes = {
-  icon: PropTypes.node,
-  label: PropTypes.node,
-  domain: PropTypes.node,
-  roleId: PropTypes.string
-};
-
 export const GroupBadge = () => (
-  <Badge icon={<GroupsIcon />} label={<FormattedMessage id='account.badges.group' defaultMessage='Group' />} />
+  <Badge
+    icon={<GroupsIcon />}
+    label={
+      <FormattedMessage id='account.badges.group' defaultMessage='Group' />
+    }
+  />
 );
 
 export const AutomatedBadge = () => (
-  <Badge icon={<SmartToyIcon />} label={<FormattedMessage id='account.badges.bot' defaultMessage='Automated' />} />
+  <Badge
+    icon={<SmartToyIcon />}
+    label={
+      <FormattedMessage id='account.badges.bot' defaultMessage='Automated' />
+    }
+  />
 );
