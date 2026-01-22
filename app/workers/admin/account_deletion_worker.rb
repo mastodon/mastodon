56a83331ce284f3e69f2569793f96a7613e12b03
Shlee <github@shl.ee>
2026-01-23 00:01:57 +1030

    Update account_deletion_worker.rb

--- app/workers/admin/account_deletion_worker.rb
+++ app/workers/admin/account_deletion_worker.rb
@@ -6,7 +6,10 @@ class Admin::AccountDeletionWorker
   sidekiq_options queue: 'pull', lock: :until_executed, lock_ttl: 1.week.to_i
 
   def perform(account_id)
-    DeleteAccountService.new.call(Account.find(account_id), reserve_username: true, reserve_email: true)
+    account = Account.find(account_id)
+    return unless account.unavailable?
+
+    DeleteAccountService.new.call(account, reserve_username: true, reserve_email: true)
   rescue ActiveRecord::RecordNotFound
     true
   end
