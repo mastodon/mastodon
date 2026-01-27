1b0be1a7252963fce7df476d0dae0448b84e5660
Claire <claire.github-309c@sitedethib.com>
2026-01-27 17:01:22 +0100

    Fix error when encountering invalid tag in updated object (#37635)

--- app/services/activitypub/process_status_update_service.rb
+++ app/services/activitypub/process_status_update_service.rb
@@ -205,7 +205,11 @@ class ActivityPub::ProcessStatusUpdateService < BaseService
 
   def update_tags!
     previous_tags = @status.tags.to_a
-    current_tags = @status.tags = Tag.find_or_create_by_names(@raw_tags)
+    current_tags = @status.tags = @raw_tags.flat_map do |tag|
+      Tag.find_or_create_by_names([tag]).filter(&:valid?)
+    rescue ActiveRecord::RecordInvalid
+      []
+    end
 
     return unless @status.distributable?
 
