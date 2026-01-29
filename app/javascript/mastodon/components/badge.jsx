3a8793a24a5710d7286c393532459dc1ee682fd7
Gaston Rial Saibene <gaston@uruguayo.me>
2026-01-29 09:56:12 +0100

    rebased

---
+++ app/javascript/mastodon/components/badge.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+import { FormattedMessage } from 'react-intl';
+
+import GroupsIcon from '@/material-icons/400-24px/group.svg?react';
+import PersonIcon from '@/material-icons/400-24px/person.svg?react';
+import SmartToyIcon from '@/material-icons/400-24px/smart_toy.svg?react';
+
+
+export const Badge = ({ icon = <PersonIcon />, label, domain, roleId }) => (
+  <div className='account-role' data-account-role-id={roleId}>
+    {icon}
+    {label}
+    {domain && <span className='account-role__domain'>{domain}</span>}
+  </div>
+);
+
+Badge.propTypes = {
+  icon: PropTypes.node,
+  label: PropTypes.node,
+  domain: PropTypes.node,
+  roleId: PropTypes.string
+};
+
+export const GroupBadge = () => (
+  <Badge icon={<GroupsIcon />} label={<FormattedMessage id='account.badges.group' defaultMessage='Group' />} />
+);
+
+export const AutomatedBadge = () => (
+  <Badge icon={<SmartToyIcon />} label={<FormattedMessage id='account.badges.bot' defaultMessage='Automated' />} />
+);
