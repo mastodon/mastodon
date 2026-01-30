mastodon/mastodon

commit d438161b9b04eda6513adb9b74e8bb5f6380ecf5

Unclosed connection leak when replacing pooled connection in SharedTimedStack.try_create (#37335)

app/lib/connection_pool/shared_timed_stack.rb
@@ -71,6 +71,7 @@ class ConnectionPool::SharedTimedStack
       throw_away_connection = @queue.pop
       @tagged_queue[throw_away_connection.site].delete(throw_away_connection)
       @create_block.call(preferred_tag)
+      throw_away_connection.close
     elsif @created != @max
       connection = @create_block.call(preferred_tag)
       @created += 1

commit 6b6df4c6d4874baf947b0d3170885962ac7d1f7a

Fix connection recycling pushing symbols to connection pool
Fixes a regression from #37335

app/lib/connection_pool/shared_timed_stack.rb
@@ -70,8 +70,8 @@ class ConnectionPool::SharedTimedStack
     if @created == @max && !@queue.empty?
       throw_away_connection = @queue.pop
       @tagged_queue[throw_away_connection.site].delete(throw_away_connection)
-      @create_block.call(preferred_tag)
       throw_away_connection.close
+      @create_block.call(preferred_tag)
     elsif @created != @max
       connection = @create_block.call(preferred_tag)
       @created += 1