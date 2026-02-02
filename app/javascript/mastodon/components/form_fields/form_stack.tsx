377792c11b5e33364d0fc2be69a7383d5436c651
diondiondion <mail@diondiondion.com>
2026-02-02 19:41:26 +0100

    Fix typing of polymorphicRef function

--- app/javascript/mastodon/components/form_fields/form_stack.tsx
+++ app/javascript/mastodon/components/form_fields/form_stack.tsx
@@ -1,5 +1,3 @@
-import type { ComponentPropsWithoutRef } from 'react';
-
 import classNames from 'classnames';
 
 import { polymorphicForwardRef } from '@/types/polymorphic';
@@ -10,17 +8,16 @@ import classes from './form_stack.module.scss';
  * A simple wrapper for providing consistent spacing to a group of form fields.
  */
 
-export const FormStack = polymorphicForwardRef<
-  'div',
-  ComponentPropsWithoutRef<'div'>
->(({ as: Element = 'div', children, className, ...otherProps }, ref) => (
-  <Element
-    ref={ref}
-    {...otherProps}
-    className={classNames(className, classes.stack)}
-  >
-    {children}
-  </Element>
-));
+export const FormStack = polymorphicForwardRef<'div'>(
+  ({ as: Element = 'div', children, className, ...otherProps }, ref) => (
+    <Element
+      ref={ref}
+      {...otherProps}
+      className={classNames(className, classes.stack)}
+    >
+      {children}
+    </Element>
+  ),
+);
 
 FormStack.displayName = 'FormStack';
