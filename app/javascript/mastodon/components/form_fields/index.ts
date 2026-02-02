ceb4a878fb3ea9a4c8cafaf5bfedfab8dc2622e1
diondiondion <mail@diondiondion.com>
2026-02-02 18:31:34 +0100

    Add `Select` component (#37702)

--- app/javascript/mastodon/components/form_fields/index.ts
+++ app/javascript/mastodon/components/form_fields/index.ts
@@ -2,4 +2,4 @@ export { TextInputField } from './text_input_field';
 export { TextAreaField } from './text_area_field';
 export { CheckboxField, Checkbox } from './checkbox_field';
 export { ToggleField, Toggle } from './toggle_field';
-export { SelectField } from './select_field';
+export { SelectField, Select } from './select_field';
--- app/javascript/mastodon/components/form_fields/select_field.stories.tsx
+++ app/javascript/mastodon/components/form_fields/select_field.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { SelectField } from './select_field';
+import { SelectField, Select } from './select_field';
 
 const meta = {
   title: 'Components/Form Fields/SelectField',
@@ -8,24 +8,19 @@ const meta = {
   args: {
     label: 'Fruit preference',
     hint: 'Select your favourite fruit or not. Up to you.',
-  },
-  render(args) {
-    // Component styles require a wrapper class at the moment
-    return (
-      <div className='simple_form'>
-        <SelectField {...args}>
-          <option>Apple</option>
-          <option>Banana</option>
-          <option>Kiwi</option>
-          <option>Lemon</option>
-          <option>Mango</option>
-          <option>Orange</option>
-          <option>Pomelo</option>
-          <option>Strawberries</option>
-          <option>Something else</option>
-        </SelectField>
-      </div>
-    );
+    children: (
+      <>
+        <option>Apple</option>
+        <option>Banana</option>
+        <option>Kiwi</option>
+        <option>Lemon</option>
+        <option>Mango</option>
+        <option>Orange</option>
+        <option>Pomelo</option>
+        <option>Strawberries</option>
+        <option>Something else</option>
+      </>
+    ),
   },
 } satisfies Meta<typeof SelectField>;
 
@@ -57,5 +52,18 @@ export const WithError: Story = {
   args: {
     required: false,
     hasError: true,
+  },
+};
+
+export const Plain: Story = {
+  render(args) {
+    return <Select {...args} />;
+  },
+};
+
+export const Disabled: Story = {
+  ...Plain,
+  args: {
+    disabled: true,
   },
 };
