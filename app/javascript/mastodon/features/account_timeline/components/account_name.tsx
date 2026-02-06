87d91e6ad568dc9159dab6b7224c2f8b812f2736
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-02-06 18:23:13 +0100

    add new account help popup

--- app/javascript/mastodon/features/account_timeline/components/account_name.tsx
+++ app/javascript/mastodon/features/account_timeline/components/account_name.tsx
@@ -1,12 +1,19 @@
+import { useCallback, useId, useRef, useState } from 'react';
 import type { FC } from 'react';
 
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+import classNames from 'classnames';
+
+import Overlay from 'react-overlays/esm/Overlay';
 
 import { DisplayName } from '@/mastodon/components/display_name';
 import { Icon } from '@/mastodon/components/icon';
 import { useAccount } from '@/mastodon/hooks/useAccount';
 import { useAppSelector } from '@/mastodon/store';
+import AtIcon from '@/material-icons/400-24px/alternate_email.svg?react';
 import HelpIcon from '@/material-icons/400-24px/help.svg?react';
+import DomainIcon from '@/material-icons/400-24px/language.svg?react';
 import LockIcon from '@/material-icons/400-24px/lock.svg?react';
 
 import { DomainPill } from '../../account/components/domain_pill';
@@ -65,15 +72,107 @@ export const AccountName: FC<{ accountId: string }> = ({ accountId }) => {
       </h1>
       <p className={classes.username}>
         @{username}@{domain}
-        <DomainPill
+        <AccountNameHelp
           username={username}
           domain={domain}
-          isSelf={me === account.id}
-          className={classes.domainPill}
-        >
-          <Icon id='help' icon={HelpIcon} />
-        </DomainPill>
+          isSelf={account.id === me}
+        />
       </p>
     </div>
+  );
+};
+
+const AccountNameHelp: FC<{
+  username: string;
+  domain: string;
+  isSelf: boolean;
+}> = ({ username, domain, isSelf }) => {
+  const accessibilityId = useId();
+  const [open, setOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement>(null);
+
+  const handleClick = useCallback(() => {
+    setOpen((prev) => !prev);
+  }, []);
+
+  return (
+    <>
+      <button
+        type='button'
+        ref={triggerRef}
+        className={classes.handleHelpButton}
+        onClick={handleClick}
+        aria-expanded={open}
+        aria-controls={accessibilityId}
+      >
+        <Icon id='help' icon={HelpIcon} />
+      </button>
+
+      <Overlay
+        show={open}
+        rootClose
+        target={triggerRef}
+        onHide={handleClick}
+        offset={[5, 5]}
+      >
+        {({ props }) => (
+          <div
+            {...props}
+            role='region'
+            id={accessibilityId}
+            className={classNames('dropdown-animation', classes.handleHelp)}
+          >
+            <FormattedMessage
+              id='account.name.help.header'
+              defaultMessage='A handle is like an email address'
+              tagName='h3'
+            />
+            <ol>
+              <li>
+                <Icon id='at' icon={AtIcon} />
+                {isSelf ? (
+                  <FormattedMessage
+                    id='account.name.help.username_self'
+                    defaultMessage='{username} is your username on this server. Someone on another server might have the same username.'
+                    values={{ username: <strong>{username}</strong> }}
+                    tagName='p'
+                  />
+                ) : (
+                  <FormattedMessage
+                    id='account.name.help.username'
+                    defaultMessage='{username} is this account’s username on their server. Someone on another server might have the same username.'
+                    values={{ username: <strong>{username}</strong> }}
+                    tagName='p'
+                  />
+                )}
+              </li>
+              <li>
+                <Icon id='domain' icon={DomainIcon} />
+                {isSelf ? (
+                  <FormattedMessage
+                    id='account.name.help.domain_self'
+                    defaultMessage='{domain} is your server that hosts your profile and posts.'
+                    values={{ domain: <strong>{domain}</strong> }}
+                    tagName='p'
+                  />
+                ) : (
+                  <FormattedMessage
+                    id='account.name.help.domain'
+                    defaultMessage='{domain} is the server that hosts the user’s profile and posts.'
+                    values={{ domain: <strong>{domain}</strong> }}
+                    tagName='p'
+                  />
+                )}
+              </li>
+            </ol>
+            <FormattedMessage
+              id='account.name.help.footer'
+              defaultMessage='Just like you can send emails to people using different email clients, you can interact with people on other Mastodon servers – and with anyone on other social apps powered by the same set of rules as Mastodon uses (the ActivityPub protocol).'
+              tagName='p'
+            />
+          </div>
+        )}
+      </Overlay>
+    </>
   );
 };
