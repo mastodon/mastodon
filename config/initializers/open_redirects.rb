8e18a5d53c246ad89ca763cf1008faf8f89c93c1
Matt Jankowski <matt@jankowski.online>
2026-02-09 09:55:12 -0500

    Restore comment explaining open redirects setting

--- config/initializers/open_redirects.rb
+++ config/initializers/open_redirects.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# In the Devise confirmations#show action, a redirect_to is called:
+# https://github.com/heartcombo/devise/blob/v5.0.0/app/controllers/devise/confirmations_controller.rb#L28
+#
+# We override the `after_confirmation_path_for` method in a way which sometimes
+# returns raw URLs to external hosts, as part of the auth workflow.
+# Discussion: https://github.com/mastodon/mastodon/pull/36505#discussion_r2782876831
+
 Rails.application.reloader.to_prepare do
   ActionController::Base.action_on_open_redirect = :log
 end
