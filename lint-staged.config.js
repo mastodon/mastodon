d9ead2414a8306a55ef693e343433fc8258e315f
Renaud Chaput <renchap@gmail.com>
2026-02-09 22:54:58 +0100

    Add a i18n check in pre-commit
    If a JS file is changed, run `yarn i18n:extract` and check there is no diff.
    
    If there is a change here, it means the author forgot to extract i18n strings after their code changes.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,6 +5,10 @@ const config = {
   '*.{css,scss}': 'stylelint --fix',
   '*.haml': 'bin/haml-lint -a',
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+  'app/javascript/**/*.{js,jsx,ts,tsx}': () => [
+    `yarn i18n:extract`,
+    'git diff --exit-code app/javascript/mastodon/locales/en.json',
+  ],
 };
 
 module.exports = config;
