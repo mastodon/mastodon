d13d7b4566d551d027830f7dfc987ba3a8e2dd4f
Claire <claire.github-309c@sitedethib.com>
2026-02-10 21:53:43 +0100

    Fix server theme migration (#37810)

--- db/migrate/20260209142402_migrate_default_theme_setting.rb
+++ db/migrate/20260209142402_migrate_default_theme_setting.rb
@@ -7,15 +7,12 @@ class MigrateDefaultThemeSetting < ActiveRecord::Migration[8.0]
     Setting.reset_column_information
 
     setting = Setting.find_by(var: 'theme')
-    return unless setting.present? && setting.attributes['value'].present? && %w(mastodon-light contrast system).include?(setting.attributes['value'])
+    return unless setting.present? && setting.attributes['value'].present?
 
-    Setting.upsert(
-      {
-        var: 'theme',
-        value: "--- default\n",
-      },
-      unique_by: index_exists?(:settings, [:thing_type, :thing_id, :var]) ? [:thing_type, :thing_id, :var] : :var
-    )
+    theme = YAML.safe_load(setting.attributes['value'], permitted_classes: [ActiveSupport::HashWithIndifferentAccess, Symbol])
+    return unless %w(mastodon-light contrast system).include?(theme)
+
+    setting.update_column('value', "--- default\n")
   end
 
   def down; end
