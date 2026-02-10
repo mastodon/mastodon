394ff368665d5e2e0fb373caa876247b0d7f16bf
Claire <claire.github-309c@sitedethib.com>
2026-02-10 15:54:31 +0100

    Fix PurgeCustomEmojiWorker not matching subdomains

--- app/workers/purge_custom_emoji_worker.rb
+++ app/workers/purge_custom_emoji_worker.rb
@@ -6,7 +6,7 @@ class PurgeCustomEmojiWorker
   def build_enumerator(domain, cursor:)
     return if domain.blank?
 
-    active_record_batches_enumerator(CustomEmoji.where(domain: domain), cursor:)
+    active_record_batches_enumerator(CustomEmoji.by_domain_and_subdomains(domain), cursor:)
   end
 
   def each_iteration(custom_emojis, _domain)
