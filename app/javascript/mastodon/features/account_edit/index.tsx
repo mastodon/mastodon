0efc4a4b267f7caa0ffb22a13ef7cedfe46ab49b
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-02-17 13:41:44 +0100

    uses custom column header instead

--- app/javascript/mastodon/features/account_edit/index.tsx
+++ app/javascript/mastodon/features/account_edit/index.tsx
@@ -1,44 +1,38 @@
 import type { FC } from 'react';
 
-import { defineMessages, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
+import { Button } from '@/mastodon/components/button';
 import { Column } from '@/mastodon/components/column';
-import { ColumnHeader } from '@/mastodon/components/column_header';
 import BundleColumnError from '@/mastodon/features/ui/components/bundle_column_error';
 import { useCurrentAccountId } from '@/mastodon/hooks/useAccountId';
-import AccountCircleIcon from '@/material-icons/400-24px/account_circle.svg?react';
 
-const messages = defineMessages({
-  title: { id: 'account_edit.column_title', defaultMessage: 'Edit Profile' },
-  doneButton: { id: 'account_edit.done_button', defaultMessage: 'Done' },
-});
+import classes from './styles.module.scss';
 
 export const AccountEdit: FC<{ multiColumn: boolean }> = ({ multiColumn }) => {
   const accountId = useCurrentAccountId();
-  const intl = useIntl();
 
   if (!accountId) {
     return <BundleColumnError multiColumn={multiColumn} errorType='routing' />;
   }
 
   return (
-    <Column label='Edit Profile' bindToDocument={!multiColumn}>
-      <ColumnHeader
-        multiColumn={multiColumn}
-        title={intl.formatMessage(messages.title)}
-        icon='account-circle'
-        iconComponent={AccountCircleIcon}
-        extraButton={
-          <button
-            aria-label={intl.formatMessage(messages.doneButton)}
-            title={intl.formatMessage(messages.doneButton)}
-            className='column-header__button'
-            type='button'
-          >
-            {intl.formatMessage(messages.doneButton)}
-          </button>
-        }
-      />
+    <Column bindToDocument={!multiColumn} className={classes.column}>
+      <header>
+        <div className={classes.nav}>
+          <FormattedMessage
+            id='account_edit.column_title'
+            defaultMessage='Edit Profile'
+            tagName='h1'
+          />
+          <Button>
+            <FormattedMessage
+              id='account_edit.column_button'
+              defaultMessage='Done'
+            />
+          </Button>
+        </div>
+      </header>
     </Column>
   );
 };
