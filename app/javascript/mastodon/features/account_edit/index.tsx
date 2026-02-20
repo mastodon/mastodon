886fefa2cfece9d7e5459648b367f98cea99bee5
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-02-20 15:29:47 +0100

    adds AccountEditColumn and useFeaturedHashtags

--- app/javascript/mastodon/features/account_edit/index.tsx
+++ app/javascript/mastodon/features/account_edit/index.tsx
@@ -1,31 +1,31 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import type { FC } from 'react';
 
-import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
 
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-import { fetchFeaturedTags } from '@/mastodon/actions/featured_tags';
 import type { ModalType } from '@/mastodon/actions/modal';
 import { openModal } from '@/mastodon/actions/modal';
 import { AccountBio } from '@/mastodon/components/account_bio';
 import { Avatar } from '@/mastodon/components/avatar';
-import { Column } from '@/mastodon/components/column';
-import { ColumnHeader } from '@/mastodon/components/column_header';
 import { DisplayNameSimple } from '@/mastodon/components/display_name/simple';
-import { LoadingIndicator } from '@/mastodon/components/loading_indicator';
-import BundleColumnError from '@/mastodon/features/ui/components/bundle_column_error';
 import { useAccount } from '@/mastodon/hooks/useAccount';
 import { useCurrentAccountId } from '@/mastodon/hooks/useAccountId';
+import { useFeaturedHashtags } from '@/mastodon/hooks/useFeaturedHashtags';
 import { autoPlayGif } from '@/mastodon/initial_state';
-import { selectAccountFeaturedTags } from '@/mastodon/selectors/accounts';
-import { useAppDispatch, useAppSelector } from '@/mastodon/store';
+import { useAppDispatch } from '@/mastodon/store';
 
+import { AccountEditColumn, AccountEditEmptyColumn } from './components/column';
 import { EditButton } from './components/edit_button';
 import { AccountEditSection } from './components/section';
 import classes from './styles.module.scss';
 
 const messages = defineMessages({
+  columnTitle: {
+    id: 'account_edit.column_title',
+    defaultMessage: 'Edit Profile',
+  },
   displayNameTitle: {
     id: 'account_edit.display_name.title',
     defaultMessage: 'Display name',
@@ -75,21 +75,14 @@ const messages = defineMessages({
   },
 });
 
-export const AccountEdit: FC<{ multiColumn: boolean }> = ({ multiColumn }) => {
+export const AccountEdit: FC = () => {
   const accountId = useCurrentAccountId();
   const account = useAccount(accountId);
   const intl = useIntl();
 
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    if (accountId) {
-      void dispatch(fetchFeaturedTags({ accountId }));
-    }
-  }, [accountId, dispatch]);
-  const featuredTags = useAppSelector((state) =>
-    accountId ? selectAccountFeaturedTags(state, accountId) : null,
-  );
+  const featuredTags = useFeaturedHashtags(accountId);
 
   const handleOpenModal = useCallback(
     (type: ModalType, props?: Record<string, unknown>) => {
@@ -109,16 +102,8 @@ export const AccountEdit: FC<{ multiColumn: boolean }> = ({ multiColumn }) => {
     history.push('/profile/featured_tags');
   }, [history]);
 
-  if (!accountId) {
-    return <BundleColumnError multiColumn={multiColumn} errorType='routing' />;
-  }
-
-  if (!account) {
-    return (
-      <Column bindToDocument={!multiColumn} className={classes.column}>
-        <LoadingIndicator />
-      </Column>
-    );
+  if (!accountId || !account) {
+    return <AccountEditEmptyColumn notFound={!accountId} />;
   }
 
   const headerSrc = autoPlayGif ? account.header : account.header_static;
@@ -127,23 +112,10 @@ export const AccountEdit: FC<{ multiColumn: boolean }> = ({ multiColumn }) => {
   const hasTags = Array.isArray(featuredTags);
 
   return (
-    <Column bindToDocument={!multiColumn} className={classes.column}>
-      <ColumnHeader
-        title={intl.formatMessage({
-          id: 'account_edit.column_title',
-          defaultMessage: 'Edit Profile',
-        })}
-        className={classes.columnHeader}
-        showBackButton
-        extraButton={
-          <Link to={`/@${account.acct}`} className='button'>
-            <FormattedMessage
-              id='account_edit.column_button'
-              defaultMessage='Done'
-            />
-          </Link>
-        }
-      />
+    <AccountEditColumn
+      title={intl.formatMessage(messages.columnTitle)}
+      acct={account.acct}
+    >
       <header>
         <div className={classes.profileImage}>
           {headerSrc && <img src={headerSrc} alt='' />}
@@ -207,6 +179,6 @@ export const AccountEdit: FC<{ multiColumn: boolean }> = ({ multiColumn }) => {
         description={messages.profileTabSubtitle}
         showDescription
       />
-    </Column>
+    </AccountEditColumn>
   );
 };
