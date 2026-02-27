3ea89e35db4ad1a6a37c9635a9ffc75015d28ed7
diondiondion <mail@diondiondion.com>
2026-02-27 10:06:41 +0100

    Add hotkey hint to skip links

--- app/javascript/mastodon/features/ui/components/skip_links/index.tsx
+++ app/javascript/mastodon/features/ui/components/skip_links/index.tsx
@@ -1,3 +1,5 @@
+import { useId } from 'react';
+
 import { useIntl } from 'react-intl';
 
 import classes from './skip_links.module.scss';
@@ -7,25 +9,57 @@ export const SKIP_LINK_TARGETS = {
   NAV: 'skip-link-target-nav',
 } as const;
 
-export const SkipLinks: React.FC = () => {
+export const SkipLinks: React.FC<{ multiColumn: boolean }> = ({
+  multiColumn,
+}) => {
   const intl = useIntl();
+  const id = useId();
+  const contentDescId = `${id}-content`;
+  const navDescId = `${id}-nav`;
   return (
-    <ul className={classes.wrapper}>
-      <li>
-        <a href={`#${SKIP_LINK_TARGETS.CONTENT}`}>
+    <ul className={classes.list}>
+      <li className={classes.listItem}>
+        <a
+          href={`#${SKIP_LINK_TARGETS.CONTENT}`}
+          aria-describedby={contentDescId}
+        >
           {intl.formatMessage({
             id: 'skip_links.skip_to_content',
             defaultMessage: 'Skip to main content',
           })}
         </a>
+        <span id={contentDescId} className={classes.hotkeyHint}>
+          {intl.formatMessage(
+            {
+              id: 'skip_links.hotkey',
+              defaultMessage: '<span>Hotkey</span> {hotkey}',
+            },
+            {
+              hotkey: multiColumn ? '1-9' : '1',
+              span: (text) => <span className='sr-only'>{text}</span>,
+            },
+          )}
+        </span>
       </li>
-      <li>
-        <a href={`#${SKIP_LINK_TARGETS.NAV}`}>
+      <li className={classes.listItem}>
+        <a href={`#${SKIP_LINK_TARGETS.NAV}`} aria-describedby={navDescId}>
           {intl.formatMessage({
-            id: 'skip_links.skip_to_content',
+            id: 'skip_links.skip_to_navigation',
             defaultMessage: 'Skip to main navigation',
           })}
         </a>
+        <span id={navDescId} className={classes.hotkeyHint}>
+          {intl.formatMessage(
+            {
+              id: 'skip_links.hotkey',
+              defaultMessage: '<span>Hotkey</span> {hotkey}',
+            },
+            {
+              hotkey: 't',
+              span: (text) => <span className='sr-only'>{text}</span>,
+            },
+          )}
+        </span>
       </li>
     </ul>
   );
