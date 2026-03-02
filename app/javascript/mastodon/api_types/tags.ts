bcbfff4e591bbb7cc36798f36789cf1b5ba27573
Echo <ChaosExAnima@users.noreply.github.com>
2026-03-02 17:32:08 +0100

    Profile editing: Allow adding arbitrary featured tags (#38012)

--- app/javascript/mastodon/api_types/tags.ts
+++ app/javascript/mastodon/api_types/tags.ts
@@ -17,16 +17,6 @@ export interface ApiHashtagJSON extends ApiHashtagBase {
 }
 
 export interface ApiFeaturedTagJSON extends ApiHashtagBase {
-  statuses_count: number;
+  statuses_count: string;
   last_status_at: string | null;
-}
-
-export function hashtagToFeaturedTag(tag: ApiHashtagJSON): ApiFeaturedTagJSON {
-  return {
-    id: tag.id,
-    name: tag.name,
-    url: tag.url,
-    statuses_count: 0,
-    last_status_at: null,
-  };
 }
