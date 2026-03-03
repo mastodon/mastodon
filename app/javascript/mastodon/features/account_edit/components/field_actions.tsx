11f91b1e7dfd2cdb958abf85e3f1c0e71de45f78
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-03-03 14:46:23 +0100

    adds id to fields and wires up delete

--- app/javascript/mastodon/features/account_edit/components/field_actions.tsx
+++ app/javascript/mastodon/features/account_edit/components/field_actions.tsx
@@ -2,31 +2,31 @@ import type { FC } from 'react';
 import { useCallback } from 'react';
 
 import { openModal } from '@/mastodon/actions/modal';
-import type { ApiAccountFieldJSON } from '@/mastodon/api_types/accounts';
 import { useAppDispatch } from '@/mastodon/store';
 
 import { EditButton, DeleteIconButton } from './edit_button';
 
-export const AccountFieldActions: FC<
-  ApiAccountFieldJSON & { item: string }
-> = ({ item, ...field }) => {
+export const AccountFieldActions: FC<{ item: string; id: string }> = ({
+  item,
+  id,
+}) => {
   const dispatch = useAppDispatch();
   const handleEdit = useCallback(() => {
     dispatch(
       openModal({
         modalType: 'ACCOUNT_EDIT_FIELD_EDIT',
-        modalProps: { field },
+        modalProps: { fieldKey: id },
       }),
     );
-  }, [dispatch, field]);
+  }, [dispatch, id]);
   const handleDelete = useCallback(() => {
     dispatch(
       openModal({
         modalType: 'ACCOUNT_EDIT_FIELD_DELETE',
-        modalProps: { field },
+        modalProps: { fieldKey: id },
       }),
     );
-  }, [dispatch, field]);
+  }, [dispatch, id]);
 
   return (
     <>
--- app/javascript/mastodon/features/account_edit/modals/fields_modals.tsx
+++ app/javascript/mastodon/features/account_edit/modals/fields_modals.tsx
@@ -1,14 +1,19 @@
+import { useCallback } from 'react';
 import type { FC } from 'react';
 
-import type { ApiAccountFieldJSON } from '@/mastodon/api_types/accounts';
-import { useAppSelector } from '@/mastodon/store';
+import { FormattedMessage } from 'react-intl';
+
+import { Button } from '@/mastodon/components/button';
+import { removeField } from '@/mastodon/reducers/slices/profile_edit';
+import type { FieldData } from '@/mastodon/reducers/slices/profile_edit';
+import { useAppDispatch, useAppSelector } from '@/mastodon/store';
 
 import type { DialogModalProps } from '../../ui/components/dialog_modal';
 import { DialogModal } from '../../ui/components/dialog_modal';
 
-export const EditFieldModal: FC<
-  DialogModalProps & { field?: ApiAccountFieldJSON }
-> = ({ onClose }) => {
+export const EditFieldModal: FC<DialogModalProps & { field?: FieldData }> = ({
+  onClose,
+}) => {
   return (
     <DialogModal onClose={onClose} title='foo'>
       <p>bar</p>
@@ -16,12 +21,39 @@ export const EditFieldModal: FC<
   );
 };
 
-export const DeleteFieldModal: FC<
-  DialogModalProps & { field: ApiAccountFieldJSON }
-> = ({ onClose }) => {
+export const DeleteFieldModal: FC<DialogModalProps & { fieldKey: string }> = ({
+  onClose,
+  fieldKey,
+}) => {
+  const isPending = useAppSelector((state) => state.profileEdit.isPending);
+  const dispatch = useAppDispatch();
+  const handleDelete = useCallback(() => {
+    void dispatch(removeField({ key: fieldKey })).then(onClose);
+  }, [dispatch, fieldKey, onClose]);
+
   return (
-    <DialogModal onClose={onClose} title='foo'>
-      <p>bar</p>
+    <DialogModal
+      onClose={onClose}
+      title={
+        <FormattedMessage
+          id='account_edit.field_delete_modal.title'
+          defaultMessage='Delete custom field?'
+        />
+      }
+      buttons={
+        <Button dangerous onClick={handleDelete} disabled={isPending}>
+          <FormattedMessage
+            id='account_edit.field_delete_modal.delete_button'
+            defaultMessage='Delete'
+          />
+        </Button>
+      }
+    >
+      <FormattedMessage
+        id='account_edit.field_delete_modal.confirm'
+        defaultMessage='Are you sure you want to delete this custom field? This action can’t be undone.'
+        tagName='p'
+      />
     </DialogModal>
   );
 };
