92c9c03bc7ad387822e74fc83048a2302cf9c1c6
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-03-03 19:19:52 +0100

    swap out bio for new inputs

--- app/javascript/mastodon/features/account_edit/components/text_input.tsx
+++ app/javascript/mastodon/features/account_edit/components/text_input.tsx
@@ -8,7 +8,11 @@ import type {
 } from 'react';
 import { useCallback, useId, useRef } from 'react';
 
-import { TextInputField } from '@/mastodon/components/form_fields';
+import {
+  TextAreaField,
+  TextInputField,
+} from '@/mastodon/components/form_fields';
+import type { TextAreaProps } from '@/mastodon/components/form_fields/text_area_field';
 import type { OmitUnion } from '@/mastodon/utils/types';
 
 import { insertEmojiAtPosition } from '../../emoji/utils';
@@ -20,7 +24,7 @@ import { EmojiPicker } from './emoji_picker';
 interface InputProps {
   value: string;
   onChange: Dispatch<SetStateAction<string>>;
-  label: ReactNode;
+  label?: ReactNode;
   hint?: ReactNode;
   maxLength?: number;
   recommended?: boolean;
@@ -28,7 +32,7 @@ interface InputProps {
 
 export const TextInput: FC<
   OmitUnion<ComponentPropsWithoutRef<'input'>, InputProps>
-> = ({ onChange, value, maxLength, recommended = false, ...props }) => {
+> = ({ onChange, value, label, maxLength, recommended = false, ...props }) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const counterId = useId();
 
@@ -52,10 +56,60 @@ export const TextInput: FC<
   return (
     <TextInputField
       {...props}
+      label={label}
       value={value}
       onChange={handleChange}
       wrapperClassName={classes.inputWrapper}
       ref={inputRef}
+      aria-describedby={counterId}
+      afterInput={
+        <>
+          <EmojiPicker onPick={handlePickEmoji} />
+          {maxLength && (
+            <CharCounter
+              currentLength={value.length}
+              maxLength={maxLength}
+              recommended={recommended}
+              id={counterId}
+            />
+          )}
+        </>
+      }
+    />
+  );
+};
+
+export const TextAreaInput: FC<
+  OmitUnion<Omit<TextAreaProps, 'style'>, InputProps>
+> = ({ onChange, value, label, maxLength, recommended = false, ...props }) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const counterId = useId();
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLTextAreaElement>) => {
+      onChange(event.target.value);
+    },
+    [onChange],
+  );
+
+  const handlePickEmoji = useCallback(
+    (emoji: string) => {
+      onChange((prev) => {
+        const position = textareaRef.current?.selectionStart ?? prev.length;
+        return insertEmojiAtPosition(prev, emoji, position);
+      });
+    },
+    [onChange],
+  );
+
+  return (
+    <TextAreaField
+      {...props}
+      label={label}
+      value={value}
+      onChange={handleChange}
+      wrapperClassName={classes.inputWrapper}
+      ref={textareaRef}
       aria-describedby={counterId}
       afterInput={
         <>
