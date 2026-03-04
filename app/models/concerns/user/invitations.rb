b63d2e10770eff7e6afd7f71522077760d58fe42
Matt Jankowski <matt@jankowski.online>
2026-03-04 16:39:12 -0500

    Move invite_request assoc

--- app/models/concerns/user/invitations.rb
+++ app/models/concerns/user/invitations.rb
@@ -6,5 +6,6 @@ module User::Invitations
   included do
     belongs_to :invite, counter_cache: :uses, optional: true
     has_many :invites, inverse_of: :user, dependent: nil
+    has_one :invite_request, class_name: 'UserInviteRequest', inverse_of: :user, dependent: :destroy
   end
 end
