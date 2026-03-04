04573a9208c040ec4e6285490fc2329c412b5fb4
Matt Jankowski <matt@jankowski.online>
2026-03-04 08:53:17 -0500

    Simplify search spec setup

---
+++ spec/support/search.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.before :suite do
+    if search_examples_present?
+      Chewy.settings[:enabled] = true
+      # Configure chewy to use `urgent` strategy to index documents immediately
+      Chewy.strategy(:urgent)
+    else
+      Chewy.settings[:enabled] = false
+    end
+  end
+
+  config.around :each, :search do |example|
+    search_indices.each(&:reset!)
+    example.run
+    search_indices.each(&:delete!)
+  end
+
+  private
+
+  def search_indices
+    [
+      AccountsIndex,
+      PublicStatusesIndex,
+      StatusesIndex,
+      TagsIndex,
+    ]
+  end
+
+  def search_examples_present?
+    RSpec.world.filtered_examples.values.flatten.any? { |example| example.metadata[:search] == true }
+  end
+end
