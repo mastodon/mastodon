79e955d6204c5f67845e8333fadf98aed0b38ce4
Matt Jankowski <matt@jankowski.online>
2026-03-04 16:39:12 -0500

    Move invite_request assoc

--- spec/support/examples/models/concerns/user/invitations.rb
+++ spec/support/examples/models/concerns/user/invitations.rb
@@ -6,5 +6,6 @@ RSpec.shared_examples 'User::Invitations' do
   describe 'Associations' do
     it { is_expected.to belong_to(:invite).optional.counter_cache(:uses) }
     it { is_expected.to have_many(:invites).inverse_of(:user).dependent(false) }
+    it { is_expected.to have_one(:invite_request).inverse_of(:user).dependent(:destroy).class_name(UserInviteRequest) }
   end
 end
