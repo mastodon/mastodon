6c2ab56c247f14aebead7938b1fbf94ee3ea8631
Matt Jankowski <matt@jankowski.online>
2026-03-04 17:05:22 -0500

    Move nested declaration

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,8 +87,6 @@ class User < ApplicationRecord
   has_many :webauthn_credentials, dependent: :destroy
   has_many :ips, class_name: 'UserIp', inverse_of: :user, dependent: nil
 
-  accepts_nested_attributes_for :invite_request, reject_if: ->(attributes) { attributes['text'].blank? && !Setting.require_invite_text }
-
   validates :email, presence: true, email_address: true, length: { maximum: 320 }
   validates :email, email_mx: { attempt_ip: :sign_up_ip }, if: :validate_email_dns?
 
