5877b2601c7ad5b72677abe297141a2ec564906a
Matt Jankowski <matt@jankowski.online>
2026-03-04 08:27:25 -0500

    Use factories in account/search_spec

--- spec/search/models/concerns/account/search_spec.rb
+++ spec/search/models/concerns/account/search_spec.rb
@@ -3,48 +3,54 @@
 require 'rails_helper'
 
 RSpec.describe Account::Search do
-  describe 'a non-discoverable account becoming discoverable' do
-    let(:account) { Account.find_by(username: 'search_test_account_1') }
+  describe 'Callbacks for discoverable changes' do
+    let(:results) { AccountsIndex.filter(term: { username: account.username }) }
 
-    context 'when picking a non-discoverable account' do
-      it 'its bio is not in the AccountsIndex' do
-        results = AccountsIndex.filter(term: { username: account.username })
-        expect(results.count).to eq(1)
-        expect(results.first.text).to be_nil
+    context 'with a non-discoverable account' do
+      let(:account) { Fabricate :account, discoverable: false }
+
+      context 'when looking for the account' do
+        it 'is missing from the AccountsIndex' do
+          expect(results.count)
+            .to eq(1)
+          expect(results.first.text)
+            .to be_nil
+        end
+      end
+
+      context 'when the account becomes discoverable' do
+        before { account.update! discoverable: true }
+
+        it 'is present in the AccountsIndex' do
+          expect(results.count)
+            .to eq(1)
+          expect(results.first.text)
+            .to eq(account.note)
+        end
       end
     end
 
-    context 'when the non-discoverable account becomes discoverable' do
-      it 'its bio is added to the AccountsIndex' do
-        account.discoverable = true
-        account.save!
+    describe 'with a discoverable account' do
+      let(:account) { Fabricate :account, discoverable: true }
 
-        results = AccountsIndex.filter(term: { username: account.username })
-        expect(results.count).to eq(1)
-        expect(results.first.text).to eq(account.note)
+      context 'when looking for the account' do
+        it 'is present in the AccountsIndex' do
+          expect(results.count)
+            .to eq(1)
+          expect(results.first.text)
+            .to eq(account.note)
+        end
       end
-    end
-  end
 
-  describe 'a discoverable account becoming non-discoverable' do
-    let(:account) { Account.find_by(username: 'search_test_account_0') }
+      context 'when the account becomes non-discoverable' do
+        before { account.update! discoverable: false }
 
-    context 'when picking an discoverable account' do
-      it 'has its bio in the AccountsIndex' do
-        results = AccountsIndex.filter(term: { username: account.username })
-        expect(results.count).to eq(1)
-        expect(results.first.text).to eq(account.note)
-      end
-    end
-
-    context 'when the discoverable account becomes non-discoverable' do
-      it 'its bio is removed from the AccountsIndex' do
-        account.discoverable = false
-        account.save!
-
-        results = AccountsIndex.filter(term: { username: account.username })
-        expect(results.count).to eq(1)
-        expect(results.first.text).to be_nil
+        it 'is missing from the AccountsIndex' do
+          expect(results.count)
+            .to eq(1)
+          expect(results.first.text)
+            .to be_nil
+        end
       end
     end
   end
