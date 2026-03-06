2452c077b5ec2883d136c701576307267932630c
Echo <ChaosExAnima@users.noreply.github.com>
2026-03-06 13:57:06 +0100

    Profile editing: Rearranging and adding fields (#38083)

--- app/javascript/mastodon/features/account_edit/modals/index.ts
+++ app/javascript/mastodon/features/account_edit/modals/index.ts
@@ -1,5 +1,6 @@
 export * from './bio_modal';
 export * from './fields_modals';
+export * from './fields_reorder_modal';
 export * from './name_modal';
 export * from './profile_display_modal';
 export * from './verified_modal';
