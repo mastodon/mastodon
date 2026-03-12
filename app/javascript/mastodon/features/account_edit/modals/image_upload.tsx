7ab9c63d508066b30968ffb6acacd41178e1251c
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-03-12 15:26:07 +0100

    handle image uploads

--- app/javascript/mastodon/features/account_edit/modals/image_upload.tsx
+++ app/javascript/mastodon/features/account_edit/modals/image_upload.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
-import type { FC } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import type { ChangeEventHandler, FC } from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
@@ -17,22 +17,30 @@ import classes from './styles.module.scss';
 export const ImageUploadModal: FC<
   DialogModalProps & { location: ImageLocation }
 > = ({ onClose, location }) => {
-  const { src } = useAppSelector((state) => selectImageInfo(state, location));
-  const [step] = useState<'upload' | 'crop' | 'alt'>('upload');
+  const { src: oldSrc } = useAppSelector((state) =>
+    selectImageInfo(state, location),
+  );
+  const hasImage = !!oldSrc;
+  const [step, setStep] = useState<'upload' | 'crop' | 'alt'>('upload');
 
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(setDragUploadEnabled(false));
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
-    return () => {
-      dispatch(setDragUploadEnabled(true));
-    };
-  }, [dispatch]);
+  const handleFile = useCallback((file: File) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      const result = reader.result;
+      if (typeof result === 'string') {
+        setImageSrc(result);
+        setStep('crop');
+      }
+    });
+    reader.readAsDataURL(file);
+  }, []);
 
   return (
     <DialogModal
       title={
-        src ? (
+        hasImage ? (
           <FormattedMessage
             id='account_edit.upload_modal.title_replace'
             defaultMessage='Replace profile photo'
@@ -48,7 +56,8 @@ export const ImageUploadModal: FC<
       wrapperClassName={classes.uploadWrapper}
       noCancelButton
     >
-      {step === 'upload' && <StepUpload />}
+      {step === 'upload' && <StepUpload onFile={handleFile} />}
+      {step === 'crop' && <span>{imageSrc?.length}</span>}
     </DialogModal>
   );
 };
@@ -61,10 +70,28 @@ const ALLOWED_MIME_TYPES = [
   'image/webp',
 ];
 
-const StepUpload: FC = () => {
+const StepUpload: FC<{ onFile: (file: File) => void }> = ({ onFile }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleUploadClick = useCallback(() => {
+    inputRef.current?.click();
+  }, []);
+
+  const handleFileChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+    (event) => {
+      const file = event.currentTarget.files?.[0];
+      if (!file || !ALLOWED_MIME_TYPES.includes(file.type)) {
+        return;
+      }
+      onFile(file);
+    },
+    [onFile],
+  );
+
+  // Handle drag and drop
   const [isDragging, setDragging] = useState(false);
 
   const handleDragOver = useCallback((event: DragEvent) => {
+    event.preventDefault();
     if (!event.dataTransfer?.types.includes('Files')) {
       return;
     }
@@ -81,19 +108,45 @@ const StepUpload: FC = () => {
 
     setDragging(true);
   }, []);
-  const handleDragLeave = useCallback(() => {
+  const handleDragDrop = useCallback(
+    (event: DragEvent) => {
+      event.preventDefault();
+      setDragging(false);
+
+      if (!event.dataTransfer?.files) {
+        return;
+      }
+
+      const file = Array.from(event.dataTransfer.files).find((f) =>
+        ALLOWED_MIME_TYPES.includes(f.type),
+      );
+      if (!file) {
+        return;
+      }
+
+      onFile(file);
+    },
+    [onFile],
+  );
+  const handleDragLeave = useCallback((event: DragEvent) => {
+    event.preventDefault();
     setDragging(false);
   }, []);
 
+  const dispatch = useAppDispatch();
   useEffect(() => {
+    dispatch(setDragUploadEnabled(false));
     document.addEventListener('dragover', handleDragOver);
+    document.addEventListener('drop', handleDragDrop);
     document.addEventListener('dragleave', handleDragLeave);
 
     return () => {
       document.removeEventListener('dragover', handleDragOver);
+      document.removeEventListener('drop', handleDragDrop);
       document.removeEventListener('dragleave', handleDragLeave);
+      dispatch(setDragUploadEnabled(true));
     };
-  }, [handleDragLeave, handleDragOver]);
+  }, [handleDragLeave, handleDragDrop, handleDragOver, dispatch]);
 
   if (isDragging) {
     return (
@@ -117,15 +170,28 @@ const StepUpload: FC = () => {
       <FormattedMessage
         id='account_edit.upload_modal.step_upload.hint'
         defaultMessage='WEBP, PNG, GIF or JPG format, up to 8MB{br}Image will be downscaled to 400x400px'
-        tagName='p'
         values={{ br: <br /> }}
+        tagName='p'
       />
-      <Button className={classes.button}>
+      <Button
+        className={classes.button}
+        onClick={handleUploadClick}
+        // eslint-disable-next-line jsx-a11y/no-autofocus -- This is the main input, so auto-focus on it.
+        autoFocus
+      >
         <FormattedMessage
           id='account_edit.upload_modal.step_upload.button'
           defaultMessage='Browse files'
         />
       </Button>
+
+      <input
+        hidden
+        type='file'
+        ref={inputRef}
+        accept={ALLOWED_MIME_TYPES.join(',')}
+        onChange={handleFileChange}
+      />
     </div>
   );
 };
