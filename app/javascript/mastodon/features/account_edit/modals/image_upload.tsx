66ae5d7cfe853b8b7251ae8172a77960797100f9
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-03-12 14:39:01 +0100

    start styling and handling drag+drop

--- app/javascript/mastodon/features/account_edit/modals/image_upload.tsx
+++ app/javascript/mastodon/features/account_edit/modals/image_upload.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import type { FC } from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
 import { setDragUploadEnabled } from '@/mastodon/actions/compose_typed';
+import { Button } from '@/mastodon/components/button';
 import { selectImageInfo } from '@/mastodon/reducers/slices/profile_edit';
 import type { ImageLocation } from '@/mastodon/reducers/slices/profile_edit';
 import { useAppDispatch, useAppSelector } from '@/mastodon/store';
@@ -11,11 +12,13 @@ import { useAppDispatch, useAppSelector } from '@/mastodon/store';
 import { DialogModal } from '../../ui/components/dialog_modal';
 import type { DialogModalProps } from '../../ui/components/dialog_modal';
 
+import classes from './styles.module.scss';
+
 export const ImageUploadModal: FC<
   DialogModalProps & { location: ImageLocation }
 > = ({ onClose, location }) => {
   const { src } = useAppSelector((state) => selectImageInfo(state, location));
-  const [step] = useState<'select' | 'crop' | 'alt'>('select');
+  const [step] = useState<'upload' | 'crop' | 'alt'>('upload');
 
   const dispatch = useAppDispatch();
   useEffect(() => {
@@ -42,9 +45,87 @@ export const ImageUploadModal: FC<
         )
       }
       onClose={onClose}
+      wrapperClassName={classes.uploadWrapper}
       noCancelButton
     >
-      {step}
+      {step === 'upload' && <StepUpload />}
     </DialogModal>
+  );
+};
+
+// Taken from app/models/concerns/account/header.rb and app/models/concerns/account/avatar.rb
+const ALLOWED_MIME_TYPES = [
+  'image/jpeg',
+  'image/png',
+  'image/gif',
+  'image/webp',
+];
+
+const StepUpload: FC = () => {
+  const [isDragging, setDragging] = useState(false);
+
+  const handleDragOver = useCallback((event: DragEvent) => {
+    if (!event.dataTransfer?.types.includes('Files')) {
+      return;
+    }
+
+    const items = Array.from(event.dataTransfer.items);
+    if (
+      !items.some(
+        (item) =>
+          item.kind === 'file' && ALLOWED_MIME_TYPES.includes(item.type),
+      )
+    ) {
+      return;
+    }
+
+    setDragging(true);
+  }, []);
+  const handleDragLeave = useCallback(() => {
+    setDragging(false);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('dragover', handleDragOver);
+    document.addEventListener('dragleave', handleDragLeave);
+
+    return () => {
+      document.removeEventListener('dragover', handleDragOver);
+      document.removeEventListener('dragleave', handleDragLeave);
+    };
+  }, [handleDragLeave, handleDragOver]);
+
+  if (isDragging) {
+    return (
+      <div className={classes.uploadStepSelect}>
+        <FormattedMessage
+          id='account_edit.upload_modal.step_upload.dragging'
+          defaultMessage='Drop to upload'
+          tagName='h2'
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.uploadStepSelect}>
+      <FormattedMessage
+        id='account_edit.upload_modal.step_upload.header'
+        defaultMessage='Choose an image'
+        tagName='h2'
+      />
+      <FormattedMessage
+        id='account_edit.upload_modal.step_upload.hint'
+        defaultMessage='WEBP, PNG, GIF or JPG format, up to 8MB{br}Image will be downscaled to 400x400px'
+        tagName='p'
+        values={{ br: <br /> }}
+      />
+      <Button className={classes.button}>
+        <FormattedMessage
+          id='account_edit.upload_modal.step_upload.button'
+          defaultMessage='Browse files'
+        />
+      </Button>
+    </div>
   );
 };
