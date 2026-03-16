8d5d66ecfc21a5d88a139f2e7e19a2ba879659ae
Matt Jankowski <matt@jankowski.online>
2026-03-16 05:02:21 -0400

    Remove unused react component helper method (#38207)

--- app/helpers/react_component_helper.rb
+++ app/helpers/react_component_helper.rb
@@ -2,33 +2,16 @@
 
 module ReactComponentHelper
   def react_component(name, props = {}, &block)
-    data = { component: name.to_s.camelcase, props: Oj.dump(props) }
-    if block.nil?
-      div_tag_with_data(data)
+    data = { component: name.to_s.camelcase, props: }
+    if block_given?
+      tag.div data:, &block
     else
-      content_tag(:div, data: data, &block)
+      tag.div nil, data:
     end
   end
 
   def react_admin_component(name, props = {})
-    data = { 'admin-component': name.to_s.camelcase, props: Oj.dump(props) }
-    div_tag_with_data(data)
-  end
-
-  def serialized_media_attachments(media_attachments)
-    media_attachments.map { |attachment| serialized_attachment(attachment) }
-  end
-
-  private
-
-  def div_tag_with_data(data)
-    content_tag(:div, nil, data: data)
-  end
-
-  def serialized_attachment(attachment)
-    ActiveModelSerializers::SerializableResource.new(
-      attachment,
-      serializer: REST::MediaAttachmentSerializer
-    ).as_json
+    data = { 'admin-component': name.to_s.camelcase, props: }
+    tag.div nil, data:
   end
 end
