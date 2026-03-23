d48470a1fcb3ddd93335a8a971fd2661b07f6d15
Claire <claire.github-309c@sitedethib.com>
2026-03-23 11:32:41 +0100

    Change media description length limit for remote media attachments from 1500 to 10000 characters (#37921)

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -39,6 +39,7 @@ class MediaAttachment < ApplicationRecord
   enum :processing, { queued: 0, in_progress: 1, complete: 2, failed: 3 }, prefix: true
 
   MAX_DESCRIPTION_LENGTH = 1_500
+  MAX_DESCRIPTION_HARD_LENGTH_LIMIT = 10_000
 
   IMAGE_LIMIT = 16.megabytes
   VIDEO_LIMIT = 99.megabytes
