e0af7ad7c9f6b5ea99d7aa6df888aca7d71c42ec
Claire <claire.github-309c@sitedethib.com>
2026-03-24 10:28:13 +0100

    Apply suggestions from code review
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>

--- app/services/process_links_service.rb
+++ app/services/process_links_service.rb
@@ -25,10 +25,10 @@ class ProcessLinksService < BaseService
 
     urls.each do |url|
       # We only support `FeaturedCollection` at this time
-      next if ActivityPub::TagManager.instance.uri_to_local_collection(url.to_s).blank?
 
       # TODO: We probably want to resolve unknown objects at authoring time
       object = ActivityPub::TagManager.instance.uri_to_resource(url.to_s, Collection)
+      next if object.nil?
 
       tagged_object = @previous_objects.find { |x| x.object == object || x.uri == url }
       tagged_object ||= @current_objects.find { |x| x.object == object || x.uri == url }
