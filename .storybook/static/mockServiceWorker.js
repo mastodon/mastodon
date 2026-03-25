d7efdd67e9b8dfbf4aa7e8a4bffcaac0ba891853
Claire <claire.github-309c@sitedethib.com>
2026-03-25 11:58:48 +0100

    Update vendored file

--- .storybook/static/mockServiceWorker.js
+++ .storybook/static/mockServiceWorker.js
@@ -7,7 +7,7 @@
  * - Please do NOT modify this file.
  */
 
-const PACKAGE_VERSION = '2.12.1'
+const PACKAGE_VERSION = '2.12.14'
 const INTEGRITY_CHECKSUM = '4db4a41e972cec1b64cc569c66952d82'
 const IS_MOCKED_RESPONSE = Symbol('isMockedResponse')
 const activeClientIds = new Set()
