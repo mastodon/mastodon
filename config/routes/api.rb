b321d5d37756cf7e96631321bc07f116667dfcdb
Matt Jankowski <matt@jankowski.online>
2026-03-26 10:07:06 -0400

    Use `resources` to declare API TOS access endpoints (#38416)

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -145,14 +145,13 @@ namespace :api, format: false do
         resources :peers, only: [:index]
         resources :rules, only: [:index]
         resources :domain_blocks, only: [:index]
+        resources :terms_of_service, only: [:index, :show], param: :date
+
         resource :privacy_policy, only: [:show]
-        resource :terms_of_service, only: [:show]
         resource :extended_description, only: [:show]
         resource :translation_languages, only: [:show]
         resource :languages, only: [:show]
         resource :activity, only: [:show], controller: :activity
-
-        get '/terms_of_service/:date', to: 'terms_of_services#show'
       end
     end
 
