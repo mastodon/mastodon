538114c50d855c3a46d6e278f99e705d7e300a74
Matt Jankowski <matt@jankowski.online>
2026-03-26 12:37:55 -0400

    Prefer `member` and let controller name be inferred

--- config/routes.rb
+++ config/routes.rb
@@ -219,7 +219,9 @@ Rails.application.routes.draw do
   resource :statuses_cleanup, controller: :statuses_cleanup, only: [:show, :update]
 
   get '/media_proxy/:id/(*any)', to: 'media_proxy#show', as: :media_proxy, format: false
-  get '/backups/:id/download', to: 'backups#download', as: :download_backup, format: false
+  resources :backups, only: [] do
+    member { get :download, format: false }
+  end
 
   resource :authorize_interaction, only: [:show]
   resource :share, only: [:show]
