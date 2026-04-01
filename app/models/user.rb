0742a01571649e7ca6dde43040959d6288d86c89
Eugen Rochko <eugen@zeonfederated.com>
2026-04-01 12:04:59 +0200

    Add MX record validation to e-mail subscriptions, refactor validator (#38502)

--- app/models/user.rb
+++ app/models/user.rb
@@ -93,9 +93,9 @@ class User < ApplicationRecord
   validates :invite_request, presence: true, on: :create, if: :invite_text_required?
 
   validates :email, presence: true, email_address: true
+  validates :email, email_mx: { attempt_ip: :sign_up_ip }, if: :validate_email_dns?
 
   validates_with UserEmailValidator, if: -> { ENV['EMAIL_DOMAIN_LISTS_APPLY_AFTER_CONFIRMATION'] == 'true' || !confirmed? }
-  validates_with EmailMxValidator, if: :validate_email_dns?
   validates :agreement, acceptance: { allow_nil: false, accept: [true, 'true', '1'] }, on: :create
 
   # Honeypot/anti-spam fields
