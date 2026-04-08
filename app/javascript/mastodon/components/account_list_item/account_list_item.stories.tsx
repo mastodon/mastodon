9dbbac53fcefe5e25d1017fb85c8f273567d983f
diondiondion <mail@diondiondion.com>
2026-04-08 11:38:34 +0200

    Add more list item stories

--- app/javascript/mastodon/components/account_list_item/account_list_item.stories.tsx
+++ app/javascript/mastodon/components/account_list_item/account_list_item.stories.tsx
@@ -25,3 +25,21 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const WithCustomStats: Story = {
+  args: {
+    stats: ['posts', 'last-active'],
+  },
+};
+
+export const WithBorder: Story = {
+  args: {
+    withBorder: true,
+  },
+};
+
+export const WithoutButton: Story = {
+  args: {
+    renderButton: () => null,
+  },
+};
