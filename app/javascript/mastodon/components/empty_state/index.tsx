288a4ac9f0af5810935620da55049003143b20a3
diondiondion <mail@diondiondion.com>
2026-04-16 12:24:23 +0200

    Move empty state image into EmptyState component

--- app/javascript/mastodon/components/empty_state/index.tsx
+++ app/javascript/mastodon/components/empty_state/index.tsx
@@ -1,31 +1,39 @@
 import { FormattedMessage } from 'react-intl';
 
+import ElephantImage from '@/images/elephant_ui.svg?react';
+
 import classes from './empty_state.module.scss';
+
+const images = {
+  default: <ElephantImage className={classes.defaultImage} />,
+};
 
 /**
  * Simple empty state component with a neutral default title and customisable message.
  *
- * Action buttons can be passed as `children`
+ * Action buttons can be passed as `children`.
  */
 
 export const EmptyState: React.FC<{
-  image?: React.ReactNode;
+  image?: keyof typeof images | React.ReactElement | null;
   title?: React.ReactNode;
   message?: React.ReactNode;
   children?: React.ReactNode;
 }> = ({
-  image,
+  image = 'default',
   title = (
     <FormattedMessage id='empty_state.no_results' defaultMessage='No results' />
   ),
   message,
   children,
 }) => {
+  const imageToRender = typeof image === 'string' ? images[image] : image;
+
   return (
     <div className={classes.wrapper}>
-      {(title || message || image) && (
+      {(title || message || imageToRender) && (
         <div className={classes.content}>
-          {image}
+          {imageToRender}
           {!!title && <h3>{title}</h3>}
           {!!message && <p>{message}</p>}
         </div>
