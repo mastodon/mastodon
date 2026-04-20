447bc9f8f7ce9fe838902ade1b3b55ceb221b7a9
Claire <claire.github-309c@sitedethib.com>
2026-04-20 12:15:15 +0200

    Fix incorrect value for `feature_approval.current_user` for local users

--- spec/serializers/rest/account_serializer_spec.rb
+++ spec/serializers/rest/account_serializer_spec.rb
@@ -97,12 +97,26 @@ RSpec.describe REST::AccountSerializer do
           context 'when account is locked' do
             let(:account) { Fabricate(:account, locked: true) }
 
-            it 'includes a policy that allows featuring for followers' do
-              expect(subject['feature_approval']).to include({
-                'automatic' => ['followers'],
-                'manual' => [],
-                'current_user' => 'automatic',
-              })
+            context 'when the current account does not follow the user' do
+              it 'includes a policy that allows featuring for followers and has "denied" for the current user' do
+                expect(subject['feature_approval']).to include({
+                  'automatic' => ['followers'],
+                  'manual' => [],
+                  'current_user' => 'denied',
+                })
+              end
+            end
+
+            context 'when the current account follows the user' do
+              before { current_user.account.follow!(account) }
+
+              it 'includes a policy that allows featuring for followers and has "automatic" for the current user' do
+                expect(subject['feature_approval']).to include({
+                  'automatic' => ['followers'],
+                  'manual' => [],
+                  'current_user' => 'automatic',
+                })
+              end
             end
           end
         end
