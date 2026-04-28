03045425b7323e8ce4e6ce81942d798a500c655a
Echo <ChaosExAnima@users.noreply.github.com>
2026-04-28 14:37:58 +0200

    Allow keyboard modal form submission (#38826)

--- app/javascript/mastodon/features/account_timeline/modals/join_modal.tsx
+++ app/javascript/mastodon/features/account_timeline/modals/join_modal.tsx
@@ -97,7 +97,7 @@ export const AccountJoinModal: FC<{
   }, [anniversary, handle, dispatch, isMe]);
 
   return (
-    <ModalShell className={classes.joinShell}>
+    <ModalShell as='div' className={classes.joinShell}>
       <ModalShellBody className={classes.joinWrapper}>
         <AccountAnniversaryImage anniversary={anniversary} />
 
