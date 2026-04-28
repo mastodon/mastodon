3d5cb624ba1bf861d41f4bf42f7ecf02b010718e
diondiondion <mail@diondiondion.com>
2026-04-28 13:32:27 +0200

    Update design of Collections list page (#38822)

--- app/javascript/mastodon/features/collections/index.tsx
+++ app/javascript/mastodon/features/collections/index.tsx
@@ -5,9 +5,9 @@ import { defineMessages, useIntl, FormattedMessage } from 'react-intl';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 
+import { EmptyState } from '@/mastodon/components/empty_state';
+import { TabLink, TabList } from '@/mastodon/components/tab_list';
 import AddIcon from '@/material-icons/400-24px/add.svg?react';
-import CollectionsFilledIcon from '@/material-icons/400-24px/category-fill.svg?react';
-import SquigglyArrow from '@/svg-icons/squiggly_arrow.svg?react';
 import { Column } from 'mastodon/components/column';
 import { ColumnHeader } from 'mastodon/components/column_header';
 import { DisplayNameSimple } from 'mastodon/components/display_name/simple';
@@ -30,15 +30,38 @@ import {
   MaxCollectionsCallout,
   userCollectionLimit,
 } from './editor';
+import classes from './styles.module.scss';
 import { areCollectionsEnabled } from './utils';
 
 const messages = defineMessages({
-  headingMe: { id: 'column.my_collections', defaultMessage: 'My collections' },
+  headingMe: {
+    id: 'column.your_collections',
+    defaultMessage: 'Your Collections',
+  },
   headingOther: {
     id: 'column.other_collections',
-    defaultMessage: 'Collections by {name}',
+    defaultMessage: "{name}'s Collections",
+  },
+  createdByYou: {
+    id: 'collections.list.created_by_you',
+    defaultMessage: 'Created by you',
+  },
+  createdByAuthor: {
+    id: 'collections.list.created_by_author',
+    defaultMessage: 'Created by {name}',
+  },
+  featuringYou: {
+    id: 'collections.list.featuring_you',
+    defaultMessage: 'Featuring you',
   },
 });
+
+const CreateButton: React.FC = () => (
+  <Link to='/collections/new' className='button button--compact'>
+    <Icon id='plus' icon={AddIcon} />
+    <FormattedMessage {...editorMessages.newCollection} />
+  </Link>
+);
 
 export function useAccountCollections(accountId: string | null | undefined) {
   const dispatch = useAppDispatch();
@@ -62,33 +85,11 @@ export const Collections: React.FC<{
 
   const { collections, status } = useAccountCollections(accountId);
 
-  const emptyMessage =
-    status === 'error' || !accountId ? (
-      <FormattedMessage
-        id='collections.error_loading_collections'
-        defaultMessage='There was an error when trying to load your collections.'
-        tagName='span'
-      />
-    ) : (
-      <>
-        <span>
-          <FormattedMessage
-            id='collections.no_collections_yet'
-            defaultMessage='No collections yet.'
-          />
-          <br />
-          <FormattedMessage
-            id='collections.create_a_collection_hint'
-            defaultMessage='Create a collection to recommend or share your favourite accounts with others.'
-          />
-        </span>
-
-        <SquigglyArrow className='empty-column-indicator__arrow' />
-      </>
-    );
-
   const canCreateMoreCollections = collections.length < userCollectionLimit;
   const isOwnCollection = accountId === me;
+  const showCreateButton =
+    isOwnCollection && status === 'idle' && canCreateMoreCollections;
+
   const titleMessage = isOwnCollection
     ? messages.headingMe
     : messages.headingOther;
@@ -100,45 +101,88 @@ export const Collections: React.FC<{
     name: <DisplayNameSimple account={account} />,
   });
 
+  const tabMessage = isOwnCollection
+    ? messages.createdByYou
+    : messages.createdByAuthor;
+
+  const errorMessage = (status === 'error' || !accountId) && (
+    <FormattedMessage
+      id='collections.error_loading_collections'
+      defaultMessage='There was an error when trying to load your collections.'
+      tagName='span'
+    />
+  );
+
   return (
     <Column bindToDocument={!multiColumn} label={pageTitle}>
-      <ColumnHeader
-        title={pageTitleHtml}
-        icon='collections'
-        iconComponent={CollectionsFilledIcon}
-        multiColumn={multiColumn}
-        extraButton={
-          isOwnCollection &&
-          status === 'idle' &&
-          canCreateMoreCollections && (
-            <Link
-              to='/collections/new'
-              className='column-header__button'
-              title={intl.formatMessage(editorMessages.create)}
-              aria-label={intl.formatMessage(editorMessages.create)}
-            >
-              <Icon id='plus' icon={AddIcon} />
-            </Link>
-          )
-        }
-      />
+      <ColumnHeader showBackButton multiColumn={multiColumn} />
 
       <Scrollable>
-        {status === 'idle' && !canCreateMoreCollections && (
-          <MaxCollectionsCallout />
+        <header className={classes.header}>
+          <h1 className={classes.heading}>{pageTitleHtml}</h1>
+          <TabList plain>
+            <TabLink exact to={`/@${account?.acct}/collections`}>
+              {intl.formatMessage(tabMessage, {
+                name: <DisplayNameSimple account={account} />,
+              })}
+            </TabLink>
+          </TabList>
+        </header>
+        {collections.length > 0 ? (
+          <>
+            {status === 'idle' && (
+              <div className={classes.listHeader}>
+                <h2 className={classes.subHeading}>
+                  <FormattedMessage
+                    id='collections.list.collections_with_count'
+                    defaultMessage='{count, plural, one {# Collection} other {# Collections}}'
+                    values={{
+                      count: collections.length,
+                    }}
+                  />
+                </h2>
+                {showCreateButton && <CreateButton />}
+              </div>
+            )}
+            <ItemList
+              emptyMessage={errorMessage}
+              isLoading={status === 'loading'}
+            >
+              {!canCreateMoreCollections && (
+                <MaxCollectionsCallout
+                  className={classes.maxCollectionsError}
+                />
+              )}
+              {collections.map((item, index) => (
+                <CollectionListItem
+                  withTimestamp
+                  withAuthorHandle={false}
+                  key={item.id}
+                  collection={item}
+                  positionInList={index + 1}
+                  listSize={collections.length}
+                />
+              ))}
+            </ItemList>
+          </>
+        ) : (
+          <EmptyState
+            title={
+              <FormattedMessage
+                id='empty_column.account_featured_self.showcase_accounts'
+                defaultMessage='Showcase your favorite accounts'
+              />
+            }
+            message={
+              <FormattedMessage
+                id='empty_column.account_featured_self.showcase_accounts_desc'
+                defaultMessage='Collections are curated lists of accounts to help others discover more of the Fediverse.'
+              />
+            }
+          >
+            <CreateButton />
+          </EmptyState>
         )}
-        <ItemList emptyMessage={emptyMessage} isLoading={status === 'loading'}>
-          {collections.map((item, index) => (
-            <CollectionListItem
-              withTimestamp
-              withAuthorHandle={false}
-              key={item.id}
-              collection={item}
-              positionInList={index + 1}
-              listSize={collections.length}
-            />
-          ))}
-        </ItemList>
       </Scrollable>
 
       <Helmet>
