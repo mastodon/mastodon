aa80e46518758682c1789a701d3957efdade17c7
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-04-28 14:10:28 +0200

    use form for ModalShell

--- app/javascript/mastodon/features/ui/components/confirmation_modals/confirmation_modal.tsx
+++ app/javascript/mastodon/features/ui/components/confirmation_modals/confirmation_modal.tsx
@@ -65,7 +65,7 @@ export const ConfirmationModal: React.FC<
   }, [onClose, onSecondary]);
 
   return (
-    <ModalShell>
+    <ModalShell onSubmit={handleClick}>
       <ModalShellBody className={className}>
         <h1 id={titleId}>{title}</h1>
         {message && <p>{message}</p>}
@@ -99,6 +99,7 @@ export const ConfirmationModal: React.FC<
 
         {/* eslint-disable jsx-a11y/no-autofocus -- we are in a modal and thus autofocusing is justified */}
         <Button
+          type='submit'
           onClick={handleClick}
           loading={updating}
           disabled={disabled}
