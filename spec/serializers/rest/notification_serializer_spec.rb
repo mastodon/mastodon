5b395774c05d7deb619e7fef725bfe957615f452
Claire <claire.github-309c@sitedethib.com>
2026-04-29 17:53:29 +0200

    Add fallback attributes to notifications for new and infrequent notifications (#38832)

--- spec/serializers/rest/notification_serializer_spec.rb
+++ spec/serializers/rest/notification_serializer_spec.rb
@@ -6,10 +6,16 @@ RSpec.describe REST::NotificationSerializer do
   subject do
     serialized_record_json(
       notification,
-      described_class
+      described_class,
+      options: {
+        scope: current_user,
+        scope_name: :current_user,
+        supported_notification_types: [],
+      }
     )
   end
 
+  let(:current_user) { Fabricate(:user) }
   let(:notification) { Fabricate :notification }
 
   context 'when created_at is populated' do
@@ -20,4 +26,25 @@ RSpec.describe REST::NotificationSerializer do
         )
     end
   end
+
+  shared_examples 'with fallback notifications' do |type, fabricator|
+    let(:notification) { Fabricate(:notification, type:, activity: Fabricate(fabricator), account: current_user.account) }
+
+    it 'renders correctly' do
+      expect(subject)
+        .to include(
+          'type' => type,
+          'fallback' => include(
+            'title' => anything,
+            'summary' => anything
+          )
+        )
+    end
+  end
+
+  it_behaves_like 'with fallback notifications', 'severed_relationships', :account_relationship_severance_event
+  it_behaves_like 'with fallback notifications', 'moderation_warning', :account_warning
+  it_behaves_like 'with fallback notifications', 'admin.report', :report
+  it_behaves_like 'with fallback notifications', 'added_to_collection', :collection_item
+  it_behaves_like 'with fallback notifications', 'collection_update', :collection
 end
