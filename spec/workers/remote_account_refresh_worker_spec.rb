0df5b67709465b4b5b60f0ae7f86f81c88bd88fe
Matt Jankowski <matt@jankowski.online>
2026-05-06 08:32:07 -0400

    Add failing service case

--- spec/workers/remote_account_refresh_worker_spec.rb
+++ spec/workers/remote_account_refresh_worker_spec.rb
@@ -4,35 +4,50 @@ require 'rails_helper'
 
 RSpec.describe RemoteAccountRefreshWorker do
   let(:worker) { described_class.new }
-  let(:service) { instance_double(ActivityPub::FetchRemoteAccountService, call: true) }
 
   describe '#perform' do
     before { stub_service }
 
     let(:account) { Fabricate(:account, domain: 'host.example') }
 
-    it 'sends the status to the service' do
-      worker.perform(account.id)
-
-      expect(service).to have_received(:call).with(account.uri)
-    end
-
-    it 'returns nil for non-existent record' do
-      result = worker.perform(123_123_123)
-
-      expect(result).to be_nil
-    end
-
-    it 'returns nil for a local record' do
-      account = Fabricate :account, domain: nil
-      result = worker.perform(account.id)
-      expect(result).to be_nil
-    end
-
     def stub_service
       allow(ActivityPub::FetchRemoteAccountService)
         .to receive(:new)
         .and_return(service)
+    end
+
+    context 'with a working service' do
+      let(:service) { instance_double(ActivityPub::FetchRemoteAccountService, call: true) }
+
+      it 'sends the status to the service' do
+        worker.perform(account.id)
+
+        expect(service).to have_received(:call).with(account.uri)
+      end
+
+      it 'returns nil for non-existent record' do
+        result = worker.perform(123_123_123)
+
+        expect(result).to be_nil
+      end
+
+      it 'returns nil for a local record' do
+        account = Fabricate :account, domain: nil
+        result = worker.perform(account.id)
+        expect(result).to be_nil
+      end
+    end
+
+    context 'with a failing service' do
+      let(:service) { instance_double(ActivityPub::FetchRemoteAccountService) }
+      let(:response) { instance_double(HTTP::Response, code: 500) }
+
+      before { allow(service).to receive(:call).and_raise(Mastodon::UnexpectedResponseError, response) }
+
+      it 'raises error when service fails' do
+        expect { worker.perform(account.id) }
+          .to raise_error(Mastodon::UnexpectedResponseError)
+      end
     end
   end
 end
