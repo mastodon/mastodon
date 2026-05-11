971928c2e0506522a58fdefd8588f11f2cdbc665
abhishek Tanwar <abhishekgangdhari@gmail.com>
2026-05-11 14:33:36 +0530

    spec: improve Web::PushSubscription branch coverage

--- spec/models/web/push_subscription_spec.rb
+++ spec/models/web/push_subscription_spec.rb
@@ -41,7 +41,8 @@ RSpec.describe Web::PushSubscription do
       let(:policy) { 'all' }
 
       it 'returns true' do
-        expect(subject.pushable?(notification)).to be true
+        # Predicate matcher syntax automatically delegates to `pushable?`
+        expect(subject).to be_pushable(notification)
       end
     end
 
@@ -49,7 +50,8 @@ RSpec.describe Web::PushSubscription do
       let(:policy) { 'none' }
 
       it 'returns false' do
-        expect(subject.pushable?(notification)).to be false
+        # Predicate matcher syntax automatically delegates to `pushable?`
+        expect(subject).to_not be_pushable(notification)
       end
     end
 
@@ -62,13 +64,13 @@ RSpec.describe Web::PushSubscription do
         end
 
         it 'returns true' do
-          expect(subject.pushable?(notification)).to be true
+          expect(subject).to be_pushable(notification)
         end
       end
 
       context 'when notification is not from someone you follow' do
         it 'returns false' do
-          expect(subject.pushable?(notification)).to be false
+          expect(subject).to_not be_pushable(notification)
         end
       end
     end
@@ -82,19 +84,88 @@ RSpec.describe Web::PushSubscription do
         end
 
         it 'returns true' do
-          expect(subject.pushable?(notification)).to be true
+          expect(subject).to be_pushable(notification)
         end
       end
 
       context 'when notification is not from someone who follows you' do
         it 'returns false' do
-          expect(subject.pushable?(notification)).to be false
+          expect(subject).to_not be_pushable(notification)
         end
+      end
+    end
+
+    context 'when alerts configuration is missing' do
+      let(:data) { nil }
+
+      it 'returns false' do
+        expect(subject).to_not be_pushable(notification)
+      end
+    end
+
+    context 'when notification type is not configured in alerts' do
+      let(:notification_type) { :poll }
+
+      it 'returns false' do
+        expect(subject).to_not be_pushable(notification)
+      end
+    end
+
+    context 'when policy is missing' do
+      let(:data) do
+        {
+          alerts: {
+            mention: true,
+          },
+        }
+      end
+
+      it 'returns true' do
+        expect(subject).to be_pushable(notification)
+      end
+    end
+
+    context 'when policy is unknown' do
+      let(:policy) { 'unknown' }
+
+      it 'returns a falsey value' do
+        # Predicate matcher syntax automatically delegates to `pushable?`
+        expect(subject).to_not be_pushable(notification)
       end
     end
   end
 
   describe 'Delegations' do
     it { is_expected.to delegate_method(:token).to(:access_token).with_prefix(:associated_access) }
+  end
+
+  describe '.unsubscribe_for' do
+    let(:application) { Fabricate(:application) }
+    let(:user) { Fabricate(:user, account: account) }
+    let(:subscription) do
+      Fabricate(
+        :web_push_subscription,
+        user: user,
+        access_token: access_token
+      )
+    end
+
+    let(:access_token) do
+      Fabricate(
+        :accessible_access_token,
+        application: application,
+        resource_owner_id: user.id
+      )
+    end
+
+    before do
+      subscription
+    end
+
+    it 'removes subscriptions for matching application and resource owner' do
+      expect do
+        described_class.unsubscribe_for(application.id, user)
+      end.to change(described_class, :count).by(-1)
+    end
   end
 end
