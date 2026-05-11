afc7b91f42704b5c6ffda1b2ccb1b0265bb0c905
Pia B <pia.bregulla@web.de>
2026-05-11 16:29:40 +0200

    add index and batch to controller

--- app/controllers/admin/collections_controller.rb
+++ app/controllers/admin/collections_controller.rb
@@ -4,12 +4,37 @@ module Admin
   class CollectionsController < BaseController
     before_action :set_account
     before_action :set_collection, only: :show
+    before_action :set_collections, except: :show
+
+    PER_PAGE = 20
+
+    def index
+      authorize [:admin, :collection], :index?
+      @collections
+    end
 
     def show
       authorize @collection, :show?
     end
 
+    def batch
+      authorize [:admin, :collection], :index?
+
+      @collection_batch_action = Admin::CollectionBatchAction.new(admin_collection_batch_action_params.merge(current_account: current_account, report_id: params[:report_id], type: 'report'))
+
+      @collection_batch_action.save!
+    rescue ActionController::ParameterMissing
+      flash[:alert] = I18n.t('admin.collections.no_collection_selected')
+    ensure
+      redirect_to admin_report_path(params[:report_id])
+    end
+
     private
+
+    def admin_collection_batch_action_params
+      params
+        .expect(admin_collection_batch_action: [collection_ids: []])
+    end
 
     def set_account
       @account = Account.find(params[:account_id])
@@ -17,6 +42,10 @@ module Admin
 
     def set_collection
       @collection = @account.collections.includes(accepted_collection_items: :account).find(params[:id])
+    end
+
+    def set_collections
+      @collections = @account.collections.includes(accepted_collection_items: :account).page(params[:page]).per(PER_PAGE)
     end
   end
 end
