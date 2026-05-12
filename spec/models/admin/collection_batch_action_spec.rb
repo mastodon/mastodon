ca720c2fe64fbde3d49f2af949b59df1ec1faa65
Pia B <pia.bregulla@web.de>
2026-05-12 16:11:21 +0200

    fix collectionbatch action spec

--- spec/models/admin/collection_batch_action_spec.rb
+++ spec/models/admin/collection_batch_action_spec.rb
@@ -20,25 +20,30 @@ RSpec.describe Admin::CollectionBatchAction do
   let(:report) { Fabricate(:report, account: target_account, target_account: account) }
   let(:type) { 'report' }
 
-  before 'create colelction report' do
+  before 'create collection report' do
     report.collections << collection
     report.save!
   end
 
   describe '#save!' do
     context 'when with_report? is true' do
+      let(:other_collection) { Fabricate(:collection, account: account) }
       let(:report_id) { report.id }
 
-      it 'creates no report' do
+      it 'creates no report and adds the collection to the existing report' do
+        collection_ids << other_collection.id
+
         expect { subject.save! }.to_not change(Report, :count)
+        expect(report.collections).to include(other_collection)
       end
     end
 
     context 'when with_report? is false' do
       let(:report_id) { nil }
 
-      it 'creates a new report' do
+      it 'creates a new report from the collection_ids' do
         expect { subject.save! }.to change(Report, :count).by(1)
+        expect(Report.last.collections).to include(collection)
       end
     end
   end
