d5a7b383faf9085e88e3c54639aefb026f2b3694
Echo <ChaosExAnima@users.noreply.github.com>
2026-05-19 14:22:52 +0200

    Autosuggest emojis rendering fix (#39077)

--- app/javascript/mastodon/components/autosuggest_emoji.tsx
+++ app/javascript/mastodon/components/autosuggest_emoji.tsx
@@ -1,7 +1,5 @@
 import type { FC } from 'react';
 
-import { useCustomEmojis } from '@/mastodon/hooks/useCustomEmojis';
-
 import { Emoji } from './emoji';
 
 interface LegacyEmoji {
@@ -12,11 +10,10 @@ interface LegacyEmoji {
 }
 
 export const AutosuggestEmoji: FC<{ emoji: LegacyEmoji }> = ({ emoji }) => {
-  const emojis = useCustomEmojis();
   const colons = `:${emoji.id}:`;
   return (
     <div className='autosuggest-emoji'>
-      <Emoji code={emoji.native ?? colons} customEmoji={emojis} />
+      <Emoji code={emoji.native ?? colons} />
       <div className='autosuggest-emoji__name'>{colons}</div>
     </div>
   );
