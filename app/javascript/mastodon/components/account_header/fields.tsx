67121ebf879c4e7a82b2f44023c81abb657778b3
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-05-19 14:38:42 +0200

    trigger initial field flow recalculation

--- app/javascript/mastodon/components/account_header/fields.tsx
+++ app/javascript/mastodon/components/account_header/fields.tsx
@@ -317,9 +317,10 @@ function useColumnWrap() {
       if (element) {
         listRef.current = element;
         observer.observe(element);
+        handleRecalculate();
       }
     },
-    [observer],
+    [handleRecalculate, observer],
   );
 
   return { wrapperRef: wrapperRefCallback };
