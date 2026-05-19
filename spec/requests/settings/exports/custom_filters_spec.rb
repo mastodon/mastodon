6ed6eaa0137f4475ce27b622e7b4992a53bda98c
Pia B <pia.bregulla@web.de>
2026-05-19 16:48:38 +0200

    add controller and request spec

---
+++ spec/requests/settings/exports/custom_filters_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Settings / Exports / CustomFilters' do
+  describe 'GET /settings/exports/custom_filters' do
+    context 'with a signed in user who has custom_filters' do
+      let(:user) { Fabricate(:user) }
+      let(:filter) { Fabricate(:custom_filter, account: user.account, phrase: 'foo') }
+      let(:keyword) { Fabricate(:custom_filter_keyword, custom_filter: filter) }
+      let(:filter_keyword) { Fabricate(:custom_filter_keyword, keyword: 'something', custom_filter: filter, whole_word: false) }
+      let(:status_filter) { Fabricate(:custom_filter_status, custom_filter: filter) }
+      let(:expected_response_body) do
+        [{
+          'title' => 'foo',
+          'expire_at' => nil,
+          'context' => ['home', 'notifications'],
+          'action' => 'warn',
+          'keywords_attributes' => [{
+            'keyword' => 'discourse',
+            'whole_word' => true,
+          }, {
+            'keyword' => 'something',
+            'whole_word' => false,
+          }],
+          'statuses' => ['Lorem ipsum dolor sit amet'],
+        }]
+      end
+
+      before do
+        sign_in user
+        filter
+        keyword
+        filter_keyword
+        status_filter
+      end
+
+      it 'returns a JSON with the custom filters' do
+        get '/settings/exports/custom_filters.json'
+
+        expect(response).to have_http_status(200)
+        expect(response.content_type).to eq('application/json')
+        expect(response.parsed_body).to eq(expected_response_body)
+      end
+    end
+  end
+end
