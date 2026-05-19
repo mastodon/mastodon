634f0db190391ddefb017410e8fd0c99fecc1ebe
diondiondion <mail@diondiondion.com>
2026-05-19 15:57:26 +0200

    Ensure that no collection preview is shown for quote posts

--- app/javascript/mastodon/components/status.jsx
+++ app/javascript/mastodon/components/status.jsx
@@ -566,7 +566,7 @@ class Status extends ImmutablePureComponent {
           />
         );
       }
-    } else if (status.get('tagged_collections').size) {
+    } else if (status.get('tagged_collections').size && !status.get('quote')) {
       const firstLinkedCollection = status.get('tagged_collections').first();
       if (firstLinkedCollection) {
         media = (
