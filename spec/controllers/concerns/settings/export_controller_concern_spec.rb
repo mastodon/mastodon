51002209674d489458fcdce3e8e7b33d10ea0fda
Pia B <pia.bregulla@web.de>
2026-05-19 15:13:22 +0200

    add to controller concern and spec

--- spec/controllers/concerns/settings/export_controller_concern_spec.rb
+++ spec/controllers/concerns/settings/export_controller_concern_spec.rb
@@ -15,10 +15,14 @@ RSpec.describe Settings::ExportControllerConcern do
     end
   end
 
+  def sign_in_user
+    user = Fabricate(:user)
+    sign_in user
+  end
+
   describe 'GET #index' do
     it 'returns a csv of the exported data when signed in' do
-      user = Fabricate(:user)
-      sign_in user
+      sign_in_user
       get :index, format: :csv
 
       expect(response).to have_http_status(200)
@@ -31,6 +35,19 @@ RSpec.describe Settings::ExportControllerConcern do
     it 'returns unauthorized when not signed in' do
       get :index, format: :csv
       expect(response).to have_http_status(401)
+    end
+  end
+
+  context 'when with json format' do
+    it 'returns a json of the exported data when signed in' do
+      sign_in_user
+      get :index, format: :json
+
+      expect(response).to have_http_status(200)
+      expect(response.media_type).to eq 'application/json'
+      expect(response)
+        .to have_attachment('anonymous.json')
+      expect(response.body).to eq 'body data value'
     end
   end
 end
