a521889ff622dc92f18cea388d76ce4ca93f8fb0
diondiondion <mail@diondiondion.com>
2026-05-19 14:31:50 +0200

    More permissive types for canAccountBeAdded utils

--- app/javascript/mastodon/features/collections/utils.ts
+++ app/javascript/mastodon/features/collections/utils.ts
@@ -1,4 +1,5 @@
 import type { ApiMutedAccountJSON } from '@/mastodon/api_types/accounts';
+import type { Account } from '@/mastodon/models/account';
 import { isServerFeatureEnabled } from '@/mastodon/utils/environment';
 
 export function areCollectionsEnabled() {
@@ -7,9 +8,11 @@ export function areCollectionsEnabled() {
 
 export const getCollectionPath = (id: string) => `/collections/${id}`;
 
-export const canAccountBeAdded = (account: ApiMutedAccountJSON) =>
+export const canAccountBeAdded = (account: ApiMutedAccountJSON | Account) =>
   ['automatic', 'manual'].includes(account.feature_approval.current_user);
 
-export const canAccountBeAddedByFollowers = (account: ApiMutedAccountJSON) =>
+export const canAccountBeAddedByFollowers = (
+  account: ApiMutedAccountJSON | Account,
+) =>
   account.feature_approval.automatic.includes('followers') ||
   account.feature_approval.manual.includes('followers');
