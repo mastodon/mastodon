mastodon/mastodon

commit d48470a1fcb3ddd93335a8a971fd2661b07f6d15

Change media description length limit for remote media attachments from 1500 to 10000 characters (#37921)

app/models/media_attachment.rb
@@ -39,6 +39,7 @@ class MediaAttachment < ApplicationRecord
   enum :processing, { queued: 0, in_progress: 1, complete: 2, failed: 3 }, prefix: true
 
   MAX_DESCRIPTION_LENGTH = 1_500
+  MAX_DESCRIPTION_HARD_LENGTH_LIMIT = 10_000
 
   IMAGE_LIMIT = 16.megabytes
   VIDEO_LIMIT = 99.megabytes

app/lib/activitypub/parser/media_attachment_parser.rb
@@ -30,7 +30,7 @@ class ActivityPub::Parser::MediaAttachmentParser
 
   def description
     str = @json['summary'].presence || @json['name'].presence
-    str = str.strip[0...MediaAttachment::MAX_DESCRIPTION_LENGTH] if str.present?
+    str = str.strip[0...MediaAttachment::MAX_DESCRIPTION_HARD_LENGTH_LIMIT] if str.present?
     str
   end
 

app/models/concerns/account/avatar.rb
@@ -25,7 +25,7 @@ module Account::Avatar
     validates_attachment_size :avatar, less_than: AVATAR_LIMIT
     remotable_attachment :avatar, AVATAR_LIMIT, suppress_errors: false
 
-    validates :avatar_description, length: { maximum: MediaAttachment::MAX_DESCRIPTION_LENGTH }
+    validates :avatar_description, length: { maximum: MediaAttachment::MAX_DESCRIPTION_LENGTH }, if: -> { local? && will_save_change_to_avatar_description? }
   end
 
   def avatar_original_url

app/models/concerns/account/header.rb
@@ -26,7 +26,7 @@ module Account::Header
     validates_attachment_size :header, less_than: HEADER_LIMIT
     remotable_attachment :header, HEADER_LIMIT, suppress_errors: false
 
-    validates :header_description, length: { maximum: MediaAttachment::MAX_DESCRIPTION_LENGTH }
+    validates :header_description, length: { maximum: MediaAttachment::MAX_DESCRIPTION_LENGTH }, if: -> { local? && will_save_change_to_header_description? }
   end
 
   def header_original_url

app/services/activitypub/process_account_service.rb
@@ -238,7 +238,7 @@ class ActivityPub::ProcessAccountService < BaseService
       url = first_of_value(value['url'])
       url = url['href'] if url.is_a?(Hash)
       description = value['summary'].presence || value['name'].presence
-      description = description.strip[0...MediaAttachment::MAX_DESCRIPTION_LENGTH] if description.present?
+      description = description.strip[0...MediaAttachment::MAX_DESCRIPTION_HARD_LENGTH_LIMIT] if description.present?
     else
       url = value
     end

commit c4004eee15c56af36c33d102c0eaa86e63c49466

Fix remote statuses with large media descriptions being rejected
Fix issue introduced by #37921

app/models/media_attachment.rb
@@ -205,7 +205,7 @@ class MediaAttachment < ApplicationRecord
   remotable_attachment :thumbnail, IMAGE_LIMIT, suppress_errors: true, download_on_assign: false
 
   validates :account, presence: true
-  validates :description, length: { maximum: MAX_DESCRIPTION_LENGTH }
+  validates :description, length: { maximum: MAX_DESCRIPTION_LENGTH }, if: :local?
   validates :file, presence: true, if: :local?
   validates :thumbnail, absence: true, if: -> { local? && !audio_or_video? }
 

spec/lib/activitypub/activity/create_spec.rb
@@ -608,19 +608,19 @@ RSpec.describe ActivityPub::Activity::Create do
                 type: 'Document',
                 mediaType: 'image/png',
                 url: 'http://example.com/attachment.png',
-                name: '*' * MediaAttachment::MAX_DESCRIPTION_LENGTH,
+                name: '*' * (MediaAttachment::MAX_DESCRIPTION_HARD_LENGTH_LIMIT + 5),
               },
             ]
           )
         end
 
-        it 'creates status' do
+        it 'creates status with truncated description' do
           expect { subject.perform }.to change(sender.statuses, :count).by(1)
 
           status = sender.statuses.first
 
           expect(status).to_not be_nil
-          expect(status.media_attachments.map(&:description)).to include('*' * MediaAttachment::MAX_DESCRIPTION_LENGTH)
+          expect(status.media_attachments.map(&:description)).to include('*' * MediaAttachment::MAX_DESCRIPTION_HARD_LENGTH_LIMIT)
         end
       end
 
@@ -632,19 +632,19 @@ RSpec.describe ActivityPub::Activity::Create do
                 type: 'Document',
                 mediaType: 'image/png',
                 url: 'http://example.com/attachment.png',
-                summary: '*' * MediaAttachment::MAX_DESCRIPTION_LENGTH,
+                summary: '*' * (MediaAttachment::MAX_DESCRIPTION_HARD_LENGTH_LIMIT + 5),
               },
             ]
           )
         end
 
-        it 'creates status' do
+        it 'creates status with truncated description' do
           expect { subject.perform }.to change(sender.statuses, :count).by(1)
 
           status = sender.statuses.first
 
           expect(status).to_not be_nil
-          expect(status.media_attachments.map(&:description)).to include('*' * MediaAttachment::MAX_DESCRIPTION_LENGTH)
+          expect(status.media_attachments.map(&:description)).to include('*' * MediaAttachment::MAX_DESCRIPTION_HARD_LENGTH_LIMIT)
         end
       end
 