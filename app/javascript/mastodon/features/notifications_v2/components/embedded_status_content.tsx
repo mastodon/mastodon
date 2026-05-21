6735902c1a6b409f58cfe7a494f1860b65a22fc3
Echo <ChaosExAnima@users.noreply.github.com>
2026-05-21 15:17:35 +0200

    Fixes collection notification urls (#39127)

--- app/javascript/mastodon/features/notifications_v2/components/embedded_status_content.tsx
+++ app/javascript/mastodon/features/notifications_v2/components/embedded_status_content.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useMemo } from 'react';
 
-import type { List } from 'immutable';
+import type { List, Map } from 'immutable';
 
 import { EmojiHTML } from '@/mastodon/components/emoji/html';
 import { useElementHandledLink } from '@/mastodon/components/status/handled_link';
@@ -23,8 +23,19 @@ export const EmbeddedStatusContent: React.FC<{
     },
     [mentions],
   );
+  const hrefToCollection = useCallback(
+    (href: string) => {
+      const collections = status.get('tagged_collections') as List<
+        Map<'url' | 'id', string>
+      >;
+      const collection = collections.find((item) => item.get('url') === href);
+      return collection?.get('id');
+    },
+    [status],
+  );
   const htmlHandlers = useElementHandledLink({
     hashtagAccountId: status.get('account') as string | undefined,
+    hrefToCollectionId: hrefToCollection,
     hrefToMention,
   });
 
