e9bd8fb686ef8f112630236264abc5d9aacb5894
Shlee <github@shl.ee>
2026-05-29 14:02:24 +0930

    Update cacheable_response.rb

--- spec/support/matchers/cacheable_response.rb
+++ spec/support/matchers/cacheable_response.rb
@@ -26,10 +26,8 @@ RSpec::Matchers.define :have_cacheable_headers do
     ERROR
   end
 
-  # Values that middleware/framework may add to Vary beyond what controllers set
-  ALLOWED_EXTRA_VARY_VALUES = %w(Accept-Encoding Origin).freeze
-
   def check_vary
+    ALLOWED_EXTRA_VARY_VALUES = %w(Accept-Encoding Origin).freeze
     actual_vary = @response.headers['Vary']&.split(',')&.map(&:strip) || []
     expected_vary = @expected_vary.split(',').map(&:strip)
     missing = expected_vary - actual_vary
