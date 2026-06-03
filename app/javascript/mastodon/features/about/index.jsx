0a35bdd6ff13d503255b107124dc0e2d822e4ae5
Renaud Chaput <renchap@gmail.com>
2026-06-03 11:05:50 +0200

    Change Mastodon gGmbH => Mastodon GmbH
    Those were leftovers we forgot to change after our non-profit status was
    stripped in Germany (see
    https://blog.joinmastodon.org/2026/02/administrative-updates/)

--- app/javascript/mastodon/features/about/index.jsx
+++ app/javascript/mastodon/features/about/index.jsx
@@ -168,7 +168,7 @@ class About extends PureComponent {
           <LinkFooter context='about' />
 
           <div className='about__footer'>
-            <p><FormattedMessage id='about.disclaimer' defaultMessage='Mastodon is free, open-source software, and a trademark of Mastodon gGmbH.' /></p>
+            <p><FormattedMessage id='about.disclaimer' defaultMessage='Mastodon is free, open-source software, and a trademark of Mastodon GmbH.' /></p>
           </div>
         </div>
 
