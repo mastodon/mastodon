1bfdfcae7b695811d021412d9d3abe6a833df7e9
diondiondion <mail@diondiondion.com>
2026-06-08 15:45:28 +0200

    Fix broken column header layout after heading refactor (#39331)

--- app/javascript/mastodon/components/column_header.tsx
+++ app/javascript/mastodon/components/column_header.tsx
@@ -276,17 +276,20 @@ export const ColumnHeader: React.FC<Props> = ({
     </>
   );
 
+  const titleClassNames = classNames('column-header__title', {
+    'column-header__title--with-back-button': !!backButton,
+  });
+
   const component = (
     <div className={wrapperClassName}>
       <div className={headingClassName}>
+        {backButton}
         {hasTitle && (
           <h1 className='column-header__title-wrapper'>
-            {backButton}
-
             {onClick ? (
               <button
                 onClick={handleTitleClick}
-                className='column-header__title'
+                className={titleClassNames}
                 type='button'
                 id={getColumnSkipLinkId(columnIndex)}
               >
@@ -294,7 +297,7 @@ export const ColumnHeader: React.FC<Props> = ({
               </button>
             ) : (
               <span
-                className='column-header__title'
+                className={titleClassNames}
                 tabIndex={-1}
                 id={getColumnSkipLinkId(columnIndex)}
               >
@@ -303,8 +306,6 @@ export const ColumnHeader: React.FC<Props> = ({
             )}
           </h1>
         )}
-
-        {!hasTitle && backButton}
 
         <div className='column-header__buttons'>
           {extraButton}
