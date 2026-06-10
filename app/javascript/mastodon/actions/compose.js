720df869da019b68be570bbaac19adc304759d79
diondiondion <mail@diondiondion.com>
2026-06-10 14:47:15 +0200

    Focus post-in-thread when navigating to post after creating it

--- app/javascript/mastodon/actions/compose.js
+++ app/javascript/mastodon/actions/compose.js
@@ -292,7 +292,10 @@ export function submitCompose(successCallback) {
         message: statusId === null ? messages.published : messages.saved,
         action: messages.open,
         dismissAfter: 10000,
-        onClick: () => browserHistory.push(`/@${response.data.account.username}/${response.data.id}`),
+        onClick: () => browserHistory.push(
+          `/@${response.data.account.username}/${response.data.id}`,
+          { focusTarget: 'detailed-status' }
+        ),
       }));
     }).catch(function (error) {
       dispatch(submitComposeFail(error));
