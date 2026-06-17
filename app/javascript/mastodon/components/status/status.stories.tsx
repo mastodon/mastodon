419925d4466935f1ec7f80d06c49bdcce84e5ee4
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-06-17 12:45:10 +0200

    more work on the status story

--- app/javascript/mastodon/components/status/status.stories.tsx
+++ app/javascript/mastodon/components/status/status.stories.tsx
@@ -16,31 +16,53 @@ import {
 
 import { TypedStatus } from './types';
 
+type ContextTypes =
+  | 'account'
+  | 'bookmarks'
+  | 'detailed'
+  | 'favourites'
+  | 'home'
+  | 'notifications'
+  | 'public'
+  | 'search'
+  | 'thread';
+
+type AttachmentTypes = 'image-1' | 'image-2' | 'image-3' | 'video' | 'audio';
+
 interface StatusStoryProps {
+  // Contents
+  text: string;
   visibility: StatusVisibility;
-  isFavourited?: boolean;
-  isReblogged?: boolean;
   isReply?: boolean;
   isQuote?: boolean;
   isPoll?: boolean;
+  attachments?: AttachmentTypes;
 
-  attachments?: 'image-1' | 'image-2' | 'image-3' | 'video';
+  // Interactions
+  hasFavourited?: boolean;
+  hasReblogged?: boolean;
+  hasVoted?: boolean;
   favouriteCount?: number;
   reblogCount?: number;
   replyCount?: number;
 
+  // Display
   hasNextReply?: boolean;
+  contextType?: ContextTypes;
   disableActions?: boolean;
 }
 
 const StatusStoryComponent: FC<StatusStoryProps> = ({
+  text,
   visibility,
   isReply,
-  isReblogged,
-  isFavourited,
+  hasReblogged: isReblogged,
+  hasFavourited: isFavourited,
   isQuote,
   isPoll,
+  hasVoted,
   hasNextReply,
+  contextType,
   favouriteCount = 0,
   replyCount = 0,
   reblogCount = 0,
@@ -51,6 +73,7 @@ const StatusStoryComponent: FC<StatusStoryProps> = ({
     return {
       account,
       status: statusFactoryState({
+        text,
         reblogged: isReblogged,
         favourited: isFavourited,
         visibility,
@@ -68,12 +91,13 @@ const StatusStoryComponent: FC<StatusStoryProps> = ({
       }).withMutations((status) => {
         status.set('account', account);
         if (isPoll) {
-          status.set('poll', '1');
+          status.set('poll', hasVoted ? '2' : '1');
         }
       }),
     };
   }, [
     favouriteCount,
+    hasVoted,
     isFavourited,
     isPoll,
     isQuote,
@@ -81,6 +105,7 @@ const StatusStoryComponent: FC<StatusStoryProps> = ({
     isReply,
     reblogCount,
     replyCount,
+    text,
     visibility,
   ]);
 
@@ -94,37 +119,64 @@ const StatusStoryComponent: FC<StatusStoryProps> = ({
       nextInReplyToId={hasNextReply ? '1' : undefined}
       showActions={!disableActions}
       showThread={isReply}
+      contextType={contextType}
     />
   );
 };
 
-const staticProps = {
-  onReply: fn(),
-  onFavourite: fn(),
-  onMention: fn(),
-  onOpenMedia: fn(),
-  onOpenVideo: fn(),
-  onQuote: fn(),
-  onReblog: fn(),
-  onToggleCollapsed: fn(),
-  onToggleHidden: fn(),
-  onTranslate: fn(),
-  onAddFilter: fn(),
-  onBlock: fn(),
-  onClick: fn(),
-  onDelete: fn(),
-  onDirect: fn(),
-  onEmbed: fn(),
-  onHeightChange: fn(),
-  onInteractionModal: fn(),
-  onPin: fn(),
-  deployPictureInPicture: fn(),
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  pictureInPicture: ImmutableMap<'inUse' | 'available', boolean>({
-    inUse: false,
-    available: true,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Casting to solves infinite recursion errors.
-  }) as any,
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const staticProps = Object.fromEntries(
+  // As Storybook auto-names from args only,
+  // we need to manually name these for proper action tracking.
+  Object.entries({
+    onReply: fn(),
+    onFavourite: fn(),
+    onMention: fn(),
+    onOpenMedia: fn(),
+    onOpenVideo: fn(),
+    onQuote: fn(),
+    onReblog: fn(),
+    onToggleCollapsed: fn(),
+    onToggleHidden: fn(),
+    onTranslate: fn(),
+    onAddFilter: fn(),
+    onBlock: fn(),
+    onClick: fn(),
+    onDelete: fn(),
+    onDirect: fn(),
+    onEmbed: fn(),
+    onHeightChange: fn(),
+    onInteractionModal: fn(),
+    onPin: fn(),
+    deployPictureInPicture: fn(),
+  } as const)
+    .map(([key, value]) => [key, value.mockName(key)])
+    .concat([
+      [
+        'pictureInPicture',
+        ImmutableMap<'inUse' | 'available', boolean>({
+          inUse: false,
+          available: true,
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Casting to solves infinite recursion errors.
+        }) as any,
+      ],
+    ]),
+);
+
+const categoryContents = {
+  table: {
+    category: 'contents',
+  },
+} as const;
+const categoryInteraction = {
+  table: {
+    category: 'interactions',
+  },
+} as const;
+const displayInteraction = {
+  table: {
+    category: 'display',
+  },
 } as const;
 
 const meta = {
@@ -132,9 +184,7 @@ const meta = {
   component: StatusStoryComponent,
   argTypes: {
     visibility: {
-      text: {
-        control: 'text',
-      },
+      ...categoryContents,
       control: 'inline-radio',
       options: [
         'direct',
@@ -143,14 +193,50 @@ const meta = {
         'unlisted',
       ] satisfies StatusVisibility[],
     },
+    isPoll: categoryContents,
+    isQuote: categoryContents,
+    isReply: categoryContents,
+    text: categoryContents,
+    contextType: {
+      ...displayInteraction,
+      control: 'select',
+      options: [
+        'account',
+        'bookmarks',
+        'detailed',
+        'favourites',
+        'home',
+        'notifications',
+        'public',
+        'search',
+        'thread',
+      ] satisfies ContextTypes[],
+    },
+    hasFavourited: categoryInteraction,
+    hasReblogged: categoryInteraction,
+    hasVoted: {
+      ...categoryInteraction,
+      if: {
+        arg: 'isPoll',
+        truthy: true,
+      },
+    },
+    favouriteCount: categoryInteraction,
+    reblogCount: categoryInteraction,
+    replyCount: categoryInteraction,
+    disableActions: displayInteraction,
+    hasNextReply: displayInteraction,
   },
   args: {
+    text: 'This is a status',
     visibility: 'public',
+    contextType: 'home',
     isQuote: false,
     isReply: false,
-    isFavourited: false,
+    hasFavourited: false,
     isPoll: false,
-    isReblogged: false,
+    hasVoted: false,
+    hasReblogged: false,
     hasNextReply: false,
     favouriteCount: 0,
     reblogCount: 0,
@@ -161,6 +247,12 @@ const meta = {
     state: {
       polls: {
         '1': pollFactory(),
+        '2': pollFactory({
+          voted: true,
+          voters_count: 1,
+          votes_count: 1,
+          own_votes: [0],
+        }),
       },
     },
   },
@@ -175,5 +267,22 @@ export const Default: Story = {};
 export const Reply: Story = {
   args: {
     isReply: true,
+  },
+};
+
+export const LongText: Story = {
+  args: {
+    text: [
+      'This is a long-form piece of text that wraps multiple lines.',
+      'It is here to test what a longer status looks like.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    ].join('\n'),
+  },
+};
+
+export const Poll: Story = {
+  args: {
+    isPoll: true,
   },
 };
