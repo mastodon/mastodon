467e2757818f0b5d58ff567cd034782caf301953
Pia B <pia.bregulla@web.de>
2026-06-18 16:59:38 +0200

    fix filteraction, cleanup spec

--- db/migrate/20260616091451_add_action_logs_data_changes.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddActionLogsDataChanges < ActiveRecord::Migration[8.1]
-  def change
-    add_column :admin_action_logs, :tag_changes, :jsonb, null: true, default: nil
-  end
-end
--- spec/requests/admin/tags_spec.rb
+++ spec/requests/admin/tags_spec.rb
@@ -19,25 +19,13 @@ RSpec.describe 'Admin Tags' do
   describe 'update tag' do
     let(:tag) { Fabricate :tag, name: '#supertag' }
 
-    it 'redirects to tag page and saves update log action for tag' do
+    it 'redirects to tag page and saves update log action for all attribute statuses' do
       put admin_tag_path(tag.id, params: { tag: { trendable: true, listable: false } })
 
       expect(response).to have_http_status(302)
       expect(Admin::ActionLog.last.human_identifier).to eq('#supertag')
       expect(Admin::ActionLog.pluck(:action)).to eq(%w(update))
       expect(Admin::ActionLog.pluck(:tag_changes)).to eq([{ 'listable' => false, 'trendable' => true }])
-    end
-
-    it 'saves update log action for every true attribute' do
-      put admin_tag_path(tag.id, params: { tag: { trendable: true, listable: true, usable: true } })
-
-      expect(Admin::ActionLog.pluck(:tag_changes)).to eq([{ 'listable' => true, 'trendable' => true, 'usable' => true }])
-    end
-
-    it 'saves update log action for every false attribute' do
-      put admin_tag_path(tag.id, params: { tag: { trendable: false, listable: false, usable: false } })
-
-      expect(Admin::ActionLog.pluck(:tag_changes)).to eq([{ 'listable' => false, 'trendable' => false, 'usable' => false }])
     end
   end
 end
