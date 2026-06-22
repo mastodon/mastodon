c999fad600e7d0f4b8269b08bc8ff823672a2e82
Claire <claire.github-309c@sitedethib.com>
2026-06-22 08:00:53 +0200

    Fix JSON canonicalization of floating points

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ require_relative '../lib/active_record/database_tasks_extensions'
 require_relative '../lib/active_record/batches'
 require_relative '../lib/simple_navigation/item_extensions'
 require_relative '../lib/vite_ruby/sri_extensions'
+require_relative '../lib/json-canonicalization/floats_fix'
 
 Bundler.require(:pam_authentication) if ENV['PAM_ENABLED'] == 'true'
 
