35233550d16c2fbd163f00066f367e7e954f6da3
David Roetzel <david@roetzel.de>
2026-06-22 10:50:33 +0200

    Small adjustments
    Use `find_each` and a single boolean to account for cases where
    a large number of collections are affected.
    
    Use `substring(... from ...)` syntax for compatibility with older
    postgresql versions.

--- app/workers/scheduler/repair_remote_collections_scheduler.rb
+++ app/workers/scheduler/repair_remote_collections_scheduler.rb
@@ -13,21 +13,27 @@ class Scheduler::RepairRemoteCollectionsScheduler
     last_known_good = redis.get('remote_collection_repair:last_known_good')
 
     affected_collections = Collection.joins(:account).where(local: false)
-      .where("regexp_substr(collections.uri, '/ap/users/\\d+/') != regexp_substr(accounts.collections_url, '/ap/users/\\d+/')")
+      .where("substring(collections.uri from '/ap/users/\\d+/') != substring(accounts.collections_url from '/ap/users/\\d+/')")
     affected_collections = affected_collections.where(id: last_known_good...) if last_known_good
 
-    successful = affected_collections.map do |collection|
+    successful = true
+    affected_collections.find_each do |collection|
       json = fetch_resource(collection.uri, true)
-      next false if json.nil? || !json.is_a?(Hash) || !json.key?('attributedTo')
+      if json.nil? || !json.is_a?(Hash) || !json.key?('attributedTo')
+        successful = false
+        next
+      end
 
       account = Account.find_by(uri: json['attributedTo'])
       account ||= ActivityPub::FetchRemoteAccountService.new.call(json['attributedTo'])
-      next false if account.nil?
+      if account.nil?
+        successful = false
+        next
+      end
 
       collection.update(account:)
-      true
     end
 
-    redis.set('remote_collection_repair:last_known_good', max_id.to_s) if successful.all?
+    redis.set('remote_collection_repair:last_known_good', max_id.to_s) if successful
   end
 end
