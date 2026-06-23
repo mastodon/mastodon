1520162a8c03a8ef9c7ba3eef65370bb2c425864
Jonas Bechtel <see jbechtel.de/site/meta/impressum>
2026-06-23 22:33:25 +0200

    Replace Array.at() access with [] operator to support Qt5WebEngine

--- app/javascript/mastodon/features/emoji/utils.ts
+++ app/javascript/mastodon/features/emoji/utils.ts
@@ -61,7 +61,7 @@ export function emojiToUnicodeHex(emoji: string): string {
   // Handles how Emojibase removes the variation selector for single code emojis.
   // See: https://emojibase.dev/docs/spec/#merged-variation-selectors
   if (
-    codes.at(1) === VARIATION_SELECTOR_CODE.toString(16).toUpperCase() &&
+    codes[1] === VARIATION_SELECTOR_CODE.toString(16).toUpperCase() &&
     codes.length === 2
   ) {
     codes.pop();
