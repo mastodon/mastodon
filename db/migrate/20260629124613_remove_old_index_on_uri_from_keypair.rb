3aa3b75da9d775052ce30f92223568b9948fb739
Claire <claire.github-309c@sitedethib.com>
2026-06-30 10:04:35 +0200

    Fix missing unique index on keypairs URI (#39668)

--- db/migrate/20260629124613_remove_old_index_on_uri_from_keypair.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveOldIndexOnUriFromKeypair < ActiveRecord::Migration[8.1]
-  disable_ddl_transaction!
-
-  def change
-    remove_index :keypairs, :uri, name: :index_keypairs_on_uri, unique: true, algorithm: :concurrently
-  end
-end
