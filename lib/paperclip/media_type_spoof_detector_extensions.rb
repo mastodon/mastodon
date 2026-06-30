bddd7e686ea00e92447a2cfd8b4bbcf359560c38
Pia B <pia.bregulla@web.de>
2026-06-30 16:04:08 +0200

    use public method to determine content type

--- lib/paperclip/media_type_spoof_detector_extensions.rb
+++ lib/paperclip/media_type_spoof_detector_extensions.rb
@@ -7,16 +7,9 @@ module Paperclip
     def calculated_content_type
       return @calculated_content_type if defined?(@calculated_content_type)
 
-      @calculated_content_type = type_from_file_command.chomp
+      detector = Paperclip::ContentTypeDetector.new(@file.path)
 
-      # The `file` command fails to recognize some MP3 files as such
-      @calculated_content_type = type_from_marcel if @calculated_content_type == 'application/octet-stream' && type_from_marcel.in?(MARCEL_MIME_TYPES)
-      @calculated_content_type
-    end
-
-    def type_from_marcel
-      @type_from_marcel ||= Marcel::MimeType.for Pathname.new(@file.path),
-                                                 name: @file.path
+      @calculated_content_type = detector.detect
     end
   end
 end
