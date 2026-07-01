cd97f4c6c717241bffcd3c6a6fd025102d90acfe
Pia B. <pia.bregulla@web.de>
2026-07-01 11:17:18 +0200

    Add Json schema validation for action logs (#39670)

--- spec/requests/admin/tags_spec.rb
+++ spec/requests/admin/tags_spec.rb
@@ -20,12 +20,18 @@ RSpec.describe 'Admin Tags' do
     let(:tag) { Fabricate :tag, name: '#supertag' }
 
     it 'redirects to tag page and saves update log action for all attribute statuses' do
-      put admin_tag_path(tag.id, params: { tag: { trendable: true, listable: false } })
+      put admin_tag_path(tag.id, params: { tag: { trendable: true, listable: false, unallowed: true } })
 
       expect(response).to have_http_status(302)
-      expect(Admin::ActionLog.last.human_identifier).to eq('#supertag')
-      expect(Admin::ActionLog.pluck(:action)).to eq(%w(update))
       expect(Admin::ActionLog.pluck(:recorded_changes)).to eq([{ 'listable' => false, 'trendable' => true }])
+      expect(Admin::ActionLog.last.recorded_changes).to match_json_schema('tags_format_1.0')
+    end
+
+    it 'returns invalid when the schema does not match' do
+      put admin_tag_path(tag.id, params: { tag: { trendable: true } })
+
+      Admin::ActionLog.last.update(recorded_changes: { 'unallowed' => false })
+      expect(Admin::ActionLog.last.recorded_changes).to_not match_json_schema('tags_format_1.0')
     end
   end
 end
