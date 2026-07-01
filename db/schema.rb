d181a52a3b0790649dc36e54e4845af3bad48e5d
Claire <claire.github-309c@sitedethib.com>
2026-07-01 18:21:10 +0200

    Drop `local_fragment IS NOT NULL` from `index_keypairs_on_account_id_and_local_fragment`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_30_070600) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_01_161826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -712,7 +712,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_30_070600) do
     t.integer "type", null: false
     t.datetime "updated_at", null: false
     t.string "uri"
-    t.index ["account_id", "local_fragment"], name: "index_keypairs_on_account_id_and_local_fragment", unique: true, where: "(local_fragment IS NOT NULL)"
+    t.index ["account_id", "local_fragment"], name: "index_keypairs_on_account_id_and_local_fragment", unique: true
     t.index ["account_id"], name: "index_keypairs_on_account_id"
     t.index ["uri"], name: "index_keypairs_on_uri", unique: true
   end
