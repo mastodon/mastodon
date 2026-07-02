e29414cd2d70be25757ca071e2a876727b83bbb2
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-07-02 18:15:27 +0200

    make simpler interactions selector, and add conditions from selectStatusState

--- app/javascript/mastodon/selectors/statuses.ts
+++ app/javascript/mastodon/selectors/statuses.ts
@@ -7,7 +7,7 @@ import type {
 } from '@/mastodon/models/status';
 import { createAppSelector } from '@/mastodon/store/typed_functions';
 
-import { selectPlainAccount } from './accounts';
+import { selectIsAccountLocal, selectPlainAccount } from './accounts';
 import type { FilterShape } from './filters';
 import { getFilters } from './filters';
 
@@ -83,16 +83,42 @@ export const selectStatusConditions = createAppSelector(
         state.statuses.getIn([statusId, 'quote', 'quoted_status']),
         'account',
       ]) as string | undefined | null,
+    (state, statusId: string) =>
+      selectIsAccountLocal(
+        state,
+        state.statuses.getIn([statusId, 'account']) as string,
+      ),
   ],
-  (status, currentAccountId, quotedAccountId) => ({
-    isPublic: status && ['public', 'unlisted'].includes(status.visibility),
-    isLoggedIn: !!currentAccountId,
-    isMine: status && !!currentAccountId && status.account === currentAccountId,
-    isQuoted:
-      status && !!currentAccountId && quotedAccountId === currentAccountId,
-    isNotDirect: status && status.visibility !== 'direct',
-  }),
+  (status, userId, quotedAccountId, isLocal) => {
+    const isPublic =
+      !!status && ['public', 'unlisted'].includes(status.visibility);
+    const isMine = status?.account === userId;
+    const isMineAndPrivate =
+      userId === status?.account && status.visibility === 'private';
+    const quoteApproval = status?.quote_approval?.current_user;
+    return {
+      isPublic,
+      isLocal,
+      isLoggedIn: !!userId,
+      isMine,
+      isNotMine: !isMine,
+      isQuoted: !!status && !!userId && quotedAccountId === userId,
+      isNotDirect: !!status && status.visibility !== 'direct',
+      isPrivateReblog:
+        userId === status?.account && status.visibility === 'private',
+      isReblogged: status?.reblogged ?? false,
+      isReblogAllowed: isPublic || isMineAndPrivate,
+      isQuoteAutomaticallyAccepted:
+        quoteApproval === 'automatic' && (isPublic || isMineAndPrivate),
+      isQuoteManuallyAccepted:
+        quoteApproval === 'manual' && (isPublic || isMineAndPrivate),
+      isQuoteFollowersOnly:
+        status?.quote_approval?.automatic[0] === 'followers' ||
+        status?.quote_approval?.manual[0] === 'followers',
+    };
+  },
 );
+export type StatusConditions = ReturnType<typeof selectStatusConditions>;
 
 export const selectStatusInteractions = createAppSelector(
   [(_, statusId: string) => statusId, selectStatusConditions],
@@ -104,8 +130,15 @@ export const selectStatusInteractions = createAppSelector(
       };
     }
 
-    const { isLoggedIn, isMine, isPublic, isQuoted, isNotDirect } =
-      conditionals;
+    const {
+      isLoggedIn,
+      isMine,
+      isNotMine,
+      isPublic,
+      isLocal,
+      isQuoted,
+      isNotDirect,
+    } = conditionals;
 
     const interactions: Record<
       StatusInteractionIntent,
@@ -115,17 +148,17 @@ export const selectStatusInteractions = createAppSelector(
       delete: addAllowed({ isMine }),
       edit: addAllowed({ isMine }),
       editQuotePolicy: addAllowed({ isMine, isPublic }),
-      embed: addAllowed({ isPublic }),
+      embed: addAllowed({ isPublic, isLocal }),
       favourite: addAllowed({ isLoggedIn }),
-      filter: addAllowed({ isLoggedIn }),
+      filter: addAllowed({ isLoggedIn, isNotMine }),
       mute: addAllowed({ isMine }),
       pin: addAllowed({ isMine, isNotDirect }),
       quote: addAllowed({ isLoggedIn }),
       reblog: addAllowed({ isLoggedIn }),
       redraft: addAllowed({ isMine }),
       reply: addAllowed({ isLoggedIn }),
-      report: addAllowed({ isLoggedIn }),
-      revokeQuote: addAllowed({ isQuoted }),
+      report: addAllowed({ isLoggedIn, isNotMine }),
+      revokeQuote: addAllowed({ isQuoted, isNotMine }),
     };
 
     return {
@@ -134,6 +167,20 @@ export const selectStatusInteractions = createAppSelector(
     };
   },
 );
+export type StatusInteractions = ReturnType<typeof selectStatusInteractions>;
+
+export const selectStatusInteractionsAllowed = createAppSelector(
+  [selectStatusInteractions],
+  ({ statusId, ...interactions }) => ({
+    statusId,
+    ...(Object.fromEntries(
+      Object.entries(interactions).map(([key, { allowed }]) => [key, allowed]),
+    ) as Record<StatusInteractionIntent, boolean>),
+  }),
+);
+export type StatusInteractionsAllowed = ReturnType<
+  typeof selectStatusInteractionsAllowed
+>;
 
 export const selectStatusIntentAllowed = createAppSelector(
   [
