ee02364e4bb0e4f287d283f984513ad231290f5b
Claire <claire.github-309c@sitedethib.com>
2026-07-06 13:43:54 +0200

    Change FEP-521 processing to happen in addition of old security context instead of replacing it (#39725)

--- app/services/activitypub/process_account_service.rb
+++ app/services/activitypub/process_account_service.rb
@@ -271,7 +271,7 @@ class ActivityPub::ProcessAccountService < BaseService
   end
 
   def public_keys
-    @public_keys ||= fep_521a_public_keys.presence || legacy_public_keys
+    @public_keys ||= (fep_521a_public_keys + legacy_public_keys).uniq { |key| key[:uri] }
   end
 
   def legacy_public_keys
@@ -305,8 +305,6 @@ class ActivityPub::ProcessAccountService < BaseService
   end
 
   def fep_521a_public_keys
-    return if @json['assertionMethod'].blank?
-
     as_array(@json['assertionMethod']).take(MAX_PUBLIC_KEYS).filter_map do |value|
       next if value.nil?
 
