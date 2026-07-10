03055a45bb168fd2bf2e0cf9b5d976324ca55528
Claire <claire.github-309c@sitedethib.com>
2026-07-10 10:50:42 +0200

    Fix incorrect error handling

--- app/services/activitypub/fetch_remote_actor_service.rb
+++ app/services/activitypub/fetch_remote_actor_service.rb
@@ -36,7 +36,7 @@ class ActivityPub::FetchRemoteActorService < BaseService
     @username, @domain = split_acct(@json['webfinger']) if @json['webfinger'].present? && @json['webfinger'].is_a?(String)
 
     if @username.blank? || @domain.blank?
-      raise "Actor #{uri} has no `preferredUsername`, and either a bogus or missing `webfinger`, which is a requirement for Mastodon compatibility" if @json['preferredUsername'].blank?
+      raise Error, "Actor #{uri} has no `preferredUsername`, and either a bogus or missing `webfinger`, which is a requirement for Mastodon compatibility" if @json['preferredUsername'].blank?
 
       Rails.logger.debug { "Actor #{uri} has an invalid `webfinger` value, falling back to `preferredUsername`" } if @json['webfinger'].present?
       @username = @json['preferredUsername']
