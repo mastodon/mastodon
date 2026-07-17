53c3508ea0e83cd687bea44a7d9ac806a583c23e
Claire <claire.github-309c@sitedethib.com>
2026-07-17 16:11:37 +0200

    Fix `account` and `keypair` fabricators not working outside of rspec (#39866)

--- spec/fabricators/account_fabricator.rb
+++ spec/fabricators/account_fabricator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../support/signing_keys_helpers'
+
 Fabricator(:account) do
   transient :suspended, :silenced, :legacy_keypair
   username            { sequence(:username) { |i| "#{Faker::Internet.user_name(separators: %w(_))}#{i}" } }
