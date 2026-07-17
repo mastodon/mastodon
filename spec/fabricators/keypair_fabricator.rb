85e0d336a6ca2c75555f035c05dc97c40dce43ff
Claire <claire.github-309c@sitedethib.com>
2026-07-17 15:49:43 +0200

    Fix `keypair` Fabricator not working outside of rspec

--- spec/fabricators/keypair_fabricator.rb
+++ spec/fabricators/keypair_fabricator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../support/signing_keys_helpers'
+
 Fabricator(:keypair) do
   account     { Fabricate(:account, keypairs: []) }
   type        :rsa
