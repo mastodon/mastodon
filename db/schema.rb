5e28d67994b0a9561674a0c86d1afa948f120154
Claire <claire.github-309c@sitedethib.com>
2026-07-20 13:57:11 +0200

    Add post-deployment migration to update blank account URIs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_20_092724) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_20_100326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
