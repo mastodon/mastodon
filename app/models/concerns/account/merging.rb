34436dd8b5a99d60ffffb7978735be8e3e9cd8ad
Claire <claire.github-309c@sitedethib.com>
2026-07-20 16:19:05 +0200

    Refactor classes map out of method

--- app/models/concerns/account/merging.rb
+++ app/models/concerns/account/merging.rb
@@ -1,7 +1,29 @@
 # frozen_string_literal: true
 
+# TODO: remove some time after 4.7.0
 module Account::Merging
   extend ActiveSupport::Concern
+
+  ACCOUNT_MERGING_CLASSES = {
+    account_id: [
+      Status, StatusPin, MediaAttachment, Poll, Report, Tombstone, Favourite,
+      Follow, FollowRequest, Block, Mute,
+      AccountModerationNote, AccountPin, AccountStat, ListAccount,
+      PollVote, Mention, AccountDeletionRequest, AccountNote, FollowRecommendationSuppression,
+      Appeal, TagFollow, Quote, Collection, CollectionItem
+    ],
+    from_account_id: [
+      Notification, NotificationPermission, NotificationRequest
+    ],
+    target_account_id: [
+      Follow, FollowRequest, Block, Mute, AccountModerationNote, AccountPin, AccountNote
+    ],
+    reference_account_id: [CanonicalEmailBlock],
+    account_warning_id: [Appeal],
+    local_account_id: [SeveredRelationship],
+    remote_account_id: [SeveredRelationship],
+    quoted_account_id: [Quote],
+  }.freeze
 
   def merge_with!(other_account)
     # Since it's the same remote resource, the remote resource likely
@@ -11,26 +33,7 @@ module Account::Merging
     # account already, therefore mass update will not work and we need
     # to check for (and skip past) uniqueness errors
 
-    {
-      account_id: [
-        Status, StatusPin, MediaAttachment, Poll, Report, Tombstone, Favourite,
-        Follow, FollowRequest, Block, Mute,
-        AccountModerationNote, AccountPin, AccountStat, ListAccount,
-        PollVote, Mention, AccountDeletionRequest, AccountNote, FollowRecommendationSuppression,
-        Appeal, TagFollow, Quote, Collection, CollectionItem
-      ],
-      from_account_id: [
-        Notification, NotificationPermission, NotificationRequest
-      ],
-      target_account_id: [
-        Follow, FollowRequest, Block, Mute, AccountModerationNote, AccountPin, AccountNote
-      ],
-      reference_account_id: [CanonicalEmailBlock],
-      account_warning_id: [Appeal],
-      local_account_id: [SeveredRelationship],
-      remote_account_id: [SeveredRelationship],
-      quoted_account_id: [Quote],
-    }.each do |attribute, classes|
+    ACCOUNT_MERGING_CLASSES.each do |attribute, classes|
       classes.each do |klass|
         klass.where({ attribute => other_account.id }).reorder(nil).find_each do |record|
           record.update_attribute(attribute, id)
