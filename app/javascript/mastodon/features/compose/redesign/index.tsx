70c89d551e4cbcedce69e9a43b7a96a288483e90
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-07-22 15:02:44 +0200

    add handlers

--- app/javascript/mastodon/features/compose/redesign/index.tsx
+++ app/javascript/mastodon/features/compose/redesign/index.tsx
@@ -1,8 +1,21 @@
+import type React from 'react';
 import { useCallback, useId, useRef } from 'react';
 
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 
-import { changeComposeSpoilerness } from '@/mastodon/actions/compose';
+import type { TextareaAutosizeProps } from 'react-textarea-autosize';
+
+import {
+  changeCompose,
+  changeComposeSpoilerness,
+  clearComposeSuggestions,
+  fetchComposeSuggestions,
+  selectComposeSuggestion,
+} from '@/mastodon/actions/compose';
+import {
+  processPasteOrDrop,
+  submitCompose,
+} from '@/mastodon/actions/compose_typed';
 import AutosuggestTextarea from '@/mastodon/components/autosuggest_textarea';
 import { ToggleField } from '@/mastodon/components/form_fields';
 import { IconButton } from '@/mastodon/components/icon_button';
@@ -10,7 +23,7 @@ import { useAppDispatch, useAppSelector } from '@/mastodon/store';
 import TranslateIcon from '@/material-icons/400-24px/translate.svg?react';
 
 import { ComposeFormHeader } from './header';
-import { selectComposeType } from './selectors';
+import { selectComposeState, selectComposeType } from './selectors';
 import classes from './styles.module.scss';
 import { ComposeVisibility } from './visibility';
 
@@ -25,16 +38,21 @@ const messages = defineMessages({
   },
 });
 
-export const RedesignComposeForm: React.FC = () => {
+interface RedesignComposeFormProps {
+  autoFocus?: boolean;
+  redirectOnSuccess?: boolean;
+}
+
+export const RedesignComposeForm: React.FC<RedesignComposeFormProps> = ({
+  autoFocus,
+  redirectOnSuccess,
+}) => {
   const type = useAppSelector(selectComposeType);
-  const sensitive = useAppSelector((state) => !!state.compose.get('spoiler'));
+  const { sensitive, suggestions, text, lang, isSubmitting } =
+    useAppSelector(selectComposeState);
 
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  const dispatch = useAppDispatch();
-  const handleSensitiveChange = useCallback(() => {
-    dispatch(changeComposeSpoilerness());
-  }, [dispatch]);
+  const { ref, onSensitiveChange, ...handlers } =
+    useHandlers(redirectOnSuccess);
 
   const intl = useIntl();
   const titleId = useId();
@@ -54,7 +72,7 @@ export const RedesignComposeForm: React.FC = () => {
         <ToggleField
           label={intl.formatMessage(messages.sensitive)}
           checked={sensitive}
-          onChange={handleSensitiveChange}
+          onChange={onSensitiveChange}
         />
         <IconButton
           icon='language'
@@ -62,7 +80,132 @@ export const RedesignComposeForm: React.FC = () => {
           title={intl.formatMessage(messages.changeLanguage)}
         />
       </div>
-      <AutosuggestTextarea ref={textareaRef} />
+      <ComposeTextarea
+        ref={ref}
+        value={text}
+        // eslint-disable-next-line jsx-a11y/no-autofocus
+        autoFocus={autoFocus}
+        lang={lang}
+        // placeholder={intl.formatMessage(messages.placeholder)}
+        disabled={isSubmitting}
+        suggestions={suggestions}
+        {...handlers}
+      />
     </div>
   );
 };
+
+type SuggestSelectedHandler = (
+  position: number,
+  token: string,
+  suggestion: unknown,
+) => void;
+
+const ComposeTextarea = AutosuggestTextarea as React.ForwardRefExoticComponent<
+  {
+    suggestions: Immutable.List<unknown>;
+    onSuggestionSelected: SuggestSelectedHandler;
+    onSuggestionsClearRequested: () => void;
+    onSuggestionsFetchRequested: (token: string) => void;
+  } & TextareaAutosizeProps &
+    React.RefAttributes<HTMLTextAreaElement>
+>;
+
+function useHandlers(redirectOnSuccess?: boolean) {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  const dispatch = useAppDispatch();
+  const onChange: React.ChangeEventHandler<HTMLTextAreaElement> = useCallback(
+    (event) => {
+      dispatch(changeCompose(event.target.value));
+    },
+    [dispatch],
+  );
+  const onSensitiveChange = useCallback(() => {
+    dispatch(changeComposeSpoilerness());
+  }, [dispatch]);
+  const handleSubmit = useCallback(
+    (event?: React.SubmitEvent) => {
+      dispatch(
+        submitCompose({
+          textareaValue: ref.current?.value,
+          redirectOnSuccess,
+        }),
+      );
+
+      if (event) {
+        event.preventDefault();
+      }
+    },
+    [dispatch, redirectOnSuccess],
+  );
+  const onKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> =
+    useCallback(
+      (event) => {
+        if (
+          event.key.toLowerCase() === 'enter' &&
+          (event.ctrlKey || event.metaKey)
+        ) {
+          handleSubmit();
+          event.preventDefault();
+        }
+        blurOnEscape(event);
+      },
+      [handleSubmit],
+    );
+  const onPaste: React.ClipboardEventHandler = useCallback(
+    (event) => {
+      if (event.clipboardData.files.length === 1) {
+        event.preventDefault();
+      }
+      dispatch(processPasteOrDrop(event.clipboardData));
+    },
+    [dispatch],
+  );
+  const onDrop: React.DragEventHandler = useCallback(
+    (event) => {
+      if (event.dataTransfer.files.length === 1) {
+        event.preventDefault();
+      }
+      dispatch(processPasteOrDrop(event.dataTransfer));
+    },
+    [dispatch],
+  );
+
+  const onSuggestionsFetchRequested = useCallback(
+    (token: string) => {
+      dispatch(fetchComposeSuggestions(token));
+    },
+    [dispatch],
+  );
+  const onSuggestionsClearRequested = useCallback(() => {
+    dispatch(clearComposeSuggestions());
+  }, [dispatch]);
+  const onSuggestionSelected: SuggestSelectedHandler = useCallback(
+    (position, token, suggestion) => {
+      dispatch(selectComposeSuggestion(position, token, suggestion));
+    },
+    [dispatch],
+  );
+
+  return {
+    ref,
+    onKeyDown,
+    onChange,
+    onPaste,
+    onDrop,
+    onSensitiveChange,
+    onSuggestionsFetchRequested,
+    onSuggestionsClearRequested,
+    onSuggestionSelected,
+  };
+}
+
+function blurOnEscape(event: React.KeyboardEvent<HTMLTextAreaElement>) {
+  if (
+    ['esc', 'escape'].includes(event.key.toLowerCase()) &&
+    event.target instanceof HTMLTextAreaElement
+  ) {
+    event.target.blur();
+  }
+}
