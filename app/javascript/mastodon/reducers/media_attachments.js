d7f1967ba6f4d313f893996cbc8d0cfb92280d11
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-07-28 17:52:37 +0200

    set correct type for media attachment reducer

--- app/javascript/mastodon/reducers/media_attachments.js
+++ app/javascript/mastodon/reducers/media_attachments.js
@@ -1,9 +1,9 @@
-import { Map as ImmutableMap } from 'immutable';
+import { Map as ImmutableMap, List as ImmutableList } from 'immutable';
 
 import { STORE_HYDRATE } from '../actions/store';
 
 const initialState = ImmutableMap({
-  accept_content_types: [],
+  accept_content_types: ImmutableList(),
 });
 
 export default function meta(state = initialState, action) {
