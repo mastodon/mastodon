2ff3509f1036fa4b5c490cfc7137dbc03270a5b9
Shlee <github@shl.ee>
2026-07-29 11:31:45 +0930

    Update attachment_batch.rb

--- app/lib/attachment_batch.rb
+++ app/lib/attachment_batch.rb
@@ -108,26 +108,26 @@ class AttachmentBatch
     # objects can be processed at once, so we have to potentially
     # separate them into multiple calls.
 
-    retries = 0
     keys.each_slice(LIMIT) do |keys_slice|
       logger.debug { "Deleting #{keys_slice.size} objects" }
-
-      with_overridden_timeout(bucket.client, 120) do
-        bucket.delete_objects(delete: {
-          objects: keys_slice.map { |key| { key: key } },
-          quiet: true,
-        })
-      end
-    rescue => e
-      retries += 1
-
-      if retries < MAX_RETRY
-        logger.debug "Retry #{retries}/#{MAX_RETRY} after #{e.message}"
-        sleep 2**retries
-        retry
-      else
-        logger.error "Batch deletion from S3 failed after #{e.message}"
-        raise e
+      retries = 0  # Reset for each slice
+      begin
+        with_overridden_timeout(bucket.client, 120) do
+          bucket.delete_objects(delete: {
+            objects: keys_slice.map { |key| { key: key } },
+            quiet: true,
+          })
+        end
+      rescue => e
+        retries += 1
+        if retries < MAX_RETRY
+          logger.debug "Retry #{retries}/#{MAX_RETRY} after #{e.message}"
+          sleep 2**retries
+          retry
+        else
+          logger.error "Batch deletion from S3 failed after #{e.message}"
+          raise e
+        end
       end
     end
   end
