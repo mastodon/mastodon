c47bf15bd93ea857a4d98b0090a878d36593e1a7
Echo <ChaosExAnima@users.noreply.github.com>
2026-07-30 12:15:10 +0200

    Redesign dropdown (#39991)

---
+++ app/javascript/mastodon/components/dropdown/redesign.tsx
@@ -0,0 +1,125 @@
+import type React from 'react';
+
+import classNames from 'classnames';
+
+import type { Merge } from 'type-fest';
+
+import { Icon } from '../icon';
+import type { IconProp } from '../icon';
+import type { PopoverProps } from '../popover';
+import { Popover } from '../popover';
+
+import classes from './redesign.module.scss';
+
+export const menuItemClass = classes.menuItem;
+
+export type DropdownProps<As extends React.ElementType> = Merge<
+  {
+    as?: As;
+    children: React.ReactNode;
+    className?: string;
+    elevation?: 1 | 2;
+  },
+  React.ComponentProps<As>
+>;
+
+export const DropdownPopover = <As extends React.ElementType = 'div'>({
+  isOpen,
+  onClose,
+  reference,
+  popoverElement,
+  container,
+  placement,
+  offset = 4,
+  flip,
+  strategy,
+  matchReferenceWidth,
+  closeOnClickOutside,
+  children,
+  ...props
+}: DropdownProps<As> & Omit<PopoverProps, 'children'>) => {
+  const popoverProps = {
+    isOpen,
+    onClose,
+    reference,
+    popoverElement,
+    container,
+    placement,
+    offset,
+    flip,
+    strategy,
+    matchReferenceWidth,
+    closeOnClickOutside,
+  };
+  return (
+    <Popover {...popoverProps}>
+      {({ props: popoverChildProps }) => (
+        <Dropdown {...props} {...popoverChildProps}>
+          {children}
+        </Dropdown>
+      )}
+    </Popover>
+  );
+};
+
+export const Dropdown = <As extends React.ElementType = 'div'>({
+  as: asComp,
+  children,
+  className,
+  elevation = 1,
+  ...props
+}: DropdownProps<As>) => {
+  const Component = asComp ?? 'div';
+  return (
+    <Component
+      {...props}
+      className={classNames(className, classes.menu)}
+      data-elevation={elevation}
+    >
+      {children}
+    </Component>
+  );
+};
+
+type DropdownItemProps<As extends React.ElementType> = Merge<
+  { as?: As; children: React.ReactNode; className?: string; active?: boolean },
+  React.ComponentPropsWithoutRef<As>
+>;
+
+export const DropdownItem = <As extends React.ElementType = 'div'>({
+  active,
+  as: AsComp,
+  children,
+  className,
+  ...props
+}: DropdownItemProps<As>) => {
+  const Component = AsComp ?? 'div';
+  return (
+    <Component
+      {...props}
+      className={classNames(
+        className,
+        classes.menuItem,
+        active && classes.menuItemActive,
+      )}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export const DropdownItemButton: React.FC<
+  { icon?: IconProp } & React.ComponentProps<'button'>
+> = ({ icon, children, className, ...props }) => {
+  return (
+    <DropdownItem
+      type='button'
+      {...props}
+      as='button'
+      className={classNames(className, classes.menuItemButton)}
+    >
+      {icon && <Icon id='menu' icon={icon} />}
+      {children}
+    </DropdownItem>
+  );
+};
