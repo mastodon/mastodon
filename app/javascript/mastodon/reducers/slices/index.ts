ff04735ba81a21d117ad901b651274f5b32c2000
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-07-31 16:37:22 +0200

    switch to new composer slice

--- app/javascript/mastodon/reducers/slices/index.ts
+++ app/javascript/mastodon/reducers/slices/index.ts
@@ -1,11 +1,13 @@
 import { annualReport } from './annual_report';
 import { collections } from './collections';
+import { composer } from './composer';
 import { emojis } from './emojis';
 import { profileEdit } from './profile_edit';
 
 export const sliceReducers = {
   annualReport,
   collections,
+  composer,
   emojis,
   profileEdit,
 };
