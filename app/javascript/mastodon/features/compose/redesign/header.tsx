95e8236c71d622f7147c1160d93e3ef5818af5c5
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-07-31 16:14:00 +0200

    adds minimize to compose header

--- app/javascript/mastodon/features/compose/redesign/header.tsx
+++ app/javascript/mastodon/features/compose/redesign/header.tsx
@@ -1,9 +1,17 @@
+import { useCallback } from 'react';
+
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 
-import { XIcon } from '@phosphor-icons/react';
+import { ArrowsOutSimpleIcon, MinusIcon, XIcon } from '@phosphor-icons/react';
 
+import { resetCompose } from '@/mastodon/actions/compose';
+import { minimizeNewComposer } from '@/mastodon/actions/compose_typed';
 import { IconButton } from '@/mastodon/components/button/redesign';
-import { createAppSelector, useAppSelector } from '@/mastodon/store';
+import {
+  createAppSelector,
+  useAppDispatch,
+  useAppSelector,
+} from '@/mastodon/store';
 
 import { selectComposeType } from './selectors';
 import classes from './styles.module.scss';
@@ -42,14 +50,50 @@ const selectComposeFormTitle = createAppSelector(
   },
 );
 
-export const ComposeFormHeader: React.FC<{ id?: string }> = ({ id }) => {
+export const ComposeFormHeader: React.FC<{
+  id?: string;
+  noMinimize?: boolean;
+}> = ({ id, noMinimize }) => {
   const intl = useIntl();
   const titleMessage = useAppSelector(selectComposeFormTitle);
+  const isMinimized = useAppSelector(
+    (state) => !!state.compose.get('minimizeNewComposer'),
+  );
+
+  const dispatch = useAppDispatch();
+  const onClose = useCallback(() => {
+    dispatch(resetCompose());
+  }, [dispatch]);
+  const onMinimize = useCallback(() => {
+    dispatch(minimizeNewComposer());
+  }, [dispatch]);
 
   return (
     <header className={classes.header}>
       <h2 id={id}>{intl.formatMessage(titleMessage)}</h2>
-      <IconButton icon={XIcon} variant='ghost' size='sm'>
+
+      {!noMinimize && (
+        <IconButton
+          size='sm'
+          variant='ghost'
+          icon={isMinimized ? ArrowsOutSimpleIcon : MinusIcon}
+          onClick={onMinimize}
+        >
+          {isMinimized ? (
+            <FormattedMessage
+              id='compose.expand'
+              defaultMessage='Show composer'
+            />
+          ) : (
+            <FormattedMessage
+              id='compose.minimize'
+              defaultMessage='Minimize composer'
+            />
+          )}
+        </IconButton>
+      )}
+
+      <IconButton icon={XIcon} variant='ghost' size='sm' onClick={onClose}>
         <FormattedMessage id='lightbox.close' defaultMessage='Close' />
       </IconButton>
     </header>
