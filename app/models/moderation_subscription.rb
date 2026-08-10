62b1c00e2579b8bb955292b8f4e1988bd1198776
Claire <claire.github-309c@sitedethib.com>
2026-08-10 13:46:52 +0200

    Add `ModerationSubscription`, `SubscribedAdvisory`, and `ModerationSuggestion` models

---
+++ app/models/moderation_subscription.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: moderation_subscriptions
+#
+#  id                     :bigint(8)        not null, primary key
+#  apply_automatically    :boolean          default(FALSE), not null
+#  last_synced_at         :datetime
+#  list_action            :integer
+#  name                   :string
+#  override_unattributed  :boolean          default(FALSE), not null
+#  preserve_relationships :boolean          default(TRUE), not null
+#  priority               :integer
+#  retract_automatically  :boolean          default(TRUE), not null
+#  type                   :integer          not null
+#  url                    :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
+class ModerationSubscription < ApplicationRecord
+  self.inheritance_column = nil
+
+  PRIORITY_LIMIT = (2**31) - 1
+
+  has_many :advisories, class_name: 'SubscribedAdvisory', inverse_of: :moderation_subscription, dependent: :delete_all
+  has_many :suggestions, class_name: 'ModerationSuggestion', inverse_of: :moderation_subscription, dependent: :delete_all
+
+  validates :name, presence: true
+  validates :url, presence: true, url: true
+  validates :priority, presence: true, numericality: { in: (-PRIORITY_LIMIT..PRIORITY_LIMIT) }
+
+  enum :type, {
+    csv_list: 0,
+  }
+
+  enum :list_action, {
+    accept: 0,
+    reject: 1,
+  }, suffix: :action
+end
