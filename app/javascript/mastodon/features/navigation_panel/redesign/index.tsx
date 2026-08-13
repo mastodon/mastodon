a84eeff8c61dc3062868b58b699459870233c5d3
diondiondion <mail@diondiondion.com>
2026-08-13 16:08:12 +0200

    Add beginnings of new NavigationPanel to Storybook

---
+++ app/javascript/mastodon/features/navigation_panel/redesign/index.tsx
@@ -0,0 +1,93 @@
+import type React from 'react';
+import { useCallback } from 'react';
+
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+
+import {
+  PenNibIcon,
+  HouseIcon,
+  MagnifyingGlassIcon,
+  BellIcon,
+  BellRingingIcon,
+  ChatCircleIcon,
+  BookmarkSimpleIcon,
+} from '@phosphor-icons/react';
+
+import { useIdentity } from '@/mastodon/identity_context';
+import { openNewComposer } from '@/mastodon/reducers/slices/composer';
+import { selectUnreadNotificationGroupsCount } from '@/mastodon/selectors/notifications';
+import { useAppDispatch, useAppSelector } from '@/mastodon/store';
+
+import { NavigationHeader } from './header';
+import { NavigationLink } from './navigation_link';
+import classes from './styles.module.scss';
+
+const messages = defineMessages({
+  main: {
+    id: 'navigation_bar.main',
+    defaultMessage: 'Main',
+    description:
+      'Label for the main navigation; should not contain the word "navigation".',
+  },
+});
+
+export const RedesignNavigationPanel: React.FC<{ siteName?: string }> = ({
+  siteName,
+}) => {
+  const intl = useIntl();
+  const dispatch = useAppDispatch();
+  const { signedIn } = useIdentity();
+  const notificationsCount = useAppSelector(
+    selectUnreadNotificationGroupsCount,
+  );
+
+  const openComposer = useCallback(() => {
+    dispatch(openNewComposer({ type: 'post' }));
+  }, [dispatch]);
+
+  return (
+    <nav
+      className={classes.root}
+      aria-label={intl.formatMessage(messages.main)}
+    >
+      <NavigationHeader siteName={siteName} />
+      {signedIn && (
+        <ul className={classes.list}>
+          <NavigationLink
+            withSpaceAfter
+            as='button'
+            onClick={openComposer}
+            iconComponent={PenNibIcon}
+          >
+            <FormattedMessage id='tabs_bar.publish' defaultMessage='New Post' />
+          </NavigationLink>
+          <NavigationLink to='/home' iconComponent={HouseIcon}>
+            <FormattedMessage id='tabs_bar.home' defaultMessage='Home' />
+          </NavigationLink>
+          <NavigationLink to='/explore' iconComponent={MagnifyingGlassIcon}>
+            <FormattedMessage id='tabs_bar.explore' defaultMessage='Explore' />
+          </NavigationLink>
+          <NavigationLink
+            to='/notifications'
+            iconComponent={notificationsCount > 0 ? BellRingingIcon : BellIcon}
+            badgeCount={notificationsCount}
+          >
+            <FormattedMessage
+              id='tabs_bar.notifications'
+              defaultMessage='Notifications'
+            />
+          </NavigationLink>
+          <NavigationLink to='/conversations' iconComponent={ChatCircleIcon}>
+            <FormattedMessage
+              id='tabs_bar.messages'
+              defaultMessage='Messages'
+            />
+          </NavigationLink>
+          <NavigationLink to='/bookmarks' iconComponent={BookmarkSimpleIcon}>
+            <FormattedMessage id='tabs_bar.saved' defaultMessage='Saved' />
+          </NavigationLink>
+        </ul>
+      )}
+    </nav>
+  );
+};
