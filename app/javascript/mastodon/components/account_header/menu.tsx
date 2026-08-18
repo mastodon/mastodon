535981a20b3e5ecfbfa8c3ea493f2ebb33ca4539
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-08-18 17:11:28 +0200

    remove old reference to redesignMessages and remove unused i18n

--- app/javascript/mastodon/components/account_header/menu.tsx
+++ app/javascript/mastodon/components/account_header/menu.tsx
@@ -96,82 +96,6 @@ interface MenuItemsParams {
 }
 
 const messages = defineMessages({
-  unblock: { id: 'account.unblock', defaultMessage: 'Unblock @{name}' },
-  mention: { id: 'account.mention', defaultMessage: 'Mention @{name}' },
-  direct: { id: 'account.direct', defaultMessage: 'Privately mention @{name}' },
-  unmute: { id: 'account.unmute', defaultMessage: 'Unmute @{name}' },
-  block: { id: 'account.block', defaultMessage: 'Block @{name}' },
-  mute: { id: 'account.mute', defaultMessage: 'Mute @{name}' },
-  report: { id: 'account.report', defaultMessage: 'Report @{name}' },
-  blockDomain: {
-    id: 'account.block_domain',
-    defaultMessage: 'Block domain {domain}',
-  },
-  unblockDomain: {
-    id: 'account.unblock_domain',
-    defaultMessage: 'Unblock domain {domain}',
-  },
-  hideReblogs: {
-    id: 'account.hide_reblogs',
-    defaultMessage: 'Hide boosts from @{name}',
-  },
-  showReblogs: {
-    id: 'account.show_reblogs',
-    defaultMessage: 'Show boosts from @{name}',
-  },
-  addNote: {
-    id: 'account.add_note',
-    defaultMessage: 'Add a personal note',
-  },
-  editNote: {
-    id: 'account.edit_note',
-    defaultMessage: 'Edit personal note',
-  },
-  endorse: { id: 'account.endorse', defaultMessage: 'Feature on profile' },
-  unendorse: {
-    id: 'account.unendorse',
-    defaultMessage: "Don't feature on profile",
-  },
-  add_or_remove_from_list: {
-    id: 'account.add_or_remove_from_list',
-    defaultMessage: 'Add or Remove from lists',
-  },
-  admin_account: {
-    id: 'status.admin_account',
-    defaultMessage: 'Open moderation interface for @{name}',
-  },
-  admin_domain: {
-    id: 'status.admin_domain',
-    defaultMessage: 'Open moderation interface for {domain}',
-  },
-  languages: {
-    id: 'account.languages',
-    defaultMessage: 'Change subscribed languages',
-  },
-  openOriginalPage: {
-    id: 'account.open_original_page',
-    defaultMessage: 'Open original page',
-  },
-  removeFromFollowers: {
-    id: 'account.remove_from_followers',
-    defaultMessage: 'Remove {name} from followers',
-  },
-  confirmRemoveFromFollowersTitle: {
-    id: 'confirmations.remove_from_followers.title',
-    defaultMessage: 'Remove follower?',
-  },
-  confirmRemoveFromFollowersMessage: {
-    id: 'confirmations.remove_from_followers.message',
-    defaultMessage:
-      '{name} will stop following you. Are you sure you want to proceed?',
-  },
-  confirmRemoveFromFollowersButton: {
-    id: 'confirmations.remove_from_followers.confirm',
-    defaultMessage: 'Remove follower',
-  },
-});
-
-const redesignMessages = defineMessages({
   share: { id: 'account.menu.share', defaultMessage: 'Share…' },
   copy: { id: 'account.menu.copy', defaultMessage: 'Copy link' },
   copied: {
@@ -234,6 +158,44 @@ const redesignMessages = defineMessages({
     id: 'account.menu.remove_follower',
     defaultMessage: 'Remove follower',
   },
+  addNote: {
+    id: 'account.add_note',
+    defaultMessage: 'Add a personal note',
+  },
+  editNote: {
+    id: 'account.edit_note',
+    defaultMessage: 'Edit personal note',
+  },
+  endorse: { id: 'account.endorse', defaultMessage: 'Feature on profile' },
+  unendorse: {
+    id: 'account.unendorse',
+    defaultMessage: "Don't feature on profile",
+  },
+  admin_account: {
+    id: 'status.admin_account',
+    defaultMessage: 'Open moderation interface for @{name}',
+  },
+  admin_domain: {
+    id: 'status.admin_domain',
+    defaultMessage: 'Open moderation interface for {domain}',
+  },
+  languages: {
+    id: 'account.languages',
+    defaultMessage: 'Change subscribed languages',
+  },
+  confirmRemoveFromFollowersTitle: {
+    id: 'confirmations.remove_from_followers.title',
+    defaultMessage: 'Remove follower?',
+  },
+  confirmRemoveFromFollowersMessage: {
+    id: 'confirmations.remove_from_followers.message',
+    defaultMessage:
+      '{name} will stop following you. Are you sure you want to proceed?',
+  },
+  confirmRemoveFromFollowersButton: {
+    id: 'confirmations.remove_from_followers.confirm',
+    defaultMessage: 'Remove follower',
+  },
 });
 
 function getMenuItems({
@@ -252,7 +214,7 @@ function getMenuItems({
   if (account.url) {
     if ('share' in navigator) {
       items.push({
-        text: intl.formatMessage(redesignMessages.share),
+        text: intl.formatMessage(messages.share),
         action: () => {
           void navigator.share({
             url: account.url,
@@ -262,10 +224,10 @@ function getMenuItems({
       });
     }
     items.push({
-      text: intl.formatMessage(redesignMessages.copy),
+      text: intl.formatMessage(messages.copy),
       action: () => {
         void navigator.clipboard.writeText(account.url);
-        dispatch(showAlert({ message: redesignMessages.copied }));
+        dispatch(showAlert({ message: messages.copied }));
       },
       icon: LinkIcon,
     });
@@ -275,8 +237,8 @@ function getMenuItems({
   if (isRemote) {
     items.push({
       text: account.invalid_handle
-        ? intl.formatMessage(redesignMessages.openOriginalPageInvalid)
-        : intl.formatMessage(redesignMessages.openOriginalPage, {
+        ? intl.formatMessage(messages.openOriginalPageInvalid)
+        : intl.formatMessage(messages.openOriginalPage, {
             domain: remoteDomain,
           }),
       href: account.url,
@@ -290,14 +252,14 @@ function getMenuItems({
       items.push(
         null,
         {
-          text: intl.formatMessage(redesignMessages.mention),
+          text: intl.formatMessage(messages.mention),
           action: () => {
             dispatch(mentionCompose(account));
           },
         },
 
         {
-          text: intl.formatMessage(redesignMessages.direct),
+          text: intl.formatMessage(messages.direct),
           action: () => {
             dispatch(directCompose(account));
           },
@@ -314,7 +276,7 @@ function getMenuItems({
   // Add to list
   if (relationship?.following) {
     items.push({
-      text: intl.formatMessage(redesignMessages.addToList),
+      text: intl.formatMessage(messages.addToList),
       action: () => {
         dispatch(
           openModal({
@@ -334,7 +296,7 @@ function getMenuItems({
     (canAccountBeAddedByFollowers(account) && relationship?.following)
   ) {
     items.push({
-      text: intl.formatMessage(redesignMessages.addToCollection),
+      text: intl.formatMessage(messages.addToCollection),
       action: () => {
         dispatch(
           openModal({
@@ -369,7 +331,7 @@ function getMenuItems({
       text: intl.formatMessage(
         relationship?.note ? messages.editNote : messages.addNote,
       ),
-      description: intl.formatMessage(redesignMessages.noteDescription),
+      description: intl.formatMessage(messages.noteDescription),
       action: () => {
         dispatch(
           openModal({
@@ -390,8 +352,8 @@ function getMenuItems({
       {
         text: intl.formatMessage(
           relationship.showing_reblogs
-            ? redesignMessages.hideReblogs
-            : redesignMessages.showReblogs,
+            ? messages.hideReblogs
+            : messages.showReblogs,
         ),
         action: () => {
           dispatch(
@@ -421,7 +383,7 @@ function getMenuItems({
   items.push(
     {
       text: intl.formatMessage(
-        relationship?.muting ? redesignMessages.unmute : redesignMessages.mute,
+        relationship?.muting ? messages.unmute : messages.mute,
       ),
       action: () => {
         if (relationship?.muting) {
@@ -436,7 +398,7 @@ function getMenuItems({
 
   if (relationship?.followed_by) {
     items.push({
-      text: intl.formatMessage(redesignMessages.removeFollower),
+      text: intl.formatMessage(messages.removeFollower),
       action: () => {
         dispatch(
           openModal({
@@ -468,9 +430,7 @@ function getMenuItems({
 
   items.push({
     text: intl.formatMessage(
-      relationship?.blocking
-        ? redesignMessages.unblock
-        : redesignMessages.block,
+      relationship?.blocking ? messages.unblock : messages.block,
     ),
     action: () => {
       if (relationship?.blocking) {
@@ -485,7 +445,7 @@ function getMenuItems({
 
   if (!account.suspended) {
     items.push({
-      text: intl.formatMessage(redesignMessages.report),
+      text: intl.formatMessage(messages.report),
       action: () => {
         dispatch(initReport(account));
       },
@@ -498,8 +458,8 @@ function getMenuItems({
     items.push(null, {
       text: intl.formatMessage(
         relationship?.domain_blocking
-          ? redesignMessages.domainUnblock
-          : redesignMessages.domainBlock,
+          ? messages.domainUnblock
+          : messages.domainBlock,
         {
           domain: remoteDomain,
         },
