364d5437440d20555d0047f61f0efe4b58680e01
Claire <claire.github-309c@sitedethib.com>
2026-08-19 11:47:05 +0200

    [Experiment] Don't use HTTP connection pool for account deletion notices

--- app/workers/activitypub/low_priority_delivery_worker.rb
+++ app/workers/activitypub/low_priority_delivery_worker.rb
@@ -2,4 +2,18 @@
 
 class ActivityPub::LowPriorityDeliveryWorker < ActivityPub::DeliveryWorker
   sidekiq_options queue: 'pull', retry: 8, dead: false
+
+  def perform_request
+    stoplight_wrapper.run do
+      build_request(nil).perform do |response|
+        if response_successful?(response)
+          @performed = true
+        elsif response_error_unsalvageable?(response) || unsalvageable_authorization_failure?(response)
+          @unsalvageable = true
+        else
+          raise Mastodon::UnexpectedResponseError, response
+        end
+      end
+    end
+  end
 end
