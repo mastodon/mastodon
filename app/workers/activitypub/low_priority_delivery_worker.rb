f28d32278eef05bb96ed0a3953c959aff4944549
Claire <claire.github-309c@sitedethib.com>
2026-08-19 14:26:19 +0200

    Disable stoplight

--- app/workers/activitypub/low_priority_delivery_worker.rb
+++ app/workers/activitypub/low_priority_delivery_worker.rb
@@ -4,15 +4,13 @@ class ActivityPub::LowPriorityDeliveryWorker < ActivityPub::DeliveryWorker
   sidekiq_options queue: 'pull', retry: 8, dead: false
 
   def perform_request
-    stoplight_wrapper.run do
-      build_request(nil).perform do |response|
-        if response_successful?(response)
-          @performed = true
-        elsif response_error_unsalvageable?(response) || unsalvageable_authorization_failure?(response)
-          @unsalvageable = true
-        else
-          raise Mastodon::UnexpectedResponseError, response
-        end
+    build_request(nil).perform do |response|
+      if response_successful?(response)
+        @performed = true
+      elsif response_error_unsalvageable?(response) || unsalvageable_authorization_failure?(response)
+        @unsalvageable = true
+      else
+        raise Mastodon::UnexpectedResponseError, response
       end
     end
   end
