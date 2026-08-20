7a4f722f7483b4a243021bb3eb96419aba61448d
Eugen Rochko <eugen@zeonfederated.com>
2026-08-20 08:14:50 +0000

    Fix autosuggestions overstaying their welcome in languages that don't use spaces (#40217)

--- app/javascript/mastodon/components/autosuggest/utils.ts
+++ app/javascript/mastodon/components/autosuggest/utils.ts
@@ -8,7 +8,7 @@ export const textAtCursorMatchesToken = (
   let word: string;
 
   const regex = new RegExp(
-    `[${searchTokens.join('')}${WORD}+-]+(\\s[${WORD}]+)?$`,
+    `[${searchTokens.join('')}${WORD}+-]+(\\s[\\p{Script=Latin}\\p{Script=Cyrillic}\\p{M}]+)?$`,
     'iu',
   );
   const left = str.slice(0, caretPosition).search(regex);
