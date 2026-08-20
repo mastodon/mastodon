c098283e906bfcdcdb3dd5c62b7ad9dd6399dc21
diondiondion <mail@diondiondion.com>
2026-08-20 09:53:07 +0200

    Redesign: Always show search field on Explore page

--- app/javascript/mastodon/features/explore/index.tsx
+++ app/javascript/mastodon/features/explore/index.tsx
@@ -1,11 +1,13 @@
 import { defineMessages, useIntl, FormattedMessage } from 'react-intl';
 
+import classNames from 'classnames';
 import { NavLink, Switch, Route } from 'react-router-dom';
 
 import { Helmet } from '@unhead/react/helmet';
 
 import { Column } from '@/mastodon/components/column';
 import { ColumnHeader } from '@/mastodon/components/column/header';
+import { isRedesignEnabled } from '@/mastodon/utils/environment';
 import TrendingUpIcon from '@/material-icons/400-24px/trending_up.svg?react';
 import { SymbolLogo } from 'mastodon/components/logo';
 import { Search } from 'mastodon/features/compose/components/search';
@@ -13,6 +15,7 @@ import { useBreakpoint } from 'mastodon/features/ui/hooks/useBreakpoint';
 import { useIdentity } from 'mastodon/identity_context';
 
 import Links from './links';
+import redesignClasses from './redesign.module.scss';
 import Statuses from './statuses';
 import Suggestions from './suggestions';
 import Tags from './tags';
@@ -39,7 +42,12 @@ const Explore: React.FC<{ multiColumn: boolean }> = ({ multiColumn }) => {
         scrollTopOnClick
       />
 
-      <div className='explore__search-header'>
+      <div
+        className={classNames(
+          'explore__search-header',
+          isRedesignEnabled() && redesignClasses.searchHeader,
+        )}
+      >
         <Search singleColumn />
       </div>
 
