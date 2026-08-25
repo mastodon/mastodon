4c1b977ff234011bf44af82a0ff2d8f22c165420
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-08-25 11:57:00 +0200

    wire up suggestions with placeholder

--- app/javascript/mastodon/features/compose/redesign/textarea.tsx
+++ app/javascript/mastodon/features/compose/redesign/textarea.tsx
@@ -1,5 +1,6 @@
 import type React from 'react';
-import { useCallback, useRef } from 'react';
+import type { RefCallback } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 import { defineMessages, useIntl } from 'react-intl';
 
@@ -14,15 +15,38 @@ import {
   selectComposeSuggestion,
 } from '@/mastodon/actions/compose';
 import { processPasteOrDrop } from '@/mastodon/actions/compose_typed';
-import AutosuggestTextareaOriginal from '@/mastodon/components/autosuggest_textarea';
-import { COMPOSER_TEXTAREA_ID } from '@/mastodon/reducers/slices/composer';
+import { textAtCursorMatchesToken } from '@/mastodon/components/autosuggest/utils';
+import { Avatar } from '@/mastodon/components/avatar';
+import { DisplayName } from '@/mastodon/components/display_name';
+import { Emoji } from '@/mastodon/components/emoji';
+import { TextArea } from '@/mastodon/components/form_fields';
+import { normalizeKey } from '@/mastodon/components/hotkeys/utils';
+import {
+  getAllMenuItems,
+  Menu,
+  MenuItem,
+  useMenuContext,
+} from '@/mastodon/components/menu';
+import { PopoverMenuCard } from '@/mastodon/components/menu/card';
+import { useMergedRefs } from '@/mastodon/hooks/useMergedRefs';
+import { usePrevious } from '@/mastodon/hooks/usePrevious';
+import {
+  COMPOSER_TEXTAREA_ID,
+  focusComposerTextarea,
+} from '@/mastodon/reducers/slices/composer';
+import { selectPlainAccount } from '@/mastodon/selectors/accounts';
 import {
   createAppSelector,
   useAppDispatch,
   useAppSelector,
 } from '@/mastodon/store';
 
-import { selectComposeType } from './selectors';
+import type { Suggestion } from './selectors';
+import {
+  selectComposeType,
+  selectSuggestions,
+  stringOrUndefined,
+} from './selectors';
 import classes from './styles.module.scss';
 
 const messages = defineMessages({
@@ -37,23 +61,6 @@ const messages = defineMessages({
       'Message refers to a direct message. For languages where this is confusing, "chat" or "direct message" can be used.',
   },
 });
-
-type SuggestSelectedHandler = (
-  position: number,
-  token: string,
-  suggestion: unknown,
-) => void;
-
-const AutosuggestTextarea =
-  AutosuggestTextareaOriginal as React.ForwardRefExoticComponent<
-    {
-      suggestions: Immutable.List<unknown>;
-      onSuggestionSelected: SuggestSelectedHandler;
-      onSuggestionsClearRequested: () => void;
-      onSuggestionsFetchRequested: (token: string) => void;
-    } & TextareaAutosizeProps &
-      React.RefAttributes<HTMLTextAreaElement>
-  >;
 
 type ComposeTextareaProps = Omit<
   TextareaAutosizeProps,
@@ -71,9 +78,6 @@ const selectComposeTextState = createAppSelector(
   (compose) => ({
     text: compose.get('text') as string,
     lang: compose.get('language') as string,
-    suggestions: compose.get(
-      'suggestions',
-    ) as unknown as Immutable.List<unknown>,
     isSubmitting: !!compose.get('is_submitting'),
   }),
 );
@@ -87,9 +91,7 @@ export const ComposeTextarea: React.FC<ComposeTextareaProps> = ({
   const intl = useIntl();
 
   const type = useAppSelector(selectComposeType);
-  const { suggestions, text, lang, isSubmitting } = useAppSelector(
-    selectComposeTextState,
-  );
+  const { text, lang, isSubmitting } = useAppSelector(selectComposeTextState);
 
   const dispatch = useAppDispatch();
   const onClickWrapper: React.MouseEventHandler<HTMLDivElement> = useCallback(
@@ -100,24 +102,77 @@ export const ComposeTextarea: React.FC<ComposeTextareaProps> = ({
     },
     [],
   );
+
+  // Suggestion logic
+  const suggestions = useAppSelector(selectSuggestions);
+  const lastTokenRef = useRef<string | null>(null);
+  const tokenStartRef = useRef(0);
+  const onSuggestionClick: React.MouseEventHandler<HTMLButtonElement> =
+    useCallback(
+      (event) => {
+        const { id, index, type } = event.currentTarget.dataset;
+        const suggestion = suggestions.find((suggestion, i) =>
+          suggestion.type === type && suggestion.id
+            ? suggestion.id === id
+            : index && Number.parseInt(index) === i,
+        );
+        if (!suggestion) {
+          return;
+        }
+
+        dispatch(
+          selectComposeSuggestion(
+            tokenStartRef.current,
+            lastTokenRef.current,
+            suggestion,
+            ['text'],
+          ),
+        );
+        focusComposerTextarea(true);
+      },
+      [dispatch, suggestions],
+    );
+  const [textArea, setTextArea] = useState<HTMLTextAreaElement | null>(null);
+  const [suggestionList, setSuggestionList] = useState<HTMLDivElement | null>(
+    null,
+  );
+
   const onChange: React.ChangeEventHandler<HTMLTextAreaElement> = useCallback(
     (event) => {
       dispatch(changeCompose(event.target.value));
+
+      const [tokenStart, token] = textAtCursorMatchesToken(
+        event.target.value,
+        event.target.selectionStart,
+        ['@', '＠', ':', '#', '＃'],
+      );
+
+      if (token !== null && lastTokenRef.current !== token) {
+        tokenStartRef.current = tokenStart;
+        lastTokenRef.current = token;
+        dispatch(fetchComposeSuggestions(token));
+      } else if (token === null) {
+        lastTokenRef.current = null;
+        dispatch(clearComposeSuggestions());
+      }
     },
-    [dispatch],
+    [dispatch, lastTokenRef],
   );
+
   const onKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> =
     useCallback(
       (event) => {
-        const key = event.key.toLowerCase();
+        const key = normalizeKey(event.key);
         if (key === 'enter' && (event.ctrlKey || event.metaKey)) {
           onSubmit();
           event.preventDefault();
-        } else if (['esc', 'escape'].includes(key)) {
+        } else if (key === 'escape') {
           event.currentTarget.blur();
+        } else if (key === 'down' && suggestions.length > 0 && suggestionList) {
+          (getAllMenuItems(suggestionList).at(0) ?? suggestionList).focus();
         }
       },
-      [onSubmit],
+      [onSubmit, suggestionList, suggestions.length],
     );
 
   const onPasteOrDrop = useCallback(
@@ -131,23 +186,6 @@ export const ComposeTextarea: React.FC<ComposeTextareaProps> = ({
     },
     [dispatch],
   );
-  const onSuggestionsFetchRequested = useCallback(
-    (token: string) => {
-      dispatch(fetchComposeSuggestions(token));
-    },
-    [dispatch],
-  );
-  const onSuggestionsClearRequested = useCallback(() => {
-    dispatch(clearComposeSuggestions());
-  }, [dispatch]);
-  const onSuggestionSelected: SuggestSelectedHandler = useCallback(
-    (position, token, suggestion) => {
-      dispatch(selectComposeSuggestion(position, token, suggestion, ['text']));
-    },
-    [dispatch],
-  );
-
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions -- This just moves focus to the textarea.
@@ -155,10 +193,10 @@ export const ComposeTextarea: React.FC<ComposeTextareaProps> = ({
       onClick={onClickWrapper}
       className={classNames(className, classes.textareaWrapper)}
     >
-      <AutosuggestTextarea
+      <TextArea
         {...props}
         id={COMPOSER_TEXTAREA_ID}
-        ref={textareaRef}
+        ref={setTextArea}
         value={text}
         lang={lang}
         placeholder={intl.formatMessage(
@@ -167,15 +205,98 @@ export const ComposeTextarea: React.FC<ComposeTextareaProps> = ({
             : messages.placeholder,
         )}
         disabled={disabled || isSubmitting}
-        suggestions={suggestions}
-        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={onSuggestionsClearRequested}
-        onSuggestionSelected={onSuggestionSelected}
         onKeyDown={onKeyDown}
         onDrop={onPasteOrDrop}
         onPaste={onPasteOrDrop}
         onChange={onChange}
       />
+
+      {suggestions.length > 0 && (
+        <Menu noFocus>
+          <AutosuggestMenuList textElement={textArea} ref={setSuggestionList}>
+            {suggestions.map((suggestion, index) => (
+              <MenuItem
+                key={`${suggestion.type}:${suggestion.id}`}
+                onClick={onSuggestionClick}
+                data-index={index}
+                data-type={suggestion.type}
+                data-id={suggestion.id}
+              >
+                <AutosuggestItem suggestion={suggestion} />
+              </MenuItem>
+            ))}
+          </AutosuggestMenuList>
+        </Menu>
+      )}
+    </div>
+  );
+};
+
+const AutosuggestMenuList: React.FC<{
+  children: React.ReactElement[];
+  textElement: HTMLTextAreaElement | null;
+  ref: RefCallback<HTMLDivElement>;
+}> = ({ children, textElement, ref }) => {
+  const suggestions = useAppSelector(selectSuggestions);
+  const token = useAppSelector((state) =>
+    stringOrUndefined(state.compose.get('suggestion_token')),
+  );
+  const lastToken = usePrevious(token);
+
+  const { popover, menuListProps } = useMenuContext();
+
+  if (!popover.isMenuOpen && token !== lastToken && suggestions.length > 0) {
+    popover.openMenu();
+  }
+
+  const mergedRef = useMergedRefs(menuListProps.ref, ref);
+
+  return (
+    <PopoverMenuCard
+      isOpen={popover.isMenuOpen}
+      onClose={popover.closeMenu}
+      reference={textElement}
+      popoverElement={popover.popover}
+      container={null}
+      placement='bottom-start'
+      {...menuListProps}
+      ref={mergedRef}
+    >
+      {children}
+    </PopoverMenuCard>
+  );
+};
+
+const AutosuggestItem: React.FC<{ suggestion: Suggestion }> = ({
+  suggestion,
+}) => {
+  if (suggestion.type === 'account') {
+    return <AutosuggestAccount id={suggestion.id} />;
+  } else if (suggestion.type === 'hashtag') {
+    return <div>#{suggestion.name}</div>;
+  } else {
+    const colons = `:${suggestion.id}:`;
+    return (
+      <div>
+        <Emoji code={suggestion.native ?? colons} />
+
+        {colons}
+      </div>
+    );
+  }
+};
+
+const AutosuggestAccount: React.FC<{ id: string }> = ({ id }) => {
+  const account = useAppSelector((state) => selectPlainAccount(state, id));
+
+  if (!account) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Avatar account={account} />
+      <DisplayName account={account} />
     </div>
   );
 };
