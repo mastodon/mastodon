64f54aaf4a9d1b2cb4d219aa3325188f256f4593
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-08-26 19:11:09 +0200

    break out item types

--- app/javascript/mastodon/components/autosuggest/items.tsx
+++ app/javascript/mastodon/components/autosuggest/items.tsx
@@ -1,3 +1,5 @@
+import type React from 'react';
+
 import { selectPlainAccount } from '@/mastodon/selectors/accounts';
 import { useAppSelector } from '@/mastodon/store';
 
@@ -5,25 +7,22 @@ import { Avatar } from '../avatar';
 import { DisplayName } from '../display_name';
 import { Emoji } from '../emoji';
 
-import type { Suggestion } from './types';
+import type {
+  EmojiSuggestion,
+  HashtagSuggestion,
+  LocalHashtagSuggestion,
+  Suggestion,
+} from './types';
 
 export const AutosuggestItem: React.FC<{ suggestion: Suggestion }> = ({
   suggestion,
 }) => {
   if (suggestion.type === 'account') {
-    return <AutosuggestAccount id={suggestion.id} />;
+    return <AutosuggestAccount {...suggestion} />;
   } else if (suggestion.type === 'hashtag') {
-    return <div>#{suggestion.name}</div>;
-  } else {
-    const colons = `:${suggestion.id}:`;
-    return (
-      <div>
-        <Emoji code={suggestion.native ?? colons} />
-
-        {colons}
-      </div>
-    );
+    return <AutosuggestHashtag {...suggestion} />;
   }
+  return <AutosuggestEmoji {...suggestion} />;
 };
 
 const AutosuggestAccount: React.FC<{ id: string }> = ({ id }) => {
@@ -39,4 +38,21 @@ const AutosuggestAccount: React.FC<{ id: string }> = ({ id }) => {
       <DisplayName account={account} />
     </div>
   );
+};
+
+const AutosuggestEmoji: React.FC<EmojiSuggestion> = ({ id, native }) => {
+  const colons = `:${id}:`;
+  return (
+    <div>
+      <Emoji code={native ?? colons} />
+
+      {colons}
+    </div>
+  );
+};
+
+const AutosuggestHashtag: React.FC<
+  HashtagSuggestion | LocalHashtagSuggestion
+> = ({ name }) => {
+  return <div>#{name}</div>;
 };
