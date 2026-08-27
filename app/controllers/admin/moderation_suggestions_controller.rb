ecc863962e3c779091ca0fcf00bb7c107a38dfad
Claire <claire.github-309c@sitedethib.com>
2026-08-27 18:02:29 +0200

    [WiP] Suggestion list

--- app/controllers/admin/moderation_suggestions_controller.rb
+++ app/controllers/admin/moderation_suggestions_controller.rb
@@ -2,14 +2,28 @@
 
 class Admin::ModerationSuggestionsController < Admin::BaseController
   before_action :set_moderation_suggestions, only: :index
+  before_action :set_moderation_suggestion, only: [:destroy]
 
   def index
     authorize :moderation_suggestion, :index?
+  end
+
+  def destroy
+    authorize @moderation_suggestion, :dismiss?
+
+    # TODO: log?
+    @moderation_suggestion.update!(state: :dismissed)
+
+    redirect_to admin_moderation_suggestions_path, notice: I18n.t('admin.moderation_suggestions.destroyed_msg')
   end
 
   private
 
   def set_moderation_suggestions
     @moderation_suggestions = ModerationSuggestion.where(state: ['new', 'mailed']).order(id: :asc)
+  end
+
+  def set_moderation_suggestion
+    @moderation_suggestion = ModerationSuggestion.find(params[:id])
   end
 end
