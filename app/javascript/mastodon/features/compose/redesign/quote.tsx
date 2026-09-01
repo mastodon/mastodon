b3654c9af773d89b5c1ea33078f5e0fa85d92165
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-09-01 18:33:52 +0200

    display images in quotes

--- app/javascript/mastodon/features/compose/redesign/quote.tsx
+++ app/javascript/mastodon/features/compose/redesign/quote.tsx
@@ -3,21 +3,34 @@ import { useCallback } from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
+import { PlayIcon } from '@phosphor-icons/react';
+
 import { quoteComposeCancel } from '@/mastodon/actions/compose_typed';
+import type {
+  ApiAudioAttachmentJSON,
+  ApiGifvAttachmentJSON,
+  ApiImageAttachmentJSON,
+  ApiVideoAttachmentJSON,
+} from '@/mastodon/api_types/media_attachments';
 import { Avatar } from '@/mastodon/components/avatar';
-import { Blurhash } from '@/mastodon/components/blurhash';
 import { Card, CardBody, CardTitle } from '@/mastodon/components/card';
 import { LinkedDisplayName } from '@/mastodon/components/display_name';
 import { EmojiHTML } from '@/mastodon/components/emoji/html';
+import { Icon } from '@/mastodon/components/icon';
 import { RelativeTimestamp } from '@/mastodon/components/relative_timestamp';
-import type { AccountStatusShape } from '@/mastodon/models/status';
+import type {
+  MediaAttachmentShape,
+  AccountStatusShape,
+} from '@/mastodon/models/status';
 import { selectAccountStatus } from '@/mastodon/selectors/statuses';
 import { useAppDispatch, useAppSelector } from '@/mastodon/store';
 import type { OnElementHandler } from '@/mastodon/utils/html';
 
 import classes from './attachments.module.scss';
+import { ComposeImage } from './upload';
 
 export const ComposeQuote: React.FC<{ id: string }> = ({ id }) => {
   const status = useAppSelector((state) => selectAccountStatus(state, id));
@@ -31,20 +44,10 @@ export const ComposeQuote: React.FC<{ id: string }> = ({ id }) => {
     return null;
   }
 
-  let imageEle: React.ReactNode = null;
-  const image = status.media_attachments.find(({ type }) => type !== 'unknown');
-  if (image) {
-    imageEle = !status.sensitive ? (
-      <img src={image.preview_url} alt={image.description} />
-    ) : (
-      <Blurhash hash={image.blurhash} width={120} />
-    );
-  }
-
   const statusTo = `/@${status.account.acct}/${status.id}`;
 
   return (
-    <Card image={imageEle} onDelete={handleDelete}>
+    <Card onDelete={handleDelete}>
       <CardTitle
         className={classes.quoteTitle}
         image={
@@ -86,15 +89,74 @@ export const ComposeQuote: React.FC<{ id: string }> = ({ id }) => {
 };
 
 const ComposeQuoteBody: React.FC<{
+  sensitive?: boolean;
   status: AccountStatusShape;
-}> = ({ status }) => {
+}> = ({ sensitive, status }) => {
+  const attachments = status.media_attachments;
+  const mainAttachment = attachments.find(
+    (
+      attachment,
+    ): attachment is
+      | MediaAttachmentShape<ApiAudioAttachmentJSON>
+      | MediaAttachmentShape<ApiVideoAttachmentJSON> =>
+      attachment.type === 'audio' || attachment.type === 'video',
+  );
+  const imageAttachments = attachments.filter(
+    (
+      attachment,
+    ): attachment is MediaAttachmentShape<
+      ApiImageAttachmentJSON | ApiGifvAttachmentJSON
+    > => attachment.type === 'gifv' || attachment.type === 'image',
+  );
+
   return (
-    <EmojiHTML
-      htmlString={status.translation?.contentHtml ?? status.contentHtml}
-      extraEmojis={status.emojis}
-      lang={status.translation?.language ?? status.language}
-      onElement={onStatusLinks}
-    />
+    <>
+      <EmojiHTML
+        htmlString={status.translation?.contentHtml ?? status.contentHtml}
+        extraEmojis={status.emojis}
+        lang={status.translation?.language ?? status.language}
+        onElement={onStatusLinks}
+      />
+      {mainAttachment?.type === 'audio' && (
+        <div className={classes.quoteSpoiler}>
+          <FormattedMessage
+            id='compose.quote.audio'
+            defaultMessage='Audio file ({duration})'
+            values={{ duration: mainAttachment.meta.original.duration }}
+          />
+        </div>
+      )}
+      {mainAttachment?.type === 'video' && (
+        <div className={classNames(classes.quoteMedia, classes.mediaSingle)}>
+          <ComposeImage attachment={mainAttachment} sensitive={sensitive}>
+            <div className={classes.quoteVideoDuration}>
+              <Icon icon={PlayIcon} weight='fill' />
+
+              {mainAttachment.meta.original.duration}
+            </div>
+          </ComposeImage>
+        </div>
+      )}
+      {imageAttachments.length > 0 ||
+        (mainAttachment?.type === 'video' && (
+          <div
+            className={classNames(
+              classes.quoteMedia,
+              imageAttachments.length === 1 && classes.mediaSingle,
+              imageAttachments.length > 1 && classes.mediaGrid,
+            )}
+            data-number={imageAttachments.length}
+          >
+            {imageAttachments.map((attachment) => (
+              <ComposeImage
+                attachment={attachment}
+                sensitive={sensitive}
+                key={attachment.id}
+              />
+            ))}
+          </div>
+        ))}
+    </>
   );
 };
 
