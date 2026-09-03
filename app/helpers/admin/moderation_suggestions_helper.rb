ccadf6a7b3bdb20fb3f50349cda29fdf274280c6
Claire <claire.github-309c@sitedethib.com>
2026-09-03 11:29:12 +0200

    Moderation suggestion interface improvements

--- app/helpers/admin/moderation_suggestions_helper.rb
+++ app/helpers/admin/moderation_suggestions_helper.rb
@@ -11,4 +11,12 @@ module Admin::ModerationSuggestionsHelper
   def advisory_action_icon(action)
     material_symbol(ACTION_ICON_NAMES.fetch(action.to_sym))
   end
+
+  def advisory_action_link(action, suggestion)
+    link_to(
+      safe_join([advisory_action_icon(action), t("admin.moderation_suggestions.apply_#{action}")]),
+      suggestion ? apply_admin_moderation_suggestion_path(suggestion) : nil,
+      { class: ['button', 'button-secondary', disabled: !suggestion], method: :post }
+    )
+  end
 end
