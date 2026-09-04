8e8afd31cb36a177544621a6a4029f8a5dfa73b5
Matt Jankowski <matt@jankowski.online>
2026-09-04 09:22:58 +0000

    Add multiple timeline update coverage to "markers" API spec (#40374)

--- spec/models/marker_spec.rb
+++ spec/models/marker_spec.rb
@@ -3,7 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Marker do
+  describe 'Associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
   describe 'Validations' do
+    it { is_expected.to validate_presence_of(:timeline) }
     it { is_expected.to validate_inclusion_of(:timeline).in_array(described_class::TIMELINES) }
   end
 end
