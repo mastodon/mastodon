bc36c6530098484bd4a288aa36c7fdba241de946
Claire <claire.github-309c@sitedethib.com>
2026-09-07 09:29:42 +0000

    Fix notifications not being cleaned up when notification requests are deleted in bulk (#40393)

--- app/workers/filtered_notification_cleanup_worker.rb
+++ app/workers/filtered_notification_cleanup_worker.rb
@@ -3,7 +3,7 @@
 class FilteredNotificationCleanupWorker
   include Sidekiq::Worker
 
-  def perform(account_id, from_account_id)
-    Notification.where(account_id: account_id, from_account_id: from_account_id, filtered: true).in_batches(order: :desc).delete_all
+  def perform(account_id, from_account_ids)
+    Notification.where(account_id: account_id, from_account_id: from_account_ids, filtered: true).in_batches(order: :desc).delete_all
   end
 end
