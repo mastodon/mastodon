d5ac4ec0afe6979a752e3c42a40c8cf751cd62c9
Claire <claire.github-309c@sitedethib.com>
2026-09-07 11:09:38 +0200

    Fix account deletion not deleting generated annual reports

--- app/services/delete_account_service.rb
+++ app/services/delete_account_service.rb
@@ -28,6 +28,7 @@ class DeleteAccountService < BaseService
     scheduled_statuses
     status_pins
     tag_follows
+    generated_annual_reports
   ).freeze
 
   # The following associations have no important side-effects
@@ -53,6 +54,7 @@ class DeleteAccountService < BaseService
     scheduled_statuses
     status_pins
     tag_follows
+    generated_annual_reports
   ).freeze
 
   ASSOCIATIONS_ON_DESTROY = %w(
