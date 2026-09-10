3ee37e05f952294944eb1f2b9e469e7338dca680
Claire <claire.github-309c@sitedethib.com>
2026-09-10 07:04:18 +0000

    Fix submitting a status twice leading to a 500 error (#40439)

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -20,6 +20,15 @@ class PostStatusService < BaseService
     end
   end
 
+  class IdempotencyError < StandardError
+    attr_reader :status
+
+    def initialize(status)
+      super()
+      @status = status
+    end
+  end
+
   # Post a text status update, fetch and notify remote users mentioned
   # @param [Account] account Account from which to post
   # @param [Hash] options
@@ -46,9 +55,10 @@ class PostStatusService < BaseService
     @in_reply_to = @options[:thread]
     @quoted_status = @options[:quoted_status]
 
+    preprocess_attributes!
+
     with_idempotency do
       validate_media!
-      preprocess_attributes!
 
       if scheduled?
         schedule_status!
@@ -63,7 +73,7 @@ class PostStatusService < BaseService
     end
 
     @status
-  rescue Antispam::SilentlyDrop => e
+  rescue Antispam::SilentlyDrop, IdempotencyError => e
     e.status
   end
 
@@ -237,7 +247,7 @@ class PostStatusService < BaseService
     return yield unless idempotency_given?
 
     with_redis_lock("idempotency:lock:status:#{@account.id}:#{@options[:idempotency]}") do
-      return idempotency_duplicate if idempotency_duplicate?
+      raise IdempotencyError, idempotency_duplicate if idempotency_duplicate?
 
       yield
 
