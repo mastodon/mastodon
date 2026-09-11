fb72f83ce09583b98541389647694fff80354ec2
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-09-11 16:59:41 +0200

    adds useOverflowButton to hashtags

--- app/javascript/mastodon/components/status/hashtag_bar.tsx
+++ app/javascript/mastodon/components/status/hashtag_bar.tsx
@@ -1,3 +1,10 @@
+import { FormattedMessage } from 'react-intl';
+
+import classNames from 'classnames';
+
+import { useOverflowButton } from '@/mastodon/hooks/useOverflow';
+import { useToggle } from '@/mastodon/hooks/useToggle';
+
 import { Button } from '../button/redesign';
 
 import classes from './styles.module.scss';
@@ -6,23 +13,46 @@ export const StatusHashtagBar: React.FC<{
   hashtags: string[];
   accountId: string;
 }> = ({ hashtags, accountId }) => {
+  const [showOverflow, { onTrue: onShowOverflow }] = useToggle();
+  const { hiddenCount, wrapperRef, listRef, hiddenIndex, maxWidth } =
+    useOverflowButton();
+
   if (hashtags.length === 0) {
     return null;
   }
 
   return (
-    <div className={classes.hashtags}>
-      {hashtags.map((hashtag) => (
-        <Button
-          key={hashtag}
-          size='xs'
-          as='link'
-          to={`/tags/${hashtag}`}
-          data-menu-hashtag={accountId}
-        >
-          #{hashtag}
+    <div className={classes.hashtagsWrapper} ref={wrapperRef}>
+      <div
+        className={classNames(
+          classes.hashtags,
+          showOverflow && classes.hashtagsShowAll,
+        )}
+        ref={listRef}
+        style={{ maxWidth }}
+      >
+        {hashtags.map((hashtag, index) => (
+          <Button
+            key={hashtag}
+            size='xs'
+            as='link'
+            to={`/tags/${hashtag}`}
+            data-menu-hashtag={accountId}
+            inert={hiddenIndex > 0 && index >= hiddenIndex}
+          >
+            #{hashtag}
+          </Button>
+        ))}
+      </div>
+      {hiddenCount > 0 && !showOverflow && (
+        <Button size='xs' onClick={onShowOverflow}>
+          <FormattedMessage
+            id='featured_tags.more_items'
+            defaultMessage='+{count}'
+            values={{ count: hiddenCount }}
+          />
         </Button>
-      ))}
+      )}
     </div>
   );
 };
