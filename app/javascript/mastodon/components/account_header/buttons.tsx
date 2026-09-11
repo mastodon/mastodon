78d16f7ec57f51c580fc89d905941567f458952b
diondiondion <mail@diondiondion.com>
2026-09-11 12:01:39 +0200

    Hide regular follow button from profile header

--- app/javascript/mastodon/components/account_header/buttons.tsx
+++ app/javascript/mastodon/components/account_header/buttons.tsx
@@ -8,6 +8,7 @@ import { useAccount } from '@/mastodon/hooks/useAccount';
 import { useFollowReference } from '@/mastodon/hooks/useFollowReference';
 import { getAccountHidden } from '@/mastodon/selectors/accounts';
 import { useAppDispatch, useAppSelector } from '@/mastodon/store';
+import { isRedesignEnabled } from '@/mastodon/utils/environment';
 import NotificationsIcon from '@/material-icons/400-24px/notifications.svg?react';
 import NotificationsActiveIcon from '@/material-icons/400-24px/notifications_active-fill.svg?react';
 import ShareIcon from '@/material-icons/400-24px/share.svg?react';
@@ -94,7 +95,7 @@ const AccountButtonsOther: FC<
 
   return (
     <>
-      {!isMovedAndUnfollowedAccount && (
+      {!isMovedAndUnfollowedAccount && !isRedesignEnabled() && (
         <FollowButton
           accountId={accountId}
           className={classes.followButton}
