17ce841c916d0ca640a02b725a92c11ac2aeb072
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-09-15 16:25:05 +0200

    starts work on spoilers

--- app/javascript/mastodon/components/status/hooks.ts
+++ app/javascript/mastodon/components/status/hooks.ts
@@ -56,7 +56,7 @@ export function useStatusHandlers({
   contextType?: StatusContextType;
   onOpen?: () => void;
 }) {
-  const matchedFilters = useAppSelector((state) =>
+  const { filterAction } = useAppSelector((state) =>
     selectStatusFilters(state, { contextType, statusId: status?.id }),
   );
   const [showDespiteFilter, { onToggle: onFilterToggle }] = useToggle(false);
@@ -65,28 +65,18 @@ export function useStatusHandlers({
   const statusId = status?.id;
 
   // Display handlers
-  const onExpandedToggle = useCallback(() => {
-    dispatch(toggleStatusSpoilers(statusId));
-  }, [dispatch, statusId]);
-
   const onToggleHidden = useCallback(() => {
     if (!status) {
       return;
     }
-    if (!matchedFilters.length || showDespiteFilter) {
+    if (!filterAction || showDespiteFilter) {
       dispatch(toggleStatusSpoilers(status.id));
     }
 
     if (!status.hidden || !status.spoiler_text) {
       onFilterToggle();
     }
-  }, [
-    dispatch,
-    matchedFilters.length,
-    onFilterToggle,
-    showDespiteFilter,
-    status,
-  ]);
+  }, [dispatch, filterAction, onFilterToggle, showDespiteFilter, status]);
 
   // Interaction handlers
   const handlerFactory = useCallback(
@@ -187,7 +177,6 @@ export function useStatusHandlers({
     () => ({
       showDespiteFilter,
       onOpenClick,
-      onExpandedToggle,
       onFilterToggle,
       onMention,
       onOpen: () => {
@@ -204,7 +193,6 @@ export function useStatusHandlers({
     }),
     [
       handlerFactory,
-      onExpandedToggle,
       onFilterToggle,
       onMention,
       onOpenCallback,
