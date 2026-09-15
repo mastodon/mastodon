c9b78c464b99f6c0b6b28e2f6b174a9ef0faa55f
Echo <ChaosExAnima@users.noreply.github.com>
2026-09-15 16:33:01 +0000

    Redesign: Use server feature flag (#40531)

--- app/javascript/mastodon/utils/environment.ts
+++ app/javascript/mastodon/utils/environment.ts
@@ -12,7 +12,7 @@ export function isProduction() {
   else return import.meta.env.PROD;
 }
 
-export type ServerFeatures = 'fasp';
+export type ServerFeatures = 'fasp' | 'redesign';
 
 export function isServerFeatureEnabled(feature: ServerFeatures) {
   return initialState?.features.includes(feature) ?? false;
@@ -33,7 +33,9 @@ export function isClientFeatureEnabled(feature: ClientFeatures) {
 
 /* Checks if the 5.0 redesign features are enabled or not. */
 export function isRedesignEnabled() {
-  return isClientFeatureEnabled('redesign');
+  return (
+    isServerFeatureEnabled('redesign') || isClientFeatureEnabled('redesign')
+  );
 }
 
 export function isRedesignStatusEnabled() {
