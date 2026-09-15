e1adc7c1aff5e014275a946e7946cee2b2121b4e
diondiondion <mail@diondiondion.com>
2026-09-15 14:49:32 +0000

    Redesign: Implement new navigation in advanced (multi-column) layout (#40522)

--- app/javascript/mastodon/features/ui/components/columns_area/redesign.tsx
+++ app/javascript/mastodon/features/ui/components/columns_area/redesign.tsx
@@ -3,13 +3,13 @@ import classNames from 'classnames';
 import { ComposeRedesignButton } from '@/mastodon/features/compose/redesign/trigger';
 import { RedesignNavigationPanel } from '@/mastodon/features/navigation_panel/redesign';
 import { RedesignMobileNavigation } from '@/mastodon/features/navigation_panel/redesign/mobile_nav';
-import { useAppSelector } from '@/mastodon/store';
 import { Footer } from 'mastodon/features/custom_homepage/components/footer';
 
 import { useBreakpoint } from '../../hooks/useBreakpoint';
 
 import { MultiColumnContent } from './multi_column_content';
 import classes from './redesign.module.scss';
+import multiColClasses from './redesign_multicol.module.scss';
 
 export const ColumnsAreaRedesign: React.FC<{
   singleColumn?: boolean;
@@ -17,14 +17,11 @@ export const ColumnsAreaRedesign: React.FC<{
   children: React.ReactElement | React.ReactElement[];
   ref?: React.Ref<HTMLDivElement>;
 }> = ({ children, minimalShell, singleColumn, ref }) => {
-  const isModalOpen = useAppSelector(
-    (state) => !state.modal.get('stack').isEmpty(),
-  );
   const isMobile = useBreakpoint('openable');
 
   if (minimalShell) {
     return (
-      <div className={classNames(classes.root, classes.rootMinimal)}>
+      <div ref={ref} className={classNames(classes.root, classes.rootMinimal)}>
         {isMobile && <RedesignMobileNavigation />}
         <div className={classes.main}>
           <div>{children}</div>
@@ -37,7 +34,7 @@ export const ColumnsAreaRedesign: React.FC<{
 
   if (singleColumn) {
     return (
-      <div className={classes.root}>
+      <div ref={ref} className={classes.root}>
         <div className={classes.navigationWrapper}>
           <RedesignNavigationPanel />
         </div>
@@ -49,11 +46,11 @@ export const ColumnsAreaRedesign: React.FC<{
   }
 
   return (
-    <main
-      className={classNames('columns-area', { unscrollable: isModalOpen })}
-      ref={ref}
-      tabIndex={isModalOpen ? undefined : 0}
-    >
+    <main ref={ref} className={multiColClasses.root}>
+      <div className={multiColClasses.navigationWrapper}>
+        <RedesignNavigationPanel />
+      </div>
+      <ComposeRedesignButton />
       <MultiColumnContent>{children}</MultiColumnContent>
     </main>
   );
