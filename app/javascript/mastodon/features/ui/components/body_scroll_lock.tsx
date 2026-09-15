0b1833e24db4022a71a9d9fb34eb89be41a8f6e3
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-09-15 13:24:00 +0200

    prevents scroll in iOS

--- app/javascript/mastodon/features/ui/components/body_scroll_lock.tsx
+++ app/javascript/mastodon/features/ui/components/body_scroll_lock.tsx
@@ -1,14 +1,22 @@
 import { useLayoutEffect } from 'react';
 
+import { isRedesignEnabled } from '@/mastodon/utils/environment';
 import { createAppSelector, useAppSelector } from 'mastodon/store';
 
 const getShouldLockBodyScroll = createAppSelector(
   [
     (state) => state.navigation.open,
     (state) => state.modal.get('stack').size > 0,
+    (state) =>
+      isRedesignEnabled() &&
+      state.composer.displayState === 'showing' &&
+      state.meta.get('layout') === 'mobile',
   ],
-  (isMobileMenuOpen: boolean, isModalOpen: boolean) =>
-    isMobileMenuOpen || isModalOpen,
+  (
+    isMobileMenuOpen: boolean,
+    isModalOpen: boolean,
+    isRedesignComposerOpen: boolean,
+  ) => isMobileMenuOpen || isModalOpen || isRedesignComposerOpen,
 );
 
 /**
