e9b728e0569b5148930f36caa2863fd23c785a47
diondiondion <mail@diondiondion.com>
2026-09-15 16:26:30 +0000

    Redesign: Never show "Getting started" column in advanced UI (#40529)

--- app/javascript/mastodon/features/ui/index.jsx
+++ app/javascript/mastodon/features/ui/index.jsx
@@ -163,14 +163,16 @@ class SwitchingColumnsArea extends PureComponent {
     const { signedIn } = this.props.identity;
     const pathName = this.props.location.pathname;
 
+    const defaultHomepage = (singleColumn || isRedesignEnabled())
+      ? '/home'
+      : '/deck/getting-started';
+
     let rootRedirect;
     if (signedIn) {
       if (forceOnboarding) {
         rootRedirect = '/start';
-      } else if (singleColumn) {
-        rootRedirect = '/home';
       } else {
-        rootRedirect = '/deck/getting-started';
+        rootRedirect = defaultHomepage;
       }
     } else if (singleUserMode && owner && initialState?.accounts[owner]) {
       rootRedirect = `/@${initialState.accounts[owner].username}`;
@@ -184,17 +186,30 @@ class SwitchingColumnsArea extends PureComponent {
       rootRedirect = '/about';
     }
 
+    // Turns a pathname into a location object with a flag to disable the
+    // automatic focusing of the page that happens for user-initiated navigations
+    const redirectWithoutFocusing = (pathname) => ({
+      pathname,
+      state: {
+        ...this.props.location.state,
+        focusTarget: false,
+      }
+    });
+
     return (
       <ColumnsContextProvider multiColumn={!singleColumn}>
         <ColumnsArea ref={this.setRef} singleColumn={singleColumn} domain={domain} minimalShell={minimalShell}>
           <WrappedSwitch>
-            <Redirect from='/' to={{pathname: rootRedirect, state: {...this.props.location.state, focusTarget: false}}} exact />
+            <Redirect from='/' to={redirectWithoutFocusing(rootRedirect)} exact />
 
-            {forceSingleColumn || transientSingleColumn ? <Redirect from='/deck' to='/home' exact /> : null}
+            {(forceSingleColumn || transientSingleColumn) ? <Redirect from='/deck' to={redirectWithoutFocusing('/home')} exact /> : null}
             {(forceSingleColumn || transientSingleColumn) && pathName.startsWith('/deck/') ? <Redirect from={pathName} to={{...this.props.location, pathname: pathName.slice(5)}} /> : null}
             {/* Redirect old bookmarks (without /deck) with home-like routes to the advanced interface */}
-            {!singleColumn && pathName === '/home' ? <Redirect from='/home' to='/deck/getting-started' exact /> : null}
-            {pathName === '/getting-started' ? <Redirect from='/getting-started' to={singleColumn ? '/home' : '/deck/getting-started'} exact /> : null}
+            {!singleColumn && pathName === '/home' ? <Redirect from='/home' to={redirectWithoutFocusing(defaultHomepage)} exact /> : null}
+            {(pathName === '/getting-started' || isRedesignEnabled())
+              ? <Redirect from='/getting-started' to={redirectWithoutFocusing(defaultHomepage)} exact />
+              : null
+            }
 
             <WrappedRoute path='/getting-started' component={GettingStarted} content={children} />
             <WrappedRoute path='/keyboard-shortcuts' component={KeyboardShortcuts} content={children} />
@@ -203,8 +218,8 @@ class SwitchingColumnsArea extends PureComponent {
             <WrappedRoute path='/terms-of-service/:date?' component={TermsOfService} content={children} />
 
             <WrappedRoute path={['/home', '/timelines/home']} component={HomeTimeline} content={children} />
-            <Redirect from='/timelines/public' to='/public' exact />
-            <Redirect from='/timelines/public/local' to='/public/local' exact />
+            <Redirect from='/timelines/public' to={redirectWithoutFocusing('/public')} exact />
+            <Redirect from='/timelines/public/local' to={redirectWithoutFocusing('/public/local')} exact />
             <WrappedRoute path='/public' exact component={Firehose} componentParams={{ feedType: 'public' }} content={children} />
             <WrappedRoute path='/public/local' exact component={Firehose} componentParams={{ feedType: 'community' }} content={children} />
             <WrappedRoute path='/public/remote' exact component={Firehose} componentParams={{ feedType: 'public:remote' }} content={children} />
