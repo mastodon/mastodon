75b935fac9b374b3ed93406102fe9313f1eeb193
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-09-16 18:33:58 +0200

    StatusContainer

--- app/javascript/mastodon/components/status/types.ts
+++ app/javascript/mastodon/components/status/types.ts
@@ -1,6 +1,6 @@
 import type { ComponentType, ReactNode } from 'react';
 
-import StatusContainer from '@/mastodon/containers/status_container';
+import StatusContainer from '@/mastodon/components/status/legacy/container';
 import type { Account as TAccount } from '@/mastodon/models/account';
 import type { Status as TStatus } from '@/mastodon/models/status';
 
