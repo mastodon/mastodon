c30230fe8857f2cb8f382347219530ae70c2fe64
Claire <claire.github-309c@sitedethib.com>
2026-09-22 09:04:55 +0000

    Fix deleted-because-of-suspension accounts being marked as manually deleted (#40645)

--- app/workers/admin/account_deletion_worker.rb
+++ app/workers/admin/account_deletion_worker.rb
@@ -6,10 +6,10 @@ class Admin::AccountDeletionWorker
   sidekiq_options queue: 'pull', lock: :until_executed, lock_ttl: 1.week.to_i
 
   def perform(account_id)
-    delete_account = Account.find(account_id)
-    return unless delete_account.unavailable?
+    account = Account.find(account_id)
+    return unless account.unavailable?
 
-    DeleteAccountService.new.call(delete_account, reserve_username: true, reserve_email: true)
+    DeleteAccountService.new.call(account, reserve_username: true, reserve_email: true, suspended_at: account.suspended_at)
   rescue ActiveRecord::RecordNotFound
     true
   end
