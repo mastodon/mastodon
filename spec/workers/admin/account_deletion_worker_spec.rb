79206902e99a0039abd4c5e86aa8e86be6732bc6
Claire <claire.github-309c@sitedethib.com>
2026-09-22 09:04:55 +0000

    Fix deleted-because-of-suspension accounts being marked as manually deleted (#40645)

--- spec/workers/admin/account_deletion_worker_spec.rb
+++ spec/workers/admin/account_deletion_worker_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Admin::AccountDeletionWorker do
       allow(DeleteAccountService).to receive(:new).and_return(service)
       worker.perform(account.id)
 
-      expect(service).to have_received(:call).with(account, { reserve_email: true, reserve_username: true })
+      expect(service).to have_received(:call).with(account, { reserve_email: true, reserve_username: true, suspended_at: account.reload.suspended_at })
     end
   end
 end
